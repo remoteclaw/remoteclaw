d19774f7761049c2b204f2662407c99e7f6168dc
Kunal Karmakar <kkdthunlshd@gmail.com>
2026-02-28 06:01:51 +0000

    Remove temperature
    (cherry picked from commit 720e1479b8796ce8e73af79bf43687a9633fb4f0)

--- src/commands/onboard-custom.ts
+++ src/commands/onboard-custom.ts
@@ -318,7 +318,6 @@ async function requestOpenAiVerification(params: {
       headers,
       body: {
         messages: [{ role: "user", content: "Hi" }],
-        temperature: 1,
         max_completion_tokens: 5,
         stream: false,
       },
@@ -330,7 +329,6 @@ async function requestOpenAiVerification(params: {
       body: {
         model: params.modelId,
         messages: [{ role: "user", content: "Hi" }],
-        temperature: 1,
         max_tokens: 1,
         stream: false,
       },

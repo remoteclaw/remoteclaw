kbarbel640-del/openclaw

feat(node): add device diagnostics and notification action commands

apps/android/app/src/main/java/ai/openclaw/android/node/InvokeCommandRegistry.kt
@@ -88,7 +88,16 @@ object InvokeCommandRegistry {
         name = OpenClawDeviceCommand.Info.rawValue,
       ),
       InvokeCommandSpec(
+        name = OpenClawDeviceCommand.Permissions.rawValue,
+      ),
+      InvokeCommandSpec(
+        name = OpenClawDeviceCommand.Health.rawValue,
+      ),
+      InvokeCommandSpec(
         name = OpenClawNotificationsCommand.List.rawValue,
+      ),
+      InvokeCommandSpec(
+        name = OpenClawNotificationsCommand.Actions.rawValue,
       ),
       InvokeCommandSpec(
         name = OpenClawSmsCommand.Send.rawValue,

apps/android/app/src/test/java/ai/openclaw/android/node/InvokeCommandRegistryTest.kt
@@ -26,7 +26,10 @@ class InvokeCommandRegistryTest {
     assertFalse(commands.contains(OpenClawLocationCommand.Get.rawValue))
     assertTrue(commands.contains(OpenClawDeviceCommand.Status.rawValue))
     assertTrue(commands.contains(OpenClawDeviceCommand.Info.rawValue))
+    assertTrue(commands.contains(OpenClawDeviceCommand.Permissions.rawValue))
+    assertTrue(commands.contains(OpenClawDeviceCommand.Health.rawValue))
     assertTrue(commands.contains(OpenClawNotificationsCommand.List.rawValue))
+    assertTrue(commands.contains(OpenClawNotificationsCommand.Actions.rawValue))
     assertFalse(commands.contains(OpenClawSmsCommand.Send.rawValue))
     assertFalse(commands.contains("debug.logs"))
     assertFalse(commands.contains("debug.ed25519"))
@@ -49,7 +52,10 @@ class InvokeCommandRegistryTest {
     assertTrue(commands.contains(OpenClawLocationCommand.Get.rawValue))
     assertTrue(commands.contains(OpenClawDeviceCommand.Status.rawValue))
     assertTrue(commands.contains(OpenClawDeviceCommand.Info.rawValue))
+    assertTrue(commands.contains(OpenClawDeviceCommand.Permissions.rawValue))
+    assertTrue(commands.contains(OpenClawDeviceCommand.Health.rawValue))
     assertTrue(commands.contains(OpenClawNotificationsCommand.List.rawValue))
+    assertTrue(commands.contains(OpenClawNotificationsCommand.Actions.rawValue))
     assertTrue(commands.contains(OpenClawSmsCommand.Send.rawValue))
     assertTrue(commands.contains("debug.logs"))
     assertTrue(commands.contains("debug.ed25519"))

remoteclaw/remoteclaw

(cherry picked from commit e99b323a6b406fd32d15fe45d3bd6bdf9ee9d609)

apps/android/app/src/main/java/org/remoteclaw/android/node/InvokeCommandRegistry.kt
@@ -3,6 +3,7 @@ package org.remoteclaw.android.node
 import org.remoteclaw.android.protocol.RemoteClawCanvasA2UICommand
 import org.remoteclaw.android.protocol.RemoteClawCanvasCommand
 import org.remoteclaw.android.protocol.RemoteClawCameraCommand
+import org.remoteclaw.android.protocol.RemoteClawDeviceCommand
 import org.remoteclaw.android.protocol.RemoteClawLocationCommand
 import org.remoteclaw.android.protocol.RemoteClawNotificationsCommand
 import org.remoteclaw.android.protocol.RemoteClawScreenCommand
@@ -76,7 +77,16 @@ object InvokeCommandRegistry {
         availability = InvokeCommandAvailability.LocationEnabled,
       ),
       InvokeCommandSpec(
+        name = RemoteClawDeviceCommand.Permissions.rawValue,
+      ),
+      InvokeCommandSpec(
+        name = RemoteClawDeviceCommand.Health.rawValue,
+      ),
+      InvokeCommandSpec(
         name = RemoteClawNotificationsCommand.List.rawValue,
+      ),
+      InvokeCommandSpec(
+        name = RemoteClawNotificationsCommand.Actions.rawValue,
       ),
       InvokeCommandSpec(
         name = RemoteClawSmsCommand.Send.rawValue,

apps/android/app/src/test/java/org/remoteclaw/android/node/InvokeCommandRegistryTest.kt
@@ -1,6 +1,7 @@
 package org.remoteclaw.android.node
 
 import org.remoteclaw.android.protocol.RemoteClawCameraCommand
+import org.remoteclaw.android.protocol.RemoteClawDeviceCommand
 import org.remoteclaw.android.protocol.RemoteClawLocationCommand
 import org.remoteclaw.android.protocol.RemoteClawNotificationsCommand
 import org.remoteclaw.android.protocol.RemoteClawSmsCommand
@@ -22,7 +23,10 @@ class InvokeCommandRegistryTest {
     assertFalse(commands.contains(RemoteClawCameraCommand.Snap.rawValue))
     assertFalse(commands.contains(RemoteClawCameraCommand.Clip.rawValue))
     assertFalse(commands.contains(RemoteClawLocationCommand.Get.rawValue))
+    assertTrue(commands.contains(RemoteClawDeviceCommand.Permissions.rawValue))
+    assertTrue(commands.contains(RemoteClawDeviceCommand.Health.rawValue))
     assertTrue(commands.contains(RemoteClawNotificationsCommand.List.rawValue))
+    assertTrue(commands.contains(RemoteClawNotificationsCommand.Actions.rawValue))
     assertFalse(commands.contains(RemoteClawSmsCommand.Send.rawValue))
     assertFalse(commands.contains("debug.logs"))
     assertFalse(commands.contains("debug.ed25519"))
@@ -42,7 +46,10 @@ class InvokeCommandRegistryTest {
     assertTrue(commands.contains(RemoteClawCameraCommand.Snap.rawValue))
     assertTrue(commands.contains(RemoteClawCameraCommand.Clip.rawValue))
     assertTrue(commands.contains(RemoteClawLocationCommand.Get.rawValue))
+    assertTrue(commands.contains(RemoteClawDeviceCommand.Permissions.rawValue))
+    assertTrue(commands.contains(RemoteClawDeviceCommand.Health.rawValue))
     assertTrue(commands.contains(RemoteClawNotificationsCommand.List.rawValue))
+    assertTrue(commands.contains(RemoteClawNotificationsCommand.Actions.rawValue))
     assertTrue(commands.contains(RemoteClawSmsCommand.Send.rawValue))
     assertTrue(commands.contains("debug.logs"))
     assertTrue(commands.contains("debug.ed25519"))
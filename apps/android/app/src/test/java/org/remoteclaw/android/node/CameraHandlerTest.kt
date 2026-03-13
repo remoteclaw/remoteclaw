ac1982/openclaw

test(android): cover camera clip upload URL JSON parsing

apps/android/app/src/test/java/ai/openclaw/android/node/CameraHandlerTest.kt
@@ -1,0 +1,31 @@
+package ai.openclaw.android.node
+
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
+import org.junit.Test
+
+class CameraHandlerTest {
+  @Test
+  fun parseCameraClipUploadUrl_returnsUrlForValidPayload() {
+    val actual = parseCameraClipUploadUrl("""{"url":"https://example.com/upload/clip.mp4"}""")
+
+    assertEquals("https://example.com/upload/clip.mp4", actual)
+  }
+
+  @Test
+  fun parseCameraClipUploadUrl_trimsUrlWhitespace() {
+    val actual = parseCameraClipUploadUrl("""{"url":"  https://example.com/u.mp4  "}""")
+
+    assertEquals("https://example.com/u.mp4", actual)
+  }
+
+  @Test
+  fun parseCameraClipUploadUrl_returnsNullForMalformedPayloads() {
+    assertNull(parseCameraClipUploadUrl(""))
+    assertNull(parseCameraClipUploadUrl("not-json"))
+    assertNull(parseCameraClipUploadUrl("""{"ok":true}"""))
+    assertNull(parseCameraClipUploadUrl("""{"url":123}"""))
+    assertNull(parseCameraClipUploadUrl("""{"url":"   "}"""))
+  }
+}
+

remoteclaw/remoteclaw

(cherry picked from commit 120a7abbabc9df3d5665f39a2eda083818cd7fd3)

apps/android/app/src/test/java/org/remoteclaw/android/node/CameraHandlerTest.kt
@@ -1,0 +1,31 @@
+package org.remoteclaw.android.node
+
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
+import org.junit.Test
+
+class CameraHandlerTest {
+  @Test
+  fun parseCameraClipUploadUrl_returnsUrlForValidPayload() {
+    val actual = parseCameraClipUploadUrl("""{"url":"https://example.com/upload/clip.mp4"}""")
+
+    assertEquals("https://example.com/upload/clip.mp4", actual)
+  }
+
+  @Test
+  fun parseCameraClipUploadUrl_trimsUrlWhitespace() {
+    val actual = parseCameraClipUploadUrl("""{"url":"  https://example.com/u.mp4  "}""")
+
+    assertEquals("https://example.com/u.mp4", actual)
+  }
+
+  @Test
+  fun parseCameraClipUploadUrl_returnsNullForMalformedPayloads() {
+    assertNull(parseCameraClipUploadUrl(""))
+    assertNull(parseCameraClipUploadUrl("not-json"))
+    assertNull(parseCameraClipUploadUrl("""{"ok":true}"""))
+    assertNull(parseCameraClipUploadUrl("""{"url":123}"""))
+    assertNull(parseCameraClipUploadUrl("""{"url":"   "}"""))
+  }
+}
+
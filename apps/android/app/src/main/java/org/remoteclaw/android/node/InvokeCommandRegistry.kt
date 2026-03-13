kbarbel640-del/openclaw

feat(android): add camera list and device selection

apps/android/app/src/main/java/ai/openclaw/android/node/InvokeCommandRegistry.kt
@@ -63,6 +63,11 @@ object InvokeCommandRegistry {
         requiresForeground = true,
       ),
       InvokeCommandSpec(
+        name = OpenClawCameraCommand.List.rawValue,
+        requiresForeground = true,
+        availability = InvokeCommandAvailability.CameraEnabled,
+      ),
+      InvokeCommandSpec(
         name = OpenClawCameraCommand.Snap.rawValue,
         requiresForeground = true,
         availability = InvokeCommandAvailability.CameraEnabled,

apps/android/app/src/main/java/ai/openclaw/android/node/InvokeDispatcher.kt
@@ -112,6 +112,7 @@ class InvokeDispatcher(
       }
 
       // Camera commands
+      OpenClawCameraCommand.List.rawValue -> cameraHandler.handleList(paramsJson)
       OpenClawCameraCommand.Snap.rawValue -> cameraHandler.handleSnap(paramsJson)
       OpenClawCameraCommand.Clip.rawValue -> cameraHandler.handleClip(paramsJson)
 

apps/android/app/src/test/java/ai/openclaw/android/node/InvokeCommandRegistryTest.kt
@@ -22,6 +22,7 @@ class InvokeCommandRegistryTest {
 
     assertFalse(commands.contains(OpenClawCameraCommand.Snap.rawValue))
     assertFalse(commands.contains(OpenClawCameraCommand.Clip.rawValue))
+    assertFalse(commands.contains(OpenClawCameraCommand.List.rawValue))
     assertFalse(commands.contains(OpenClawLocationCommand.Get.rawValue))
     assertTrue(commands.contains(OpenClawDeviceCommand.Status.rawValue))
     assertTrue(commands.contains(OpenClawDeviceCommand.Info.rawValue))
@@ -44,6 +45,7 @@ class InvokeCommandRegistryTest {
 
     assertTrue(commands.contains(OpenClawCameraCommand.Snap.rawValue))
     assertTrue(commands.contains(OpenClawCameraCommand.Clip.rawValue))
+    assertTrue(commands.contains(OpenClawCameraCommand.List.rawValue))
     assertTrue(commands.contains(OpenClawLocationCommand.Get.rawValue))
     assertTrue(commands.contains(OpenClawDeviceCommand.Status.rawValue))
     assertTrue(commands.contains(OpenClawDeviceCommand.Info.rawValue))

remoteclaw/remoteclaw

(cherry picked from commit 1f7b3c613d93b219635f3d57bb9cfd88d8fd6883)

apps/android/app/src/main/java/org/remoteclaw/android/node/InvokeCommandRegistry.kt
@@ -63,6 +63,11 @@ object InvokeCommandRegistry {
         requiresForeground = true,
       ),
       InvokeCommandSpec(
+        name = RemoteClawCameraCommand.List.rawValue,
+        requiresForeground = true,
+        availability = InvokeCommandAvailability.CameraEnabled,
+      ),
+      InvokeCommandSpec(
         name = RemoteClawCameraCommand.Snap.rawValue,
         requiresForeground = true,
         availability = InvokeCommandAvailability.CameraEnabled,

apps/android/app/src/main/java/org/remoteclaw/android/node/InvokeDispatcher.kt
@@ -110,6 +110,7 @@ class InvokeDispatcher(
       }
 
       // Camera commands
+      RemoteClawCameraCommand.List.rawValue -> cameraHandler.handleList(paramsJson)
       RemoteClawCameraCommand.Snap.rawValue -> cameraHandler.handleSnap(paramsJson)
       RemoteClawCameraCommand.Clip.rawValue -> cameraHandler.handleClip(paramsJson)
 

apps/android/app/src/test/java/org/remoteclaw/android/node/InvokeCommandRegistryTest.kt
@@ -22,6 +22,7 @@ class InvokeCommandRegistryTest {
 
     assertFalse(commands.contains(RemoteClawCameraCommand.Snap.rawValue))
     assertFalse(commands.contains(RemoteClawCameraCommand.Clip.rawValue))
+    assertFalse(commands.contains(RemoteClawCameraCommand.List.rawValue))
     assertFalse(commands.contains(RemoteClawLocationCommand.Get.rawValue))
     assertTrue(commands.contains(RemoteClawDeviceCommand.Permissions.rawValue))
     assertTrue(commands.contains(RemoteClawDeviceCommand.Health.rawValue))
@@ -45,6 +46,7 @@ class InvokeCommandRegistryTest {
 
     assertTrue(commands.contains(RemoteClawCameraCommand.Snap.rawValue))
     assertTrue(commands.contains(RemoteClawCameraCommand.Clip.rawValue))
+    assertTrue(commands.contains(RemoteClawCameraCommand.List.rawValue))
     assertTrue(commands.contains(RemoteClawLocationCommand.Get.rawValue))
     assertTrue(commands.contains(RemoteClawDeviceCommand.Permissions.rawValue))
     assertTrue(commands.contains(RemoteClawDeviceCommand.Health.rawValue))
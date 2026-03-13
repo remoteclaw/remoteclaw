nigoshu-roby/openclaw-roby

fix(cli): accept node24 executable names in argv reparse

src/cli/argv.test.ts
@@ -205,6 +205,18 @@ describe("argv helpers", () => {
         expected: ["/usr/bin/node-22.2.0", "openclaw", "status"],
       },
       {
+        rawArgs: ["node24", "openclaw", "status"],
+        expected: ["node24", "openclaw", "status"],
+      },
+      {
+        rawArgs: ["/usr/bin/node24", "openclaw", "status"],
+        expected: ["/usr/bin/node24", "openclaw", "status"],
+      },
+      {
+        rawArgs: ["node24.exe", "openclaw", "status"],
+        expected: ["node24.exe", "openclaw", "status"],
+      },
+      {
         rawArgs: ["nodejs", "openclaw", "status"],
         expected: ["nodejs", "openclaw", "status"],
       },

remoteclaw/remoteclaw

(cherry picked from commit ca2ae342db478d8a0df5f81fe3224fbbb68ab744)

src/cli/argv.test.ts
@@ -205,6 +205,18 @@ describe("argv helpers", () => {
         expected: ["/usr/bin/node-22.2.0", "remoteclaw", "status"],
       },
       {
+        rawArgs: ["node24", "remoteclaw", "status"],
+        expected: ["node24", "remoteclaw", "status"],
+      },
+      {
+        rawArgs: ["/usr/bin/node24", "remoteclaw", "status"],
+        expected: ["/usr/bin/node24", "remoteclaw", "status"],
+      },
+      {
+        rawArgs: ["node24.exe", "remoteclaw", "status"],
+        expected: ["node24.exe", "remoteclaw", "status"],
+      },
+      {
         rawArgs: ["nodejs", "remoteclaw", "status"],
         expected: ["nodejs", "remoteclaw", "status"],
       },
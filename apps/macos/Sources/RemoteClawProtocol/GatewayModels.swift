f3d55aa54c35e41396b56b08aa9d497fc4f668e3
Alexey Pelykh <alexey.pelykh@gmail.com>
2026-03-14 22:29:22 +0100

    Protocol: regenerate Swift models for systemRunPlanV2 (#1349)
    (cherry picked from commit a29b18c0031991587d29bffc9da1e61794cbb536)
    
    Co-authored-by: Philipp Spiess <hello@philippspiess.com>

--- apps/macos/Sources/RemoteClawProtocol/GatewayModels.swift
+++ apps/macos/Sources/RemoteClawProtocol/GatewayModels.swift
@@ -2796,6 +2796,7 @@ public struct ExecApprovalRequestParams: Codable, Sendable {
     public let id: String?
     public let command: String
     public let commandargv: [String]?
+    public let systemrunplanv2: [String: AnyCodable]?
     public let env: [String: AnyCodable]?
     public let cwd: AnyCodable?
     public let nodeid: AnyCodable?
@@ -2816,6 +2817,7 @@ public struct ExecApprovalRequestParams: Codable, Sendable {
         id: String?,
         command: String,
         commandargv: [String]?,
+        systemrunplanv2: [String: AnyCodable]?,
         env: [String: AnyCodable]?,
         cwd: AnyCodable?,
         nodeid: AnyCodable?,
@@ -2835,6 +2837,7 @@ public struct ExecApprovalRequestParams: Codable, Sendable {
         self.id = id
         self.command = command
         self.commandargv = commandargv
+        self.systemrunplanv2 = systemrunplanv2
         self.env = env
         self.cwd = cwd
         self.nodeid = nodeid
@@ -2856,6 +2859,7 @@ public struct ExecApprovalRequestParams: Codable, Sendable {
         case id
         case command
         case commandargv = "commandArgv"
+        case systemrunplanv2 = "systemRunPlanV2"
         case env
         case cwd
         case nodeid = "nodeId"

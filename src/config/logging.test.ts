wonglaitung/openclaw_mini

fix: resolve live config paths in status and gateway metadata (#39952)
* fix: resolve live config paths in status and gateway metadata

* fix: resolve remaining runtime config path references

* test: cover gateway config.set config path response

src/config/logging.test.ts
@@ -1,0 +1,25 @@
+import { describe, expect, it, vi } from "vitest";
+
+const mocks = vi.hoisted(() => ({
+  createConfigIO: vi.fn().mockReturnValue({
+    configPath: "/tmp/openclaw-dev/openclaw.json",
+  }),
+}));
+
+vi.mock("./io.js", () => ({
+  createConfigIO: mocks.createConfigIO,
+}));
+
+import { formatConfigPath, logConfigUpdated } from "./logging.js";
+
+describe("config logging", () => {
+  it("formats the live config path when no explicit path is provided", () => {
+    expect(formatConfigPath()).toBe("/tmp/openclaw-dev/openclaw.json");
+  });
+
+  it("logs the live config path when no explicit path is provided", () => {
+    const runtime = { log: vi.fn() };
+    logConfigUpdated(runtime as never);
+    expect(runtime.log).toHaveBeenCalledWith("Updated /tmp/openclaw-dev/openclaw.json");
+  });
+});

remoteclaw/remoteclaw

(cherry picked from commit d9e8e8ac159bdc5d049a210396e5317a31da7486)

src/config/logging.test.ts
@@ -1,0 +1,25 @@
+import { describe, expect, it, vi } from "vitest";
+
+const mocks = vi.hoisted(() => ({
+  createConfigIO: vi.fn().mockReturnValue({
+    configPath: "/tmp/remoteclaw-dev/remoteclaw.json",
+  }),
+}));
+
+vi.mock("./io.js", () => ({
+  createConfigIO: mocks.createConfigIO,
+}));
+
+import { formatConfigPath, logConfigUpdated } from "./logging.js";
+
+describe("config logging", () => {
+  it("formats the live config path when no explicit path is provided", () => {
+    expect(formatConfigPath()).toBe("/tmp/remoteclaw-dev/remoteclaw.json");
+  });
+
+  it("logs the live config path when no explicit path is provided", () => {
+    const runtime = { log: vi.fn() };
+    logConfigUpdated(runtime as never);
+    expect(runtime.log).toHaveBeenCalledWith("Updated /tmp/remoteclaw-dev/remoteclaw.json");
+  });
+});
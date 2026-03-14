nigoshu-roby/openclaw-roby

Docker: replace npm link with root CLI symlink (#28312)

Dockerfile
@@ -50,7 +50,11 @@ RUN pnpm build
 # Force pnpm for UI build (Bun may fail on ARM/Synology architectures)
 ENV OPENCLAW_PREFER_PNPM=1
 RUN pnpm ui:build
-RUN npm link
+
+# Expose the CLI binary without requiring npm global writes as non-root.
+USER root
+RUN ln -sf /app/openclaw.mjs /usr/local/bin/openclaw \
+ && chmod 755 /app/openclaw.mjs
 
 ENV NODE_ENV=production
 

remoteclaw/remoteclaw

(cherry picked from commit 22ad7523f19a31271cc45998c4987e0d17659839)

Dockerfile
@@ -51,7 +51,11 @@ RUN pnpm build
 # Force pnpm for UI build (Bun may fail on ARM/Synology architectures)
 ENV REMOTECLAW_PREFER_PNPM=1
 RUN pnpm ui:build
-RUN npm link
+
+# Expose the CLI binary without requiring npm global writes as non-root.
+USER root
+RUN ln -sf /app/remoteclaw.mjs /usr/local/bin/remoteclaw \
+ && chmod 755 /app/remoteclaw.mjs
 
 ENV NODE_ENV=production
 
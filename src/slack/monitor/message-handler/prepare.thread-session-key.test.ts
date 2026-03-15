wonglaitung/openclaw_mini

fix(slack): use thread-level sessions for channels to prevent context mixing (#10686)
* fix(slack): use thread-level sessions for channels to prevent context mixing

All messages in a Slack channel share a single session, causing context from
different threads to mix together. When users have multiple conversations in
different threads of the same channel, the agent sees combined context from
all threads, leading to confused responses.

Session key was: `slack:channel:${channelId}` (no thread identifier)

1. **Thread-level session keys**: Each message in channels/groups now gets
   its own session based on thread_ts:
   - Thread replies: use the parent thread's ts
   - New messages: use the message's own ts (becomes thread root)
   - DMs: unchanged (no thread-level sessions needed)

   New session key format: `slack:channel:${channelId}:thread:${threadTs}`

2. **Increased thread cache TTL**: Changed from 60 seconds to 6 hours.
   Users often pause conversations, and the short TTL caused unnecessary
   API calls and thread resolution failures.

3. **Increased cache size**: Changed from 500 to 10,000 entries to support
   busy workspaces with many active threads.

1. Create two threads in the same Slack channel
2. In Thread A: tell the bot your name is "Alice" and ask about "billing"
3. In Thread B: tell the bot your name is "Bob" and ask about "API"
4. Reply in Thread A and ask "what's my name?" - should say "Alice"
5. Check sessions: each thread should have a unique session key with :thread: suffix

Fixes context bleed issues related to #758

* fix(slack): also update resolveSlackSystemEventSessionKey for thread-level sessions

The context.ts file has a separate function for resolving session keys for
system events (reactions, file uploads, etc.). This also needs to support
thread-level sessions to ensure all Slack events route to the correct
thread-specific session.

Added threadTs and messageTs parameters to resolveSlackSystemEventSessionKey
and updated the implem

src/slack/monitor/message-handler/prepare.thread-session-key.test.ts
@@ -1,0 +1,142 @@
+import type { App } from "@slack/bolt";
+import { describe, expect, it } from "vitest";
+import type { OpenClawConfig } from "../../../config/config.js";
+import type { RuntimeEnv } from "../../../runtime.js";
+import type { ResolvedSlackAccount } from "../../accounts.js";
+import type { SlackMessageEvent } from "../../types.js";
+import { createSlackMonitorContext } from "../context.js";
+import { prepareSlackMessage } from "./prepare.js";
+
+function buildCtx(overrides?: { replyToMode?: "all" | "first" | "off" }) {
+  return createSlackMonitorContext({
+    cfg: {
+      channels: {
+        slack: { enabled: true, replyToMode: overrides?.replyToMode ?? "all" },
+      },
+    } as OpenClawConfig,
+    accountId: "default",
+    botToken: "token",
+    app: { client: {} } as App,
+    runtime: {} as RuntimeEnv,
+    botUserId: "B1",
+    teamId: "T1",
+    apiAppId: "A1",
+    historyLimit: 0,
+    sessionScope: "per-sender",
+    mainKey: "main",
+    dmEnabled: true,
+    dmPolicy: "open",
+    allowFrom: [],
+    groupDmEnabled: true,
+    groupDmChannels: [],
+    defaultRequireMention: false,
+    groupPolicy: "open",
+    allowNameMatching: false,
+    useAccessGroups: false,
+    reactionMode: "off",
+    reactionAllowlist: [],
+    replyToMode: overrides?.replyToMode ?? "all",
+    threadHistoryScope: "thread",
+    threadInheritParent: false,
+    slashCommand: {
+      enabled: false,
+      name: "openclaw",
+      sessionPrefix: "slack:slash",
+      ephemeral: true,
+    },
+    textLimit: 4000,
+    ackReactionScope: "group-mentions",
+    mediaMaxBytes: 1024,
+    removeAckAfterReply: false,
+  });
+}
+
+const account: ResolvedSlackAccount = {
+  accountId: "default",
+  enabled: true,
+  botTokenSource: "config",
+  appTokenSource: "config",
+  userTokenSource: "none",
+  config: {},
+};
+
+describe("thread-level session keys", () => {
+  it("uses thread-level session key for channel messages", async () => {
+    const ctx = buildCtx();
+    ctx.resolveUserName = async () => ({ name: "Alice" });
+
+    const message: SlackMessageEvent = {
+      channel: "C123",
+      channel_type: "channel",
+      user: "U1",
+      text: "hello",
+      ts: "1770408518.451689",
+    } as SlackMessageEvent;
+
+    const prepared = await prepareSlackMessage({
+      ctx,
+      account,
+      message,
+      opts: { source: "message" },
+    });
+
+    expect(prepared).toBeTruthy();
+    // Channel messages should get thread-level session key with :thread: suffix
+    // The resolved session key is in ctxPayload.SessionKey, not route.sessionKey
+    const sessionKey = prepared!.ctxPayload.SessionKey as string;
+    expect(sessionKey).toContain(":thread:");
+    expect(sessionKey).toContain("1770408518.451689");
+  });
+
+  it("uses parent thread_ts for thread replies", async () => {
+    const ctx = buildCtx();
+    ctx.resolveUserName = async () => ({ name: "Bob" });
+
+    const message: SlackMessageEvent = {
+      channel: "C123",
+      channel_type: "channel",
+      user: "U2",
+      text: "reply",
+      ts: "1770408522.168859",
+      thread_ts: "1770408518.451689",
+    } as SlackMessageEvent;
+
+    const prepared = await prepareSlackMessage({
+      ctx,
+      account,
+      message,
+      opts: { source: "message" },
+    });
+
+    expect(prepared).toBeTruthy();
+    // Thread replies should use the parent thread_ts, not the reply ts
+    const sessionKey = prepared!.ctxPayload.SessionKey as string;
+    expect(sessionKey).toContain(":thread:1770408518.451689");
+    expect(sessionKey).not.toContain("1770408522.168859");
+  });
+
+  it("does not add thread suffix for DMs", async () => {
+    const ctx = buildCtx();
+    ctx.resolveUserName = async () => ({ name: "Carol" });
+
+    const message: SlackMessageEvent = {
+      channel: "D456",
+      channel_type: "im",
+      user: "U3",
+      text: "dm message",
+      ts: "1770408530.000000",
+    } as SlackMessageEvent;
+
+    const prepared = await prepareSlackMessage({
+      ctx,
+      account,
+      message,
+      opts: { source: "message" },
+    });
+
+    expect(prepared).toBeTruthy();
+    // DMs should NOT have :thread: in the session key
+    const sessionKey = prepared!.ctxPayload.SessionKey as string;
+    expect(sessionKey).not.toContain(":thread:");
+  });
+});

remoteclaw/remoteclaw

(cherry picked from commit 11d34700c0fe828c91e393ea5a9e877d96619e98)

src/slack/monitor/message-handler/prepare.thread-session-key.test.ts
@@ -1,0 +1,142 @@
+import type { App } from "@slack/bolt";
+import { describe, expect, it } from "vitest";
+import type { RemoteClawConfig } from "../../../config/config.js";
+import type { RuntimeEnv } from "../../../runtime.js";
+import type { ResolvedSlackAccount } from "../../accounts.js";
+import type { SlackMessageEvent } from "../../types.js";
+import { createSlackMonitorContext } from "../context.js";
+import { prepareSlackMessage } from "./prepare.js";
+
+function buildCtx(overrides?: { replyToMode?: "all" | "first" | "off" }) {
+  return createSlackMonitorContext({
+    cfg: {
+      channels: {
+        slack: { enabled: true, replyToMode: overrides?.replyToMode ?? "all" },
+      },
+    } as RemoteClawConfig,
+    accountId: "default",
+    botToken: "token",
+    app: { client: {} } as App,
+    runtime: {} as RuntimeEnv,
+    botUserId: "B1",
+    teamId: "T1",
+    apiAppId: "A1",
+    historyLimit: 0,
+    sessionScope: "per-sender",
+    mainKey: "main",
+    dmEnabled: true,
+    dmPolicy: "open",
+    allowFrom: [],
+    groupDmEnabled: true,
+    groupDmChannels: [],
+    defaultRequireMention: false,
+    groupPolicy: "open",
+    allowNameMatching: false,
+    useAccessGroups: false,
+    reactionMode: "off",
+    reactionAllowlist: [],
+    replyToMode: overrides?.replyToMode ?? "all",
+    threadHistoryScope: "thread",
+    threadInheritParent: false,
+    slashCommand: {
+      enabled: false,
+      name: "remoteclaw",
+      sessionPrefix: "slack:slash",
+      ephemeral: true,
+    },
+    textLimit: 4000,
+    ackReactionScope: "group-mentions",
+    mediaMaxBytes: 1024,
+    removeAckAfterReply: false,
+  });
+}
+
+const account: ResolvedSlackAccount = {
+  accountId: "default",
+  enabled: true,
+  botTokenSource: "config",
+  appTokenSource: "config",
+  userTokenSource: "none",
+  config: {},
+};
+
+describe("thread-level session keys", () => {
+  it("uses thread-level session key for channel messages", async () => {
+    const ctx = buildCtx();
+    ctx.resolveUserName = async () => ({ name: "Alice" });
+
+    const message: SlackMessageEvent = {
+      channel: "C123",
+      channel_type: "channel",
+      user: "U1",
+      text: "hello",
+      ts: "1770408518.451689",
+    } as SlackMessageEvent;
+
+    const prepared = await prepareSlackMessage({
+      ctx,
+      account,
+      message,
+      opts: { source: "message" },
+    });
+
+    expect(prepared).toBeTruthy();
+    // Channel messages should get thread-level session key with :thread: suffix
+    // The resolved session key is in ctxPayload.SessionKey, not route.sessionKey
+    const sessionKey = prepared!.ctxPayload.SessionKey as string;
+    expect(sessionKey).toContain(":thread:");
+    expect(sessionKey).toContain("1770408518.451689");
+  });
+
+  it("uses parent thread_ts for thread replies", async () => {
+    const ctx = buildCtx();
+    ctx.resolveUserName = async () => ({ name: "Bob" });
+
+    const message: SlackMessageEvent = {
+      channel: "C123",
+      channel_type: "channel",
+      user: "U2",
+      text: "reply",
+      ts: "1770408522.168859",
+      thread_ts: "1770408518.451689",
+    } as SlackMessageEvent;
+
+    const prepared = await prepareSlackMessage({
+      ctx,
+      account,
+      message,
+      opts: { source: "message" },
+    });
+
+    expect(prepared).toBeTruthy();
+    // Thread replies should use the parent thread_ts, not the reply ts
+    const sessionKey = prepared!.ctxPayload.SessionKey as string;
+    expect(sessionKey).toContain(":thread:1770408518.451689");
+    expect(sessionKey).not.toContain("1770408522.168859");
+  });
+
+  it("does not add thread suffix for DMs", async () => {
+    const ctx = buildCtx();
+    ctx.resolveUserName = async () => ({ name: "Carol" });
+
+    const message: SlackMessageEvent = {
+      channel: "D456",
+      channel_type: "im",
+      user: "U3",
+      text: "dm message",
+      ts: "1770408530.000000",
+    } as SlackMessageEvent;
+
+    const prepared = await prepareSlackMessage({
+      ctx,
+      account,
+      message,
+      opts: { source: "message" },
+    });
+
+    expect(prepared).toBeTruthy();
+    // DMs should NOT have :thread: in the session key
+    const sessionKey = prepared!.ctxPayload.SessionKey as string;
+    expect(sessionKey).not.toContain(":thread:");
+  });
+});
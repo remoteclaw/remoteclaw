35d61d641eeb344d7907206109d1299c840d497a
Peter Steinberger <steipete@gmail.com>
2026-03-15 16:17:24 -0700

    refactor: decouple channel setup discovery
    (cherry picked from commit 74c762beb0efab0df6037638f781327fbdb23991)

--- src/gateway/server-plugins.test.ts
+++ src/gateway/server-plugins.test.ts
@@ -15,6 +15,7 @@ const createRegistry = (diagnostics: PluginDiagnostic[]): PluginRegistry => ({
   hooks: [],
   typedHooks: [],
   channels: [],
+  channelSetups: [],
   commands: [],
   providers: [],
   sttProviders: [],

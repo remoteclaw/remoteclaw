openclaw/openclaw

fix(android): update onboarding pairing commands

apps/android/app/src/main/java/ai/openclaw/android/ui/OnboardingFlow.kt
@@ -1563,8 +1563,8 @@ private fun FinalStep(
       } else {
         GuideBlock(title = "Pairing Required") {
           Text("Run these on the gateway host:", style = onboardingCalloutStyle, color = onboardingTextSecondary)
-          CommandBlock("openclaw nodes pending")
-          CommandBlock("openclaw nodes approve <requestId>")
+          CommandBlock("openclaw devices list")
+          CommandBlock("openclaw devices approve <requestId>")
           Text("Then tap Connect again.", style = onboardingCalloutStyle, color = onboardingTextSecondary)
         }
       }

remoteclaw/remoteclaw

(cherry picked from commit eea081c709689a835657625c3ca1fdaccb9cc358)

apps/android/app/src/main/java/org/remoteclaw/android/ui/OnboardingFlow.kt
@@ -1563,8 +1563,8 @@ private fun FinalStep(
       } else {
         GuideBlock(title = "Pairing Required") {
           Text("Run these on the gateway host:", style = onboardingCalloutStyle, color = onboardingTextSecondary)
-          CommandBlock("remoteclaw nodes pending")
-          CommandBlock("remoteclaw nodes approve <requestId>")
+          CommandBlock("remoteclaw devices list")
+          CommandBlock("remoteclaw devices approve <requestId>")
           Text("Then tap Connect again.", style = onboardingCalloutStyle, color = onboardingTextSecondary)
         }
       }
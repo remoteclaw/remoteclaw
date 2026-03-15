sypherin/openclaw

fix(android): add missing capability setup surfaces

apps/android/app/src/main/java/ai/openclaw/android/node/InvokeCommandRegistry.kt
@@ -61,6 +61,9 @@ object InvokeCommandRegistry {
       NodeCapabilitySpec(name = OpenClawCapability.Canvas.rawValue),
       NodeCapabilitySpec(name = OpenClawCapability.Screen.rawValue),
       NodeCapabilitySpec(name = OpenClawCapability.Device.rawValue),
+      NodeCapabilitySpec(name = OpenClawCapability.Notifications.rawValue),
+      NodeCapabilitySpec(name = OpenClawCapability.System.rawValue),
+      NodeCapabilitySpec(name = OpenClawCapability.AppUpdate.rawValue),
       NodeCapabilitySpec(
         name = OpenClawCapability.Camera.rawValue,
         availability = NodeCapabilityAvailability.CameraEnabled,

apps/android/app/src/test/java/ai/openclaw/android/node/InvokeCommandRegistryTest.kt
@@ -34,6 +34,9 @@ class InvokeCommandRegistryTest {
     assertTrue(capabilities.contains(OpenClawCapability.Canvas.rawValue))
     assertTrue(capabilities.contains(OpenClawCapability.Screen.rawValue))
     assertTrue(capabilities.contains(OpenClawCapability.Device.rawValue))
+    assertTrue(capabilities.contains(OpenClawCapability.Notifications.rawValue))
+    assertTrue(capabilities.contains(OpenClawCapability.System.rawValue))
+    assertTrue(capabilities.contains(OpenClawCapability.AppUpdate.rawValue))
     assertFalse(capabilities.contains(OpenClawCapability.Camera.rawValue))
     assertFalse(capabilities.contains(OpenClawCapability.Location.rawValue))
     assertFalse(capabilities.contains(OpenClawCapability.Sms.rawValue))
@@ -62,6 +65,9 @@ class InvokeCommandRegistryTest {
     assertTrue(capabilities.contains(OpenClawCapability.Canvas.rawValue))
     assertTrue(capabilities.contains(OpenClawCapability.Screen.rawValue))
     assertTrue(capabilities.contains(OpenClawCapability.Device.rawValue))
+    assertTrue(capabilities.contains(OpenClawCapability.Notifications.rawValue))
+    assertTrue(capabilities.contains(OpenClawCapability.System.rawValue))
+    assertTrue(capabilities.contains(OpenClawCapability.AppUpdate.rawValue))
     assertTrue(capabilities.contains(OpenClawCapability.Camera.rawValue))
     assertTrue(capabilities.contains(OpenClawCapability.Location.rawValue))
     assertTrue(capabilities.contains(OpenClawCapability.Sms.rawValue))

remoteclaw/remoteclaw

(cherry picked from commit cd61edb0f3470294ac9abfcce6ebecafc278029a)

apps/android/app/src/main/java/org/remoteclaw/android/node/InvokeCommandRegistry.kt
@@ -61,6 +61,9 @@ object InvokeCommandRegistry {
       NodeCapabilitySpec(name = RemoteClawCapability.Canvas.rawValue),
       NodeCapabilitySpec(name = RemoteClawCapability.Screen.rawValue),
       NodeCapabilitySpec(name = RemoteClawCapability.Device.rawValue),
+      NodeCapabilitySpec(name = RemoteClawCapability.Notifications.rawValue),
+      NodeCapabilitySpec(name = RemoteClawCapability.System.rawValue),
+      NodeCapabilitySpec(name = RemoteClawCapability.AppUpdate.rawValue),
       NodeCapabilitySpec(
         name = RemoteClawCapability.Camera.rawValue,
         availability = NodeCapabilityAvailability.CameraEnabled,

apps/android/app/src/test/java/org/remoteclaw/android/node/InvokeCommandRegistryTest.kt
@@ -34,6 +34,9 @@ class InvokeCommandRegistryTest {
     assertTrue(capabilities.contains(RemoteClawCapability.Canvas.rawValue))
     assertTrue(capabilities.contains(RemoteClawCapability.Screen.rawValue))
     assertTrue(capabilities.contains(RemoteClawCapability.Device.rawValue))
+    assertTrue(capabilities.contains(RemoteClawCapability.Notifications.rawValue))
+    assertTrue(capabilities.contains(RemoteClawCapability.System.rawValue))
+    assertTrue(capabilities.contains(RemoteClawCapability.AppUpdate.rawValue))
     assertFalse(capabilities.contains(RemoteClawCapability.Camera.rawValue))
     assertFalse(capabilities.contains(RemoteClawCapability.Location.rawValue))
     assertFalse(capabilities.contains(RemoteClawCapability.Sms.rawValue))
@@ -62,6 +65,9 @@ class InvokeCommandRegistryTest {
     assertTrue(capabilities.contains(RemoteClawCapability.Canvas.rawValue))
     assertTrue(capabilities.contains(RemoteClawCapability.Screen.rawValue))
     assertTrue(capabilities.contains(RemoteClawCapability.Device.rawValue))
+    assertTrue(capabilities.contains(RemoteClawCapability.Notifications.rawValue))
+    assertTrue(capabilities.contains(RemoteClawCapability.System.rawValue))
+    assertTrue(capabilities.contains(RemoteClawCapability.AppUpdate.rawValue))
     assertTrue(capabilities.contains(RemoteClawCapability.Camera.rawValue))
     assertTrue(capabilities.contains(RemoteClawCapability.Location.rawValue))
     assertTrue(capabilities.contains(RemoteClawCapability.Sms.rawValue))
wonglaitung/openclaw_mini

Slack: redact and cap interaction system events (#28982)

src/slack/monitor/events/interactions.test.ts
@@ -214,8 +214,8 @@ describe("registerSlackInteractionEvents", () => {
       value: "approved",
       userId: "U123",
       teamId: "T9",
-      triggerId: "123.trigger",
-      responseUrl: "https://hooks.slack.test/response",
+      triggerId: "[redacted]",
+      responseUrl: "[redacted]",
       channelId: "C1",
       messageTs: "100.200",
       threadTs: "100.100",
@@ -885,7 +885,7 @@ describe("registerSlackInteractionEvents", () => {
     };
     expect(payload).toMatchObject({
       actionType: "workflow_button",
-      workflowTriggerUrl: "https://slack.com/workflows/triggers/T420/12345",
+      workflowTriggerUrl: "[redacted]",
       workflowId: "Wf12345",
       teamId: "T420",
       channelId: "C420",
@@ -979,7 +979,7 @@ describe("registerSlackInteractionEvents", () => {
       rootViewId: "VROOT",
       previousViewId: "VPREV",
       externalId: "deploy-ext-1",
-      viewHash: "view-hash-1",
+      viewHash: "[redacted]",
       isStackedView: true,
     });
     expect(payload.inputs).toEqual(
@@ -1378,14 +1378,11 @@ describe("registerSlackInteractionEvents", () => {
       viewId: "V900",
       userId: "U900",
       isCleared: true,
-      privateMetadata: JSON.stringify({
-        sessionKey: "agent:main:slack:channel:C99",
-        userId: "U900",
-      }),
+      privateMetadata: "[redacted]",
       rootViewId: "VROOT900",
       previousViewId: "VPREV900",
       externalId: "deploy-ext-900",
-      viewHash: "view-hash-900",
+      viewHash: "[redacted]",
       isStackedView: true,
     });
     expect(payload.inputs).toEqual(
@@ -1425,6 +1422,65 @@ describe("registerSlackInteractionEvents", () => {
     };
     expect(payload.interactionType).toBe("view_closed");
     expect(payload.isCleared).toBe(false);
+  });
+
+  it("caps oversized interaction payloads with compact summaries", async () => {
+    enqueueSystemEventMock.mockClear();
+    const { ctx, getViewHandler } = createContext();
+    registerSlackInteractionEvents({ ctx: ctx as never });
+    const viewHandler = getViewHandler();
+    expect(viewHandler).toBeTruthy();
+
+    const richTextValue = {
+      type: "rich_text",
+      elements: Array.from({ length: 20 }, (_, index) => ({
+        type: "rich_text_section",
+        elements: [{ type: "text", text: `chunk-${index}-${"x".repeat(400)}` }],
+      })),
+    };
+    const values: Record<string, Record<string, unknown>> = {};
+    for (let index = 0; index < 20; index += 1) {
+      values[`block_${index}`] = {
+        [`input_${index}`]: {
+          type: "rich_text_input",
+          rich_text_value: richTextValue,
+        },
+      };
+    }
+
+    const ack = vi.fn().mockResolvedValue(undefined);
+    await viewHandler!({
+      ack,
+      body: {
+        user: { id: "U915" },
+        team: { id: "T1" },
+        view: {
+          id: "V915",
+          callback_id: "openclaw:oversize",
+          private_metadata: JSON.stringify({
+            channelId: "D915",
+            channelType: "im",
+            userId: "U915",
+          }),
+          state: {
+            values,
+          },
+        },
+      },
+    } as never);
+
+    expect(ack).toHaveBeenCalled();
+    expect(enqueueSystemEventMock).toHaveBeenCalledTimes(1);
+    const [eventText] = enqueueSystemEventMock.mock.calls[0] as [string];
+    expect(eventText.length).toBeLessThanOrEqual(2400);
+    const payload = JSON.parse(eventText.replace("Slack interaction: ", "")) as {
+      payloadTruncated?: boolean;
+      inputs?: unknown[];
+      inputsOmitted?: number;
+    };
+    expect(payload.payloadTruncated).toBe(true);
+    expect(Array.isArray(payload.inputs) ? payload.inputs.length : 0).toBeLessThanOrEqual(3);
+    expect((payload.inputsOmitted ?? 0) >= 1).toBe(true);
   });
 });
 const selectedDateTimeEpoch = 1_771_632_300;

remoteclaw/remoteclaw

(cherry picked from commit 746688ddc98c91604d4f9ff835057e8dae5ecaf8)

src/slack/monitor/events/interactions.test.ts
@@ -214,8 +214,8 @@ describe("registerSlackInteractionEvents", () => {
       value: "approved",
       userId: "U123",
       teamId: "T9",
-      triggerId: "123.trigger",
-      responseUrl: "https://hooks.slack.test/response",
+      triggerId: "[redacted]",
+      responseUrl: "[redacted]",
       channelId: "C1",
       messageTs: "100.200",
       threadTs: "100.100",
@@ -885,7 +885,7 @@ describe("registerSlackInteractionEvents", () => {
     };
     expect(payload).toMatchObject({
       actionType: "workflow_button",
-      workflowTriggerUrl: "https://slack.com/workflows/triggers/T420/12345",
+      workflowTriggerUrl: "[redacted]",
       workflowId: "Wf12345",
       teamId: "T420",
       channelId: "C420",
@@ -979,7 +979,7 @@ describe("registerSlackInteractionEvents", () => {
       rootViewId: "VROOT",
       previousViewId: "VPREV",
       externalId: "deploy-ext-1",
-      viewHash: "view-hash-1",
+      viewHash: "[redacted]",
       isStackedView: true,
     });
     expect(payload.inputs).toEqual(
@@ -1378,14 +1378,11 @@ describe("registerSlackInteractionEvents", () => {
       viewId: "V900",
       userId: "U900",
       isCleared: true,
-      privateMetadata: JSON.stringify({
-        sessionKey: "agent:main:slack:channel:C99",
-        userId: "U900",
-      }),
+      privateMetadata: "[redacted]",
       rootViewId: "VROOT900",
       previousViewId: "VPREV900",
       externalId: "deploy-ext-900",
-      viewHash: "view-hash-900",
+      viewHash: "[redacted]",
       isStackedView: true,
     });
     expect(payload.inputs).toEqual(
@@ -1425,6 +1422,65 @@ describe("registerSlackInteractionEvents", () => {
     };
     expect(payload.interactionType).toBe("view_closed");
     expect(payload.isCleared).toBe(false);
+  });
+
+  it("caps oversized interaction payloads with compact summaries", async () => {
+    enqueueSystemEventMock.mockClear();
+    const { ctx, getViewHandler } = createContext();
+    registerSlackInteractionEvents({ ctx: ctx as never });
+    const viewHandler = getViewHandler();
+    expect(viewHandler).toBeTruthy();
+
+    const richTextValue = {
+      type: "rich_text",
+      elements: Array.from({ length: 20 }, (_, index) => ({
+        type: "rich_text_section",
+        elements: [{ type: "text", text: `chunk-${index}-${"x".repeat(400)}` }],
+      })),
+    };
+    const values: Record<string, Record<string, unknown>> = {};
+    for (let index = 0; index < 20; index += 1) {
+      values[`block_${index}`] = {
+        [`input_${index}`]: {
+          type: "rich_text_input",
+          rich_text_value: richTextValue,
+        },
+      };
+    }
+
+    const ack = vi.fn().mockResolvedValue(undefined);
+    await viewHandler!({
+      ack,
+      body: {
+        user: { id: "U915" },
+        team: { id: "T1" },
+        view: {
+          id: "V915",
+          callback_id: "remoteclaw:oversize",
+          private_metadata: JSON.stringify({
+            channelId: "D915",
+            channelType: "im",
+            userId: "U915",
+          }),
+          state: {
+            values,
+          },
+        },
+      },
+    } as never);
+
+    expect(ack).toHaveBeenCalled();
+    expect(enqueueSystemEventMock).toHaveBeenCalledTimes(1);
+    const [eventText] = enqueueSystemEventMock.mock.calls[0] as [string];
+    expect(eventText.length).toBeLessThanOrEqual(2400);
+    const payload = JSON.parse(eventText.replace("Slack interaction: ", "")) as {
+      payloadTruncated?: boolean;
+      inputs?: unknown[];
+      inputsOmitted?: number;
+    };
+    expect(payload.payloadTruncated).toBe(true);
+    expect(Array.isArray(payload.inputs) ? payload.inputs.length : 0).toBeLessThanOrEqual(3);
+    expect((payload.inputsOmitted ?? 0) >= 1).toBe(true);
   });
 });
 const selectedDateTimeEpoch = 1_771_632_300;
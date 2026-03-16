b20eaaca6afe4a24fe6c209846bddb6fa1d68f48
Peter Steinberger <steipete@gmail.com>
2026-03-16 00:25:22 -0700

    refactor(plugin-sdk): clean shared core imports
    (cherry picked from commit b3025e6d8eb43a8fba08c0b87139b926c6568b28)

--- extensions/shared/runtime.ts
+++ extensions/shared/runtime.ts
@@ -1,4 +1,4 @@
-import { createLoggerBackedRuntime } from "remoteclaw/plugin-sdk";
+import { createLoggerBackedRuntime } from "remoteclaw/plugin-sdk/core";
 
 export function resolveLoggerBackedRuntime<TRuntime>(
   runtime: TRuntime | undefined,

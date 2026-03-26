wonglaitung/openclaw_mini

refactor(plugin-sdk): clean shared core imports

extensions/shared/runtime.ts
@@ -1,4 +1,4 @@
-import { createLoggerBackedRuntime } from "openclaw/plugin-sdk";
+import { createLoggerBackedRuntime } from "openclaw/plugin-sdk/core";
 
 export function resolveLoggerBackedRuntime<TRuntime>(
   runtime: TRuntime | undefined,

remoteclaw/remoteclaw

(cherry picked from commit b3025e6d8eb43a8fba08c0b87139b926c6568b28)

extensions/shared/runtime.ts
@@ -1,4 +1,4 @@
-import { createLoggerBackedRuntime } from "remoteclaw/plugin-sdk";
+import { createLoggerBackedRuntime } from "remoteclaw/plugin-sdk/core";
 
 export function resolveLoggerBackedRuntime<TRuntime>(
   runtime: TRuntime | undefined,
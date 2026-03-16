wonglaitung/openclaw_mini

test(android): dedupe node and gateway invoke tests

apps/android/app/src/test/java/ai/openclaw/android/node/NodeHandlerRobolectricTest.kt
@@ -1,0 +1,11 @@
+package ai.openclaw.android.node
+
+import android.content.Context
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.RuntimeEnvironment
+
+@RunWith(RobolectricTestRunner::class)
+abstract class NodeHandlerRobolectricTest {
+  protected fun appContext(): Context = RuntimeEnvironment.getApplication()
+}

remoteclaw/remoteclaw

(cherry picked from commit 2d8b8a17ab3a3298c8125d6e7563bff1cbeb1b6c)

apps/android/app/src/test/java/org/remoteclaw/android/node/NodeHandlerRobolectricTest.kt
@@ -1,0 +1,11 @@
+package org.remoteclaw.android.node
+
+import android.content.Context
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.RuntimeEnvironment
+
+@RunWith(RobolectricTestRunner::class)
+abstract class NodeHandlerRobolectricTest {
+  protected fun appContext(): Context = RuntimeEnvironment.getApplication()
+}
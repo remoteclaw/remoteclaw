29b6e170f1c7c12c75de2e684be175060f93aac3
Gustavo Madeira Santana <gumadeiras@gmail.com>
2026-03-16 09:51:28 +0000

    Plugin SDK: restore scoped imports for bundled channels
    (cherry picked from commit 92700940d901e40127c28cb8f04bc3c3e2e20a1c)

--- src/plugin-sdk/core.ts
+++ src/plugin-sdk/core.ts
@@ -6,6 +6,7 @@ export type {
   ProviderAuthResult,
 } from "../plugins/types.js";
 export type { ChannelPlugin } from "../channels/plugins/types.plugin.js";
+export type { ChannelMessageActionContext } from "../channels/plugins/types.js";
 export type { PluginRuntime } from "../plugins/runtime/types.js";
 export type { RemoteClawConfig } from "../config/config.js";
 export type { GatewayRequestHandlerOptions } from "../gateway/server-methods/types.js";

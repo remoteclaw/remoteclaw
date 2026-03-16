wonglaitung/openclaw_mini

refactor(tests): dedupe ios gateway and deeplink fixtures

apps/ios/Tests/GatewayConnectionSecurityTests.swift
@@ -5,6 +5,32 @@ import Testing
 @testable import OpenClaw
 
 @Suite(.serialized) struct GatewayConnectionSecurityTests {
+    private func makeController() -> GatewayConnectionController {
+        GatewayConnectionController(appModel: NodeAppModel(), startDiscovery: false)
+    }
+
+    private func makeDiscoveredGateway(
+        stableID: String,
+        lanHost: String?,
+        tailnetDns: String?,
+        gatewayPort: Int?,
+        fingerprint: String?) -> GatewayDiscoveryModel.DiscoveredGateway
+    {
+        let endpoint: NWEndpoint = .service(name: "Test", type: "_openclaw-gw._tcp", domain: "local.", interface: nil)
+        return GatewayDiscoveryModel.DiscoveredGateway(
+            name: "Test",
+            endpoint: endpoint,
+            stableID: stableID,
+            debugID: "debug",
+            lanHost: lanHost,
+            tailnetDns: tailnetDns,
+            gatewayPort: gatewayPort,
+            canvasPort: nil,
+            tlsEnabled: true,
+            tlsFingerprintSha256: fingerprint,
+            cliPath: nil)
+    }
+
     private func clearTLSFingerprint(stableID: String) {
         let suite = UserDefaults(suiteName: "ai.openclaw.shared") ?? .standard
         suite.removeObject(forKey: "gateway.tls.\(stableID)")
@@ -17,22 +43,13 @@ import Testing
 
         GatewayTLSStore.saveFingerprint("11", stableID: stableID)
 
-        let endpoint: NWEndpoint = .service(name: "Test", type: "_openclaw-gw._tcp", domain: "local.", interface: nil)
-        let gateway = GatewayDiscoveryModel.DiscoveredGateway(
-            name: "Test",
-            endpoint: endpoint,
+        let gateway = makeDiscoveredGateway(
             stableID: stableID,
-            debugID: "debug",
             lanHost: "evil.example.com",
             tailnetDns: "evil.example.com",
             gatewayPort: 12345,
-            canvasPort: nil,
-            tlsEnabled: true,
-            tlsFingerprintSha256: "22",
-            cliPath: nil)
-
-        let appModel = NodeAppModel()
-        let controller = GatewayConnectionController(appModel: appModel, startDiscovery: false)
+            fingerprint: "22")
+        let controller = makeController()
 
         let params = controller._test_resolveDiscoveredTLSParams(gateway: gateway, allowTOFU: true)
         #expect(params?.expectedFingerprint == "11")
@@ -44,22 +61,13 @@ import Testing
         defer { clearTLSFingerprint(stableID: stableID) }
         clearTLSFingerprint(stableID: stableID)
 
-        let endpoint: NWEndpoint = .service(name: "Test", type: "_openclaw-gw._tcp", domain: "local.", interface: nil)
-        let gateway = GatewayDiscoveryModel.DiscoveredGateway(
-            name: "Test",
-            endpoint: endpoint,
+        let gateway = makeDiscoveredGateway(
             stableID: stableID,
-            debugID: "debug",
             lanHost: nil,
             tailnetDns: nil,
             gatewayPort: nil,
-            canvasPort: nil,
-            tlsEnabled: true,
-            tlsFingerprintSha256: "22",
-            cliPath: nil)
-
-        let appModel = NodeAppModel()
-        let controller = GatewayConnectionController(appModel: appModel, startDiscovery: false)
+            fingerprint: "22")
+        let controller = makeController()
 
         let params = controller._test_resolveDiscoveredTLSParams(gateway: gateway, allowTOFU: true)
         #expect(params?.expectedFingerprint == nil)
@@ -82,22 +90,13 @@ import Testing
         defaults.removeObject(forKey: "gateway.preferredStableID")
         defaults.set(stableID, forKey: "gateway.lastDiscoveredStableID")
 
-        let endpoint: NWEndpoint = .service(name: "Test", type: "_openclaw-gw._tcp", domain: "local.", interface: nil)
-        let gateway = GatewayDiscoveryModel.DiscoveredGateway(
-            name: "Test",
-            endpoint: endpoint,
+        let gateway = makeDiscoveredGateway(
             stableID: stableID,
-            debugID: "debug",
             lanHost: "test.local",
             tailnetDns: nil,
             gatewayPort: 18789,
-            canvasPort: nil,
-            tlsEnabled: true,
-            tlsFingerprintSha256: nil,
-            cliPath: nil)
-
-        let appModel = NodeAppModel()
-        let controller = GatewayConnectionController(appModel: appModel, startDiscovery: false)
+            fingerprint: nil)
+        let controller = makeController()
         controller._test_setGateways([gateway])
         controller._test_triggerAutoConnect()
 
@@ -105,8 +104,7 @@ import Testing
     }
 
     @Test @MainActor func manualConnectionsForceTLSForNonLoopbackHosts() async {
-        let appModel = NodeAppModel()
-        let controller = GatewayConnectionController(appModel: appModel, startDiscovery: false)
+        let controller = makeController()
 
         #expect(controller._test_resolveManualUseTLS(host: "gateway.example.com", useTLS: false) == true)
         #expect(controller._test_resolveManualUseTLS(host: "openclaw.local", useTLS: false) == true)
@@ -121,8 +119,7 @@ import Testing
     }
 
     @Test @MainActor func manualDefaultPortUses443OnlyForTailnetTLSHosts() async {
-        let appModel = NodeAppModel()
-        let controller = GatewayConnectionController(appModel: appModel, startDiscovery: false)
+        let controller = makeController()
 
         #expect(controller._test_resolveManualPort(host: "gateway.example.com", port: 0, useTLS: true) == 18789)
         #expect(controller._test_resolveManualPort(host: "device.sample.ts.net", port: 0, useTLS: true) == 443)

apps/ios/Tests/VoiceWakeManagerExtractCommandTests.swift
@@ -3,6 +3,19 @@ import SwabbleKit
 import Testing
 @testable import OpenClaw
 
+private let openclawTranscript = "hey openclaw do thing"
+
+private func openclawSegments(postTriggerStart: TimeInterval) -> [WakeWordSegment] {
+    makeSegments(
+        transcript: openclawTranscript,
+        words: [
+            ("hey", 0.0, 0.1),
+            ("openclaw", 0.2, 0.1),
+            ("do", postTriggerStart, 0.1),
+            ("thing", postTriggerStart + 0.2, 0.1),
+        ])
+}
+
 @Suite struct VoiceWakeManagerExtractCommandTests {
     @Test func extractCommandReturnsNilWhenNoTriggerFound() {
         let transcript = "hello world"
@@ -13,17 +26,9 @@ import Testing
     }
 
     @Test func extractCommandTrimsTokensAndResult() {
-        let transcript = "hey openclaw do thing"
-        let segments = makeSegments(
-            transcript: transcript,
-            words: [
-                ("hey", 0.0, 0.1),
-                ("openclaw", 0.2, 0.1),
-                ("do", 0.9, 0.1),
-                ("thing", 1.1, 0.1),
-            ])
+        let segments = openclawSegments(postTriggerStart: 0.9)
         let cmd = VoiceWakeManager.extractCommand(
-            from: transcript,
+            from: openclawTranscript,
             segments: segments,
             triggers: ["  openclaw  "],
             minPostTriggerGap: 0.3)
@@ -31,17 +36,9 @@ import Testing
     }
 
     @Test func extractCommandReturnsNilWhenGapTooShort() {
-        let transcript = "hey openclaw do thing"
-        let segments = makeSegments(
-            transcript: transcript,
-            words: [
-                ("hey", 0.0, 0.1),
-                ("openclaw", 0.2, 0.1),
-                ("do", 0.35, 0.1),
-                ("thing", 0.5, 0.1),
-            ])
+        let segments = openclawSegments(postTriggerStart: 0.35)
         let cmd = VoiceWakeManager.extractCommand(
-            from: transcript,
+            from: openclawTranscript,
             segments: segments,
             triggers: ["openclaw"],
             minPostTriggerGap: 0.3)
@@ -57,17 +54,9 @@ import Testing
     }
 
     @Test func extractCommandIgnoresEmptyTriggers() {
-        let transcript = "hey openclaw do thing"
-        let segments = makeSegments(
-            transcript: transcript,
-            words: [
-                ("hey", 0.0, 0.1),
-                ("openclaw", 0.2, 0.1),
-                ("do", 0.9, 0.1),
-                ("thing", 1.1, 0.1),
-            ])
+        let segments = openclawSegments(postTriggerStart: 0.9)
         let cmd = VoiceWakeManager.extractCommand(
-            from: transcript,
+            from: openclawTranscript,
             segments: segments,
             triggers: ["", "   ", "openclaw"],
             minPostTriggerGap: 0.3)

remoteclaw/remoteclaw

(cherry picked from commit 8553d224288fbddd3e5326191a898e3aa8d62794)

apps/ios/Tests/GatewayConnectionSecurityTests.swift
@@ -5,6 +5,32 @@ import Testing
 @testable import RemoteClaw
 
 @Suite(.serialized) struct GatewayConnectionSecurityTests {
+    private func makeController() -> GatewayConnectionController {
+        GatewayConnectionController(appModel: NodeAppModel(), startDiscovery: false)
+    }
+
+    private func makeDiscoveredGateway(
+        stableID: String,
+        lanHost: String?,
+        tailnetDns: String?,
+        gatewayPort: Int?,
+        fingerprint: String?) -> GatewayDiscoveryModel.DiscoveredGateway
+    {
+        let endpoint: NWEndpoint = .service(name: "Test", type: "_remoteclaw-gw._tcp", domain: "local.", interface: nil)
+        return GatewayDiscoveryModel.DiscoveredGateway(
+            name: "Test",
+            endpoint: endpoint,
+            stableID: stableID,
+            debugID: "debug",
+            lanHost: lanHost,
+            tailnetDns: tailnetDns,
+            gatewayPort: gatewayPort,
+            canvasPort: nil,
+            tlsEnabled: true,
+            tlsFingerprintSha256: fingerprint,
+            cliPath: nil)
+    }
+
     private func clearTLSFingerprint(stableID: String) {
         let suite = UserDefaults(suiteName: "org.remoteclaw.shared") ?? .standard
         suite.removeObject(forKey: "gateway.tls.\(stableID)")
@@ -17,22 +43,13 @@ import Testing
 
         GatewayTLSStore.saveFingerprint("11", stableID: stableID)
 
-        let endpoint: NWEndpoint = .service(name: "Test", type: "_remoteclaw-gw._tcp", domain: "local.", interface: nil)
-        let gateway = GatewayDiscoveryModel.DiscoveredGateway(
-            name: "Test",
-            endpoint: endpoint,
+        let gateway = makeDiscoveredGateway(
             stableID: stableID,
-            debugID: "debug",
             lanHost: "evil.example.com",
             tailnetDns: "evil.example.com",
             gatewayPort: 12345,
-            canvasPort: nil,
-            tlsEnabled: true,
-            tlsFingerprintSha256: "22",
-            cliPath: nil)
-
-        let appModel = NodeAppModel()
-        let controller = GatewayConnectionController(appModel: appModel, startDiscovery: false)
+            fingerprint: "22")
+        let controller = makeController()
 
         let params = controller._test_resolveDiscoveredTLSParams(gateway: gateway, allowTOFU: true)
         #expect(params?.expectedFingerprint == "11")
@@ -44,22 +61,13 @@ import Testing
         defer { clearTLSFingerprint(stableID: stableID) }
         clearTLSFingerprint(stableID: stableID)
 
-        let endpoint: NWEndpoint = .service(name: "Test", type: "_remoteclaw-gw._tcp", domain: "local.", interface: nil)
-        let gateway = GatewayDiscoveryModel.DiscoveredGateway(
-            name: "Test",
-            endpoint: endpoint,
+        let gateway = makeDiscoveredGateway(
             stableID: stableID,
-            debugID: "debug",
             lanHost: nil,
             tailnetDns: nil,
             gatewayPort: nil,
-            canvasPort: nil,
-            tlsEnabled: true,
-            tlsFingerprintSha256: "22",
-            cliPath: nil)
-
-        let appModel = NodeAppModel()
-        let controller = GatewayConnectionController(appModel: appModel, startDiscovery: false)
+            fingerprint: "22")
+        let controller = makeController()
 
         let params = controller._test_resolveDiscoveredTLSParams(gateway: gateway, allowTOFU: true)
         #expect(params?.expectedFingerprint == nil)
@@ -82,22 +90,13 @@ import Testing
         defaults.removeObject(forKey: "gateway.preferredStableID")
         defaults.set(stableID, forKey: "gateway.lastDiscoveredStableID")
 
-        let endpoint: NWEndpoint = .service(name: "Test", type: "_remoteclaw-gw._tcp", domain: "local.", interface: nil)
-        let gateway = GatewayDiscoveryModel.DiscoveredGateway(
-            name: "Test",
-            endpoint: endpoint,
+        let gateway = makeDiscoveredGateway(
             stableID: stableID,
-            debugID: "debug",
             lanHost: "test.local",
             tailnetDns: nil,
             gatewayPort: 18789,
-            canvasPort: nil,
-            tlsEnabled: true,
-            tlsFingerprintSha256: nil,
-            cliPath: nil)
-
-        let appModel = NodeAppModel()
-        let controller = GatewayConnectionController(appModel: appModel, startDiscovery: false)
+            fingerprint: nil)
+        let controller = makeController()
         controller._test_setGateways([gateway])
         controller._test_triggerAutoConnect()
 
@@ -105,8 +104,7 @@ import Testing
     }
 
     @Test @MainActor func manualConnectionsForceTLSForNonLoopbackHosts() async {
-        let appModel = NodeAppModel()
-        let controller = GatewayConnectionController(appModel: appModel, startDiscovery: false)
+        let controller = makeController()
 
         #expect(controller._test_resolveManualUseTLS(host: "gateway.example.com", useTLS: false) == true)
         #expect(controller._test_resolveManualUseTLS(host: "remoteclaw.local", useTLS: false) == true)
@@ -121,8 +119,7 @@ import Testing
     }
 
     @Test @MainActor func manualDefaultPortUses443OnlyForTailnetTLSHosts() async {
-        let appModel = NodeAppModel()
-        let controller = GatewayConnectionController(appModel: appModel, startDiscovery: false)
+        let controller = makeController()
 
         #expect(controller._test_resolveManualPort(host: "gateway.example.com", port: 0, useTLS: true) == 18789)
         #expect(controller._test_resolveManualPort(host: "device.sample.ts.net", port: 0, useTLS: true) == 443)

apps/ios/Tests/VoiceWakeManagerExtractCommandTests.swift
@@ -3,6 +3,19 @@ import SwabbleKit
 import Testing
 @testable import RemoteClaw
 
+private let remoteClawTranscript = "hey remoteclaw do thing"
+
+private func remoteClawSegments(postTriggerStart: TimeInterval) -> [WakeWordSegment] {
+    makeSegments(
+        transcript: remoteClawTranscript,
+        words: [
+            ("hey", 0.0, 0.1),
+            ("remoteclaw", 0.2, 0.1),
+            ("do", postTriggerStart, 0.1),
+            ("thing", postTriggerStart + 0.2, 0.1),
+        ])
+}
+
 @Suite struct VoiceWakeManagerExtractCommandTests {
     @Test func extractCommandReturnsNilWhenNoTriggerFound() {
         let transcript = "hello world"
@@ -13,17 +26,9 @@ import Testing
     }
 
     @Test func extractCommandTrimsTokensAndResult() {
-        let transcript = "hey remoteclaw do thing"
-        let segments = makeSegments(
-            transcript: transcript,
-            words: [
-                ("hey", 0.0, 0.1),
-                ("remoteclaw", 0.2, 0.1),
-                ("do", 0.9, 0.1),
-                ("thing", 1.1, 0.1),
-            ])
+        let segments = remoteClawSegments(postTriggerStart: 0.9)
         let cmd = VoiceWakeManager.extractCommand(
-            from: transcript,
+            from: remoteClawTranscript,
             segments: segments,
             triggers: ["  remoteclaw  "],
             minPostTriggerGap: 0.3)
@@ -31,17 +36,9 @@ import Testing
     }
 
     @Test func extractCommandReturnsNilWhenGapTooShort() {
-        let transcript = "hey remoteclaw do thing"
-        let segments = makeSegments(
-            transcript: transcript,
-            words: [
-                ("hey", 0.0, 0.1),
-                ("remoteclaw", 0.2, 0.1),
-                ("do", 0.35, 0.1),
-                ("thing", 0.5, 0.1),
-            ])
+        let segments = remoteClawSegments(postTriggerStart: 0.35)
         let cmd = VoiceWakeManager.extractCommand(
-            from: transcript,
+            from: remoteClawTranscript,
             segments: segments,
             triggers: ["remoteclaw"],
             minPostTriggerGap: 0.3)
@@ -57,17 +54,9 @@ import Testing
     }
 
     @Test func extractCommandIgnoresEmptyTriggers() {
-        let transcript = "hey remoteclaw do thing"
-        let segments = makeSegments(
-            transcript: transcript,
-            words: [
-                ("hey", 0.0, 0.1),
-                ("remoteclaw", 0.2, 0.1),
-                ("do", 0.9, 0.1),
-                ("thing", 1.1, 0.1),
-            ])
+        let segments = remoteClawSegments(postTriggerStart: 0.9)
         let cmd = VoiceWakeManager.extractCommand(
-            from: transcript,
+            from: remoteClawTranscript,
             segments: segments,
             triggers: ["", "   ", "remoteclaw"],
             minPostTriggerGap: 0.3)
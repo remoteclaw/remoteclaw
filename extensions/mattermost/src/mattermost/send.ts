wonglaitung/openclaw_mini

refactor: centralize delivery/path/media/version lifecycle

extensions/mattermost/src/mattermost/send.ts
@@ -1,3 +1,4 @@
+import { loadOutboundMediaFromUrl } from "openclaw/plugin-sdk";
 import { getMattermostRuntime } from "../runtime.js";
 import { resolveMattermostAccount } from "./accounts.js";
 import {
@@ -16,6 +17,7 @@ export type MattermostSendOpts = {
   baseUrl?: string;
   accountId?: string;
   mediaUrl?: string;
+  mediaLocalRoots?: readonly string[];
   replyToId?: string;
 };
 
@@ -176,7 +178,9 @@ export async function sendMessageMattermost(
   const mediaUrl = opts.mediaUrl?.trim();
   if (mediaUrl) {
     try {
-      const media = await core.media.loadWebMedia(mediaUrl);
+      const media = await loadOutboundMediaFromUrl(mediaUrl, {
+        mediaLocalRoots: opts.mediaLocalRoots,
+      });
       const fileInfo = await uploadMattermostFile(client, {
         channelId,
         buffer: media.buffer,

extensions/msteams/src/send.test.ts
@@ -1,0 +1,109 @@
+import type { OpenClawConfig } from "openclaw/plugin-sdk";
+import { beforeEach, describe, expect, it, vi } from "vitest";
+import { sendMessageMSTeams } from "./send.js";
+
+const mockState = vi.hoisted(() => ({
+  loadOutboundMediaFromUrl: vi.fn(),
+  resolveMSTeamsSendContext: vi.fn(),
+  requiresFileConsent: vi.fn(),
+  prepareFileConsentActivity: vi.fn(),
+  extractFilename: vi.fn(async () => "fallback.bin"),
+  sendMSTeamsMessages: vi.fn(),
+}));
+
+vi.mock("openclaw/plugin-sdk", () => ({
+  loadOutboundMediaFromUrl: mockState.loadOutboundMediaFromUrl,
+}));
+
+vi.mock("./send-context.js", () => ({
+  resolveMSTeamsSendContext: mockState.resolveMSTeamsSendContext,
+}));
+
+vi.mock("./file-consent-helpers.js", () => ({
+  requiresFileConsent: mockState.requiresFileConsent,
+  prepareFileConsentActivity: mockState.prepareFileConsentActivity,
+}));
+
+vi.mock("./media-helpers.js", () => ({
+  extractFilename: mockState.extractFilename,
+  extractMessageId: () => "message-1",
+}));
+
+vi.mock("./messenger.js", () => ({
+  sendMSTeamsMessages: mockState.sendMSTeamsMessages,
+  buildConversationReference: () => ({}),
+}));
+
+vi.mock("./runtime.js", () => ({
+  getMSTeamsRuntime: () => ({
+    channel: {
+      text: {
+        resolveMarkdownTableMode: () => "off",
+        convertMarkdownTables: (text: string) => text,
+      },
+    },
+  }),
+}));
+
+describe("sendMessageMSTeams", () => {
+  beforeEach(() => {
+    mockState.loadOutboundMediaFromUrl.mockReset();
+    mockState.resolveMSTeamsSendContext.mockReset();
+    mockState.requiresFileConsent.mockReset();
+    mockState.prepareFileConsentActivity.mockReset();
+    mockState.extractFilename.mockReset();
+    mockState.sendMSTeamsMessages.mockReset();
+
+    mockState.extractFilename.mockResolvedValue("fallback.bin");
+    mockState.requiresFileConsent.mockReturnValue(false);
+    mockState.resolveMSTeamsSendContext.mockResolvedValue({
+      adapter: {},
+      appId: "app-id",
+      conversationId: "19:conversation@thread.tacv2",
+      ref: {},
+      log: { debug: vi.fn(), info: vi.fn(), warn: vi.fn(), error: vi.fn() },
+      conversationType: "personal",
+      tokenProvider: { getAccessToken: vi.fn(async () => "token") },
+      mediaMaxBytes: 8 * 1024,
+      sharePointSiteId: undefined,
+    });
+    mockState.sendMSTeamsMessages.mockResolvedValue(["message-1"]);
+  });
+
+  it("loads media through shared helper and forwards mediaLocalRoots", async () => {
+    const mediaBuffer = Buffer.from("tiny-image");
+    mockState.loadOutboundMediaFromUrl.mockResolvedValueOnce({
+      buffer: mediaBuffer,
+      contentType: "image/png",
+      fileName: "inline.png",
+      kind: "image",
+    });
+
+    await sendMessageMSTeams({
+      cfg: {} as OpenClawConfig,
+      to: "conversation:19:conversation@thread.tacv2",
+      text: "hello",
+      mediaUrl: "file:///tmp/agent-workspace/inline.png",
+      mediaLocalRoots: ["/tmp/agent-workspace"],
+    });
+
+    expect(mockState.loadOutboundMediaFromUrl).toHaveBeenCalledWith(
+      "file:///tmp/agent-workspace/inline.png",
+      {
+        maxBytes: 8 * 1024,
+        mediaLocalRoots: ["/tmp/agent-workspace"],
+      },
+    );
+
+    expect(mockState.sendMSTeamsMessages).toHaveBeenCalledWith(
+      expect.objectContaining({
+        messages: [
+          expect.objectContaining({
+            text: "hello",
+            mediaUrl: `data:image/png;base64,${mediaBuffer.toString("base64")}`,
+          }),
+        ],
+      }),
+    );
+  });
+});

extensions/msteams/src/send.ts
@@ -1,5 +1,5 @@
 import type { OpenClawConfig } from "openclaw/plugin-sdk";
-import { loadWebMedia, resolveChannelMediaMaxBytes } from "openclaw/plugin-sdk";
+import { loadOutboundMediaFromUrl } from "openclaw/plugin-sdk";
 import { createMSTeamsConversationStoreFs } from "./conversation-store-fs.js";
 import {
   classifyMSTeamsSendError,
@@ -28,6 +28,7 @@ export type SendMSTeamsMessageParams = {
   text: string;
   /** Optional media URL */
   mediaUrl?: string;
+  mediaLocalRoots?: readonly string[];
 };
 
 export type SendMSTeamsMessageResult = {
@@ -93,7 +94,7 @@ export type SendMSTeamsCardResult = {
 export async function sendMessageMSTeams(
   params: SendMSTeamsMessageParams,
 ): Promise<SendMSTeamsMessageResult> {
-  const { cfg, to, text, mediaUrl } = params;
+  const { cfg, to, text, mediaUrl, mediaLocalRoots } = params;
   const tableMode = getMSTeamsRuntime().channel.text.resolveMarkdownTableMode({
     cfg,
     channel: "msteams",
@@ -120,12 +121,11 @@ export async function sendMessageMSTeams(
 
   // Handle media if present
   if (mediaUrl) {
-    const mediaMaxBytes =
-      resolveChannelMediaMaxBytes({
-        cfg,
-        resolveChannelLimitMb: ({ cfg }) => cfg.channels?.msteams?.mediaMaxMb,
-      }) ?? MSTEAMS_MAX_MEDIA_BYTES;
-    const media = await loadWebMedia(mediaUrl, mediaMaxBytes);
+    const mediaMaxBytes = ctx.mediaMaxBytes ?? MSTEAMS_MAX_MEDIA_BYTES;
+    const media = await loadOutboundMediaFromUrl(mediaUrl, {
+      maxBytes: mediaMaxBytes,
+      mediaLocalRoots,
+    });
     const isLargeFile = media.buffer.length >= FILE_CONSENT_THRESHOLD_BYTES;
     const isImage = media.contentType?.startsWith("image/") ?? false;
     const fallbackFileName = await extractFilename(mediaUrl);

src/version.test.ts
@@ -6,6 +6,7 @@ import { describe, expect, it } from "vitest";
 import {
   readVersionFromBuildInfoForModuleUrl,
   readVersionFromPackageJsonForModuleUrl,
+  resolveBinaryVersion,
   resolveRuntimeServiceVersion,
   resolveVersionFromModuleUrl,
 } from "./version.js";
@@ -92,6 +93,42 @@ describe("version resolution", () => {
     expect(readVersionFromPackageJsonForModuleUrl("not-a-valid-url")).toBeNull();
     expect(readVersionFromBuildInfoForModuleUrl("not-a-valid-url")).toBeNull();
     expect(resolveVersionFromModuleUrl("not-a-valid-url")).toBeNull();
+  });
+
+  it("resolves binary version with explicit precedence", async () => {
+    await withTempDir(async (root) => {
+      await writeJsonFixture(root, "package.json", { name: "openclaw", version: "2.3.4" });
+      const moduleUrl = await ensureModuleFixture(root);
+      expect(
+        resolveBinaryVersion({
+          moduleUrl,
+          injectedVersion: "9.9.9",
+          bundledVersion: "8.8.8",
+          fallback: "0.0.0",
+        }),
+      ).toBe("9.9.9");
+      expect(
+        resolveBinaryVersion({
+          moduleUrl,
+          bundledVersion: "8.8.8",
+          fallback: "0.0.0",
+        }),
+      ).toBe("2.3.4");
+      expect(
+        resolveBinaryVersion({
+          moduleUrl: "not-a-valid-url",
+          bundledVersion: "8.8.8",
+          fallback: "0.0.0",
+        }),
+      ).toBe("8.8.8");
+      expect(
+        resolveBinaryVersion({
+          moduleUrl: "not-a-valid-url",
+          bundledVersion: "   ",
+          fallback: "0.0.0",
+        }),
+      ).toBe("0.0.0");
+    });
   });
 
   it("prefers OPENCLAW_VERSION over service and package versions", () => {

src/version.ts
@@ -71,6 +71,21 @@ export function resolveVersionFromModuleUrl(moduleUrl: string): string | null {
   );
 }
 
+export function resolveBinaryVersion(params: {
+  moduleUrl: string;
+  injectedVersion?: string;
+  bundledVersion?: string;
+  fallback?: string;
+}): string {
+  return (
+    firstNonEmpty(params.injectedVersion) ||
+    resolveVersionFromModuleUrl(params.moduleUrl) ||
+    firstNonEmpty(params.bundledVersion) ||
+    params.fallback ||
+    "0.0.0"
+  );
+}
+
 export type RuntimeVersionEnv = {
   [key: string]: string | undefined;
 };
@@ -91,8 +106,8 @@ export function resolveRuntimeServiceVersion(
 // Single source of truth for the current OpenClaw version.
 // - Embedded/bundled builds: injected define or env var.
 // - Dev/npm builds: package.json.
-export const VERSION =
-  (typeof __OPENCLAW_VERSION__ === "string" && __OPENCLAW_VERSION__) ||
-  process.env.OPENCLAW_BUNDLED_VERSION ||
-  resolveVersionFromModuleUrl(import.meta.url) ||
-  "0.0.0";
+export const VERSION = resolveBinaryVersion({
+  moduleUrl: import.meta.url,
+  injectedVersion: typeof __OPENCLAW_VERSION__ === "string" ? __OPENCLAW_VERSION__ : undefined,
+  bundledVersion: process.env.OPENCLAW_BUNDLED_VERSION,
+});

remoteclaw/remoteclaw

(cherry picked from commit c0bf42f2a8754b4736dc5991c63313db9525b90a)

extensions/mattermost/src/mattermost/send.ts
@@ -1,3 +1,4 @@
+import { loadOutboundMediaFromUrl } from "remoteclaw/plugin-sdk";
 import { getMattermostRuntime } from "../runtime.js";
 import { resolveMattermostAccount } from "./accounts.js";
 import {
@@ -16,6 +17,7 @@ export type MattermostSendOpts = {
   baseUrl?: string;
   accountId?: string;
   mediaUrl?: string;
+  mediaLocalRoots?: readonly string[];
   replyToId?: string;
 };
 
@@ -176,7 +178,9 @@ export async function sendMessageMattermost(
   const mediaUrl = opts.mediaUrl?.trim();
   if (mediaUrl) {
     try {
-      const media = await core.media.loadWebMedia(mediaUrl);
+      const media = await loadOutboundMediaFromUrl(mediaUrl, {
+        mediaLocalRoots: opts.mediaLocalRoots,
+      });
       const fileInfo = await uploadMattermostFile(client, {
         channelId,
         buffer: media.buffer,

extensions/msteams/src/send.test.ts
@@ -1,0 +1,109 @@
+import type { RemoteClawConfig } from "remoteclaw/plugin-sdk";
+import { beforeEach, describe, expect, it, vi } from "vitest";
+import { sendMessageMSTeams } from "./send.js";
+
+const mockState = vi.hoisted(() => ({
+  loadOutboundMediaFromUrl: vi.fn(),
+  resolveMSTeamsSendContext: vi.fn(),
+  requiresFileConsent: vi.fn(),
+  prepareFileConsentActivity: vi.fn(),
+  extractFilename: vi.fn(async () => "fallback.bin"),
+  sendMSTeamsMessages: vi.fn(),
+}));
+
+vi.mock("remoteclaw/plugin-sdk", () => ({
+  loadOutboundMediaFromUrl: mockState.loadOutboundMediaFromUrl,
+}));
+
+vi.mock("./send-context.js", () => ({
+  resolveMSTeamsSendContext: mockState.resolveMSTeamsSendContext,
+}));
+
+vi.mock("./file-consent-helpers.js", () => ({
+  requiresFileConsent: mockState.requiresFileConsent,
+  prepareFileConsentActivity: mockState.prepareFileConsentActivity,
+}));
+
+vi.mock("./media-helpers.js", () => ({
+  extractFilename: mockState.extractFilename,
+  extractMessageId: () => "message-1",
+}));
+
+vi.mock("./messenger.js", () => ({
+  sendMSTeamsMessages: mockState.sendMSTeamsMessages,
+  buildConversationReference: () => ({}),
+}));
+
+vi.mock("./runtime.js", () => ({
+  getMSTeamsRuntime: () => ({
+    channel: {
+      text: {
+        resolveMarkdownTableMode: () => "off",
+        convertMarkdownTables: (text: string) => text,
+      },
+    },
+  }),
+}));
+
+describe("sendMessageMSTeams", () => {
+  beforeEach(() => {
+    mockState.loadOutboundMediaFromUrl.mockReset();
+    mockState.resolveMSTeamsSendContext.mockReset();
+    mockState.requiresFileConsent.mockReset();
+    mockState.prepareFileConsentActivity.mockReset();
+    mockState.extractFilename.mockReset();
+    mockState.sendMSTeamsMessages.mockReset();
+
+    mockState.extractFilename.mockResolvedValue("fallback.bin");
+    mockState.requiresFileConsent.mockReturnValue(false);
+    mockState.resolveMSTeamsSendContext.mockResolvedValue({
+      adapter: {},
+      appId: "app-id",
+      conversationId: "19:conversation@thread.tacv2",
+      ref: {},
+      log: { debug: vi.fn(), info: vi.fn(), warn: vi.fn(), error: vi.fn() },
+      conversationType: "personal",
+      tokenProvider: { getAccessToken: vi.fn(async () => "token") },
+      mediaMaxBytes: 8 * 1024,
+      sharePointSiteId: undefined,
+    });
+    mockState.sendMSTeamsMessages.mockResolvedValue(["message-1"]);
+  });
+
+  it("loads media through shared helper and forwards mediaLocalRoots", async () => {
+    const mediaBuffer = Buffer.from("tiny-image");
+    mockState.loadOutboundMediaFromUrl.mockResolvedValueOnce({
+      buffer: mediaBuffer,
+      contentType: "image/png",
+      fileName: "inline.png",
+      kind: "image",
+    });
+
+    await sendMessageMSTeams({
+      cfg: {} as RemoteClawConfig,
+      to: "conversation:19:conversation@thread.tacv2",
+      text: "hello",
+      mediaUrl: "file:///tmp/agent-workspace/inline.png",
+      mediaLocalRoots: ["/tmp/agent-workspace"],
+    });
+
+    expect(mockState.loadOutboundMediaFromUrl).toHaveBeenCalledWith(
+      "file:///tmp/agent-workspace/inline.png",
+      {
+        maxBytes: 8 * 1024,
+        mediaLocalRoots: ["/tmp/agent-workspace"],
+      },
+    );
+
+    expect(mockState.sendMSTeamsMessages).toHaveBeenCalledWith(
+      expect.objectContaining({
+        messages: [
+          expect.objectContaining({
+            text: "hello",
+            mediaUrl: `data:image/png;base64,${mediaBuffer.toString("base64")}`,
+          }),
+        ],
+      }),
+    );
+  });
+});

extensions/msteams/src/send.ts
@@ -1,5 +1,5 @@
 import type { RemoteClawConfig } from "remoteclaw/plugin-sdk";
-import { loadWebMedia, resolveChannelMediaMaxBytes } from "remoteclaw/plugin-sdk";
+import { loadOutboundMediaFromUrl } from "remoteclaw/plugin-sdk";
 import { createMSTeamsConversationStoreFs } from "./conversation-store-fs.js";
 import {
   classifyMSTeamsSendError,
@@ -28,6 +28,7 @@ export type SendMSTeamsMessageParams = {
   text: string;
   /** Optional media URL */
   mediaUrl?: string;
+  mediaLocalRoots?: readonly string[];
 };
 
 export type SendMSTeamsMessageResult = {
@@ -93,7 +94,7 @@ export type SendMSTeamsCardResult = {
 export async function sendMessageMSTeams(
   params: SendMSTeamsMessageParams,
 ): Promise<SendMSTeamsMessageResult> {
-  const { cfg, to, text, mediaUrl } = params;
+  const { cfg, to, text, mediaUrl, mediaLocalRoots } = params;
   const tableMode = getMSTeamsRuntime().channel.text.resolveMarkdownTableMode({
     cfg,
     channel: "msteams",
@@ -120,12 +121,11 @@ export async function sendMessageMSTeams(
 
   // Handle media if present
   if (mediaUrl) {
-    const mediaMaxBytes =
-      resolveChannelMediaMaxBytes({
-        cfg,
-        resolveChannelLimitMb: ({ cfg }) => cfg.channels?.msteams?.mediaMaxMb,
-      }) ?? MSTEAMS_MAX_MEDIA_BYTES;
-    const media = await loadWebMedia(mediaUrl, mediaMaxBytes);
+    const mediaMaxBytes = ctx.mediaMaxBytes ?? MSTEAMS_MAX_MEDIA_BYTES;
+    const media = await loadOutboundMediaFromUrl(mediaUrl, {
+      maxBytes: mediaMaxBytes,
+      mediaLocalRoots,
+    });
     const isLargeFile = media.buffer.length >= FILE_CONSENT_THRESHOLD_BYTES;
     const isImage = media.contentType?.startsWith("image/") ?? false;
     const fallbackFileName = await extractFilename(mediaUrl);

src/version.test.ts
@@ -6,6 +6,7 @@ import { describe, expect, it } from "vitest";
 import {
   readVersionFromBuildInfoForModuleUrl,
   readVersionFromPackageJsonForModuleUrl,
+  resolveBinaryVersion,
   resolveRuntimeServiceVersion,
   resolveVersionFromModuleUrl,
 } from "./version.js";
@@ -92,6 +93,42 @@ describe("version resolution", () => {
     expect(readVersionFromPackageJsonForModuleUrl("not-a-valid-url")).toBeNull();
     expect(readVersionFromBuildInfoForModuleUrl("not-a-valid-url")).toBeNull();
     expect(resolveVersionFromModuleUrl("not-a-valid-url")).toBeNull();
+  });
+
+  it("resolves binary version with explicit precedence", async () => {
+    await withTempDir(async (root) => {
+      await writeJsonFixture(root, "package.json", { name: "remoteclaw", version: "2.3.4" });
+      const moduleUrl = await ensureModuleFixture(root);
+      expect(
+        resolveBinaryVersion({
+          moduleUrl,
+          injectedVersion: "9.9.9",
+          bundledVersion: "8.8.8",
+          fallback: "0.0.0",
+        }),
+      ).toBe("9.9.9");
+      expect(
+        resolveBinaryVersion({
+          moduleUrl,
+          bundledVersion: "8.8.8",
+          fallback: "0.0.0",
+        }),
+      ).toBe("2.3.4");
+      expect(
+        resolveBinaryVersion({
+          moduleUrl: "not-a-valid-url",
+          bundledVersion: "8.8.8",
+          fallback: "0.0.0",
+        }),
+      ).toBe("8.8.8");
+      expect(
+        resolveBinaryVersion({
+          moduleUrl: "not-a-valid-url",
+          bundledVersion: "   ",
+          fallback: "0.0.0",
+        }),
+      ).toBe("0.0.0");
+    });
   });
 
   it("prefers REMOTECLAW_VERSION over service and package versions", () => {

src/version.ts
@@ -71,6 +71,21 @@ export function resolveVersionFromModuleUrl(moduleUrl: string): string | null {
   );
 }
 
+export function resolveBinaryVersion(params: {
+  moduleUrl: string;
+  injectedVersion?: string;
+  bundledVersion?: string;
+  fallback?: string;
+}): string {
+  return (
+    firstNonEmpty(params.injectedVersion) ||
+    resolveVersionFromModuleUrl(params.moduleUrl) ||
+    firstNonEmpty(params.bundledVersion) ||
+    params.fallback ||
+    "0.0.0"
+  );
+}
+
 export type RuntimeVersionEnv = {
   [key: string]: string | undefined;
 };
@@ -91,8 +106,8 @@ export function resolveRuntimeServiceVersion(
 // Single source of truth for the current RemoteClaw version.
 // - Embedded/bundled builds: injected define or env var.
 // - Dev/npm builds: package.json.
-export const VERSION =
-  (typeof __REMOTECLAW_VERSION__ === "string" && __REMOTECLAW_VERSION__) ||
-  process.env.REMOTECLAW_BUNDLED_VERSION ||
-  resolveVersionFromModuleUrl(import.meta.url) ||
-  "0.0.0";
+export const VERSION = resolveBinaryVersion({
+  moduleUrl: import.meta.url,
+  injectedVersion: typeof __REMOTECLAW_VERSION__ === "string" ? __REMOTECLAW_VERSION__ : undefined,
+  bundledVersion: process.env.REMOTECLAW_BUNDLED_VERSION,
+});
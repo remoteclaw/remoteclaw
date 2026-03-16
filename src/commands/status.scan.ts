bochek/openclaw

Status scan: skip channel table work in JSON mode

src/commands/status.scan.ts
@@ -141,11 +141,13 @@ export async function scanStatus(
       progress.tick();
 
       progress.setLabel("Summarizing channels…");
-      const channels = await buildChannelsTable(cfg, {
-        // Show token previews in regular status; keep `status --all` redacted.
-        // Set `CLAWDBOT_SHOW_SECRETS=0` to force redaction.
-        showSecrets: process.env.CLAWDBOT_SHOW_SECRETS?.trim() !== "0",
-      });
+      const channels = opts.json
+        ? []
+        : await buildChannelsTable(cfg, {
+            // Show token previews in regular status; keep `status --all` redacted.
+            // Set `CLAWDBOT_SHOW_SECRETS=0` to force redaction.
+            showSecrets: process.env.CLAWDBOT_SHOW_SECRETS?.trim() !== "0",
+          });
       progress.tick();
 
       progress.setLabel("Checking memory…");

remoteclaw/remoteclaw

(cherry picked from commit 22653c0e27778382e42df32f1b168f56f5f2370b)

src/commands/status.scan.ts
@@ -115,11 +115,13 @@ export async function scanStatus(
       progress.tick();
 
       progress.setLabel("Summarizing channels…");
-      const channels = await buildChannelsTable(cfg, {
-        // Show token previews in regular status; keep `status --all` redacted.
-        // Set `REMOTECLAW_SHOW_SECRETS=0` to force redaction.
-        showSecrets: process.env.REMOTECLAW_SHOW_SECRETS?.trim() !== "0",
-      });
+      const channels = opts.json
+        ? []
+        : await buildChannelsTable(cfg, {
+            // Show token previews in regular status; keep `status --all` redacted.
+            // Set `REMOTECLAW_SHOW_SECRETS=0` to force redaction.
+            showSecrets: process.env.REMOTECLAW_SHOW_SECRETS?.trim() !== "0",
+          });
       progress.tick();
 
       progress.tick();
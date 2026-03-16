wonglaitung/openclaw_mini

fix(channels): add optional defaultAccount routing

extensions/nostr/src/types.ts
@@ -1,4 +1,9 @@
 import type { OpenClawConfig } from "openclaw/plugin-sdk";
+import {
+  DEFAULT_ACCOUNT_ID,
+  normalizeAccountId,
+  normalizeOptionalAccountId,
+} from "openclaw/plugin-sdk/account-id";
 import type { NostrProfile } from "./config-schema.js";
 import { getPublicKeyFromPrivate } from "./nostr-bus.js";
 import { DEFAULT_RELAYS } from "./nostr-bus.js";
@@ -6,6 +11,7 @@ import { DEFAULT_RELAYS } from "./nostr-bus.js";
 export interface NostrAccountConfig {
   enabled?: boolean;
   name?: string;
+  defaultAccount?: string;
   privateKey?: string;
   relays?: string[];
   dmPolicy?: "pairing" | "allowlist" | "open" | "disabled";
@@ -25,7 +31,12 @@ export interface ResolvedNostrAccount {
   config: NostrAccountConfig;
 }
 
-const DEFAULT_ACCOUNT_ID = "default";
+function resolveConfiguredDefaultNostrAccountId(cfg: OpenClawConfig): string | undefined {
+  const nostrCfg = (cfg.channels as Record<string, unknown> | undefined)?.nostr as
+    | NostrAccountConfig
+    | undefined;
+  return normalizeOptionalAccountId(nostrCfg?.defaultAccount);
+}
 
 /**
  * List all configured Nostr account IDs
@@ -37,7 +48,7 @@ export function listNostrAccountIds(cfg: OpenClawConfig): string[] {
 
   // If privateKey is configured at top level, we have a default account
   if (nostrCfg?.privateKey) {
-    return [DEFAULT_ACCOUNT_ID];
+    return [resolveConfiguredDefaultNostrAccountId(cfg) ?? DEFAULT_ACCOUNT_ID];
   }
 
   return [];
@@ -47,6 +58,10 @@ export function listNostrAccountIds(cfg: OpenClawConfig): string[] {
  * Get the default account ID
  */
 export function resolveDefaultNostrAccountId(cfg: OpenClawConfig): string {
+  const preferred = resolveConfiguredDefaultNostrAccountId(cfg);
+  if (preferred) {
+    return preferred;
+  }
   const ids = listNostrAccountIds(cfg);
   if (ids.includes(DEFAULT_ACCOUNT_ID)) {
     return DEFAULT_ACCOUNT_ID;
@@ -61,7 +76,7 @@ export function resolveNostrAccount(opts: {
   cfg: OpenClawConfig;
   accountId?: string | null;
 }): ResolvedNostrAccount {
-  const accountId = opts.accountId ?? DEFAULT_ACCOUNT_ID;
+  const accountId = normalizeAccountId(opts.accountId ?? resolveDefaultNostrAccountId(opts.cfg));
   const nostrCfg = (opts.cfg.channels as Record<string, unknown> | undefined)?.nostr as
     | NostrAccountConfig
     | undefined;

extensions/zalouser/src/accounts.ts
@@ -1,5 +1,9 @@
 import type { OpenClawConfig } from "openclaw/plugin-sdk";
-import { DEFAULT_ACCOUNT_ID, normalizeAccountId } from "openclaw/plugin-sdk/account-id";
+import {
+  DEFAULT_ACCOUNT_ID,
+  normalizeAccountId,
+  normalizeOptionalAccountId,
+} from "openclaw/plugin-sdk/account-id";
 import type { ResolvedZalouserAccount, ZalouserAccountConfig, ZalouserConfig } from "./types.js";
 import { runZca, parseJsonOutput } from "./zca.js";
 
@@ -21,8 +25,12 @@ export function listZalouserAccountIds(cfg: OpenClawConfig): string[] {
 
 export function resolveDefaultZalouserAccountId(cfg: OpenClawConfig): string {
   const zalouserConfig = cfg.channels?.zalouser as ZalouserConfig | undefined;
-  if (zalouserConfig?.defaultAccount?.trim()) {
-    return zalouserConfig.defaultAccount.trim();
+  const preferred = normalizeOptionalAccountId(zalouserConfig?.defaultAccount);
+  if (
+    preferred &&
+    listZalouserAccountIds(cfg).some((accountId) => normalizeAccountId(accountId) === preferred)
+  ) {
+    return preferred;
   }
   const ids = listZalouserAccountIds(cfg);
   if (ids.includes(DEFAULT_ACCOUNT_ID)) {

src/channels/plugins/account-helpers.ts
@@ -1,7 +1,26 @@
 import type { OpenClawConfig } from "../../config/config.js";
-import { DEFAULT_ACCOUNT_ID } from "../../routing/session-key.js";
+import {
+  DEFAULT_ACCOUNT_ID,
+  normalizeAccountId,
+  normalizeOptionalAccountId,
+} from "../../routing/session-key.js";
 
 export function createAccountListHelpers(channelKey: string) {
+  function resolveConfiguredDefaultAccountId(cfg: OpenClawConfig): string | undefined {
+    const channel = cfg.channels?.[channelKey] as Record<string, unknown> | undefined;
+    const preferred = normalizeOptionalAccountId(
+      typeof channel?.defaultAccount === "string" ? channel.defaultAccount : undefined,
+    );
+    if (!preferred) {
+      return undefined;
+    }
+    const ids = listAccountIds(cfg);
+    if (ids.some((id) => normalizeAccountId(id) === preferred)) {
+      return preferred;
+    }
+    return undefined;
+  }
+
   function listConfiguredAccountIds(cfg: OpenClawConfig): string[] {
     const channel = cfg.channels?.[channelKey];
     const accounts = (channel as Record<string, unknown> | undefined)?.accounts;
@@ -20,6 +39,10 @@ export function createAccountListHelpers(channelKey: string) {
   }
 
   function resolveDefaultAccountId(cfg: OpenClawConfig): string {
+    const preferred = resolveConfiguredDefaultAccountId(cfg);
+    if (preferred) {
+      return preferred;
+    }
     const ids = listAccountIds(cfg);
     if (ids.includes(DEFAULT_ACCOUNT_ID)) {
       return DEFAULT_ACCOUNT_ID;

remoteclaw/remoteclaw

(cherry picked from commit 41537e93039d6767acf34d303cf1d33e98f4b2c8)

extensions/nostr/src/types.ts
@@ -1,4 +1,9 @@
 import type { RemoteClawConfig } from "remoteclaw/plugin-sdk";
+import {
+  DEFAULT_ACCOUNT_ID,
+  normalizeAccountId,
+  normalizeOptionalAccountId,
+} from "remoteclaw/plugin-sdk/account-id";
 import type { NostrProfile } from "./config-schema.js";
 import { getPublicKeyFromPrivate } from "./nostr-bus.js";
 import { DEFAULT_RELAYS } from "./nostr-bus.js";
@@ -6,6 +11,7 @@ import { DEFAULT_RELAYS } from "./nostr-bus.js";
 export interface NostrAccountConfig {
   enabled?: boolean;
   name?: string;
+  defaultAccount?: string;
   privateKey?: string;
   relays?: string[];
   dmPolicy?: "pairing" | "allowlist" | "open" | "disabled";
@@ -25,7 +31,12 @@ export interface ResolvedNostrAccount {
   config: NostrAccountConfig;
 }
 
-const DEFAULT_ACCOUNT_ID = "default";
+function resolveConfiguredDefaultNostrAccountId(cfg: RemoteClawConfig): string | undefined {
+  const nostrCfg = (cfg.channels as Record<string, unknown> | undefined)?.nostr as
+    | NostrAccountConfig
+    | undefined;
+  return normalizeOptionalAccountId(nostrCfg?.defaultAccount);
+}
 
 /**
  * List all configured Nostr account IDs
@@ -37,7 +48,7 @@ export function listNostrAccountIds(cfg: RemoteClawConfig): string[] {
 
   // If privateKey is configured at top level, we have a default account
   if (nostrCfg?.privateKey) {
-    return [DEFAULT_ACCOUNT_ID];
+    return [resolveConfiguredDefaultNostrAccountId(cfg) ?? DEFAULT_ACCOUNT_ID];
   }
 
   return [];
@@ -47,6 +58,10 @@ export function listNostrAccountIds(cfg: RemoteClawConfig): string[] {
  * Get the default account ID
  */
 export function resolveDefaultNostrAccountId(cfg: RemoteClawConfig): string {
+  const preferred = resolveConfiguredDefaultNostrAccountId(cfg);
+  if (preferred) {
+    return preferred;
+  }
   const ids = listNostrAccountIds(cfg);
   if (ids.includes(DEFAULT_ACCOUNT_ID)) {
     return DEFAULT_ACCOUNT_ID;
@@ -61,7 +76,7 @@ export function resolveNostrAccount(opts: {
   cfg: RemoteClawConfig;
   accountId?: string | null;
 }): ResolvedNostrAccount {
-  const accountId = opts.accountId ?? DEFAULT_ACCOUNT_ID;
+  const accountId = normalizeAccountId(opts.accountId ?? resolveDefaultNostrAccountId(opts.cfg));
   const nostrCfg = (opts.cfg.channels as Record<string, unknown> | undefined)?.nostr as
     | NostrAccountConfig
     | undefined;

extensions/zalouser/src/accounts.ts
@@ -1,5 +1,9 @@
 import type { RemoteClawConfig } from "remoteclaw/plugin-sdk";
-import { DEFAULT_ACCOUNT_ID, normalizeAccountId } from "remoteclaw/plugin-sdk/account-id";
+import {
+  DEFAULT_ACCOUNT_ID,
+  normalizeAccountId,
+  normalizeOptionalAccountId,
+} from "remoteclaw/plugin-sdk/account-id";
 import type { ResolvedZalouserAccount, ZalouserAccountConfig, ZalouserConfig } from "./types.js";
 import { runZca, parseJsonOutput } from "./zca.js";
 
@@ -21,8 +25,12 @@ export function listZalouserAccountIds(cfg: RemoteClawConfig): string[] {
 
 export function resolveDefaultZalouserAccountId(cfg: RemoteClawConfig): string {
   const zalouserConfig = cfg.channels?.zalouser as ZalouserConfig | undefined;
-  if (zalouserConfig?.defaultAccount?.trim()) {
-    return zalouserConfig.defaultAccount.trim();
+  const preferred = normalizeOptionalAccountId(zalouserConfig?.defaultAccount);
+  if (
+    preferred &&
+    listZalouserAccountIds(cfg).some((accountId) => normalizeAccountId(accountId) === preferred)
+  ) {
+    return preferred;
   }
   const ids = listZalouserAccountIds(cfg);
   if (ids.includes(DEFAULT_ACCOUNT_ID)) {

src/channels/plugins/account-helpers.ts
@@ -1,7 +1,26 @@
 import type { RemoteClawConfig } from "../../config/config.js";
-import { DEFAULT_ACCOUNT_ID } from "../../routing/session-key.js";
+import {
+  DEFAULT_ACCOUNT_ID,
+  normalizeAccountId,
+  normalizeOptionalAccountId,
+} from "../../routing/session-key.js";
 
 export function createAccountListHelpers(channelKey: string) {
+  function resolveConfiguredDefaultAccountId(cfg: RemoteClawConfig): string | undefined {
+    const channel = cfg.channels?.[channelKey] as Record<string, unknown> | undefined;
+    const preferred = normalizeOptionalAccountId(
+      typeof channel?.defaultAccount === "string" ? channel.defaultAccount : undefined,
+    );
+    if (!preferred) {
+      return undefined;
+    }
+    const ids = listAccountIds(cfg);
+    if (ids.some((id) => normalizeAccountId(id) === preferred)) {
+      return preferred;
+    }
+    return undefined;
+  }
+
   function listConfiguredAccountIds(cfg: RemoteClawConfig): string[] {
     const channel = cfg.channels?.[channelKey];
     const accounts = (channel as Record<string, unknown> | undefined)?.accounts;
@@ -20,6 +39,10 @@ export function createAccountListHelpers(channelKey: string) {
   }
 
   function resolveDefaultAccountId(cfg: RemoteClawConfig): string {
+    const preferred = resolveConfiguredDefaultAccountId(cfg);
+    if (preferred) {
+      return preferred;
+    }
     const ids = listAccountIds(cfg);
     if (ids.includes(DEFAULT_ACCOUNT_ID)) {
       return DEFAULT_ACCOUNT_ID;
7e1b04d85a15cc3e7f78fad5df797220591f021b
Peter Steinberger <steipete@gmail.com>
2026-03-16 00:24:40 -0700

    refactor: finish plugin-owned channel runtime seams
    (cherry picked from commit 7964563299b2f561cca07d0597047797cc7867a1)

--- src/plugins/commands.test.ts
+++ src/plugins/commands.test.ts
@@ -1,10 +1,19 @@
-import { afterEach, describe, expect, it } from "vitest";
+import { afterEach, beforeEach, describe, expect, it } from "vitest";
+import { discordPlugin } from "../../extensions/discord/src/channel.js";
+import { createTestRegistry } from "../test-utils/channel-plugins.js";
 import {
   clearPluginCommands,
   getPluginCommandSpecs,
   listPluginCommands,
   registerPluginCommand,
 } from "./commands.js";
+import { setActivePluginRegistry } from "./runtime.js";
+
+beforeEach(() => {
+  setActivePluginRegistry(
+    createTestRegistry([{ pluginId: "discord", source: "test", plugin: discordPlugin }]),
+  );
+});
 
 afterEach(() => {
   clearPluginCommands();

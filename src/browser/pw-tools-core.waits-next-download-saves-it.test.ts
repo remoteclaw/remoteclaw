wonglaitung/openclaw_mini

test(browser): fix windows download tmp path assertions

src/browser/pw-tools-core.waits-next-download-saves-it.test.ts
@@ -200,11 +200,8 @@ describe("pw-tools-core", () => {
       suggestedFilename: "file.bin",
     });
     expect(typeof outPath).toBe("string");
-    const expectedRootedDownloadsDir = path.join(
-      path.sep,
-      "tmp",
-      "openclaw-preferred",
-      "downloads",
+    const expectedRootedDownloadsDir = path.resolve(
+      path.join(path.sep, "tmp", "openclaw-preferred", "downloads"),
     );
     const expectedDownloadsTail = `${path.join("tmp", "openclaw-preferred", "downloads")}${path.sep}`;
     expect(path.dirname(String(outPath))).toBe(expectedRootedDownloadsDir);
@@ -221,7 +218,7 @@ describe("pw-tools-core", () => {
     });
     expect(typeof outPath).toBe("string");
     expect(path.dirname(String(outPath))).toBe(
-      path.join(path.sep, "tmp", "openclaw-preferred", "downloads"),
+      path.resolve(path.join(path.sep, "tmp", "openclaw-preferred", "downloads")),
     );
     expect(path.basename(String(outPath))).toMatch(/-passwd$/);
     expect(path.normalize(res.path)).toContain(

remoteclaw/remoteclaw

(cherry picked from commit 949200d7cb8e9a1ab1fb7f3e82d5f6fac54a8b30)

src/browser/pw-tools-core.waits-next-download-saves-it.test.ts
@@ -200,11 +200,8 @@ describe("pw-tools-core", () => {
       suggestedFilename: "file.bin",
     });
     expect(typeof outPath).toBe("string");
-    const expectedRootedDownloadsDir = path.join(
-      path.sep,
-      "tmp",
-      "remoteclaw-preferred",
-      "downloads",
+    const expectedRootedDownloadsDir = path.resolve(
+      path.join(path.sep, "tmp", "remoteclaw-preferred", "downloads"),
     );
     const expectedDownloadsTail = `${path.join("tmp", "remoteclaw-preferred", "downloads")}${path.sep}`;
     expect(path.dirname(String(outPath))).toBe(expectedRootedDownloadsDir);
@@ -221,7 +218,7 @@ describe("pw-tools-core", () => {
     });
     expect(typeof outPath).toBe("string");
     expect(path.dirname(String(outPath))).toBe(
-      path.join(path.sep, "tmp", "remoteclaw-preferred", "downloads"),
+      path.resolve(path.join(path.sep, "tmp", "remoteclaw-preferred", "downloads")),
     );
     expect(path.basename(String(outPath))).toMatch(/-passwd$/);
     expect(path.normalize(res.path)).toContain(
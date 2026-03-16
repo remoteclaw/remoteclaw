nigoshu-roby/openclaw-roby

fix(infra): land #29078 from @cathrynlavery with restart fallback
Co-authored-by: Cathryn Lavery <cathryn@littlemight.com>

src/infra/process-respawn.test.ts
@@ -3,9 +3,13 @@ import { captureFullEnv } from "../test-utils/env.js";
 import { SUPERVISOR_HINT_ENV_VARS } from "./supervisor-markers.js";
 
 const spawnMock = vi.hoisted(() => vi.fn());
+const triggerOpenClawRestartMock = vi.hoisted(() => vi.fn());
 
 vi.mock("node:child_process", () => ({
   spawn: (...args: unknown[]) => spawnMock(...args),
+}));
+vi.mock("./restart.js", () => ({
+  triggerOpenClawRestart: (...args: unknown[]) => triggerOpenClawRestartMock(...args),
 }));
 
 import { restartGatewayProcessWithFreshPid } from "./process-respawn.js";
@@ -30,6 +34,7 @@ afterEach(() => {
   process.argv = [...originalArgv];
   process.execArgv = [...originalExecArgv];
   spawnMock.mockClear();
+  triggerOpenClawRestartMock.mockClear();
   if (originalPlatformDescriptor) {
     Object.defineProperty(process, "platform", originalPlatformDescriptor);
   }
@@ -56,40 +61,33 @@ describe("restartGatewayProcessWithFreshPid", () => {
     expect(spawnMock).not.toHaveBeenCalled();
   });
 
-  it("schedules detached launchctl kickstart on macOS when launchd label is set", () => {
+  it("runs launchd kickstart helper on macOS when launchd label is set", () => {
     setPlatform("darwin");
     process.env.LAUNCH_JOB_LABEL = "ai.openclaw.gateway";
     process.env.OPENCLAW_LAUNCHD_LABEL = "ai.openclaw.gateway";
-    const unrefMock = vi.fn();
-    spawnMock.mockReturnValue({ unref: unrefMock, on: vi.fn() });
+    triggerOpenClawRestartMock.mockReturnValue({ ok: true, method: "launchctl" });
 
     const result = restartGatewayProcessWithFreshPid();
 
     expect(result.mode).toBe("supervised");
-    expect(spawnMock).toHaveBeenCalledWith(
-      "launchctl",
-      ["kickstart", "-k", expect.stringContaining("ai.openclaw.gateway")],
-      expect.objectContaining({ detached: true, stdio: "ignore" }),
-    );
-    expect(unrefMock).toHaveBeenCalledOnce();
+    expect(triggerOpenClawRestartMock).toHaveBeenCalledOnce();
+    expect(spawnMock).not.toHaveBeenCalled();
   });
 
-  it("still returns supervised even if kickstart spawn throws", () => {
+  it("returns failed when launchd kickstart helper fails", () => {
     setPlatform("darwin");
     process.env.LAUNCH_JOB_LABEL = "ai.openclaw.gateway";
     process.env.OPENCLAW_LAUNCHD_LABEL = "ai.openclaw.gateway";
-    spawnMock.mockImplementation((...args: unknown[]) => {
-      const [cmd] = args as [string];
-      if (cmd === "launchctl") {
-        throw new Error("spawn failed");
-      }
-      return { unref: vi.fn(), on: vi.fn() };
+    triggerOpenClawRestartMock.mockReturnValue({
+      ok: false,
+      method: "launchctl",
+      detail: "spawn failed",
     });
 
     const result = restartGatewayProcessWithFreshPid();
 
-    // Kickstart is best-effort; failure should not block supervised exit
-    expect(result.mode).toBe("supervised");
+    expect(result.mode).toBe("failed");
+    expect(result.detail).toContain("spawn failed");
   });
 
   it("does not schedule kickstart on non-darwin platforms", () => {
@@ -100,6 +98,7 @@ describe("restartGatewayProcessWithFreshPid", () => {
     const result = restartGatewayProcessWithFreshPid();
 
     expect(result.mode).toBe("supervised");
+    expect(triggerOpenClawRestartMock).not.toHaveBeenCalled();
     expect(spawnMock).not.toHaveBeenCalled();
   });
 
@@ -127,16 +126,11 @@ describe("restartGatewayProcessWithFreshPid", () => {
     clearSupervisorHints();
     setPlatform("darwin");
     process.env.OPENCLAW_LAUNCHD_LABEL = "ai.openclaw.gateway";
-    const unrefMock = vi.fn();
-    spawnMock.mockReturnValue({ unref: unrefMock, on: vi.fn() });
+    triggerOpenClawRestartMock.mockReturnValue({ ok: true, method: "launchctl" });
     const result = restartGatewayProcessWithFreshPid();
     expect(result.mode).toBe("supervised");
-    expect(spawnMock).toHaveBeenCalledWith(
-      "launchctl",
-      expect.arrayContaining(["kickstart", "-k"]),
-      expect.objectContaining({ detached: true }),
-    );
-    expect(unrefMock).toHaveBeenCalledOnce();
+    expect(triggerOpenClawRestartMock).toHaveBeenCalledOnce();
+    expect(spawnMock).not.toHaveBeenCalled();
   });
 
   it("returns supervised when OPENCLAW_SYSTEMD_UNIT is set", () => {

src/infra/process-respawn.ts
@@ -1,4 +1,5 @@
 import { spawn } from "node:child_process";
+import { triggerOpenClawRestart } from "./restart.js";
 import { hasSupervisorHint } from "./supervisor-markers.js";
 
 type RespawnMode = "spawned" | "supervised" | "disabled" | "failed";
@@ -22,29 +23,6 @@ function isLikelySupervisedProcess(env: NodeJS.ProcessEnv = process.env): boolea
 }
 
 /**
- * Spawn a detached `launchctl kickstart -k` to force an immediate launchd
- * restart, bypassing ThrottleInterval.  The -k flag sends SIGTERM to the
- * current process, so this MUST be non-blocking (spawn, not spawnSync) to
- * avoid deadlocking — the gateway needs to be free to handle the signal
- * and exit so launchd can start the replacement.
- */
-function schedulelaunchdKickstart(label: string): boolean {
-  const uid = typeof process.getuid === "function" ? process.getuid() : undefined;
-  const target = uid !== undefined ? `gui/${uid}/${label}` : label;
-  try {
-    const child = spawn("launchctl", ["kickstart", "-k", target], {
-      detached: true,
-      stdio: "ignore",
-    });
-    child.on("error", () => {}); // best-effort; suppress uncaught error event
-    child.unref();
-    return true;
-  } catch {
-    return false;
-  }
-}
-
-/**
  * Attempt to restart this process with a fresh PID.
  * - supervised environments (launchd/systemd): caller should exit and let supervisor restart
  * - OPENCLAW_NO_RESPAWN=1: caller should keep in-process restart behavior (tests/dev)
@@ -55,10 +33,16 @@ export function restartGatewayProcessWithFreshPid(): GatewayRespawnResult {
     return { mode: "disabled" };
   }
   if (isLikelySupervisedProcess(process.env)) {
-    // On macOS under launchd, fire a detached kickstart so launchd restarts
-    // us immediately instead of waiting for ThrottleInterval (up to 60s).
+    // On macOS under launchd, actively kickstart the supervised service to
+    // bypass ThrottleInterval delays for intentional restarts.
     if (process.platform === "darwin" && process.env.OPENCLAW_LAUNCHD_LABEL?.trim()) {
-      schedulelaunchdKickstart(process.env.OPENCLAW_LAUNCHD_LABEL.trim());
+      const restart = triggerOpenClawRestart();
+      if (!restart.ok) {
+        return {
+          mode: "failed",
+          detail: restart.detail ?? "launchctl kickstart failed",
+        };
+      }
     }
     return { mode: "supervised" };
   }

remoteclaw/remoteclaw

(cherry picked from commit 4aa2dc685792a53e5134aa911b14dd7a291d9e41)

src/infra/process-respawn.test.ts
@@ -3,9 +3,13 @@ import { captureFullEnv } from "../test-utils/env.js";
 import { SUPERVISOR_HINT_ENV_VARS } from "./supervisor-markers.js";
 
 const spawnMock = vi.hoisted(() => vi.fn());
+const triggerRemoteClawRestartMock = vi.hoisted(() => vi.fn());
 
 vi.mock("node:child_process", () => ({
   spawn: (...args: unknown[]) => spawnMock(...args),
+}));
+vi.mock("./restart.js", () => ({
+  triggerRemoteClawRestart: (...args: unknown[]) => triggerRemoteClawRestartMock(...args),
 }));
 
 import { restartGatewayProcessWithFreshPid } from "./process-respawn.js";
@@ -30,6 +34,7 @@ afterEach(() => {
   process.argv = [...originalArgv];
   process.execArgv = [...originalExecArgv];
   spawnMock.mockClear();
+  triggerRemoteClawRestartMock.mockClear();
   if (originalPlatformDescriptor) {
     Object.defineProperty(process, "platform", originalPlatformDescriptor);
   }
@@ -56,50 +61,44 @@ describe("restartGatewayProcessWithFreshPid", () => {
     expect(spawnMock).not.toHaveBeenCalled();
   });
 
-  it("schedules detached launchctl kickstart on macOS when launchd label is set", () => {
+  it("runs launchd kickstart helper on macOS when launchd label is set", () => {
     setPlatform("darwin");
-    process.env.LAUNCH_JOB_LABEL = "ai.openclaw.gateway";
-    process.env.OPENCLAW_LAUNCHD_LABEL = "ai.openclaw.gateway";
-    const unrefMock = vi.fn();
-    spawnMock.mockReturnValue({ unref: unrefMock, on: vi.fn() });
+    process.env.LAUNCH_JOB_LABEL = "ai.remoteclaw.gateway";
+    process.env.REMOTECLAW_LAUNCHD_LABEL = "ai.remoteclaw.gateway";
+    triggerRemoteClawRestartMock.mockReturnValue({ ok: true, method: "launchctl" });
 
     const result = restartGatewayProcessWithFreshPid();
 
     expect(result.mode).toBe("supervised");
-    expect(spawnMock).toHaveBeenCalledWith(
-      "launchctl",
-      ["kickstart", "-k", expect.stringContaining("ai.openclaw.gateway")],
-      expect.objectContaining({ detached: true, stdio: "ignore" }),
-    );
-    expect(unrefMock).toHaveBeenCalledOnce();
+    expect(triggerRemoteClawRestartMock).toHaveBeenCalledOnce();
+    expect(spawnMock).not.toHaveBeenCalled();
   });
 
-  it("still returns supervised even if kickstart spawn throws", () => {
+  it("returns failed when launchd kickstart helper fails", () => {
     setPlatform("darwin");
-    process.env.LAUNCH_JOB_LABEL = "ai.openclaw.gateway";
-    process.env.OPENCLAW_LAUNCHD_LABEL = "ai.openclaw.gateway";
-    spawnMock.mockImplementation((...args: unknown[]) => {
-      const [cmd] = args as [string];
-      if (cmd === "launchctl") {
-        throw new Error("spawn failed");
-      }
-      return { unref: vi.fn(), on: vi.fn() };
+    process.env.LAUNCH_JOB_LABEL = "ai.remoteclaw.gateway";
+    process.env.REMOTECLAW_LAUNCHD_LABEL = "ai.remoteclaw.gateway";
+    triggerRemoteClawRestartMock.mockReturnValue({
+      ok: false,
+      method: "launchctl",
+      detail: "spawn failed",
     });
 
     const result = restartGatewayProcessWithFreshPid();
 
-    // Kickstart is best-effort; failure should not block supervised exit
-    expect(result.mode).toBe("supervised");
+    expect(result.mode).toBe("failed");
+    expect(result.detail).toContain("spawn failed");
   });
 
   it("does not schedule kickstart on non-darwin platforms", () => {
     setPlatform("linux");
     process.env.INVOCATION_ID = "abc123";
-    process.env.OPENCLAW_LAUNCHD_LABEL = "ai.openclaw.gateway";
+    process.env.REMOTECLAW_LAUNCHD_LABEL = "ai.remoteclaw.gateway";
 
     const result = restartGatewayProcessWithFreshPid();
 
     expect(result.mode).toBe("supervised");
+    expect(triggerRemoteClawRestartMock).not.toHaveBeenCalled();
     expect(spawnMock).not.toHaveBeenCalled();
   });
 
@@ -127,16 +126,11 @@ describe("restartGatewayProcessWithFreshPid", () => {
     clearSupervisorHints();
     setPlatform("darwin");
     process.env.REMOTECLAW_LAUNCHD_LABEL = "ai.remoteclaw.gateway";
-    const unrefMock = vi.fn();
-    spawnMock.mockReturnValue({ unref: unrefMock, on: vi.fn() });
+    triggerRemoteClawRestartMock.mockReturnValue({ ok: true, method: "launchctl" });
     const result = restartGatewayProcessWithFreshPid();
     expect(result.mode).toBe("supervised");
-    expect(spawnMock).toHaveBeenCalledWith(
-      "launchctl",
-      expect.arrayContaining(["kickstart", "-k"]),
-      expect.objectContaining({ detached: true }),
-    );
-    expect(unrefMock).toHaveBeenCalledOnce();
+    expect(triggerRemoteClawRestartMock).toHaveBeenCalledOnce();
+    expect(spawnMock).not.toHaveBeenCalled();
   });
 
   it("returns supervised when REMOTECLAW_SYSTEMD_UNIT is set", () => {

src/infra/process-respawn.ts
@@ -1,4 +1,5 @@
 import { spawn } from "node:child_process";
+import { triggerRemoteClawRestart } from "./restart.js";
 import { hasSupervisorHint } from "./supervisor-markers.js";
 
 type RespawnMode = "spawned" | "supervised" | "disabled" | "failed";
@@ -22,29 +23,6 @@ function isLikelySupervisedProcess(env: NodeJS.ProcessEnv = process.env): boolea
 }
 
 /**
- * Spawn a detached `launchctl kickstart -k` to force an immediate launchd
- * restart, bypassing ThrottleInterval.  The -k flag sends SIGTERM to the
- * current process, so this MUST be non-blocking (spawn, not spawnSync) to
- * avoid deadlocking — the gateway needs to be free to handle the signal
- * and exit so launchd can start the replacement.
- */
-function schedulelaunchdKickstart(label: string): boolean {
-  const uid = typeof process.getuid === "function" ? process.getuid() : undefined;
-  const target = uid !== undefined ? `gui/${uid}/${label}` : label;
-  try {
-    const child = spawn("launchctl", ["kickstart", "-k", target], {
-      detached: true,
-      stdio: "ignore",
-    });
-    child.on("error", () => {}); // best-effort; suppress uncaught error event
-    child.unref();
-    return true;
-  } catch {
-    return false;
-  }
-}
-
-/**
  * Attempt to restart this process with a fresh PID.
  * - supervised environments (launchd/systemd): caller should exit and let supervisor restart
  * - REMOTECLAW_NO_RESPAWN=1: caller should keep in-process restart behavior (tests/dev)
@@ -55,10 +33,16 @@ export function restartGatewayProcessWithFreshPid(): GatewayRespawnResult {
     return { mode: "disabled" };
   }
   if (isLikelySupervisedProcess(process.env)) {
-    // On macOS under launchd, fire a detached kickstart so launchd restarts
-    // us immediately instead of waiting for ThrottleInterval (up to 60s).
-    if (process.platform === "darwin" && process.env.OPENCLAW_LAUNCHD_LABEL?.trim()) {
-      schedulelaunchdKickstart(process.env.OPENCLAW_LAUNCHD_LABEL.trim());
+    // On macOS under launchd, actively kickstart the supervised service to
+    // bypass ThrottleInterval delays for intentional restarts.
+    if (process.platform === "darwin" && process.env.REMOTECLAW_LAUNCHD_LABEL?.trim()) {
+      const restart = triggerRemoteClawRestart();
+      if (!restart.ok) {
+        return {
+          mode: "failed",
+          detail: restart.detail ?? "launchctl kickstart failed",
+        };
+      }
     }
     return { mode: "supervised" };
   }
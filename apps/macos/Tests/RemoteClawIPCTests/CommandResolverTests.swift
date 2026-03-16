wonglaitung/openclaw_mini

style(swift): apply swiftformat and swiftlint fixes

apps/macos/Tests/OpenClawIPCTests/CommandResolverTests.swift
@@ -24,7 +24,7 @@ import Testing
         try FileManager().setAttributes([.posixPermissions: 0o755], ofItemAtPath: path.path)
     }
 
-    @Test func prefersOpenClawBinary() async throws {
+    @Test func prefersOpenClawBinary() throws {
         let defaults = self.makeDefaults()
         defaults.set(AppState.ConnectionMode.local.rawValue, forKey: connectionModeKey)
 
@@ -38,7 +38,7 @@ import Testing
         #expect(cmd.prefix(2).elementsEqual([openclawPath.path, "gateway"]))
     }
 
-    @Test func fallsBackToNodeAndScript() async throws {
+    @Test func fallsBackToNodeAndScript() throws {
         let defaults = self.makeDefaults()
         defaults.set(AppState.ConnectionMode.local.rawValue, forKey: connectionModeKey)
 
@@ -66,7 +66,7 @@ import Testing
         }
     }
 
-    @Test func prefersOpenClawBinaryOverPnpm() async throws {
+    @Test func prefersOpenClawBinaryOverPnpm() throws {
         let defaults = self.makeDefaults()
         defaults.set(AppState.ConnectionMode.local.rawValue, forKey: connectionModeKey)
 
@@ -88,7 +88,7 @@ import Testing
         #expect(cmd.prefix(2).elementsEqual([openclawPath.path, "rpc"]))
     }
 
-    @Test func usesOpenClawBinaryWithoutNodeRuntime() async throws {
+    @Test func usesOpenClawBinaryWithoutNodeRuntime() throws {
         let defaults = self.makeDefaults()
         defaults.set(AppState.ConnectionMode.local.rawValue, forKey: connectionModeKey)
 
@@ -108,7 +108,7 @@ import Testing
         #expect(cmd.prefix(2).elementsEqual([openclawPath.path, "gateway"]))
     }
 
-    @Test func fallsBackToPnpm() async throws {
+    @Test func fallsBackToPnpm() throws {
         let defaults = self.makeDefaults()
         defaults.set(AppState.ConnectionMode.local.rawValue, forKey: connectionModeKey)
 
@@ -127,7 +127,7 @@ import Testing
         #expect(cmd.prefix(4).elementsEqual([pnpmPath.path, "--silent", "openclaw", "rpc"]))
     }
 
-    @Test func pnpmKeepsExtraArgsAfterSubcommand() async throws {
+    @Test func pnpmKeepsExtraArgsAfterSubcommand() throws {
         let defaults = self.makeDefaults()
         defaults.set(AppState.ConnectionMode.local.rawValue, forKey: connectionModeKey)
 
@@ -148,7 +148,7 @@ import Testing
         #expect(cmd.suffix(2).elementsEqual(["--timeout", "5"]))
     }
 
-    @Test func preferredPathsStartWithProjectNodeBins() async throws {
+    @Test func preferredPathsStartWithProjectNodeBins() throws {
         let tmp = try makeTempDir()
         CommandResolver.setProjectRoot(tmp.path)
 
@@ -156,7 +156,7 @@ import Testing
         #expect(first == tmp.appendingPathComponent("node_modules/.bin").path)
     }
 
-    @Test func buildsSSHCommandForRemoteMode() async throws {
+    @Test func buildsSSHCommandForRemoteMode() {
         let defaults = self.makeDefaults()
         defaults.set(AppState.ConnectionMode.remote.rawValue, forKey: connectionModeKey)
         defaults.set("openclaw@example.com:2222", forKey: remoteTargetKey)
@@ -187,13 +187,13 @@ import Testing
         }
     }
 
-    @Test func rejectsUnsafeSSHTargets() async throws {
+    @Test func rejectsUnsafeSSHTargets() {
         #expect(CommandResolver.parseSSHTarget("-oProxyCommand=calc") == nil)
         #expect(CommandResolver.parseSSHTarget("host:-oProxyCommand=calc") == nil)
         #expect(CommandResolver.parseSSHTarget("user@host:2222")?.port == 2222)
     }
 
-    @Test func configRootLocalOverridesRemoteDefaults() async throws {
+    @Test func configRootLocalOverridesRemoteDefaults() throws {
         let defaults = self.makeDefaults()
         defaults.set(AppState.ConnectionMode.remote.rawValue, forKey: connectionModeKey)
         defaults.set("openclaw@example.com:2222", forKey: remoteTargetKey)

remoteclaw/remoteclaw

(cherry picked from commit 7b3f506e64c1a511120fadd19c5727e20fac5de2)

apps/macos/Tests/RemoteClawIPCTests/CommandResolverTests.swift
@@ -24,7 +24,7 @@ import Testing
         try FileManager().setAttributes([.posixPermissions: 0o755], ofItemAtPath: path.path)
     }
 
-    @Test func prefersRemoteClawBinary() async throws {
+    @Test func prefersRemoteClawBinary() throws {
         let defaults = self.makeDefaults()
         defaults.set(AppState.ConnectionMode.local.rawValue, forKey: connectionModeKey)
 
@@ -38,7 +38,7 @@ import Testing
         #expect(cmd.prefix(2).elementsEqual([remoteclawPath.path, "gateway"]))
     }
 
-    @Test func fallsBackToNodeAndScript() async throws {
+    @Test func fallsBackToNodeAndScript() throws {
         let defaults = self.makeDefaults()
         defaults.set(AppState.ConnectionMode.local.rawValue, forKey: connectionModeKey)
 
@@ -66,7 +66,7 @@ import Testing
         }
     }
 
-    @Test func prefersRemoteClawBinaryOverPnpm() async throws {
+    @Test func prefersRemoteClawBinaryOverPnpm() throws {
         let defaults = self.makeDefaults()
         defaults.set(AppState.ConnectionMode.local.rawValue, forKey: connectionModeKey)
 
@@ -88,7 +88,7 @@ import Testing
         #expect(cmd.prefix(2).elementsEqual([remoteclawPath.path, "rpc"]))
     }
 
-    @Test func usesRemoteClawBinaryWithoutNodeRuntime() async throws {
+    @Test func usesRemoteClawBinaryWithoutNodeRuntime() throws {
         let defaults = self.makeDefaults()
         defaults.set(AppState.ConnectionMode.local.rawValue, forKey: connectionModeKey)
 
@@ -108,7 +108,7 @@ import Testing
         #expect(cmd.prefix(2).elementsEqual([remoteclawPath.path, "gateway"]))
     }
 
-    @Test func fallsBackToPnpm() async throws {
+    @Test func fallsBackToPnpm() throws {
         let defaults = self.makeDefaults()
         defaults.set(AppState.ConnectionMode.local.rawValue, forKey: connectionModeKey)
 
@@ -127,7 +127,7 @@ import Testing
         #expect(cmd.prefix(4).elementsEqual([pnpmPath.path, "--silent", "remoteclaw", "rpc"]))
     }
 
-    @Test func pnpmKeepsExtraArgsAfterSubcommand() async throws {
+    @Test func pnpmKeepsExtraArgsAfterSubcommand() throws {
         let defaults = self.makeDefaults()
         defaults.set(AppState.ConnectionMode.local.rawValue, forKey: connectionModeKey)
 
@@ -148,7 +148,7 @@ import Testing
         #expect(cmd.suffix(2).elementsEqual(["--timeout", "5"]))
     }
 
-    @Test func preferredPathsStartWithProjectNodeBins() async throws {
+    @Test func preferredPathsStartWithProjectNodeBins() throws {
         let tmp = try makeTempDir()
         CommandResolver.setProjectRoot(tmp.path)
 
@@ -156,7 +156,7 @@ import Testing
         #expect(first == tmp.appendingPathComponent("node_modules/.bin").path)
     }
 
-    @Test func buildsSSHCommandForRemoteMode() async throws {
+    @Test func buildsSSHCommandForRemoteMode() {
         let defaults = self.makeDefaults()
         defaults.set(AppState.ConnectionMode.remote.rawValue, forKey: connectionModeKey)
         defaults.set("remoteclaw@example.com:2222", forKey: remoteTargetKey)
@@ -187,13 +187,13 @@ import Testing
         }
     }
 
-    @Test func rejectsUnsafeSSHTargets() async throws {
+    @Test func rejectsUnsafeSSHTargets() {
         #expect(CommandResolver.parseSSHTarget("-oProxyCommand=calc") == nil)
         #expect(CommandResolver.parseSSHTarget("host:-oProxyCommand=calc") == nil)
         #expect(CommandResolver.parseSSHTarget("user@host:2222")?.port == 2222)
     }
 
-    @Test func configRootLocalOverridesRemoteDefaults() async throws {
+    @Test func configRootLocalOverridesRemoteDefaults() throws {
         let defaults = self.makeDefaults()
         defaults.set(AppState.ConnectionMode.remote.rawValue, forKey: connectionModeKey)
         defaults.set("remoteclaw@example.com:2222", forKey: remoteTargetKey)
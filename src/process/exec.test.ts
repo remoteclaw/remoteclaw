wonglaitung/openclaw_mini

refactor(process): extract command env resolution helper

src/process/exec.test.ts
@@ -2,9 +2,8 @@ import type { ChildProcess } from "node:child_process";
 import { EventEmitter } from "node:events";
 import process from "node:process";
 import { describe, expect, it, vi } from "vitest";
-import { withEnvAsync } from "../test-utils/env.js";
 import { attachChildProcessBridge } from "./child-process-bridge.js";
-import { runCommandWithTimeout, shouldSpawnWithShell } from "./exec.js";
+import { resolveCommandEnv, runCommandWithTimeout, shouldSpawnWithShell } from "./exec.js";
 
 describe("runCommandWithTimeout", () => {
   it("never enables shell execution (Windows cmd.exe injection hardening)", () => {
@@ -16,24 +15,31 @@ describe("runCommandWithTimeout", () => {
     ).toBe(false);
   });
 
-  it("merges custom env with process.env", async () => {
-    await withEnvAsync({ OPENCLAW_BASE_ENV: "base" }, async () => {
-      const result = await runCommandWithTimeout(
-        [
-          process.execPath,
-          "-e",
-          'process.stdout.write((process.env.OPENCLAW_BASE_ENV ?? "") + "|" + (process.env.OPENCLAW_TEST_ENV ?? ""))',
-        ],
-        {
-          timeoutMs: 80,
-          env: { OPENCLAW_TEST_ENV: "ok" },
-        },
-      );
-
-      expect(result.code).toBe(0);
-      expect(result.stdout).toBe("base|ok");
-      expect(result.termination).toBe("exit");
+  it("merges custom env with base env and drops undefined values", async () => {
+    const resolved = resolveCommandEnv({
+      argv: ["node", "script.js"],
+      baseEnv: {
+        OPENCLAW_BASE_ENV: "base",
+        OPENCLAW_TO_REMOVE: undefined,
+      },
+      env: {
+        OPENCLAW_TEST_ENV: "ok",
+      },
     });
+
+    expect(resolved.OPENCLAW_BASE_ENV).toBe("base");
+    expect(resolved.OPENCLAW_TEST_ENV).toBe("ok");
+    expect(resolved.OPENCLAW_TO_REMOVE).toBeUndefined();
+  });
+
+  it("suppresses npm fund prompts for npm argv", async () => {
+    const resolved = resolveCommandEnv({
+      argv: ["npm", "--version"],
+      baseEnv: {},
+    });
+
+    expect(resolved.NPM_CONFIG_FUND).toBe("false");
+    expect(resolved.npm_config_fund).toBe("false");
   });
 
   it("kills command when no output timeout elapses", async () => {

remoteclaw/remoteclaw

(cherry picked from commit 234e07fcc0f2577f2a6967f13470e8700f3ff765)

src/process/exec.test.ts
@@ -2,9 +2,8 @@ import type { ChildProcess } from "node:child_process";
 import { EventEmitter } from "node:events";
 import process from "node:process";
 import { describe, expect, it, vi } from "vitest";
-import { withEnvAsync } from "../test-utils/env.js";
 import { attachChildProcessBridge } from "./child-process-bridge.js";
-import { runCommandWithTimeout, shouldSpawnWithShell } from "./exec.js";
+import { resolveCommandEnv, runCommandWithTimeout, shouldSpawnWithShell } from "./exec.js";
 
 describe("runCommandWithTimeout", () => {
   it("never enables shell execution (Windows cmd.exe injection hardening)", () => {
@@ -16,24 +15,31 @@ describe("runCommandWithTimeout", () => {
     ).toBe(false);
   });
 
-  it("merges custom env with process.env", async () => {
-    await withEnvAsync({ REMOTECLAW_BASE_ENV: "base" }, async () => {
-      const result = await runCommandWithTimeout(
-        [
-          process.execPath,
-          "-e",
-          'process.stdout.write((process.env.REMOTECLAW_BASE_ENV ?? "") + "|" + (process.env.REMOTECLAW_TEST_ENV ?? ""))',
-        ],
-        {
-          timeoutMs: 80,
-          env: { REMOTECLAW_TEST_ENV: "ok" },
-        },
-      );
-
-      expect(result.code).toBe(0);
-      expect(result.stdout).toBe("base|ok");
-      expect(result.termination).toBe("exit");
+  it("merges custom env with base env and drops undefined values", async () => {
+    const resolved = resolveCommandEnv({
+      argv: ["node", "script.js"],
+      baseEnv: {
+        REMOTECLAW_BASE_ENV: "base",
+        REMOTECLAW_TO_REMOVE: undefined,
+      },
+      env: {
+        REMOTECLAW_TEST_ENV: "ok",
+      },
     });
+
+    expect(resolved.REMOTECLAW_BASE_ENV).toBe("base");
+    expect(resolved.REMOTECLAW_TEST_ENV).toBe("ok");
+    expect(resolved.REMOTECLAW_TO_REMOVE).toBeUndefined();
+  });
+
+  it("suppresses npm fund prompts for npm argv", async () => {
+    const resolved = resolveCommandEnv({
+      argv: ["npm", "--version"],
+      baseEnv: {},
+    });
+
+    expect(resolved.NPM_CONFIG_FUND).toBe("false");
+    expect(resolved.npm_config_fund).toBe("false");
   });
 
   it("kills command when no output timeout elapses", async () => {
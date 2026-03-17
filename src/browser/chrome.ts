wonglaitung/openclaw_mini

refactor: split browser context/actions and unify CDP timeout policy

src/browser/chrome.ts
@@ -5,6 +5,17 @@ import path from "node:path";
 import { ensurePortAvailable } from "../infra/ports.js";
 import { createSubsystemLogger } from "../logging/subsystem.js";
 import { CONFIG_DIR } from "../utils.js";
+import {
+  CHROME_BOOTSTRAP_EXIT_TIMEOUT_MS,
+  CHROME_BOOTSTRAP_PREFS_TIMEOUT_MS,
+  CHROME_LAUNCH_READY_POLL_MS,
+  CHROME_LAUNCH_READY_WINDOW_MS,
+  CHROME_REACHABILITY_TIMEOUT_MS,
+  CHROME_STDERR_HINT_MAX_CHARS,
+  CHROME_STOP_PROBE_TIMEOUT_MS,
+  CHROME_STOP_TIMEOUT_MS,
+  CHROME_WS_READY_TIMEOUT_MS,
+} from "./cdp-timeouts.js";
 import { appendCdpPath, fetchCdpChecked, openCdpWebSocket } from "./cdp.helpers.js";
 import { normalizeCdpWsUrl } from "./cdp.js";
 import {
@@ -66,7 +77,10 @@ function cdpUrlForPort(cdpPort: number) {
   return `http://127.0.0.1:${cdpPort}`;
 }
 
-export async function isChromeReachable(cdpUrl: string, timeoutMs = 500): Promise<boolean> {
+export async function isChromeReachable(
+  cdpUrl: string,
+  timeoutMs = CHROME_REACHABILITY_TIMEOUT_MS,
+): Promise<boolean> {
   const version = await fetchChromeVersion(cdpUrl, timeoutMs);
   return Boolean(version);
 }
@@ -77,7 +91,10 @@ type ChromeVersion = {
   "User-Agent"?: string;
 };
 
-async function fetchChromeVersion(cdpUrl: string, timeoutMs = 500): Promise<ChromeVersion | null> {
+async function fetchChromeVersion(
+  cdpUrl: string,
+  timeoutMs = CHROME_REACHABILITY_TIMEOUT_MS,
+): Promise<ChromeVersion | null> {
   const ctrl = new AbortController();
   const t = setTimeout(ctrl.abort.bind(ctrl), timeoutMs);
   try {
@@ -97,7 +114,7 @@ async function fetchChromeVersion(cdpUrl: string, timeoutMs = 500): Promise<Chro
 
 export async function getChromeWebSocketUrl(
   cdpUrl: string,
-  timeoutMs = 500,
+  timeoutMs = CHROME_REACHABILITY_TIMEOUT_MS,
 ): Promise<string | null> {
   const version = await fetchChromeVersion(cdpUrl, timeoutMs);
   const wsUrl = String(version?.webSocketDebuggerUrl ?? "").trim();
@@ -107,7 +124,10 @@ export async function getChromeWebSocketUrl(
   return normalizeCdpWsUrl(wsUrl, cdpUrl);
 }
 
-async function canOpenWebSocket(wsUrl: string, timeoutMs = 800): Promise<boolean> {
+async function canOpenWebSocket(
+  wsUrl: string,
+  timeoutMs = CHROME_WS_READY_TIMEOUT_MS,
+): Promise<boolean> {
   return await new Promise<boolean>((resolve) => {
     const ws = openCdpWebSocket(wsUrl, {
       handshakeTimeoutMs: timeoutMs,
@@ -141,8 +161,8 @@ async function canOpenWebSocket(wsUrl: string, timeoutMs = 800): Promise<boolean
 
 export async function isChromeCdpReady(
   cdpUrl: string,
-  timeoutMs = 500,
-  handshakeTimeoutMs = 800,
+  timeoutMs = CHROME_REACHABILITY_TIMEOUT_MS,
+  handshakeTimeoutMs = CHROME_WS_READY_TIMEOUT_MS,
 ): Promise<boolean> {
   const wsUrl = await getChromeWebSocketUrl(cdpUrl, timeoutMs);
   if (!wsUrl) {
@@ -236,7 +256,7 @@ export async function launchOpenClawChrome(
   // Then decorate (if needed) before the "real" run.
   if (needsBootstrap) {
     const bootstrap = spawnOnce();
-    const deadline = Date.now() + 10_000;
+    const deadline = Date.now() + CHROME_BOOTSTRAP_PREFS_TIMEOUT_MS;
     while (Date.now() < deadline) {
       if (exists(localStatePath) && exists(preferencesPath)) {
         break;
@@ -248,7 +268,7 @@ export async function launchOpenClawChrome(
     } catch {
       // ignore
     }
-    const exitDeadline = Date.now() + 5000;
+    const exitDeadline = Date.now() + CHROME_BOOTSTRAP_EXIT_TIMEOUT_MS;
     while (Date.now() < exitDeadline) {
       if (bootstrap.exitCode != null) {
         break;
@@ -287,17 +307,19 @@ export async function launchOpenClawChrome(
   proc.stderr?.on("data", onStderr);
 
   // Wait for CDP to come up.
-  const readyDeadline = Date.now() + 15_000;
+  const readyDeadline = Date.now() + CHROME_LAUNCH_READY_WINDOW_MS;
   while (Date.now() < readyDeadline) {
-    if (await isChromeReachable(profile.cdpUrl, 500)) {
+    if (await isChromeReachable(profile.cdpUrl)) {
       break;
     }
-    await new Promise((r) => setTimeout(r, 200));
+    await new Promise((r) => setTimeout(r, CHROME_LAUNCH_READY_POLL_MS));
   }
 
-  if (!(await isChromeReachable(profile.cdpUrl, 500))) {
+  if (!(await isChromeReachable(profile.cdpUrl))) {
     const stderrOutput = Buffer.concat(stderrChunks).toString("utf8").trim();
-    const stderrHint = stderrOutput ? `\nChrome stderr:\n${stderrOutput.slice(0, 2000)}` : "";
+    const stderrHint = stderrOutput
+      ? `\nChrome stderr:\n${stderrOutput.slice(0, CHROME_STDERR_HINT_MAX_CHARS)}`
+      : "";
     const sandboxHint =
       process.platform === "linux" && !resolved.noSandbox
         ? "\nHint: If running in a container or as root, try setting browser.noSandbox: true in config."
@@ -331,7 +353,10 @@ export async function launchOpenClawChrome(
   };
 }
 
-export async function stopOpenClawChrome(running: RunningChrome, timeoutMs = 2500) {
+export async function stopOpenClawChrome(
+  running: RunningChrome,
+  timeoutMs = CHROME_STOP_TIMEOUT_MS,
+) {
   const proc = running.proc;
   if (proc.killed) {
     return;
@@ -347,7 +372,7 @@ export async function stopOpenClawChrome(running: RunningChrome, timeoutMs = 250
     if (!proc.exitCode && proc.killed) {
       break;
     }
-    if (!(await isChromeReachable(cdpUrlForPort(running.cdpPort), 200))) {
+    if (!(await isChromeReachable(cdpUrlForPort(running.cdpPort), CHROME_STOP_PROBE_TIMEOUT_MS))) {
       return;
     }
     await new Promise((r) => setTimeout(r, 100));

src/browser/server-context.reset.test.ts
@@ -1,0 +1,145 @@
+import fs from "node:fs";
+import os from "node:os";
+import path from "node:path";
+import { afterEach, describe, expect, it, vi } from "vitest";
+import { createProfileResetOps } from "./server-context.reset.js";
+
+const relayMocks = vi.hoisted(() => ({
+  stopChromeExtensionRelayServer: vi.fn(async () => true),
+}));
+
+const trashMocks = vi.hoisted(() => ({
+  movePathToTrash: vi.fn(async (from: string) => `${from}.trashed`),
+}));
+
+const pwAiMocks = vi.hoisted(() => ({
+  closePlaywrightBrowserConnection: vi.fn(async () => {}),
+}));
+
+vi.mock("./extension-relay.js", () => relayMocks);
+vi.mock("./trash.js", () => trashMocks);
+vi.mock("./pw-ai.js", () => pwAiMocks);
+
+afterEach(() => {
+  vi.clearAllMocks();
+});
+
+describe("createProfileResetOps", () => {
+  it("stops extension relay for extension profiles", async () => {
+    const ops = createProfileResetOps({
+      profile: {
+        name: "chrome",
+        cdpUrl: "http://127.0.0.1:18800",
+        cdpHost: "127.0.0.1",
+        cdpIsLoopback: true,
+        cdpPort: 18800,
+        color: "#f60",
+        driver: "extension",
+        attachOnly: false,
+      },
+      getProfileState: () => ({ profile: {} as never, running: null }),
+      stopRunningBrowser: vi.fn(async () => ({ stopped: false })),
+      isHttpReachable: vi.fn(async () => false),
+      resolveOpenClawUserDataDir: (name: string) => `/tmp/${name}`,
+    });
+
+    await expect(ops.resetProfile()).resolves.toEqual({
+      moved: false,
+      from: "http://127.0.0.1:18800",
+    });
+    expect(relayMocks.stopChromeExtensionRelayServer).toHaveBeenCalledWith({
+      cdpUrl: "http://127.0.0.1:18800",
+    });
+    expect(trashMocks.movePathToTrash).not.toHaveBeenCalled();
+  });
+
+  it("rejects remote non-extension profiles", async () => {
+    const ops = createProfileResetOps({
+      profile: {
+        name: "remote",
+        cdpUrl: "https://browserless.example/chrome",
+        cdpHost: "browserless.example",
+        cdpIsLoopback: false,
+        cdpPort: 443,
+        color: "#0f0",
+        driver: "openclaw",
+        attachOnly: false,
+      },
+      getProfileState: () => ({ profile: {} as never, running: null }),
+      stopRunningBrowser: vi.fn(async () => ({ stopped: false })),
+      isHttpReachable: vi.fn(async () => false),
+      resolveOpenClawUserDataDir: (name: string) => `/tmp/${name}`,
+    });
+
+    await expect(ops.resetProfile()).rejects.toThrow(/only supported for local profiles/i);
+  });
+
+  it("stops local browser, closes playwright connection, and trashes profile dir", async () => {
+    const tempRoot = fs.mkdtempSync(path.join(os.tmpdir(), "openclaw-reset-"));
+    const profileDir = path.join(tempRoot, "openclaw");
+    fs.mkdirSync(profileDir, { recursive: true });
+
+    const stopRunningBrowser = vi.fn(async () => ({ stopped: true }));
+    const isHttpReachable = vi.fn(async () => true);
+    const getProfileState = vi.fn(() => ({
+      profile: {} as never,
+      running: { pid: 1 } as never,
+    }));
+
+    const ops = createProfileResetOps({
+      profile: {
+        name: "openclaw",
+        cdpUrl: "http://127.0.0.1:18800",
+        cdpHost: "127.0.0.1",
+        cdpIsLoopback: true,
+        cdpPort: 18800,
+        color: "#f60",
+        driver: "openclaw",
+        attachOnly: false,
+      },
+      getProfileState,
+      stopRunningBrowser,
+      isHttpReachable,
+      resolveOpenClawUserDataDir: () => profileDir,
+    });
+
+    const result = await ops.resetProfile();
+    expect(result).toEqual({
+      moved: true,
+      from: profileDir,
+      to: `${profileDir}.trashed`,
+    });
+    expect(isHttpReachable).toHaveBeenCalledWith(300);
+    expect(stopRunningBrowser).toHaveBeenCalledTimes(1);
+    expect(pwAiMocks.closePlaywrightBrowserConnection).toHaveBeenCalledTimes(1);
+    expect(trashMocks.movePathToTrash).toHaveBeenCalledWith(profileDir);
+  });
+
+  it("forces playwright disconnect when loopback cdp is occupied by non-owned process", async () => {
+    const tempRoot = fs.mkdtempSync(path.join(os.tmpdir(), "openclaw-reset-no-own-"));
+    const profileDir = path.join(tempRoot, "openclaw");
+    fs.mkdirSync(profileDir, { recursive: true });
+
+    const stopRunningBrowser = vi.fn(async () => ({ stopped: false }));
+    const ops = createProfileResetOps({
+      profile: {
+        name: "openclaw",
+        cdpUrl: "http://127.0.0.1:18800",
+        cdpHost: "127.0.0.1",
+        cdpIsLoopback: true,
+        cdpPort: 18800,
+        color: "#f60",
+        driver: "openclaw",
+        attachOnly: false,
+      },
+      getProfileState: () => ({ profile: {} as never, running: null }),
+      stopRunningBrowser,
+      isHttpReachable: vi.fn(async () => true),
+      resolveOpenClawUserDataDir: () => profileDir,
+    });
+
+    await ops.resetProfile();
+    expect(stopRunningBrowser).not.toHaveBeenCalled();
+    expect(pwAiMocks.closePlaywrightBrowserConnection).toHaveBeenCalledTimes(2);
+  });
+});

src/browser/server-context.reset.ts
@@ -1,0 +1,69 @@
+import fs from "node:fs";
+import type { ResolvedBrowserProfile } from "./config.js";
+import { stopChromeExtensionRelayServer } from "./extension-relay.js";
+import type { ProfileRuntimeState } from "./server-context.types.js";
+import { movePathToTrash } from "./trash.js";
+
+type ResetDeps = {
+  profile: ResolvedBrowserProfile;
+  getProfileState: () => ProfileRuntimeState;
+  stopRunningBrowser: () => Promise<{ stopped: boolean }>;
+  isHttpReachable: (timeoutMs?: number) => Promise<boolean>;
+  resolveOpenClawUserDataDir: (profileName: string) => string;
+};
+
+type ResetOps = {
+  resetProfile: () => Promise<{ moved: boolean; from: string; to?: string }>;
+};
+
+async function closePlaywrightBrowserConnection(): Promise<void> {
+  try {
+    const mod = await import("./pw-ai.js");
+    await mod.closePlaywrightBrowserConnection();
+  } catch {
+    // ignore
+  }
+}
+
+export function createProfileResetOps({
+  profile,
+  getProfileState,
+  stopRunningBrowser,
+  isHttpReachable,
+  resolveOpenClawUserDataDir,
+}: ResetDeps): ResetOps {
+  const resetProfile = async () => {
+    if (profile.driver === "extension") {
+      await stopChromeExtensionRelayServer({ cdpUrl: profile.cdpUrl }).catch(() => {});
+      return { moved: false, from: profile.cdpUrl };
+    }
+    if (!profile.cdpIsLoopback) {
+      throw new Error(
+        `reset-profile is only supported for local profiles (profile "${profile.name}" is remote).`,
+      );
+    }
+
+    const userDataDir = resolveOpenClawUserDataDir(profile.name);
+    const profileState = getProfileState();
+    const httpReachable = await isHttpReachable(300);
+    if (httpReachable && !profileState.running) {
+      // Port in use but not by us - kill it.
+      await closePlaywrightBrowserConnection();
+    }
+
+    if (profileState.running) {
+      await stopRunningBrowser();
+    }
+
+    await closePlaywrightBrowserConnection();
+
+    if (!fs.existsSync(userDataDir)) {
+      return { moved: false, from: userDataDir };
+    }
+
+    const moved = await movePathToTrash(userDataDir);
+    return { moved: true, from: userDataDir, to: moved };
+  };
+
+  return { resetProfile };
+}

remoteclaw/remoteclaw

(cherry picked from commit 17c434f2f3fd1b758e138a2b010f0d0af7629130)

src/browser/chrome.ts
@@ -5,6 +5,17 @@ import path from "node:path";
 import { ensurePortAvailable } from "../infra/ports.js";
 import { createSubsystemLogger } from "../logging/subsystem.js";
 import { CONFIG_DIR } from "../utils.js";
+import {
+  CHROME_BOOTSTRAP_EXIT_TIMEOUT_MS,
+  CHROME_BOOTSTRAP_PREFS_TIMEOUT_MS,
+  CHROME_LAUNCH_READY_POLL_MS,
+  CHROME_LAUNCH_READY_WINDOW_MS,
+  CHROME_REACHABILITY_TIMEOUT_MS,
+  CHROME_STDERR_HINT_MAX_CHARS,
+  CHROME_STOP_PROBE_TIMEOUT_MS,
+  CHROME_STOP_TIMEOUT_MS,
+  CHROME_WS_READY_TIMEOUT_MS,
+} from "./cdp-timeouts.js";
 import { appendCdpPath, fetchCdpChecked, isWebSocketUrl, openCdpWebSocket } from "./cdp.helpers.js";
 import { normalizeCdpWsUrl } from "./cdp.js";
 import {
@@ -18,7 +29,7 @@ import {
 } from "./chrome.profile-decoration.js";
 import type { ResolvedBrowserConfig, ResolvedBrowserProfile } from "./config.js";
 import {
-  DEFAULT_REMOTECLAW_BROWSER_COLOR,
+  DEFAULT_OPENCLAW_BROWSER_COLOR,
   DEFAULT_REMOTECLAW_BROWSER_PROFILE_NAME,
 } from "./constants.js";
 
@@ -68,9 +79,12 @@ function cdpUrlForPort(cdpPort: number) {
   return `http://127.0.0.1:${cdpPort}`;
 }
 
-export async function isChromeReachable(cdpUrl: string, timeoutMs = 500): Promise<boolean> {
+export async function isChromeReachable(
+  cdpUrl: string,
+  timeoutMs = CHROME_REACHABILITY_TIMEOUT_MS,
+): Promise<boolean> {
   if (isWebSocketUrl(cdpUrl)) {
-    // Direct WebSocket endpoint — probe via WS handshake.
+    // Direct WebSocket endpoint (e.g. Browserbase) — probe via WS handshake.
     return await canOpenWebSocket(cdpUrl, timeoutMs);
   }
   const version = await fetchChromeVersion(cdpUrl, timeoutMs);
@@ -83,7 +97,10 @@ type ChromeVersion = {
   "User-Agent"?: string;
 };
 
-async function fetchChromeVersion(cdpUrl: string, timeoutMs = 500): Promise<ChromeVersion | null> {
+async function fetchChromeVersion(
+  cdpUrl: string,
+  timeoutMs = CHROME_REACHABILITY_TIMEOUT_MS,
+): Promise<ChromeVersion | null> {
   const ctrl = new AbortController();
   const t = setTimeout(ctrl.abort.bind(ctrl), timeoutMs);
   try {
@@ -103,7 +120,7 @@ async function fetchChromeVersion(cdpUrl: string, timeoutMs = 500): Promise<Chro
 
 export async function getChromeWebSocketUrl(
   cdpUrl: string,
-  timeoutMs = 500,
+  timeoutMs = CHROME_REACHABILITY_TIMEOUT_MS,
 ): Promise<string | null> {
   if (isWebSocketUrl(cdpUrl)) {
     // Direct WebSocket endpoint — the cdpUrl is already the WebSocket URL.
@@ -117,7 +134,10 @@ export async function getChromeWebSocketUrl(
   return normalizeCdpWsUrl(wsUrl, cdpUrl);
 }
 
-async function canOpenWebSocket(wsUrl: string, timeoutMs = 800): Promise<boolean> {
+async function canOpenWebSocket(
+  wsUrl: string,
+  timeoutMs = CHROME_WS_READY_TIMEOUT_MS,
+): Promise<boolean> {
   return await new Promise<boolean>((resolve) => {
     const ws = openCdpWebSocket(wsUrl, {
       handshakeTimeoutMs: timeoutMs,
@@ -151,8 +171,8 @@ async function canOpenWebSocket(wsUrl: string, timeoutMs = 800): Promise<boolean
 
 export async function isChromeCdpReady(
   cdpUrl: string,
-  timeoutMs = 500,
-  handshakeTimeoutMs = 800,
+  timeoutMs = CHROME_REACHABILITY_TIMEOUT_MS,
+  handshakeTimeoutMs = CHROME_WS_READY_TIMEOUT_MS,
 ): Promise<boolean> {
   const wsUrl = await getChromeWebSocketUrl(cdpUrl, timeoutMs);
   if (!wsUrl) {
@@ -183,7 +203,7 @@ export async function launchRemoteClawChrome(
   const needsDecorate = !isProfileDecorated(
     userDataDir,
     profile.name,
-    (profile.color ?? DEFAULT_REMOTECLAW_BROWSER_COLOR).toUpperCase(),
+    (profile.color ?? DEFAULT_OPENCLAW_BROWSER_COLOR).toUpperCase(),
   );
 
   // First launch to create preference files if missing, then decorate and relaunch.
@@ -246,7 +266,7 @@ export async function launchRemoteClawChrome(
   // Then decorate (if needed) before the "real" run.
   if (needsBootstrap) {
     const bootstrap = spawnOnce();
-    const deadline = Date.now() + 10_000;
+    const deadline = Date.now() + CHROME_BOOTSTRAP_PREFS_TIMEOUT_MS;
     while (Date.now() < deadline) {
       if (exists(localStatePath) && exists(preferencesPath)) {
         break;
@@ -258,7 +278,7 @@ export async function launchRemoteClawChrome(
     } catch {
       // ignore
     }
-    const exitDeadline = Date.now() + 5000;
+    const exitDeadline = Date.now() + CHROME_BOOTSTRAP_EXIT_TIMEOUT_MS;
     while (Date.now() < exitDeadline) {
       if (bootstrap.exitCode != null) {
         break;
@@ -273,7 +293,7 @@ export async function launchRemoteClawChrome(
         name: profile.name,
         color: profile.color,
       });
-      log.info(`🦀 remoteclaw browser profile decorated (${profile.color})`);
+      log.info(`🦞 remoteclaw browser profile decorated (${profile.color})`);
     } catch (err) {
       log.warn(`remoteclaw browser profile decoration failed: ${String(err)}`);
     }
@@ -297,17 +317,19 @@ export async function launchRemoteClawChrome(
   proc.stderr?.on("data", onStderr);
 
   // Wait for CDP to come up.
-  const readyDeadline = Date.now() + 15_000;
+  const readyDeadline = Date.now() + CHROME_LAUNCH_READY_WINDOW_MS;
   while (Date.now() < readyDeadline) {
-    if (await isChromeReachable(profile.cdpUrl, 500)) {
+    if (await isChromeReachable(profile.cdpUrl)) {
       break;
     }
-    await new Promise((r) => setTimeout(r, 200));
+    await new Promise((r) => setTimeout(r, CHROME_LAUNCH_READY_POLL_MS));
   }
 
-  if (!(await isChromeReachable(profile.cdpUrl, 500))) {
+  if (!(await isChromeReachable(profile.cdpUrl))) {
     const stderrOutput = Buffer.concat(stderrChunks).toString("utf8").trim();
-    const stderrHint = stderrOutput ? `\nChrome stderr:\n${stderrOutput.slice(0, 2000)}` : "";
+    const stderrHint = stderrOutput
+      ? `\nChrome stderr:\n${stderrOutput.slice(0, CHROME_STDERR_HINT_MAX_CHARS)}`
+      : "";
     const sandboxHint =
       process.platform === "linux" && !resolved.noSandbox
         ? "\nHint: If running in a container or as root, try setting browser.noSandbox: true in config."
@@ -328,7 +350,7 @@ export async function launchRemoteClawChrome(
 
   const pid = proc.pid ?? -1;
   log.info(
-    `🦀 remoteclaw browser started (${exe.kind}) profile "${profile.name}" on 127.0.0.1:${profile.cdpPort} (pid ${pid})`,
+    `🦞 remoteclaw browser started (${exe.kind}) profile "${profile.name}" on 127.0.0.1:${profile.cdpPort} (pid ${pid})`,
   );
 
   return {
@@ -341,7 +363,10 @@ export async function launchRemoteClawChrome(
   };
 }
 
-export async function stopRemoteClawChrome(running: RunningChrome, timeoutMs = 2500) {
+export async function stopRemoteClawChrome(
+  running: RunningChrome,
+  timeoutMs = CHROME_STOP_TIMEOUT_MS,
+) {
   const proc = running.proc;
   if (proc.killed) {
     return;
@@ -357,7 +382,7 @@ export async function stopRemoteClawChrome(running: RunningChrome, timeoutMs = 2
     if (!proc.exitCode && proc.killed) {
       break;
     }
-    if (!(await isChromeReachable(cdpUrlForPort(running.cdpPort), 200))) {
+    if (!(await isChromeReachable(cdpUrlForPort(running.cdpPort), CHROME_STOP_PROBE_TIMEOUT_MS))) {
       return;
     }
     await new Promise((r) => setTimeout(r, 100));

src/browser/server-context.reset.test.ts
@@ -1,0 +1,145 @@
+import fs from "node:fs";
+import os from "node:os";
+import path from "node:path";
+import { afterEach, describe, expect, it, vi } from "vitest";
+import { createProfileResetOps } from "./server-context.reset.js";
+
+const relayMocks = vi.hoisted(() => ({
+  stopChromeExtensionRelayServer: vi.fn(async () => true),
+}));
+
+const trashMocks = vi.hoisted(() => ({
+  movePathToTrash: vi.fn(async (from: string) => `${from}.trashed`),
+}));
+
+const pwAiMocks = vi.hoisted(() => ({
+  closePlaywrightBrowserConnection: vi.fn(async () => {}),
+}));
+
+vi.mock("./extension-relay.js", () => relayMocks);
+vi.mock("./trash.js", () => trashMocks);
+vi.mock("./pw-ai.js", () => pwAiMocks);
+
+afterEach(() => {
+  vi.clearAllMocks();
+});
+
+describe("createProfileResetOps", () => {
+  it("stops extension relay for extension profiles", async () => {
+    const ops = createProfileResetOps({
+      profile: {
+        name: "chrome",
+        cdpUrl: "http://127.0.0.1:18800",
+        cdpHost: "127.0.0.1",
+        cdpIsLoopback: true,
+        cdpPort: 18800,
+        color: "#f60",
+        driver: "extension",
+        attachOnly: false,
+      },
+      getProfileState: () => ({ profile: {} as never, running: null }),
+      stopRunningBrowser: vi.fn(async () => ({ stopped: false })),
+      isHttpReachable: vi.fn(async () => false),
+      resolveRemoteClawUserDataDir: (name: string) => `/tmp/${name}`,
+    });
+
+    await expect(ops.resetProfile()).resolves.toEqual({
+      moved: false,
+      from: "http://127.0.0.1:18800",
+    });
+    expect(relayMocks.stopChromeExtensionRelayServer).toHaveBeenCalledWith({
+      cdpUrl: "http://127.0.0.1:18800",
+    });
+    expect(trashMocks.movePathToTrash).not.toHaveBeenCalled();
+  });
+
+  it("rejects remote non-extension profiles", async () => {
+    const ops = createProfileResetOps({
+      profile: {
+        name: "remote",
+        cdpUrl: "https://browserless.example/chrome",
+        cdpHost: "browserless.example",
+        cdpIsLoopback: false,
+        cdpPort: 443,
+        color: "#0f0",
+        driver: "remoteclaw",
+        attachOnly: false,
+      },
+      getProfileState: () => ({ profile: {} as never, running: null }),
+      stopRunningBrowser: vi.fn(async () => ({ stopped: false })),
+      isHttpReachable: vi.fn(async () => false),
+      resolveRemoteClawUserDataDir: (name: string) => `/tmp/${name}`,
+    });
+
+    await expect(ops.resetProfile()).rejects.toThrow(/only supported for local profiles/i);
+  });
+
+  it("stops local browser, closes playwright connection, and trashes profile dir", async () => {
+    const tempRoot = fs.mkdtempSync(path.join(os.tmpdir(), "remoteclaw-reset-"));
+    const profileDir = path.join(tempRoot, "remoteclaw");
+    fs.mkdirSync(profileDir, { recursive: true });
+
+    const stopRunningBrowser = vi.fn(async () => ({ stopped: true }));
+    const isHttpReachable = vi.fn(async () => true);
+    const getProfileState = vi.fn(() => ({
+      profile: {} as never,
+      running: { pid: 1 } as never,
+    }));
+
+    const ops = createProfileResetOps({
+      profile: {
+        name: "remoteclaw",
+        cdpUrl: "http://127.0.0.1:18800",
+        cdpHost: "127.0.0.1",
+        cdpIsLoopback: true,
+        cdpPort: 18800,
+        color: "#f60",
+        driver: "remoteclaw",
+        attachOnly: false,
+      },
+      getProfileState,
+      stopRunningBrowser,
+      isHttpReachable,
+      resolveRemoteClawUserDataDir: () => profileDir,
+    });
+
+    const result = await ops.resetProfile();
+    expect(result).toEqual({
+      moved: true,
+      from: profileDir,
+      to: `${profileDir}.trashed`,
+    });
+    expect(isHttpReachable).toHaveBeenCalledWith(300);
+    expect(stopRunningBrowser).toHaveBeenCalledTimes(1);
+    expect(pwAiMocks.closePlaywrightBrowserConnection).toHaveBeenCalledTimes(1);
+    expect(trashMocks.movePathToTrash).toHaveBeenCalledWith(profileDir);
+  });
+
+  it("forces playwright disconnect when loopback cdp is occupied by non-owned process", async () => {
+    const tempRoot = fs.mkdtempSync(path.join(os.tmpdir(), "remoteclaw-reset-no-own-"));
+    const profileDir = path.join(tempRoot, "remoteclaw");
+    fs.mkdirSync(profileDir, { recursive: true });
+
+    const stopRunningBrowser = vi.fn(async () => ({ stopped: false }));
+    const ops = createProfileResetOps({
+      profile: {
+        name: "remoteclaw",
+        cdpUrl: "http://127.0.0.1:18800",
+        cdpHost: "127.0.0.1",
+        cdpIsLoopback: true,
+        cdpPort: 18800,
+        color: "#f60",
+        driver: "remoteclaw",
+        attachOnly: false,
+      },
+      getProfileState: () => ({ profile: {} as never, running: null }),
+      stopRunningBrowser,
+      isHttpReachable: vi.fn(async () => true),
+      resolveRemoteClawUserDataDir: () => profileDir,
+    });
+
+    await ops.resetProfile();
+    expect(stopRunningBrowser).not.toHaveBeenCalled();
+    expect(pwAiMocks.closePlaywrightBrowserConnection).toHaveBeenCalledTimes(2);
+  });
+});

src/browser/server-context.reset.ts
@@ -1,0 +1,69 @@
+import fs from "node:fs";
+import type { ResolvedBrowserProfile } from "./config.js";
+import { stopChromeExtensionRelayServer } from "./extension-relay.js";
+import type { ProfileRuntimeState } from "./server-context.types.js";
+import { movePathToTrash } from "./trash.js";
+
+type ResetDeps = {
+  profile: ResolvedBrowserProfile;
+  getProfileState: () => ProfileRuntimeState;
+  stopRunningBrowser: () => Promise<{ stopped: boolean }>;
+  isHttpReachable: (timeoutMs?: number) => Promise<boolean>;
+  resolveRemoteClawUserDataDir: (profileName: string) => string;
+};
+
+type ResetOps = {
+  resetProfile: () => Promise<{ moved: boolean; from: string; to?: string }>;
+};
+
+async function closePlaywrightBrowserConnection(): Promise<void> {
+  try {
+    const mod = await import("./pw-ai.js");
+    await mod.closePlaywrightBrowserConnection();
+  } catch {
+    // ignore
+  }
+}
+
+export function createProfileResetOps({
+  profile,
+  getProfileState,
+  stopRunningBrowser,
+  isHttpReachable,
+  resolveRemoteClawUserDataDir,
+}: ResetDeps): ResetOps {
+  const resetProfile = async () => {
+    if (profile.driver === "extension") {
+      await stopChromeExtensionRelayServer({ cdpUrl: profile.cdpUrl }).catch(() => {});
+      return { moved: false, from: profile.cdpUrl };
+    }
+    if (!profile.cdpIsLoopback) {
+      throw new Error(
+        `reset-profile is only supported for local profiles (profile "${profile.name}" is remote).`,
+      );
+    }
+
+    const userDataDir = resolveRemoteClawUserDataDir(profile.name);
+    const profileState = getProfileState();
+    const httpReachable = await isHttpReachable(300);
+    if (httpReachable && !profileState.running) {
+      // Port in use but not by us - kill it.
+      await closePlaywrightBrowserConnection();
+    }
+
+    if (profileState.running) {
+      await stopRunningBrowser();
+    }
+
+    await closePlaywrightBrowserConnection();
+
+    if (!fs.existsSync(userDataDir)) {
+      return { moved: false, from: userDataDir };
+    }
+
+    const moved = await movePathToTrash(userDataDir);
+    return { moved: true, from: userDataDir, to: moved };
+  };
+
+  return { resetProfile };
+}
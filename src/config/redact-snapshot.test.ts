wonglaitung/openclaw_mini

test(perf): cache redact hints and tune guardrail scan concurrency

src/config/redact-snapshot.test.ts
@@ -10,6 +10,7 @@ import type { ConfigFileSnapshot } from "./types.openclaw.js";
 import { OpenClawSchema } from "./zod-schema.js";
 
 const { mapSensitivePaths } = __test__;
+const mainSchemaHints = mapSensitivePaths(OpenClawSchema, "", {});
 
 type TestSnapshot<TConfig extends Record<string, unknown>> = ConfigFileSnapshot & {
   parsed: TConfig;
@@ -757,7 +758,7 @@ describe("redactConfigSnapshot", () => {
   });
 
   it("contract-covers dynamic catchall/record paths for redact+restore", () => {
-    const hints = mapSensitivePaths(OpenClawSchema, "", {});
+    const hints = mainSchemaHints;
     const snapshot = makeSnapshot({
       env: {
         GROQ_API_KEY: "gsk-contract-123",
@@ -1035,7 +1036,7 @@ describe("realredactConfigSnapshot_real", () => {
       unrepresentable: "any",
     });
     schema.title = "OpenClawConfig";
-    const hints = mapSensitivePaths(OpenClawSchema, "", {});
+    const hints = mainSchemaHints;
 
     const snapshot = makeSnapshot({
       agents: {

remoteclaw/remoteclaw

(cherry picked from commit 19f5d1345c3c136ec1175a2c2dd16b7059937bd2)

src/config/redact-snapshot.test.ts
@@ -10,6 +10,7 @@ import type { ConfigFileSnapshot } from "./types.remoteclaw.js";
 import { RemoteClawSchema } from "./zod-schema.js";
 
 const { mapSensitivePaths } = __test__;
+const mainSchemaHints = mapSensitivePaths(RemoteClawSchema, "", {});
 
 type TestSnapshot<TConfig extends Record<string, unknown>> = ConfigFileSnapshot & {
   parsed: TConfig;
@@ -725,7 +726,7 @@ describe("redactConfigSnapshot", () => {
   });
 
   it("contract-covers dynamic catchall/record paths for redact+restore", () => {
-    const hints = mapSensitivePaths(RemoteClawSchema, "", {});
+    const hints = mainSchemaHints;
     const snapshot = makeSnapshot({
       env: {
         GROQ_API_KEY: "gsk-contract-123",
@@ -991,7 +992,7 @@ describe("realredactConfigSnapshot_real", () => {
       unrepresentable: "any",
     });
     schema.title = "RemoteClawConfig";
-    const hints = mapSensitivePaths(RemoteClawSchema, "", {});
+    const hints = mainSchemaHints;
 
     const snapshot = makeSnapshot({
       agents: {
a73b865e4bfbf05f7f4dfdaabc069a9d1ef2adcd
Peter Steinberger <steipete@gmail.com>
2026-03-16 22:11:35 -0700

    refactor: move plugin sdk setup helpers out of commands

---
+++ src/plugin-sdk/ollama-setup.ts
@@ -0,0 +1,17 @@
+export type {
+  RemoteClawPluginApi,
+  ProviderAuthContext,
+  ProviderAuthMethodNonInteractiveContext,
+  ProviderAuthResult,
+  ProviderDiscoveryContext,
+} from "../plugins/types.js";
+
+export {
+  OLLAMA_DEFAULT_BASE_URL,
+  OLLAMA_DEFAULT_MODEL,
+  configureOllamaNonInteractive,
+  ensureOllamaModelPulled,
+  promptAndConfigureOllama,
+} from "../plugins/provider-ollama-setup.js";
+
+export { buildOllamaProvider } from "../agents/models-config.providers.discovery.js";

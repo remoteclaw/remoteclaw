gutchapa/openclaw

fix(gateway): let POST requests pass through root-mounted Control UI to plugin handlers
The Control UI handler checked HTTP method before path routing, causing
all POST requests (including plugin webhook endpoints like /bluebubbles-webhook)
to receive 405 Method Not Allowed.  Move the method check after path-based
exclusions so non-GET/HEAD requests reach plugin HTTP handlers.

Closes #31344

Made-with: Cursor

src/gateway/control-ui.http.test.ts
@@ -326,6 +326,38 @@ describe("handleControlUiHttpRequest", () => {
     });
   });
 
+  it("does not handle POST to root-mounted paths (plugin webhook passthrough)", async () => {
+    await withControlUiRoot({
+      fn: async (tmp) => {
+        for (const webhookPath of ["/bluebubbles-webhook", "/custom-webhook", "/callback"]) {
+          const { res } = makeMockHttpResponse();
+          const handled = handleControlUiHttpRequest(
+            { url: webhookPath, method: "POST" } as IncomingMessage,
+            res,
+            { root: { kind: "resolved", path: tmp } },
+          );
+          expect(handled, `POST to ${webhookPath} should pass through to plugin handlers`).toBe(
+            false,
+          );
+        }
+      },
+    });
+  });
+
+  it("does not handle POST to paths outside basePath", async () => {
+    await withControlUiRoot({
+      fn: async (tmp) => {
+        const { res } = makeMockHttpResponse();
+        const handled = handleControlUiHttpRequest(
+          { url: "/bluebubbles-webhook", method: "POST" } as IncomingMessage,
+          res,
+          { basePath: "/openclaw", root: { kind: "resolved", path: tmp } },
+        );
+        expect(handled).toBe(false);
+      },
+    });
+  });
+
   it("does not handle /api paths when basePath is empty", async () => {
     await withControlUiRoot({
       fn: async (tmp) => {
@@ -373,15 +405,17 @@ describe("handleControlUiHttpRequest", () => {
   it("returns 405 for POST requests under configured basePath", async () => {
     await withControlUiRoot({
       fn: async (tmp) => {
-        const { handled, res, end } = runControlUiRequest({
-          url: "/openclaw/",
-          method: "POST",
-          rootPath: tmp,
-          basePath: "/openclaw",
-        });
-        expect(handled).toBe(true);
-        expect(res.statusCode).toBe(405);
-        expect(end).toHaveBeenCalledWith("Method Not Allowed");
+        for (const route of ["/openclaw", "/openclaw/", "/openclaw/some-page"]) {
+          const { handled, res, end } = runControlUiRequest({
+            url: route,
+            method: "POST",
+            rootPath: tmp,
+            basePath: "/openclaw",
+          });
+          expect(handled, `expected ${route} to be handled`).toBe(true);
+          expect(res.statusCode, `expected ${route} status`).toBe(405);
+          expect(end, `expected ${route} body`).toHaveBeenCalledWith("Method Not Allowed");
+        }
       },
     });
   });

src/gateway/server.plugin-http-auth.test.ts
@@ -554,6 +554,40 @@ describe("gateway plugin HTTP auth boundary", () => {
     });
   });
 
+  test("passes POST webhook routes through root-mounted control ui to plugins", async () => {
+    const handlePluginRequest = vi.fn(async (req: IncomingMessage, res: ServerResponse) => {
+      const pathname = new URL(req.url ?? "/", "http://localhost").pathname;
+      if (req.method !== "POST" || pathname !== "/bluebubbles-webhook") {
+        return false;
+      }
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "text/plain; charset=utf-8");
+      res.end("plugin-webhook");
+      return true;
+    });
+
+    await withGatewayServer({
+      prefix: "openclaw-plugin-http-control-ui-webhook-post-test-",
+      resolvedAuth: AUTH_NONE,
+      overrides: {
+        controlUiEnabled: true,
+        controlUiBasePath: "",
+        controlUiRoot: { kind: "missing" },
+        handlePluginRequest,
+      },
+      run: async (server) => {
+        const response = await sendRequest(server, {
+          path: "/bluebubbles-webhook",
+          method: "POST",
+        });
+
+        expect(response.res.statusCode).toBe(200);
+        expect(response.getBody()).toBe("plugin-webhook");
+        expect(handlePluginRequest).toHaveBeenCalledTimes(1);
+      },
+    });
+  });
+
   test("does not let plugin handlers shadow control ui routes", async () => {
     const handlePluginRequest = vi.fn(async (req: IncomingMessage, res: ServerResponse) => {
       const pathname = new URL(req.url ?? "/", "http://localhost").pathname;

remoteclaw/remoteclaw

(cherry picked from commit c4711a9b694938fbcc32b30ffd6b72576f0901fc)

src/gateway/control-ui.http.test.ts
@@ -329,6 +329,38 @@ describe("handleControlUiHttpRequest", () => {
     });
   });
 
+  it("does not handle POST to root-mounted paths (plugin webhook passthrough)", async () => {
+    await withControlUiRoot({
+      fn: async (tmp) => {
+        for (const webhookPath of ["/bluebubbles-webhook", "/custom-webhook", "/callback"]) {
+          const { res } = makeMockHttpResponse();
+          const handled = handleControlUiHttpRequest(
+            { url: webhookPath, method: "POST" } as IncomingMessage,
+            res,
+            { root: { kind: "resolved", path: tmp } },
+          );
+          expect(handled, `POST to ${webhookPath} should pass through to plugin handlers`).toBe(
+            false,
+          );
+        }
+      },
+    });
+  });
+
+  it("does not handle POST to paths outside basePath", async () => {
+    await withControlUiRoot({
+      fn: async (tmp) => {
+        const { res } = makeMockHttpResponse();
+        const handled = handleControlUiHttpRequest(
+          { url: "/bluebubbles-webhook", method: "POST" } as IncomingMessage,
+          res,
+          { basePath: "/openclaw", root: { kind: "resolved", path: tmp } },
+        );
+        expect(handled).toBe(false);
+      },
+    });
+  });
+
   it("does not handle /api paths when basePath is empty", async () => {
     await withControlUiRoot({
       fn: async (tmp) => {
@@ -376,15 +408,17 @@ describe("handleControlUiHttpRequest", () => {
   it("returns 405 for POST requests under configured basePath", async () => {
     await withControlUiRoot({
       fn: async (tmp) => {
-        const { handled, res, end } = runControlUiRequest({
-          url: "/remoteclaw/",
-          method: "POST",
-          rootPath: tmp,
-          basePath: "/remoteclaw",
-        });
-        expect(handled).toBe(true);
-        expect(res.statusCode).toBe(405);
-        expect(end).toHaveBeenCalledWith("Method Not Allowed");
+        for (const route of ["/remoteclaw", "/remoteclaw/", "/remoteclaw/some-page"]) {
+          const { handled, res, end } = runControlUiRequest({
+            url: route,
+            method: "POST",
+            rootPath: tmp,
+            basePath: "/remoteclaw",
+          });
+          expect(handled, `expected ${route} to be handled`).toBe(true);
+          expect(res.statusCode, `expected ${route} status`).toBe(405);
+          expect(end, `expected ${route} body`).toHaveBeenCalledWith("Method Not Allowed");
+        }
       },
     });
   });

src/gateway/server.plugin-http-auth.test.ts
@@ -613,6 +613,40 @@ describe("gateway plugin HTTP auth boundary", () => {
     });
   });
 
+  test("passes POST webhook routes through root-mounted control ui to plugins", async () => {
+    const handlePluginRequest = vi.fn(async (req: IncomingMessage, res: ServerResponse) => {
+      const pathname = new URL(req.url ?? "/", "http://localhost").pathname;
+      if (req.method !== "POST" || pathname !== "/bluebubbles-webhook") {
+        return false;
+      }
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "text/plain; charset=utf-8");
+      res.end("plugin-webhook");
+      return true;
+    });
+
+    await withGatewayServer({
+      prefix: "remoteclaw-plugin-http-control-ui-webhook-post-test-",
+      resolvedAuth: AUTH_NONE,
+      overrides: {
+        controlUiEnabled: true,
+        controlUiBasePath: "",
+        controlUiRoot: { kind: "missing" },
+        handlePluginRequest,
+      },
+      run: async (server) => {
+        const response = await sendRequest(server, {
+          path: "/bluebubbles-webhook",
+          method: "POST",
+        });
+
+        expect(response.res.statusCode).toBe(200);
+        expect(response.getBody()).toBe("plugin-webhook");
+        expect(handlePluginRequest).toHaveBeenCalledTimes(1);
+      },
+    });
+  });
+
   test("does not let plugin handlers shadow control ui routes", async () => {
     const handlePluginRequest = vi.fn(async (req: IncomingMessage, res: ServerResponse) => {
       const pathname = new URL(req.url ?? "/", "http://localhost").pathname;
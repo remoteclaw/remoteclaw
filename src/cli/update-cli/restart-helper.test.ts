wonglaitung/openclaw_mini

fix(cli): wait for process exit before restarting gateway on Windows (openclaw#27913) thanks @tda1017
Verified:
- pnpm vitest src/cli/update-cli/restart-helper.test.ts
- pnpm check
- pnpm build

Co-authored-by: tda1017 <95275462+tda1017@users.noreply.github.com>
Co-authored-by: Tak Hoffman <781889+Takhoffman@users.noreply.github.com>

src/cli/update-cli/restart-helper.test.ts
@@ -11,8 +11,8 @@ describe("restart-helper", () => {
   const originalPlatform = process.platform;
   const originalGetUid = process.getuid;
 
-  async function prepareAndReadScript(env: Record<string, string>) {
-    const scriptPath = await prepareRestartScript(env);
+  async function prepareAndReadScript(env: Record<string, string>, gatewayPort = 18789) {
+    const scriptPath = await prepareRestartScript(env, gatewayPort);
     expect(scriptPath).toBeTruthy();
     const content = await fs.readFile(scriptPath!, "utf-8");
     return { scriptPath: scriptPath!, content };
@@ -20,6 +20,39 @@ describe("restart-helper", () => {
 
   async function cleanupScript(scriptPath: string) {
     await fs.unlink(scriptPath);
+  }
+
+  function expectWindowsRestartWaitOrdering(content: string, port = 18789) {
+    const endCommand = 'schtasks /End /TN "';
+    const pollAttemptsInit = "set /a attempts=0";
+    const pollLabel = ":wait_for_port_release";
+    const pollAttemptIncrement = "set /a attempts+=1";
+    const pollNetstatCheck = `netstat -ano | findstr /R /C:":${port} .*LISTENING" >nul`;
+    const forceKillLabel = ":force_kill_listener";
+    const forceKillCommand = "taskkill /F /PID %%P >nul 2>&1";
+    const portReleasedLabel = ":port_released";
+    const runCommand = 'schtasks /Run /TN "';
+    const endIndex = content.indexOf(endCommand);
+    const attemptsInitIndex = content.indexOf(pollAttemptsInit, endIndex);
+    const pollLabelIndex = content.indexOf(pollLabel, attemptsInitIndex);
+    const pollAttemptIncrementIndex = content.indexOf(pollAttemptIncrement, pollLabelIndex);
+    const pollNetstatCheckIndex = content.indexOf(pollNetstatCheck, pollAttemptIncrementIndex);
+    const forceKillLabelIndex = content.indexOf(forceKillLabel, pollNetstatCheckIndex);
+    const forceKillCommandIndex = content.indexOf(forceKillCommand, forceKillLabelIndex);
+    const portReleasedLabelIndex = content.indexOf(portReleasedLabel, forceKillCommandIndex);
+    const runIndex = content.indexOf(runCommand, portReleasedLabelIndex);
+
+    expect(endIndex).toBeGreaterThanOrEqual(0);
+    expect(attemptsInitIndex).toBeGreaterThan(endIndex);
+    expect(pollLabelIndex).toBeGreaterThan(attemptsInitIndex);
+    expect(pollAttemptIncrementIndex).toBeGreaterThan(pollLabelIndex);
+    expect(pollNetstatCheckIndex).toBeGreaterThan(pollAttemptIncrementIndex);
+    expect(forceKillLabelIndex).toBeGreaterThan(pollNetstatCheckIndex);
+    expect(forceKillCommandIndex).toBeGreaterThan(forceKillLabelIndex);
+    expect(portReleasedLabelIndex).toBeGreaterThan(forceKillCommandIndex);
+    expect(runIndex).toBeGreaterThan(portReleasedLabelIndex);
+
+    expect(content).not.toContain("timeout /t 3 /nobreak >nul");
   }
 
   beforeEach(() => {
@@ -91,6 +124,7 @@ describe("restart-helper", () => {
       expect(content).toContain("@echo off");
       expect(content).toContain('schtasks /End /TN "OpenClaw Gateway"');
       expect(content).toContain('schtasks /Run /TN "OpenClaw Gateway"');
+      expectWindowsRestartWaitOrdering(content);
       // Batch self-cleanup
       expect(content).toContain('del "%~f0"');
       await cleanupScript(scriptPath);
@@ -105,6 +139,25 @@ describe("restart-helper", () => {
       });
       expect(content).toContain('schtasks /End /TN "OpenClaw Gateway (custom)"');
       expect(content).toContain('schtasks /Run /TN "OpenClaw Gateway (custom)"');
+      expectWindowsRestartWaitOrdering(content);
+      await cleanupScript(scriptPath);
+    });
+
+    it("uses passed gateway port for port polling on Windows", async () => {
+      Object.defineProperty(process, "platform", { value: "win32" });
+      const customPort = 9999;
+
+      const { scriptPath, content } = await prepareAndReadScript(
+        {
+          OPENCLAW_PROFILE: "default",
+        },
+        customPort,
+      );
+      expect(content).toContain(`netstat -ano | findstr /R /C:":${customPort} .*LISTENING" >nul`);
+      expect(content).toContain(
+        `for /f "tokens=5" %%P in ('netstat -ano ^| findstr /R /C:":${customPort} .*LISTENING"') do (`,
+      );
+      expectWindowsRestartWaitOrdering(content, customPort);
       await cleanupScript(scriptPath);
     });
 
@@ -135,6 +188,7 @@ describe("restart-helper", () => {
         OPENCLAW_PROFILE: "production",
       });
       expect(content).toContain('schtasks /End /TN "OpenClaw Gateway (production)"');
+      expectWindowsRestartWaitOrdering(content);
       await cleanupScript(scriptPath);
     });
 

remoteclaw/remoteclaw

(cherry picked from commit d145518f94be0919c2407b98543666b4b7fab28d)

src/cli/update-cli/restart-helper.test.ts
@@ -11,8 +11,8 @@ describe("restart-helper", () => {
   const originalPlatform = process.platform;
   const originalGetUid = process.getuid;
 
-  async function prepareAndReadScript(env: Record<string, string>) {
-    const scriptPath = await prepareRestartScript(env);
+  async function prepareAndReadScript(env: Record<string, string>, gatewayPort = 18789) {
+    const scriptPath = await prepareRestartScript(env, gatewayPort);
     expect(scriptPath).toBeTruthy();
     const content = await fs.readFile(scriptPath!, "utf-8");
     return { scriptPath: scriptPath!, content };
@@ -20,6 +20,39 @@ describe("restart-helper", () => {
 
   async function cleanupScript(scriptPath: string) {
     await fs.unlink(scriptPath);
+  }
+
+  function expectWindowsRestartWaitOrdering(content: string, port = 18789) {
+    const endCommand = 'schtasks /End /TN "';
+    const pollAttemptsInit = "set /a attempts=0";
+    const pollLabel = ":wait_for_port_release";
+    const pollAttemptIncrement = "set /a attempts+=1";
+    const pollNetstatCheck = `netstat -ano | findstr /R /C:":${port} .*LISTENING" >nul`;
+    const forceKillLabel = ":force_kill_listener";
+    const forceKillCommand = "taskkill /F /PID %%P >nul 2>&1";
+    const portReleasedLabel = ":port_released";
+    const runCommand = 'schtasks /Run /TN "';
+    const endIndex = content.indexOf(endCommand);
+    const attemptsInitIndex = content.indexOf(pollAttemptsInit, endIndex);
+    const pollLabelIndex = content.indexOf(pollLabel, attemptsInitIndex);
+    const pollAttemptIncrementIndex = content.indexOf(pollAttemptIncrement, pollLabelIndex);
+    const pollNetstatCheckIndex = content.indexOf(pollNetstatCheck, pollAttemptIncrementIndex);
+    const forceKillLabelIndex = content.indexOf(forceKillLabel, pollNetstatCheckIndex);
+    const forceKillCommandIndex = content.indexOf(forceKillCommand, forceKillLabelIndex);
+    const portReleasedLabelIndex = content.indexOf(portReleasedLabel, forceKillCommandIndex);
+    const runIndex = content.indexOf(runCommand, portReleasedLabelIndex);
+
+    expect(endIndex).toBeGreaterThanOrEqual(0);
+    expect(attemptsInitIndex).toBeGreaterThan(endIndex);
+    expect(pollLabelIndex).toBeGreaterThan(attemptsInitIndex);
+    expect(pollAttemptIncrementIndex).toBeGreaterThan(pollLabelIndex);
+    expect(pollNetstatCheckIndex).toBeGreaterThan(pollAttemptIncrementIndex);
+    expect(forceKillLabelIndex).toBeGreaterThan(pollNetstatCheckIndex);
+    expect(forceKillCommandIndex).toBeGreaterThan(forceKillLabelIndex);
+    expect(portReleasedLabelIndex).toBeGreaterThan(forceKillCommandIndex);
+    expect(runIndex).toBeGreaterThan(portReleasedLabelIndex);
+
+    expect(content).not.toContain("timeout /t 3 /nobreak >nul");
   }
 
   beforeEach(() => {
@@ -91,6 +124,7 @@ describe("restart-helper", () => {
       expect(content).toContain("@echo off");
       expect(content).toContain('schtasks /End /TN "RemoteClaw Gateway"');
       expect(content).toContain('schtasks /Run /TN "RemoteClaw Gateway"');
+      expectWindowsRestartWaitOrdering(content);
       // Batch self-cleanup
       expect(content).toContain('del "%~f0"');
       await cleanupScript(scriptPath);
@@ -105,6 +139,25 @@ describe("restart-helper", () => {
       });
       expect(content).toContain('schtasks /End /TN "RemoteClaw Gateway (custom)"');
       expect(content).toContain('schtasks /Run /TN "RemoteClaw Gateway (custom)"');
+      expectWindowsRestartWaitOrdering(content);
+      await cleanupScript(scriptPath);
+    });
+
+    it("uses passed gateway port for port polling on Windows", async () => {
+      Object.defineProperty(process, "platform", { value: "win32" });
+      const customPort = 9999;
+
+      const { scriptPath, content } = await prepareAndReadScript(
+        {
+          REMOTECLAW_PROFILE: "default",
+        },
+        customPort,
+      );
+      expect(content).toContain(`netstat -ano | findstr /R /C:":${customPort} .*LISTENING" >nul`);
+      expect(content).toContain(
+        `for /f "tokens=5" %%P in ('netstat -ano ^| findstr /R /C:":${customPort} .*LISTENING"') do (`,
+      );
+      expectWindowsRestartWaitOrdering(content, customPort);
       await cleanupScript(scriptPath);
     });
 
@@ -135,6 +188,7 @@ describe("restart-helper", () => {
         REMOTECLAW_PROFILE: "production",
       });
       expect(content).toContain('schtasks /End /TN "RemoteClaw Gateway (production)"');
+      expectWindowsRestartWaitOrdering(content);
       await cleanupScript(scriptPath);
     });
 
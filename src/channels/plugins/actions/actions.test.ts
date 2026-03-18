openclaw/openclaw

fix(signal): fall back to toolContext.currentMessageId for reactions
Signal reactions required an explicit messageId parameter, unlike
Telegram which already fell back to toolContext.currentMessageId.
This made agent-initiated reactions fail on Signal because the
inbound message ID was available in tool context but never used.

- Destructure toolContext in Signal action handler
- Fall back to toolContext.currentMessageId when messageId omitted
- Update reaction schema descriptions (not Telegram-specific)
- Add tests for fallback and missing-messageId rejection

Closes #17651

src/channels/plugins/actions/actions.test.ts
@@ -61,7 +61,11 @@ type SignalActionInput = Parameters<NonNullable<typeof signalMessageActions.hand
 async function runSignalAction(
   action: SignalActionInput["action"],
   params: SignalActionInput["params"],
-  options?: { cfg?: OpenClawConfig; accountId?: string },
+  options?: {
+    cfg?: OpenClawConfig;
+    accountId?: string;
+    toolContext?: SignalActionInput["toolContext"];
+  },
 ) {
   const cfg =
     options?.cfg ?? ({ channels: { signal: { account: "+15550001111" } } } as OpenClawConfig);
@@ -75,6 +79,7 @@ async function runSignalAction(
     params,
     cfg,
     accountId: options?.accountId,
+    toolContext: options?.toolContext,
   });
   return { cfg };
 }
@@ -850,6 +855,33 @@ describe("signalMessageActions", () => {
       });
       expect(sendReactionSignal, testCase.name).toHaveBeenCalledWith(...testCase.expectedArgs);
     }
+  });
+
+  it("falls back to toolContext.currentMessageId for reactions when messageId is omitted", async () => {
+    sendReactionSignal.mockClear();
+    await runSignalAction(
+      "react",
+      { to: "+15559999999", emoji: "🔥" },
+      { toolContext: { currentMessageId: "1737630212345" } },
+    );
+    expect(sendReactionSignal).toHaveBeenCalledTimes(1);
+    expect(sendReactionSignal).toHaveBeenCalledWith(
+      "+15559999999",
+      1737630212345,
+      "🔥",
+      expect.objectContaining({}),
+    );
+  });
+
+  it("rejects reaction when neither messageId nor toolContext.currentMessageId is provided", async () => {
+    const cfg = {
+      channels: { signal: { account: "+15550001111" } },
+    } as OpenClawConfig;
+    await expectSignalActionRejected(
+      { to: "+15559999999", emoji: "✅" },
+      /messageId.*required/,
+      cfg,
+    );
   });
 
   it("requires targetAuthor for group reactions", async () => {

remoteclaw/remoteclaw

(cherry picked from commit d9fdec12abe9dd5a68fb703d601537ce1149bc90)

src/channels/plugins/actions/actions.test.ts
@@ -61,7 +61,11 @@ type SignalActionInput = Parameters<NonNullable<typeof signalMessageActions.hand
 async function runSignalAction(
   action: SignalActionInput["action"],
   params: SignalActionInput["params"],
-  options?: { cfg?: RemoteClawConfig; accountId?: string },
+  options?: {
+    cfg?: RemoteClawConfig;
+    accountId?: string;
+    toolContext?: SignalActionInput["toolContext"];
+  },
 ) {
   const cfg =
     options?.cfg ?? ({ channels: { signal: { account: "+15550001111" } } } as RemoteClawConfig);
@@ -75,6 +79,7 @@ async function runSignalAction(
     params,
     cfg,
     accountId: options?.accountId,
+    toolContext: options?.toolContext,
   });
   return { cfg };
 }
@@ -850,6 +855,33 @@ describe("signalMessageActions", () => {
       });
       expect(sendReactionSignal, testCase.name).toHaveBeenCalledWith(...testCase.expectedArgs);
     }
+  });
+
+  it("falls back to toolContext.currentMessageId for reactions when messageId is omitted", async () => {
+    sendReactionSignal.mockClear();
+    await runSignalAction(
+      "react",
+      { to: "+15559999999", emoji: "🔥" },
+      { toolContext: { currentMessageId: "1737630212345" } },
+    );
+    expect(sendReactionSignal).toHaveBeenCalledTimes(1);
+    expect(sendReactionSignal).toHaveBeenCalledWith(
+      "+15559999999",
+      1737630212345,
+      "🔥",
+      expect.objectContaining({}),
+    );
+  });
+
+  it("rejects reaction when neither messageId nor toolContext.currentMessageId is provided", async () => {
+    const cfg = {
+      channels: { signal: { account: "+15550001111" } },
+    } as OpenClawConfig;
+    await expectSignalActionRejected(
+      { to: "+15559999999", emoji: "✅" },
+      /messageId.*required/,
+      cfg,
+    );
   });
 
   it("requires targetAuthor for group reactions", async () => {
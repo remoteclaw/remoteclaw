wonglaitung/openclaw_mini

test(perf): trim repeated setup in cron memory and config suites

src/config/config.plugin-validation.test.ts
@@ -41,10 +41,7 @@ describe("config plugin validation", () => {
     OPENCLAW_PLUGIN_MANIFEST_CACHE_MS: process.env.OPENCLAW_PLUGIN_MANIFEST_CACHE_MS,
   };
 
-  const validateInSuite = (raw: unknown) => {
-    process.env.OPENCLAW_STATE_DIR = path.join(suiteHome, ".openclaw");
-    return validateConfigObjectWithPlugins(raw);
-  };
+  const validateInSuite = (raw: unknown) => validateConfigObjectWithPlugins(raw);
 
   beforeAll(async () => {
     fixtureRoot = await fs.mkdtemp(path.join(os.tmpdir(), "openclaw-config-plugin-validation-"));
@@ -70,6 +67,7 @@ describe("config plugin validation", () => {
       channels: ["bluebubbles"],
       schema: { type: "object" },
     });
+    process.env.OPENCLAW_STATE_DIR = path.join(suiteHome, ".openclaw");
     process.env.OPENCLAW_PLUGIN_MANIFEST_CACHE_MS = "10000";
     clearPluginManifestRegistryCache();
   });

remoteclaw/remoteclaw

(cherry picked from commit 83ec545bed5131ce4b8ea5c7b52f9ac4f9b21e1f)

src/config/config.plugin-validation.test.ts
@@ -39,15 +39,13 @@ describe("config plugin validation", () => {
     REMOTECLAW_PLUGIN_MANIFEST_CACHE_MS: process.env.REMOTECLAW_PLUGIN_MANIFEST_CACHE_MS,
   };
 
-  const validateInSuite = (raw: unknown) => {
-    process.env.REMOTECLAW_STATE_DIR = path.join(suiteHome, ".remoteclaw");
-    return validateConfigObjectWithPlugins(raw);
-  };
+  const validateInSuite = (raw: unknown) => validateConfigObjectWithPlugins(raw);
 
   beforeAll(async () => {
     fixtureRoot = await fs.mkdtemp(path.join(os.tmpdir(), "remoteclaw-config-plugin-validation-"));
     suiteHome = path.join(fixtureRoot, "home");
     await fs.mkdir(suiteHome, { recursive: true });
+    process.env.REMOTECLAW_STATE_DIR = path.join(suiteHome, ".remoteclaw");
     process.env.REMOTECLAW_PLUGIN_MANIFEST_CACHE_MS = "10000";
     clearPluginManifestRegistryCache();
   });
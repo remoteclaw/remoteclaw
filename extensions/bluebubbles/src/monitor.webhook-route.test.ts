wonglaitung/openclaw_mini

refactor(gateway): unify control-ui and plugin webhook routing

extensions/bluebubbles/src/monitor.webhook-route.test.ts
@@ -1,0 +1,44 @@
+import type { OpenClawConfig } from "openclaw/plugin-sdk";
+import { afterEach, describe, expect, it } from "vitest";
+import { createEmptyPluginRegistry } from "../../../src/plugins/registry.js";
+import { setActivePluginRegistry } from "../../../src/plugins/runtime.js";
+import type { WebhookTarget } from "./monitor-shared.js";
+import { registerBlueBubblesWebhookTarget } from "./monitor.js";
+
+function createTarget(): WebhookTarget {
+  return {
+    account: { accountId: "default" } as WebhookTarget["account"],
+    config: {} as OpenClawConfig,
+    runtime: {},
+    core: {} as WebhookTarget["core"],
+    path: "/bluebubbles-webhook",
+  };
+}
+
+describe("registerBlueBubblesWebhookTarget", () => {
+  afterEach(() => {
+    setActivePluginRegistry(createEmptyPluginRegistry());
+  });
+
+  it("registers and unregisters plugin HTTP route at path boundaries", () => {
+    const registry = createEmptyPluginRegistry();
+    setActivePluginRegistry(registry);
+
+    const unregisterA = registerBlueBubblesWebhookTarget(createTarget());
+    const unregisterB = registerBlueBubblesWebhookTarget(createTarget());
+
+    expect(registry.httpRoutes).toHaveLength(1);
+    expect(registry.httpRoutes[0]).toEqual(
+      expect.objectContaining({
+        pluginId: "bluebubbles",
+        path: "/bluebubbles-webhook",
+        source: "bluebubbles-webhook",
+      }),
+    );
+
+    unregisterA();
+    expect(registry.httpRoutes).toHaveLength(1);
+    unregisterB();
+    expect(registry.httpRoutes).toHaveLength(0);
+  });
+});

extensions/googlechat/src/monitor.webhook-routing.test.ts
@@ -1,7 +1,9 @@
 import { EventEmitter } from "node:events";
 import type { IncomingMessage } from "node:http";
 import type { OpenClawConfig, PluginRuntime } from "openclaw/plugin-sdk";
-import { describe, expect, it, vi } from "vitest";
+import { afterEach, describe, expect, it, vi } from "vitest";
+import { createEmptyPluginRegistry } from "../../../src/plugins/registry.js";
+import { setActivePluginRegistry } from "../../../src/plugins/runtime.js";
 import { createMockServerResponse } from "../../../src/test-utils/mock-http-response.js";
 import type { ResolvedGoogleChatAccount } from "./accounts.js";
 import { verifyGoogleChatRequest } from "./auth.js";
@@ -86,6 +88,47 @@ function registerTwoTargets() {
 }
 
 describe("Google Chat webhook routing", () => {
+  afterEach(() => {
+    setActivePluginRegistry(createEmptyPluginRegistry());
+  });
+
+  it("registers and unregisters plugin HTTP route at path boundaries", () => {
+    const registry = createEmptyPluginRegistry();
+    setActivePluginRegistry(registry);
+    const unregisterA = registerGoogleChatWebhookTarget({
+      account: baseAccount("A"),
+      config: {} as OpenClawConfig,
+      runtime: {},
+      core: {} as PluginRuntime,
+      path: "/googlechat",
+      statusSink: vi.fn(),
+      mediaMaxMb: 5,
+    });
+    const unregisterB = registerGoogleChatWebhookTarget({
+      account: baseAccount("B"),
+      config: {} as OpenClawConfig,
+      runtime: {},
+      core: {} as PluginRuntime,
+      path: "/googlechat",
+      statusSink: vi.fn(),
+      mediaMaxMb: 5,
+    });
+
+    expect(registry.httpRoutes).toHaveLength(1);
+    expect(registry.httpRoutes[0]).toEqual(
+      expect.objectContaining({
+        pluginId: "googlechat",
+        path: "/googlechat",
+        source: "googlechat-webhook",
+      }),
+    );
+
+    unregisterA();
+    expect(registry.httpRoutes).toHaveLength(1);
+    unregisterB();
+    expect(registry.httpRoutes).toHaveLength(0);
+  });
+
   it("rejects ambiguous routing when multiple targets on the same path verify successfully", async () => {
     vi.mocked(verifyGoogleChatRequest).mockResolvedValue({ ok: true });
 

src/gateway/control-ui-routing.test.ts
@@ -1,0 +1,54 @@
+import { describe, expect, it } from "vitest";
+import { classifyControlUiRequest } from "./control-ui-routing.js";
+
+describe("classifyControlUiRequest", () => {
+  it("falls through non-read root requests for plugin webhooks", () => {
+    const classified = classifyControlUiRequest({
+      basePath: "",
+      pathname: "/bluebubbles-webhook",
+      search: "",
+      method: "POST",
+    });
+    expect(classified).toEqual({ kind: "not-control-ui" });
+  });
+
+  it("returns not-found for legacy /ui routes when root-mounted", () => {
+    const classified = classifyControlUiRequest({
+      basePath: "",
+      pathname: "/ui/settings",
+      search: "",
+      method: "GET",
+    });
+    expect(classified).toEqual({ kind: "not-found" });
+  });
+
+  it("returns method-not-allowed for basePath non-read methods", () => {
+    const classified = classifyControlUiRequest({
+      basePath: "/openclaw",
+      pathname: "/openclaw",
+      search: "",
+      method: "POST",
+    });
+    expect(classified).toEqual({ kind: "method-not-allowed" });
+  });
+
+  it("returns redirect for basePath entrypoint GET", () => {
+    const classified = classifyControlUiRequest({
+      basePath: "/openclaw",
+      pathname: "/openclaw",
+      search: "?foo=1",
+      method: "GET",
+    });
+    expect(classified).toEqual({ kind: "redirect", location: "/openclaw/?foo=1" });
+  });
+
+  it("classifies basePath subroutes as control ui", () => {
+    const classified = classifyControlUiRequest({
+      basePath: "/openclaw",
+      pathname: "/openclaw/chat",
+      search: "",
+      method: "HEAD",
+    });
+    expect(classified).toEqual({ kind: "serve" });
+  });
+});

remoteclaw/remoteclaw

(cherry picked from commit b13d48987ccab4f7bf6007b2bbedeb650f6d4531)

extensions/bluebubbles/src/monitor.webhook-route.test.ts
@@ -1,0 +1,44 @@
+import type { RemoteClawConfig } from "remoteclaw/plugin-sdk";
+import { afterEach, describe, expect, it } from "vitest";
+import { createEmptyPluginRegistry } from "../../../src/plugins/registry.js";
+import { setActivePluginRegistry } from "../../../src/plugins/runtime.js";
+import type { WebhookTarget } from "./monitor-shared.js";
+import { registerBlueBubblesWebhookTarget } from "./monitor.js";
+
+function createTarget(): WebhookTarget {
+  return {
+    account: { accountId: "default" } as WebhookTarget["account"],
+    config: {} as RemoteClawConfig,
+    runtime: {},
+    core: {} as WebhookTarget["core"],
+    path: "/bluebubbles-webhook",
+  };
+}
+
+describe("registerBlueBubblesWebhookTarget", () => {
+  afterEach(() => {
+    setActivePluginRegistry(createEmptyPluginRegistry());
+  });
+
+  it("registers and unregisters plugin HTTP route at path boundaries", () => {
+    const registry = createEmptyPluginRegistry();
+    setActivePluginRegistry(registry);
+
+    const unregisterA = registerBlueBubblesWebhookTarget(createTarget());
+    const unregisterB = registerBlueBubblesWebhookTarget(createTarget());
+
+    expect(registry.httpRoutes).toHaveLength(1);
+    expect(registry.httpRoutes[0]).toEqual(
+      expect.objectContaining({
+        pluginId: "bluebubbles",
+        path: "/bluebubbles-webhook",
+        source: "bluebubbles-webhook",
+      }),
+    );
+
+    unregisterA();
+    expect(registry.httpRoutes).toHaveLength(1);
+    unregisterB();
+    expect(registry.httpRoutes).toHaveLength(0);
+  });
+});

extensions/googlechat/src/monitor.webhook-routing.test.ts
@@ -1,7 +1,10 @@
 import { EventEmitter } from "node:events";
-import type { IncomingMessage, ServerResponse } from "node:http";
+import type { IncomingMessage } from "node:http";
 import type { RemoteClawConfig, PluginRuntime } from "remoteclaw/plugin-sdk";
-import { describe, expect, it, vi } from "vitest";
+import { afterEach, describe, expect, it, vi } from "vitest";
+import { createEmptyPluginRegistry } from "../../../src/plugins/registry.js";
+import { setActivePluginRegistry } from "../../../src/plugins/runtime.js";
+import { createMockServerResponse } from "../../../src/test-utils/mock-http-response.js";
 import type { ResolvedGoogleChatAccount } from "./accounts.js";
 import { verifyGoogleChatRequest } from "./auth.js";
 import { handleGoogleChatWebhookRequest, registerGoogleChatWebhookTarget } from "./monitor.js";
@@ -9,30 +12,6 @@ import { handleGoogleChatWebhookRequest, registerGoogleChatWebhookTarget } from 
 vi.mock("./auth.js", () => ({
   verifyGoogleChatRequest: vi.fn(),
 }));
-
-function createMockServerResponse(): ServerResponse & { body?: string } {
-  const headers: Record<string, string> = {};
-  const res: {
-    headersSent: boolean;
-    statusCode: number;
-    body?: string;
-    setHeader: (key: string, value: string) => unknown;
-    end: (body?: string) => unknown;
-  } = {
-    headersSent: false,
-    statusCode: 200,
-    setHeader: (key: string, value: string) => {
-      headers[key.toLowerCase()] = value;
-      return res;
-    },
-    end: (body?: string) => {
-      res.headersSent = true;
-      res.body = body;
-      return res;
-    },
-  };
-  return res as unknown as ServerResponse & { body?: string };
-}
 
 function createWebhookRequest(params: {
   authorization?: string;
@@ -42,7 +21,6 @@ function createWebhookRequest(params: {
   const req = new EventEmitter() as IncomingMessage & {
     destroyed?: boolean;
     destroy: (error?: Error) => IncomingMessage;
-    on: (event: string, listener: (...args: unknown[]) => void) => IncomingMessage;
   };
   req.method = "POST";
   req.url = params.path ?? "/googlechat";
@@ -51,47 +29,18 @@ function createWebhookRequest(params: {
     "content-type": "application/json",
   };
   req.destroyed = false;
-  (req as unknown as { socket: { remoteAddress: string } }).socket = {
-    remoteAddress: "127.0.0.1",
-  };
   req.destroy = () => {
     req.destroyed = true;
     return req;
   };
 
-  const originalOn = req.on.bind(req);
-  let bodyScheduled = false;
-  req.on = ((event: string, listener: (...args: unknown[]) => void) => {
-    const result = originalOn(event, listener);
-    if (!bodyScheduled && event === "data") {
-      bodyScheduled = true;
-      void Promise.resolve().then(() => {
-        req.emit("data", Buffer.from(JSON.stringify(params.payload), "utf-8"));
-        if (!req.destroyed) {
-          req.emit("end");
-        }
-      });
+  void Promise.resolve().then(() => {
+    req.emit("data", Buffer.from(JSON.stringify(params.payload), "utf-8"));
+    if (!req.destroyed) {
+      req.emit("end");
     }
-    return result;
-  }) as IncomingMessage["on"];
+  });
 
-  return req;
-}
-
-function createHeaderOnlyWebhookRequest(params: {
-  authorization?: string;
-  path?: string;
-}): IncomingMessage {
-  const req = new EventEmitter() as IncomingMessage;
-  req.method = "POST";
-  req.url = params.path ?? "/googlechat";
-  req.headers = {
-    authorization: params.authorization ?? "",
-    "content-type": "application/json",
-  };
-  (req as unknown as { socket: { remoteAddress: string } }).socket = {
-    remoteAddress: "127.0.0.1",
-  };
   return req;
 }
 
@@ -139,6 +88,47 @@ function registerTwoTargets() {
 }
 
 describe("Google Chat webhook routing", () => {
+  afterEach(() => {
+    setActivePluginRegistry(createEmptyPluginRegistry());
+  });
+
+  it("registers and unregisters plugin HTTP route at path boundaries", () => {
+    const registry = createEmptyPluginRegistry();
+    setActivePluginRegistry(registry);
+    const unregisterA = registerGoogleChatWebhookTarget({
+      account: baseAccount("A"),
+      config: {} as RemoteClawConfig,
+      runtime: {},
+      core: {} as PluginRuntime,
+      path: "/googlechat",
+      statusSink: vi.fn(),
+      mediaMaxMb: 5,
+    });
+    const unregisterB = registerGoogleChatWebhookTarget({
+      account: baseAccount("B"),
+      config: {} as RemoteClawConfig,
+      runtime: {},
+      core: {} as PluginRuntime,
+      path: "/googlechat",
+      statusSink: vi.fn(),
+      mediaMaxMb: 5,
+    });
+
+    expect(registry.httpRoutes).toHaveLength(1);
+    expect(registry.httpRoutes[0]).toEqual(
+      expect.objectContaining({
+        pluginId: "googlechat",
+        path: "/googlechat",
+        source: "googlechat-webhook",
+      }),
+    );
+
+    unregisterA();
+    expect(registry.httpRoutes).toHaveLength(1);
+    unregisterB();
+    expect(registry.httpRoutes).toHaveLength(0);
+  });
+
   it("rejects ambiguous routing when multiple targets on the same path verify successfully", async () => {
     vi.mocked(verifyGoogleChatRequest).mockResolvedValue({ ok: true });
 
@@ -176,61 +166,6 @@ describe("Google Chat webhook routing", () => {
         createWebhookRequest({
           authorization: "Bearer test-token",
           payload: { type: "ADDED_TO_SPACE", space: { name: "spaces/BBB" } },
-        }),
-        res,
-      );
-
-      expect(handled).toBe(true);
-      expect(res.statusCode).toBe(200);
-      expect(sinkA).not.toHaveBeenCalled();
-      expect(sinkB).toHaveBeenCalledTimes(1);
-    } finally {
-      unregister();
-    }
-  });
-
-  it("rejects invalid bearer before attempting to read the body", async () => {
-    vi.mocked(verifyGoogleChatRequest).mockResolvedValue({ ok: false, reason: "invalid" });
-    const { unregister } = registerTwoTargets();
-
-    try {
-      const req = createHeaderOnlyWebhookRequest({
-        authorization: "Bearer invalid-token",
-      });
-      const onSpy = vi.spyOn(req, "on");
-      const res = createMockServerResponse();
-      const handled = await handleGoogleChatWebhookRequest(req, res);
-
-      expect(handled).toBe(true);
-      expect(res.statusCode).toBe(401);
-      expect(onSpy).not.toHaveBeenCalledWith("data", expect.any(Function));
-    } finally {
-      unregister();
-    }
-  });
-
-  it("supports add-on requests that provide systemIdToken in the body", async () => {
-    vi.mocked(verifyGoogleChatRequest)
-      .mockResolvedValueOnce({ ok: false, reason: "invalid" })
-      .mockResolvedValueOnce({ ok: true });
-    const { sinkA, sinkB, unregister } = registerTwoTargets();
-
-    try {
-      const res = createMockServerResponse();
-      const handled = await handleGoogleChatWebhookRequest(
-        createWebhookRequest({
-          payload: {
-            commonEventObject: { hostApp: "CHAT" },
-            authorizationEventObject: { systemIdToken: "addon-token" },
-            chat: {
-              eventTime: "2026-03-02T00:00:00.000Z",
-              user: { name: "users/12345", displayName: "Test User" },
-              messagePayload: {
-                space: { name: "spaces/AAA" },
-                message: { text: "Hello from add-on" },
-              },
-            },
-          },
         }),
         res,
       );

src/gateway/control-ui-routing.test.ts
@@ -1,0 +1,54 @@
+import { describe, expect, it } from "vitest";
+import { classifyControlUiRequest } from "./control-ui-routing.js";
+
+describe("classifyControlUiRequest", () => {
+  it("falls through non-read root requests for plugin webhooks", () => {
+    const classified = classifyControlUiRequest({
+      basePath: "",
+      pathname: "/bluebubbles-webhook",
+      search: "",
+      method: "POST",
+    });
+    expect(classified).toEqual({ kind: "not-control-ui" });
+  });
+
+  it("returns not-found for legacy /ui routes when root-mounted", () => {
+    const classified = classifyControlUiRequest({
+      basePath: "",
+      pathname: "/ui/settings",
+      search: "",
+      method: "GET",
+    });
+    expect(classified).toEqual({ kind: "not-found" });
+  });
+
+  it("returns method-not-allowed for basePath non-read methods", () => {
+    const classified = classifyControlUiRequest({
+      basePath: "/remoteclaw",
+      pathname: "/remoteclaw",
+      search: "",
+      method: "POST",
+    });
+    expect(classified).toEqual({ kind: "method-not-allowed" });
+  });
+
+  it("returns redirect for basePath entrypoint GET", () => {
+    const classified = classifyControlUiRequest({
+      basePath: "/remoteclaw",
+      pathname: "/remoteclaw",
+      search: "?foo=1",
+      method: "GET",
+    });
+    expect(classified).toEqual({ kind: "redirect", location: "/remoteclaw/?foo=1" });
+  });
+
+  it("classifies basePath subroutes as control ui", () => {
+    const classified = classifyControlUiRequest({
+      basePath: "/remoteclaw",
+      pathname: "/remoteclaw/chat",
+      search: "",
+      method: "HEAD",
+    });
+    expect(classified).toEqual({ kind: "serve" });
+  });
+});
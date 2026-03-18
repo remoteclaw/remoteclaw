wonglaitung/openclaw_mini

refactor(core): dedupe command, hook, and cron fixtures

src/commands/doctor-state-integrity.test.ts
@@ -65,6 +65,20 @@ async function runStateIntegrity(cfg: OpenClawConfig) {
   return confirmSkipInNonInteractive;
 }
 
+function writeSessionStore(
+  cfg: OpenClawConfig,
+  sessions: Record<string, { sessionId: string; updatedAt: number }>,
+) {
+  setupSessionState(cfg, process.env, process.env.HOME ?? "");
+  const storePath = resolveStorePath(cfg.session?.store, { agentId: "main" });
+  fs.writeFileSync(storePath, JSON.stringify(sessions, null, 2));
+}
+
+async function runStateIntegrityText(cfg: OpenClawConfig): Promise<string> {
+  await noteStateIntegrity(cfg, { confirmSkipInNonInteractive: vi.fn(async () => false) });
+  return stateIntegrityText();
+}
+
 describe("doctor state integrity oauth dir checks", () => {
   let envSnapshot: EnvSnapshot;
   let tempHome = "";
@@ -146,25 +160,13 @@ describe("doctor state integrity oauth dir checks", () => {
 
   it("prints openclaw-only verification hints when recent sessions are missing transcripts", async () => {
     const cfg: OpenClawConfig = {};
-    setupSessionState(cfg, process.env, process.env.HOME ?? "");
-    const storePath = resolveStorePath(cfg.session?.store, { agentId: "main" });
-    fs.writeFileSync(
-      storePath,
-      JSON.stringify(
-        {
-          "agent:main:main": {
-            sessionId: "missing-transcript",
-            updatedAt: Date.now(),
-          },
-        },
-        null,
-        2,
-      ),
-    );
-
-    await noteStateIntegrity(cfg, { confirmSkipInNonInteractive: vi.fn(async () => false) });
-
-    const text = stateIntegrityText();
+    writeSessionStore(cfg, {
+      "agent:main:main": {
+        sessionId: "missing-transcript",
+        updatedAt: Date.now(),
+      },
+    });
+    const text = await runStateIntegrityText(cfg);
     expect(text).toContain("recent sessions are missing transcripts");
     expect(text).toMatch(/openclaw sessions --store ".*sessions\.json"/);
     expect(text).toMatch(/openclaw sessions cleanup --store ".*sessions\.json" --dry-run/);
@@ -177,25 +179,13 @@ describe("doctor state integrity oauth dir checks", () => {
 
   it("ignores slash-routing sessions for recent missing transcript warnings", async () => {
     const cfg: OpenClawConfig = {};
-    setupSessionState(cfg, process.env, process.env.HOME ?? "");
-    const storePath = resolveStorePath(cfg.session?.store, { agentId: "main" });
-    fs.writeFileSync(
-      storePath,
-      JSON.stringify(
-        {
-          "agent:main:telegram:slash:6790081233": {
-            sessionId: "missing-slash-transcript",
-            updatedAt: Date.now(),
-          },
-        },
-        null,
-        2,
-      ),
-    );
-
-    await noteStateIntegrity(cfg, { confirmSkipInNonInteractive: vi.fn(async () => false) });
-
-    const text = stateIntegrityText();
+    writeSessionStore(cfg, {
+      "agent:main:telegram:slash:6790081233": {
+        sessionId: "missing-slash-transcript",
+        updatedAt: Date.now(),
+      },
+    });
+    const text = await runStateIntegrityText(cfg);
     expect(text).not.toContain("recent sessions are missing transcripts");
   });
 });

src/cron/isolated-agent/delivery-target.test.ts
@@ -35,6 +35,17 @@ function makeCfg(overrides?: Partial<OpenClawConfig>): OpenClawConfig {
   } as OpenClawConfig;
 }
 
+function makeTelegramBoundCfg(accountId = "account-b"): OpenClawConfig {
+  return makeCfg({
+    bindings: [
+      {
+        agentId: AGENT_ID,
+        match: { channel: "telegram", accountId },
+      },
+    ],
+  });
+}
+
 const AGENT_ID = "agent-b";
 const DEFAULT_TARGET = {
   channel: "telegram" as const,
@@ -109,16 +120,7 @@ describe("resolveDeliveryTarget", () => {
 
   it("falls back to bound accountId when session has no lastAccountId", async () => {
     setMainSessionEntry(undefined);
-
-    const cfg = makeCfg({
-      bindings: [
-        {
-          agentId: "agent-b",
-          match: { channel: "telegram", accountId: "account-b" },
-        },
-      ],
-    });
-
+    const cfg = makeTelegramBoundCfg();
     const result = await resolveForAgent({ cfg });
 
     expect(result.accountId).toBe("account-b");
@@ -133,15 +135,7 @@ describe("resolveDeliveryTarget", () => {
       lastAccountId: "session-account",
     });
 
-    const cfg = makeCfg({
-      bindings: [
-        {
-          agentId: "agent-b",
-          match: { channel: "telegram", accountId: "account-b" },
-        },
-      ],
-    });
-
+    const cfg = makeTelegramBoundCfg();
     const result = await resolveForAgent({ cfg });
 
     // Session-derived accountId should take precedence over binding

remoteclaw/remoteclaw

(cherry picked from commit 91dd89313a301c7d61e28d7b8eb93823bfb31405)

src/commands/doctor-state-integrity.test.ts
@@ -65,6 +65,20 @@ async function runStateIntegrity(cfg: RemoteClawConfig) {
   return confirmSkipInNonInteractive;
 }
 
+function writeSessionStore(
+  cfg: RemoteClawConfig,
+  sessions: Record<string, { sessionId: string; updatedAt: number }>,
+) {
+  setupSessionState(cfg, process.env, process.env.HOME ?? "");
+  const storePath = resolveStorePath(cfg.session?.store, { agentId: "main" });
+  fs.writeFileSync(storePath, JSON.stringify(sessions, null, 2));
+}
+
+async function runStateIntegrityText(cfg: RemoteClawConfig): Promise<string> {
+  await noteStateIntegrity(cfg, { confirmSkipInNonInteractive: vi.fn(async () => false) });
+  return stateIntegrityText();
+}
+
 describe("doctor state integrity oauth dir checks", () => {
   let envSnapshot: EnvSnapshot;
   let tempHome = "";
@@ -146,25 +160,13 @@ describe("doctor state integrity oauth dir checks", () => {
 
   it("prints remoteclaw-only verification hints when recent sessions are missing transcripts", async () => {
     const cfg: RemoteClawConfig = {};
-    setupSessionState(cfg, process.env, process.env.HOME ?? "");
-    const storePath = resolveStorePath(cfg.session?.store, { agentId: "main" });
-    fs.writeFileSync(
-      storePath,
-      JSON.stringify(
-        {
-          "agent:main:main": {
-            sessionId: "missing-transcript",
-            updatedAt: Date.now(),
-          },
-        },
-        null,
-        2,
-      ),
-    );
-
-    await noteStateIntegrity(cfg, { confirmSkipInNonInteractive: vi.fn(async () => false) });
-
-    const text = stateIntegrityText();
+    writeSessionStore(cfg, {
+      "agent:main:main": {
+        sessionId: "missing-transcript",
+        updatedAt: Date.now(),
+      },
+    });
+    const text = await runStateIntegrityText(cfg);
     expect(text).toContain("recent sessions are missing transcripts");
     expect(text).toMatch(/remoteclaw sessions --store ".*sessions\.json"/);
     expect(text).toMatch(/remoteclaw sessions cleanup --store ".*sessions\.json" --dry-run/);
@@ -177,25 +179,13 @@ describe("doctor state integrity oauth dir checks", () => {
 
   it("ignores slash-routing sessions for recent missing transcript warnings", async () => {
     const cfg: RemoteClawConfig = {};
-    setupSessionState(cfg, process.env, process.env.HOME ?? "");
-    const storePath = resolveStorePath(cfg.session?.store, { agentId: "main" });
-    fs.writeFileSync(
-      storePath,
-      JSON.stringify(
-        {
-          "agent:main:telegram:slash:6790081233": {
-            sessionId: "missing-slash-transcript",
-            updatedAt: Date.now(),
-          },
-        },
-        null,
-        2,
-      ),
-    );
-
-    await noteStateIntegrity(cfg, { confirmSkipInNonInteractive: vi.fn(async () => false) });
-
-    const text = stateIntegrityText();
+    writeSessionStore(cfg, {
+      "agent:main:telegram:slash:6790081233": {
+        sessionId: "missing-slash-transcript",
+        updatedAt: Date.now(),
+      },
+    });
+    const text = await runStateIntegrityText(cfg);
     expect(text).not.toContain("recent sessions are missing transcripts");
   });
 });

src/cron/isolated-agent/delivery-target.test.ts
@@ -35,6 +35,17 @@ function makeCfg(overrides?: Partial<RemoteClawConfig>): RemoteClawConfig {
   } as RemoteClawConfig;
 }
 
+function makeTelegramBoundCfg(accountId = "account-b"): RemoteClawConfig {
+  return makeCfg({
+    bindings: [
+      {
+        agentId: AGENT_ID,
+        match: { channel: "telegram", accountId },
+      },
+    ],
+  });
+}
+
 const AGENT_ID = "agent-b";
 const DEFAULT_TARGET = {
   channel: "telegram" as const,
@@ -109,16 +120,7 @@ describe("resolveDeliveryTarget", () => {
 
   it("falls back to bound accountId when session has no lastAccountId", async () => {
     setMainSessionEntry(undefined);
-
-    const cfg = makeCfg({
-      bindings: [
-        {
-          agentId: "agent-b",
-          match: { channel: "telegram", accountId: "account-b" },
-        },
-      ],
-    });
-
+    const cfg = makeTelegramBoundCfg();
     const result = await resolveForAgent({ cfg });
 
     expect(result.accountId).toBe("account-b");
@@ -133,15 +135,7 @@ describe("resolveDeliveryTarget", () => {
       lastAccountId: "session-account",
     });
 
-    const cfg = makeCfg({
-      bindings: [
-        {
-          agentId: "agent-b",
-          match: { channel: "telegram", accountId: "account-b" },
-        },
-      ],
-    });
-
+    const cfg = makeTelegramBoundCfg();
     const result = await resolveForAgent({ cfg });
 
     // Session-derived accountId should take precedence over binding
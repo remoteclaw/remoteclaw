bmendonca3/openclaw

matrix: bootstrap crypto runtime when npm scripts are skipped

extensions/matrix/index.ts
@@ -1,6 +1,6 @@
 import type { OpenClawPluginApi } from "openclaw/plugin-sdk";
 import { emptyPluginConfigSchema } from "openclaw/plugin-sdk";
-import { matrixPlugin } from "./src/channel.js";
+import { ensureMatrixCryptoRuntime } from "./src/matrix/deps.js";
 import { setMatrixRuntime } from "./src/runtime.js";
 
 const plugin = {
@@ -8,8 +8,10 @@ const plugin = {
   name: "Matrix",
   description: "Matrix channel plugin (matrix-js-sdk)",
   configSchema: emptyPluginConfigSchema(),
-  register(api: OpenClawPluginApi) {
+  async register(api: OpenClawPluginApi) {
     setMatrixRuntime(api.runtime);
+    await ensureMatrixCryptoRuntime();
+    const { matrixPlugin } = await import("./src/channel.js");
     api.registerChannel({ plugin: matrixPlugin });
   },
 };

remoteclaw/remoteclaw

(cherry picked from commit 66c1da45d4c98b4200aca4bdbfc36dbe4688ed11)

extensions/matrix/index.ts
@@ -1,6 +1,6 @@
 import type { RemoteClawPluginApi } from "remoteclaw/plugin-sdk";
 import { emptyPluginConfigSchema } from "remoteclaw/plugin-sdk";
-import { matrixPlugin } from "./src/channel.js";
+import { ensureMatrixCryptoRuntime } from "./src/matrix/deps.js";
 import { setMatrixRuntime } from "./src/runtime.js";
 
 const plugin = {
@@ -8,8 +8,10 @@ const plugin = {
   name: "Matrix",
   description: "Matrix channel plugin (matrix-js-sdk)",
   configSchema: emptyPluginConfigSchema(),
-  register(api: RemoteClawPluginApi) {
+  async register(api: RemoteClawPluginApi) {
     setMatrixRuntime(api.runtime);
+    await ensureMatrixCryptoRuntime();
+    const { matrixPlugin } = await import("./src/channel.js");
     api.registerChannel({ plugin: matrixPlugin });
   },
 };
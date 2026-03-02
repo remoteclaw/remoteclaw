26452682f1bc5964251f6d21c92e52a018dcb640
bmendonca3 <bmendonca3@users.noreply.github.com>
2026-03-02 10:46:37 -0700

    matrix: bootstrap crypto runtime when npm scripts are skipped
    (cherry picked from commit 66c1da45d4c98b4200aca4bdbfc36dbe4688ed11)

--- extensions/matrix/index.ts
+++ extensions/matrix/index.ts
@@ -1,6 +1,6 @@
 import type { RemoteClawPluginApi } from "remoteclaw/plugin-sdk";
 import { emptyPluginConfigSchema } from "remoteclaw/plugin-sdk";
-import { matrixPlugin } from "./src/channel.js";
+import { ensureMatrixCryptoRuntime } from "./src/matrix/deps.js";
 import { setMatrixRuntime } from "./src/runtime.js";
 
 const plugin = {
@@ -8,8 +8,10 @@ const plugin = {
   name: "Matrix",
   description: "Matrix channel plugin (matrix-js-sdk)",
   configSchema: emptyPluginConfigSchema(),
-  register(api: RemoteClawPluginApi) {
+  async register(api: RemoteClawPluginApi) {
     setMatrixRuntime(api.runtime);
+    await ensureMatrixCryptoRuntime();
+    const { matrixPlugin } = await import("./src/channel.js");
     api.registerChannel({ plugin: matrixPlugin });
   },
 };

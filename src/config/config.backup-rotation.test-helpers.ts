wonglaitung/openclaw_mini

refactor(auto-reply,telegram,config): extract guard and forum helpers

src/config/config.backup-rotation.test-helpers.ts
@@ -1,0 +1,19 @@
+import path from "node:path";
+import { expect } from "vitest";
+
+export const IS_WINDOWS = process.platform === "win32";
+
+export function resolveConfigPathFromTempState(fileName = "openclaw.json"): string {
+  const stateDir = process.env.OPENCLAW_STATE_DIR?.trim();
+  if (!stateDir) {
+    throw new Error("Expected OPENCLAW_STATE_DIR to be set by withTempHome");
+  }
+  return path.join(stateDir, fileName);
+}
+
+export function expectPosixMode(statMode: number, expectedMode: number): void {
+  if (IS_WINDOWS) {
+    return;
+  }
+  expect(statMode & 0o777).toBe(expectedMode);
+}

remoteclaw/remoteclaw

(cherry picked from commit 32ecd6f5796c3a136529a2df0d65d1c84c85d2c0)

src/config/config.backup-rotation.test-helpers.ts
@@ -1,0 +1,19 @@
+import path from "node:path";
+import { expect } from "vitest";
+
+export const IS_WINDOWS = process.platform === "win32";
+
+export function resolveConfigPathFromTempState(fileName = "remoteclaw.json"): string {
+  const stateDir = process.env.REMOTECLAW_STATE_DIR?.trim();
+  if (!stateDir) {
+    throw new Error("Expected REMOTECLAW_STATE_DIR to be set by withTempHome");
+  }
+  return path.join(stateDir, fileName);
+}
+
+export function expectPosixMode(statMode: number, expectedMode: number): void {
+  if (IS_WINDOWS) {
+    return;
+  }
+  expect(statMode & 0o777).toBe(expectedMode);
+}
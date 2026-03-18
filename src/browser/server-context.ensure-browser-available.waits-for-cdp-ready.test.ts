wonglaitung/openclaw_mini

refactor: dedupe agent and reply runtimes

src/browser/server-context.ensure-browser-available.waits-for-cdp-ready.test.ts
@@ -13,6 +13,7 @@ vi.mock("./chrome.js", () => ({
 }));
 
 import * as chromeModule from "./chrome.js";
+import type { RunningChrome } from "./chrome.js";
 import type { BrowserServerState } from "./server-context.js";
 import { createBrowserRouteContext } from "./server-context.js";
 
@@ -47,6 +48,21 @@ function makeBrowserState(): BrowserServerState {
   };
 }
 
+function mockLaunchedChrome(
+  launchOpenClawChrome: { mockResolvedValue: (value: RunningChrome) => unknown },
+  pid: number,
+) {
+  const proc = new EventEmitter() as unknown as ChildProcessWithoutNullStreams;
+  launchOpenClawChrome.mockResolvedValue({
+    pid,
+    exe: { kind: "chromium", path: "/usr/bin/chromium" },
+    userDataDir: "/tmp/openclaw-test",
+    cdpPort: 18800,
+    startedAt: Date.now(),
+    proc,
+  });
+}
+
 afterEach(() => {
   vi.useRealTimers();
   vi.clearAllMocks();
@@ -64,16 +80,7 @@ describe("browser server-context ensureBrowserAvailable", () => {
 
     isChromeReachable.mockResolvedValue(false);
     isChromeCdpReady.mockResolvedValueOnce(false).mockResolvedValue(true);
-
-    const proc = new EventEmitter() as unknown as ChildProcessWithoutNullStreams;
-    launchOpenClawChrome.mockResolvedValue({
-      pid: 123,
-      exe: { kind: "chromium", path: "/usr/bin/chromium" },
-      userDataDir: "/tmp/openclaw-test",
-      cdpPort: 18800,
-      startedAt: Date.now(),
-      proc,
-    });
+    mockLaunchedChrome(launchOpenClawChrome, 123);
 
     const state = makeBrowserState();
     const ctx = createBrowserRouteContext({ getState: () => state });
@@ -98,16 +105,7 @@ describe("browser server-context ensureBrowserAvailable", () => {
 
     isChromeReachable.mockResolvedValue(false);
     isChromeCdpReady.mockResolvedValue(false);
-
-    const proc = new EventEmitter() as unknown as ChildProcessWithoutNullStreams;
-    launchOpenClawChrome.mockResolvedValue({
-      pid: 321,
-      exe: { kind: "chromium", path: "/usr/bin/chromium" },
-      userDataDir: "/tmp/openclaw-test",
-      cdpPort: 18800,
-      startedAt: Date.now(),
-      proc,
-    });
+    mockLaunchedChrome(launchOpenClawChrome, 321);
 
     const state = makeBrowserState();
     const ctx = createBrowserRouteContext({ getState: () => state });

remoteclaw/remoteclaw

(cherry picked from commit 9617ac9dd57f83fa7f4b0a8359cc83f539710425)

src/browser/server-context.ensure-browser-available.waits-for-cdp-ready.test.ts
@@ -19,6 +19,8 @@ vi.mock("./chrome.js", () => ({
   stopRemoteClawChrome: vi.fn(async () => {}),
 }));
 
+import type { RunningChrome } from "./chrome.js";
+
 function makeBrowserState(): BrowserServerState {
   return {
     // oxlint-disable-next-line typescript/no-explicit-any
@@ -50,6 +52,21 @@ function makeBrowserState(): BrowserServerState {
   };
 }
 
+function mockLaunchedChrome(
+  launchRemoteClawChrome: { mockResolvedValue: (value: RunningChrome) => unknown },
+  pid: number,
+) {
+  const proc = new EventEmitter() as unknown as ChildProcessWithoutNullStreams;
+  launchRemoteClawChrome.mockResolvedValue({
+    pid,
+    exe: { kind: "chromium", path: "/usr/bin/chromium" },
+    userDataDir: "/tmp/remoteclaw-test",
+    cdpPort: 18800,
+    startedAt: Date.now(),
+    proc,
+  });
+}
+
 afterEach(() => {
   vi.useRealTimers();
   vi.clearAllMocks();
@@ -66,16 +83,7 @@ describe("browser server-context ensureBrowserAvailable", () => {
 
     isChromeReachable.mockResolvedValue(false);
     isChromeCdpReady.mockResolvedValueOnce(false).mockResolvedValue(true);
-
-    const proc = new EventEmitter() as unknown as ChildProcessWithoutNullStreams;
-    launchRemoteClawChrome.mockResolvedValue({
-      pid: 123,
-      exe: { kind: "chromium", path: "/usr/bin/chromium" },
-      userDataDir: "/tmp/remoteclaw-test",
-      cdpPort: 18800,
-      startedAt: Date.now(),
-      proc,
-    });
+    mockLaunchedChrome(launchRemoteClawChrome, 123);
 
     const state = makeBrowserState();
     const ctx = createBrowserRouteContext({ getState: () => state });
@@ -100,16 +108,7 @@ describe("browser server-context ensureBrowserAvailable", () => {
 
     isChromeReachable.mockResolvedValue(false);
     isChromeCdpReady.mockResolvedValue(false);
-
-    const proc = new EventEmitter() as unknown as ChildProcessWithoutNullStreams;
-    launchRemoteClawChrome.mockResolvedValue({
-      pid: 321,
-      exe: { kind: "chromium", path: "/usr/bin/chromium" },
-      userDataDir: "/tmp/remoteclaw-test",
-      cdpPort: 18800,
-      startedAt: Date.now(),
-      proc,
-    });
+    mockLaunchedChrome(launchRemoteClawChrome, 321);
 
     const state = makeBrowserState();
     const ctx = createBrowserRouteContext({ getState: () => state });
wonglaitung/openclaw_mini

Plugins/test-utils: migrate to scoped plugin-sdk imports

extensions/test-utils/plugin-runtime-mock.ts
@@ -1,5 +1,5 @@
-import type { PluginRuntime } from "openclaw/plugin-sdk/compat";
-import { removeAckReactionAfterReply, shouldAckReaction } from "openclaw/plugin-sdk/compat";
+import type { PluginRuntime } from "openclaw/plugin-sdk/test-utils";
+import { removeAckReactionAfterReply, shouldAckReaction } from "openclaw/plugin-sdk/test-utils";
 import { vi } from "vitest";
 
 type DeepPartial<T> = {

extensions/test-utils/runtime-env.ts
@@ -1,4 +1,4 @@
-import type { RuntimeEnv } from "openclaw/plugin-sdk/compat";
+import type { RuntimeEnv } from "openclaw/plugin-sdk/test-utils";
 import { vi } from "vitest";
 
 export function createRuntimeEnv(): RuntimeEnv {

extensions/test-utils/start-account-context.ts
@@ -2,7 +2,7 @@ import type {
   ChannelAccountSnapshot,
   ChannelGatewayContext,
   OpenClawConfig,
-} from "openclaw/plugin-sdk/compat";
+} from "openclaw/plugin-sdk/test-utils";
 import { vi } from "vitest";
 import { createRuntimeEnv } from "./runtime-env.js";
 

remoteclaw/remoteclaw

(cherry picked from commit 8377dbba309b204ba6b1f2b3a370c2eae8a3ff55)

extensions/test-utils/plugin-runtime-mock.ts
@@ -1,5 +1,5 @@
-import type { PluginRuntime } from "remoteclaw/plugin-sdk/compat";
-import { removeAckReactionAfterReply, shouldAckReaction } from "remoteclaw/plugin-sdk/compat";
+import type { PluginRuntime } from "remoteclaw/plugin-sdk/test-utils";
+import { removeAckReactionAfterReply, shouldAckReaction } from "remoteclaw/plugin-sdk/test-utils";
 import { vi } from "vitest";
 
 type DeepPartial<T> = {

extensions/test-utils/runtime-env.ts
@@ -1,4 +1,4 @@
-import type { RuntimeEnv } from "remoteclaw/plugin-sdk/compat";
+import type { RuntimeEnv } from "remoteclaw/plugin-sdk/test-utils";
 import { vi } from "vitest";
 
 export function createRuntimeEnv(): RuntimeEnv {

extensions/test-utils/start-account-context.ts
@@ -2,7 +2,7 @@ import type {
   ChannelAccountSnapshot,
   ChannelGatewayContext,
   OpenClawConfig,
-} from "remoteclaw/plugin-sdk/compat";
+} from "remoteclaw/plugin-sdk/test-utils";
 import { vi } from "vitest";
 import { createRuntimeEnv } from "./runtime-env.js";
 
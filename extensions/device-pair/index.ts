wonglaitung/openclaw_mini

Plugins/device-pair: migrate to scoped plugin-sdk imports

extensions/device-pair/index.ts
@@ -1,12 +1,12 @@
 import os from "node:os";
-import type { OpenClawPluginApi } from "openclaw/plugin-sdk/core";
+import type { OpenClawPluginApi } from "openclaw/plugin-sdk/device-pair";
 import {
   approveDevicePairing,
   listDevicePairing,
   resolveGatewayBindUrl,
   runPluginCommandWithTimeout,
   resolveTailnetHostWithRunner,
-} from "openclaw/plugin-sdk/core";
+} from "openclaw/plugin-sdk/device-pair";
 import qrcode from "qrcode-terminal";
 import {
   armPairNotifyOnce,

extensions/device-pair/notify.ts
@@ -1,7 +1,7 @@
 import { promises as fs } from "node:fs";
 import path from "node:path";
-import type { OpenClawPluginApi } from "openclaw/plugin-sdk/compat";
-import { listDevicePairing } from "openclaw/plugin-sdk/compat";
+import type { OpenClawPluginApi } from "openclaw/plugin-sdk/device-pair";
+import { listDevicePairing } from "openclaw/plugin-sdk/device-pair";
 
 const NOTIFY_STATE_FILE = "device-pair-notify.json";
 const NOTIFY_POLL_INTERVAL_MS = 10_000;

remoteclaw/remoteclaw

(cherry picked from commit 04385a61b7760a91a1d384f5e2181b6ee35f9ef2)

extensions/device-pair/index.ts
@@ -1,13 +1,13 @@
 import os from "node:os";
 import qrcode from "qrcode-terminal";
-import type { RemoteClawPluginApi } from "remoteclaw/plugin-sdk/core";
+import type { OpenClawPluginApi } from "remoteclaw/plugin-sdk/device-pair";
 import {
   approveDevicePairing,
   listDevicePairing,
   resolveGatewayBindUrl,
   runPluginCommandWithTimeout,
   resolveTailnetHostWithRunner,
-} from "remoteclaw/plugin-sdk/core";
+} from "remoteclaw/plugin-sdk/device-pair";
 import {
   armPairNotifyOnce,
   formatPendingRequests,

extensions/device-pair/notify.ts
@@ -1,7 +1,7 @@
 import { promises as fs } from "node:fs";
 import path from "node:path";
-import type { OpenClawPluginApi } from "remoteclaw/plugin-sdk/compat";
-import { listDevicePairing } from "remoteclaw/plugin-sdk/compat";
+import type { OpenClawPluginApi } from "remoteclaw/plugin-sdk/device-pair";
+import { listDevicePairing } from "remoteclaw/plugin-sdk/device-pair";
 
 const NOTIFY_STATE_FILE = "device-pair-notify.json";
 const NOTIFY_POLL_INTERVAL_MS = 10_000;
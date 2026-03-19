wonglaitung/openclaw_mini

fix: substitute YYYY-MM-DD at session startup and post-compaction (#32363) (#32381)
Merged via squash.

Prepared head SHA: aee998a2c1a911d3fef771aa891ac315a2f7dc53
Co-authored-by: chengzhichao-xydt <264300353+chengzhichao-xydt@users.noreply.github.com>
Co-authored-by: jalehman <550978+jalehman@users.noreply.github.com>
Reviewed-by: @jalehman

src/auto-reply/reply/session-reset-prompt.ts
@@ -1,2 +1,21 @@
-export const BARE_SESSION_RESET_PROMPT =
+import { appendCronStyleCurrentTimeLine } from "../../agents/current-time.js";
+import type { OpenClawConfig } from "../../config/config.js";
+
+const BARE_SESSION_RESET_PROMPT_BASE =
   "A new session was started via /new or /reset. Execute your Session Startup sequence now - read the required files before responding to the user. Then greet the user in your configured persona, if one is provided. Be yourself - use your defined voice, mannerisms, and mood. Keep it to 1-3 sentences and ask what they want to do. If the runtime model differs from default_model in the system prompt, mention the default model. Do not mention internal steps, files, tools, or reasoning.";
+
+/**
+ * Build the bare session reset prompt, appending the current date/time so agents
+ * know which daily memory files to read during their Session Startup sequence.
+ * Without this, agents on /new or /reset guess the date from their training cutoff.
+ */
+export function buildBareSessionResetPrompt(cfg?: OpenClawConfig, nowMs?: number): string {
+  return appendCronStyleCurrentTimeLine(
+    BARE_SESSION_RESET_PROMPT_BASE,
+    cfg ?? {},
+    nowMs ?? Date.now(),
+  );
+}
+
+/** @deprecated Use buildBareSessionResetPrompt(cfg) instead */
+export const BARE_SESSION_RESET_PROMPT = BARE_SESSION_RESET_PROMPT_BASE;

remoteclaw/remoteclaw

(cherry picked from commit 53727c72f4d22e39b5eba07efc78e66344605c2a)

src/auto-reply/reply/session-reset-prompt.ts
@@ -1,2 +1,21 @@
-export const BARE_SESSION_RESET_PROMPT =
+import { appendCronStyleCurrentTimeLine } from "../../agents/current-time.js";
+import type { RemoteClawConfig } from "../../config/config.js";
+
+const BARE_SESSION_RESET_PROMPT_BASE =
   "A new session was started via /new or /reset. Execute your Session Startup sequence now - read the required files before responding to the user. Then greet the user in your configured persona, if one is provided. Be yourself - use your defined voice, mannerisms, and mood. Keep it to 1-3 sentences and ask what they want to do. If the runtime model differs from default_model in the system prompt, mention the default model. Do not mention internal steps, files, tools, or reasoning.";
+
+/**
+ * Build the bare session reset prompt, appending the current date/time so agents
+ * know which daily memory files to read during their Session Startup sequence.
+ * Without this, agents on /new or /reset guess the date from their training cutoff.
+ */
+export function buildBareSessionResetPrompt(cfg?: RemoteClawConfig, nowMs?: number): string {
+  return appendCronStyleCurrentTimeLine(
+    BARE_SESSION_RESET_PROMPT_BASE,
+    cfg ?? {},
+    nowMs ?? Date.now(),
+  );
+}
+
+/** @deprecated Use buildBareSessionResetPrompt(cfg) instead */
+export const BARE_SESSION_RESET_PROMPT = BARE_SESSION_RESET_PROMPT_BASE;
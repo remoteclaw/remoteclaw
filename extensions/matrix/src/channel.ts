wonglaitung/openclaw_mini

Extensions: migrate matrix plugin-sdk imports

extensions/matrix/src/channel.ts
@@ -11,7 +11,7 @@ import {
   resolveDefaultGroupPolicy,
   setAccountEnabledInConfigSection,
   type ChannelPlugin,
-} from "openclaw/plugin-sdk";
+} from "openclaw/plugin-sdk/compat";
 import { matrixMessageActions } from "./actions.js";
 import { MatrixConfigSchema } from "./config-schema.js";
 import { listMatrixDirectoryGroupsLive, listMatrixDirectoryPeersLive } from "./directory-live.js";

extensions/matrix/src/matrix/monitor/access-policy.ts
@@ -3,7 +3,7 @@ import {
   issuePairingChallenge,
   readStoreAllowFromForDmPolicy,
   resolveDmGroupAccessWithLists,
-} from "openclaw/plugin-sdk";
+} from "openclaw/plugin-sdk/compat";
 import {
   normalizeMatrixAllowList,
   resolveMatrixAllowListMatch,

extensions/matrix/src/matrix/monitor/auto-join.ts
@@ -1,5 +1,5 @@
 import type { MatrixClient } from "@vector-im/matrix-bot-sdk";
-import type { RuntimeEnv } from "openclaw/plugin-sdk";
+import type { RuntimeEnv } from "openclaw/plugin-sdk/compat";
 import { getMatrixRuntime } from "../../runtime.js";
 import type { CoreConfig } from "../../types.js";
 import { loadMatrixSdk } from "../sdk-runtime.js";

extensions/matrix/src/matrix/poll-types.ts
@@ -7,7 +7,7 @@
  * - m.poll.end - Closes a poll
  */
 
-import type { PollInput } from "openclaw/plugin-sdk";
+import type { PollInput } from "openclaw/plugin-sdk/compat";
 
 export const M_POLL_START = "m.poll.start" as const;
 export const M_POLL_RESPONSE = "m.poll.response" as const;

extensions/matrix/src/matrix/send.ts
@@ -1,5 +1,5 @@
 import type { MatrixClient } from "@vector-im/matrix-bot-sdk";
-import type { PollInput } from "openclaw/plugin-sdk";
+import type { PollInput } from "openclaw/plugin-sdk/compat";
 import { getMatrixRuntime } from "../runtime.js";
 import { buildPollStartContent, M_POLL_START } from "./poll-types.js";
 import { enqueueSend } from "./send-queue.js";

extensions/matrix/src/outbound.ts
@@ -1,4 +1,4 @@
-import type { ChannelOutboundAdapter } from "openclaw/plugin-sdk";
+import type { ChannelOutboundAdapter } from "openclaw/plugin-sdk/compat";
 import { sendMessageMatrix, sendPollMatrix } from "./matrix/send.js";
 import { getMatrixRuntime } from "./runtime.js";
 

extensions/matrix/src/tool-actions.ts
@@ -5,7 +5,7 @@ import {
   readNumberParam,
   readReactionParams,
   readStringParam,
-} from "openclaw/plugin-sdk";
+} from "openclaw/plugin-sdk/compat";
 import {
   deleteMatrixMessage,
   editMatrixMessage,

remoteclaw/remoteclaw

(cherry picked from commit 15f7e329c24e4e51feb95f8b66b407cf06b02ef3)

extensions/matrix/src/channel.ts
@@ -11,7 +11,7 @@ import {
   resolveDefaultGroupPolicy,
   setAccountEnabledInConfigSection,
   type ChannelPlugin,
-} from "remoteclaw/plugin-sdk";
+} from "remoteclaw/plugin-sdk/compat";
 import { matrixMessageActions } from "./actions.js";
 import { MatrixConfigSchema } from "./config-schema.js";
 import { listMatrixDirectoryGroupsLive, listMatrixDirectoryPeersLive } from "./directory-live.js";

extensions/matrix/src/matrix/monitor/access-policy.ts
@@ -3,7 +3,7 @@ import {
   issuePairingChallenge,
   readStoreAllowFromForDmPolicy,
   resolveDmGroupAccessWithLists,
-} from "remoteclaw/plugin-sdk";
+} from "remoteclaw/plugin-sdk/compat";
 import {
   normalizeMatrixAllowList,
   resolveMatrixAllowListMatch,

extensions/matrix/src/matrix/monitor/auto-join.ts
@@ -1,5 +1,5 @@
 import type { MatrixClient } from "@vector-im/matrix-bot-sdk";
-import type { RuntimeEnv } from "remoteclaw/plugin-sdk";
+import type { RuntimeEnv } from "remoteclaw/plugin-sdk/compat";
 import { getMatrixRuntime } from "../../runtime.js";
 import type { CoreConfig } from "../../types.js";
 import { loadMatrixSdk } from "../sdk-runtime.js";

extensions/matrix/src/matrix/poll-types.ts
@@ -7,7 +7,7 @@
  * - m.poll.end - Closes a poll
  */
 
-import type { PollInput } from "remoteclaw/plugin-sdk";
+import type { PollInput } from "remoteclaw/plugin-sdk/compat";
 
 export const M_POLL_START = "m.poll.start" as const;
 export const M_POLL_RESPONSE = "m.poll.response" as const;

extensions/matrix/src/matrix/send.ts
@@ -1,5 +1,5 @@
 import type { MatrixClient } from "@vector-im/matrix-bot-sdk";
-import type { PollInput } from "remoteclaw/plugin-sdk";
+import type { PollInput } from "remoteclaw/plugin-sdk/compat";
 import { getMatrixRuntime } from "../runtime.js";
 import { buildPollStartContent, M_POLL_START } from "./poll-types.js";
 import { enqueueSend } from "./send-queue.js";

extensions/matrix/src/outbound.ts
@@ -1,4 +1,4 @@
-import type { ChannelOutboundAdapter } from "remoteclaw/plugin-sdk";
+import type { ChannelOutboundAdapter } from "remoteclaw/plugin-sdk/compat";
 import { sendMessageMatrix, sendPollMatrix } from "./matrix/send.js";
 import { getMatrixRuntime } from "./runtime.js";
 

extensions/matrix/src/tool-actions.ts
@@ -4,8 +4,7 @@ import {
   readNumberParam,
   readReactionParams,
   readStringParam,
-  type AgentToolResult,
-} from "remoteclaw/plugin-sdk";
+} from "remoteclaw/plugin-sdk/compat";
 import {
   deleteMatrixMessage,
   editMatrixMessage,
wonglaitung/openclaw_mini

Extensions: migrate device-pair plugin-sdk imports

extensions/device-pair/notify.ts
@@ -1,7 +1,7 @@
 import { promises as fs } from "node:fs";
 import path from "node:path";
-import type { OpenClawPluginApi } from "openclaw/plugin-sdk/core";
-import { listDevicePairing } from "openclaw/plugin-sdk/core";
+import type { OpenClawPluginApi } from "openclaw/plugin-sdk/compat";
+import { listDevicePairing } from "openclaw/plugin-sdk/compat";
 
 const NOTIFY_STATE_FILE = "device-pair-notify.json";
 const NOTIFY_POLL_INTERVAL_MS = 10_000;

remoteclaw/remoteclaw

(cherry picked from commit 2bb63868c6278d1f94bb92267cc10b7b19343809)

extensions/device-pair/notify.ts
@@ -1,7 +1,7 @@
 import { promises as fs } from "node:fs";
 import path from "node:path";
-import type { RemoteClawPluginApi } from "remoteclaw/plugin-sdk/core";
-import { listDevicePairing } from "remoteclaw/plugin-sdk/core";
+import type { OpenClawPluginApi } from "remoteclaw/plugin-sdk/compat";
+import { listDevicePairing } from "remoteclaw/plugin-sdk/compat";
 
 const NOTIFY_STATE_FILE = "device-pair-notify.json";
 const NOTIFY_POLL_INTERVAL_MS = 10_000;
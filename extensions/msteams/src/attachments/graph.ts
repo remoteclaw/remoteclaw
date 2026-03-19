wonglaitung/openclaw_mini

Extensions: migrate msteams plugin-sdk imports

extensions/msteams/src/attachments/graph.ts
@@ -1,4 +1,4 @@
-import { fetchWithSsrFGuard, type SsrFPolicy } from "openclaw/plugin-sdk";
+import { fetchWithSsrFGuard, type SsrFPolicy } from "openclaw/plugin-sdk/compat";
 import { getMSTeamsRuntime } from "../runtime.js";
 import { downloadMSTeamsAttachments } from "./download.js";
 import { downloadAndStoreMSTeamsRemoteMedia } from "./remote-media.js";

extensions/msteams/src/graph.ts
@@ -1,4 +1,4 @@
-import type { MSTeamsConfig } from "openclaw/plugin-sdk";
+import type { MSTeamsConfig } from "openclaw/plugin-sdk/compat";
 import { GRAPH_ROOT } from "./attachments/shared.js";
 import { loadMSTeamsSdkWithAuth } from "./sdk.js";
 import { readAccessToken } from "./token-response.js";

extensions/msteams/src/monitor-handler/message-handler.authz.test.ts
@@ -1,4 +1,4 @@
-import type { OpenClawConfig, PluginRuntime, RuntimeEnv } from "openclaw/plugin-sdk";
+import type { OpenClawConfig, PluginRuntime, RuntimeEnv } from "openclaw/plugin-sdk/compat";
 import { describe, expect, it, vi } from "vitest";
 import type { MSTeamsMessageHandlerDeps } from "../monitor-handler.js";
 import { setMSTeamsRuntime } from "../runtime.js";

extensions/msteams/src/monitor-handler/message-handler.ts
@@ -15,7 +15,7 @@ import {
   resolveEffectiveAllowFromLists,
   resolveDmGroupAccessWithLists,
   type HistoryEntry,
-} from "openclaw/plugin-sdk";
+} from "openclaw/plugin-sdk/compat";
 import {
   buildMSTeamsAttachmentPlaceholder,
   buildMSTeamsMediaPayload,

extensions/msteams/src/monitor.ts
@@ -7,7 +7,7 @@ import {
   summarizeMapping,
   type OpenClawConfig,
   type RuntimeEnv,
-} from "openclaw/plugin-sdk";
+} from "openclaw/plugin-sdk/compat";
 import { createMSTeamsConversationStoreFs } from "./conversation-store-fs.js";
 import type { MSTeamsConversationStore } from "./conversation-store.js";
 import { formatUnknownError } from "./errors.js";

extensions/msteams/src/outbound.ts
@@ -1,4 +1,4 @@
-import type { ChannelOutboundAdapter } from "openclaw/plugin-sdk";
+import type { ChannelOutboundAdapter } from "openclaw/plugin-sdk/compat";
 import { createMSTeamsPollStoreFs } from "./polls.js";
 import { getMSTeamsRuntime } from "./runtime.js";
 import { sendMessageMSTeams, sendPollMSTeams } from "./send.js";

extensions/msteams/src/policy.test.ts
@@ -1,4 +1,4 @@
-import type { MSTeamsConfig } from "openclaw/plugin-sdk";
+import type { MSTeamsConfig } from "openclaw/plugin-sdk/compat";
 import { describe, expect, it } from "vitest";
 import {
   isMSTeamsGroupAllowed,

extensions/msteams/src/test-runtime.ts
@@ -1,6 +1,6 @@
 import os from "node:os";
 import path from "node:path";
-import type { PluginRuntime } from "openclaw/plugin-sdk";
+import type { PluginRuntime } from "openclaw/plugin-sdk/compat";
 
 export const msteamsRuntimeStub = {
   state: {

remoteclaw/remoteclaw

(cherry picked from commit 10bd6ae3c8e110027208aa3ff0b1d6d0b5363afb)

extensions/msteams/src/attachments/graph.ts
@@ -1,4 +1,4 @@
-import { fetchWithSsrFGuard, type SsrFPolicy } from "remoteclaw/plugin-sdk";
+import { fetchWithSsrFGuard, type SsrFPolicy } from "remoteclaw/plugin-sdk/compat";
 import { getMSTeamsRuntime } from "../runtime.js";
 import { downloadMSTeamsAttachments } from "./download.js";
 import { downloadAndStoreMSTeamsRemoteMedia } from "./remote-media.js";

extensions/msteams/src/graph.ts
@@ -1,4 +1,4 @@
-import type { MSTeamsConfig } from "remoteclaw/plugin-sdk";
+import type { MSTeamsConfig } from "remoteclaw/plugin-sdk/compat";
 import { GRAPH_ROOT } from "./attachments/shared.js";
 import { loadMSTeamsSdkWithAuth } from "./sdk.js";
 import { readAccessToken } from "./token-response.js";

extensions/msteams/src/monitor-handler/message-handler.authz.test.ts
@@ -1,4 +1,4 @@
-import type { RemoteClawConfig, PluginRuntime, RuntimeEnv } from "remoteclaw/plugin-sdk";
+import type { OpenClawConfig, PluginRuntime, RuntimeEnv } from "remoteclaw/plugin-sdk/compat";
 import { describe, expect, it, vi } from "vitest";
 import type { MSTeamsMessageHandlerDeps } from "../monitor-handler.js";
 import { setMSTeamsRuntime } from "../runtime.js";

extensions/msteams/src/monitor-handler/message-handler.ts
@@ -15,7 +15,7 @@ import {
   resolveEffectiveAllowFromLists,
   resolveDmGroupAccessWithLists,
   type HistoryEntry,
-} from "remoteclaw/plugin-sdk";
+} from "remoteclaw/plugin-sdk/compat";
 import {
   buildMSTeamsAttachmentPlaceholder,
   buildMSTeamsMediaPayload,

extensions/msteams/src/monitor.ts
@@ -7,7 +7,7 @@ import {
   summarizeMapping,
   type RemoteClawConfig,
   type RuntimeEnv,
-} from "remoteclaw/plugin-sdk";
+} from "remoteclaw/plugin-sdk/compat";
 import { createMSTeamsConversationStoreFs } from "./conversation-store-fs.js";
 import type { MSTeamsConversationStore } from "./conversation-store.js";
 import { formatUnknownError } from "./errors.js";

extensions/msteams/src/outbound.ts
@@ -1,4 +1,4 @@
-import type { ChannelOutboundAdapter } from "remoteclaw/plugin-sdk";
+import type { ChannelOutboundAdapter } from "remoteclaw/plugin-sdk/compat";
 import { createMSTeamsPollStoreFs } from "./polls.js";
 import { getMSTeamsRuntime } from "./runtime.js";
 import { sendMessageMSTeams, sendPollMSTeams } from "./send.js";

extensions/msteams/src/policy.test.ts
@@ -1,4 +1,4 @@
-import type { MSTeamsConfig } from "remoteclaw/plugin-sdk";
+import type { MSTeamsConfig } from "remoteclaw/plugin-sdk/compat";
 import { describe, expect, it } from "vitest";
 import {
   isMSTeamsGroupAllowed,

extensions/msteams/src/test-runtime.ts
@@ -1,6 +1,6 @@
 import os from "node:os";
 import path from "node:path";
-import type { PluginRuntime } from "remoteclaw/plugin-sdk";
+import type { PluginRuntime } from "remoteclaw/plugin-sdk/compat";
 
 export const msteamsRuntimeStub = {
   state: {
wonglaitung/openclaw_mini

Extensions: migrate zalo plugin-sdk imports

extensions/zalo/src/accounts.ts
@@ -1,9 +1,9 @@
-import type { OpenClawConfig } from "openclaw/plugin-sdk";
 import {
   DEFAULT_ACCOUNT_ID,
   normalizeAccountId,
   normalizeOptionalAccountId,
 } from "openclaw/plugin-sdk/account-id";
+import type { OpenClawConfig } from "openclaw/plugin-sdk/compat";
 import { resolveZaloToken } from "./token.js";
 import type { ResolvedZaloAccount, ZaloAccountConfig, ZaloConfig } from "./types.js";
 

extensions/zalo/src/channel.sendpayload.test.ts
@@ -1,4 +1,4 @@
-import type { ReplyPayload } from "openclaw/plugin-sdk";
+import type { ReplyPayload } from "openclaw/plugin-sdk/compat";
 import { beforeEach, describe, expect, it, vi } from "vitest";
 import { zaloPlugin } from "./channel.js";
 

extensions/zalo/src/config-schema.ts
@@ -1,4 +1,4 @@
-import { MarkdownConfigSchema } from "openclaw/plugin-sdk";
+import { MarkdownConfigSchema } from "openclaw/plugin-sdk/compat";
 import { z } from "zod";
 import { buildSecretInputSchema } from "./secret-input.js";
 

extensions/zalo/src/group-access.ts
@@ -1,9 +1,9 @@
-import type { GroupPolicy, SenderGroupAccessDecision } from "openclaw/plugin-sdk";
+import type { GroupPolicy, SenderGroupAccessDecision } from "openclaw/plugin-sdk/compat";
 import {
   evaluateSenderGroupAccess,
   isNormalizedSenderAllowed,
   resolveOpenProviderRuntimeGroupPolicy,
-} from "openclaw/plugin-sdk";
+} from "openclaw/plugin-sdk/compat";
 
 const ZALO_ALLOW_FROM_PREFIX_RE = /^(zalo|zl):/i;
 

extensions/zalo/src/monitor.webhook.test.ts
@@ -1,6 +1,6 @@
 import { createServer, type RequestListener } from "node:http";
 import type { AddressInfo } from "node:net";
-import type { OpenClawConfig, PluginRuntime } from "openclaw/plugin-sdk";
+import type { OpenClawConfig, PluginRuntime } from "openclaw/plugin-sdk/compat";
 import { afterEach, describe, expect, it, vi } from "vitest";
 import { createEmptyPluginRegistry } from "../../../src/plugins/registry.js";
 import { setActivePluginRegistry } from "../../../src/plugins/runtime.js";

extensions/zalo/src/monitor.webhook.ts
@@ -1,6 +1,6 @@
 import { timingSafeEqual } from "node:crypto";
 import type { IncomingMessage, ServerResponse } from "node:http";
-import type { OpenClawConfig } from "openclaw/plugin-sdk";
+import type { OpenClawConfig } from "openclaw/plugin-sdk/compat";
 import {
   createDedupeCache,
   createFixedWindowRateLimiter,
@@ -15,7 +15,7 @@ import {
   resolveWebhookTargets,
   WEBHOOK_ANOMALY_COUNTER_DEFAULTS,
   WEBHOOK_RATE_LIMIT_DEFAULTS,
-} from "openclaw/plugin-sdk";
+} from "openclaw/plugin-sdk/compat";
 import type { ResolvedZaloAccount } from "./accounts.js";
 import type { ZaloFetch, ZaloUpdate } from "./api.js";
 import type { ZaloRuntimeEnv } from "./monitor.js";

extensions/zalo/src/probe.ts
@@ -1,4 +1,4 @@
-import type { BaseProbeResult } from "openclaw/plugin-sdk";
+import type { BaseProbeResult } from "openclaw/plugin-sdk/compat";
 import { getMe, ZaloApiError, type ZaloBotInfo, type ZaloFetch } from "./api.js";
 
 export type ZaloProbeResult = BaseProbeResult<string> & {

remoteclaw/remoteclaw

(cherry picked from commit dda86af8664f30e7d37ffa359849984a583ec47d)

extensions/zalo/src/accounts.ts
@@ -1,9 +1,9 @@
-import type { RemoteClawConfig } from "remoteclaw/plugin-sdk";
 import {
   DEFAULT_ACCOUNT_ID,
   normalizeAccountId,
   normalizeOptionalAccountId,
 } from "remoteclaw/plugin-sdk/account-id";
+import type { OpenClawConfig } from "remoteclaw/plugin-sdk/compat";
 import { resolveZaloToken } from "./token.js";
 import type { ResolvedZaloAccount, ZaloAccountConfig, ZaloConfig } from "./types.js";
 

extensions/zalo/src/channel.sendpayload.test.ts
@@ -1,4 +1,4 @@
-import type { ReplyPayload } from "remoteclaw/plugin-sdk";
+import type { ReplyPayload } from "remoteclaw/plugin-sdk/compat";
 import { beforeEach, describe, expect, it, vi } from "vitest";
 import { zaloPlugin } from "./channel.js";
 

extensions/zalo/src/config-schema.ts
@@ -1,8 +1,4 @@
-import {
-  AllowFromEntrySchema,
-  buildCatchallMultiAccountChannelSchema,
-  MarkdownConfigSchema,
-} from "remoteclaw/plugin-sdk";
+import { MarkdownConfigSchema } from "remoteclaw/plugin-sdk/compat";
 import { z } from "zod";
 
 const zaloAccountSchema = z.object({

extensions/zalo/src/group-access.ts
@@ -1,9 +1,9 @@
-import type { GroupPolicy, SenderGroupAccessDecision } from "remoteclaw/plugin-sdk";
+import type { GroupPolicy, SenderGroupAccessDecision } from "remoteclaw/plugin-sdk/compat";
 import {
   evaluateSenderGroupAccess,
   isNormalizedSenderAllowed,
   resolveOpenProviderRuntimeGroupPolicy,
-} from "remoteclaw/plugin-sdk";
+} from "remoteclaw/plugin-sdk/compat";
 
 const ZALO_ALLOW_FROM_PREFIX_RE = /^(zalo|zl):/i;
 

extensions/zalo/src/monitor.webhook.test.ts
@@ -1,6 +1,6 @@
 import { createServer, type RequestListener } from "node:http";
 import type { AddressInfo } from "node:net";
-import type { RemoteClawConfig, PluginRuntime } from "remoteclaw/plugin-sdk";
+import type { OpenClawConfig, PluginRuntime } from "remoteclaw/plugin-sdk/compat";
 import { afterEach, describe, expect, it, vi } from "vitest";
 import { createEmptyPluginRegistry } from "../../../src/plugins/registry.js";
 import { setActivePluginRegistry } from "../../../src/plugins/runtime.js";

extensions/zalo/src/monitor.webhook.ts
@@ -1,6 +1,6 @@
 import { timingSafeEqual } from "node:crypto";
 import type { IncomingMessage, ServerResponse } from "node:http";
-import type { RemoteClawConfig } from "remoteclaw/plugin-sdk";
+import type { OpenClawConfig } from "remoteclaw/plugin-sdk/compat";
 import {
   createDedupeCache,
   createFixedWindowRateLimiter,
@@ -13,7 +13,7 @@ import {
   resolveWebhookTargets,
   WEBHOOK_ANOMALY_COUNTER_DEFAULTS,
   WEBHOOK_RATE_LIMIT_DEFAULTS,
-} from "remoteclaw/plugin-sdk";
+} from "remoteclaw/plugin-sdk/compat";
 import type { ResolvedZaloAccount } from "./accounts.js";
 import type { ZaloFetch, ZaloUpdate } from "./api.js";
 import type { ZaloRuntimeEnv } from "./monitor.js";

extensions/zalo/src/probe.ts
@@ -1,4 +1,4 @@
-import type { BaseProbeResult } from "remoteclaw/plugin-sdk";
+import type { BaseProbeResult } from "remoteclaw/plugin-sdk/compat";
 import { getMe, ZaloApiError, type ZaloBotInfo, type ZaloFetch } from "./api.js";
 
 export type ZaloProbeResult = BaseProbeResult<string> & {
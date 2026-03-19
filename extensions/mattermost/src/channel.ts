wonglaitung/openclaw_mini

Extensions: migrate mattermost plugin-sdk imports

extensions/mattermost/src/channel.ts
@@ -12,7 +12,7 @@ import {
   type ChannelMessageActionAdapter,
   type ChannelMessageActionName,
   type ChannelPlugin,
-} from "openclaw/plugin-sdk";
+} from "openclaw/plugin-sdk/compat";
 import { MattermostConfigSchema } from "./config-schema.js";
 import { resolveMattermostGroupRequireMention } from "./group-mentions.js";
 import {

extensions/mattermost/src/group-mentions.ts
@@ -1,4 +1,4 @@
-import type { ChannelGroupContext } from "openclaw/plugin-sdk";
+import type { ChannelGroupContext } from "openclaw/plugin-sdk/compat";
 import { resolveMattermostAccount } from "./mattermost/accounts.js";
 
 export function resolveMattermostGroupRequireMention(

extensions/mattermost/src/mattermost/accounts.test.ts
@@ -1,4 +1,4 @@
-import type { OpenClawConfig } from "openclaw/plugin-sdk";
+import type { OpenClawConfig } from "openclaw/plugin-sdk/compat";
 import { describe, expect, it } from "vitest";
 import { resolveDefaultMattermostAccountId } from "./accounts.js";
 

extensions/mattermost/src/mattermost/accounts.ts
@@ -1,9 +1,9 @@
-import type { OpenClawConfig } from "openclaw/plugin-sdk";
 import {
   DEFAULT_ACCOUNT_ID,
   normalizeAccountId,
   normalizeOptionalAccountId,
 } from "openclaw/plugin-sdk/account-id";
+import type { OpenClawConfig } from "openclaw/plugin-sdk/compat";
 import { normalizeResolvedSecretInputString, normalizeSecretInputString } from "../secret-input.js";
 import type { MattermostAccountConfig, MattermostChatMode } from "../types.js";
 import { normalizeMattermostBaseUrl } from "./client.js";

extensions/mattermost/src/mattermost/monitor-auth.ts
@@ -1,4 +1,7 @@
-import { resolveAllowlistMatchSimple, resolveEffectiveAllowFromLists } from "openclaw/plugin-sdk";
+import {
+  resolveAllowlistMatchSimple,
+  resolveEffectiveAllowFromLists,
+} from "openclaw/plugin-sdk/compat";
 
 export function normalizeMattermostAllowEntry(entry: string): string {
   const trimmed = entry.trim();

extensions/mattermost/src/mattermost/monitor-helpers.ts
@@ -2,8 +2,8 @@ import {
   formatInboundFromLabel as formatInboundFromLabelShared,
   resolveThreadSessionKeys as resolveThreadSessionKeysShared,
   type OpenClawConfig,
-} from "openclaw/plugin-sdk";
-export { createDedupeCache, rawDataToString } from "openclaw/plugin-sdk";
+} from "openclaw/plugin-sdk/compat";
+export { createDedupeCache, rawDataToString } from "openclaw/plugin-sdk/compat";
 
 export type ResponsePrefixContext = {
   model?: string;

extensions/mattermost/src/mattermost/monitor-websocket.test.ts
@@ -1,4 +1,4 @@
-import type { RuntimeEnv } from "openclaw/plugin-sdk";
+import type { RuntimeEnv } from "openclaw/plugin-sdk/compat";
 import { describe, expect, it, vi } from "vitest";
 import {
   createMattermostConnectOnce,

remoteclaw/remoteclaw

(cherry picked from commit 009d4d115ada6825e6b6a8ac0b08d24d50d3da16)

extensions/mattermost/src/channel.ts
@@ -12,7 +12,7 @@ import {
   type ChannelMessageActionAdapter,
   type ChannelMessageActionName,
   type ChannelPlugin,
-} from "remoteclaw/plugin-sdk";
+} from "remoteclaw/plugin-sdk/compat";
 import { MattermostConfigSchema } from "./config-schema.js";
 import { resolveMattermostGroupRequireMention } from "./group-mentions.js";
 import {

extensions/mattermost/src/group-mentions.ts
@@ -1,4 +1,4 @@
-import type { ChannelGroupContext } from "remoteclaw/plugin-sdk";
+import type { ChannelGroupContext } from "remoteclaw/plugin-sdk/compat";
 import { resolveMattermostAccount } from "./mattermost/accounts.js";
 
 export function resolveMattermostGroupRequireMention(

extensions/mattermost/src/mattermost/accounts.test.ts
@@ -1,4 +1,4 @@
-import type { RemoteClawConfig } from "remoteclaw/plugin-sdk";
+import type { OpenClawConfig } from "remoteclaw/plugin-sdk/compat";
 import { describe, expect, it } from "vitest";
 import { resolveDefaultMattermostAccountId } from "./accounts.js";
 

extensions/mattermost/src/mattermost/accounts.ts
@@ -1,9 +1,10 @@
-import type { RemoteClawConfig } from "remoteclaw/plugin-sdk";
 import {
   DEFAULT_ACCOUNT_ID,
   normalizeAccountId,
   normalizeOptionalAccountId,
 } from "remoteclaw/plugin-sdk/account-id";
+import type { OpenClawConfig } from "remoteclaw/plugin-sdk/compat";
+import { normalizeResolvedSecretInputString, normalizeSecretInputString } from "../secret-input.js";
 import type { MattermostAccountConfig, MattermostChatMode } from "../types.js";
 import { normalizeMattermostBaseUrl } from "./client.js";
 

extensions/mattermost/src/mattermost/monitor-auth.ts
@@ -1,4 +1,7 @@
-import { resolveAllowlistMatchSimple, resolveEffectiveAllowFromLists } from "remoteclaw/plugin-sdk";
+import {
+  resolveAllowlistMatchSimple,
+  resolveEffectiveAllowFromLists,
+} from "remoteclaw/plugin-sdk/compat";
 
 export function normalizeMattermostAllowEntry(entry: string): string {
   const trimmed = entry.trim();

extensions/mattermost/src/mattermost/monitor-helpers.ts
@@ -1,9 +1,9 @@
 import {
   formatInboundFromLabel as formatInboundFromLabelShared,
   resolveThreadSessionKeys as resolveThreadSessionKeysShared,
-  type RemoteClawConfig,
-} from "remoteclaw/plugin-sdk";
-export { createDedupeCache, rawDataToString } from "remoteclaw/plugin-sdk";
+  type OpenClawConfig,
+} from "remoteclaw/plugin-sdk/compat";
+export { createDedupeCache, rawDataToString } from "remoteclaw/plugin-sdk/compat";
 
 export type ResponsePrefixContext = {
   model?: string;

extensions/mattermost/src/mattermost/monitor-websocket.test.ts
@@ -1,4 +1,4 @@
-import type { RuntimeEnv } from "remoteclaw/plugin-sdk";
+import type { RuntimeEnv } from "remoteclaw/plugin-sdk/compat";
 import { describe, expect, it, vi } from "vitest";
 import {
   createMattermostConnectOnce,
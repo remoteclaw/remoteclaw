wonglaitung/openclaw_mini

Extensions: migrate diagnostics-otel plugin-sdk imports

extensions/diagnostics-otel/src/service.test.ts
@@ -98,16 +98,18 @@ vi.mock("@opentelemetry/semantic-conventions", () => ({
   ATTR_SERVICE_NAME: "service.name",
 }));
 
-vi.mock("openclaw/plugin-sdk", async () => {
-  const actual = await vi.importActual<typeof import("openclaw/plugin-sdk")>("openclaw/plugin-sdk");
+vi.mock("openclaw/plugin-sdk/compat", async () => {
+  const actual = await vi.importActual<typeof import("openclaw/plugin-sdk/compat")>(
+    "openclaw/plugin-sdk/compat",
+  );
   return {
     ...actual,
     registerLogTransport: registerLogTransportMock,
   };
 });
 
-import type { OpenClawPluginServiceContext } from "openclaw/plugin-sdk";
-import { emitDiagnosticEvent } from "openclaw/plugin-sdk";
+import type { OpenClawPluginServiceContext } from "openclaw/plugin-sdk/compat";
+import { emitDiagnosticEvent } from "openclaw/plugin-sdk/compat";
 import { createDiagnosticsOtelService } from "./service.js";
 
 const OTEL_TEST_STATE_DIR = "/tmp/openclaw-diagnostics-otel-test";

extensions/diagnostics-otel/src/service.ts
@@ -9,8 +9,12 @@ import { PeriodicExportingMetricReader } from "@opentelemetry/sdk-metrics";
 import { NodeSDK } from "@opentelemetry/sdk-node";
 import { ParentBasedSampler, TraceIdRatioBasedSampler } from "@opentelemetry/sdk-trace-base";
 import { ATTR_SERVICE_NAME } from "@opentelemetry/semantic-conventions";
-import type { DiagnosticEventPayload, OpenClawPluginService } from "openclaw/plugin-sdk";
-import { onDiagnosticEvent, redactSensitiveText, registerLogTransport } from "openclaw/plugin-sdk";
+import type { DiagnosticEventPayload, OpenClawPluginService } from "openclaw/plugin-sdk/compat";
+import {
+  onDiagnosticEvent,
+  redactSensitiveText,
+  registerLogTransport,
+} from "openclaw/plugin-sdk/compat";
 
 const DEFAULT_SERVICE_NAME = "openclaw";
 

remoteclaw/remoteclaw

(cherry picked from commit 56d98a50cfff2aee9b8e36bde12b9537783b7692)

extensions/diagnostics-otel/src/service.test.ts
@@ -98,17 +98,18 @@ vi.mock("@opentelemetry/semantic-conventions", () => ({
   ATTR_SERVICE_NAME: "service.name",
 }));
 
-vi.mock("remoteclaw/plugin-sdk", async () => {
-  const actual =
-    await vi.importActual<typeof import("remoteclaw/plugin-sdk")>("remoteclaw/plugin-sdk");
+vi.mock("remoteclaw/plugin-sdk/compat", async () => {
+  const actual = await vi.importActual<typeof import("remoteclaw/plugin-sdk/compat")>(
+    "remoteclaw/plugin-sdk/compat",
+  );
   return {
     ...actual,
     registerLogTransport: registerLogTransportMock,
   };
 });
 
-import type { RemoteClawPluginServiceContext } from "remoteclaw/plugin-sdk";
-import { emitDiagnosticEvent } from "remoteclaw/plugin-sdk";
+import type { OpenClawPluginServiceContext } from "remoteclaw/plugin-sdk/compat";
+import { emitDiagnosticEvent } from "remoteclaw/plugin-sdk/compat";
 import { createDiagnosticsOtelService } from "./service.js";
 
 const OTEL_TEST_STATE_DIR = "/tmp/remoteclaw-diagnostics-otel-test";

extensions/diagnostics-otel/src/service.ts
@@ -9,12 +9,12 @@ import { PeriodicExportingMetricReader } from "@opentelemetry/sdk-metrics";
 import { NodeSDK } from "@opentelemetry/sdk-node";
 import { ParentBasedSampler, TraceIdRatioBasedSampler } from "@opentelemetry/sdk-trace-base";
 import { ATTR_SERVICE_NAME } from "@opentelemetry/semantic-conventions";
-import type { DiagnosticEventPayload, RemoteClawPluginService } from "remoteclaw/plugin-sdk";
+import type { DiagnosticEventPayload, OpenClawPluginService } from "remoteclaw/plugin-sdk/compat";
 import {
   onDiagnosticEvent,
   redactSensitiveText,
   registerLogTransport,
-} from "remoteclaw/plugin-sdk";
+} from "remoteclaw/plugin-sdk/compat";
 
 const DEFAULT_SERVICE_NAME = "remoteclaw";
 
wonglaitung/openclaw_mini

Plugins/phone-control: migrate to scoped plugin-sdk imports

extensions/phone-control/index.test.ts
@@ -1,12 +1,12 @@
 import fs from "node:fs/promises";
 import os from "node:os";
 import path from "node:path";
-import { describe, expect, it, vi } from "vitest";
 import type {
   OpenClawPluginApi,
   OpenClawPluginCommandDefinition,
   PluginCommandContext,
-} from "../../src/plugins/types.js";
+} from "openclaw/plugin-sdk/phone-control";
+import { describe, expect, it, vi } from "vitest";
 import registerPhoneControl from "./index.js";
 
 function createApi(params: {

extensions/phone-control/index.ts
@@ -1,6 +1,6 @@
 import fs from "node:fs/promises";
 import path from "node:path";
-import type { OpenClawPluginApi, OpenClawPluginService } from "openclaw/plugin-sdk/core";
+import type { OpenClawPluginApi, OpenClawPluginService } from "openclaw/plugin-sdk/phone-control";
 
 type ArmGroup = "camera" | "screen" | "writes" | "all";
 

remoteclaw/remoteclaw

(cherry picked from commit 71e62a77e8d45c675c01cf4096f74e2c696d7f54)

extensions/phone-control/index.test.ts
@@ -1,12 +1,12 @@
 import fs from "node:fs/promises";
 import os from "node:os";
 import path from "node:path";
-import { describe, expect, it, vi } from "vitest";
 import type {
   RemoteClawPluginApi,
   RemoteClawPluginCommandDefinition,
   PluginCommandContext,
-} from "../../src/plugins/types.js";
+} from "remoteclaw/plugin-sdk/phone-control";
+import { describe, expect, it, vi } from "vitest";
 import registerPhoneControl from "./index.js";
 
 function createApi(params: {

extensions/phone-control/index.ts
@@ -1,6 +1,6 @@
 import fs from "node:fs/promises";
 import path from "node:path";
-import type { RemoteClawPluginApi, RemoteClawPluginService } from "remoteclaw/plugin-sdk/core";
+import type { OpenClawPluginApi, OpenClawPluginService } from "remoteclaw/plugin-sdk/phone-control";
 
 type ArmGroup = "camera" | "screen" | "writes" | "all";
 
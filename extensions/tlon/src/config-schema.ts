wonglaitung/openclaw_mini

Plugins/tlon: migrate to scoped plugin-sdk imports

extensions/tlon/src/config-schema.ts
@@ -1,4 +1,4 @@
-import { buildChannelConfigSchema } from "openclaw/plugin-sdk/compat";
+import { buildChannelConfigSchema } from "openclaw/plugin-sdk/tlon";
 import { z } from "zod";
 
 const ShipSchema = z.string().min(1);

extensions/tlon/src/monitor/discovery.ts
@@ -1,4 +1,4 @@
-import type { RuntimeEnv } from "openclaw/plugin-sdk/compat";
+import type { RuntimeEnv } from "openclaw/plugin-sdk/tlon";
 import type { Foreigns } from "../urbit/foreigns.js";
 import { formatChangesDate } from "./utils.js";
 

extensions/tlon/src/monitor/history.ts
@@ -1,4 +1,4 @@
-import type { RuntimeEnv } from "openclaw/plugin-sdk/compat";
+import type { RuntimeEnv } from "openclaw/plugin-sdk/tlon";
 import { extractMessageText } from "./utils.js";
 
 /**

extensions/tlon/src/monitor/index.ts
@@ -1,5 +1,5 @@
-import type { RuntimeEnv, ReplyPayload, OpenClawConfig } from "openclaw/plugin-sdk/compat";
-import { createLoggerBackedRuntime, createReplyPrefixOptions } from "openclaw/plugin-sdk/compat";
+import type { RuntimeEnv, ReplyPayload, OpenClawConfig } from "openclaw/plugin-sdk/tlon";
+import { createLoggerBackedRuntime, createReplyPrefixOptions } from "openclaw/plugin-sdk/tlon";
 import { getTlonRuntime } from "../runtime.js";
 import { createSettingsManager, type TlonSettingsStore } from "../settings.js";
 import { normalizeShip, parseChannelNest } from "../targets.js";

extensions/tlon/src/runtime.ts
@@ -1,4 +1,4 @@
-import type { PluginRuntime } from "openclaw/plugin-sdk/compat";
+import type { PluginRuntime } from "openclaw/plugin-sdk/tlon";
 
 let runtime: PluginRuntime | null = null;
 

extensions/tlon/src/types.ts
@@ -1,4 +1,4 @@
-import type { OpenClawConfig } from "openclaw/plugin-sdk/compat";
+import type { OpenClawConfig } from "openclaw/plugin-sdk/tlon";
 
 export type TlonResolvedAccount = {
   accountId: string;

extensions/tlon/src/urbit/channel-ops.ts
@@ -1,4 +1,4 @@
-import type { LookupFn, SsrFPolicy } from "openclaw/plugin-sdk/compat";
+import type { LookupFn, SsrFPolicy } from "openclaw/plugin-sdk/tlon";
 import { UrbitHttpError } from "./errors.js";
 import { urbitFetch } from "./fetch.js";
 

extensions/tlon/src/urbit/context.ts
@@ -1,4 +1,4 @@
-import type { SsrFPolicy } from "openclaw/plugin-sdk/compat";
+import type { SsrFPolicy } from "openclaw/plugin-sdk/tlon";
 import { validateUrbitBaseUrl } from "./base-url.js";
 import { UrbitUrlError } from "./errors.js";
 

remoteclaw/remoteclaw

(cherry picked from commit 72e774431c1e425e34b64574aa429f9f88a2ceba)

extensions/tlon/src/config-schema.ts
@@ -1,4 +1,4 @@
-import { buildChannelConfigSchema } from "remoteclaw/plugin-sdk/compat";
+import { buildChannelConfigSchema } from "remoteclaw/plugin-sdk/tlon";
 import { z } from "zod";
 
 const ShipSchema = z.string().min(1);

extensions/tlon/src/monitor/discovery.ts
@@ -1,4 +1,4 @@
-import type { RuntimeEnv } from "remoteclaw/plugin-sdk/compat";
+import type { RuntimeEnv } from "remoteclaw/plugin-sdk/tlon";
 import type { Foreigns } from "../urbit/foreigns.js";
 import { formatChangesDate } from "./utils.js";
 

extensions/tlon/src/monitor/history.ts
@@ -1,4 +1,4 @@
-import type { RuntimeEnv } from "remoteclaw/plugin-sdk/compat";
+import type { RuntimeEnv } from "remoteclaw/plugin-sdk/tlon";
 import { extractMessageText } from "./utils.js";
 
 /**

extensions/tlon/src/monitor/index.ts
@@ -1,5 +1,5 @@
-import type { RuntimeEnv, ReplyPayload, OpenClawConfig } from "remoteclaw/plugin-sdk/compat";
-import { createLoggerBackedRuntime, createReplyPrefixOptions } from "remoteclaw/plugin-sdk/compat";
+import type { RuntimeEnv, ReplyPayload, OpenClawConfig } from "remoteclaw/plugin-sdk/tlon";
+import { createLoggerBackedRuntime, createReplyPrefixOptions } from "remoteclaw/plugin-sdk/tlon";
 import { getTlonRuntime } from "../runtime.js";
 import { createSettingsManager, type TlonSettingsStore } from "../settings.js";
 import { normalizeShip, parseChannelNest } from "../targets.js";

extensions/tlon/src/runtime.ts
@@ -1,4 +1,4 @@
-import type { PluginRuntime } from "remoteclaw/plugin-sdk/compat";
+import type { PluginRuntime } from "remoteclaw/plugin-sdk/tlon";
 
 const { setRuntime: setTlonRuntime, getRuntime: getTlonRuntime } =
   createPluginRuntimeStore<PluginRuntime>("Tlon runtime not initialized");

extensions/tlon/src/types.ts
@@ -1,4 +1,4 @@
-import type { OpenClawConfig } from "remoteclaw/plugin-sdk/compat";
+import type { OpenClawConfig } from "remoteclaw/plugin-sdk/tlon";
 
 export type TlonResolvedAccount = {
   accountId: string;

extensions/tlon/src/urbit/channel-ops.ts
@@ -1,4 +1,4 @@
-import type { LookupFn, SsrFPolicy } from "remoteclaw/plugin-sdk/compat";
+import type { LookupFn, SsrFPolicy } from "remoteclaw/plugin-sdk/tlon";
 import { UrbitHttpError } from "./errors.js";
 import { urbitFetch } from "./fetch.js";
 

extensions/tlon/src/urbit/context.ts
@@ -1,4 +1,4 @@
-import type { SsrFPolicy } from "remoteclaw/plugin-sdk/compat";
+import type { SsrFPolicy } from "remoteclaw/plugin-sdk/tlon";
 import { validateUrbitBaseUrl } from "./base-url.js";
 import { UrbitUrlError } from "./errors.js";
 
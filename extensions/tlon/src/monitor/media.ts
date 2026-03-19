wonglaitung/openclaw_mini

feat: update tlon channel/plugin to be more fully featured (#21208)
* feat(tlon): sync with openclaw-tlon master

- Add tlon CLI tool registration with binary lookup
- Add approval, media, settings, foreigns, story, upload modules
- Add http-api wrapper for Urbit connection patching
- Update types for defaultAuthorizedShips support
- Fix type compatibility with core plugin SDK
- Stub uploadFile (API not yet available in @tloncorp/api-beta)
- Remove incompatible test files (security, sse-client, upload)

* chore(tlon): remove dead code

Remove unused Urbit channel client files:
- channel-client.ts
- channel-ops.ts
- context.ts

These were not imported anywhere in the extension.

* feat(tlon): add image upload support via @tloncorp/api

- Import configureClient and uploadFile from @tloncorp/api
- Implement uploadImageFromUrl using uploadFile
- Configure API client before media uploads
- Update dependency to github:tloncorp/api-beta#main

* fix(tlon): restore SSRF protection with event ack tracking

- Restore context.ts and channel-ops.ts for SSRF support
- Restore sse-client.ts with urbitFetch for SSRF-protected requests
- Add event ack tracking from openclaw-tlon (acks every 20 events)
- Pass ssrfPolicy through authenticate() and UrbitSSEClient
- Fixes security regression from sync with openclaw-tlon

* fix(tlon): restore buildTlonAccountFields for allowPrivateNetwork

The inlined payload building was missing allowPrivateNetwork field,
which would prevent the setting from being persisted to config.

* fix(tlon): restore SSRF protection in probeAccount

- Restore channel-client.ts for UrbitChannelClient
- Use UrbitChannelClient with ssrfPolicy in probeAccount
- Ensures account probe respects allowPrivateNetwork setting

* feat(tlon): add ownerShip to setup flow

ownerShip should always be set as it controls who receives
approval requests and can approve/deny actions.

* chore(tlon): remove unused http-api.ts

After restoring SSRF protection, probeAccount uses UrbitCha

extensions/tlon/src/monitor/media.ts
@@ -1,0 +1,166 @@
+import { randomUUID } from "node:crypto";
+import { createWriteStream } from "node:fs";
+import { mkdir } from "node:fs/promises";
+import { homedir } from "node:os";
+import * as path from "node:path";
+import { Readable } from "node:stream";
+import { pipeline } from "node:stream/promises";
+import { fetchWithSsrFGuard } from "openclaw/plugin-sdk";
+import { getDefaultSsrFPolicy } from "../urbit/context.js";
+
+// Default to OpenClaw workspace media directory
+const DEFAULT_MEDIA_DIR = path.join(homedir(), ".openclaw", "workspace", "media", "inbound");
+
+export interface ExtractedImage {
+  url: string;
+  alt?: string;
+}
+
+export interface DownloadedMedia {
+  localPath: string;
+  contentType: string;
+  originalUrl: string;
+}
+
+/**
+ * Extract image blocks from Tlon message content.
+ * Returns array of image URLs found in the message.
+ */
+export function extractImageBlocks(content: unknown): ExtractedImage[] {
+  if (!content || !Array.isArray(content)) {
+    return [];
+  }
+
+  const images: ExtractedImage[] = [];
+
+  for (const verse of content) {
+    if (verse?.block?.image?.src) {
+      images.push({
+        url: verse.block.image.src,
+        alt: verse.block.image.alt,
+      });
+    }
+  }
+
+  return images;
+}
+
+/**
+ * Download a media file from URL to local storage.
+ * Returns the local path where the file was saved.
+ */
+export async function downloadMedia(
+  url: string,
+  mediaDir: string = DEFAULT_MEDIA_DIR,
+): Promise<DownloadedMedia | null> {
+  try {
+    // Validate URL is http/https before fetching
+    const parsedUrl = new URL(url);
+    if (parsedUrl.protocol !== "http:" && parsedUrl.protocol !== "https:") {
+      console.warn(`[tlon-media] Rejected non-http(s) URL: ${url}`);
+      return null;
+    }
+
+    // Ensure media directory exists
+    await mkdir(mediaDir, { recursive: true });
+
+    // Fetch with SSRF protection
+    // Use fetchWithSsrFGuard directly (not urbitFetch) to preserve the full URL path
+    const { response, release } = await fetchWithSsrFGuard({
+      url,
+      init: { method: "GET" },
+      policy: getDefaultSsrFPolicy(),
+      auditContext: "tlon-media-download",
+    });
+
+    try {
+      if (!response.ok) {
+        console.error(`[tlon-media] Failed to fetch ${url}: ${response.status}`);
+        return null;
+      }
+
+      // Determine content type and extension
+      const contentType = response.headers.get("content-type") || "application/octet-stream";
+      const ext = getExtensionFromContentType(contentType) || getExtensionFromUrl(url) || "bin";
+
+      // Generate unique filename
+      const filename = `${randomUUID()}.${ext}`;
+      const localPath = path.join(mediaDir, filename);
+
+      // Stream to file
+      const body = response.body;
+      if (!body) {
+        console.error(`[tlon-media] No response body for ${url}`);
+        return null;
+      }
+
+      const writeStream = createWriteStream(localPath);
+      await pipeline(Readable.fromWeb(body as any), writeStream);
+
+      return {
+        localPath,
+        contentType,
+        originalUrl: url,
+      };
+    } finally {
+      await release();
+    }
+  } catch (error: any) {
+    console.error(`[tlon-media] Error downloading ${url}: ${error?.message ?? String(error)}`);
+    return null;
+  }
+}
+
+function getExtensionFromContentType(contentType: string): string | null {
+  const map: Record<string, string> = {
+    "image/jpeg": "jpg",
+    "image/jpg": "jpg",
+    "image/png": "png",
+    "image/gif": "gif",
+    "image/webp": "webp",
+    "image/svg+xml": "svg",
+    "video/mp4": "mp4",
+    "video/webm": "webm",
+    "audio/mpeg": "mp3",
+    "audio/ogg": "ogg",
+  };
+  return map[contentType.split(";")[0].trim()] ?? null;
+}
+
+function getExtensionFromUrl(url: string): string | null {
+  try {
+    const pathname = new URL(url).pathname;
+    const match = pathname.match(/\.([a-z0-9]+)$/i);
+    return match ? match[1].toLowerCase() : null;
+  } catch {
+    return null;
+  }
+}
+
+/**
+ * Download all images from a message and return attachment metadata.
+ * Format matches OpenClaw's expected attachment structure.
+ */
+export async function downloadMessageImages(
+  content: unknown,
+  mediaDir?: string,
+): Promise<Array<{ path: string; contentType: string }>> {
+  const images = extractImageBlocks(content);
+  if (images.length === 0) {
+    return [];
+  }
+
+  const attachments: Array<{ path: string; contentType: string }> = [];
+
+  for (const image of images) {
+    const downloaded = await downloadMedia(image.url, mediaDir);
+    if (downloaded) {
+      attachments.push({
+        path: downloaded.localPath,
+        contentType: downloaded.contentType,
+      });
+    }
+  }
+
+  return attachments;
+}

extensions/tlon/src/urbit/upload.ts
@@ -1,0 +1,60 @@
+/**
+ * Upload an image from a URL to Tlon storage.
+ */
+import { uploadFile } from "@tloncorp/api";
+import { fetchWithSsrFGuard } from "openclaw/plugin-sdk";
+import { getDefaultSsrFPolicy } from "./context.js";
+
+/**
+ * Fetch an image from a URL and upload it to Tlon storage.
+ * Returns the uploaded URL, or falls back to the original URL on error.
+ *
+ * Note: configureClient must be called before using this function.
+ */
+export async function uploadImageFromUrl(imageUrl: string): Promise<string> {
+  try {
+    // Validate URL is http/https before fetching
+    const url = new URL(imageUrl);
+    if (url.protocol !== "http:" && url.protocol !== "https:") {
+      console.warn(`[tlon] Rejected non-http(s) URL: ${imageUrl}`);
+      return imageUrl;
+    }
+
+    // Fetch the image with SSRF protection
+    // Use fetchWithSsrFGuard directly (not urbitFetch) to preserve the full URL path
+    const { response, release } = await fetchWithSsrFGuard({
+      url: imageUrl,
+      init: { method: "GET" },
+      policy: getDefaultSsrFPolicy(),
+      auditContext: "tlon-upload-image",
+    });
+
+    try {
+      if (!response.ok) {
+        console.warn(`[tlon] Failed to fetch image from ${imageUrl}: ${response.status}`);
+        return imageUrl;
+      }
+
+      const contentType = response.headers.get("content-type") || "image/png";
+      const blob = await response.blob();
+
+      // Extract filename from URL or use a default
+      const urlPath = new URL(imageUrl).pathname;
+      const fileName = urlPath.split("/").pop() || `upload-${Date.now()}.png`;
+
+      // Upload to Tlon storage
+      const result = await uploadFile({
+        blob,
+        fileName,
+        contentType,
+      });
+
+      return result.url;
+    } finally {
+      await release();
+    }
+  } catch (err) {
+    console.warn(`[tlon] Failed to upload image, using original URL: ${err}`);
+    return imageUrl;
+  }
+}

remoteclaw/remoteclaw

extensions/tlon/src/monitor/media.ts
@@ -1,0 +1,166 @@
+import { randomUUID } from "node:crypto";
+import { createWriteStream } from "node:fs";
+import { mkdir } from "node:fs/promises";
+import { homedir } from "node:os";
+import * as path from "node:path";
+import { Readable } from "node:stream";
+import { pipeline } from "node:stream/promises";
+import { fetchWithSsrFGuard } from "remoteclaw/plugin-sdk";
+import { getDefaultSsrFPolicy } from "../urbit/context.js";
+
+// Default to RemoteClaw workspace media directory
+const DEFAULT_MEDIA_DIR = path.join(homedir(), ".remoteclaw", "workspace", "media", "inbound");
+
+export interface ExtractedImage {
+  url: string;
+  alt?: string;
+}
+
+export interface DownloadedMedia {
+  localPath: string;
+  contentType: string;
+  originalUrl: string;
+}
+
+/**
+ * Extract image blocks from Tlon message content.
+ * Returns array of image URLs found in the message.
+ */
+export function extractImageBlocks(content: unknown): ExtractedImage[] {
+  if (!content || !Array.isArray(content)) {
+    return [];
+  }
+
+  const images: ExtractedImage[] = [];
+
+  for (const verse of content) {
+    if (verse?.block?.image?.src) {
+      images.push({
+        url: verse.block.image.src,
+        alt: verse.block.image.alt,
+      });
+    }
+  }
+
+  return images;
+}
+
+/**
+ * Download a media file from URL to local storage.
+ * Returns the local path where the file was saved.
+ */
+export async function downloadMedia(
+  url: string,
+  mediaDir: string = DEFAULT_MEDIA_DIR,
+): Promise<DownloadedMedia | null> {
+  try {
+    // Validate URL is http/https before fetching
+    const parsedUrl = new URL(url);
+    if (parsedUrl.protocol !== "http:" && parsedUrl.protocol !== "https:") {
+      console.warn(`[tlon-media] Rejected non-http(s) URL: ${url}`);
+      return null;
+    }
+
+    // Ensure media directory exists
+    await mkdir(mediaDir, { recursive: true });
+
+    // Fetch with SSRF protection
+    // Use fetchWithSsrFGuard directly (not urbitFetch) to preserve the full URL path
+    const { response, release } = await fetchWithSsrFGuard({
+      url,
+      init: { method: "GET" },
+      policy: getDefaultSsrFPolicy(),
+      auditContext: "tlon-media-download",
+    });
+
+    try {
+      if (!response.ok) {
+        console.error(`[tlon-media] Failed to fetch ${url}: ${response.status}`);
+        return null;
+      }
+
+      // Determine content type and extension
+      const contentType = response.headers.get("content-type") || "application/octet-stream";
+      const ext = getExtensionFromContentType(contentType) || getExtensionFromUrl(url) || "bin";
+
+      // Generate unique filename
+      const filename = `${randomUUID()}.${ext}`;
+      const localPath = path.join(mediaDir, filename);
+
+      // Stream to file
+      const body = response.body;
+      if (!body) {
+        console.error(`[tlon-media] No response body for ${url}`);
+        return null;
+      }
+
+      const writeStream = createWriteStream(localPath);
+      await pipeline(Readable.fromWeb(body as any), writeStream);
+
+      return {
+        localPath,
+        contentType,
+        originalUrl: url,
+      };
+    } finally {
+      await release();
+    }
+  } catch (error: any) {
+    console.error(`[tlon-media] Error downloading ${url}: ${error?.message ?? String(error)}`);
+    return null;
+  }
+}
+
+function getExtensionFromContentType(contentType: string): string | null {
+  const map: Record<string, string> = {
+    "image/jpeg": "jpg",
+    "image/jpg": "jpg",
+    "image/png": "png",
+    "image/gif": "gif",
+    "image/webp": "webp",
+    "image/svg+xml": "svg",
+    "video/mp4": "mp4",
+    "video/webm": "webm",
+    "audio/mpeg": "mp3",
+    "audio/ogg": "ogg",
+  };
+  return map[contentType.split(";")[0].trim()] ?? null;
+}
+
+function getExtensionFromUrl(url: string): string | null {
+  try {
+    const pathname = new URL(url).pathname;
+    const match = pathname.match(/\.([a-z0-9]+)$/i);
+    return match ? match[1].toLowerCase() : null;
+  } catch {
+    return null;
+  }
+}
+
+/**
+ * Download all images from a message and return attachment metadata.
+ * Format matches RemoteClaw's expected attachment structure.
+ */
+export async function downloadMessageImages(
+  content: unknown,
+  mediaDir?: string,
+): Promise<Array<{ path: string; contentType: string }>> {
+  const images = extractImageBlocks(content);
+  if (images.length === 0) {
+    return [];
+  }
+
+  const attachments: Array<{ path: string; contentType: string }> = [];
+
+  for (const image of images) {
+    const downloaded = await downloadMedia(image.url, mediaDir);
+    if (downloaded) {
+      attachments.push({
+        path: downloaded.localPath,
+        contentType: downloaded.contentType,
+      });
+    }
+  }
+
+  return attachments;
+}

extensions/tlon/src/urbit/upload.ts
@@ -1,0 +1,60 @@
+/**
+ * Upload an image from a URL to Tlon storage.
+ */
+import { uploadFile } from "@tloncorp/api";
+import { fetchWithSsrFGuard } from "remoteclaw/plugin-sdk";
+import { getDefaultSsrFPolicy } from "./context.js";
+
+/**
+ * Fetch an image from a URL and upload it to Tlon storage.
+ * Returns the uploaded URL, or falls back to the original URL on error.
+ *
+ * Note: configureClient must be called before using this function.
+ */
+export async function uploadImageFromUrl(imageUrl: string): Promise<string> {
+  try {
+    // Validate URL is http/https before fetching
+    const url = new URL(imageUrl);
+    if (url.protocol !== "http:" && url.protocol !== "https:") {
+      console.warn(`[tlon] Rejected non-http(s) URL: ${imageUrl}`);
+      return imageUrl;
+    }
+
+    // Fetch the image with SSRF protection
+    // Use fetchWithSsrFGuard directly (not urbitFetch) to preserve the full URL path
+    const { response, release } = await fetchWithSsrFGuard({
+      url: imageUrl,
+      init: { method: "GET" },
+      policy: getDefaultSsrFPolicy(),
+      auditContext: "tlon-upload-image",
+    });
+
+    try {
+      if (!response.ok) {
+        console.warn(`[tlon] Failed to fetch image from ${imageUrl}: ${response.status}`);
+        return imageUrl;
+      }
+
+      const contentType = response.headers.get("content-type") || "image/png";
+      const blob = await response.blob();
+
+      // Extract filename from URL or use a default
+      const urlPath = new URL(imageUrl).pathname;
+      const fileName = urlPath.split("/").pop() || `upload-${Date.now()}.png`;
+
+      // Upload to Tlon storage
+      const result = await uploadFile({
+        blob,
+        fileName,
+        contentType,
+      });
+
+      return result.url;
+    } finally {
+      await release();
+    }
+  } catch (err) {
+    console.warn(`[tlon] Failed to upload image, using original URL: ${err}`);
+    return imageUrl;
+  }
+}
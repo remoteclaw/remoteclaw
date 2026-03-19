wonglaitung/openclaw_mini

Extensions: migrate googlechat plugin-sdk imports

extensions/googlechat/src/actions.ts
@@ -2,7 +2,7 @@ import type {
   ChannelMessageActionAdapter,
   ChannelMessageActionName,
   OpenClawConfig,
-} from "openclaw/plugin-sdk";
+} from "openclaw/plugin-sdk/compat";
 import {
   createActionGate,
   extractToolSend,
@@ -10,7 +10,7 @@ import {
   readNumberParam,
   readReactionParams,
   readStringParam,
-} from "openclaw/plugin-sdk";
+} from "openclaw/plugin-sdk/compat";
 import { listEnabledGoogleChatAccounts, resolveGoogleChatAccount } from "./accounts.js";
 import {
   createGoogleChatReaction,

extensions/googlechat/src/api.ts
@@ -1,5 +1,5 @@
 import crypto from "node:crypto";
-import { fetchWithSsrFGuard } from "openclaw/plugin-sdk";
+import { fetchWithSsrFGuard } from "openclaw/plugin-sdk/compat";
 import type { ResolvedGoogleChatAccount } from "./accounts.js";
 import { getGoogleChatAccessToken } from "./auth.js";
 import type { GoogleChatReaction } from "./types.js";

extensions/googlechat/src/channel.startup.test.ts
@@ -1,4 +1,4 @@
-import type { ChannelAccountSnapshot } from "openclaw/plugin-sdk";
+import type { ChannelAccountSnapshot } from "openclaw/plugin-sdk/compat";
 import { afterEach, describe, expect, it, vi } from "vitest";
 import { createStartAccountContext } from "../../test-utils/start-account-context.js";
 import type { ResolvedGoogleChatAccount } from "./accounts.js";

extensions/googlechat/src/channel.ts
@@ -19,8 +19,8 @@ import {
   type ChannelPlugin,
   type ChannelStatusIssue,
   type OpenClawConfig,
-} from "openclaw/plugin-sdk";
-import { GoogleChatConfigSchema } from "openclaw/plugin-sdk";
+} from "openclaw/plugin-sdk/compat";
+import { GoogleChatConfigSchema } from "openclaw/plugin-sdk/compat";
 import {
   listGoogleChatAccountIds,
   resolveDefaultGoogleChatAccountId,

extensions/googlechat/src/monitor.ts
@@ -1,12 +1,12 @@
 import type { IncomingMessage, ServerResponse } from "node:http";
-import type { OpenClawConfig } from "openclaw/plugin-sdk";
+import type { OpenClawConfig } from "openclaw/plugin-sdk/compat";
 import {
   createWebhookInFlightLimiter,
   createReplyPrefixOptions,
   registerWebhookTargetWithPluginRoute,
   resolveInboundRouteEnvelopeBuilderWithRuntime,
   resolveWebhookPath,
-} from "openclaw/plugin-sdk";
+} from "openclaw/plugin-sdk/compat";
 import { type ResolvedGoogleChatAccount } from "./accounts.js";
 import {
   downloadGoogleChatMedia,

extensions/googlechat/src/onboarding.ts
@@ -1,4 +1,4 @@
-import type { OpenClawConfig, DmPolicy } from "openclaw/plugin-sdk";
+import type { OpenClawConfig, DmPolicy } from "openclaw/plugin-sdk/compat";
 import {
   addWildcardAllowFrom,
   formatDocsLink,
@@ -10,7 +10,7 @@ import {
   DEFAULT_ACCOUNT_ID,
   normalizeAccountId,
   migrateBaseNameToDefaultAccount,
-} from "openclaw/plugin-sdk";
+} from "openclaw/plugin-sdk/compat";
 import {
   listGoogleChatAccountIds,
   resolveDefaultGoogleChatAccountId,

extensions/googlechat/src/runtime.ts
@@ -1,4 +1,4 @@
-import type { PluginRuntime } from "openclaw/plugin-sdk";
+import type { PluginRuntime } from "openclaw/plugin-sdk/compat";
 
 let runtime: PluginRuntime | null = null;
 

extensions/googlechat/src/types.config.ts
@@ -1,3 +1,3 @@
-import type { GoogleChatAccountConfig, GoogleChatConfig } from "openclaw/plugin-sdk";
+import type { GoogleChatAccountConfig, GoogleChatConfig } from "openclaw/plugin-sdk/compat";
 
 export type { GoogleChatAccountConfig, GoogleChatConfig };

remoteclaw/remoteclaw

(cherry picked from commit 39a55844bc5dc02da225adad0b37c00f5d5f53ff)

extensions/googlechat/src/actions.ts
@@ -1,8 +1,8 @@
 import type {
   ChannelMessageActionAdapter,
   ChannelMessageActionName,
-  RemoteClawConfig,
-} from "remoteclaw/plugin-sdk";
+  OpenClawConfig,
+} from "remoteclaw/plugin-sdk/compat";
 import {
   createActionGate,
   extractToolSend,
@@ -10,7 +10,7 @@ import {
   readNumberParam,
   readReactionParams,
   readStringParam,
-} from "remoteclaw/plugin-sdk";
+} from "remoteclaw/plugin-sdk/compat";
 import { listEnabledGoogleChatAccounts, resolveGoogleChatAccount } from "./accounts.js";
 import {
   createGoogleChatReaction,

extensions/googlechat/src/api.ts
@@ -1,5 +1,5 @@
 import crypto from "node:crypto";
-import { fetchWithSsrFGuard } from "remoteclaw/plugin-sdk";
+import { fetchWithSsrFGuard } from "remoteclaw/plugin-sdk/compat";
 import type { ResolvedGoogleChatAccount } from "./accounts.js";
 import { getGoogleChatAccessToken } from "./auth.js";
 import type { GoogleChatReaction } from "./types.js";

extensions/googlechat/src/channel.startup.test.ts
@@ -1,4 +1,4 @@
-import type { ChannelAccountSnapshot } from "remoteclaw/plugin-sdk";
+import type { ChannelAccountSnapshot } from "remoteclaw/plugin-sdk/compat";
 import { afterEach, describe, expect, it, vi } from "vitest";
 import { createStartAccountContext } from "../../test-utils/start-account-context.js";
 import type { ResolvedGoogleChatAccount } from "./accounts.js";

extensions/googlechat/src/channel.ts
@@ -18,9 +18,9 @@ import {
   type ChannelMessageActionAdapter,
   type ChannelPlugin,
   type ChannelStatusIssue,
-  type RemoteClawConfig,
-} from "remoteclaw/plugin-sdk";
-import { GoogleChatConfigSchema } from "remoteclaw/plugin-sdk";
+  type OpenClawConfig,
+} from "remoteclaw/plugin-sdk/compat";
+import { GoogleChatConfigSchema } from "remoteclaw/plugin-sdk/compat";
 import {
   listGoogleChatAccountIds,
   resolveDefaultGoogleChatAccountId,

extensions/googlechat/src/monitor.ts
@@ -1,5 +1,5 @@
 import type { IncomingMessage, ServerResponse } from "node:http";
-import type { RemoteClawConfig } from "remoteclaw/plugin-sdk";
+import type { OpenClawConfig } from "remoteclaw/plugin-sdk/compat";
 import {
   GROUP_POLICY_BLOCKED_LABEL,
   createScopedPairingAccess,
@@ -14,12 +14,7 @@ import {
   resolveInboundRouteEnvelopeBuilderWithRuntime,
   resolveSingleWebhookTargetAsync,
   resolveWebhookPath,
-  resolveWebhookTargets,
-  warnMissingProviderGroupPolicyFallbackOnce,
-  requestBodyErrorToText,
-  resolveMentionGatingWithBypass,
-  resolveDmGroupAccessWithLists,
-} from "remoteclaw/plugin-sdk";
+} from "remoteclaw/plugin-sdk/compat";
 import { type ResolvedGoogleChatAccount } from "./accounts.js";
 import {
   downloadGoogleChatMedia,

extensions/googlechat/src/onboarding.ts
@@ -1,4 +1,4 @@
-import type { RemoteClawConfig, DmPolicy } from "remoteclaw/plugin-sdk";
+import type { OpenClawConfig, DmPolicy } from "remoteclaw/plugin-sdk/compat";
 import {
   addWildcardAllowFrom,
   formatDocsLink,
@@ -10,7 +10,7 @@ import {
   DEFAULT_ACCOUNT_ID,
   normalizeAccountId,
   migrateBaseNameToDefaultAccount,
-} from "remoteclaw/plugin-sdk";
+} from "remoteclaw/plugin-sdk/compat";
 import {
   listGoogleChatAccountIds,
   resolveDefaultGoogleChatAccountId,

extensions/googlechat/src/runtime.ts
@@ -1,5 +1,4 @@
-import { createPluginRuntimeStore } from "remoteclaw/plugin-sdk";
-import type { PluginRuntime } from "remoteclaw/plugin-sdk";
+import type { PluginRuntime } from "remoteclaw/plugin-sdk/compat";
 
 const { setRuntime: setGoogleChatRuntime, getRuntime: getGoogleChatRuntime } =
   createPluginRuntimeStore<PluginRuntime>("Google Chat runtime not initialized");

extensions/googlechat/src/types.config.ts
@@ -1,3 +1,3 @@
-import type { GoogleChatAccountConfig, GoogleChatConfig } from "remoteclaw/plugin-sdk";
+import type { GoogleChatAccountConfig, GoogleChatConfig } from "remoteclaw/plugin-sdk/compat";
 
 export type { GoogleChatAccountConfig, GoogleChatConfig };
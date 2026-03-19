wonglaitung/openclaw_mini

Extensions: migrate nextcloud-talk plugin-sdk imports

extensions/nextcloud-talk/src/accounts.ts
@@ -1,13 +1,13 @@
 import { readFileSync } from "node:fs";
 import {
-  listConfiguredAccountIds as listConfiguredAccountIdsFromSection,
-  resolveAccountWithDefaultFallback,
-} from "openclaw/plugin-sdk";
-import {
   DEFAULT_ACCOUNT_ID,
   normalizeAccountId,
   normalizeOptionalAccountId,
 } from "openclaw/plugin-sdk/account-id";
+import {
+  listConfiguredAccountIds as listConfiguredAccountIdsFromSection,
+  resolveAccountWithDefaultFallback,
+} from "openclaw/plugin-sdk/compat";
 import { normalizeResolvedSecretInputString } from "./secret-input.js";
 import type { CoreConfig, NextcloudTalkAccountConfig } from "./types.js";
 

extensions/nextcloud-talk/src/channel.ts
@@ -11,7 +11,7 @@ import {
   type ChannelPlugin,
   type OpenClawConfig,
   type ChannelSetupInput,
-} from "openclaw/plugin-sdk";
+} from "openclaw/plugin-sdk/compat";
 import { waitForAbortSignal } from "../../../src/infra/abort-signal.js";
 import {
   listNextcloudTalkAccountIds,

extensions/nextcloud-talk/src/inbound.authz.test.ts
@@ -1,4 +1,4 @@
-import type { PluginRuntime, RuntimeEnv } from "openclaw/plugin-sdk";
+import type { PluginRuntime, RuntimeEnv } from "openclaw/plugin-sdk/compat";
 import { describe, expect, it, vi } from "vitest";
 import type { ResolvedNextcloudTalkAccount } from "./accounts.js";
 import { handleNextcloudTalkInbound } from "./inbound.js";

extensions/nextcloud-talk/src/inbound.ts
@@ -14,7 +14,7 @@ import {
   type OutboundReplyPayload,
   type OpenClawConfig,
   type RuntimeEnv,
-} from "openclaw/plugin-sdk";
+} from "openclaw/plugin-sdk/compat";
 import type { ResolvedNextcloudTalkAccount } from "./accounts.js";
 import {
   normalizeNextcloudTalkAllowlist,

extensions/nextcloud-talk/src/onboarding.ts
@@ -12,7 +12,7 @@ import {
   type ChannelOnboardingDmPolicy,
   type OpenClawConfig,
   type WizardPrompter,
-} from "openclaw/plugin-sdk";
+} from "openclaw/plugin-sdk/compat";
 import {
   listNextcloudTalkAccountIds,
   resolveDefaultNextcloudTalkAccountId,

extensions/nextcloud-talk/src/replay-guard.ts
@@ -1,5 +1,5 @@
 import path from "node:path";
-import { createPersistentDedupe } from "openclaw/plugin-sdk";
+import { createPersistentDedupe } from "openclaw/plugin-sdk/compat";
 
 const DEFAULT_REPLAY_TTL_MS = 24 * 60 * 60 * 1000;
 const DEFAULT_MEMORY_MAX_SIZE = 1_000;

extensions/nextcloud-talk/src/room-info.ts
@@ -1,6 +1,6 @@
 import { readFileSync } from "node:fs";
-import { fetchWithSsrFGuard } from "openclaw/plugin-sdk";
-import type { RuntimeEnv } from "openclaw/plugin-sdk";
+import { fetchWithSsrFGuard } from "openclaw/plugin-sdk/compat";
+import type { RuntimeEnv } from "openclaw/plugin-sdk/compat";
 import type { ResolvedNextcloudTalkAccount } from "./accounts.js";
 import { normalizeResolvedSecretInputString } from "./secret-input.js";
 

extensions/nextcloud-talk/src/types.ts
@@ -4,7 +4,7 @@ import type {
   DmPolicy,
   GroupPolicy,
   SecretInput,
-} from "openclaw/plugin-sdk";
+} from "openclaw/plugin-sdk/compat";
 
 export type { DmPolicy, GroupPolicy };
 

remoteclaw/remoteclaw

(cherry picked from commit ed29472af6176f4ef3705c9e6e85c132e5a95e6a)

extensions/nextcloud-talk/src/accounts.ts
@@ -1,13 +1,14 @@
 import { readFileSync } from "node:fs";
 import {
-  listConfiguredAccountIds as listConfiguredAccountIdsFromSection,
-  resolveAccountWithDefaultFallback,
-} from "remoteclaw/plugin-sdk";
-import {
   DEFAULT_ACCOUNT_ID,
   normalizeAccountId,
   normalizeOptionalAccountId,
 } from "remoteclaw/plugin-sdk/account-id";
+import {
+  listConfiguredAccountIds as listConfiguredAccountIdsFromSection,
+  resolveAccountWithDefaultFallback,
+} from "remoteclaw/plugin-sdk/compat";
+import { normalizeResolvedSecretInputString } from "./secret-input.js";
 import type { CoreConfig, NextcloudTalkAccountConfig } from "./types.js";
 
 function isTruthyEnvValue(value?: string): boolean {

extensions/nextcloud-talk/src/channel.ts
@@ -11,7 +11,7 @@ import {
   type ChannelPlugin,
   type RemoteClawConfig,
   type ChannelSetupInput,
-} from "remoteclaw/plugin-sdk";
+} from "remoteclaw/plugin-sdk/compat";
 import { waitForAbortSignal } from "../../../src/infra/abort-signal.js";
 import {
   listNextcloudTalkAccountIds,

extensions/nextcloud-talk/src/inbound.authz.test.ts
@@ -1,4 +1,4 @@
-import type { PluginRuntime, RuntimeEnv } from "remoteclaw/plugin-sdk";
+import type { PluginRuntime, RuntimeEnv } from "remoteclaw/plugin-sdk/compat";
 import { describe, expect, it, vi } from "vitest";
 import type { ResolvedNextcloudTalkAccount } from "./accounts.js";
 import { handleNextcloudTalkInbound } from "./inbound.js";

extensions/nextcloud-talk/src/inbound.ts
@@ -14,7 +14,7 @@ import {
   type OutboundReplyPayload,
   type RemoteClawConfig,
   type RuntimeEnv,
-} from "remoteclaw/plugin-sdk";
+} from "remoteclaw/plugin-sdk/compat";
 import type { ResolvedNextcloudTalkAccount } from "./accounts.js";
 import {
   normalizeNextcloudTalkAllowlist,

extensions/nextcloud-talk/src/onboarding.ts
@@ -9,7 +9,7 @@ import {
   type ChannelOnboardingDmPolicy,
   type RemoteClawConfig,
   type WizardPrompter,
-} from "remoteclaw/plugin-sdk";
+} from "remoteclaw/plugin-sdk/compat";
 import {
   listNextcloudTalkAccountIds,
   resolveDefaultNextcloudTalkAccountId,

extensions/nextcloud-talk/src/replay-guard.ts
@@ -1,5 +1,5 @@
 import path from "node:path";
-import { createPersistentDedupe } from "remoteclaw/plugin-sdk";
+import { createPersistentDedupe } from "remoteclaw/plugin-sdk/compat";
 
 const DEFAULT_REPLAY_TTL_MS = 24 * 60 * 60 * 1000;
 const DEFAULT_MEMORY_MAX_SIZE = 1_000;

extensions/nextcloud-talk/src/room-info.ts
@@ -1,5 +1,6 @@
 import { readFileSync } from "node:fs";
-import type { RuntimeEnv } from "remoteclaw/plugin-sdk";
+import { fetchWithSsrFGuard } from "remoteclaw/plugin-sdk/compat";
+import type { RuntimeEnv } from "remoteclaw/plugin-sdk/compat";
 import type { ResolvedNextcloudTalkAccount } from "./accounts.js";
 
 const ROOM_CACHE_TTL_MS = 5 * 60 * 1000;

extensions/nextcloud-talk/src/types.ts
@@ -3,7 +3,8 @@ import type {
   DmConfig,
   DmPolicy,
   GroupPolicy,
-} from "remoteclaw/plugin-sdk";
+  SecretInput,
+} from "remoteclaw/plugin-sdk/compat";
 
 export type { DmPolicy, GroupPolicy };
 
wonglaitung/openclaw_mini

Plugins/voice-call: migrate to scoped plugin-sdk imports

extensions/voice-call/index.ts
@@ -1,5 +1,8 @@
 import { Type } from "@sinclair/typebox";
-import type { GatewayRequestHandlerOptions, OpenClawPluginApi } from "openclaw/plugin-sdk/core";
+import type {
+  GatewayRequestHandlerOptions,
+  OpenClawPluginApi,
+} from "openclaw/plugin-sdk/voice-call";
 import { registerVoiceCallCli } from "./src/cli.js";
 import {
   VoiceCallConfigSchema,

extensions/voice-call/src/cli.ts
@@ -2,7 +2,7 @@ import fs from "node:fs";
 import os from "node:os";
 import path from "node:path";
 import type { Command } from "commander";
-import { sleep } from "openclaw/plugin-sdk/compat";
+import { sleep } from "openclaw/plugin-sdk/voice-call";
 import type { VoiceCallConfig } from "./config.js";
 import type { VoiceCallRuntime } from "./runtime.js";
 import { resolveUserPath } from "./utils.js";

extensions/voice-call/src/config.ts
@@ -3,7 +3,7 @@ import {
   TtsConfigSchema,
   TtsModeSchema,
   TtsProviderSchema,
-} from "openclaw/plugin-sdk/compat";
+} from "openclaw/plugin-sdk/voice-call";
 import { z } from "zod";
 
 // -----------------------------------------------------------------------------

extensions/voice-call/src/providers/shared/guarded-json-api.ts
@@ -1,4 +1,4 @@
-import { fetchWithSsrFGuard } from "openclaw/plugin-sdk/compat";
+import { fetchWithSsrFGuard } from "openclaw/plugin-sdk/voice-call";
 
 type GuardedJsonApiRequestParams = {
   url: string;

extensions/voice-call/src/webhook.ts
@@ -4,7 +4,7 @@ import {
   isRequestBodyLimitError,
   readRequestBodyWithLimit,
   requestBodyErrorToText,
-} from "openclaw/plugin-sdk/compat";
+} from "openclaw/plugin-sdk/voice-call";
 import type { VoiceCallConfig } from "./config.js";
 import type { CoreConfig } from "./core-bridge.js";
 import type { CallManager } from "./manager.js";

remoteclaw/remoteclaw

(cherry picked from commit bbf29201b8fefc507c7339421810fa61559743a6)

extensions/voice-call/index.ts
@@ -1,5 +1,8 @@
 import { Type } from "@sinclair/typebox";
-import type { GatewayRequestHandlerOptions, RemoteClawPluginApi } from "remoteclaw/plugin-sdk/core";
+import type {
+  GatewayRequestHandlerOptions,
+  OpenClawPluginApi,
+} from "remoteclaw/plugin-sdk/voice-call";
 import { registerVoiceCallCli } from "./src/cli.js";
 import {
   VoiceCallConfigSchema,

extensions/voice-call/src/cli.ts
@@ -2,7 +2,7 @@ import fs from "node:fs";
 import os from "node:os";
 import path from "node:path";
 import type { Command } from "commander";
-import { sleep } from "remoteclaw/plugin-sdk/compat";
+import { sleep } from "remoteclaw/plugin-sdk/voice-call";
 import type { VoiceCallConfig } from "./config.js";
 import type { VoiceCallRuntime } from "./runtime.js";
 import { resolveUserPath } from "./utils.js";

extensions/voice-call/src/config.ts
@@ -3,7 +3,7 @@ import {
   TtsConfigSchema,
   TtsModeSchema,
   TtsProviderSchema,
-} from "remoteclaw/plugin-sdk/compat";
+} from "remoteclaw/plugin-sdk/voice-call";
 import { z } from "zod";
 
 // -----------------------------------------------------------------------------

extensions/voice-call/src/providers/shared/guarded-json-api.ts
@@ -1,4 +1,4 @@
-import { fetchWithSsrFGuard } from "remoteclaw/plugin-sdk/compat";
+import { fetchWithSsrFGuard } from "remoteclaw/plugin-sdk/voice-call";
 
 type GuardedJsonApiRequestParams = {
   url: string;

extensions/voice-call/src/webhook.ts
@@ -4,7 +4,7 @@ import {
   isRequestBodyLimitError,
   readRequestBodyWithLimit,
   requestBodyErrorToText,
-} from "remoteclaw/plugin-sdk/compat";
+} from "remoteclaw/plugin-sdk/voice-call";
 import type { VoiceCallConfig } from "./config.js";
 import type { CoreConfig } from "./core-bridge.js";
 import type { CallManager } from "./manager.js";
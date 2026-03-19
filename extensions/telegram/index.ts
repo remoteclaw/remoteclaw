wonglaitung/openclaw_mini

Extensions: migrate telegram plugin-sdk imports

extensions/telegram/index.ts
@@ -1,5 +1,5 @@
-import type { ChannelPlugin, OpenClawPluginApi } from "openclaw/plugin-sdk/core";
-import { emptyPluginConfigSchema } from "openclaw/plugin-sdk/core";
+import type { ChannelPlugin, OpenClawPluginApi } from "openclaw/plugin-sdk/telegram";
+import { emptyPluginConfigSchema } from "openclaw/plugin-sdk/telegram";
 import { telegramPlugin } from "./src/channel.js";
 import { setTelegramRuntime } from "./src/runtime.js";
 

extensions/telegram/src/channel.test.ts
@@ -4,7 +4,7 @@ import type {
   OpenClawConfig,
   PluginRuntime,
   ResolvedTelegramAccount,
-} from "openclaw/plugin-sdk";
+} from "openclaw/plugin-sdk/telegram";
 import { describe, expect, it, vi } from "vitest";
 import { createRuntimeEnv } from "../../test-utils/runtime-env.js";
 import { telegramPlugin } from "./channel.js";

extensions/telegram/src/runtime.ts
@@ -1,4 +1,4 @@
-import type { PluginRuntime } from "openclaw/plugin-sdk/core";
+import type { PluginRuntime } from "openclaw/plugin-sdk/telegram";
 
 let runtime: PluginRuntime | null = null;
 

remoteclaw/remoteclaw

(cherry picked from commit 7a9754c9271dcb80da8ef39a2c1d85110dfea8ea)

extensions/telegram/index.ts
@@ -1,5 +1,5 @@
-import type { ChannelPlugin, RemoteClawPluginApi } from "remoteclaw/plugin-sdk/core";
-import { emptyPluginConfigSchema } from "remoteclaw/plugin-sdk/core";
+import type { ChannelPlugin, OpenClawPluginApi } from "remoteclaw/plugin-sdk/telegram";
+import { emptyPluginConfigSchema } from "remoteclaw/plugin-sdk/telegram";
 import { telegramPlugin } from "./src/channel.js";
 import { setTelegramRuntime } from "./src/runtime.js";
 

extensions/telegram/src/channel.test.ts
@@ -4,7 +4,7 @@ import type {
   RemoteClawConfig,
   PluginRuntime,
   ResolvedTelegramAccount,
-} from "remoteclaw/plugin-sdk";
+} from "remoteclaw/plugin-sdk/telegram";
 import { describe, expect, it, vi } from "vitest";
 import { createRuntimeEnv } from "../../test-utils/runtime-env.js";
 import { telegramPlugin } from "./channel.js";

extensions/telegram/src/runtime.ts
@@ -1,5 +1,4 @@
-import { createPluginRuntimeStore } from "remoteclaw/plugin-sdk/core";
-import type { PluginRuntime } from "remoteclaw/plugin-sdk/core";
+import type { PluginRuntime } from "remoteclaw/plugin-sdk/telegram";
 
 const { setRuntime: setTelegramRuntime, getRuntime: getTelegramRuntime } =
   createPluginRuntimeStore<PluginRuntime>("Telegram runtime not initialized");
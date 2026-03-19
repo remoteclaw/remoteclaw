wonglaitung/openclaw_mini

Extensions: migrate zalouser plugin-sdk imports

extensions/zalouser/src/accounts.test.ts
@@ -1,5 +1,5 @@
-import type { OpenClawConfig } from "openclaw/plugin-sdk";
 import { DEFAULT_ACCOUNT_ID } from "openclaw/plugin-sdk/account-id";
+import type { OpenClawConfig } from "openclaw/plugin-sdk/compat";
 import { beforeEach, describe, expect, it, vi } from "vitest";
 import {
   getZcaUserInfo,

extensions/zalouser/src/accounts.ts
@@ -1,9 +1,9 @@
-import type { OpenClawConfig } from "openclaw/plugin-sdk";
 import {
   DEFAULT_ACCOUNT_ID,
   normalizeAccountId,
   normalizeOptionalAccountId,
 } from "openclaw/plugin-sdk/account-id";
+import type { OpenClawConfig } from "openclaw/plugin-sdk/compat";
 import type { ResolvedZalouserAccount, ZalouserAccountConfig, ZalouserConfig } from "./types.js";
 import { checkZaloAuthenticated, getZaloUserInfo } from "./zalo-js.js";
 

extensions/zalouser/src/config-schema.ts
@@ -1,4 +1,4 @@
-import { MarkdownConfigSchema, ToolPolicySchema } from "openclaw/plugin-sdk";
+import { MarkdownConfigSchema, ToolPolicySchema } from "openclaw/plugin-sdk/compat";
 import { z } from "zod";
 
 const allowFromEntry = z.union([z.string(), z.number()]);

extensions/zalouser/src/monitor.group-gating.test.ts
@@ -1,4 +1,4 @@
-import type { OpenClawConfig, PluginRuntime, RuntimeEnv } from "openclaw/plugin-sdk";
+import type { OpenClawConfig, PluginRuntime, RuntimeEnv } from "openclaw/plugin-sdk/compat";
 import { beforeEach, describe, expect, it, vi } from "vitest";
 import { __testing } from "./monitor.js";
 import { setZalouserRuntime } from "./runtime.js";

extensions/zalouser/src/monitor.ts
@@ -3,7 +3,7 @@ import type {
   OpenClawConfig,
   OutboundReplyPayload,
   RuntimeEnv,
-} from "openclaw/plugin-sdk";
+} from "openclaw/plugin-sdk/compat";
 import {
   createTypingCallbacks,
   createScopedPairingAccess,
@@ -17,7 +17,7 @@ import {
   sendMediaWithLeadingCaption,
   summarizeMapping,
   warnMissingProviderGroupPolicyFallbackOnce,
-} from "openclaw/plugin-sdk";
+} from "openclaw/plugin-sdk/compat";
 import {
   buildZalouserGroupCandidates,
   findZalouserGroupEntry,

extensions/zalouser/src/onboarding.ts
@@ -5,7 +5,7 @@ import type {
   ChannelOnboardingDmPolicy,
   OpenClawConfig,
   WizardPrompter,
-} from "openclaw/plugin-sdk";
+} from "openclaw/plugin-sdk/compat";
 import {
   addWildcardAllowFrom,
   DEFAULT_ACCOUNT_ID,
@@ -15,7 +15,7 @@ import {
   promptAccountId,
   promptChannelAccessConfig,
   resolvePreferredOpenClawTmpDir,
-} from "openclaw/plugin-sdk";
+} from "openclaw/plugin-sdk/compat";
 import {
   listZalouserAccountIds,
   resolveDefaultZalouserAccountId,

extensions/zalouser/src/status-issues.ts
@@ -1,4 +1,4 @@
-import type { ChannelAccountSnapshot, ChannelStatusIssue } from "openclaw/plugin-sdk";
+import type { ChannelAccountSnapshot, ChannelStatusIssue } from "openclaw/plugin-sdk/compat";
 
 type ZalouserAccountStatus = {
   accountId?: unknown;

extensions/zalouser/src/zalo-js.ts
@@ -3,7 +3,7 @@ import fs from "node:fs";
 import fsp from "node:fs/promises";
 import os from "node:os";
 import path from "node:path";
-import { loadOutboundMediaFromUrl } from "openclaw/plugin-sdk";
+import { loadOutboundMediaFromUrl } from "openclaw/plugin-sdk/compat";
 import { normalizeZaloReactionIcon } from "./reaction.js";
 import { getZalouserRuntime } from "./runtime.js";
 import type {

remoteclaw/remoteclaw

(cherry picked from commit 37a8caee42bdb94292f6e00480d896e96764e407)

extensions/zalouser/src/accounts.test.ts
@@ -1,5 +1,5 @@
-import type { RemoteClawConfig } from "remoteclaw/plugin-sdk";
 import { DEFAULT_ACCOUNT_ID } from "remoteclaw/plugin-sdk/account-id";
+import type { OpenClawConfig } from "remoteclaw/plugin-sdk/compat";
 import { beforeEach, describe, expect, it, vi } from "vitest";
 import {
   getZcaUserInfo,

extensions/zalouser/src/accounts.ts
@@ -1,9 +1,9 @@
-import type { RemoteClawConfig } from "remoteclaw/plugin-sdk";
 import {
   DEFAULT_ACCOUNT_ID,
   normalizeAccountId,
   normalizeOptionalAccountId,
 } from "remoteclaw/plugin-sdk/account-id";
+import type { OpenClawConfig } from "remoteclaw/plugin-sdk/compat";
 import type { ResolvedZalouserAccount, ZalouserAccountConfig, ZalouserConfig } from "./types.js";
 import { checkZaloAuthenticated, getZaloUserInfo } from "./zalo-js.js";
 

extensions/zalouser/src/config-schema.ts
@@ -1,9 +1,4 @@
-import {
-  AllowFromEntrySchema,
-  buildCatchallMultiAccountChannelSchema,
-  MarkdownConfigSchema,
-  ToolPolicySchema,
-} from "remoteclaw/plugin-sdk";
+import { MarkdownConfigSchema, ToolPolicySchema } from "remoteclaw/plugin-sdk/compat";
 import { z } from "zod";
 
 const groupConfigSchema = z.object({

extensions/zalouser/src/monitor.group-gating.test.ts
@@ -1,4 +1,4 @@
-import type { RemoteClawConfig, PluginRuntime, RuntimeEnv } from "remoteclaw/plugin-sdk";
+import type { OpenClawConfig, PluginRuntime, RuntimeEnv } from "remoteclaw/plugin-sdk/compat";
 import { beforeEach, describe, expect, it, vi } from "vitest";
 import { __testing } from "./monitor.js";
 import { setZalouserRuntime } from "./runtime.js";

extensions/zalouser/src/monitor.ts
@@ -3,7 +3,7 @@ import type {
   RemoteClawConfig,
   OutboundReplyPayload,
   RuntimeEnv,
-} from "remoteclaw/plugin-sdk";
+} from "remoteclaw/plugin-sdk/compat";
 import {
   createTypingCallbacks,
   createScopedPairingAccess,
@@ -19,7 +19,7 @@ import {
   sendMediaWithLeadingCaption,
   summarizeMapping,
   warnMissingProviderGroupPolicyFallbackOnce,
-} from "remoteclaw/plugin-sdk";
+} from "remoteclaw/plugin-sdk/compat";
 import {
   buildZalouserGroupCandidates,
   findZalouserGroupEntry,

extensions/zalouser/src/onboarding.ts
@@ -5,7 +5,7 @@ import type {
   ChannelOnboardingDmPolicy,
   RemoteClawConfig,
   WizardPrompter,
-} from "remoteclaw/plugin-sdk";
+} from "remoteclaw/plugin-sdk/compat";
 import {
   addWildcardAllowFrom,
   DEFAULT_ACCOUNT_ID,
@@ -14,8 +14,8 @@ import {
   normalizeAccountId,
   promptAccountId,
   promptChannelAccessConfig,
-  resolvePreferredRemoteClawTmpDir,
-} from "remoteclaw/plugin-sdk";
+  resolvePreferredOpenClawTmpDir,
+} from "remoteclaw/plugin-sdk/compat";
 import {
   listZalouserAccountIds,
   resolveDefaultZalouserAccountId,

extensions/zalouser/src/status-issues.ts
@@ -1,4 +1,4 @@
-import type { ChannelAccountSnapshot, ChannelStatusIssue } from "remoteclaw/plugin-sdk";
+import type { ChannelAccountSnapshot, ChannelStatusIssue } from "remoteclaw/plugin-sdk/compat";
 
 type ZalouserAccountStatus = {
   accountId?: unknown;

extensions/zalouser/src/zalo-js.ts
@@ -3,7 +3,7 @@ import fs from "node:fs";
 import fsp from "node:fs/promises";
 import os from "node:os";
 import path from "node:path";
-import { loadOutboundMediaFromUrl } from "remoteclaw/plugin-sdk";
+import { loadOutboundMediaFromUrl } from "remoteclaw/plugin-sdk/compat";
 import { normalizeZaloReactionIcon } from "./reaction.js";
 import { getZalouserRuntime } from "./runtime.js";
 import type {
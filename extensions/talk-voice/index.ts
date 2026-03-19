wonglaitung/openclaw_mini

Plugins/talk-voice: migrate to scoped plugin-sdk imports

extensions/talk-voice/index.ts
@@ -1,4 +1,4 @@
-import type { OpenClawPluginApi } from "openclaw/plugin-sdk/core";
+import type { OpenClawPluginApi } from "openclaw/plugin-sdk/talk-voice";
 
 type ElevenLabsVoice = {
   voice_id: string;

remoteclaw/remoteclaw

(cherry picked from commit f006c5f5c1125b75b5f193906957e3a792a0ef5b)

extensions/talk-voice/index.ts
@@ -1,4 +1,4 @@
-import type { RemoteClawPluginApi } from "remoteclaw/plugin-sdk/core";
+import type { OpenClawPluginApi } from "remoteclaw/plugin-sdk/talk-voice";
 
 type ElevenLabsVoice = {
   voice_id: string;
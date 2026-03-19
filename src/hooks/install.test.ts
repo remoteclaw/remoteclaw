wonglaitung/openclaw_mini

refactor(hooks): dedupe install parameter wiring

src/hooks/install.test.ts
@@ -87,6 +87,43 @@ function expectInstallFailureContains(
   }
 }
 
+function writeHookPackManifest(params: {
+  pkgDir: string;
+  hooks: string[];
+  dependencies?: Record<string, string>;
+}) {
+  fs.writeFileSync(
+    path.join(params.pkgDir, "package.json"),
+    JSON.stringify({
+      name: "@openclaw/test-hooks",
+      version: "0.0.1",
+      openclaw: { hooks: params.hooks },
+      ...(params.dependencies ? { dependencies: params.dependencies } : {}),
+    }),
+    "utf-8",
+  );
+}
+
+async function installArchiveFixture(params: { fileName: string; contents: Buffer }) {
+  const fixture = writeArchiveFixture(params);
+  const result = await installHooksFromArchive({
+    archivePath: fixture.archivePath,
+    hooksDir: fixture.hooksDir,
+  });
+  return { fixture, result };
+}
+
+function expectPathInstallFailureContains(
+  result: Awaited<ReturnType<typeof installHooksFromPath>>,
+  snippet: string,
+) {
+  expect(result.ok).toBe(false);
+  if (result.ok) {
+    throw new Error("expected install failure");
+  }
+  expect(result.error).toContain(snippet);
+}
+
 describe("installHooksFromArchive", () => {
   it.each([
     {
@@ -104,10 +141,9 @@ describe("installHooksFromArchive", () => {
       expectedHook: "tar-hook",
     },
   ])("installs hook packs from $name archives", async (tc) => {
-    const fixture = writeArchiveFixture({ fileName: tc.fileName, contents: tc.contents });
-    const result = await installHooksFromArchive({
-      archivePath: fixture.archivePath,
-      hooksDir: fixture.hooksDir,
+    const { fixture, result } = await installArchiveFixture({
+      fileName: tc.fileName,
+      contents: tc.contents,
     });
 
     expect(result.ok).toBe(true);
@@ -136,10 +172,9 @@ describe("installHooksFromArchive", () => {
       expectedDetail: "escapes destination",
     },
   ])("rejects $name archives with traversal entries", async (tc) => {
-    const fixture = writeArchiveFixture({ fileName: tc.fileName, contents: tc.contents });
-    const result = await installHooksFromArchive({
-      archivePath: fixture.archivePath,
-      hooksDir: fixture.hooksDir,
+    const { result } = await installArchiveFixture({
+      fileName: tc.fileName,
+      contents: tc.contents,
     });
     expectInstallFailureContains(result, ["failed to extract archive", tc.expectedDetail]);
   });
@@ -154,10 +189,9 @@ describe("installHooksFromArchive", () => {
       contents: tarReservedIdBuffer,
     },
   ])("rejects hook packs with $name", async (tc) => {
-    const fixture = writeArchiveFixture({ fileName: "hooks.tar", contents: tc.contents });
-    const result = await installHooksFromArchive({
-      archivePath: fixture.archivePath,
-      hooksDir: fixture.hooksDir,
+    const { result } = await installArchiveFixture({
+      fileName: "hooks.tar",
+      contents: tc.contents,
     });
     expectInstallFailureContains(result, ["reserved path segment"]);
   });
@@ -169,16 +203,11 @@ describe("installHooksFromPath", () => {
     const stateDir = makeTempDir();
     const pkgDir = path.join(workDir, "package");
     fs.mkdirSync(path.join(pkgDir, "hooks", "one-hook"), { recursive: true });
-    fs.writeFileSync(
-      path.join(pkgDir, "package.json"),
-      JSON.stringify({
-        name: "@openclaw/test-hooks",
-        version: "0.0.1",
-        openclaw: { hooks: ["./hooks/one-hook"] },
-        dependencies: { "left-pad": "1.3.0" },
-      }),
-      "utf-8",
-    );
+    writeHookPackManifest({
+      pkgDir,
+      hooks: ["./hooks/one-hook"],
+      dependencies: { "left-pad": "1.3.0" },
+    });
     fs.writeFileSync(
       path.join(pkgDir, "hooks", "one-hook", "HOOK.md"),
       [
@@ -249,15 +278,10 @@ describe("installHooksFromPath", () => {
     const outsideHookDir = path.join(workDir, "outside");
     fs.mkdirSync(pkgDir, { recursive: true });
     fs.mkdirSync(outsideHookDir, { recursive: true });
-    fs.writeFileSync(
-      path.join(pkgDir, "package.json"),
-      JSON.stringify({
-        name: "@openclaw/test-hooks",
-        version: "0.0.1",
-        openclaw: { hooks: ["../outside"] },
-      }),
-      "utf-8",
-    );
+    writeHookPackManifest({
+      pkgDir,
+      hooks: ["../outside"],
+    });
     fs.writeFileSync(path.join(outsideHookDir, "HOOK.md"), "---\nname: outside\n---\n", "utf-8");
     fs.writeFileSync(path.join(outsideHookDir, "handler.ts"), "export default async () => {};\n");
 
@@ -266,11 +290,7 @@ describe("installHooksFromPath", () => {
       hooksDir: path.join(stateDir, "hooks"),
     });
 
-    expect(result.ok).toBe(false);
-    if (result.ok) {
-      return;
-    }
-    expect(result.error).toContain("openclaw.hooks entry escapes package directory");
+    expectPathInstallFailureContains(result, "openclaw.hooks entry escapes package directory");
   });
 
   it("rejects hook pack entries that escape via symlink", async () => {
@@ -288,26 +308,20 @@ describe("installHooksFromPath", () => {
     } catch {
       return;
     }
-    fs.writeFileSync(
-      path.join(pkgDir, "package.json"),
-      JSON.stringify({
-        name: "@openclaw/test-hooks",
-        version: "0.0.1",
-        openclaw: { hooks: ["./linked"] },
-      }),
-      "utf-8",
-    );
+    writeHookPackManifest({
+      pkgDir,
+      hooks: ["./linked"],
+    });
 
     const result = await installHooksFromPath({
       path: pkgDir,
       hooksDir: path.join(stateDir, "hooks"),
     });
 
-    expect(result.ok).toBe(false);
-    if (result.ok) {
-      return;
-    }
-    expect(result.error).toContain("openclaw.hooks entry resolves outside package directory");
+    expectPathInstallFailureContains(
+      result,
+      "openclaw.hooks entry resolves outside package directory",
+    );
   });
 });
 

remoteclaw/remoteclaw

(cherry picked from commit c3d51591219665b7d974a571e3c6e823b9ff34ac)

src/hooks/install.test.ts
@@ -87,6 +87,43 @@ function expectInstallFailureContains(
   }
 }
 
+function writeHookPackManifest(params: {
+  pkgDir: string;
+  hooks: string[];
+  dependencies?: Record<string, string>;
+}) {
+  fs.writeFileSync(
+    path.join(params.pkgDir, "package.json"),
+    JSON.stringify({
+      name: "@remoteclaw/test-hooks",
+      version: "0.0.1",
+      remoteclaw: { hooks: params.hooks },
+      ...(params.dependencies ? { dependencies: params.dependencies } : {}),
+    }),
+    "utf-8",
+  );
+}
+
+async function installArchiveFixture(params: { fileName: string; contents: Buffer }) {
+  const fixture = writeArchiveFixture(params);
+  const result = await installHooksFromArchive({
+    archivePath: fixture.archivePath,
+    hooksDir: fixture.hooksDir,
+  });
+  return { fixture, result };
+}
+
+function expectPathInstallFailureContains(
+  result: Awaited<ReturnType<typeof installHooksFromPath>>,
+  snippet: string,
+) {
+  expect(result.ok).toBe(false);
+  if (result.ok) {
+    throw new Error("expected install failure");
+  }
+  expect(result.error).toContain(snippet);
+}
+
 describe("installHooksFromArchive", () => {
   it.each([
     {
@@ -104,10 +141,9 @@ describe("installHooksFromArchive", () => {
       expectedHook: "tar-hook",
     },
   ])("installs hook packs from $name archives", async (tc) => {
-    const fixture = writeArchiveFixture({ fileName: tc.fileName, contents: tc.contents });
-    const result = await installHooksFromArchive({
-      archivePath: fixture.archivePath,
-      hooksDir: fixture.hooksDir,
+    const { fixture, result } = await installArchiveFixture({
+      fileName: tc.fileName,
+      contents: tc.contents,
     });
 
     expect(result.ok).toBe(true);
@@ -136,10 +172,9 @@ describe("installHooksFromArchive", () => {
       expectedDetail: "escapes destination",
     },
   ])("rejects $name archives with traversal entries", async (tc) => {
-    const fixture = writeArchiveFixture({ fileName: tc.fileName, contents: tc.contents });
-    const result = await installHooksFromArchive({
-      archivePath: fixture.archivePath,
-      hooksDir: fixture.hooksDir,
+    const { result } = await installArchiveFixture({
+      fileName: tc.fileName,
+      contents: tc.contents,
     });
     expectInstallFailureContains(result, ["failed to extract archive", tc.expectedDetail]);
   });
@@ -154,10 +189,9 @@ describe("installHooksFromArchive", () => {
       contents: tarReservedIdBuffer,
     },
   ])("rejects hook packs with $name", async (tc) => {
-    const fixture = writeArchiveFixture({ fileName: "hooks.tar", contents: tc.contents });
-    const result = await installHooksFromArchive({
-      archivePath: fixture.archivePath,
-      hooksDir: fixture.hooksDir,
+    const { result } = await installArchiveFixture({
+      fileName: "hooks.tar",
+      contents: tc.contents,
     });
     expectInstallFailureContains(result, ["reserved path segment"]);
   });
@@ -169,16 +203,11 @@ describe("installHooksFromPath", () => {
     const stateDir = makeTempDir();
     const pkgDir = path.join(workDir, "package");
     fs.mkdirSync(path.join(pkgDir, "hooks", "one-hook"), { recursive: true });
-    fs.writeFileSync(
-      path.join(pkgDir, "package.json"),
-      JSON.stringify({
-        name: "@remoteclaw/test-hooks",
-        version: "0.0.1",
-        remoteclaw: { hooks: ["./hooks/one-hook"] },
-        dependencies: { "left-pad": "1.3.0" },
-      }),
-      "utf-8",
-    );
+    writeHookPackManifest({
+      pkgDir,
+      hooks: ["./hooks/one-hook"],
+      dependencies: { "left-pad": "1.3.0" },
+    });
     fs.writeFileSync(
       path.join(pkgDir, "hooks", "one-hook", "HOOK.md"),
       [
@@ -249,15 +278,10 @@ describe("installHooksFromPath", () => {
     const outsideHookDir = path.join(workDir, "outside");
     fs.mkdirSync(pkgDir, { recursive: true });
     fs.mkdirSync(outsideHookDir, { recursive: true });
-    fs.writeFileSync(
-      path.join(pkgDir, "package.json"),
-      JSON.stringify({
-        name: "@remoteclaw/test-hooks",
-        version: "0.0.1",
-        remoteclaw: { hooks: ["../outside"] },
-      }),
-      "utf-8",
-    );
+    writeHookPackManifest({
+      pkgDir,
+      hooks: ["../outside"],
+    });
     fs.writeFileSync(path.join(outsideHookDir, "HOOK.md"), "---\nname: outside\n---\n", "utf-8");
     fs.writeFileSync(path.join(outsideHookDir, "handler.ts"), "export default async () => {};\n");
 
@@ -266,11 +290,7 @@ describe("installHooksFromPath", () => {
       hooksDir: path.join(stateDir, "hooks"),
     });
 
-    expect(result.ok).toBe(false);
-    if (result.ok) {
-      return;
-    }
-    expect(result.error).toContain("remoteclaw.hooks entry escapes package directory");
+    expectPathInstallFailureContains(result, "remoteclaw.hooks entry escapes package directory");
   });
 
   it("rejects hook pack entries that escape via symlink", async () => {
@@ -288,26 +308,20 @@ describe("installHooksFromPath", () => {
     } catch {
       return;
     }
-    fs.writeFileSync(
-      path.join(pkgDir, "package.json"),
-      JSON.stringify({
-        name: "@remoteclaw/test-hooks",
-        version: "0.0.1",
-        remoteclaw: { hooks: ["./linked"] },
-      }),
-      "utf-8",
-    );
+    writeHookPackManifest({
+      pkgDir,
+      hooks: ["./linked"],
+    });
 
     const result = await installHooksFromPath({
       path: pkgDir,
       hooksDir: path.join(stateDir, "hooks"),
     });
 
-    expect(result.ok).toBe(false);
-    if (result.ok) {
-      return;
-    }
-    expect(result.error).toContain("remoteclaw.hooks entry resolves outside package directory");
+    expectPathInstallFailureContains(
+      result,
+      "remoteclaw.hooks entry resolves outside package directory",
+    );
   });
 });
 
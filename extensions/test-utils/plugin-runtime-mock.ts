wonglaitung/openclaw_mini

Extensions: migrate test-utils plugin-sdk imports

extensions/test-utils/plugin-runtime-mock.ts
@@ -1,5 +1,5 @@
-import type { PluginRuntime } from "openclaw/plugin-sdk";
-import { removeAckReactionAfterReply, shouldAckReaction } from "openclaw/plugin-sdk";
+import type { PluginRuntime } from "openclaw/plugin-sdk/compat";
+import { removeAckReactionAfterReply, shouldAckReaction } from "openclaw/plugin-sdk/compat";
 import { vi } from "vitest";
 
 type DeepPartial<T> = {

extensions/test-utils/runtime-env.ts
@@ -1,4 +1,4 @@
-import type { RuntimeEnv } from "openclaw/plugin-sdk";
+import type { RuntimeEnv } from "openclaw/plugin-sdk/compat";
 import { vi } from "vitest";
 
 export function createRuntimeEnv(): RuntimeEnv {

extensions/test-utils/start-account-context.ts
@@ -2,7 +2,7 @@ import type {
   ChannelAccountSnapshot,
   ChannelGatewayContext,
   OpenClawConfig,
-} from "openclaw/plugin-sdk";
+} from "openclaw/plugin-sdk/compat";
 import { vi } from "vitest";
 import { createRuntimeEnv } from "./runtime-env.js";
 

remoteclaw/remoteclaw

(cherry picked from commit 9bf08c926b70fe2e51925025ba98d17d74638f9b)

extensions/test-utils/plugin-runtime-mock.ts
@@ -1,5 +1,5 @@
-import type { PluginRuntime } from "remoteclaw/plugin-sdk";
-import { removeAckReactionAfterReply, shouldAckReaction } from "remoteclaw/plugin-sdk";
+import type { PluginRuntime } from "remoteclaw/plugin-sdk/compat";
+import { removeAckReactionAfterReply, shouldAckReaction } from "remoteclaw/plugin-sdk/compat";
 import { vi } from "vitest";
 
 type DeepPartial<T> = {

extensions/test-utils/runtime-env.ts
@@ -1,4 +1,4 @@
-import type { RuntimeEnv } from "remoteclaw/plugin-sdk";
+import type { RuntimeEnv } from "remoteclaw/plugin-sdk/compat";
 import { vi } from "vitest";
 
 export function createRuntimeEnv(): RuntimeEnv {

extensions/test-utils/start-account-context.ts
@@ -1,8 +1,8 @@
 import type {
   ChannelAccountSnapshot,
   ChannelGatewayContext,
-  RemoteClawConfig,
-} from "remoteclaw/plugin-sdk";
+  OpenClawConfig,
+} from "remoteclaw/plugin-sdk/compat";
 import { vi } from "vitest";
 import { createRuntimeEnv } from "./runtime-env.js";
 
wonglaitung/openclaw_mini

Plugins/thread-ownership: migrate to scoped plugin-sdk imports

extensions/thread-ownership/index.ts
@@ -1,4 +1,4 @@
-import type { OpenClawConfig, OpenClawPluginApi } from "openclaw/plugin-sdk/core";
+import type { OpenClawConfig, OpenClawPluginApi } from "openclaw/plugin-sdk/thread-ownership";
 
 type ThreadOwnershipConfig = {
   forwarderUrl?: string;

remoteclaw/remoteclaw

(cherry picked from commit 7c96d821129ad46e0a4f61e6e2d97923d95b9693)

extensions/thread-ownership/index.ts
@@ -1,4 +1,4 @@
-import type { RemoteClawConfig, RemoteClawPluginApi } from "remoteclaw/plugin-sdk/core";
+import type { OpenClawConfig, OpenClawPluginApi } from "remoteclaw/plugin-sdk/thread-ownership";
 
 type ThreadOwnershipConfig = {
   forwarderUrl?: string;
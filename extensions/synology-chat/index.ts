wonglaitung/openclaw_mini

Plugins/synology-chat: migrate to scoped plugin-sdk imports

extensions/synology-chat/index.ts
@@ -1,5 +1,5 @@
-import type { OpenClawPluginApi } from "openclaw/plugin-sdk/core";
-import { emptyPluginConfigSchema } from "openclaw/plugin-sdk/core";
+import type { OpenClawPluginApi } from "openclaw/plugin-sdk/synology-chat";
+import { emptyPluginConfigSchema } from "openclaw/plugin-sdk/synology-chat";
 import { createSynologyChatPlugin } from "./src/channel.js";
 import { setSynologyRuntime } from "./src/runtime.js";
 

extensions/synology-chat/src/channel.integration.test.ts
@@ -11,8 +11,8 @@ type RegisteredRoute = {
 const registerPluginHttpRouteMock = vi.fn<(params: RegisteredRoute) => () => void>(() => vi.fn());
 const dispatchReplyWithBufferedBlockDispatcher = vi.fn().mockResolvedValue({ counts: {} });
 
-vi.mock("openclaw/plugin-sdk/compat", async (importOriginal) => {
-  const actual = await importOriginal<typeof import("openclaw/plugin-sdk/compat")>();
+vi.mock("openclaw/plugin-sdk/synology-chat", async (importOriginal) => {
+  const actual = await importOriginal<typeof import("openclaw/plugin-sdk/synology-chat")>();
   return {
     ...actual,
     DEFAULT_ACCOUNT_ID: "default",

extensions/synology-chat/src/channel.test.ts
@@ -1,7 +1,7 @@
 import { describe, it, expect, vi, beforeEach } from "vitest";
 
 // Mock external dependencies
-vi.mock("openclaw/plugin-sdk/compat", () => ({
+vi.mock("openclaw/plugin-sdk/synology-chat", () => ({
   DEFAULT_ACCOUNT_ID: "default",
   setAccountEnabledInConfigSection: vi.fn((_opts: any) => ({})),
   registerPluginHttpRoute: vi.fn(() => vi.fn()),
@@ -44,7 +44,7 @@ vi.mock("zod", () => ({
 }));
 
 const { createSynologyChatPlugin } = await import("./channel.js");
-const { registerPluginHttpRoute } = await import("openclaw/plugin-sdk/compat");
+const { registerPluginHttpRoute } = await import("openclaw/plugin-sdk/synology-chat");
 
 describe("createSynologyChatPlugin", () => {
   it("returns a plugin object with all required sections", () => {

extensions/synology-chat/src/channel.ts
@@ -9,7 +9,7 @@ import {
   setAccountEnabledInConfigSection,
   registerPluginHttpRoute,
   buildChannelConfigSchema,
-} from "openclaw/plugin-sdk/compat";
+} from "openclaw/plugin-sdk/synology-chat";
 import { z } from "zod";
 import { listAccountIds, resolveAccount } from "./accounts.js";
 import { sendMessage, sendFileUrl } from "./client.js";

extensions/synology-chat/src/security.ts
@@ -6,7 +6,7 @@ import * as crypto from "node:crypto";
 import {
   createFixedWindowRateLimiter,
   type FixedWindowRateLimiter,
-} from "openclaw/plugin-sdk/compat";
+} from "openclaw/plugin-sdk/synology-chat";
 
 export type DmAuthorizationResult =
   | { allowed: true }

extensions/synology-chat/src/webhook-handler.ts
@@ -9,7 +9,7 @@ import {
   isRequestBodyLimitError,
   readRequestBodyWithLimit,
   requestBodyErrorToText,
-} from "openclaw/plugin-sdk/compat";
+} from "openclaw/plugin-sdk/synology-chat";
 import { sendMessage, resolveChatUserId } from "./client.js";
 import { validateToken, authorizeUserForDm, sanitizeInput, RateLimiter } from "./security.js";
 import type { SynologyWebhookPayload, ResolvedSynologyChatAccount } from "./types.js";

remoteclaw/remoteclaw

(cherry picked from commit 65ffa676a51f58901eae0c64403ac8dc4e2b3734)

extensions/synology-chat/index.ts
@@ -1,5 +1,5 @@
-import type { RemoteClawPluginApi } from "remoteclaw/plugin-sdk/core";
-import { emptyPluginConfigSchema } from "remoteclaw/plugin-sdk/core";
+import type { OpenClawPluginApi } from "remoteclaw/plugin-sdk/synology-chat";
+import { emptyPluginConfigSchema } from "remoteclaw/plugin-sdk/synology-chat";
 import { createSynologyChatPlugin } from "./src/channel.js";
 import { setSynologyRuntime } from "./src/runtime.js";
 

extensions/synology-chat/src/channel.integration.test.ts
@@ -11,8 +11,8 @@ type RegisteredRoute = {
 const registerPluginHttpRouteMock = vi.fn<(params: RegisteredRoute) => () => void>(() => vi.fn());
 const dispatchReplyWithBufferedBlockDispatcher = vi.fn().mockResolvedValue({ counts: {} });
 
-vi.mock("remoteclaw/plugin-sdk/compat", async (importOriginal) => {
-  const actual = await importOriginal<typeof import("remoteclaw/plugin-sdk/compat")>();
+vi.mock("remoteclaw/plugin-sdk/synology-chat", async (importOriginal) => {
+  const actual = await importOriginal<typeof import("remoteclaw/plugin-sdk/synology-chat")>();
   return {
     ...actual,
     DEFAULT_ACCOUNT_ID: "default",

extensions/synology-chat/src/channel.test.ts
@@ -1,7 +1,7 @@
 import { describe, it, expect, vi, beforeEach } from "vitest";
 
 // Mock external dependencies
-vi.mock("remoteclaw/plugin-sdk/compat", () => ({
+vi.mock("remoteclaw/plugin-sdk/synology-chat", () => ({
   DEFAULT_ACCOUNT_ID: "default",
   setAccountEnabledInConfigSection: vi.fn((_opts: any) => ({})),
   registerPluginHttpRoute: vi.fn(() => vi.fn()),
@@ -44,7 +44,7 @@ vi.mock("zod", () => ({
 }));
 
 const { createSynologyChatPlugin } = await import("./channel.js");
-const { registerPluginHttpRoute } = await import("remoteclaw/plugin-sdk/compat");
+const { registerPluginHttpRoute } = await import("remoteclaw/plugin-sdk/synology-chat");
 
 describe("createSynologyChatPlugin", () => {
   it("returns a plugin object with all required sections", () => {

extensions/synology-chat/src/channel.ts
@@ -9,7 +9,7 @@ import {
   setAccountEnabledInConfigSection,
   registerPluginHttpRoute,
   buildChannelConfigSchema,
-} from "remoteclaw/plugin-sdk/compat";
+} from "remoteclaw/plugin-sdk/synology-chat";
 import { z } from "zod";
 import { listAccountIds, resolveAccount } from "./accounts.js";
 import { sendMessage, sendFileUrl } from "./client.js";

extensions/synology-chat/src/security.ts
@@ -6,7 +6,7 @@ import * as crypto from "node:crypto";
 import {
   createFixedWindowRateLimiter,
   type FixedWindowRateLimiter,
-} from "remoteclaw/plugin-sdk/compat";
+} from "remoteclaw/plugin-sdk/synology-chat";
 
 export type DmAuthorizationResult =
   | { allowed: true }

extensions/synology-chat/src/webhook-handler.ts
@@ -9,7 +9,7 @@ import {
   isRequestBodyLimitError,
   readRequestBodyWithLimit,
   requestBodyErrorToText,
-} from "remoteclaw/plugin-sdk/compat";
+} from "remoteclaw/plugin-sdk/synology-chat";
 import { sendMessage, resolveChatUserId } from "./client.js";
 import { validateToken, authorizeUserForDm, sanitizeInput, RateLimiter } from "./security.js";
 import type { SynologyWebhookPayload, ResolvedSynologyChatAccount } from "./types.js";
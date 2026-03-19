wonglaitung/openclaw_mini

fix(gateway): harden message action channel fallback and startup grace
Take the safe, tested subset from #32367:\n- per-channel startup connect grace in health monitor\n- tool-context channel-provider fallback for message actions\n\nCo-authored-by: Munem Hashmi <munem.hashmi@gmail.com>

src/infra/outbound/message-action-runner.ts
@@ -217,13 +217,28 @@ async function maybeApplyCrossContextMarker(params: {
   });
 }
 
-async function resolveChannel(cfg: OpenClawConfig, params: Record<string, unknown>) {
+async function resolveChannel(
+  cfg: OpenClawConfig,
+  params: Record<string, unknown>,
+  toolContext?: { currentChannelProvider?: string },
+) {
   const channelHint = readStringParam(params, "channel");
-  const selection = await resolveMessageChannelSelection({
-    cfg,
-    channel: channelHint,
-  });
-  return selection.channel;
+  try {
+    const selection = await resolveMessageChannelSelection({
+      cfg,
+      channel: channelHint,
+    });
+    return selection.channel;
+  } catch (error) {
+    if (channelHint && toolContext?.currentChannelProvider) {
+      const fallback = normalizeMessageChannel(toolContext.currentChannelProvider);
+      if (fallback && isDeliverableMessageChannel(fallback)) {
+        params.channel = fallback;
+        return fallback;
+      }
+    }
+    throw error;
+  }
 }
 
 async function resolveActionTarget(params: {
@@ -317,7 +332,7 @@ async function handleBroadcastAction(
   }
   const targetChannels =
     channelHint && channelHint.trim().toLowerCase() !== "all"
-      ? [await resolveChannel(input.cfg, { channel: channelHint })]
+      ? [await resolveChannel(input.cfg, { channel: channelHint }, input.toolContext)]
       : configured;
   const results: Array<{
     channel: ChannelId;
@@ -754,7 +769,7 @@ export async function runMessageAction(
     }
   }
 
-  const channel = await resolveChannel(cfg, params);
+  const channel = await resolveChannel(cfg, params, input.toolContext);
   let accountId = readStringParam(params, "accountId") ?? input.defaultAccountId;
   if (!accountId && resolvedAgentId) {
     const byAgent = buildChannelAccountBindings(cfg).get(channel);

remoteclaw/remoteclaw

(cherry picked from commit 71cd3371372763e1f6c3d1fa7c11cb302285f619)

src/infra/outbound/message-action-runner.ts
@@ -208,13 +208,28 @@ async function maybeApplyCrossContextMarker(params: {
   });
 }
 
-async function resolveChannel(cfg: RemoteClawConfig, params: Record<string, unknown>) {
+async function resolveChannel(
+  cfg: RemoteClawConfig,
+  params: Record<string, unknown>,
+  toolContext?: { currentChannelProvider?: string },
+) {
   const channelHint = readStringParam(params, "channel");
-  const selection = await resolveMessageChannelSelection({
-    cfg,
-    channel: channelHint,
-  });
-  return selection.channel;
+  try {
+    const selection = await resolveMessageChannelSelection({
+      cfg,
+      channel: channelHint,
+    });
+    return selection.channel;
+  } catch (error) {
+    if (channelHint && toolContext?.currentChannelProvider) {
+      const fallback = normalizeMessageChannel(toolContext.currentChannelProvider);
+      if (fallback && isDeliverableMessageChannel(fallback)) {
+        params.channel = fallback;
+        return fallback;
+      }
+    }
+    throw error;
+  }
 }
 
 async function resolveActionTarget(params: {
@@ -308,7 +323,7 @@ async function handleBroadcastAction(
   }
   const targetChannels =
     channelHint && channelHint.trim().toLowerCase() !== "all"
-      ? [await resolveChannel(input.cfg, { channel: channelHint })]
+      ? [await resolveChannel(input.cfg, { channel: channelHint }, input.toolContext)]
       : configured;
   const results: Array<{
     channel: ChannelId;
@@ -706,7 +721,7 @@ export async function runMessageAction(
     toolContext: input.toolContext,
   });
 
-  const channel = await resolveChannel(cfg, params);
+  const channel = await resolveChannel(cfg, params, input.toolContext);
   let accountId = readStringParam(params, "accountId") ?? input.defaultAccountId;
   if (!accountId && resolvedAgentId) {
     const byAgent = buildChannelAccountBindings(cfg).get(channel);
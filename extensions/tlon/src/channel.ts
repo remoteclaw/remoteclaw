wonglaitung/openclaw_mini

Extensions: migrate tlon plugin-sdk imports

extensions/tlon/src/channel.ts
@@ -5,12 +5,12 @@ import type {
   ChannelPlugin,
   ChannelSetupInput,
   OpenClawConfig,
-} from "openclaw/plugin-sdk";
+} from "openclaw/plugin-sdk/compat";
 import {
   applyAccountNameToChannelSection,
   DEFAULT_ACCOUNT_ID,
   normalizeAccountId,
-} from "openclaw/plugin-sdk";
+} from "openclaw/plugin-sdk/compat";
 import { buildTlonAccountFields } from "./account-fields.js";
 import { tlonChannelConfigSchema } from "./config-schema.js";
 import { monitorTlonProvider } from "./monitor/index.js";
@@ -497,7 +497,7 @@ export const tlonPlugin: ChannelPlugin = {
         lastError: runtime?.lastError ?? null,
         probe,
       };
-      return snapshot as import("openclaw/plugin-sdk").ChannelAccountSnapshot;
+      return snapshot as import("openclaw/plugin-sdk/compat").ChannelAccountSnapshot;
     },
   },
   gateway: {
@@ -507,7 +507,7 @@ export const tlonPlugin: ChannelPlugin = {
         accountId: account.accountId,
         ship: account.ship,
         url: account.url,
-      } as import("openclaw/plugin-sdk").ChannelAccountSnapshot);
+      } as import("openclaw/plugin-sdk/compat").ChannelAccountSnapshot);
       ctx.log?.info(`[${account.accountId}] starting Tlon provider for ${account.ship ?? "tlon"}`);
       return monitorTlonProvider({
         runtime: ctx.runtime,

extensions/tlon/src/config-schema.ts
@@ -1,4 +1,4 @@
-import { buildChannelConfigSchema } from "openclaw/plugin-sdk";
+import { buildChannelConfigSchema } from "openclaw/plugin-sdk/compat";
 import { z } from "zod";
 
 const ShipSchema = z.string().min(1);

extensions/tlon/src/monitor/media.ts
@@ -5,7 +5,7 @@ import { homedir } from "node:os";
 import * as path from "node:path";
 import { Readable } from "node:stream";
 import { pipeline } from "node:stream/promises";
-import { fetchWithSsrFGuard } from "openclaw/plugin-sdk";
+import { fetchWithSsrFGuard } from "openclaw/plugin-sdk/compat";
 import { getDefaultSsrFPolicy } from "../urbit/context.js";
 
 // Default to OpenClaw workspace media directory

extensions/tlon/src/onboarding.ts
@@ -1,4 +1,4 @@
-import type { OpenClawConfig } from "openclaw/plugin-sdk";
+import type { OpenClawConfig } from "openclaw/plugin-sdk/compat";
 import {
   formatDocsLink,
   promptAccountId,
@@ -6,7 +6,7 @@ import {
   normalizeAccountId,
   type ChannelOnboardingAdapter,
   type WizardPrompter,
-} from "openclaw/plugin-sdk";
+} from "openclaw/plugin-sdk/compat";
 import { buildTlonAccountFields } from "./account-fields.js";
 import type { TlonResolvedAccount } from "./types.js";
 import { listTlonAccountIds, resolveTlonAccount } from "./types.js";

extensions/tlon/src/types.ts
@@ -1,4 +1,4 @@
-import type { OpenClawConfig } from "openclaw/plugin-sdk";
+import type { OpenClawConfig } from "openclaw/plugin-sdk/compat";
 
 export type TlonResolvedAccount = {
   accountId: string;

extensions/tlon/src/urbit/auth.ssrf.test.ts
@@ -1,5 +1,5 @@
-import type { LookupFn } from "openclaw/plugin-sdk";
-import { SsrFBlockedError } from "openclaw/plugin-sdk";
+import type { LookupFn } from "openclaw/plugin-sdk/compat";
+import { SsrFBlockedError } from "openclaw/plugin-sdk/compat";
 import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
 import { authenticate } from "./auth.js";
 

extensions/tlon/src/urbit/channel-ops.ts
@@ -1,4 +1,4 @@
-import type { LookupFn, SsrFPolicy } from "openclaw/plugin-sdk";
+import type { LookupFn, SsrFPolicy } from "openclaw/plugin-sdk/compat";
 import { UrbitHttpError } from "./errors.js";
 import { urbitFetch } from "./fetch.js";
 

extensions/tlon/src/urbit/upload.test.ts
@@ -1,8 +1,8 @@
 import { describe, expect, it, vi, afterEach, beforeEach } from "vitest";
 
 // Mock fetchWithSsrFGuard from plugin-sdk
-vi.mock("openclaw/plugin-sdk", async (importOriginal) => {
-  const actual = await importOriginal<typeof import("openclaw/plugin-sdk")>();
+vi.mock("openclaw/plugin-sdk/compat", async (importOriginal) => {
+  const actual = await importOriginal<typeof import("openclaw/plugin-sdk/compat")>();
   return {
     ...actual,
     fetchWithSsrFGuard: vi.fn(),
@@ -24,7 +24,7 @@ describe("uploadImageFromUrl", () => {
   });
 
   it("fetches image and calls uploadFile, returns uploaded URL", async () => {
-    const { fetchWithSsrFGuard } = await import("openclaw/plugin-sdk");
+    const { fetchWithSsrFGuard } = await import("openclaw/plugin-sdk/compat");
     const mockFetch = vi.mocked(fetchWithSsrFGuard);
 
     const { uploadFile } = await import("@tloncorp/api");
@@ -59,7 +59,7 @@ describe("uploadImageFromUrl", () => {
   });
 
   it("returns original URL if fetch fails", async () => {
-    const { fetchWithSsrFGuard } = await import("openclaw/plugin-sdk");
+    const { fetchWithSsrFGuard } = await import("openclaw/plugin-sdk/compat");
     const mockFetch = vi.mocked(fetchWithSsrFGuard);
 
     // Mock fetchWithSsrFGuard to return a failed response
@@ -79,7 +79,7 @@ describe("uploadImageFromUrl", () => {
   });
 
   it("returns original URL if upload fails", async () => {
-    const { fetchWithSsrFGuard } = await import("openclaw/plugin-sdk");
+    const { fetchWithSsrFGuard } = await import("openclaw/plugin-sdk/compat");
     const mockFetch = vi.mocked(fetchWithSsrFGuard);
 
     const { uploadFile } = await import("@tloncorp/api");
@@ -127,7 +127,7 @@ describe("uploadImageFromUrl", () => {
   });
 
   it("extracts filename from URL path", async () => {
-    const { fetchWithSsrFGuard } = await import("openclaw/plugin-sdk");
+    const { fetchWithSsrFGuard } = await import("openclaw/plugin-sdk/compat");
     const mockFetch = vi.mocked(fetchWithSsrFGuard);
 
     const { uploadFile } = await import("@tloncorp/api");
@@ -157,7 +157,7 @@ describe("uploadImageFromUrl", () => {
   });
 
   it("uses default filename when URL has no path", async () => {
-    const { fetchWithSsrFGuard } = await import("openclaw/plugin-sdk");
+    const { fetchWithSsrFGuard } = await import("openclaw/plugin-sdk/compat");
     const mockFetch = vi.mocked(fetchWithSsrFGuard);
 
     const { uploadFile } = await import("@tloncorp/api");

remoteclaw/remoteclaw

(cherry picked from commit b0bca8d6e95c97156c72185e030a29ab115b2654)

extensions/tlon/src/channel.ts
@@ -4,13 +4,13 @@ import type {
   ChannelOutboundAdapter,
   ChannelPlugin,
   ChannelSetupInput,
-  RemoteClawConfig,
-} from "remoteclaw/plugin-sdk";
+  OpenClawConfig,
+} from "remoteclaw/plugin-sdk/compat";
 import {
   applyAccountNameToChannelSection,
   DEFAULT_ACCOUNT_ID,
   normalizeAccountId,
-} from "remoteclaw/plugin-sdk";
+} from "remoteclaw/plugin-sdk/compat";
 import { buildTlonAccountFields } from "./account-fields.js";
 import { tlonChannelConfigSchema } from "./config-schema.js";
 import { monitorTlonProvider } from "./monitor/index.js";
@@ -497,7 +497,7 @@ export const tlonPlugin: ChannelPlugin = {
         lastError: runtime?.lastError ?? null,
         probe,
       };
-      return snapshot as import("remoteclaw/plugin-sdk").ChannelAccountSnapshot;
+      return snapshot as import("remoteclaw/plugin-sdk/compat").ChannelAccountSnapshot;
     },
   },
   gateway: {
@@ -507,7 +507,7 @@ export const tlonPlugin: ChannelPlugin = {
         accountId: account.accountId,
         ship: account.ship,
         url: account.url,
-      } as import("remoteclaw/plugin-sdk").ChannelAccountSnapshot);
+      } as import("remoteclaw/plugin-sdk/compat").ChannelAccountSnapshot);
       ctx.log?.info(`[${account.accountId}] starting Tlon provider for ${account.ship ?? "tlon"}`);
       return monitorTlonProvider({
         runtime: ctx.runtime,

extensions/tlon/src/config-schema.ts
@@ -1,4 +1,4 @@
-import { buildChannelConfigSchema } from "remoteclaw/plugin-sdk";
+import { buildChannelConfigSchema } from "remoteclaw/plugin-sdk/compat";
 import { z } from "zod";
 
 const ShipSchema = z.string().min(1);

extensions/tlon/src/monitor/media.ts
@@ -5,7 +5,7 @@ import { homedir } from "node:os";
 import * as path from "node:path";
 import { Readable } from "node:stream";
 import { pipeline } from "node:stream/promises";
-import { fetchWithSsrFGuard } from "remoteclaw/plugin-sdk";
+import { fetchWithSsrFGuard } from "remoteclaw/plugin-sdk/compat";
 import { getDefaultSsrFPolicy } from "../urbit/context.js";
 
 // Default to RemoteClaw workspace media directory

extensions/tlon/src/onboarding.ts
@@ -1,4 +1,4 @@
-import type { RemoteClawConfig } from "remoteclaw/plugin-sdk";
+import type { OpenClawConfig } from "remoteclaw/plugin-sdk/compat";
 import {
   formatDocsLink,
   promptAccountId,
@@ -6,7 +6,7 @@ import {
   normalizeAccountId,
   type ChannelOnboardingAdapter,
   type WizardPrompter,
-} from "remoteclaw/plugin-sdk";
+} from "remoteclaw/plugin-sdk/compat";
 import { buildTlonAccountFields } from "./account-fields.js";
 import type { TlonResolvedAccount } from "./types.js";
 import { listTlonAccountIds, resolveTlonAccount } from "./types.js";

extensions/tlon/src/types.ts
@@ -1,4 +1,4 @@
-import type { RemoteClawConfig } from "remoteclaw/plugin-sdk";
+import type { OpenClawConfig } from "remoteclaw/plugin-sdk/compat";
 
 export type TlonResolvedAccount = {
   accountId: string;

extensions/tlon/src/urbit/auth.ssrf.test.ts
@@ -1,5 +1,5 @@
-import type { LookupFn } from "remoteclaw/plugin-sdk";
-import { SsrFBlockedError } from "remoteclaw/plugin-sdk";
+import type { LookupFn } from "remoteclaw/plugin-sdk/compat";
+import { SsrFBlockedError } from "remoteclaw/plugin-sdk/compat";
 import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
 import { authenticate } from "./auth.js";
 

extensions/tlon/src/urbit/channel-ops.ts
@@ -1,4 +1,4 @@
-import type { LookupFn, SsrFPolicy } from "remoteclaw/plugin-sdk";
+import type { LookupFn, SsrFPolicy } from "remoteclaw/plugin-sdk/compat";
 import { UrbitHttpError } from "./errors.js";
 import { urbitFetch } from "./fetch.js";
 

extensions/tlon/src/urbit/upload.test.ts
@@ -1,8 +1,8 @@
 import { describe, expect, it, vi, afterEach, beforeEach } from "vitest";
 
 // Mock fetchWithSsrFGuard from plugin-sdk
-vi.mock("remoteclaw/plugin-sdk", async (importOriginal) => {
-  const actual = await importOriginal<typeof import("remoteclaw/plugin-sdk")>();
+vi.mock("remoteclaw/plugin-sdk/compat", async (importOriginal) => {
+  const actual = await importOriginal<typeof import("remoteclaw/plugin-sdk/compat")>();
   return {
     ...actual,
     fetchWithSsrFGuard: vi.fn(),
@@ -24,7 +24,7 @@ describe("uploadImageFromUrl", () => {
   });
 
   it("fetches image and calls uploadFile, returns uploaded URL", async () => {
-    const { fetchWithSsrFGuard } = await import("remoteclaw/plugin-sdk");
+    const { fetchWithSsrFGuard } = await import("remoteclaw/plugin-sdk/compat");
     const mockFetch = vi.mocked(fetchWithSsrFGuard);
 
     const { uploadFile } = await import("@tloncorp/api");
@@ -59,7 +59,7 @@ describe("uploadImageFromUrl", () => {
   });
 
   it("returns original URL if fetch fails", async () => {
-    const { fetchWithSsrFGuard } = await import("remoteclaw/plugin-sdk");
+    const { fetchWithSsrFGuard } = await import("remoteclaw/plugin-sdk/compat");
     const mockFetch = vi.mocked(fetchWithSsrFGuard);
 
     // Mock fetchWithSsrFGuard to return a failed response
@@ -79,7 +79,7 @@ describe("uploadImageFromUrl", () => {
   });
 
   it("returns original URL if upload fails", async () => {
-    const { fetchWithSsrFGuard } = await import("remoteclaw/plugin-sdk");
+    const { fetchWithSsrFGuard } = await import("remoteclaw/plugin-sdk/compat");
     const mockFetch = vi.mocked(fetchWithSsrFGuard);
 
     const { uploadFile } = await import("@tloncorp/api");
@@ -127,7 +127,7 @@ describe("uploadImageFromUrl", () => {
   });
 
   it("extracts filename from URL path", async () => {
-    const { fetchWithSsrFGuard } = await import("remoteclaw/plugin-sdk");
+    const { fetchWithSsrFGuard } = await import("remoteclaw/plugin-sdk/compat");
     const mockFetch = vi.mocked(fetchWithSsrFGuard);
 
     const { uploadFile } = await import("@tloncorp/api");
@@ -157,7 +157,7 @@ describe("uploadImageFromUrl", () => {
   });
 
   it("uses default filename when URL has no path", async () => {
-    const { fetchWithSsrFGuard } = await import("remoteclaw/plugin-sdk");
+    const { fetchWithSsrFGuard } = await import("remoteclaw/plugin-sdk/compat");
     const mockFetch = vi.mocked(fetchWithSsrFGuard);
 
     const { uploadFile } = await import("@tloncorp/api");
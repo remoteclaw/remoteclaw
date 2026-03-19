wonglaitung/openclaw_mini

Plugin SDK: add full bundled subpath wiring

src/plugin-sdk/irc.ts
@@ -1,0 +1,70 @@
+// Narrow plugin-sdk surface for the bundled irc plugin.
+// Keep this list additive and scoped to symbols used under extensions/irc.
+
+export { resolveControlCommandGate } from "../channels/command-gating.js";
+export { logInboundDrop } from "../channels/logging.js";
+export {
+  deleteAccountFromConfigSection,
+  setAccountEnabledInConfigSection,
+} from "../channels/plugins/config-helpers.js";
+export { buildChannelConfigSchema } from "../channels/plugins/config-schema.js";
+export { formatPairingApproveHint } from "../channels/plugins/helpers.js";
+export type {
+  ChannelOnboardingAdapter,
+  ChannelOnboardingDmPolicy,
+} from "../channels/plugins/onboarding-types.js";
+export { promptChannelAccessConfig } from "../channels/plugins/onboarding/channel-access.js";
+export { addWildcardAllowFrom, promptAccountId } from "../channels/plugins/onboarding/helpers.js";
+export { PAIRING_APPROVED_MESSAGE } from "../channels/plugins/pairing-message.js";
+export type { BaseProbeResult } from "../channels/plugins/types.js";
+export type { ChannelPlugin } from "../channels/plugins/types.plugin.js";
+export { getChatChannelMeta } from "../channels/registry.js";
+export { createReplyPrefixOptions } from "../channels/reply-prefix.js";
+export type { OpenClawConfig } from "../config/config.js";
+export { isDangerousNameMatchingEnabled } from "../config/dangerous-name-matching.js";
+export {
+  GROUP_POLICY_BLOCKED_LABEL,
+  resolveAllowlistProviderRuntimeGroupPolicy,
+  resolveDefaultGroupPolicy,
+  warnMissingProviderGroupPolicyFallbackOnce,
+} from "../config/runtime-group-policy.js";
+export type {
+  BlockStreamingCoalesceConfig,
+  DmConfig,
+  DmPolicy,
+  GroupPolicy,
+  GroupToolPolicyBySenderConfig,
+  GroupToolPolicyConfig,
+  MarkdownConfig,
+} from "../config/types.js";
+export { normalizeResolvedSecretInputString } from "../config/types.secrets.js";
+export { ToolPolicySchema } from "../config/zod-schema.agent-runtime.js";
+export {
+  BlockStreamingCoalesceSchema,
+  DmConfigSchema,
+  DmPolicySchema,
+  GroupPolicySchema,
+  MarkdownConfigSchema,
+  ReplyRuntimeConfigSchemaShape,
+  requireOpenAllowFrom,
+} from "../config/zod-schema.core.js";
+export { emptyPluginConfigSchema } from "../plugins/config-schema.js";
+export type { PluginRuntime } from "../plugins/runtime/types.js";
+export type { OpenClawPluginApi } from "../plugins/types.js";
+export { DEFAULT_ACCOUNT_ID } from "../routing/session-key.js";
+export type { RuntimeEnv } from "../runtime.js";
+export {
+  readStoreAllowFromForDmPolicy,
+  resolveEffectiveAllowFromLists,
+} from "../security/dm-policy-shared.js";
+export { formatDocsLink } from "../terminal/links.js";
+export type { WizardPrompter } from "../wizard/prompts.js";
+export { createScopedPairingAccess } from "./pairing-access.js";
+export type { OutboundReplyPayload } from "./reply-payload.js";
+export {
+  createNormalizedOutboundDeliverer,
+  formatTextWithAttachmentLinks,
+  resolveOutboundMediaUrls,
+} from "./reply-payload.js";
+export { createLoggerBackedRuntime } from "./runtime.js";
+export { buildBaseAccountStatusSnapshot, buildBaseChannelStatusSummary } from "./status-helpers.js";

src/plugin-sdk/nextcloud-talk.ts
@@ -1,0 +1,92 @@
+// Narrow plugin-sdk surface for the bundled nextcloud-talk plugin.
+// Keep this list additive and scoped to symbols used under extensions/nextcloud-talk.
+
+export { logInboundDrop } from "../channels/logging.js";
+export { resolveMentionGatingWithBypass } from "../channels/mention-gating.js";
+export type { AllowlistMatch } from "../channels/plugins/allowlist-match.js";
+export {
+  buildChannelKeyCandidates,
+  normalizeChannelSlug,
+  resolveChannelEntryMatchWithFallback,
+  resolveNestedAllowlistDecision,
+} from "../channels/plugins/channel-config.js";
+export {
+  deleteAccountFromConfigSection,
+  setAccountEnabledInConfigSection,
+} from "../channels/plugins/config-helpers.js";
+export { buildChannelConfigSchema } from "../channels/plugins/config-schema.js";
+export { formatPairingApproveHint } from "../channels/plugins/helpers.js";
+export type {
+  ChannelOnboardingAdapter,
+  ChannelOnboardingDmPolicy,
+} from "../channels/plugins/onboarding-types.js";
+export {
+  addWildcardAllowFrom,
+  mergeAllowFromEntries,
+  promptAccountId,
+  promptSingleChannelSecretInput,
+} from "../channels/plugins/onboarding/helpers.js";
+export { applyAccountNameToChannelSection } from "../channels/plugins/setup-helpers.js";
+export type { ChannelGroupContext, ChannelSetupInput } from "../channels/plugins/types.js";
+export type { ChannelPlugin } from "../channels/plugins/types.plugin.js";
+export { createReplyPrefixOptions } from "../channels/reply-prefix.js";
+export type { OpenClawConfig } from "../config/config.js";
+export {
+  GROUP_POLICY_BLOCKED_LABEL,
+  resolveAllowlistProviderRuntimeGroupPolicy,
+  resolveDefaultGroupPolicy,
+  warnMissingProviderGroupPolicyFallbackOnce,
+} from "../config/runtime-group-policy.js";
+export type {
+  BlockStreamingCoalesceConfig,
+  DmConfig,
+  DmPolicy,
+  GroupPolicy,
+  GroupToolPolicyConfig,
+} from "../config/types.js";
+export type { SecretInput } from "../config/types.secrets.js";
+export {
+  hasConfiguredSecretInput,
+  normalizeResolvedSecretInputString,
+  normalizeSecretInputString,
+} from "../config/types.secrets.js";
+export { ToolPolicySchema } from "../config/zod-schema.agent-runtime.js";
+export {
+  BlockStreamingCoalesceSchema,
+  DmConfigSchema,
+  DmPolicySchema,
+  GroupPolicySchema,
+  MarkdownConfigSchema,
+  ReplyRuntimeConfigSchemaShape,
+  requireOpenAllowFrom,
+} from "../config/zod-schema.core.js";
+export {
+  isRequestBodyLimitError,
+  readRequestBodyWithLimit,
+  requestBodyErrorToText,
+} from "../infra/http-body.js";
+export { fetchWithSsrFGuard } from "../infra/net/fetch-guard.js";
+export { emptyPluginConfigSchema } from "../plugins/config-schema.js";
+export type { PluginRuntime } from "../plugins/runtime/types.js";
+export type { OpenClawPluginApi } from "../plugins/types.js";
+export { DEFAULT_ACCOUNT_ID, normalizeAccountId } from "../routing/session-key.js";
+export type { RuntimeEnv } from "../runtime.js";
+export {
+  readStoreAllowFromForDmPolicy,
+  resolveDmGroupAccessWithCommandGate,
+} from "../security/dm-policy-shared.js";
+export { formatDocsLink } from "../terminal/links.js";
+export type { WizardPrompter } from "../wizard/prompts.js";
+export {
+  listConfiguredAccountIds,
+  resolveAccountWithDefaultFallback,
+} from "./account-resolution.js";
+export { createScopedPairingAccess } from "./pairing-access.js";
+export { createPersistentDedupe } from "./persistent-dedupe.js";
+export type { OutboundReplyPayload } from "./reply-payload.js";
+export {
+  createNormalizedOutboundDeliverer,
+  formatTextWithAttachmentLinks,
+  resolveOutboundMediaUrls,
+} from "./reply-payload.js";
+export { createLoggerBackedRuntime } from "./runtime.js";

src/plugin-sdk/open-prose.ts
@@ -1,0 +1,4 @@
+// Narrow plugin-sdk surface for the bundled open-prose plugin.
+// Keep this list additive and scoped to symbols used under extensions/open-prose.
+
+export type { OpenClawPluginApi } from "../plugins/types.js";

vitest.config.ts
@@ -8,55 +8,60 @@ const isCI = process.env.CI === "true" || process.env.GITHUB_ACTIONS === "true";
 const isWindows = process.platform === "win32";
 const localWorkers = Math.max(4, Math.min(16, os.cpus().length));
 const ciWorkers = isWindows ? 2 : 3;
+const pluginSdkSubpaths = [
+  "account-id",
+  "core",
+  "compat",
+  "telegram",
+  "discord",
+  "slack",
+  "signal",
+  "imessage",
+  "whatsapp",
+  "line",
+  "msteams",
+  "acpx",
+  "bluebubbles",
+  "copilot-proxy",
+  "device-pair",
+  "diagnostics-otel",
+  "diffs",
+  "feishu",
+  "google-gemini-cli-auth",
+  "googlechat",
+  "irc",
+  "llm-task",
+  "lobster",
+  "matrix",
+  "mattermost",
+  "memory-core",
+  "memory-lancedb",
+  "minimax-portal-auth",
+  "nextcloud-talk",
+  "nostr",
+  "open-prose",
+  "phone-control",
+  "qwen-portal-auth",
+  "synology-chat",
+  "talk-voice",
+  "test-utils",
+  "thread-ownership",
+  "tlon",
+  "twitch",
+  "voice-call",
+  "zalo",
+  "zalouser",
+  "keyed-async-queue",
+] as const;
 
 export default defineConfig({
   resolve: {
     // Keep this ordered: the base `openclaw/plugin-sdk` alias is a prefix match.
     alias: [
-      {
-        find: "openclaw/plugin-sdk/account-id",
-        replacement: path.join(repoRoot, "src", "plugin-sdk", "account-id.ts"),
-      },
-      {
-        find: "openclaw/plugin-sdk/core",
-        replacement: path.join(repoRoot, "src", "plugin-sdk", "core.ts"),
-      },
-      {
-        find: "openclaw/plugin-sdk/compat",
-        replacement: path.join(repoRoot, "src", "plugin-sdk", "compat.ts"),
-      },
-      {
-        find: "openclaw/plugin-sdk/telegram",
-        replacement: path.join(repoRoot, "src", "plugin-sdk", "telegram.ts"),
-      },
-      {
-        find: "openclaw/plugin-sdk/discord",
-        replacement: path.join(repoRoot, "src", "plugin-sdk", "discord.ts"),
-      },
-      {
-        find: "openclaw/plugin-sdk/slack",
-        replacement: path.join(repoRoot, "src", "plugin-sdk", "slack.ts"),
-      },
-      {
-        find: "openclaw/plugin-sdk/signal",
-        replacement: path.join(repoRoot, "src", "plugin-sdk", "signal.ts"),
-      },
-      {
-        find: "openclaw/plugin-sdk/imessage",
-        replacement: path.join(repoRoot, "src", "plugin-sdk", "imessage.ts"),
-      },
-      {
-        find: "openclaw/plugin-sdk/whatsapp",
-        replacement: path.join(repoRoot, "src", "plugin-sdk", "whatsapp.ts"),
-      },
-      {
-        find: "openclaw/plugin-sdk/line",
-        replacement: path.join(repoRoot, "src", "plugin-sdk", "line.ts"),
-      },
-      {
-        find: "openclaw/plugin-sdk/keyed-async-queue",
-        replacement: path.join(repoRoot, "src", "plugin-sdk", "keyed-async-queue.ts"),
-      },
+      ...pluginSdkSubpaths.map((subpath) => ({
+        find: `openclaw/plugin-sdk/${subpath}`,
+        replacement: path.join(repoRoot, "src", "plugin-sdk", `${subpath}.ts`),
+      })),
       {
         find: "openclaw/plugin-sdk",
         replacement: path.join(repoRoot, "src", "plugin-sdk", "index.ts"),

remoteclaw/remoteclaw

(cherry picked from commit 7a2f5a0098d192944825f17d3be457af91d2bed1)

src/plugin-sdk/irc.ts
@@ -1,0 +1,70 @@
+// Narrow plugin-sdk surface for the bundled irc plugin.
+// Keep this list additive and scoped to symbols used under extensions/irc.
+
+export { resolveControlCommandGate } from "../channels/command-gating.js";
+export { logInboundDrop } from "../channels/logging.js";
+export {
+  deleteAccountFromConfigSection,
+  setAccountEnabledInConfigSection,
+} from "../channels/plugins/config-helpers.js";
+export { buildChannelConfigSchema } from "../channels/plugins/config-schema.js";
+export { formatPairingApproveHint } from "../channels/plugins/helpers.js";
+export type {
+  ChannelOnboardingAdapter,
+  ChannelOnboardingDmPolicy,
+} from "../channels/plugins/onboarding-types.js";
+export { promptChannelAccessConfig } from "../channels/plugins/onboarding/channel-access.js";
+export { addWildcardAllowFrom, promptAccountId } from "../channels/plugins/onboarding/helpers.js";
+export { PAIRING_APPROVED_MESSAGE } from "../channels/plugins/pairing-message.js";
+export type { BaseProbeResult } from "../channels/plugins/types.js";
+export type { ChannelPlugin } from "../channels/plugins/types.plugin.js";
+export { getChatChannelMeta } from "../channels/registry.js";
+export { createReplyPrefixOptions } from "../channels/reply-prefix.js";
+export type { RemoteClawConfig } from "../config/config.js";
+export { isDangerousNameMatchingEnabled } from "../config/dangerous-name-matching.js";
+export {
+  GROUP_POLICY_BLOCKED_LABEL,
+  resolveAllowlistProviderRuntimeGroupPolicy,
+  resolveDefaultGroupPolicy,
+  warnMissingProviderGroupPolicyFallbackOnce,
+} from "../config/runtime-group-policy.js";
+export type {
+  BlockStreamingCoalesceConfig,
+  DmConfig,
+  DmPolicy,
+  GroupPolicy,
+  GroupToolPolicyBySenderConfig,
+  GroupToolPolicyConfig,
+  MarkdownConfig,
+} from "../config/types.js";
+export { normalizeSecretInputString } from "../config/types.secrets.js";
+export { ToolPolicySchema } from "../config/zod-schema.agent-runtime.js";
+export {
+  BlockStreamingCoalesceSchema,
+  DmConfigSchema,
+  DmPolicySchema,
+  GroupPolicySchema,
+  MarkdownConfigSchema,
+  ReplyRuntimeConfigSchemaShape,
+  requireOpenAllowFrom,
+} from "../config/zod-schema.core.js";
+export { emptyPluginConfigSchema } from "../plugins/config-schema.js";
+export type { PluginRuntime } from "../plugins/runtime/types.js";
+export type { RemoteClawPluginApi } from "../plugins/types.js";
+export { DEFAULT_ACCOUNT_ID } from "../routing/session-key.js";
+export type { RuntimeEnv } from "../runtime.js";
+export {
+  readStoreAllowFromForDmPolicy,
+  resolveEffectiveAllowFromLists,
+} from "../security/dm-policy-shared.js";
+export { formatDocsLink } from "../terminal/links.js";
+export type { WizardPrompter } from "../wizard/prompts.js";
+export { createScopedPairingAccess } from "./pairing-access.js";
+export type { OutboundReplyPayload } from "./reply-payload.js";
+export {
+  createNormalizedOutboundDeliverer,
+  formatTextWithAttachmentLinks,
+  resolveOutboundMediaUrls,
+} from "./reply-payload.js";
+export { createLoggerBackedRuntime } from "./runtime.js";
+export { buildBaseAccountStatusSnapshot, buildBaseChannelStatusSummary } from "./status-helpers.js";

src/plugin-sdk/nextcloud-talk.ts
@@ -1,0 +1,88 @@
+// Narrow plugin-sdk surface for the bundled nextcloud-talk plugin.
+// Keep this list additive and scoped to symbols used under extensions/nextcloud-talk.
+
+export { logInboundDrop } from "../channels/logging.js";
+export { resolveMentionGatingWithBypass } from "../channels/mention-gating.js";
+export type { AllowlistMatch } from "../channels/plugins/allowlist-match.js";
+export {
+  buildChannelKeyCandidates,
+  normalizeChannelSlug,
+  resolveChannelEntryMatchWithFallback,
+  resolveNestedAllowlistDecision,
+} from "../channels/plugins/channel-config.js";
+export {
+  deleteAccountFromConfigSection,
+  setAccountEnabledInConfigSection,
+} from "../channels/plugins/config-helpers.js";
+export { buildChannelConfigSchema } from "../channels/plugins/config-schema.js";
+export { formatPairingApproveHint } from "../channels/plugins/helpers.js";
+export type {
+  ChannelOnboardingAdapter,
+  ChannelOnboardingDmPolicy,
+} from "../channels/plugins/onboarding-types.js";
+export {
+  addWildcardAllowFrom,
+  mergeAllowFromEntries,
+  promptAccountId,
+  promptSingleChannelToken,
+} from "../channels/plugins/onboarding/helpers.js";
+export { applyAccountNameToChannelSection } from "../channels/plugins/setup-helpers.js";
+export type { ChannelGroupContext, ChannelSetupInput } from "../channels/plugins/types.js";
+export type { ChannelPlugin } from "../channels/plugins/types.plugin.js";
+export { createReplyPrefixOptions } from "../channels/reply-prefix.js";
+export type { RemoteClawConfig } from "../config/config.js";
+export {
+  GROUP_POLICY_BLOCKED_LABEL,
+  resolveAllowlistProviderRuntimeGroupPolicy,
+  resolveDefaultGroupPolicy,
+  warnMissingProviderGroupPolicyFallbackOnce,
+} from "../config/runtime-group-policy.js";
+export type {
+  BlockStreamingCoalesceConfig,
+  DmConfig,
+  DmPolicy,
+  GroupPolicy,
+  GroupToolPolicyConfig,
+} from "../config/types.js";
+export type { SecretInput } from "../config/types.secrets.js";
+export { normalizeSecretInputString } from "../config/types.secrets.js";
+export { ToolPolicySchema } from "../config/zod-schema.agent-runtime.js";
+export {
+  BlockStreamingCoalesceSchema,
+  DmConfigSchema,
+  DmPolicySchema,
+  GroupPolicySchema,
+  MarkdownConfigSchema,
+  ReplyRuntimeConfigSchemaShape,
+  requireOpenAllowFrom,
+} from "../config/zod-schema.core.js";
+export {
+  isRequestBodyLimitError,
+  readRequestBodyWithLimit,
+  requestBodyErrorToText,
+} from "../infra/http-body.js";
+export { fetchWithSsrFGuard } from "../infra/net/fetch-guard.js";
+export { emptyPluginConfigSchema } from "../plugins/config-schema.js";
+export type { PluginRuntime } from "../plugins/runtime/types.js";
+export type { RemoteClawPluginApi } from "../plugins/types.js";
+export { DEFAULT_ACCOUNT_ID, normalizeAccountId } from "../routing/session-key.js";
+export type { RuntimeEnv } from "../runtime.js";
+export {
+  readStoreAllowFromForDmPolicy,
+  resolveDmGroupAccessWithCommandGate,
+} from "../security/dm-policy-shared.js";
+export { formatDocsLink } from "../terminal/links.js";
+export type { WizardPrompter } from "../wizard/prompts.js";
+export {
+  listConfiguredAccountIds,
+  resolveAccountWithDefaultFallback,
+} from "./account-resolution.js";
+export { createScopedPairingAccess } from "./pairing-access.js";
+export { createPersistentDedupe } from "./persistent-dedupe.js";
+export type { OutboundReplyPayload } from "./reply-payload.js";
+export {
+  createNormalizedOutboundDeliverer,
+  formatTextWithAttachmentLinks,
+  resolveOutboundMediaUrls,
+} from "./reply-payload.js";
+export { createLoggerBackedRuntime } from "./runtime.js";

src/plugin-sdk/open-prose.ts
@@ -1,0 +1,4 @@
+// Narrow plugin-sdk surface for the bundled open-prose plugin.
+// Keep this list additive and scoped to symbols used under extensions/open-prose.
+
+export type { RemoteClawPluginApi } from "../plugins/types.js";

vitest.config.ts
@@ -8,55 +8,58 @@ const isCI = process.env.CI === "true" || process.env.GITHUB_ACTIONS === "true";
 const isWindows = process.platform === "win32";
 const localWorkers = Math.max(4, Math.min(16, os.cpus().length));
 const ciWorkers = isWindows ? 2 : 3;
+const pluginSdkSubpaths = [
+  "account-id",
+  "core",
+  "compat",
+  "telegram",
+  "discord",
+  "slack",
+  "signal",
+  "imessage",
+  "whatsapp",
+  "line",
+  "msteams",
+  "acpx",
+  "bluebubbles",
+  "copilot-proxy",
+  "device-pair",
+  "diagnostics-otel",
+  "diffs",
+  "feishu",
+  "google-gemini-cli-auth",
+  "googlechat",
+  "irc",
+  "llm-task",
+  "lobster",
+  "matrix",
+  "mattermost",
+  "minimax-portal-auth",
+  "nextcloud-talk",
+  "nostr",
+  "open-prose",
+  "phone-control",
+  "qwen-portal-auth",
+  "synology-chat",
+  "talk-voice",
+  "test-utils",
+  "thread-ownership",
+  "tlon",
+  "twitch",
+  "voice-call",
+  "zalo",
+  "zalouser",
+  "keyed-async-queue",
+] as const;
 
 export default defineConfig({
   resolve: {
     // Keep this ordered: the base `remoteclaw/plugin-sdk` alias is a prefix match.
     alias: [
-      {
-        find: "remoteclaw/plugin-sdk/account-id",
-        replacement: path.join(repoRoot, "src", "plugin-sdk", "account-id.ts"),
-      },
-      {
-        find: "remoteclaw/plugin-sdk/core",
-        replacement: path.join(repoRoot, "src", "plugin-sdk", "core.ts"),
-      },
-      {
-        find: "remoteclaw/plugin-sdk/compat",
-        replacement: path.join(repoRoot, "src", "plugin-sdk", "compat.ts"),
-      },
-      {
-        find: "remoteclaw/plugin-sdk/telegram",
-        replacement: path.join(repoRoot, "src", "plugin-sdk", "telegram.ts"),
-      },
-      {
-        find: "remoteclaw/plugin-sdk/discord",
-        replacement: path.join(repoRoot, "src", "plugin-sdk", "discord.ts"),
-      },
-      {
-        find: "remoteclaw/plugin-sdk/slack",
-        replacement: path.join(repoRoot, "src", "plugin-sdk", "slack.ts"),
-      },
-      {
-        find: "remoteclaw/plugin-sdk/signal",
-        replacement: path.join(repoRoot, "src", "plugin-sdk", "signal.ts"),
-      },
-      {
-        find: "remoteclaw/plugin-sdk/imessage",
-        replacement: path.join(repoRoot, "src", "plugin-sdk", "imessage.ts"),
-      },
-      {
-        find: "remoteclaw/plugin-sdk/whatsapp",
-        replacement: path.join(repoRoot, "src", "plugin-sdk", "whatsapp.ts"),
-      },
-      {
-        find: "remoteclaw/plugin-sdk/line",
-        replacement: path.join(repoRoot, "src", "plugin-sdk", "line.ts"),
-      },
-      {
-        find: "remoteclaw/plugin-sdk/keyed-async-queue",
-        replacement: path.join(repoRoot, "src", "plugin-sdk", "keyed-async-queue.ts"),
-      },
+      ...pluginSdkSubpaths.map((subpath) => ({
+        find: `remoteclaw/plugin-sdk/${subpath}`,
+        replacement: path.join(repoRoot, "src", "plugin-sdk", `${subpath}.ts`),
+      })),
       {
         find: "remoteclaw/plugin-sdk",
         replacement: path.join(repoRoot, "src", "plugin-sdk", "index.ts"),
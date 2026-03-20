wonglaitung/openclaw_mini

refactor(slack): reuse shared account merge helper

src/slack/accounts.ts
@@ -43,7 +43,10 @@ function resolveAccountConfig(
   return resolveAccountEntry(cfg.channels?.slack?.accounts, accountId);
 }
 
-function mergeSlackAccountConfig(cfg: OpenClawConfig, accountId: string): SlackAccountConfig {
+export function mergeSlackAccountConfig(
+  cfg: OpenClawConfig,
+  accountId: string,
+): SlackAccountConfig {
   const { accounts: _ignored, ...base } = (cfg.channels?.slack ?? {}) as SlackAccountConfig & {
     accounts?: unknown;
   };

remoteclaw/remoteclaw

(cherry picked from commit 398bf516597d8eea1760e5ca3e773687a0fca2af)

src/slack/accounts.ts
@@ -43,7 +43,10 @@ function resolveAccountConfig(
   return resolveAccountEntry(cfg.channels?.slack?.accounts, accountId);
 }
 
-function mergeSlackAccountConfig(cfg: RemoteClawConfig, accountId: string): SlackAccountConfig {
+export function mergeSlackAccountConfig(
+  cfg: RemoteClawConfig,
+  accountId: string,
+): SlackAccountConfig {
   const { accounts: _ignored, ...base } = (cfg.channels?.slack ?? {}) as SlackAccountConfig & {
     accounts?: unknown;
   };
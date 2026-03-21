wonglaitung/openclaw_mini

refactor: share daemon install plan runtime scaffolding

src/commands/daemon-install-plan.shared.test.ts
@@ -1,0 +1,31 @@
+import { describe, expect, it } from "vitest";
+import {
+  resolveDaemonInstallRuntimeInputs,
+  resolveGatewayDevMode,
+} from "./daemon-install-plan.shared.js";
+
+describe("resolveGatewayDevMode", () => {
+  it("detects src ts entrypoints", () => {
+    expect(resolveGatewayDevMode(["node", "/Users/me/openclaw/src/cli/index.ts"])).toBe(true);
+    expect(resolveGatewayDevMode(["node", "C:\\Users\\me\\openclaw\\src\\cli\\index.ts"])).toBe(
+      true,
+    );
+    expect(resolveGatewayDevMode(["node", "/Users/me/openclaw/dist/cli/index.js"])).toBe(false);
+  });
+});
+
+describe("resolveDaemonInstallRuntimeInputs", () => {
+  it("keeps explicit devMode and nodePath overrides", async () => {
+    await expect(
+      resolveDaemonInstallRuntimeInputs({
+        env: {},
+        runtime: "node",
+        devMode: false,
+        nodePath: "/custom/node",
+      }),
+    ).resolves.toEqual({
+      devMode: false,
+      nodePath: "/custom/node",
+    });
+  });
+});

remoteclaw/remoteclaw

(cherry picked from commit c35f529fac4bc0f09076cb500aa376c7aaa8f100)

src/commands/daemon-install-plan.shared.test.ts
@@ -1,0 +1,31 @@
+import { describe, expect, it } from "vitest";
+import {
+  resolveDaemonInstallRuntimeInputs,
+  resolveGatewayDevMode,
+} from "./daemon-install-plan.shared.js";
+
+describe("resolveGatewayDevMode", () => {
+  it("detects src ts entrypoints", () => {
+    expect(resolveGatewayDevMode(["node", "/Users/me/remoteclaw/src/cli/index.ts"])).toBe(true);
+    expect(resolveGatewayDevMode(["node", "C:\\Users\\me\\remoteclaw\\src\\cli\\index.ts"])).toBe(
+      true,
+    );
+    expect(resolveGatewayDevMode(["node", "/Users/me/remoteclaw/dist/cli/index.js"])).toBe(false);
+  });
+});
+
+describe("resolveDaemonInstallRuntimeInputs", () => {
+  it("keeps explicit devMode and nodePath overrides", async () => {
+    await expect(
+      resolveDaemonInstallRuntimeInputs({
+        env: {},
+        runtime: "node",
+        devMode: false,
+        nodePath: "/custom/node",
+      }),
+    ).resolves.toEqual({
+      devMode: false,
+      nodePath: "/custom/node",
+    });
+  });
+});
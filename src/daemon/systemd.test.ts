wonglaitung/openclaw_mini

fix(ci): stabilize detect-secrets baseline

src/daemon/systemd.test.ts
@@ -601,7 +601,7 @@ describe("readSystemdServiceExecStart", () => {
     });
     expect(command?.environmentValueSources).toEqual({
       OPENCLAW_GATEWAY_TOKEN: "file",
-      OPENCLAW_GATEWAY_PASSWORD: "file",
+      OPENCLAW_GATEWAY_PASSWORD: "file", // pragma: allowlist secret
     });
   });
 });

remoteclaw/remoteclaw

(cherry picked from commit 08597e817d04b3c2fdfce9f58e29e47b4ad26d45)

src/daemon/systemd.test.ts
@@ -604,7 +604,7 @@ describe("readSystemdServiceExecStart", () => {
     });
     expect(command?.environmentValueSources).toEqual({
       REMOTECLAW_GATEWAY_TOKEN: "file",
-      REMOTECLAW_GATEWAY_PASSWORD: "file",
+      REMOTECLAW_GATEWAY_PASSWORD: "file", // pragma: allowlist secret
     });
   });
 });
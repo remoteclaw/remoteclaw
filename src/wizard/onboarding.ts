wonglaitung/openclaw_mini

fix(onboard): avoid persisting talk fallback on fresh setup

src/wizard/onboarding.ts
@@ -81,7 +81,7 @@ export async function runOnboardingWizard(
   await requireRiskAcknowledgement({ opts, prompter });
 
   const snapshot = await readConfigFileSnapshot();
-  let baseConfig: OpenClawConfig = snapshot.valid ? snapshot.config : {};
+  let baseConfig: OpenClawConfig = snapshot.valid ? (snapshot.exists ? snapshot.config : {}) : {};
 
   if (snapshot.exists && !snapshot.valid) {
     await prompter.note(onboardHelpers.summarizeExistingConfig(baseConfig), "Invalid config");

remoteclaw/remoteclaw

(cherry picked from commit 7217b9765833f4e9bc1c7b6c10218be8f9ad9e50)

src/wizard/onboarding.ts
@@ -331,7 +331,7 @@ export async function runOnboardingWizard(
   }
 
   const snapshot = await readConfigFileSnapshot();
-  let baseConfig: RemoteClawConfig = snapshot.valid ? snapshot.config : {};
+  let baseConfig: RemoteClawConfig = snapshot.valid ? (snapshot.exists ? snapshot.config : {}) : {};
 
   if (snapshot.exists && !snapshot.valid) {
     await prompter.note(onboardHelpers.summarizeExistingConfig(baseConfig), "Invalid config");
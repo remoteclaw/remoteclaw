wonglaitung/openclaw_mini

fix(mattermost): pass mediaLocalRoots through reply delivery (#44021)
Merged via squash.

Prepared head SHA: 856f11f129f7d6a4bc8f23e8d13c786ecb871f52
Co-authored-by: LyleLiu666 <31182860+LyleLiu666@users.noreply.github.com>
Co-authored-by: mukhtharcm <56378562+mukhtharcm@users.noreply.github.com>
Reviewed-by: @mukhtharcm

extensions/mattermost/src/mattermost/reply-delivery.test.ts
@@ -1,0 +1,95 @@
+import fs from "node:fs/promises";
+import os from "node:os";
+import path from "node:path";
+import type { OpenClawConfig } from "openclaw/plugin-sdk/mattermost";
+import { describe, expect, it, vi } from "vitest";
+import { deliverMattermostReplyPayload } from "./reply-delivery.js";
+
+describe("deliverMattermostReplyPayload", () => {
+  it("passes agent-scoped mediaLocalRoots when sending media paths", async () => {
+    const previousStateDir = process.env.OPENCLAW_STATE_DIR;
+    const stateDir = await fs.mkdtemp(path.join(os.tmpdir(), "openclaw-mm-state-"));
+    process.env.OPENCLAW_STATE_DIR = stateDir;
+
+    try {
+      const sendMessage = vi.fn(async () => undefined);
+      const core = {
+        channel: {
+          text: {
+            convertMarkdownTables: vi.fn((text: string) => text),
+            resolveChunkMode: vi.fn(() => "length"),
+            chunkMarkdownTextWithMode: vi.fn((text: string) => [text]),
+          },
+        },
+      } as any;
+
+      const agentId = "agent-1";
+      const mediaUrl = `file://${path.join(stateDir, `workspace-${agentId}`, "photo.png")}`;
+      const cfg = {} satisfies OpenClawConfig;
+
+      await deliverMattermostReplyPayload({
+        core,
+        cfg,
+        payload: { text: "caption", mediaUrl },
+        to: "channel:town-square",
+        accountId: "default",
+        agentId,
+        replyToId: "root-post",
+        textLimit: 4000,
+        tableMode: "off",
+        sendMessage,
+      });
+
+      expect(sendMessage).toHaveBeenCalledTimes(1);
+      expect(sendMessage).toHaveBeenCalledWith(
+        "channel:town-square",
+        "caption",
+        expect.objectContaining({
+          accountId: "default",
+          mediaUrl,
+          replyToId: "root-post",
+          mediaLocalRoots: expect.arrayContaining([path.join(stateDir, `workspace-${agentId}`)]),
+        }),
+      );
+    } finally {
+      if (previousStateDir === undefined) {
+        delete process.env.OPENCLAW_STATE_DIR;
+      } else {
+        process.env.OPENCLAW_STATE_DIR = previousStateDir;
+      }
+      await fs.rm(stateDir, { recursive: true, force: true });
+    }
+  });
+
+  it("forwards replyToId for text-only chunked replies", async () => {
+    const sendMessage = vi.fn(async () => undefined);
+    const core = {
+      channel: {
+        text: {
+          convertMarkdownTables: vi.fn((text: string) => text),
+          resolveChunkMode: vi.fn(() => "length"),
+          chunkMarkdownTextWithMode: vi.fn(() => ["hello"]),
+        },
+      },
+    } as any;
+
+    await deliverMattermostReplyPayload({
+      core,
+      cfg: {} satisfies OpenClawConfig,
+      payload: { text: "hello" },
+      to: "channel:town-square",
+      accountId: "default",
+      agentId: "agent-1",
+      replyToId: "root-post",
+      textLimit: 4000,
+      tableMode: "off",
+      sendMessage,
+    });
+
+    expect(sendMessage).toHaveBeenCalledTimes(1);
+    expect(sendMessage).toHaveBeenCalledWith("channel:town-square", "hello", {
+      accountId: "default",
+      replyToId: "root-post",
+    });
+  });
+});

extensions/mattermost/src/mattermost/reply-delivery.ts
@@ -1,0 +1,71 @@
+import type { OpenClawConfig, PluginRuntime, ReplyPayload } from "openclaw/plugin-sdk/mattermost";
+import { getAgentScopedMediaLocalRoots } from "openclaw/plugin-sdk/mattermost";
+
+type MarkdownTableMode = Parameters<PluginRuntime["channel"]["text"]["convertMarkdownTables"]>[1];
+
+type SendMattermostMessage = (
+  to: string,
+  text: string,
+  opts: {
+    accountId?: string;
+    mediaUrl?: string;
+    mediaLocalRoots?: readonly string[];
+    replyToId?: string;
+  },
+) => Promise<unknown>;
+
+export async function deliverMattermostReplyPayload(params: {
+  core: PluginRuntime;
+  cfg: OpenClawConfig;
+  payload: ReplyPayload;
+  to: string;
+  accountId: string;
+  agentId?: string;
+  replyToId?: string;
+  textLimit: number;
+  tableMode: MarkdownTableMode;
+  sendMessage: SendMattermostMessage;
+}): Promise<void> {
+  const mediaUrls =
+    params.payload.mediaUrls ?? (params.payload.mediaUrl ? [params.payload.mediaUrl] : []);
+  const text = params.core.channel.text.convertMarkdownTables(
+    params.payload.text ?? "",
+    params.tableMode,
+  );
+
+  if (mediaUrls.length === 0) {
+    const chunkMode = params.core.channel.text.resolveChunkMode(
+      params.cfg,
+      "mattermost",
+      params.accountId,
+    );
+    const chunks = params.core.channel.text.chunkMarkdownTextWithMode(
+      text,
+      params.textLimit,
+      chunkMode,
+    );
+    for (const chunk of chunks.length > 0 ? chunks : [text]) {
+      if (!chunk) {
+        continue;
+      }
+      await params.sendMessage(params.to, chunk, {
+        accountId: params.accountId,
+        replyToId: params.replyToId,
+      });
+    }
+    return;
+  }
+
+  const mediaLocalRoots = getAgentScopedMediaLocalRoots(params.cfg, params.agentId);
+  let first = true;
+  for (const mediaUrl of mediaUrls) {
+    const caption = first ? text : "";
+    first = false;
+    await params.sendMessage(params.to, caption, {
+      accountId: params.accountId,
+      mediaUrl,
+      mediaLocalRoots,
+      replyToId: params.replyToId,
+    });
+  }
+}

remoteclaw/remoteclaw

(cherry picked from commit c965049dc69c1e436939f5ae228e8f6fc9355528)

extensions/mattermost/src/mattermost/reply-delivery.test.ts
@@ -1,0 +1,95 @@
+import fs from "node:fs/promises";
+import os from "node:os";
+import path from "node:path";
+import type { RemoteClawConfig } from "remoteclaw/plugin-sdk/mattermost";
+import { describe, expect, it, vi } from "vitest";
+import { deliverMattermostReplyPayload } from "./reply-delivery.js";
+
+describe("deliverMattermostReplyPayload", () => {
+  it("passes agent-scoped mediaLocalRoots when sending media paths", async () => {
+    const previousStateDir = process.env.REMOTECLAW_STATE_DIR;
+    const stateDir = await fs.mkdtemp(path.join(os.tmpdir(), "remoteclaw-mm-state-"));
+    process.env.REMOTECLAW_STATE_DIR = stateDir;
+
+    try {
+      const sendMessage = vi.fn(async () => undefined);
+      const core = {
+        channel: {
+          text: {
+            convertMarkdownTables: vi.fn((text: string) => text),
+            resolveChunkMode: vi.fn(() => "length"),
+            chunkMarkdownTextWithMode: vi.fn((text: string) => [text]),
+          },
+        },
+      } as any;
+
+      const agentId = "agent-1";
+      const mediaUrl = `file://${path.join(stateDir, `workspace-${agentId}`, "photo.png")}`;
+      const cfg = {} satisfies RemoteClawConfig;
+
+      await deliverMattermostReplyPayload({
+        core,
+        cfg,
+        payload: { text: "caption", mediaUrl },
+        to: "channel:town-square",
+        accountId: "default",
+        agentId,
+        replyToId: "root-post",
+        textLimit: 4000,
+        tableMode: "off",
+        sendMessage,
+      });
+
+      expect(sendMessage).toHaveBeenCalledTimes(1);
+      expect(sendMessage).toHaveBeenCalledWith(
+        "channel:town-square",
+        "caption",
+        expect.objectContaining({
+          accountId: "default",
+          mediaUrl,
+          replyToId: "root-post",
+          mediaLocalRoots: expect.arrayContaining([path.join(stateDir, `workspace-${agentId}`)]),
+        }),
+      );
+    } finally {
+      if (previousStateDir === undefined) {
+        delete process.env.REMOTECLAW_STATE_DIR;
+      } else {
+        process.env.REMOTECLAW_STATE_DIR = previousStateDir;
+      }
+      await fs.rm(stateDir, { recursive: true, force: true });
+    }
+  });
+
+  it("forwards replyToId for text-only chunked replies", async () => {
+    const sendMessage = vi.fn(async () => undefined);
+    const core = {
+      channel: {
+        text: {
+          convertMarkdownTables: vi.fn((text: string) => text),
+          resolveChunkMode: vi.fn(() => "length"),
+          chunkMarkdownTextWithMode: vi.fn(() => ["hello"]),
+        },
+      },
+    } as any;
+
+    await deliverMattermostReplyPayload({
+      core,
+      cfg: {} satisfies RemoteClawConfig,
+      payload: { text: "hello" },
+      to: "channel:town-square",
+      accountId: "default",
+      agentId: "agent-1",
+      replyToId: "root-post",
+      textLimit: 4000,
+      tableMode: "off",
+      sendMessage,
+    });
+
+    expect(sendMessage).toHaveBeenCalledTimes(1);
+    expect(sendMessage).toHaveBeenCalledWith("channel:town-square", "hello", {
+      accountId: "default",
+      replyToId: "root-post",
+    });
+  });
+});

extensions/mattermost/src/mattermost/reply-delivery.ts
@@ -1,0 +1,75 @@
+import type {
+  RemoteClawConfig,
+  PluginRuntime,
+  ReplyPayload,
+} from "remoteclaw/plugin-sdk/mattermost";
+import { getAgentScopedMediaLocalRoots } from "remoteclaw/plugin-sdk/mattermost";
+
+type MarkdownTableMode = Parameters<PluginRuntime["channel"]["text"]["convertMarkdownTables"]>[1];
+
+type SendMattermostMessage = (
+  to: string,
+  text: string,
+  opts: {
+    accountId?: string;
+    mediaUrl?: string;
+    mediaLocalRoots?: readonly string[];
+    replyToId?: string;
+  },
+) => Promise<unknown>;
+
+export async function deliverMattermostReplyPayload(params: {
+  core: PluginRuntime;
+  cfg: RemoteClawConfig;
+  payload: ReplyPayload;
+  to: string;
+  accountId: string;
+  agentId?: string;
+  replyToId?: string;
+  textLimit: number;
+  tableMode: MarkdownTableMode;
+  sendMessage: SendMattermostMessage;
+}): Promise<void> {
+  const mediaUrls =
+    params.payload.mediaUrls ?? (params.payload.mediaUrl ? [params.payload.mediaUrl] : []);
+  const text = params.core.channel.text.convertMarkdownTables(
+    params.payload.text ?? "",
+    params.tableMode,
+  );
+
+  if (mediaUrls.length === 0) {
+    const chunkMode = params.core.channel.text.resolveChunkMode(
+      params.cfg,
+      "mattermost",
+      params.accountId,
+    );
+    const chunks = params.core.channel.text.chunkMarkdownTextWithMode(
+      text,
+      params.textLimit,
+      chunkMode,
+    );
+    for (const chunk of chunks.length > 0 ? chunks : [text]) {
+      if (!chunk) {
+        continue;
+      }
+      await params.sendMessage(params.to, chunk, {
+        accountId: params.accountId,
+        replyToId: params.replyToId,
+      });
+    }
+    return;
+  }
+
+  const mediaLocalRoots = getAgentScopedMediaLocalRoots(params.cfg, params.agentId);
+  let first = true;
+  for (const mediaUrl of mediaUrls) {
+    const caption = first ? text : "";
+    first = false;
+    await params.sendMessage(params.to, caption, {
+      accountId: params.accountId,
+      mediaUrl,
+      mediaLocalRoots,
+      replyToId: params.replyToId,
+    });
+  }
+}
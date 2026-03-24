wonglaitung/openclaw_mini

refactor(plugin-sdk): use scoped core imports for bundled channels

src/plugin-sdk/subpaths.test.ts
@@ -1,5 +1,6 @@
 import * as extensionApi from "openclaw/extension-api";
 import * as compatSdk from "openclaw/plugin-sdk/compat";
+import * as coreSdk from "openclaw/plugin-sdk/core";
 import * as discordSdk from "openclaw/plugin-sdk/discord";
 import * as imessageSdk from "openclaw/plugin-sdk/imessage";
 import * as lineSdk from "openclaw/plugin-sdk/line";
@@ -23,6 +24,11 @@ describe("plugin-sdk subpath exports", () => {
   it("exports compat helpers", () => {
     expect(typeof compatSdk.emptyPluginConfigSchema).toBe("function");
     expect(typeof compatSdk.resolveControlCommandGate).toBe("function");
+  });
+
+  it("exports core routing helpers", () => {
+    expect(typeof coreSdk.buildAgentSessionKey).toBe("function");
+    expect(typeof coreSdk.resolveThreadSessionKeys).toBe("function");
   });
 
   it("exports Discord helpers", () => {

remoteclaw/remoteclaw

(cherry picked from commit 99c501a9a797cd0e1b8376ce3bc291869d1d4c86)

src/plugin-sdk/subpaths.test.ts
@@ -1,4 +1,5 @@
 import * as compatSdk from "remoteclaw/plugin-sdk/compat";
+import * as coreSdk from "remoteclaw/plugin-sdk/core";
 import * as discordSdk from "remoteclaw/plugin-sdk/discord";
 import * as imessageSdk from "remoteclaw/plugin-sdk/imessage";
 import * as lineSdk from "remoteclaw/plugin-sdk/line";
@@ -50,6 +51,11 @@ describe("plugin-sdk subpath exports", () => {
   it("exports compat helpers", () => {
     expect(typeof compatSdk.emptyPluginConfigSchema).toBe("function");
     expect(typeof compatSdk.resolveControlCommandGate).toBe("function");
+  });
+
+  it("exports core routing helpers", () => {
+    expect(typeof coreSdk.buildAgentSessionKey).toBe("function");
+    expect(typeof coreSdk.resolveThreadSessionKeys).toBe("function");
   });
 
   it("exports Discord helpers", () => {
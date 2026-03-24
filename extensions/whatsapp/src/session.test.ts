wonglaitung/openclaw_mini

refactor: move WhatsApp channel implementation to extensions/ (#45725)
* refactor: move WhatsApp channel from src/web/ to extensions/whatsapp/

Move all WhatsApp implementation code (77 source/test files + 9 channel
plugin files) from src/web/ and src/channels/plugins/*/whatsapp* to
extensions/whatsapp/src/.

- Leave thin re-export shims at all original locations so cross-cutting
  imports continue to resolve
- Update plugin-sdk/whatsapp.ts to only re-export generic framework
  utilities; channel-specific functions imported locally by the extension
- Update vi.mock paths in 15 cross-cutting test files
- Rename outbound.ts -> send.ts to match extension naming conventions
  and avoid false positive in cfg-threading guard test
- Widen tsconfig.plugin-sdk.dts.json rootDir to support shim->extension
  cross-directory references

Part of the core-channels-to-extensions migration (PR 6/10).

* style: format WhatsApp extension files

* fix: correct stale import paths in WhatsApp extension tests

Fix vi.importActual, test mock, and hardcoded source paths that weren't
updated during the file move:
- media.test.ts: vi.importActual path
- onboarding.test.ts: vi.importActual path
- test-helpers.ts: test/mocks/baileys.js path
- monitor-inbox.test-harness.ts: incomplete media/store mock
- login.test.ts: hardcoded source file path
- message-action-runner.media.test.ts: vi.mock/importActual path

extensions/whatsapp/src/session.test.ts
@@ -1,0 +1,228 @@
+import { EventEmitter } from "node:events";
+import fsSync from "node:fs";
+import path from "node:path";
+import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
+import { resetLogger, setLoggerOverride } from "../../../src/logging.js";
+import { baileys, getLastSocket, resetBaileysMocks, resetLoadConfigMock } from "./test-helpers.js";
+
+const { createWaSocket, formatError, logWebSelfId, waitForWaConnection } =
+  await import("./session.js");
+const useMultiFileAuthStateMock = vi.mocked(baileys.useMultiFileAuthState);
+
+async function flushCredsUpdate() {
+  await new Promise<void>((resolve) => setImmediate(resolve));
+}
+
+async function emitCredsUpdateAndReadSaveCreds() {
+  const sock = getLastSocket();
+  const saveCreds = (await useMultiFileAuthStateMock.mock.results[0]?.value)?.saveCreds;
+  sock.ev.emit("creds.update", {});
+  await flushCredsUpdate();
+  return saveCreds;
+}
+
+function mockCredsJsonSpies(readContents: string) {
+  const credsSuffix = path.join(".openclaw", "credentials", "whatsapp", "default", "creds.json");
+  const copySpy = vi.spyOn(fsSync, "copyFileSync").mockImplementation(() => {});
+  const existsSpy = vi.spyOn(fsSync, "existsSync").mockImplementation((p) => {
+    if (typeof p !== "string") {
+      return false;
+    }
+    return p.endsWith(credsSuffix);
+  });
+  const statSpy = vi.spyOn(fsSync, "statSync").mockImplementation((p) => {
+    if (typeof p === "string" && p.endsWith(credsSuffix)) {
+      return { isFile: () => true, size: 12 } as never;
+    }
+    throw new Error(`unexpected statSync path: ${String(p)}`);
+  });
+  const readSpy = vi.spyOn(fsSync, "readFileSync").mockImplementation((p) => {
+    if (typeof p === "string" && p.endsWith(credsSuffix)) {
+      return readContents as never;
+    }
+    throw new Error(`unexpected readFileSync path: ${String(p)}`);
+  });
+  return {
+    copySpy,
+    credsSuffix,
+    restore: () => {
+      copySpy.mockRestore();
+      existsSpy.mockRestore();
+      statSpy.mockRestore();
+      readSpy.mockRestore();
+    },
+  };
+}
+
+describe("web session", () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+    resetBaileysMocks();
+    resetLoadConfigMock();
+  });
+
+  afterEach(() => {
+    resetLogger();
+    setLoggerOverride(null);
+    vi.useRealTimers();
+  });
+
+  it("creates WA socket with QR handler", async () => {
+    await createWaSocket(true, false);
+    const makeWASocket = baileys.makeWASocket as ReturnType<typeof vi.fn>;
+    expect(makeWASocket).toHaveBeenCalledWith(
+      expect.objectContaining({ printQRInTerminal: false }),
+    );
+    const passed = makeWASocket.mock.calls[0][0];
+    const passedLogger = (passed as { logger?: { level?: string; trace?: unknown } }).logger;
+    expect(passedLogger?.level).toBe("silent");
+    expect(typeof passedLogger?.trace).toBe("function");
+    const sock = getLastSocket();
+    const saveCreds = (await useMultiFileAuthStateMock.mock.results[0]?.value)?.saveCreds;
+    // trigger creds.update listener
+    sock.ev.emit("creds.update", {});
+    await flushCredsUpdate();
+    expect(saveCreds).toHaveBeenCalled();
+  });
+
+  it("waits for connection open", async () => {
+    const ev = new EventEmitter();
+    const promise = waitForWaConnection({ ev } as unknown as ReturnType<
+      typeof baileys.makeWASocket
+    >);
+    ev.emit("connection.update", { connection: "open" });
+    await expect(promise).resolves.toBeUndefined();
+  });
+
+  it("rejects when connection closes", async () => {
+    const ev = new EventEmitter();
+    const promise = waitForWaConnection({ ev } as unknown as ReturnType<
+      typeof baileys.makeWASocket
+    >);
+    ev.emit("connection.update", {
+      connection: "close",
+      lastDisconnect: new Error("bye"),
+    });
+    await expect(promise).rejects.toBeInstanceOf(Error);
+  });
+
+  it("logWebSelfId prints cached E.164 when creds exist", () => {
+    const existsSpy = vi.spyOn(fsSync, "existsSync").mockImplementation((p) => {
+      if (typeof p !== "string") {
+        return false;
+      }
+      return p.endsWith("creds.json");
+    });
+    const readSpy = vi.spyOn(fsSync, "readFileSync").mockImplementation((p) => {
+      if (typeof p === "string" && p.endsWith("creds.json")) {
+        return JSON.stringify({ me: { id: "12345@s.whatsapp.net" } });
+      }
+      throw new Error(`unexpected readFileSync path: ${String(p)}`);
+    });
+    const runtime = {
+      log: vi.fn(),
+      error: vi.fn(),
+      exit: vi.fn(),
+    };
+
+    logWebSelfId("/tmp/wa-creds", runtime as never, true);
+
+    expect(runtime.log).toHaveBeenCalledWith(
+      expect.stringContaining("Web Channel: +12345 (jid 12345@s.whatsapp.net)"),
+    );
+    existsSpy.mockRestore();
+    readSpy.mockRestore();
+  });
+
+  it("formatError prints Boom-like payload message", () => {
+    const err = {
+      error: {
+        isBoom: true,
+        output: {
+          statusCode: 408,
+          payload: {
+            statusCode: 408,
+            error: "Request Time-out",
+            message: "QR refs attempts ended",
+          },
+        },
+      },
+    };
+    expect(formatError(err)).toContain("status=408");
+    expect(formatError(err)).toContain("Request Time-out");
+    expect(formatError(err)).toContain("QR refs attempts ended");
+  });
+
+  it("does not clobber creds backup when creds.json is corrupted", async () => {
+    const creds = mockCredsJsonSpies("{");
+
+    await createWaSocket(false, false);
+    const saveCreds = await emitCredsUpdateAndReadSaveCreds();
+
+    expect(creds.copySpy).not.toHaveBeenCalled();
+    expect(saveCreds).toHaveBeenCalled();
+
+    creds.restore();
+  });
+
+  it("serializes creds.update saves to avoid overlapping writes", async () => {
+    let inFlight = 0;
+    let maxInFlight = 0;
+    let release: (() => void) | null = null;
+    const gate = new Promise<void>((resolve) => {
+      release = resolve;
+    });
+
+    const saveCreds = vi.fn(async () => {
+      inFlight += 1;
+      maxInFlight = Math.max(maxInFlight, inFlight);
+      await gate;
+      inFlight -= 1;
+    });
+    useMultiFileAuthStateMock.mockResolvedValueOnce({
+      state: { creds: {} as never, keys: {} as never },
+      saveCreds,
+    });
+
+    await createWaSocket(false, false);
+    const sock = getLastSocket();
+
+    sock.ev.emit("creds.update", {});
+    sock.ev.emit("creds.update", {});
+
+    await flushCredsUpdate();
+    expect(inFlight).toBe(1);
+
+    (release as (() => void) | null)?.();
+
+    // let both queued saves complete
+    await flushCredsUpdate();
+    await flushCredsUpdate();
+
+    expect(saveCreds).toHaveBeenCalledTimes(2);
+    expect(maxInFlight).toBe(1);
+    expect(inFlight).toBe(0);
+  });
+
+  it("rotates creds backup when creds.json is valid JSON", async () => {
+    const creds = mockCredsJsonSpies("{}");
+    const backupSuffix = path.join(
+      ".openclaw",
+      "credentials",
+      "whatsapp",
+      "default",
+      "creds.json.bak",
+    );
+
+    await createWaSocket(false, false);
+    const saveCreds = await emitCredsUpdateAndReadSaveCreds();
+
+    expect(creds.copySpy).toHaveBeenCalledTimes(1);
+    const args = creds.copySpy.mock.calls[0] ?? [];
+    expect(String(args[0] ?? "")).toContain(creds.credsSuffix);
+    expect(String(args[1] ?? "")).toContain(backupSuffix);
+    expect(saveCreds).toHaveBeenCalled();
+
+    creds.restore();
+  });
+});

remoteclaw/remoteclaw

(cherry picked from commit 16505718e8278e6c8dff0e5227a5cb5a9f7c56df)

extensions/whatsapp/src/session.test.ts
@@ -1,0 +1,228 @@
+import { EventEmitter } from "node:events";
+import fsSync from "node:fs";
+import path from "node:path";
+import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
+import { resetLogger, setLoggerOverride } from "../../../src/logging.js";
+import { baileys, getLastSocket, resetBaileysMocks, resetLoadConfigMock } from "./test-helpers.js";
+
+const { createWaSocket, formatError, logWebSelfId, waitForWaConnection } =
+  await import("./session.js");
+const useMultiFileAuthStateMock = vi.mocked(baileys.useMultiFileAuthState);
+
+async function flushCredsUpdate() {
+  await new Promise<void>((resolve) => setImmediate(resolve));
+}
+
+async function emitCredsUpdateAndReadSaveCreds() {
+  const sock = getLastSocket();
+  const saveCreds = (await useMultiFileAuthStateMock.mock.results[0]?.value)?.saveCreds;
+  sock.ev.emit("creds.update", {});
+  await flushCredsUpdate();
+  return saveCreds;
+}
+
+function mockCredsJsonSpies(readContents: string) {
+  const credsSuffix = path.join(".remoteclaw", "credentials", "whatsapp", "default", "creds.json");
+  const copySpy = vi.spyOn(fsSync, "copyFileSync").mockImplementation(() => {});
+  const existsSpy = vi.spyOn(fsSync, "existsSync").mockImplementation((p) => {
+    if (typeof p !== "string") {
+      return false;
+    }
+    return p.endsWith(credsSuffix);
+  });
+  const statSpy = vi.spyOn(fsSync, "statSync").mockImplementation((p) => {
+    if (typeof p === "string" && p.endsWith(credsSuffix)) {
+      return { isFile: () => true, size: 12 } as never;
+    }
+    throw new Error(`unexpected statSync path: ${String(p)}`);
+  });
+  const readSpy = vi.spyOn(fsSync, "readFileSync").mockImplementation((p) => {
+    if (typeof p === "string" && p.endsWith(credsSuffix)) {
+      return readContents as never;
+    }
+    throw new Error(`unexpected readFileSync path: ${String(p)}`);
+  });
+  return {
+    copySpy,
+    credsSuffix,
+    restore: () => {
+      copySpy.mockRestore();
+      existsSpy.mockRestore();
+      statSpy.mockRestore();
+      readSpy.mockRestore();
+    },
+  };
+}
+
+describe("web session", () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+    resetBaileysMocks();
+    resetLoadConfigMock();
+  });
+
+  afterEach(() => {
+    resetLogger();
+    setLoggerOverride(null);
+    vi.useRealTimers();
+  });
+
+  it("creates WA socket with QR handler", async () => {
+    await createWaSocket(true, false);
+    const makeWASocket = baileys.makeWASocket as ReturnType<typeof vi.fn>;
+    expect(makeWASocket).toHaveBeenCalledWith(
+      expect.objectContaining({ printQRInTerminal: false }),
+    );
+    const passed = makeWASocket.mock.calls[0][0];
+    const passedLogger = (passed as { logger?: { level?: string; trace?: unknown } }).logger;
+    expect(passedLogger?.level).toBe("silent");
+    expect(typeof passedLogger?.trace).toBe("function");
+    const sock = getLastSocket();
+    const saveCreds = (await useMultiFileAuthStateMock.mock.results[0]?.value)?.saveCreds;
+    // trigger creds.update listener
+    sock.ev.emit("creds.update", {});
+    await flushCredsUpdate();
+    expect(saveCreds).toHaveBeenCalled();
+  });
+
+  it("waits for connection open", async () => {
+    const ev = new EventEmitter();
+    const promise = waitForWaConnection({ ev } as unknown as ReturnType<
+      typeof baileys.makeWASocket
+    >);
+    ev.emit("connection.update", { connection: "open" });
+    await expect(promise).resolves.toBeUndefined();
+  });
+
+  it("rejects when connection closes", async () => {
+    const ev = new EventEmitter();
+    const promise = waitForWaConnection({ ev } as unknown as ReturnType<
+      typeof baileys.makeWASocket
+    >);
+    ev.emit("connection.update", {
+      connection: "close",
+      lastDisconnect: new Error("bye"),
+    });
+    await expect(promise).rejects.toBeInstanceOf(Error);
+  });
+
+  it("logWebSelfId prints cached E.164 when creds exist", () => {
+    const existsSpy = vi.spyOn(fsSync, "existsSync").mockImplementation((p) => {
+      if (typeof p !== "string") {
+        return false;
+      }
+      return p.endsWith("creds.json");
+    });
+    const readSpy = vi.spyOn(fsSync, "readFileSync").mockImplementation((p) => {
+      if (typeof p === "string" && p.endsWith("creds.json")) {
+        return JSON.stringify({ me: { id: "12345@s.whatsapp.net" } });
+      }
+      throw new Error(`unexpected readFileSync path: ${String(p)}`);
+    });
+    const runtime = {
+      log: vi.fn(),
+      error: vi.fn(),
+      exit: vi.fn(),
+    };
+
+    logWebSelfId("/tmp/wa-creds", runtime as never, true);
+
+    expect(runtime.log).toHaveBeenCalledWith(
+      expect.stringContaining("Web Channel: +12345 (jid 12345@s.whatsapp.net)"),
+    );
+    existsSpy.mockRestore();
+    readSpy.mockRestore();
+  });
+
+  it("formatError prints Boom-like payload message", () => {
+    const err = {
+      error: {
+        isBoom: true,
+        output: {
+          statusCode: 408,
+          payload: {
+            statusCode: 408,
+            error: "Request Time-out",
+            message: "QR refs attempts ended",
+          },
+        },
+      },
+    };
+    expect(formatError(err)).toContain("status=408");
+    expect(formatError(err)).toContain("Request Time-out");
+    expect(formatError(err)).toContain("QR refs attempts ended");
+  });
+
+  it("does not clobber creds backup when creds.json is corrupted", async () => {
+    const creds = mockCredsJsonSpies("{");
+
+    await createWaSocket(false, false);
+    const saveCreds = await emitCredsUpdateAndReadSaveCreds();
+
+    expect(creds.copySpy).not.toHaveBeenCalled();
+    expect(saveCreds).toHaveBeenCalled();
+
+    creds.restore();
+  });
+
+  it("serializes creds.update saves to avoid overlapping writes", async () => {
+    let inFlight = 0;
+    let maxInFlight = 0;
+    let release: (() => void) | null = null;
+    const gate = new Promise<void>((resolve) => {
+      release = resolve;
+    });
+
+    const saveCreds = vi.fn(async () => {
+      inFlight += 1;
+      maxInFlight = Math.max(maxInFlight, inFlight);
+      await gate;
+      inFlight -= 1;
+    });
+    useMultiFileAuthStateMock.mockResolvedValueOnce({
+      state: { creds: {} as never, keys: {} as never },
+      saveCreds,
+    });
+
+    await createWaSocket(false, false);
+    const sock = getLastSocket();
+
+    sock.ev.emit("creds.update", {});
+    sock.ev.emit("creds.update", {});
+
+    await flushCredsUpdate();
+    expect(inFlight).toBe(1);
+
+    (release as (() => void) | null)?.();
+
+    // let both queued saves complete
+    await flushCredsUpdate();
+    await flushCredsUpdate();
+
+    expect(saveCreds).toHaveBeenCalledTimes(2);
+    expect(maxInFlight).toBe(1);
+    expect(inFlight).toBe(0);
+  });
+
+  it("rotates creds backup when creds.json is valid JSON", async () => {
+    const creds = mockCredsJsonSpies("{}");
+    const backupSuffix = path.join(
+      ".remoteclaw",
+      "credentials",
+      "whatsapp",
+      "default",
+      "creds.json.bak",
+    );
+
+    await createWaSocket(false, false);
+    const saveCreds = await emitCredsUpdateAndReadSaveCreds();
+
+    expect(creds.copySpy).toHaveBeenCalledTimes(1);
+    const args = creds.copySpy.mock.calls[0] ?? [];
+    expect(String(args[0] ?? "")).toContain(creds.credsSuffix);
+    expect(String(args[1] ?? "")).toContain(backupSuffix);
+    expect(saveCreds).toHaveBeenCalled();
+
+    creds.restore();
+  });
+});
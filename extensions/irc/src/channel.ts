Nachx639/clawdbot

refactor: share account snapshot descriptions

extensions/irc/src/channel.ts
@@ -1,3 +1,4 @@
+import { describeAccountSnapshot } from "openclaw/plugin-sdk/account-helpers";
 import { formatNormalizedAllowFromEntries } from "openclaw/plugin-sdk/allow-from";
 import {
   adaptScopedAccountAccessor,
@@ -156,17 +157,18 @@ export const ircPlugin: ChannelPlugin<ResolvedIrcAccount, IrcProbe> = {
   config: {
     ...ircConfigAdapter,
     isConfigured: (account) => account.configured,
-    describeAccount: (account) => ({
-      accountId: account.accountId,
-      name: account.name,
-      enabled: account.enabled,
-      configured: account.configured,
-      host: account.host,
-      port: account.port,
-      tls: account.tls,
-      nick: account.nick,
-      passwordSource: account.passwordSource,
-    }),
+    describeAccount: (account) =>
+      describeAccountSnapshot({
+        account,
+        configured: account.configured,
+        extra: {
+          host: account.host,
+          port: account.port,
+          tls: account.tls,
+          nick: account.nick,
+          passwordSource: account.passwordSource,
+        },
+      }),
   },
   security: {
     resolveDmPolicy: resolveIrcDmPolicy,

extensions/mattermost/src/channel.ts
@@ -1,3 +1,4 @@
+import { describeAccountSnapshot } from "openclaw/plugin-sdk/account-helpers";
 import { formatNormalizedAllowFromEntries } from "openclaw/plugin-sdk/allow-from";
 import { createMessageToolButtonsSchema } from "openclaw/plugin-sdk/channel-actions";
 import {
@@ -328,14 +329,15 @@ export const mattermostPlugin: ChannelPlugin<ResolvedMattermostAccount> = {
   config: {
     ...mattermostConfigAdapter,
     isConfigured: (account) => Boolean(account.botToken && account.baseUrl),
-    describeAccount: (account) => ({
-      accountId: account.accountId,
-      name: account.name,
-      enabled: account.enabled,
-      configured: Boolean(account.botToken && account.baseUrl),
-      botTokenSource: account.botTokenSource,
-      baseUrl: account.baseUrl,
-    }),
+    describeAccount: (account) =>
+      describeAccountSnapshot({
+        account,
+        configured: Boolean(account.botToken && account.baseUrl),
+        extra: {
+          botTokenSource: account.botTokenSource,
+          baseUrl: account.baseUrl,
+        },
+      }),
   },
   security: {
     resolveDmPolicy: resolveMattermostDmPolicy,

extensions/msteams/src/channel.ts
@@ -1,3 +1,4 @@
+import { describeAccountSnapshot } from "openclaw/plugin-sdk/account-helpers";
 import { formatAllowFromLowercase } from "openclaw/plugin-sdk/allow-from";
 import { createMessageToolCardSchema } from "openclaw/plugin-sdk/channel-actions";
 import { createTopLevelChannelConfigAdapter } from "openclaw/plugin-sdk/channel-config-helpers";
@@ -183,11 +184,11 @@ export const msteamsPlugin: ChannelPlugin<ResolvedMSTeamsAccount> = {
   config: {
     ...msteamsConfigAdapter,
     isConfigured: (_account, cfg) => Boolean(resolveMSTeamsCredentials(cfg.channels?.msteams)),
-    describeAccount: (account) => ({
-      accountId: account.accountId,
-      enabled: account.enabled,
-      configured: account.configured,
-    }),
+    describeAccount: (account) =>
+      describeAccountSnapshot({
+        account,
+        configured: account.configured,
+      }),
   },
   security: {
     collectWarnings: projectWarningCollector(

remoteclaw/remoteclaw

(cherry picked from commit 00b2f10dec41435079d18997a90a6349d1f12996)

extensions/irc/src/channel.ts
@@ -1,3 +1,5 @@
+import { describeAccountSnapshot } from "remoteclaw/plugin-sdk/account-helpers";
+import { formatNormalizedAllowFromEntries } from "remoteclaw/plugin-sdk/allow-from";
 import {
   buildAccountScopedDmSecurityPolicy,
   buildOpenGroupPolicyWarning,
@@ -116,17 +118,18 @@ export const ircPlugin: ChannelPlugin<ResolvedIrcAccount, IrcProbe> = {
         ],
       }),
     isConfigured: (account) => account.configured,
-    describeAccount: (account) => ({
-      accountId: account.accountId,
-      name: account.name,
-      enabled: account.enabled,
-      configured: account.configured,
-      host: account.host,
-      port: account.port,
-      tls: account.tls,
-      nick: account.nick,
-      passwordSource: account.passwordSource,
-    }),
+    describeAccount: (account) =>
+      describeAccountSnapshot({
+        account,
+        configured: account.configured,
+        extra: {
+          host: account.host,
+          port: account.port,
+          tls: account.tls,
+          nick: account.nick,
+          passwordSource: account.passwordSource,
+        },
+      }),
     ...ircConfigAccessors,
   },
   security: {

extensions/mattermost/src/channel.ts
@@ -1,3 +1,6 @@
+import { describeAccountSnapshot } from "remoteclaw/plugin-sdk/account-helpers";
+import { formatNormalizedAllowFromEntries } from "remoteclaw/plugin-sdk/allow-from";
+import { createMessageToolButtonsSchema } from "remoteclaw/plugin-sdk/channel-actions";
 import {
   buildAccountScopedDmSecurityPolicy,
   collectAllowlistProviderRestrictSendersWarnings,
@@ -290,14 +293,15 @@ export const mattermostPlugin: ChannelPlugin<ResolvedMattermostAccount> = {
         clearBaseFields: ["botToken", "baseUrl", "name"],
       }),
     isConfigured: (account) => Boolean(account.botToken && account.baseUrl),
-    describeAccount: (account) => ({
-      accountId: account.accountId,
-      name: account.name,
-      enabled: account.enabled,
-      configured: Boolean(account.botToken && account.baseUrl),
-      botTokenSource: account.botTokenSource,
-      baseUrl: account.baseUrl,
-    }),
+    describeAccount: (account) =>
+      describeAccountSnapshot({
+        account,
+        configured: Boolean(account.botToken && account.baseUrl),
+        extra: {
+          botTokenSource: account.botTokenSource,
+          baseUrl: account.baseUrl,
+        },
+      }),
     ...mattermostConfigAccessors,
   },
   security: {

extensions/msteams/src/channel.ts
@@ -1,4 +1,9 @@
 import { collectAllowlistProviderRestrictSendersWarnings } from "remoteclaw/plugin-sdk";
+
+import { describeAccountSnapshot } from "remoteclaw/plugin-sdk/account-helpers";
+import { formatAllowFromLowercase } from "remoteclaw/plugin-sdk/allow-from";
+import { createMessageToolCardSchema } from "remoteclaw/plugin-sdk/channel-actions";
+import { createTopLevelChannelConfigAdapter } from "remoteclaw/plugin-sdk/channel-config-helpers";
 import type {
   ChannelMessageActionName,
   ChannelPlugin,
@@ -119,11 +124,11 @@ export const msteamsPlugin: ChannelPlugin<ResolvedMSTeamsAccount> = {
       return next;
     },
     isConfigured: (_account, cfg) => Boolean(resolveMSTeamsCredentials(cfg.channels?.msteams)),
-    describeAccount: (account) => ({
-      accountId: account.accountId,
-      enabled: account.enabled,
-      configured: account.configured,
-    }),
+    describeAccount: (account) =>
+      describeAccountSnapshot({
+        account,
+        configured: account.configured,
+      }),
     resolveAllowFrom: ({ cfg }) => cfg.channels?.msteams?.allowFrom ?? [],
     formatAllowFrom: ({ allowFrom }) => formatAllowFromLowercase({ allowFrom }),
     resolveDefaultTo: ({ cfg }) => cfg.channels?.msteams?.defaultTo?.trim() || undefined,
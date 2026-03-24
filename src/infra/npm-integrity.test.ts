wonglaitung/openclaw_mini

test: tighten warning and npm integrity coverage

src/infra/npm-integrity.test.ts
@@ -6,23 +6,34 @@ import {
 
 describe("resolveNpmIntegrityDrift", () => {
   it("returns proceed=true when integrity is missing or unchanged", async () => {
-    await expect(
-      resolveNpmIntegrityDrift({
-        spec: "@openclaw/test@1.0.0",
-        expectedIntegrity: "sha512-same",
+    const createPayload = vi.fn(() => "unused");
+    const cases = [
+      {
+        expectedIntegrity: undefined,
         resolution: { integrity: "sha512-same", resolvedAt: "2026-01-01T00:00:00.000Z" },
-        createPayload: () => "unused",
-      }),
-    ).resolves.toEqual({ proceed: true });
-
-    await expect(
-      resolveNpmIntegrityDrift({
-        spec: "@openclaw/test@1.0.0",
+      },
+      {
         expectedIntegrity: "sha512-same",
         resolution: { resolvedAt: "2026-01-01T00:00:00.000Z" },
-        createPayload: () => "unused",
-      }),
-    ).resolves.toEqual({ proceed: true });
+      },
+      {
+        expectedIntegrity: "sha512-same",
+        resolution: { integrity: "sha512-same", resolvedAt: "2026-01-01T00:00:00.000Z" },
+      },
+    ];
+
+    for (const testCase of cases) {
+      await expect(
+        resolveNpmIntegrityDrift({
+          spec: "@openclaw/test@1.0.0",
+          expectedIntegrity: testCase.expectedIntegrity,
+          resolution: testCase.resolution,
+          createPayload,
+        }),
+      ).resolves.toEqual({ proceed: true });
+    }
+
+    expect(createPayload).not.toHaveBeenCalled();
   });
 
   it("uses callback on integrity drift", async () => {
@@ -49,6 +60,31 @@ describe("resolveNpmIntegrityDrift", () => {
     expect(result.integrityDrift).toEqual({
       expectedIntegrity: "sha512-old",
       actualIntegrity: "sha512-new",
+    });
+  });
+
+  it("returns payload when the drift callback allows continuing", async () => {
+    const result = await resolveNpmIntegrityDrift({
+      spec: "@openclaw/test@1.0.0",
+      expectedIntegrity: "sha512-old",
+      resolution: {
+        integrity: "sha512-new",
+        resolvedAt: "2026-01-01T00:00:00.000Z",
+      },
+      createPayload: ({ spec, actualIntegrity }) => ({ spec, actualIntegrity }),
+      onIntegrityDrift: async () => true,
+    });
+
+    expect(result).toEqual({
+      integrityDrift: {
+        expectedIntegrity: "sha512-old",
+        actualIntegrity: "sha512-new",
+      },
+      payload: {
+        spec: "@openclaw/test@1.0.0",
+        actualIntegrity: "sha512-new",
+      },
+      proceed: true,
     });
   });
 
@@ -98,6 +134,24 @@ describe("resolveNpmIntegrityDrift", () => {
     });
     expect(abortResult.error).toBe(
       "aborted: npm package integrity drift detected for @openclaw/test@1.0.0",
+    );
+  });
+
+  it("falls back to the original spec when resolvedSpec is missing", async () => {
+    const warn = vi.fn();
+
+    await resolveNpmIntegrityDriftWithDefaultMessage({
+      spec: "@openclaw/test@1.0.0",
+      expectedIntegrity: "sha512-old",
+      resolution: {
+        integrity: "sha512-new",
+        resolvedAt: "2026-01-01T00:00:00.000Z",
+      },
+      warn,
+    });
+
+    expect(warn).toHaveBeenCalledWith(
+      "Integrity drift detected for @openclaw/test@1.0.0: expected sha512-old, got sha512-new",
     );
   });
 });

remoteclaw/remoteclaw

(cherry picked from commit e928f5553755bde2805a83a06db818c2281ca1d6)

src/infra/npm-integrity.test.ts
@@ -6,23 +6,34 @@ import {
 
 describe("resolveNpmIntegrityDrift", () => {
   it("returns proceed=true when integrity is missing or unchanged", async () => {
-    await expect(
-      resolveNpmIntegrityDrift({
-        spec: "@remoteclaw/test@1.0.0",
-        expectedIntegrity: "sha512-same",
+    const createPayload = vi.fn(() => "unused");
+    const cases = [
+      {
+        expectedIntegrity: undefined,
         resolution: { integrity: "sha512-same", resolvedAt: "2026-01-01T00:00:00.000Z" },
-        createPayload: () => "unused",
-      }),
-    ).resolves.toEqual({ proceed: true });
-
-    await expect(
-      resolveNpmIntegrityDrift({
-        spec: "@remoteclaw/test@1.0.0",
+      },
+      {
         expectedIntegrity: "sha512-same",
         resolution: { resolvedAt: "2026-01-01T00:00:00.000Z" },
-        createPayload: () => "unused",
-      }),
-    ).resolves.toEqual({ proceed: true });
+      },
+      {
+        expectedIntegrity: "sha512-same",
+        resolution: { integrity: "sha512-same", resolvedAt: "2026-01-01T00:00:00.000Z" },
+      },
+    ];
+
+    for (const testCase of cases) {
+      await expect(
+        resolveNpmIntegrityDrift({
+          spec: "@remoteclaw/test@1.0.0",
+          expectedIntegrity: testCase.expectedIntegrity,
+          resolution: testCase.resolution,
+          createPayload,
+        }),
+      ).resolves.toEqual({ proceed: true });
+    }
+
+    expect(createPayload).not.toHaveBeenCalled();
   });
 
   it("uses callback on integrity drift", async () => {
@@ -49,6 +60,31 @@ describe("resolveNpmIntegrityDrift", () => {
     expect(result.integrityDrift).toEqual({
       expectedIntegrity: "sha512-old",
       actualIntegrity: "sha512-new",
+    });
+  });
+
+  it("returns payload when the drift callback allows continuing", async () => {
+    const result = await resolveNpmIntegrityDrift({
+      spec: "@remoteclaw/test@1.0.0",
+      expectedIntegrity: "sha512-old",
+      resolution: {
+        integrity: "sha512-new",
+        resolvedAt: "2026-01-01T00:00:00.000Z",
+      },
+      createPayload: ({ spec, actualIntegrity }) => ({ spec, actualIntegrity }),
+      onIntegrityDrift: async () => true,
+    });
+
+    expect(result).toEqual({
+      integrityDrift: {
+        expectedIntegrity: "sha512-old",
+        actualIntegrity: "sha512-new",
+      },
+      payload: {
+        spec: "@remoteclaw/test@1.0.0",
+        actualIntegrity: "sha512-new",
+      },
+      proceed: true,
     });
   });
 
@@ -98,6 +134,24 @@ describe("resolveNpmIntegrityDrift", () => {
     });
     expect(abortResult.error).toBe(
       "aborted: npm package integrity drift detected for @remoteclaw/test@1.0.0",
+    );
+  });
+
+  it("falls back to the original spec when resolvedSpec is missing", async () => {
+    const warn = vi.fn();
+
+    await resolveNpmIntegrityDriftWithDefaultMessage({
+      spec: "@remoteclaw/test@1.0.0",
+      expectedIntegrity: "sha512-old",
+      resolution: {
+        integrity: "sha512-new",
+        resolvedAt: "2026-01-01T00:00:00.000Z",
+      },
+      warn,
+    });
+
+    expect(warn).toHaveBeenCalledWith(
+      "Integrity drift detected for @remoteclaw/test@1.0.0: expected sha512-old, got sha512-new",
     );
   });
 });
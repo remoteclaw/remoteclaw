wonglaitung/openclaw_mini

fix(ci): repair security and route test fixtures

src/security/audit.test.ts
@@ -1803,7 +1803,14 @@ description: test skill
   });
 
   it("warns when multiple DM senders share the main session", async () => {
-    const cfg: OpenClawConfig = { session: { dmScope: "main" } };
+    const cfg: OpenClawConfig = {
+      session: { dmScope: "main" },
+      channels: {
+        whatsapp: {
+          enabled: true,
+        },
+      },
+    };
     const plugins: ChannelPlugin[] = [
       {
         id: "whatsapp",

remoteclaw/remoteclaw

(cherry picked from commit 3f12e90f3e42fda423ce59e4e328277447a63856)

src/security/audit.test.ts
@@ -1163,7 +1163,14 @@ describe("security audit", () => {
   });
 
   it("warns when multiple DM senders share the main session", async () => {
-    const cfg: RemoteClawConfig = { session: { dmScope: "main" } };
+    const cfg: RemoteClawConfig = {
+      session: { dmScope: "main" },
+      channels: {
+        whatsapp: {
+          enabled: true,
+        },
+      },
+    };
     const plugins: ChannelPlugin[] = [
       {
         id: "whatsapp",
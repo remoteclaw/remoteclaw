wonglaitung/openclaw_mini

test: clarify system presence version precedence

src/infra/system-presence.version.test.ts
@@ -46,6 +46,28 @@ describe("system-presence version fallback", () => {
     );
   });
 
+  it("still prefers runtime VERSION over OPENCLAW_SERVICE_VERSION when OPENCLAW_VERSION is blank", async () => {
+    await expectSelfVersion(
+      {
+        OPENCLAW_VERSION: " ",
+        OPENCLAW_SERVICE_VERSION: "2.4.6-service",
+        npm_package_version: "1.0.0-package",
+      },
+      async () => (await import("../version.js")).VERSION,
+    );
+  });
+
+  it("still prefers runtime VERSION over npm_package_version when service markers are blank", async () => {
+    await expectSelfVersion(
+      {
+        OPENCLAW_VERSION: " ",
+        OPENCLAW_SERVICE_VERSION: "\t",
+        npm_package_version: "1.0.0-package",
+      },
+      async () => (await import("../version.js")).VERSION,
+    );
+  });
+
   it("uses runtime VERSION when OPENCLAW_VERSION and OPENCLAW_SERVICE_VERSION are blank", async () => {
     await expectSelfVersion(
       {

remoteclaw/remoteclaw

(cherry picked from commit 6a545c04eb2e685c67e617d8c13f4e6f97688261)

src/infra/system-presence.version.test.ts
@@ -41,6 +41,36 @@ describe("system-presence version fallback", () => {
     );
   });
 
+  it("still prefers runtime VERSION over REMOTECLAW_SERVICE_VERSION when REMOTECLAW_VERSION is blank", async () => {
+    await withPresenceModule(
+      {
+        REMOTECLAW_VERSION: " ",
+        REMOTECLAW_SERVICE_VERSION: "2.4.6-service",
+        npm_package_version: "1.0.0-package",
+      },
+      async ({ listSystemPresence }) => {
+        const { VERSION } = await import("../version.js");
+        const selfEntry = listSystemPresence().find((entry) => entry.reason === "self");
+        expect(selfEntry?.version).toBe(VERSION);
+      },
+    );
+  });
+
+  it("still prefers runtime VERSION over npm_package_version when service markers are blank", async () => {
+    await withPresenceModule(
+      {
+        REMOTECLAW_VERSION: " ",
+        REMOTECLAW_SERVICE_VERSION: "\t",
+        npm_package_version: "1.0.0-package",
+      },
+      async ({ listSystemPresence }) => {
+        const { VERSION } = await import("../version.js");
+        const selfEntry = listSystemPresence().find((entry) => entry.reason === "self");
+        expect(selfEntry?.version).toBe(VERSION);
+      },
+    );
+  });
+
   it("uses runtime VERSION when REMOTECLAW_VERSION and REMOTECLAW_SERVICE_VERSION are blank", async () => {
     await withPresenceModule(
       {
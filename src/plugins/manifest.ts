wonglaitung/openclaw_mini

Gateway: gate deferred channel startup behind opt-in

src/plugins/manifest.ts
@@ -242,11 +242,20 @@ export type PluginPackageInstall = {
   defaultChoice?: "npm" | "local";
 };
 
+export type OpenClawPackageStartup = {
+  /**
+   * Opt-in for channel plugins whose `setupEntry` fully covers the gateway
+   * startup surface needed before the server starts listening.
+   */
+  deferConfiguredChannelFullLoadUntilAfterListen?: boolean;
+};
+
 export type OpenClawPackageManifest = {
   extensions?: string[];
   setupEntry?: string;
   channel?: PluginPackageChannel;
   install?: PluginPackageInstall;
+  startup?: OpenClawPackageStartup;
 };
 
 export const DEFAULT_PLUGIN_ENTRY_CANDIDATES = [

remoteclaw/remoteclaw

(cherry picked from commit 96ed010a37d372246c702ad2cb0e63210a51d017)

src/plugins/manifest.ts
@@ -139,10 +139,19 @@ export type PluginPackageInstall = {
   defaultChoice?: "npm" | "local";
 };
 
+export type RemoteClawPackageStartup = {
+  /**
+   * Opt-in for channel plugins whose `setupEntry` fully covers the gateway
+   * startup surface needed before the server starts listening.
+   */
+  deferConfiguredChannelFullLoadUntilAfterListen?: boolean;
+};
+
 export type RemoteClawPackageManifest = {
   extensions?: string[];
   channel?: PluginPackageChannel;
   install?: PluginPackageInstall;
+  startup?: RemoteClawPackageStartup;
 };
 
 export const DEFAULT_PLUGIN_ENTRY_CANDIDATES = [
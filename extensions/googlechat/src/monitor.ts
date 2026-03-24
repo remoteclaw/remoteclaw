wonglaitung/openclaw_mini

refactor: deduplicate reply payload helpers

extensions/googlechat/src/monitor.ts
@@ -1,4 +1,5 @@
 import type { IncomingMessage, ServerResponse } from "node:http";
+import { deliverTextOrMediaReply } from "openclaw/plugin-sdk/reply-payload";
 import type { OpenClawConfig } from "../runtime-api.js";
 import {
   createWebhookInFlightLimiter,
@@ -375,14 +376,12 @@ async function deliverGoogleChatReply(params: {
 }): Promise<void> {
   const { payload, account, spaceId, runtime, core, config, statusSink, typingMessageName } =
     params;
-  const mediaList = payload.mediaUrls?.length
-    ? payload.mediaUrls
-    : payload.mediaUrl
-      ? [payload.mediaUrl]
-      : [];
+  const hasMedia = Boolean(payload.mediaUrls?.length) || Boolean(payload.mediaUrl);
+  const text = payload.text ?? "";
+  let firstTextChunk = true;
+  let suppressCaption = false;
 
-  if (mediaList.length > 0) {
-    let suppressCaption = false;
+  if (hasMedia) {
     if (typingMessageName) {
       try {
         await deleteGoogleChatMessage({
@@ -391,9 +390,10 @@ async function deliverGoogleChatReply(params: {
         });
       } catch (err) {
         runtime.error?.(`Google Chat typing cleanup failed: ${String(err)}`);
-        const fallbackText = payload.text?.trim()
-          ? payload.text
-          : mediaList.length > 1
+        const mediaCount = payload.mediaUrls?.length ?? (payload.mediaUrl ? 1 : 0);
+        const fallbackText = text.trim()
+          ? text
+          : mediaCount > 1
             ? "Sent attachments."
             : "Sent attachment.";
         try {
@@ -402,16 +402,43 @@ async function deliverGoogleChatReply(params: {
             messageName: typingMessageName,
             text: fallbackText,
           });
-          suppressCaption = Boolean(payload.text?.trim());
+          suppressCaption = Boolean(text.trim());
         } catch (updateErr) {
           runtime.error?.(`Google Chat typing update failed: ${String(updateErr)}`);
         }
       }
     }
-    let first = true;
-    for (const mediaUrl of mediaList) {
-      const caption = first && !suppressCaption ? payload.text : undefined;
-      first = false;
+  }
+
+  const chunkLimit = account.config.textChunkLimit ?? 4000;
+  const chunkMode = core.channel.text.resolveChunkMode(config, "googlechat", account.accountId);
+  await deliverTextOrMediaReply({
+    payload,
+    text: suppressCaption ? "" : text,
+    chunkText: (value) => core.channel.text.chunkMarkdownTextWithMode(value, chunkLimit, chunkMode),
+    sendText: async (chunk) => {
+      try {
+        if (firstTextChunk && typingMessageName) {
+          await updateGoogleChatMessage({
+            account,
+            messageName: typingMessageName,
+            text: chunk,
+          });
+        } else {
+          await sendGoogleChatMessage({
+            account,
+            space: spaceId,
+            text: chunk,
+            thread: payload.replyToId,
+          });
+        }
+        firstTextChunk = false;
+        statusSink?.({ lastOutboundAt: Date.now() });
+      } catch (err) {
+        runtime.error?.(`Google Chat message send failed: ${String(err)}`);
+      }
+    },
+    sendMedia: async ({ mediaUrl, caption }) => {
       try {
         const loaded = await core.channel.media.fetchRemoteMedia({
           url: mediaUrl,
@@ -440,38 +467,8 @@ async function deliverGoogleChatReply(params: {
       } catch (err) {
         runtime.error?.(`Google Chat attachment send failed: ${String(err)}`);
       }
-    }
-    return;
-  }
-
-  if (payload.text) {
-    const chunkLimit = account.config.textChunkLimit ?? 4000;
-    const chunkMode = core.channel.text.resolveChunkMode(config, "googlechat", account.accountId);
-    const chunks = core.channel.text.chunkMarkdownTextWithMode(payload.text, chunkLimit, chunkMode);
-    for (let i = 0; i < chunks.length; i++) {
-      const chunk = chunks[i];
-      try {
-        // Edit typing message with first chunk if available
-        if (i === 0 && typingMessageName) {
-          await updateGoogleChatMessage({
-            account,
-            messageName: typingMessageName,
-            text: chunk,
-          });
-        } else {
-          await sendGoogleChatMessage({
-            account,
-            space: spaceId,
-            text: chunk,
-            thread: payload.replyToId,
-          });
-        }
-        statusSink?.({ lastOutboundAt: Date.now() });
-      } catch (err) {
-        runtime.error?.(`Google Chat message send failed: ${String(err)}`);
-      }
-    }
-  }
+    },
+  });
 }
 
 async function uploadAttachmentForReply(params: {

extensions/imessage/src/channel.ts
@@ -1,5 +1,8 @@
 import { buildDmGroupAccountAllowlistAdapter } from "openclaw/plugin-sdk/allowlist-config-edit";
-import { resolveOutboundSendDep } from "openclaw/plugin-sdk/channel-runtime";
+import {
+  createAttachedChannelResultAdapter,
+  resolveOutboundSendDep,
+} from "openclaw/plugin-sdk/channel-runtime";
 import { buildOutboundBaseSessionKey } from "openclaw/plugin-sdk/core";
 import { createLazyRuntimeModule } from "openclaw/plugin-sdk/lazy-runtime";
 import { type RoutePeer } from "openclaw/plugin-sdk/routing";
@@ -160,34 +163,33 @@ export const imessagePlugin: ChannelPlugin<ResolvedIMessageAccount> = {
     chunker: (text, limit) => getIMessageRuntime().channel.text.chunkText(text, limit),
     chunkerMode: "text",
     textChunkLimit: 4000,
-    sendText: async ({ cfg, to, text, accountId, deps, replyToId }) => {
-      const result = await (
-        await loadIMessageChannelRuntime()
-      ).sendIMessageOutbound({
-        cfg,
-        to,
-        text,
-        accountId: accountId ?? undefined,
-        deps,
-        replyToId: replyToId ?? undefined,
-      });
-      return { channel: "imessage", ...result };
-    },
-    sendMedia: async ({ cfg, to, text, mediaUrl, mediaLocalRoots, accountId, deps, replyToId }) => {
-      const result = await (
-        await loadIMessageChannelRuntime()
-      ).sendIMessageOutbound({
-        cfg,
-        to,
-        text,
-        mediaUrl,
-        mediaLocalRoots,
-        accountId: accountId ?? undefined,
-        deps,
-        replyToId: replyToId ?? undefined,
-      });
-      return { channel: "imessage", ...result };
-    },
+    ...createAttachedChannelResultAdapter({
+      channel: "imessage",
+      sendText: async ({ cfg, to, text, accountId, deps, replyToId }) =>
+        await (
+          await loadIMessageChannelRuntime()
+        ).sendIMessageOutbound({
+          cfg,
+          to,
+          text,
+          accountId: accountId ?? undefined,
+          deps,
+          replyToId: replyToId ?? undefined,
+        }),
+      sendMedia: async ({ cfg, to, text, mediaUrl, mediaLocalRoots, accountId, deps, replyToId }) =>
+        await (
+          await loadIMessageChannelRuntime()
+        ).sendIMessageOutbound({
+          cfg,
+          to,
+          text,
+          mediaUrl,
+          mediaLocalRoots,
+          accountId: accountId ?? undefined,
+          deps,
+          replyToId: replyToId ?? undefined,
+        }),
+    }),
   },
   status: {
     defaultRuntime: {

extensions/discord/src/send.shared.ts
@@ -17,6 +17,7 @@ import {
   normalizePollInput,
   type PollInput,
 } from "openclaw/plugin-sdk/media-runtime";
+import { resolveTextChunksWithFallback } from "openclaw/plugin-sdk/reply-payload";
 import type { ChunkMode } from "openclaw/plugin-sdk/reply-runtime";
 import { loadWebMedia } from "openclaw/plugin-sdk/web-media";
 import { resolveDiscordAccount } from "./accounts.js";
@@ -276,10 +277,7 @@ export function buildDiscordTextChunks(
     maxLines: opts.maxLinesPerMessage,
     chunkMode: opts.chunkMode,
   });
-  if (!chunks.length && text) {
-    chunks.push(text);
-  }
-  return chunks;
+  return resolveTextChunksWithFallback(text, chunks);
 }
 
 function hasV2Components(components?: TopLevelComponents[]): boolean {

remoteclaw/remoteclaw

(cherry picked from commit 8d73bc77fa5d4eb733891efd8bbca5a5d14d9d58)

extensions/googlechat/src/monitor.ts
@@ -1,5 +1,6 @@
 import type { IncomingMessage, ServerResponse } from "node:http";
-import type { RemoteClawConfig } from "remoteclaw/plugin-sdk";
+import { deliverTextOrMediaReply } from "remoteclaw/plugin-sdk/reply-payload";
+import type { RemoteClawConfig } from "../runtime-api.js";
 import {
   GROUP_POLICY_BLOCKED_LABEL,
   createScopedPairingAccess,
@@ -836,14 +837,12 @@ async function deliverGoogleChatReply(params: {
 }): Promise<void> {
   const { payload, account, spaceId, runtime, core, config, statusSink, typingMessageName } =
     params;
-  const mediaList = payload.mediaUrls?.length
-    ? payload.mediaUrls
-    : payload.mediaUrl
-      ? [payload.mediaUrl]
-      : [];
+  const hasMedia = Boolean(payload.mediaUrls?.length) || Boolean(payload.mediaUrl);
+  const text = payload.text ?? "";
+  let firstTextChunk = true;
+  let suppressCaption = false;
 
-  if (mediaList.length > 0) {
-    let suppressCaption = false;
+  if (hasMedia) {
     if (typingMessageName) {
       try {
         await deleteGoogleChatMessage({
@@ -852,9 +851,10 @@ async function deliverGoogleChatReply(params: {
         });
       } catch (err) {
         runtime.error?.(`Google Chat typing cleanup failed: ${String(err)}`);
-        const fallbackText = payload.text?.trim()
-          ? payload.text
-          : mediaList.length > 1
+        const mediaCount = payload.mediaUrls?.length ?? (payload.mediaUrl ? 1 : 0);
+        const fallbackText = text.trim()
+          ? text
+          : mediaCount > 1
             ? "Sent attachments."
             : "Sent attachment.";
         try {
@@ -863,16 +863,43 @@ async function deliverGoogleChatReply(params: {
             messageName: typingMessageName,
             text: fallbackText,
           });
-          suppressCaption = Boolean(payload.text?.trim());
+          suppressCaption = Boolean(text.trim());
         } catch (updateErr) {
           runtime.error?.(`Google Chat typing update failed: ${String(updateErr)}`);
         }
       }
     }
-    let first = true;
-    for (const mediaUrl of mediaList) {
-      const caption = first && !suppressCaption ? payload.text : undefined;
-      first = false;
+  }
+
+  const chunkLimit = account.config.textChunkLimit ?? 4000;
+  const chunkMode = core.channel.text.resolveChunkMode(config, "googlechat", account.accountId);
+  await deliverTextOrMediaReply({
+    payload,
+    text: suppressCaption ? "" : text,
+    chunkText: (value) => core.channel.text.chunkMarkdownTextWithMode(value, chunkLimit, chunkMode),
+    sendText: async (chunk) => {
+      try {
+        if (firstTextChunk && typingMessageName) {
+          await updateGoogleChatMessage({
+            account,
+            messageName: typingMessageName,
+            text: chunk,
+          });
+        } else {
+          await sendGoogleChatMessage({
+            account,
+            space: spaceId,
+            text: chunk,
+            thread: payload.replyToId,
+          });
+        }
+        firstTextChunk = false;
+        statusSink?.({ lastOutboundAt: Date.now() });
+      } catch (err) {
+        runtime.error?.(`Google Chat message send failed: ${String(err)}`);
+      }
+    },
+    sendMedia: async ({ mediaUrl, caption }) => {
       try {
         const loaded = await core.channel.media.fetchRemoteMedia({
           url: mediaUrl,
@@ -901,38 +928,8 @@ async function deliverGoogleChatReply(params: {
       } catch (err) {
         runtime.error?.(`Google Chat attachment send failed: ${String(err)}`);
       }
-    }
-    return;
-  }
-
-  if (payload.text) {
-    const chunkLimit = account.config.textChunkLimit ?? 4000;
-    const chunkMode = core.channel.text.resolveChunkMode(config, "googlechat", account.accountId);
-    const chunks = core.channel.text.chunkMarkdownTextWithMode(payload.text, chunkLimit, chunkMode);
-    for (let i = 0; i < chunks.length; i++) {
-      const chunk = chunks[i];
-      try {
-        // Edit typing message with first chunk if available
-        if (i === 0 && typingMessageName) {
-          await updateGoogleChatMessage({
-            account,
-            messageName: typingMessageName,
-            text: chunk,
-          });
-        } else {
-          await sendGoogleChatMessage({
-            account,
-            space: spaceId,
-            text: chunk,
-            thread: payload.replyToId,
-          });
-        }
-        statusSink?.({ lastOutboundAt: Date.now() });
-      } catch (err) {
-        runtime.error?.(`Google Chat message send failed: ${String(err)}`);
-      }
-    }
-  }
+    },
+  });
 }
 
 async function uploadAttachmentForReply(params: {

extensions/imessage/src/channel.ts
@@ -1,5 +1,8 @@
 import { buildDmGroupAccountAllowlistAdapter } from "remoteclaw/plugin-sdk/allowlist-config-edit";
-import { resolveOutboundSendDep } from "remoteclaw/plugin-sdk/channel-runtime";
+import {
+  createAttachedChannelResultAdapter,
+  resolveOutboundSendDep,
+} from "remoteclaw/plugin-sdk/channel-runtime";
 import { buildOutboundBaseSessionKey } from "remoteclaw/plugin-sdk/core";
 import { createLazyRuntimeModule } from "remoteclaw/plugin-sdk/lazy-runtime";
 import { type RoutePeer } from "remoteclaw/plugin-sdk/routing";
@@ -220,30 +223,33 @@ export const imessagePlugin: ChannelPlugin<ResolvedIMessageAccount> = {
     chunker: (text, limit) => getIMessageRuntime().channel.text.chunkText(text, limit),
     chunkerMode: "text",
     textChunkLimit: 4000,
-    sendText: async ({ cfg, to, text, accountId, deps, replyToId }) => {
-      const result = await sendIMessageOutbound({
-        cfg,
-        to,
-        text,
-        accountId: accountId ?? undefined,
-        deps,
-        replyToId: replyToId ?? undefined,
-      });
-      return { channel: "imessage", ...result };
-    },
-    sendMedia: async ({ cfg, to, text, mediaUrl, mediaLocalRoots, accountId, deps, replyToId }) => {
-      const result = await sendIMessageOutbound({
-        cfg,
-        to,
-        text,
-        mediaUrl,
-        mediaLocalRoots,
-        accountId: accountId ?? undefined,
-        deps,
-        replyToId: replyToId ?? undefined,
-      });
-      return { channel: "imessage", ...result };
-    },
+    ...createAttachedChannelResultAdapter({
+      channel: "imessage",
+      sendText: async ({ cfg, to, text, accountId, deps, replyToId }) =>
+        await (
+          await loadIMessageChannelRuntime()
+        ).sendIMessageOutbound({
+          cfg,
+          to,
+          text,
+          accountId: accountId ?? undefined,
+          deps,
+          replyToId: replyToId ?? undefined,
+        }),
+      sendMedia: async ({ cfg, to, text, mediaUrl, mediaLocalRoots, accountId, deps, replyToId }) =>
+        await (
+          await loadIMessageChannelRuntime()
+        ).sendIMessageOutbound({
+          cfg,
+          to,
+          text,
+          mediaUrl,
+          mediaLocalRoots,
+          accountId: accountId ?? undefined,
+          deps,
+          replyToId: replyToId ?? undefined,
+        }),
+    }),
   },
   status: {
     defaultRuntime: {

src/discord/send.shared.ts
@@ -17,6 +17,7 @@ import {
   normalizePollInput,
   type PollInput,
 } from "remoteclaw/plugin-sdk/media-runtime";
+import { resolveTextChunksWithFallback } from "remoteclaw/plugin-sdk/reply-payload";
 import type { ChunkMode } from "remoteclaw/plugin-sdk/reply-runtime";
 import { loadWebMedia } from "remoteclaw/plugin-sdk/web-media";
 import { resolveDiscordAccount } from "./accounts.js";
@@ -276,10 +277,7 @@ export function buildDiscordTextChunks(
     maxLines: opts.maxLinesPerMessage,
     chunkMode: opts.chunkMode,
   });
-  if (!chunks.length && text) {
-    chunks.push(text);
-  }
-  return chunks;
+  return resolveTextChunksWithFallback(text, chunks);
 }
 
 function hasV2Components(components?: TopLevelComponents[]): boolean {
Nachx639/clawdbot

refactor: adopt shared account merge helpers

extensions/bluebubbles/src/accounts.ts
@@ -1,4 +1,4 @@
-import { createAccountListHelpers } from "openclaw/plugin-sdk/account-helpers";
+import { createAccountListHelpers, mergeAccountConfig } from "openclaw/plugin-sdk/account-helpers";
 import { normalizeAccountId } from "openclaw/plugin-sdk/account-id";
 import type { OpenClawConfig } from "openclaw/plugin-sdk/core";
 import { hasConfiguredSecretInput, normalizeSecretInputString } from "./secret-input.js";
@@ -34,14 +34,14 @@ function mergeBlueBubblesAccountConfig(
   cfg: OpenClawConfig,
   accountId: string,
 ): BlueBubblesAccountConfig {
-  const base = (cfg.channels?.bluebubbles ?? {}) as BlueBubblesAccountConfig & {
-    accounts?: unknown;
-    defaultAccount?: unknown;
-  };
-  const { accounts: _ignored, defaultAccount: _ignoredDefaultAccount, ...rest } = base;
   const account = resolveAccountConfig(cfg, accountId) ?? {};
-  const chunkMode = account.chunkMode ?? rest.chunkMode ?? "length";
-  return { ...rest, ...account, chunkMode };
+  const merged = mergeAccountConfig<BlueBubblesAccountConfig>({
+    channelConfig: cfg.channels?.bluebubbles as BlueBubblesAccountConfig | undefined,
+    accountConfig: account,
+    omitKeys: ["defaultAccount"],
+  });
+  const chunkMode = account.chunkMode ?? merged.chunkMode ?? "length";
+  return { ...merged, chunkMode };
 }
 
 export function resolveBlueBubblesAccount(params: {

extensions/googlechat/src/accounts.ts
@@ -1,4 +1,4 @@
-import { createAccountListHelpers } from "openclaw/plugin-sdk/account-helpers";
+import { createAccountListHelpers, mergeAccountConfig } from "openclaw/plugin-sdk/account-helpers";
 import { DEFAULT_ACCOUNT_ID, normalizeAccountId } from "openclaw/plugin-sdk/account-id";
 import { isSecretRef, type OpenClawConfig } from "openclaw/plugin-sdk/core";
 import type { GoogleChatAccountConfig } from "./types.config.js";
@@ -40,7 +40,11 @@ function mergeGoogleChatAccountConfig(
   accountId: string,
 ): GoogleChatAccountConfig {
   const raw = cfg.channels?.["googlechat"] ?? {};
-  const { accounts: _ignored, defaultAccount: _ignored2, ...base } = raw;
+  const base = mergeAccountConfig<GoogleChatAccountConfig>({
+    channelConfig: raw as GoogleChatAccountConfig,
+    accountConfig: undefined,
+    omitKeys: ["defaultAccount"],
+  });
   const defaultAccountConfig = resolveAccountConfig(cfg, DEFAULT_ACCOUNT_ID) ?? {};
   const account = resolveAccountConfig(cfg, accountId) ?? {};
   if (accountId === DEFAULT_ACCOUNT_ID) {

extensions/irc/src/accounts.ts
@@ -1,4 +1,4 @@
-import { createAccountListHelpers } from "openclaw/plugin-sdk/account-helpers";
+import { createAccountListHelpers, mergeAccountConfig } from "openclaw/plugin-sdk/account-helpers";
 import { DEFAULT_ACCOUNT_ID, normalizeAccountId } from "openclaw/plugin-sdk/account-id";
 import { parseOptionalDelimitedEntries } from "openclaw/plugin-sdk/core";
 import { tryReadSecretFileSync } from "openclaw/plugin-sdk/infra-runtime";
@@ -60,19 +60,16 @@ function resolveAccountConfig(cfg: CoreConfig, accountId: string): IrcAccountCon
 }
 
 function mergeIrcAccountConfig(cfg: CoreConfig, accountId: string): IrcAccountConfig {
-  const {
-    accounts: _ignored,
-    defaultAccount: _ignoredDefaultAccount,
-    ...base
-  } = (cfg.channels?.irc ?? {}) as IrcAccountConfig & {
-    accounts?: unknown;
-    defaultAccount?: unknown;
-  };
   const account = resolveAccountConfig(cfg, accountId) ?? {};
-  const merged: IrcAccountConfig = { ...base, ...account };
-  if (base.nickserv || account.nickserv) {
+  const merged: IrcAccountConfig = mergeAccountConfig<IrcAccountConfig>({
+    channelConfig: cfg.channels?.irc as IrcAccountConfig | undefined,
+    accountConfig: account,
+    omitKeys: ["defaultAccount"],
+  });
+  const baseNickServ = (cfg.channels?.irc as IrcAccountConfig | undefined)?.nickserv;
+  if (baseNickServ || account.nickserv) {
     merged.nickserv = {
-      ...base.nickserv,
+      ...baseNickServ,
       ...account.nickserv,
     };
   }

extensions/mattermost/src/mattermost/accounts.ts
@@ -1,4 +1,5 @@
 import { DEFAULT_ACCOUNT_ID, normalizeAccountId } from "openclaw/plugin-sdk/account-id";
+import { mergeAccountConfig, resolveAccountEntry } from "openclaw/plugin-sdk/account-resolution";
 import { createAccountListHelpers, type OpenClawConfig } from "../runtime-api.js";
 import { normalizeResolvedSecretInputString, normalizeSecretInputString } from "../secret-input.js";
 import type {
@@ -39,36 +40,27 @@ function resolveAccountConfig(
   cfg: OpenClawConfig,
   accountId: string,
 ): MattermostAccountConfig | undefined {
-  const accounts = cfg.channels?.mattermost?.accounts;
-  if (!accounts || typeof accounts !== "object") {
-    return undefined;
-  }
-  return accounts[accountId] as MattermostAccountConfig | undefined;
+  return resolveAccountEntry(cfg.channels?.mattermost?.accounts, accountId);
 }
 
 function mergeMattermostAccountConfig(
   cfg: OpenClawConfig,
   accountId: string,
 ): MattermostAccountConfig {
-  const {
-    accounts: _ignored,
-    defaultAccount: _ignoredDefaultAccount,
-    ...base
-  } = (cfg.channels?.mattermost ?? {}) as MattermostAccountConfig & {
-    accounts?: unknown;
-    defaultAccount?: unknown;
-  };
   const account = resolveAccountConfig(cfg, accountId) ?? {};
+  const merged = mergeAccountConfig<MattermostAccountConfig>({
+    channelConfig: cfg.channels?.mattermost as MattermostAccountConfig | undefined,
+    accountConfig: account,
+    omitKeys: ["defaultAccount"],
+  });
 
   // Shallow merging is fine for most keys, but `commands` should be merged
   // so that account-specific overrides (callbackPath/callbackUrl) do not
   // accidentally reset global settings like `native: true`.
   const mergedCommands = {
-    ...(base.commands ?? {}),
+    ...((cfg.channels?.mattermost as MattermostAccountConfig | undefined)?.commands ?? {}),
     ...(account.commands ?? {}),
   };
-
-  const merged = { ...base, ...account };
   if (Object.keys(mergedCommands).length > 0) {
     merged.commands = mergedCommands;
   }

extensions/nextcloud-talk/src/accounts.ts
@@ -1,3 +1,4 @@
+import { mergeAccountConfig } from "openclaw/plugin-sdk/account-resolution";
 import { tryReadSecretFileSync } from "openclaw/plugin-sdk/infra-runtime";
 import {
   createAccountListHelpers,
@@ -64,16 +65,11 @@ function mergeNextcloudTalkAccountConfig(
   cfg: CoreConfig,
   accountId: string,
 ): NextcloudTalkAccountConfig {
-  const {
-    accounts: _ignored,
-    defaultAccount: _ignoredDefaultAccount,
-    ...base
-  } = (cfg.channels?.["nextcloud-talk"] ?? {}) as NextcloudTalkAccountConfig & {
-    accounts?: unknown;
-    defaultAccount?: unknown;
-  };
-  const account = resolveAccountConfig(cfg, accountId) ?? {};
-  return { ...base, ...account };
+  return mergeAccountConfig<NextcloudTalkAccountConfig>({
+    channelConfig: cfg.channels?.["nextcloud-talk"] as NextcloudTalkAccountConfig | undefined,
+    accountConfig: resolveAccountConfig(cfg, accountId),
+    omitKeys: ["defaultAccount"],
+  });
 }
 
 function resolveNextcloudTalkSecret(

remoteclaw/remoteclaw

(cherry picked from commit 017d295edb6f9535601866c83aa26cc162b4f9b2)

extensions/bluebubbles/src/accounts.ts
@@ -1,8 +1,5 @@
-import {
-  createAccountListHelpers,
-  normalizeAccountId,
-  resolveMergedAccountConfig,
-} from "remoteclaw/plugin-sdk/account-resolution";
+import { createAccountListHelpers, mergeAccountConfig } from "remoteclaw/plugin-sdk/account-helpers";
+import { normalizeAccountId } from "remoteclaw/plugin-sdk/account-id";
 import type { RemoteClawConfig } from "remoteclaw/plugin-sdk/core";
 import { hasConfiguredSecretInput, normalizeSecretInputString } from "./secret-input.js";
 import { normalizeBlueBubblesServerUrl, type BlueBubblesAccountConfig } from "./types.js";
@@ -26,15 +23,14 @@ function mergeBlueBubblesAccountConfig(
   cfg: RemoteClawConfig,
   accountId: string,
 ): BlueBubblesAccountConfig {
-  const merged = resolveMergedAccountConfig<BlueBubblesAccountConfig>({
+  const account = resolveAccountConfig(cfg, accountId) ?? {};
+  const merged = mergeAccountConfig<BlueBubblesAccountConfig>({
     channelConfig: cfg.channels?.bluebubbles as BlueBubblesAccountConfig | undefined,
-    accounts: cfg.channels?.bluebubbles?.accounts as
-      | Record<string, Partial<BlueBubblesAccountConfig>>
-      | undefined,
-    accountId,
+    accountConfig: account,
     omitKeys: ["defaultAccount"],
   });
-  return { ...merged, chunkMode: merged.chunkMode ?? "length" };
+  const chunkMode = account.chunkMode ?? merged.chunkMode ?? "length";
+  return { ...merged, chunkMode };
 }
 
 export function resolveBlueBubblesAccount(params: {

extensions/googlechat/src/accounts.ts
@@ -1,10 +1,5 @@
-import {
-  createAccountListHelpers,
-  DEFAULT_ACCOUNT_ID,
-  normalizeAccountId,
-  resolveAccountEntry,
-  resolveMergedAccountConfig,
-} from "remoteclaw/plugin-sdk/account-resolution";
+import { createAccountListHelpers, mergeAccountConfig } from "remoteclaw/plugin-sdk/account-helpers";
+import { DEFAULT_ACCOUNT_ID, normalizeAccountId } from "remoteclaw/plugin-sdk/account-id";
 import { isSecretRef, type RemoteClawConfig } from "remoteclaw/plugin-sdk/core";
 import type { GoogleChatAccountConfig } from "./types.config.js";
 
@@ -34,17 +29,13 @@ function mergeGoogleChatAccountConfig(
   accountId: string,
 ): GoogleChatAccountConfig {
   const raw = cfg.channels?.["googlechat"] ?? {};
-  const base = resolveMergedAccountConfig<GoogleChatAccountConfig>({
+  const base = mergeAccountConfig<GoogleChatAccountConfig>({
     channelConfig: raw as GoogleChatAccountConfig,
-    accounts: raw.accounts as Record<string, Partial<GoogleChatAccountConfig>> | undefined,
-    accountId,
+    accountConfig: undefined,
     omitKeys: ["defaultAccount"],
   });
-  const defaultAccountConfig =
-    resolveAccountEntry(
-      raw.accounts as Record<string, GoogleChatAccountConfig> | undefined,
-      DEFAULT_ACCOUNT_ID,
-    ) ?? {};
+  const defaultAccountConfig = resolveAccountConfig(cfg, DEFAULT_ACCOUNT_ID) ?? {};
+  const account = resolveAccountConfig(cfg, accountId) ?? {};
   if (accountId === DEFAULT_ACCOUNT_ID) {
     return base;
   }

extensions/irc/src/accounts.ts
@@ -1,6 +1,8 @@
-import { readFileSync } from "node:fs";
+import { createAccountListHelpers, mergeAccountConfig } from "remoteclaw/plugin-sdk/account-helpers";
 import { DEFAULT_ACCOUNT_ID, normalizeAccountId } from "remoteclaw/plugin-sdk/account-id";
-import { createAccountListHelpers } from "remoteclaw/plugin-sdk/irc";
+import { parseOptionalDelimitedEntries } from "remoteclaw/plugin-sdk/core";
+import { tryReadSecretFileSync } from "remoteclaw/plugin-sdk/infra-runtime";
+import { normalizeResolvedSecretInputString } from "remoteclaw/plugin-sdk/secret-input";
 import type { CoreConfig, IrcAccountConfig, IrcNickServConfig } from "./types.js";
 
 const TRUTHY_ENV = new Set(["true", "1", "yes", "on"]);
@@ -69,19 +71,16 @@ function resolveAccountConfig(cfg: CoreConfig, accountId: string): IrcAccountCon
 }
 
 function mergeIrcAccountConfig(cfg: CoreConfig, accountId: string): IrcAccountConfig {
-  const {
-    accounts: _ignored,
-    defaultAccount: _ignoredDefaultAccount,
-    ...base
-  } = (cfg.channels?.irc ?? {}) as IrcAccountConfig & {
-    accounts?: unknown;
-    defaultAccount?: unknown;
-  };
   const account = resolveAccountConfig(cfg, accountId) ?? {};
-  const merged: IrcAccountConfig = { ...base, ...account };
-  if (base.nickserv || account.nickserv) {
+  const merged: IrcAccountConfig = mergeAccountConfig<IrcAccountConfig>({
+    channelConfig: cfg.channels?.irc as IrcAccountConfig | undefined,
+    accountConfig: account,
+    omitKeys: ["defaultAccount"],
+  });
+  const baseNickServ = (cfg.channels?.irc as IrcAccountConfig | undefined)?.nickserv;
+  if (baseNickServ || account.nickserv) {
     merged.nickserv = {
-      ...base.nickserv,
+      ...baseNickServ,
       ...account.nickserv,
     };
   }

extensions/mattermost/src/mattermost/accounts.ts
@@ -1,5 +1,7 @@
 import { DEFAULT_ACCOUNT_ID, normalizeAccountId } from "remoteclaw/plugin-sdk/account-id";
-import { createAccountListHelpers, type RemoteClawConfig } from "remoteclaw/plugin-sdk/mattermost";
+import { mergeAccountConfig, resolveAccountEntry } from "remoteclaw/plugin-sdk/account-resolution";
+import { createAccountListHelpers, type RemoteClawConfig } from "../runtime-api.js";
+import { normalizeResolvedSecretInputString, normalizeSecretInputString } from "../secret-input.js";
 import type {
   MattermostAccountConfig,
   MattermostChatMode,
@@ -38,36 +40,27 @@ function resolveAccountConfig(
   cfg: RemoteClawConfig,
   accountId: string,
 ): MattermostAccountConfig | undefined {
-  const accounts = cfg.channels?.mattermost?.accounts;
-  if (!accounts || typeof accounts !== "object") {
-    return undefined;
-  }
-  return accounts[accountId] as MattermostAccountConfig | undefined;
+  return resolveAccountEntry(cfg.channels?.mattermost?.accounts, accountId);
 }
 
 function mergeMattermostAccountConfig(
   cfg: RemoteClawConfig,
   accountId: string,
 ): MattermostAccountConfig {
-  const {
-    accounts: _ignored,
-    defaultAccount: _ignoredDefaultAccount,
-    ...base
-  } = (cfg.channels?.mattermost ?? {}) as MattermostAccountConfig & {
-    accounts?: unknown;
-    defaultAccount?: unknown;
-  };
   const account = resolveAccountConfig(cfg, accountId) ?? {};
+  const merged = mergeAccountConfig<MattermostAccountConfig>({
+    channelConfig: cfg.channels?.mattermost as MattermostAccountConfig | undefined,
+    accountConfig: account,
+    omitKeys: ["defaultAccount"],
+  });
 
   // Shallow merging is fine for most keys, but `commands` should be merged
   // so that account-specific overrides (callbackPath/callbackUrl) do not
   // accidentally reset global settings like `native: true`.
   const mergedCommands = {
-    ...(base.commands ?? {}),
+    ...((cfg.channels?.mattermost as MattermostAccountConfig | undefined)?.commands ?? {}),
     ...(account.commands ?? {}),
   };
-
-  const merged = { ...base, ...account };
   if (Object.keys(mergedCommands).length > 0) {
     merged.commands = mergedCommands;
   }

extensions/nextcloud-talk/src/accounts.ts
@@ -1,4 +1,5 @@
-import { readFileSync } from "node:fs";
+import { mergeAccountConfig } from "remoteclaw/plugin-sdk/account-resolution";
+import { tryReadSecretFileSync } from "remoteclaw/plugin-sdk/infra-runtime";
 import {
   createAccountListHelpers,
   DEFAULT_ACCOUNT_ID,
@@ -63,16 +64,11 @@ function mergeNextcloudTalkAccountConfig(
   cfg: CoreConfig,
   accountId: string,
 ): NextcloudTalkAccountConfig {
-  const {
-    accounts: _ignored,
-    defaultAccount: _ignoredDefaultAccount,
-    ...base
-  } = (cfg.channels?.["nextcloud-talk"] ?? {}) as NextcloudTalkAccountConfig & {
-    accounts?: unknown;
-    defaultAccount?: unknown;
-  };
-  const account = resolveAccountConfig(cfg, accountId) ?? {};
-  return { ...base, ...account };
+  return mergeAccountConfig<NextcloudTalkAccountConfig>({
+    channelConfig: cfg.channels?.["nextcloud-talk"] as NextcloudTalkAccountConfig | undefined,
+    accountConfig: resolveAccountConfig(cfg, accountId),
+    omitKeys: ["defaultAccount"],
+  });
 }
 
 function resolveNextcloudTalkSecret(
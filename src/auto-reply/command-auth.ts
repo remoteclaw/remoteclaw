wonglaitung/openclaw_mini

fix(core): restore session reset defaults and type seams

src/auto-reply/command-auth.ts
@@ -30,6 +30,7 @@ function resolveProviderFromContext(ctx: MsgContext, cfg: OpenClawConfig): Chann
   }
   const direct =
     normalizeAnyChannelId(explicitMessageChannel ?? undefined) ??
+    (explicitMessageChannel as ChannelId | undefined) ??
     normalizeAnyChannelId(ctx.Provider) ??
     normalizeAnyChannelId(ctx.Surface) ??
     normalizeAnyChannelId(ctx.OriginatingChannel);
@@ -46,6 +47,7 @@ function resolveProviderFromContext(ctx: MsgContext, cfg: OpenClawConfig): Chann
     }
     const normalized =
       normalizeAnyChannelId(normalizedCandidateChannel ?? undefined) ??
+      (normalizedCandidateChannel as ChannelId | undefined) ??
       normalizeAnyChannelId(candidate);
     if (normalized) {
       return normalized;
@@ -254,6 +256,50 @@ function resolveSenderCandidates(params: {
   return normalized;
 }
 
+function resolveFallbackAllowFrom(params: {
+  cfg: OpenClawConfig;
+  providerId?: ChannelId;
+  accountId?: string | null;
+}): Array<string | number> {
+  const providerId = params.providerId?.trim();
+  if (!providerId) {
+    return [];
+  }
+  const channels = params.cfg.channels as
+    | Record<
+        string,
+        | {
+            allowFrom?: Array<string | number>;
+            dm?: { allowFrom?: Array<string | number> };
+            accounts?: Record<
+              string,
+              {
+                allowFrom?: Array<string | number>;
+                dm?: { allowFrom?: Array<string | number> };
+              }
+            >;
+          }
+        | undefined
+      >
+    | undefined;
+  const channelCfg = channels?.[providerId];
+  const accountCfg = params.accountId ? channelCfg?.accounts?.[params.accountId] : undefined;
+  const allowFrom =
+    accountCfg?.allowFrom ??
+    accountCfg?.dm?.allowFrom ??
+    channelCfg?.allowFrom ??
+    channelCfg?.dm?.allowFrom;
+  return Array.isArray(allowFrom) ? allowFrom : [];
+}
+
+function resolveFallbackCommandOptions(providerId?: ChannelId): {
+  enforceOwnerForCommands: boolean;
+} {
+  return {
+    enforceOwnerForCommands: providerId === "whatsapp",
+  };
+}
+
 export function resolveCommandAuthorization(params: {
   ctx: MsgContext;
   cfg: OpenClawConfig;
@@ -275,7 +321,11 @@ export function resolveCommandAuthorization(params: {
 
   const allowFromRaw = plugin?.config?.resolveAllowFrom
     ? plugin.config.resolveAllowFrom({ cfg, accountId: ctx.AccountId })
-    : [];
+    : resolveFallbackAllowFrom({
+        cfg,
+        providerId,
+        accountId: ctx.AccountId,
+      });
   const allowFromList = formatAllowFromList({
     plugin,
     cfg,
@@ -344,7 +394,10 @@ export function resolveCommandAuthorization(params: {
     : undefined;
   const senderId = matchedSender ?? senderCandidates[0];
 
-  const enforceOwner = Boolean(plugin?.commands?.enforceOwnerForCommands);
+  const enforceOwner = Boolean(
+    plugin?.commands?.enforceOwnerForCommands ??
+    resolveFallbackCommandOptions(providerId).enforceOwnerForCommands,
+  );
   const senderIsOwnerByIdentity = Boolean(matchedSender);
   const senderIsOwnerByScope =
     isInternalMessageChannel(ctx.Provider) &&

src/auto-reply/reply/acp-reset-target.ts
@@ -1,10 +1,62 @@
+import {
+  buildConfiguredAcpSessionKey,
+  normalizeBindingConfig,
+  type ConfiguredAcpBindingChannel,
+} from "../../acp/persistent-bindings.types.js";
 import { resolveConfiguredBindingRecord } from "../../channels/plugins/binding-registry.js";
+import { listAcpBindings } from "../../config/bindings.js";
 import type { OpenClawConfig } from "../../config/config.js";
 import { getSessionBindingService } from "../../infra/outbound/session-binding-service.js";
 import { DEFAULT_ACCOUNT_ID, isAcpSessionKey } from "../../routing/session-key.js";
 
 function normalizeText(value: string | undefined | null): string {
   return value?.trim() ?? "";
+}
+
+function resolveRawConfiguredAcpSessionKey(params: {
+  cfg: OpenClawConfig;
+  channel: string;
+  accountId: string;
+  conversationId: string;
+  parentConversationId?: string;
+}): string | undefined {
+  for (const binding of listAcpBindings(params.cfg)) {
+    const bindingChannel = normalizeText(binding.match.channel).toLowerCase();
+    if (!bindingChannel || bindingChannel !== params.channel) {
+      continue;
+    }
+
+    const bindingAccountId = normalizeText(binding.match.accountId);
+    if (bindingAccountId && bindingAccountId !== "*" && bindingAccountId !== params.accountId) {
+      continue;
+    }
+
+    const peerId = normalizeText(binding.match.peer?.id);
+    const matchedConversationId =
+      peerId === params.conversationId
+        ? params.conversationId
+        : peerId && peerId === params.parentConversationId
+          ? params.parentConversationId
+          : undefined;
+    if (!matchedConversationId) {
+      continue;
+    }
+
+    const acp = normalizeBindingConfig(binding.acp);
+    return buildConfiguredAcpSessionKey({
+      channel: params.channel as ConfiguredAcpBindingChannel,
+      accountId: bindingAccountId && bindingAccountId !== "*" ? bindingAccountId : params.accountId,
+      conversationId: matchedConversationId,
+      ...(params.parentConversationId ? { parentConversationId: params.parentConversationId } : {}),
+      agentId: binding.agentId,
+      mode: acp.mode === "oneshot" ? "oneshot" : "persistent",
+      ...(acp.cwd ? { cwd: acp.cwd } : {}),
+      ...(acp.backend ? { backend: acp.backend } : {}),
+      ...(acp.label ? { label: acp.label } : {}),
+    });
+  }
+
+  return undefined;
 }
 
 export function resolveEffectiveResetTargetSessionKey(params: {
@@ -68,6 +120,18 @@ export function resolveEffectiveResetTargetSessionKey(params: {
     }
     return isAcpSessionKey(configuredSessionKey) ? configuredSessionKey : undefined;
   }
+
+  const rawConfiguredSessionKey = resolveRawConfiguredAcpSessionKey({
+    cfg: params.cfg,
+    channel,
+    accountId,
+    conversationId,
+    ...(parentConversationId ? { parentConversationId } : {}),
+  });
+  if (rawConfiguredSessionKey) {
+    return rawConfiguredSessionKey;
+  }
+
   if (params.fallbackToActiveAcpWhenUnbound === false) {
     return undefined;
   }

src/auto-reply/reply/mentions.ts
@@ -117,6 +117,17 @@ function resolveMentionPatterns(cfg: OpenClawConfig | undefined, agentId?: strin
   return derived.length > 0 ? derived : [];
 }
 
+function resolveFallbackProviderMentionStripRegexes(providerId?: string | null): RegExp[] {
+  switch (providerId?.trim().toLowerCase()) {
+    case "discord":
+      return [/<@!?\d+>/gi];
+    case "slack":
+      return [/<@[^>\s]+>/gi];
+    default:
+      return [];
+  }
+}
+
 export function buildMentionRegexes(cfg: OpenClawConfig | undefined, agentId?: string): RegExp[] {
   const patterns = normalizeMentionPatterns(resolveMentionPatterns(cfg, agentId));
   return compileMentionPatternsCached({
@@ -215,7 +226,9 @@ export function stripMentions(
       cache: mentionStripRegexCompileCache,
       warnRejected: false,
     });
-  for (const re of [...configRegexes, ...providerRegexes]) {
+  const fallbackProviderRegexes =
+    providerRegexes.length > 0 ? [] : resolveFallbackProviderMentionStripRegexes(providerId);
+  for (const re of [...configRegexes, ...providerRegexes, ...fallbackProviderRegexes]) {
     result = result.replace(re, " ");
   }
   if (providerMentions?.stripMentions) {

remoteclaw/remoteclaw

(cherry picked from commit 0aa4950d21af10163244c8cd6d07016fd607ad77)

src/auto-reply/command-auth.ts
@@ -31,6 +31,7 @@ function resolveProviderFromContext(ctx: MsgContext, cfg: RemoteClawConfig): Cha
   }
   const direct =
     normalizeAnyChannelId(explicitMessageChannel ?? undefined) ??
+    (explicitMessageChannel as ChannelId | undefined) ??
     normalizeAnyChannelId(ctx.Provider) ??
     normalizeAnyChannelId(ctx.Surface) ??
     normalizeAnyChannelId(ctx.OriginatingChannel);
@@ -47,6 +48,7 @@ function resolveProviderFromContext(ctx: MsgContext, cfg: RemoteClawConfig): Cha
     }
     const normalized =
       normalizeAnyChannelId(normalizedCandidateChannel ?? undefined) ??
+      (normalizedCandidateChannel as ChannelId | undefined) ??
       normalizeAnyChannelId(candidate);
     if (normalized) {
       return normalized;
@@ -255,6 +257,50 @@ function resolveSenderCandidates(params: {
   return normalized;
 }
 
+function resolveFallbackAllowFrom(params: {
+  cfg: OpenClawConfig;
+  providerId?: ChannelId;
+  accountId?: string | null;
+}): Array<string | number> {
+  const providerId = params.providerId?.trim();
+  if (!providerId) {
+    return [];
+  }
+  const channels = params.cfg.channels as
+    | Record<
+        string,
+        | {
+            allowFrom?: Array<string | number>;
+            dm?: { allowFrom?: Array<string | number> };
+            accounts?: Record<
+              string,
+              {
+                allowFrom?: Array<string | number>;
+                dm?: { allowFrom?: Array<string | number> };
+              }
+            >;
+          }
+        | undefined
+      >
+    | undefined;
+  const channelCfg = channels?.[providerId];
+  const accountCfg = params.accountId ? channelCfg?.accounts?.[params.accountId] : undefined;
+  const allowFrom =
+    accountCfg?.allowFrom ??
+    accountCfg?.dm?.allowFrom ??
+    channelCfg?.allowFrom ??
+    channelCfg?.dm?.allowFrom;
+  return Array.isArray(allowFrom) ? allowFrom : [];
+}
+
+function resolveFallbackCommandOptions(providerId?: ChannelId): {
+  enforceOwnerForCommands: boolean;
+} {
+  return {
+    enforceOwnerForCommands: providerId === "whatsapp",
+  };
+}
+
 export function resolveCommandAuthorization(params: {
   ctx: MsgContext;
   cfg: RemoteClawConfig;
@@ -276,7 +322,11 @@ export function resolveCommandAuthorization(params: {
 
   const allowFromRaw = dock?.config?.resolveAllowFrom
     ? dock.config.resolveAllowFrom({ cfg, accountId: ctx.AccountId })
-    : [];
+    : resolveFallbackAllowFrom({
+        cfg,
+        providerId,
+        accountId: ctx.AccountId,
+      });
   const allowFromList = formatAllowFromList({
     dock,
     cfg,
@@ -345,7 +395,10 @@ export function resolveCommandAuthorization(params: {
     : undefined;
   const senderId = matchedSender ?? senderCandidates[0];
 
-  const enforceOwner = Boolean(dock?.commands?.enforceOwnerForCommands);
+  const enforceOwner = Boolean(
+    dock?.commands?.enforceOwnerForCommands ??
+    resolveFallbackCommandOptions(providerId).enforceOwnerForCommands,
+  );
   const senderIsOwnerByIdentity = Boolean(matchedSender);
   const senderIsOwnerByScope =
     isInternalMessageChannel(ctx.Provider) &&

src/auto-reply/reply/acp-reset-target.ts
@@ -1,10 +1,62 @@
+import {
+  buildConfiguredAcpSessionKey,
+  normalizeBindingConfig,
+  type ConfiguredAcpBindingChannel,
+} from "../../acp/persistent-bindings.types.js";
 import { resolveConfiguredAcpBindingRecord } from "../../acp/persistent-bindings.js";
+import { listAcpBindings } from "../../config/bindings.js";
 import type { RemoteClawConfig } from "../../config/config.js";
 import { getSessionBindingService } from "../../infra/outbound/session-binding-service.js";
 import { DEFAULT_ACCOUNT_ID, isAcpSessionKey } from "../../routing/session-key.js";
 
 function normalizeText(value: string | undefined | null): string {
   return value?.trim() ?? "";
+}
+
+function resolveRawConfiguredAcpSessionKey(params: {
+  cfg: RemoteClawConfig;
+  channel: string;
+  accountId: string;
+  conversationId: string;
+  parentConversationId?: string;
+}): string | undefined {
+  for (const binding of listAcpBindings(params.cfg)) {
+    const bindingChannel = normalizeText(binding.match.channel).toLowerCase();
+    if (!bindingChannel || bindingChannel !== params.channel) {
+      continue;
+    }
+
+    const bindingAccountId = normalizeText(binding.match.accountId);
+    if (bindingAccountId && bindingAccountId !== "*" && bindingAccountId !== params.accountId) {
+      continue;
+    }
+
+    const peerId = normalizeText(binding.match.peer?.id);
+    const matchedConversationId =
+      peerId === params.conversationId
+        ? params.conversationId
+        : peerId && peerId === params.parentConversationId
+          ? params.parentConversationId
+          : undefined;
+    if (!matchedConversationId) {
+      continue;
+    }
+
+    const acp = normalizeBindingConfig(binding.acp);
+    return buildConfiguredAcpSessionKey({
+      channel: params.channel as ConfiguredAcpBindingChannel,
+      accountId: bindingAccountId && bindingAccountId !== "*" ? bindingAccountId : params.accountId,
+      conversationId: matchedConversationId,
+      ...(params.parentConversationId ? { parentConversationId: params.parentConversationId } : {}),
+      agentId: binding.agentId,
+      mode: acp.mode === "oneshot" ? "oneshot" : "persistent",
+      ...(acp.cwd ? { cwd: acp.cwd } : {}),
+      ...(acp.backend ? { backend: acp.backend } : {}),
+      ...(acp.label ? { label: acp.label } : {}),
+    });
+  }
+
+  return undefined;
 }
 
 export function resolveEffectiveResetTargetSessionKey(params: {
@@ -68,6 +120,18 @@ export function resolveEffectiveResetTargetSessionKey(params: {
     }
     return isAcpSessionKey(configuredSessionKey) ? configuredSessionKey : undefined;
   }
+
+  const rawConfiguredSessionKey = resolveRawConfiguredAcpSessionKey({
+    cfg: params.cfg,
+    channel,
+    accountId,
+    conversationId,
+    ...(parentConversationId ? { parentConversationId } : {}),
+  });
+  if (rawConfiguredSessionKey) {
+    return rawConfiguredSessionKey;
+  }
+
   if (params.fallbackToActiveAcpWhenUnbound === false) {
     return undefined;
   }

src/auto-reply/reply/mentions.ts
@@ -54,6 +54,17 @@ function resolveMentionPatterns(cfg: RemoteClawConfig | undefined, agentId?: str
   return derived.length > 0 ? derived : [];
 }
 
+function resolveFallbackProviderMentionStripRegexes(providerId?: string | null): RegExp[] {
+  switch (providerId?.trim().toLowerCase()) {
+    case "discord":
+      return [/<@!?\d+>/gi];
+    case "slack":
+      return [/<@[^>\s]+>/gi];
+    default:
+      return [];
+  }
+}
+
 export function buildMentionRegexes(cfg: RemoteClawConfig | undefined, agentId?: string): RegExp[] {
   const patterns = normalizeMentionPatterns(resolveMentionPatterns(cfg, agentId));
   if (patterns.length === 0) {
@@ -164,6 +175,11 @@ export function stripMentions(
     } catch {
       // ignore invalid regex
     }
+  }
+  const fallbackProviderRegexes =
+    patterns.length > 0 ? [] : resolveFallbackProviderMentionStripRegexes(providerId);
+  for (const re of fallbackProviderRegexes) {
+    result = result.replace(re, " ");
   }
   if (providerMentions?.stripMentions) {
     result = providerMentions.stripMentions({
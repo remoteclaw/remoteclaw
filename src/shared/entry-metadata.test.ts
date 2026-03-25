wonglaitung/openclaw_mini

test: add shared node and usage helper coverage

src/shared/entry-metadata.test.ts
@@ -1,0 +1,33 @@
+import { describe, expect, it } from "vitest";
+import { resolveEmojiAndHomepage } from "./entry-metadata.js";
+
+describe("shared/entry-metadata", () => {
+  it("prefers metadata emoji and homepage when present", () => {
+    expect(
+      resolveEmojiAndHomepage({
+        metadata: { emoji: "🦀", homepage: " https://openclaw.ai " },
+        frontmatter: { emoji: "🙂", homepage: "https://example.com" },
+      }),
+    ).toEqual({
+      emoji: "🦀",
+      homepage: "https://openclaw.ai",
+    });
+  });
+
+  it("falls back through frontmatter homepage aliases and drops blanks", () => {
+    expect(
+      resolveEmojiAndHomepage({
+        frontmatter: { emoji: "🙂", website: " https://docs.openclaw.ai " },
+      }),
+    ).toEqual({
+      emoji: "🙂",
+      homepage: "https://docs.openclaw.ai",
+    });
+    expect(
+      resolveEmojiAndHomepage({
+        metadata: { homepage: "   " },
+        frontmatter: { url: "   " },
+      }),
+    ).toEqual({});
+  });
+});

remoteclaw/remoteclaw

(cherry picked from commit d291148e93130e789a9e4ad230a1f249587bf065)

src/shared/entry-metadata.test.ts
@@ -1,0 +1,33 @@
+import { describe, expect, it } from "vitest";
+import { resolveEmojiAndHomepage } from "./entry-metadata.js";
+
+describe("shared/entry-metadata", () => {
+  it("prefers metadata emoji and homepage when present", () => {
+    expect(
+      resolveEmojiAndHomepage({
+        metadata: { emoji: "🦀", homepage: " https://remoteclaw.org " },
+        frontmatter: { emoji: "🙂", homepage: "https://example.com" },
+      }),
+    ).toEqual({
+      emoji: "🦀",
+      homepage: "https://remoteclaw.org",
+    });
+  });
+
+  it("falls back through frontmatter homepage aliases and drops blanks", () => {
+    expect(
+      resolveEmojiAndHomepage({
+        frontmatter: { emoji: "🙂", website: " https://docs.remoteclaw.org " },
+      }),
+    ).toEqual({
+      emoji: "🙂",
+      homepage: "https://docs.remoteclaw.org",
+    });
+    expect(
+      resolveEmojiAndHomepage({
+        metadata: { homepage: "   " },
+        frontmatter: { url: "   " },
+      }),
+    ).toEqual({});
+  });
+});
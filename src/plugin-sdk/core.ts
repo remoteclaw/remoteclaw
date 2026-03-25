gumadeiras/clawdbot

Plugin SDK: add outbound session route helpers

src/plugin-sdk/core.ts
@@ -1,5 +1,11 @@
+import type {
+  ChannelMessagingAdapter,
+  ChannelOutboundSessionRoute,
+} from "../channels/plugins/types.core.js";
 import type { ChannelPlugin } from "../channels/plugins/types.plugin.js";
 import { getChatChannelMeta } from "../channels/registry.js";
+import type { OpenClawConfig } from "../config/config.js";
+import { buildOutboundBaseSessionKey } from "../infra/outbound/base-session-key.js";
 import { emptyPluginConfigSchema } from "../plugins/config-schema.js";
 import type { PluginRuntime } from "../plugins/runtime/types.js";
 import type {
@@ -49,6 +55,10 @@ export type {
 } from "../plugins/types.js";
 export type { OpenClawConfig } from "../config/config.js";
 export type { GatewayRequestHandlerOptions } from "../gateway/server-methods/types.js";
+export type {
+  ChannelOutboundSessionRoute,
+  ChannelMessagingAdapter,
+} from "../channels/plugins/types.core.js";
 export type {
   ProviderUsageSnapshot,
   UsageProviderId,
@@ -104,6 +114,54 @@ export {
 export { buildOutboundBaseSessionKey } from "../infra/outbound/base-session-key.js";
 export { normalizeOutboundThreadId } from "../infra/outbound/thread-id.js";
 export { resolveThreadSessionKeys } from "../routing/session-key.js";
+
+export type ChannelOutboundSessionRouteParams = Parameters<
+  NonNullable<ChannelMessagingAdapter["resolveOutboundSessionRoute"]>
+>[0];
+
+export function stripChannelTargetPrefix(raw: string, ...providers: string[]): string {
+  const trimmed = raw.trim();
+  for (const provider of providers) {
+    const prefix = `${provider.toLowerCase()}:`;
+    if (trimmed.toLowerCase().startsWith(prefix)) {
+      return trimmed.slice(prefix.length).trim();
+    }
+  }
+  return trimmed;
+}
+
+export function stripTargetKindPrefix(raw: string): string {
+  return raw.replace(/^(user|channel|group|conversation|room|dm):/i, "").trim();
+}
+
+export function buildChannelOutboundSessionRoute(params: {
+  cfg: OpenClawConfig;
+  agentId: string;
+  channel: string;
+  accountId?: string | null;
+  peer: { kind: "direct" | "group" | "channel"; id: string };
+  chatType: "direct" | "group" | "channel";
+  from: string;
+  to: string;
+  threadId?: string | number;
+}): ChannelOutboundSessionRoute {
+  const baseSessionKey = buildOutboundBaseSessionKey({
+    cfg: params.cfg,
+    agentId: params.agentId,
+    channel: params.channel,
+    accountId: params.accountId,
+    peer: params.peer,
+  });
+  return {
+    sessionKey: baseSessionKey,
+    baseSessionKey,
+    peer: params.peer,
+    chatType: params.chatType,
+    from: params.from,
+    to: params.to,
+    ...(params.threadId !== undefined ? { threadId: params.threadId } : {}),
+  };
+}
 
 type DefineChannelPluginEntryOptions<TPlugin extends ChannelPlugin = ChannelPlugin> = {
   id: string;

remoteclaw/remoteclaw

(cherry picked from commit 826c592debf5ccad84b5f46e50f19e36216ee44d)

src/plugin-sdk/core.ts
@@ -1,3 +1,10 @@
+import type {
+  ChannelMessagingAdapter,
+  ChannelOutboundSessionRoute,
+} from "../channels/plugins/types.core.js";
+import type { RemoteClawConfig } from "../config/config.js";
+import { buildOutboundBaseSessionKey } from "../infra/outbound/base-session-key.js";
+
 export type {
   AnyAgentTool,
   RemoteClawPluginApi,
@@ -5,6 +12,10 @@ export type {
   ProviderAuthContext,
   ProviderAuthResult,
 } from "../plugins/types.js";
+export type {
+  ChannelOutboundSessionRoute,
+  ChannelMessagingAdapter,
+} from "../channels/plugins/types.core.js";
 export type { ChannelPlugin } from "../channels/plugins/types.plugin.js";
 export type { ChannelMessageActionContext } from "../channels/plugins/types.js";
 export type { PluginRuntime } from "../plugins/runtime/types.js";
@@ -42,3 +53,51 @@ export {
 export { buildOutboundBaseSessionKey } from "../infra/outbound/base-session-key.js";
 export { normalizeOutboundThreadId } from "../infra/outbound/thread-id.js";
 export { resolveThreadSessionKeys } from "../routing/session-key.js";
+
+export type ChannelOutboundSessionRouteParams = Parameters<
+  NonNullable<ChannelMessagingAdapter["resolveOutboundSessionRoute"]>
+>[0];
+
+export function stripChannelTargetPrefix(raw: string, ...providers: string[]): string {
+  const trimmed = raw.trim();
+  for (const provider of providers) {
+    const prefix = `${provider.toLowerCase()}:`;
+    if (trimmed.toLowerCase().startsWith(prefix)) {
+      return trimmed.slice(prefix.length).trim();
+    }
+  }
+  return trimmed;
+}
+
+export function stripTargetKindPrefix(raw: string): string {
+  return raw.replace(/^(user|channel|group|conversation|room|dm):/i, "").trim();
+}
+
+export function buildChannelOutboundSessionRoute(params: {
+  cfg: RemoteClawConfig;
+  agentId: string;
+  channel: string;
+  accountId?: string | null;
+  peer: { kind: "direct" | "group" | "channel"; id: string };
+  chatType: "direct" | "group" | "channel";
+  from: string;
+  to: string;
+  threadId?: string | number;
+}): ChannelOutboundSessionRoute {
+  const baseSessionKey = buildOutboundBaseSessionKey({
+    cfg: params.cfg,
+    agentId: params.agentId,
+    channel: params.channel,
+    accountId: params.accountId,
+    peer: params.peer,
+  });
+  return {
+    sessionKey: baseSessionKey,
+    baseSessionKey,
+    peer: params.peer,
+    chatType: params.chatType,
+    from: params.from,
+    to: params.to,
+    ...(params.threadId !== undefined ? { threadId: params.threadId } : {}),
+  };
+}
wonglaitung/openclaw_mini

fix: isolate plugin discovery env from global state

src/plugins/bundled-dir.ts
@@ -2,8 +2,8 @@ import fs from "node:fs";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 
-export function resolveBundledPluginsDir(): string | undefined {
-  const override = process.env.OPENCLAW_BUNDLED_PLUGINS_DIR?.trim();
+export function resolveBundledPluginsDir(env: NodeJS.ProcessEnv = process.env): string | undefined {
+  const override = env.OPENCLAW_BUNDLED_PLUGINS_DIR?.trim();
   if (override) {
     return override;
   }

remoteclaw/remoteclaw

(cherry picked from commit 43a10677edf7388b5db8ddd28e07fd49d6a9fa2d)

src/plugins/bundled-dir.ts
@@ -2,8 +2,8 @@ import fs from "node:fs";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 
-export function resolveBundledPluginsDir(): string | undefined {
-  const override = process.env.REMOTECLAW_BUNDLED_PLUGINS_DIR?.trim();
+export function resolveBundledPluginsDir(env: NodeJS.ProcessEnv = process.env): string | undefined {
+  const override = env.REMOTECLAW_BUNDLED_PLUGINS_DIR?.trim();
   if (override) {
     return override;
   }
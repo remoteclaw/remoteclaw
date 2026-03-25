wonglaitung/openclaw_mini

fix(plugin-sdk): stop library import warmup side effects

src/plugin-sdk/root-alias.test.ts
@@ -109,6 +109,17 @@ describe("plugin-sdk root alias", () => {
     expect(lazyModule.jitiLoadCalls).toBe(0);
   });
 
+  it("does not load the monolithic sdk for promise-like or symbol reflection probes", () => {
+    const lazyModule = loadRootAliasWithStubs();
+    const lazyRootSdk = lazyModule.moduleExports;
+
+    expect("then" in lazyRootSdk).toBe(false);
+    expect(Reflect.get(lazyRootSdk, Symbol.toStringTag)).toBeUndefined();
+    expect(Object.getOwnPropertyDescriptor(lazyRootSdk, Symbol.toStringTag)).toBeUndefined();
+    expect(lazyModule.createJitiCalls).toBe(0);
+    expect(lazyModule.jitiLoadCalls).toBe(0);
+  });
+
   it("loads legacy root exports on demand and preserves reflection", () => {
     const lazyModule = loadRootAliasWithStubs({
       monolithicExports: {

remoteclaw/remoteclaw

(cherry picked from commit 009f494cd94590d4d309d0e1bf5148d4dd6fffb5)

src/plugin-sdk/root-alias.test.ts
@@ -27,6 +27,12 @@ describe("plugin-sdk root alias", () => {
     expect(parsed.success).toBe(false);
   });
 
+  it("does not trigger monolithic sdk for promise-like or symbol reflection probes", () => {
+    expect("then" in rootSdk).toBe(false);
+    expect(Reflect.get(rootSdk, Symbol.toStringTag)).toBeUndefined();
+    expect(Object.getOwnPropertyDescriptor(rootSdk, Symbol.toStringTag)).toBeUndefined();
+  });
+
   it("loads legacy root exports lazily through the proxy", { timeout: 240_000 }, () => {
     expect(typeof rootSdk.resolveControlCommandGate).toBe("function");
     expect(typeof rootSdk.default).toBe("object");
wonglaitung/openclaw_mini

refactor: move bundled extension deps to plugin packages

extensions/discord/src/retry.ts
@@ -1,0 +1,27 @@
+import { RateLimitError } from "@buape/carbon";
+import {
+  createRateLimitRetryRunner,
+  type RetryConfig,
+  type RetryRunner,
+} from "openclaw/plugin-sdk/infra-runtime";
+
+export const DISCORD_RETRY_DEFAULTS = {
+  attempts: 3,
+  minDelayMs: 500,
+  maxDelayMs: 30_000,
+  jitter: 0.1,
+} satisfies RetryConfig;
+
+export function createDiscordRetryRunner(params: {
+  retry?: RetryConfig;
+  configRetry?: RetryConfig;
+  verbose?: boolean;
+}): RetryRunner {
+  return createRateLimitRetryRunner({
+    ...params,
+    defaults: DISCORD_RETRY_DEFAULTS,
+    logLabel: "discord",
+    shouldRetry: (err) => err instanceof RateLimitError,
+    retryAfterMs: (err) => (err instanceof RateLimitError ? err.retryAfter * 1000 : undefined),
+  });
+}

remoteclaw/remoteclaw

(cherry picked from commit d7018aaf19147c9092c8d63c056bb86e6c721c9d)

extensions/discord/src/retry.ts
@@ -1,0 +1,27 @@
+import { RateLimitError } from "@buape/carbon";
+import {
+  createRateLimitRetryRunner,
+  type RetryConfig,
+  type RetryRunner,
+} from "remoteclaw/plugin-sdk/infra-runtime";
+
+export const DISCORD_RETRY_DEFAULTS = {
+  attempts: 3,
+  minDelayMs: 500,
+  maxDelayMs: 30_000,
+  jitter: 0.1,
+} satisfies RetryConfig;
+
+export function createDiscordRetryRunner(params: {
+  retry?: RetryConfig;
+  configRetry?: RetryConfig;
+  verbose?: boolean;
+}): RetryRunner {
+  return createRateLimitRetryRunner({
+    ...params,
+    defaults: DISCORD_RETRY_DEFAULTS,
+    logLabel: "discord",
+    shouldRetry: (err) => err instanceof RateLimitError,
+    retryAfterMs: (err) => (err instanceof RateLimitError ? err.retryAfter * 1000 : undefined),
+  });
+}
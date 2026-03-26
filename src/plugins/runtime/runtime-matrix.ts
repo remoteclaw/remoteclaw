wonglaitung/openclaw_mini

Matrix: wire thread binding command support

src/plugins/runtime/runtime-matrix.ts
@@ -1,0 +1,14 @@
+import {
+  setMatrixThreadBindingIdleTimeoutBySessionKey,
+  setMatrixThreadBindingMaxAgeBySessionKey,
+} from "openclaw/plugin-sdk/matrix";
+import type { PluginRuntimeChannel } from "./types-channel.js";
+
+export function createRuntimeMatrix(): PluginRuntimeChannel["matrix"] {
+  return {
+    threadBindings: {
+      setIdleTimeoutBySessionKey: setMatrixThreadBindingIdleTimeoutBySessionKey,
+      setMaxAgeBySessionKey: setMatrixThreadBindingMaxAgeBySessionKey,
+    },
+  };
+}

remoteclaw/remoteclaw

(cherry picked from commit dd10f290e825d6fa2d04f805234c4508c763804b)

src/plugins/runtime/runtime-matrix.ts
@@ -1,0 +1,14 @@
+import {
+  setMatrixThreadBindingIdleTimeoutBySessionKey,
+  setMatrixThreadBindingMaxAgeBySessionKey,
+} from "remoteclaw/plugin-sdk/matrix";
+import type { PluginRuntimeChannel } from "./types-channel.js";
+
+export function createRuntimeMatrix(): PluginRuntimeChannel["matrix"] {
+  return {
+    threadBindings: {
+      setIdleTimeoutBySessionKey: setMatrixThreadBindingIdleTimeoutBySessionKey,
+      setMaxAgeBySessionKey: setMatrixThreadBindingMaxAgeBySessionKey,
+    },
+  };
+}
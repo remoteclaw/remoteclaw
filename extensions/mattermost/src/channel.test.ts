wonglaitung/openclaw_mini

Tests: migrate channel action discovery to describeMessageTool

extensions/mattermost/src/channel.test.ts
@@ -17,6 +17,10 @@ import {
   withMockedGlobalFetch,
 } from "./mattermost/reactions.test-helpers.js";
 
+function getDescribedActions(cfg: OpenClawConfig): string[] {
+  return [...(mattermostPlugin.actions?.describeMessageTool?.({ cfg })?.actions ?? [])];
+}
+
 describe("mattermostPlugin", () => {
   beforeEach(() => {
     sendMessageMattermostMock.mockReset();
@@ -132,7 +136,7 @@ describe("mattermostPlugin", () => {
         },
       };
 
-      const actions = mattermostPlugin.actions?.listActions?.({ cfg }) ?? [];
+      const actions = getDescribedActions(cfg);
       expect(actions).toContain("react");
       expect(actions).toContain("send");
       expect(mattermostPlugin.actions?.supportsAction?.({ action: "react" })).toBe(true);
@@ -148,7 +152,7 @@ describe("mattermostPlugin", () => {
         },
       };
 
-      const actions = mattermostPlugin.actions?.listActions?.({ cfg }) ?? [];
+      const actions = getDescribedActions(cfg);
       expect(actions).toEqual([]);
     });
 
@@ -164,7 +168,7 @@ describe("mattermostPlugin", () => {
         },
       };
 
-      const actions = mattermostPlugin.actions?.listActions?.({ cfg }) ?? [];
+      const actions = getDescribedActions(cfg);
       expect(actions).not.toContain("react");
       expect(actions).toContain("send");
     });
@@ -187,7 +191,7 @@ describe("mattermostPlugin", () => {
         },
       };
 
-      const actions = mattermostPlugin.actions?.listActions?.({ cfg }) ?? [];
+      const actions = getDescribedActions(cfg);
       expect(actions).toContain("react");
     });
 

remoteclaw/remoteclaw

(cherry picked from commit fb0d04c8342c1ee12b7eaae159573699b9a4bdd9)

extensions/mattermost/src/channel.test.ts
@@ -17,6 +17,10 @@ import {
   withMockedGlobalFetch,
 } from "./mattermost/reactions.test-helpers.js";
 
+function getDescribedActions(cfg: RemoteClawConfig): string[] {
+  return [...(mattermostPlugin.actions?.describeMessageTool?.({ cfg })?.actions ?? [])];
+}
+
 describe("mattermostPlugin", () => {
   beforeEach(() => {
     sendMessageMattermostMock.mockReset();
@@ -132,7 +136,7 @@ describe("mattermostPlugin", () => {
         },
       };
 
-      const actions = mattermostPlugin.actions?.listActions?.({ cfg }) ?? [];
+      const actions = getDescribedActions(cfg);
       expect(actions).toContain("react");
       expect(actions).toContain("send");
       expect(mattermostPlugin.actions?.supportsAction?.({ action: "react" })).toBe(true);
@@ -148,7 +152,7 @@ describe("mattermostPlugin", () => {
         },
       };
 
-      const actions = mattermostPlugin.actions?.listActions?.({ cfg }) ?? [];
+      const actions = getDescribedActions(cfg);
       expect(actions).toEqual([]);
     });
 
@@ -164,7 +168,7 @@ describe("mattermostPlugin", () => {
         },
       };
 
-      const actions = mattermostPlugin.actions?.listActions?.({ cfg }) ?? [];
+      const actions = getDescribedActions(cfg);
       expect(actions).not.toContain("react");
       expect(actions).toContain("send");
     });
@@ -187,7 +191,7 @@ describe("mattermostPlugin", () => {
         },
       };
 
-      const actions = mattermostPlugin.actions?.listActions?.({ cfg }) ?? [];
+      const actions = getDescribedActions(cfg);
       expect(actions).toContain("react");
     });
 
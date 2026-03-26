wonglaitung/openclaw_mini

refactor: install heavy plugins on demand

src/cli/channel-auth.ts
@@ -1,6 +1,7 @@
 import { resolveChannelDefaultAccountId } from "../channels/plugins/helpers.js";
 import { getChannelPlugin, normalizeChannelId } from "../channels/plugins/index.js";
-import { loadConfig, type OpenClawConfig } from "../config/config.js";
+import { resolveInstallableChannelPlugin } from "../commands/channel-setup/channel-plugin-resolution.js";
+import { loadConfig, writeConfigFile, type OpenClawConfig } from "../config/config.js";
 import { setVerbose } from "../globals.js";
 import { resolveMessageChannelSelection } from "../infra/outbound/channel-selection.js";
 import { defaultRuntime, type RuntimeEnv } from "../runtime.js";
@@ -18,7 +19,14 @@ async function resolveChannelPluginForMode(
   opts: ChannelAuthOptions,
   mode: ChannelAuthMode,
   cfg: OpenClawConfig,
-): Promise<{ channelInput: string; channelId: string; plugin: ChannelPlugin }> {
+  runtime: RuntimeEnv,
+): Promise<{
+  cfg: OpenClawConfig;
+  configChanged: boolean;
+  channelInput: string;
+  channelId: string;
+  plugin: ChannelPlugin;
+}> {
   const explicitChannel = opts.channel?.trim();
   const channelInput = explicitChannel
     ? explicitChannel
@@ -27,13 +35,28 @@ async function resolveChannelPluginForMode(
   if (!channelId) {
     throw new Error(`Unsupported channel: ${channelInput}`);
   }
-  const plugin = getChannelPlugin(channelId);
+
+  const resolved = await resolveInstallableChannelPlugin({
+    cfg,
+    runtime,
+    channelId,
+    allowInstall: true,
+    supports: (candidate) =>
+      mode === "login" ? Boolean(candidate.auth?.login) : Boolean(candidate.gateway?.logoutAccount),
+  });
+  const plugin = resolved.plugin;
   const supportsMode =
     mode === "login" ? Boolean(plugin?.auth?.login) : Boolean(plugin?.gateway?.logoutAccount);
   if (!supportsMode) {
     throw new Error(`Channel ${channelId} does not support ${mode}`);
   }
-  return { channelInput, channelId, plugin: plugin as ChannelPlugin };
+  return {
+    cfg: resolved.cfg,
+    configChanged: resolved.configChanged,
+    channelInput,
+    channelId,
+    plugin: plugin as ChannelPlugin,
+  };
 }
 
 function resolveAccountContext(
@@ -49,8 +72,16 @@ export async function runChannelLogin(
   opts: ChannelAuthOptions,
   runtime: RuntimeEnv = defaultRuntime,
 ) {
-  const cfg = loadConfig();
-  const { channelInput, plugin } = await resolveChannelPluginForMode(opts, "login", cfg);
+  const loadedCfg = loadConfig();
+  const { cfg, configChanged, channelInput, plugin } = await resolveChannelPluginForMode(
+    opts,
+    "login",
+    loadedCfg,
+    runtime,
+  );
+  if (configChanged) {
+    await writeConfigFile(cfg);
+  }
   const login = plugin.auth?.login;
   if (!login) {
     throw new Error(`Channel ${channelInput} does not support login`);
@@ -71,8 +102,16 @@ export async function runChannelLogout(
   opts: ChannelAuthOptions,
   runtime: RuntimeEnv = defaultRuntime,
 ) {
-  const cfg = loadConfig();
-  const { channelInput, plugin } = await resolveChannelPluginForMode(opts, "logout", cfg);
+  const loadedCfg = loadConfig();
+  const { cfg, configChanged, channelInput, plugin } = await resolveChannelPluginForMode(
+    opts,
+    "logout",
+    loadedCfg,
+    runtime,
+  );
+  if (configChanged) {
+    await writeConfigFile(cfg);
+  }
   const logoutAccount = plugin.gateway?.logoutAccount;
   if (!logoutAccount) {
     throw new Error(`Channel ${channelInput} does not support logout`);

src/commands/channel-setup/channel-plugin-resolution.ts
@@ -1,0 +1,192 @@
+import { resolveAgentWorkspaceDir, resolveDefaultAgentId } from "../../agents/agent-scope.js";
+import {
+  getChannelPluginCatalogEntry,
+  listChannelPluginCatalogEntries,
+  type ChannelPluginCatalogEntry,
+} from "../../channels/plugins/catalog.js";
+import { getChannelPlugin, normalizeChannelId } from "../../channels/plugins/index.js";
+import type { ChannelId, ChannelPlugin } from "../../channels/plugins/types.js";
+import type { OpenClawConfig } from "../../config/config.js";
+import type { RuntimeEnv } from "../../runtime.js";
+import { createClackPrompter } from "../../wizard/clack-prompter.js";
+import type { WizardPrompter } from "../../wizard/prompts.js";
+import {
+  ensureChannelSetupPluginInstalled,
+  loadChannelSetupPluginRegistrySnapshotForChannel,
+} from "./plugin-install.js";
+
+type ChannelPluginSnapshot = {
+  channels: Array<{ plugin: ChannelPlugin }>;
+  channelSetups: Array<{ plugin: ChannelPlugin }>;
+};
+
+type ResolveInstallableChannelPluginResult = {
+  cfg: OpenClawConfig;
+  channelId?: ChannelId;
+  plugin?: ChannelPlugin;
+  catalogEntry?: ChannelPluginCatalogEntry;
+  configChanged: boolean;
+};
+
+function resolveWorkspaceDir(cfg: OpenClawConfig) {
+  return resolveAgentWorkspaceDir(cfg, resolveDefaultAgentId(cfg));
+}
+
+function resolveResolvedChannelId(params: {
+  rawChannel?: string | null;
+  catalogEntry?: ChannelPluginCatalogEntry;
+}): ChannelId | undefined {
+  const normalized = normalizeChannelId(params.rawChannel);
+  if (normalized) {
+    return normalized;
+  }
+  if (!params.catalogEntry) {
+    return undefined;
+  }
+  return normalizeChannelId(params.catalogEntry.id) ?? (params.catalogEntry.id as ChannelId);
+}
+
+export function resolveCatalogChannelEntry(raw: string, cfg: OpenClawConfig | null) {
+  const trimmed = raw.trim().toLowerCase();
+  if (!trimmed) {
+    return undefined;
+  }
+  const workspaceDir = cfg ? resolveWorkspaceDir(cfg) : undefined;
+  return listChannelPluginCatalogEntries({ workspaceDir }).find((entry) => {
+    if (entry.id.toLowerCase() === trimmed) {
+      return true;
+    }
+    return (entry.meta.aliases ?? []).some((alias) => alias.trim().toLowerCase() === trimmed);
+  });
+}
+
+function findScopedChannelPlugin(
+  snapshot: ChannelPluginSnapshot,
+  channelId: ChannelId,
+): ChannelPlugin | undefined {
+  return (
+    snapshot.channels.find((entry) => entry.plugin.id === channelId)?.plugin ??
+    snapshot.channelSetups.find((entry) => entry.plugin.id === channelId)?.plugin
+  );
+}
+
+function loadScopedChannelPlugin(params: {
+  cfg: OpenClawConfig;
+  runtime: RuntimeEnv;
+  channelId: ChannelId;
+  pluginId?: string;
+  workspaceDir?: string;
+}): ChannelPlugin | undefined {
+  const snapshot = loadChannelSetupPluginRegistrySnapshotForChannel({
+    cfg: params.cfg,
+    runtime: params.runtime,
+    channel: params.channelId,
+    ...(params.pluginId ? { pluginId: params.pluginId } : {}),
+    workspaceDir: params.workspaceDir,
+  });
+  return findScopedChannelPlugin(snapshot, params.channelId);
+}
+
+export async function resolveInstallableChannelPlugin(params: {
+  cfg: OpenClawConfig;
+  runtime: RuntimeEnv;
+  rawChannel?: string | null;
+  channelId?: ChannelId;
+  allowInstall?: boolean;
+  prompter?: WizardPrompter;
+  supports?: (plugin: ChannelPlugin) => boolean;
+}): Promise<ResolveInstallableChannelPluginResult> {
+  const supports = params.supports ?? (() => true);
+  let nextCfg = params.cfg;
+  const workspaceDir = resolveWorkspaceDir(nextCfg);
+  const catalogEntry =
+    (params.rawChannel ? resolveCatalogChannelEntry(params.rawChannel, nextCfg) : undefined) ??
+    (params.channelId
+      ? getChannelPluginCatalogEntry(params.channelId, {
+          workspaceDir,
+        })
+      : undefined);
+  const channelId =
+    params.channelId ??
+    resolveResolvedChannelId({
+      rawChannel: params.rawChannel,
+      catalogEntry,
+    });
+  if (!channelId) {
+    return {
+      cfg: nextCfg,
+      catalogEntry,
+      configChanged: false,
+    };
+  }
+
+  const existing = getChannelPlugin(channelId);
+  if (existing && supports(existing)) {
+    return {
+      cfg: nextCfg,
+      channelId,
+      plugin: existing,
+      catalogEntry,
+      configChanged: false,
+    };
+  }
+
+  const resolvedPluginId = catalogEntry?.pluginId;
+  if (catalogEntry) {
+    const scoped = loadScopedChannelPlugin({
+      cfg: nextCfg,
+      runtime: params.runtime,
+      channelId,
+      pluginId: resolvedPluginId,
+      workspaceDir,
+    });
+    if (scoped && supports(scoped)) {
+      return {
+        cfg: nextCfg,
+        channelId,
+        plugin: scoped,
+        catalogEntry,
+        configChanged: false,
+      };
+    }
+
+    if (params.allowInstall !== false) {
+      const installResult = await ensureChannelSetupPluginInstalled({
+        cfg: nextCfg,
+        entry: catalogEntry,
+        prompter: params.prompter ?? createClackPrompter(),
+        runtime: params.runtime,
+        workspaceDir,
+      });
+      nextCfg = installResult.cfg;
+      const installedPluginId = installResult.pluginId ?? resolvedPluginId;
+      const installedPlugin = installResult.installed
+        ? loadScopedChannelPlugin({
+            cfg: nextCfg,
+            runtime: params.runtime,
+            channelId,
+            pluginId: installedPluginId,
+            workspaceDir: resolveWorkspaceDir(nextCfg),
+          })
+        : undefined;
+      return {
+        cfg: nextCfg,
+        channelId,
+        plugin: installedPlugin ?? existing,
+        catalogEntry:
+          installedPluginId && catalogEntry.pluginId !== installedPluginId
+            ? { ...catalogEntry, pluginId: installedPluginId }
+            : catalogEntry,
+        configChanged: nextCfg !== params.cfg,
+      };
+    }
+  }
+
+  return {
+    cfg: nextCfg,
+    channelId,
+    plugin: existing,
+    catalogEntry,
+    configChanged: false,
+  };
+}

remoteclaw/remoteclaw

(cherry picked from commit b7ca56f6625da230df4606831978ab66236fccbe)

src/cli/channel-auth.ts
@@ -1,6 +1,7 @@
 import { resolveChannelDefaultAccountId } from "../channels/plugins/helpers.js";
 import { getChannelPlugin, normalizeChannelId } from "../channels/plugins/index.js";
-import { loadConfig, type RemoteClawConfig } from "../config/config.js";
+import { resolveInstallableChannelPlugin } from "../commands/channel-setup/channel-plugin-resolution.js";
+import { loadConfig, writeConfigFile, type RemoteClawConfig } from "../config/config.js";
 import { setVerbose } from "../globals.js";
 import { resolveMessageChannelSelection } from "../infra/outbound/channel-selection.js";
 import { defaultRuntime, type RuntimeEnv } from "../runtime.js";
@@ -18,7 +19,14 @@ async function resolveChannelPluginForMode(
   opts: ChannelAuthOptions,
   mode: ChannelAuthMode,
   cfg: RemoteClawConfig,
-): Promise<{ channelInput: string; channelId: string; plugin: ChannelPlugin }> {
+  runtime: RuntimeEnv,
+): Promise<{
+  cfg: RemoteClawConfig;
+  configChanged: boolean;
+  channelInput: string;
+  channelId: string;
+  plugin: ChannelPlugin;
+}> {
   const explicitChannel = opts.channel?.trim();
   const channelInput = explicitChannel
     ? explicitChannel
@@ -27,13 +35,28 @@ async function resolveChannelPluginForMode(
   if (!channelId) {
     throw new Error(`Unsupported channel: ${channelInput}`);
   }
-  const plugin = getChannelPlugin(channelId);
+
+  const resolved = await resolveInstallableChannelPlugin({
+    cfg,
+    runtime,
+    channelId,
+    allowInstall: true,
+    supports: (candidate) =>
+      mode === "login" ? Boolean(candidate.auth?.login) : Boolean(candidate.gateway?.logoutAccount),
+  });
+  const plugin = resolved.plugin;
   const supportsMode =
     mode === "login" ? Boolean(plugin?.auth?.login) : Boolean(plugin?.gateway?.logoutAccount);
   if (!supportsMode) {
     throw new Error(`Channel ${channelId} does not support ${mode}`);
   }
-  return { channelInput, channelId, plugin: plugin as ChannelPlugin };
+  return {
+    cfg: resolved.cfg,
+    configChanged: resolved.configChanged,
+    channelInput,
+    channelId,
+    plugin: plugin as ChannelPlugin,
+  };
 }
 
 function resolveAccountContext(
@@ -49,8 +72,16 @@ export async function runChannelLogin(
   opts: ChannelAuthOptions,
   runtime: RuntimeEnv = defaultRuntime,
 ) {
-  const cfg = loadConfig();
-  const { channelInput, plugin } = await resolveChannelPluginForMode(opts, "login", cfg);
+  const loadedCfg = loadConfig();
+  const { cfg, configChanged, channelInput, plugin } = await resolveChannelPluginForMode(
+    opts,
+    "login",
+    loadedCfg,
+    runtime,
+  );
+  if (configChanged) {
+    await writeConfigFile(cfg);
+  }
   const login = plugin.auth?.login;
   if (!login) {
     throw new Error(`Channel ${channelInput} does not support login`);
@@ -71,8 +102,16 @@ export async function runChannelLogout(
   opts: ChannelAuthOptions,
   runtime: RuntimeEnv = defaultRuntime,
 ) {
-  const cfg = loadConfig();
-  const { channelInput, plugin } = await resolveChannelPluginForMode(opts, "logout", cfg);
+  const loadedCfg = loadConfig();
+  const { cfg, configChanged, channelInput, plugin } = await resolveChannelPluginForMode(
+    opts,
+    "logout",
+    loadedCfg,
+    runtime,
+  );
+  if (configChanged) {
+    await writeConfigFile(cfg);
+  }
   const logoutAccount = plugin.gateway?.logoutAccount;
   if (!logoutAccount) {
     throw new Error(`Channel ${channelInput} does not support logout`);

src/commands/onboarding/channel-plugin-resolution.ts
@@ -1,0 +1,192 @@
+import { resolveAgentWorkspaceDir, resolveDefaultAgentId } from "../../agents/agent-scope.js";
+import {
+  getChannelPluginCatalogEntry,
+  listChannelPluginCatalogEntries,
+  type ChannelPluginCatalogEntry,
+} from "../../channels/plugins/catalog.js";
+import { getChannelPlugin, normalizeChannelId } from "../../channels/plugins/index.js";
+import type { ChannelId, ChannelPlugin } from "../../channels/plugins/types.js";
+import type { RemoteClawConfig } from "../../config/config.js";
+import type { RuntimeEnv } from "../../runtime.js";
+import { createClackPrompter } from "../../wizard/clack-prompter.js";
+import type { WizardPrompter } from "../../wizard/prompts.js";
+import {
+  ensureChannelSetupPluginInstalled,
+  loadChannelSetupPluginRegistrySnapshotForChannel,
+} from "./plugin-install.js";
+
+type ChannelPluginSnapshot = {
+  channels: Array<{ plugin: ChannelPlugin }>;
+  channelSetups: Array<{ plugin: ChannelPlugin }>;
+};
+
+type ResolveInstallableChannelPluginResult = {
+  cfg: RemoteClawConfig;
+  channelId?: ChannelId;
+  plugin?: ChannelPlugin;
+  catalogEntry?: ChannelPluginCatalogEntry;
+  configChanged: boolean;
+};
+
+function resolveWorkspaceDir(cfg: RemoteClawConfig) {
+  return resolveAgentWorkspaceDir(cfg, resolveDefaultAgentId(cfg));
+}
+
+function resolveResolvedChannelId(params: {
+  rawChannel?: string | null;
+  catalogEntry?: ChannelPluginCatalogEntry;
+}): ChannelId | undefined {
+  const normalized = normalizeChannelId(params.rawChannel);
+  if (normalized) {
+    return normalized;
+  }
+  if (!params.catalogEntry) {
+    return undefined;
+  }
+  return normalizeChannelId(params.catalogEntry.id) ?? (params.catalogEntry.id as ChannelId);
+}
+
+export function resolveCatalogChannelEntry(raw: string, cfg: RemoteClawConfig | null) {
+  const trimmed = raw.trim().toLowerCase();
+  if (!trimmed) {
+    return undefined;
+  }
+  const workspaceDir = cfg ? resolveWorkspaceDir(cfg) : undefined;
+  return listChannelPluginCatalogEntries({ workspaceDir }).find((entry) => {
+    if (entry.id.toLowerCase() === trimmed) {
+      return true;
+    }
+    return (entry.meta.aliases ?? []).some((alias) => alias.trim().toLowerCase() === trimmed);
+  });
+}
+
+function findScopedChannelPlugin(
+  snapshot: ChannelPluginSnapshot,
+  channelId: ChannelId,
+): ChannelPlugin | undefined {
+  return (
+    snapshot.channels.find((entry) => entry.plugin.id === channelId)?.plugin ??
+    snapshot.channelSetups.find((entry) => entry.plugin.id === channelId)?.plugin
+  );
+}
+
+function loadScopedChannelPlugin(params: {
+  cfg: RemoteClawConfig;
+  runtime: RuntimeEnv;
+  channelId: ChannelId;
+  pluginId?: string;
+  workspaceDir?: string;
+}): ChannelPlugin | undefined {
+  const snapshot = loadChannelSetupPluginRegistrySnapshotForChannel({
+    cfg: params.cfg,
+    runtime: params.runtime,
+    channel: params.channelId,
+    ...(params.pluginId ? { pluginId: params.pluginId } : {}),
+    workspaceDir: params.workspaceDir,
+  });
+  return findScopedChannelPlugin(snapshot, params.channelId);
+}
+
+export async function resolveInstallableChannelPlugin(params: {
+  cfg: RemoteClawConfig;
+  runtime: RuntimeEnv;
+  rawChannel?: string | null;
+  channelId?: ChannelId;
+  allowInstall?: boolean;
+  prompter?: WizardPrompter;
+  supports?: (plugin: ChannelPlugin) => boolean;
+}): Promise<ResolveInstallableChannelPluginResult> {
+  const supports = params.supports ?? (() => true);
+  let nextCfg = params.cfg;
+  const workspaceDir = resolveWorkspaceDir(nextCfg);
+  const catalogEntry =
+    (params.rawChannel ? resolveCatalogChannelEntry(params.rawChannel, nextCfg) : undefined) ??
+    (params.channelId
+      ? getChannelPluginCatalogEntry(params.channelId, {
+          workspaceDir,
+        })
+      : undefined);
+  const channelId =
+    params.channelId ??
+    resolveResolvedChannelId({
+      rawChannel: params.rawChannel,
+      catalogEntry,
+    });
+  if (!channelId) {
+    return {
+      cfg: nextCfg,
+      catalogEntry,
+      configChanged: false,
+    };
+  }
+
+  const existing = getChannelPlugin(channelId);
+  if (existing && supports(existing)) {
+    return {
+      cfg: nextCfg,
+      channelId,
+      plugin: existing,
+      catalogEntry,
+      configChanged: false,
+    };
+  }
+
+  const resolvedPluginId = catalogEntry?.pluginId;
+  if (catalogEntry) {
+    const scoped = loadScopedChannelPlugin({
+      cfg: nextCfg,
+      runtime: params.runtime,
+      channelId,
+      pluginId: resolvedPluginId,
+      workspaceDir,
+    });
+    if (scoped && supports(scoped)) {
+      return {
+        cfg: nextCfg,
+        channelId,
+        plugin: scoped,
+        catalogEntry,
+        configChanged: false,
+      };
+    }
+
+    if (params.allowInstall !== false) {
+      const installResult = await ensureChannelSetupPluginInstalled({
+        cfg: nextCfg,
+        entry: catalogEntry,
+        prompter: params.prompter ?? createClackPrompter(),
+        runtime: params.runtime,
+        workspaceDir,
+      });
+      nextCfg = installResult.cfg;
+      const installedPluginId = installResult.pluginId ?? resolvedPluginId;
+      const installedPlugin = installResult.installed
+        ? loadScopedChannelPlugin({
+            cfg: nextCfg,
+            runtime: params.runtime,
+            channelId,
+            pluginId: installedPluginId,
+            workspaceDir: resolveWorkspaceDir(nextCfg),
+          })
+        : undefined;
+      return {
+        cfg: nextCfg,
+        channelId,
+        plugin: installedPlugin ?? existing,
+        catalogEntry:
+          installedPluginId && catalogEntry.pluginId !== installedPluginId
+            ? { ...catalogEntry, pluginId: installedPluginId }
+            : catalogEntry,
+        configChanged: nextCfg !== params.cfg,
+      };
+    }
+  }
+
+  return {
+    cfg: nextCfg,
+    channelId,
+    plugin: existing,
+    catalogEntry,
+    configChanged: false,
+  };
+}
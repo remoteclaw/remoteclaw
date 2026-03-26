Nachx639/clawdbot

perf: precompute base config schema

src/config/schema-base.ts
@@ -1,0 +1,72 @@
+import { VERSION } from "../version.js";
+import type { ConfigUiHints } from "./schema.hints.js";
+import { buildBaseHints, mapSensitivePaths } from "./schema.hints.js";
+import { applyDerivedTags } from "./schema.tags.js";
+import { OpenClawSchema } from "./zod-schema.js";
+
+type ConfigSchema = Record<string, unknown>;
+
+type JsonSchemaObject = Record<string, unknown> & {
+  properties?: Record<string, JsonSchemaObject>;
+  required?: string[];
+  additionalProperties?: JsonSchemaObject | boolean;
+};
+
+export type BaseConfigSchemaResponse = {
+  schema: ConfigSchema;
+  uiHints: ConfigUiHints;
+  version: string;
+  generatedAt: string;
+};
+
+function cloneSchema<T>(value: T): T {
+  if (typeof structuredClone === "function") {
+    return structuredClone(value);
+  }
+  return JSON.parse(JSON.stringify(value)) as T;
+}
+
+function asSchemaObject(value: unknown): JsonSchemaObject | null {
+  if (!value || typeof value !== "object" || Array.isArray(value)) {
+    return null;
+  }
+  return value as JsonSchemaObject;
+}
+
+function stripChannelSchema(schema: ConfigSchema): ConfigSchema {
+  const next = cloneSchema(schema);
+  const root = asSchemaObject(next);
+  if (!root || !root.properties) {
+    return next;
+  }
+  // Allow `$schema` in config files for editor tooling, but hide it from the
+  // Control UI form schema so it does not show up as a configurable section.
+  delete root.properties.$schema;
+  if (Array.isArray(root.required)) {
+    root.required = root.required.filter((key) => key !== "$schema");
+  }
+  const channelsNode = asSchemaObject(root.properties.channels);
+  if (channelsNode) {
+    channelsNode.properties = {};
+    channelsNode.required = [];
+    channelsNode.additionalProperties = true;
+  }
+  return next;
+}
+
+export function computeBaseConfigSchemaResponse(params?: {
+  generatedAt?: string;
+}): BaseConfigSchemaResponse {
+  const schema = OpenClawSchema.toJSONSchema({
+    target: "draft-07",
+    unrepresentable: "any",
+  });
+  schema.title = "OpenClawConfig";
+  const hints = applyDerivedTags(mapSensitivePaths(OpenClawSchema, "", buildBaseHints()));
+  return {
+    schema: stripChannelSchema(schema),
+    uiHints: hints,
+    version: VERSION,
+    generatedAt: params?.generatedAt ?? new Date().toISOString(),
+  };
+}

remoteclaw/remoteclaw

(cherry picked from commit ca99163b9838ff412cd01040123c1ff8318c30d2)

src/config/schema-base.ts
@@ -1,0 +1,72 @@
+import { VERSION } from "../version.js";
+import type { ConfigUiHints } from "./schema.hints.js";
+import { buildBaseHints, mapSensitivePaths } from "./schema.hints.js";
+import { applyDerivedTags } from "./schema.tags.js";
+import { RemoteClawSchema } from "./zod-schema.js";
+
+type ConfigSchema = Record<string, unknown>;
+
+type JsonSchemaObject = Record<string, unknown> & {
+  properties?: Record<string, JsonSchemaObject>;
+  required?: string[];
+  additionalProperties?: JsonSchemaObject | boolean;
+};
+
+export type BaseConfigSchemaResponse = {
+  schema: ConfigSchema;
+  uiHints: ConfigUiHints;
+  version: string;
+  generatedAt: string;
+};
+
+function cloneSchema<T>(value: T): T {
+  if (typeof structuredClone === "function") {
+    return structuredClone(value);
+  }
+  return JSON.parse(JSON.stringify(value)) as T;
+}
+
+function asSchemaObject(value: unknown): JsonSchemaObject | null {
+  if (!value || typeof value !== "object" || Array.isArray(value)) {
+    return null;
+  }
+  return value as JsonSchemaObject;
+}
+
+function stripChannelSchema(schema: ConfigSchema): ConfigSchema {
+  const next = cloneSchema(schema);
+  const root = asSchemaObject(next);
+  if (!root || !root.properties) {
+    return next;
+  }
+  // Allow `$schema` in config files for editor tooling, but hide it from the
+  // Control UI form schema so it does not show up as a configurable section.
+  delete root.properties.$schema;
+  if (Array.isArray(root.required)) {
+    root.required = root.required.filter((key) => key !== "$schema");
+  }
+  const channelsNode = asSchemaObject(root.properties.channels);
+  if (channelsNode) {
+    channelsNode.properties = {};
+    channelsNode.required = [];
+    channelsNode.additionalProperties = true;
+  }
+  return next;
+}
+
+export function computeBaseConfigSchemaResponse(params?: {
+  generatedAt?: string;
+}): BaseConfigSchemaResponse {
+  const schema = RemoteClawSchema.toJSONSchema({
+    target: "draft-07",
+    unrepresentable: "any",
+  });
+  schema.title = "RemoteClawConfig";
+  const hints = applyDerivedTags(mapSensitivePaths(RemoteClawSchema, "", buildBaseHints()));
+  return {
+    schema: stripChannelSchema(schema),
+    uiHints: hints,
+    version: VERSION,
+    generatedAt: params?.generatedAt ?? new Date().toISOString(),
+  };
+}
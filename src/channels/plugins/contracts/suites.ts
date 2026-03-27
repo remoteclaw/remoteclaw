wonglaitung/openclaw_mini

Channels: add global threading and directory contracts

src/channels/plugins/contracts/suites.ts
@@ -5,13 +5,21 @@ import type {
   ResolveProviderRuntimeGroupPolicyParams,
   RuntimeGroupPolicyResolution,
 } from "../../../config/runtime-group-policy.js";
+import type {
+  SessionBindingCapabilities,
+  SessionBindingRecord,
+} from "../../../infra/outbound/session-binding-service.js";
 import { normalizeChatType } from "../../chat-type.js";
 import { resolveConversationLabel } from "../../conversation-label.js";
 import { validateSenderIdentity } from "../../sender-identity.js";
 import type {
   ChannelAccountSnapshot,
   ChannelAccountState,
+  ChannelDirectoryEntry,
+  ChannelFocusedBindingContext,
+  ChannelReplyTransport,
   ChannelSetupInput,
+  ChannelThreadingToolContext,
 } from "../types.core.js";
 import type {
   ChannelMessageActionName,
@@ -21,6 +29,68 @@ import type {
 
 function sortStrings(values: readonly string[]) {
   return [...values].toSorted((left, right) => left.localeCompare(right));
+}
+
+function expectDirectoryEntryShape(entry: ChannelDirectoryEntry) {
+  expect(["user", "group", "channel"]).toContain(entry.kind);
+  expect(typeof entry.id).toBe("string");
+  expect(entry.id.trim()).not.toBe("");
+  if (entry.name !== undefined) {
+    expect(typeof entry.name).toBe("string");
+  }
+  if (entry.handle !== undefined) {
+    expect(typeof entry.handle).toBe("string");
+  }
+  if (entry.avatarUrl !== undefined) {
+    expect(typeof entry.avatarUrl).toBe("string");
+  }
+  if (entry.rank !== undefined) {
+    expect(typeof entry.rank).toBe("number");
+  }
+}
+
+function expectThreadingToolContextShape(context: ChannelThreadingToolContext) {
+  if (context.currentChannelId !== undefined) {
+    expect(typeof context.currentChannelId).toBe("string");
+  }
+  if (context.currentChannelProvider !== undefined) {
+    expect(typeof context.currentChannelProvider).toBe("string");
+  }
+  if (context.currentThreadTs !== undefined) {
+    expect(typeof context.currentThreadTs).toBe("string");
+  }
+  if (context.currentMessageId !== undefined) {
+    expect(["string", "number"]).toContain(typeof context.currentMessageId);
+  }
+  if (context.replyToMode !== undefined) {
+    expect(["off", "first", "all"]).toContain(context.replyToMode);
+  }
+  if (context.hasRepliedRef !== undefined) {
+    expect(typeof context.hasRepliedRef).toBe("object");
+  }
+  if (context.skipCrossContextDecoration !== undefined) {
+    expect(typeof context.skipCrossContextDecoration).toBe("boolean");
+  }
+}
+
+function expectReplyTransportShape(transport: ChannelReplyTransport) {
+  if (transport.replyToId !== undefined && transport.replyToId !== null) {
+    expect(typeof transport.replyToId).toBe("string");
+  }
+  if (transport.threadId !== undefined && transport.threadId !== null) {
+    expect(["string", "number"]).toContain(typeof transport.threadId);
+  }
+}
+
+function expectFocusedBindingShape(binding: ChannelFocusedBindingContext) {
+  expect(typeof binding.conversationId).toBe("string");
+  expect(binding.conversationId.trim()).not.toBe("");
+  if (binding.parentConversationId !== undefined) {
+    expect(typeof binding.parentConversationId).toBe("string");
+  }
+  expect(["current", "child"]).toContain(binding.placement);
+  expect(typeof binding.labelNoun).toBe("string");
+  expect(binding.labelNoun.trim()).not.toBe("");
 }
 
 export function installChannelPluginContractSuite(params: {
@@ -225,6 +295,186 @@ export function installChannelSurfaceContractSuite(params: {
         gateway?.logoutAccount,
       ].some((value) => typeof value === "function"),
     ).toBe(true);
+  });
+}
+
+export function installChannelThreadingContractSuite(params: {
+  plugin: Pick<ChannelPlugin, "id" | "threading">;
+}) {
+  it("exposes the base threading contract", () => {
+    expect(params.plugin.threading).toBeDefined();
+  });
+
+  it("keeps threading return values normalized", () => {
+    const threading = params.plugin.threading;
+    expect(threading).toBeDefined();
+
+    if (threading?.resolveReplyToMode) {
+      expect(
+        ["off", "first", "all"].includes(
+          threading.resolveReplyToMode({
+            cfg: {} as OpenClawConfig,
+            accountId: "default",
+            chatType: "group",
+          }),
+        ),
+      ).toBe(true);
+    }
+
+    const repliedRef = { value: false };
+    const toolContext = threading?.buildToolContext?.({
+      cfg: {} as OpenClawConfig,
+      accountId: "default",
+      context: {
+        Channel: "group:test",
+        From: "user:test",
+        To: "group:test",
+        ChatType: "group",
+        CurrentMessageId: "msg-1",
+        ReplyToId: "msg-0",
+        ReplyToIdFull: "thread-0",
+        MessageThreadId: "thread-0",
+        NativeChannelId: "native:test",
+      },
+      hasRepliedRef: repliedRef,
+    });
+
+    if (toolContext) {
+      expectThreadingToolContextShape(toolContext);
+      if (toolContext.hasRepliedRef) {
+        expect(toolContext.hasRepliedRef).toBe(repliedRef);
+      }
+    }
+
+    const autoThreadId = threading?.resolveAutoThreadId?.({
+      cfg: {} as OpenClawConfig,
+      accountId: "default",
+      to: "group:test",
+      toolContext,
+      replyToId: null,
+    });
+    if (autoThreadId !== undefined) {
+      expect(typeof autoThreadId).toBe("string");
+      expect(autoThreadId.trim()).not.toBe("");
+    }
+
+    const replyTransport = threading?.resolveReplyTransport?.({
+      cfg: {} as OpenClawConfig,
+      accountId: "default",
+      threadId: "thread-0",
+      replyToId: "msg-0",
+    });
+    if (replyTransport) {
+      expectReplyTransportShape(replyTransport);
+    }
+
+    const focusedBinding = threading?.resolveFocusedBinding?.({
+      cfg: {} as OpenClawConfig,
+      accountId: "default",
+      context: {
+        Channel: "group:test",
+        From: "user:test",
+        To: "group:test",
+        ChatType: "group",
+        CurrentMessageId: "msg-1",
+        ReplyToId: "msg-0",
+        ReplyToIdFull: "thread-0",
+        MessageThreadId: "thread-0",
+        NativeChannelId: "native:test",
+      },
+    });
+    if (focusedBinding) {
+      expectFocusedBindingShape(focusedBinding);
+    }
+  });
+}
+
+export function installChannelDirectoryContractSuite(params: {
+  plugin: Pick<ChannelPlugin, "id" | "directory">;
+  invokeLookups?: boolean;
+}) {
+  it("exposes the base directory contract", async () => {
+    const directory = params.plugin.directory;
+    expect(directory).toBeDefined();
+
+    if (params.invokeLookups === false) {
+      return;
+    }
+
+    const self = await directory?.self?.({
+      cfg: {} as OpenClawConfig,
+      accountId: "default",
+    });
+    if (self) {
+      expectDirectoryEntryShape(self);
+    }
+
+    const peers =
+      (await directory?.listPeers?.({
+        cfg: {} as OpenClawConfig,
+        accountId: "default",
+        query: "",
+        limit: 5,
+      })) ?? [];
+    expect(Array.isArray(peers)).toBe(true);
+    for (const peer of peers) {
+      expectDirectoryEntryShape(peer);
+    }
+
+    const groups =
+      (await directory?.listGroups?.({
+        cfg: {} as OpenClawConfig,
+        accountId: "default",
+        query: "",
+        limit: 5,
+      })) ?? [];
+    expect(Array.isArray(groups)).toBe(true);
+    for (const group of groups) {
+      expectDirectoryEntryShape(group);
+    }
+
+    if (directory?.listGroupMembers && groups[0]?.id) {
+      const members = await directory.listGroupMembers({
+        cfg: {} as OpenClawConfig,
+        accountId: "default",
+        groupId: groups[0].id,
+        query: "",
+        limit: 5,
+      });
+      expect(Array.isArray(members)).toBe(true);
+      for (const member of members) {
+        expectDirectoryEntryShape(member);
+      }
+    }
+  });
+}
+
+export function installSessionBindingContractSuite(params: {
+  getCapabilities: () => SessionBindingCapabilities;
+  bindAndResolve: () => Promise<SessionBindingRecord>;
+  cleanup: () => Promise<void> | void;
+  expectedCapabilities: SessionBindingCapabilities;
+}) {
+  it("registers the expected session binding capabilities", () => {
+    expect(params.getCapabilities()).toEqual(params.expectedCapabilities);
+  });
+
+  it("binds and resolves a session binding through the shared service", async () => {
+    const binding = await params.bindAndResolve();
+    expect(typeof binding.bindingId).toBe("string");
+    expect(binding.bindingId.trim()).not.toBe("");
+    expect(typeof binding.targetSessionKey).toBe("string");
+    expect(binding.targetSessionKey.trim()).not.toBe("");
+    expect(["session", "subagent"]).toContain(binding.targetKind);
+    expect(typeof binding.conversation.channel).toBe("string");
+    expect(typeof binding.conversation.accountId).toBe("string");
+    expect(typeof binding.conversation.conversationId).toBe("string");
+    expect(["active", "ending", "ended"]).toContain(binding.status);
+    expect(typeof binding.boundAt).toBe("number");
+  });
+
+  it("cleans up registered bindings", async () => {
+    await params.cleanup();
   });
 }
 

remoteclaw/remoteclaw

(cherry picked from commit 0f013575f8e79116cc3ccf1aa3722ee77e0468d1)

src/channels/plugins/contracts/suites.ts
@@ -1,14 +1,84 @@
 import { expect, it } from "vitest";
 import type { RemoteClawConfig } from "../../../config/config.js";
 import type {
+  SessionBindingCapabilities,
+  SessionBindingRecord,
+} from "../../../infra/outbound/session-binding-service.js";
+import type {
   ChannelAccountSnapshot,
   ChannelAccountState,
+  ChannelDirectoryEntry,
+  ChannelFocusedBindingContext,
+  ChannelReplyTransport,
   ChannelSetupInput,
+  ChannelThreadingToolContext,
 } from "../types.core.js";
 import type { ChannelMessageActionName, ChannelPlugin } from "../types.js";
 
 function sortStrings(values: readonly string[]) {
   return [...values].toSorted((left, right) => left.localeCompare(right));
+}
+
+function expectDirectoryEntryShape(entry: ChannelDirectoryEntry) {
+  expect(["user", "group", "channel"]).toContain(entry.kind);
+  expect(typeof entry.id).toBe("string");
+  expect(entry.id.trim()).not.toBe("");
+  if (entry.name !== undefined) {
+    expect(typeof entry.name).toBe("string");
+  }
+  if (entry.handle !== undefined) {
+    expect(typeof entry.handle).toBe("string");
+  }
+  if (entry.avatarUrl !== undefined) {
+    expect(typeof entry.avatarUrl).toBe("string");
+  }
+  if (entry.rank !== undefined) {
+    expect(typeof entry.rank).toBe("number");
+  }
+}
+
+function expectThreadingToolContextShape(context: ChannelThreadingToolContext) {
+  if (context.currentChannelId !== undefined) {
+    expect(typeof context.currentChannelId).toBe("string");
+  }
+  if (context.currentChannelProvider !== undefined) {
+    expect(typeof context.currentChannelProvider).toBe("string");
+  }
+  if (context.currentThreadTs !== undefined) {
+    expect(typeof context.currentThreadTs).toBe("string");
+  }
+  if (context.currentMessageId !== undefined) {
+    expect(["string", "number"]).toContain(typeof context.currentMessageId);
+  }
+  if (context.replyToMode !== undefined) {
+    expect(["off", "first", "all"]).toContain(context.replyToMode);
+  }
+  if (context.hasRepliedRef !== undefined) {
+    expect(typeof context.hasRepliedRef).toBe("object");
+  }
+  if (context.skipCrossContextDecoration !== undefined) {
+    expect(typeof context.skipCrossContextDecoration).toBe("boolean");
+  }
+}
+
+function expectReplyTransportShape(transport: ChannelReplyTransport) {
+  if (transport.replyToId !== undefined && transport.replyToId !== null) {
+    expect(typeof transport.replyToId).toBe("string");
+  }
+  if (transport.threadId !== undefined && transport.threadId !== null) {
+    expect(["string", "number"]).toContain(typeof transport.threadId);
+  }
+}
+
+function expectFocusedBindingShape(binding: ChannelFocusedBindingContext) {
+  expect(typeof binding.conversationId).toBe("string");
+  expect(binding.conversationId.trim()).not.toBe("");
+  if (binding.parentConversationId !== undefined) {
+    expect(typeof binding.parentConversationId).toBe("string");
+  }
+  expect(["current", "child"]).toContain(binding.placement);
+  expect(typeof binding.labelNoun).toBe("string");
+  expect(binding.labelNoun.trim()).not.toBe("");
 }
 
 export function installChannelPluginContractSuite(params: {
@@ -84,6 +154,322 @@ export function installChannelActionsContractSuite(params: {
       }
     });
   }
+}
+
+export function installChannelSurfaceContractSuite(params: {
+  plugin: Pick<
+    ChannelPlugin,
+    | "id"
+    | "actions"
+    | "setup"
+    | "status"
+    | "outbound"
+    | "messaging"
+    | "threading"
+    | "directory"
+    | "gateway"
+  >;
+  surface:
+    | "actions"
+    | "setup"
+    | "status"
+    | "outbound"
+    | "messaging"
+    | "threading"
+    | "directory"
+    | "gateway";
+}) {
+  const { plugin, surface } = params;
+
+  it(`exposes the ${surface} surface contract`, () => {
+    if (surface === "actions") {
+      expect(plugin.actions).toBeDefined();
+      expect(typeof plugin.actions?.listActions).toBe("function");
+      return;
+    }
+
+    if (surface === "setup") {
+      expect(plugin.setup).toBeDefined();
+      expect(typeof plugin.setup?.applyAccountConfig).toBe("function");
+      return;
+    }
+
+    if (surface === "status") {
+      expect(plugin.status).toBeDefined();
+      expect(typeof plugin.status?.buildAccountSnapshot).toBe("function");
+      return;
+    }
+
+    if (surface === "outbound") {
+      const outbound = plugin.outbound;
+      expect(outbound).toBeDefined();
+      expect(["direct", "gateway", "hybrid"]).toContain(outbound?.deliveryMode);
+      expect(
+        [
+          outbound?.sendPayload,
+          outbound?.sendFormattedText,
+          outbound?.sendFormattedMedia,
+          outbound?.sendText,
+          outbound?.sendMedia,
+          outbound?.sendPoll,
+        ].some((value) => typeof value === "function"),
+      ).toBe(true);
+      return;
+    }
+
+    if (surface === "messaging") {
+      const messaging = plugin.messaging;
+      expect(messaging).toBeDefined();
+      expect(
+        [
+          messaging?.normalizeTarget,
+          messaging?.parseExplicitTarget,
+          messaging?.inferTargetChatType,
+          messaging?.buildCrossContextComponents,
+          messaging?.enableInteractiveReplies,
+          messaging?.hasStructuredReplyPayload,
+          messaging?.formatTargetDisplay,
+          messaging?.resolveOutboundSessionRoute,
+        ].some((value) => typeof value === "function"),
+      ).toBe(true);
+      if (messaging?.targetResolver) {
+        if (messaging.targetResolver.looksLikeId) {
+          expect(typeof messaging.targetResolver.looksLikeId).toBe("function");
+        }
+        if (messaging.targetResolver.hint !== undefined) {
+          expect(typeof messaging.targetResolver.hint).toBe("string");
+          expect(messaging.targetResolver.hint.trim()).not.toBe("");
+        }
+        if (messaging.targetResolver.resolveTarget) {
+          expect(typeof messaging.targetResolver.resolveTarget).toBe("function");
+        }
+      }
+      return;
+    }
+
+    if (surface === "threading") {
+      const threading = plugin.threading;
+      expect(threading).toBeDefined();
+      expect(
+        [
+          threading?.resolveReplyToMode,
+          threading?.buildToolContext,
+          threading?.resolveAutoThreadId,
+          threading?.resolveReplyTransport,
+          threading?.resolveFocusedBinding,
+        ].some((value) => typeof value === "function"),
+      ).toBe(true);
+      return;
+    }
+
+    if (surface === "directory") {
+      const directory = plugin.directory;
+      expect(directory).toBeDefined();
+      expect(
+        [
+          directory?.self,
+          directory?.listPeers,
+          directory?.listPeersLive,
+          directory?.listGroups,
+          directory?.listGroupsLive,
+          directory?.listGroupMembers,
+        ].some((value) => typeof value === "function"),
+      ).toBe(true);
+      return;
+    }
+
+    const gateway = plugin.gateway;
+    expect(gateway).toBeDefined();
+    expect(
+      [
+        gateway?.startAccount,
+        gateway?.stopAccount,
+        gateway?.loginWithQrStart,
+        gateway?.loginWithQrWait,
+        gateway?.logoutAccount,
+      ].some((value) => typeof value === "function"),
+    ).toBe(true);
+  });
+}
+
+export function installChannelThreadingContractSuite(params: {
+  plugin: Pick<ChannelPlugin, "id" | "threading">;
+}) {
+  it("exposes the base threading contract", () => {
+    expect(params.plugin.threading).toBeDefined();
+  });
+
+  it("keeps threading return values normalized", () => {
+    const threading = params.plugin.threading;
+    expect(threading).toBeDefined();
+
+    if (threading?.resolveReplyToMode) {
+      expect(
+        ["off", "first", "all"].includes(
+          threading.resolveReplyToMode({
+            cfg: {} as RemoteClawConfig,
+            accountId: "default",
+            chatType: "group",
+          }),
+        ),
+      ).toBe(true);
+    }
+
+    const repliedRef = { value: false };
+    const toolContext = threading?.buildToolContext?.({
+      cfg: {} as RemoteClawConfig,
+      accountId: "default",
+      context: {
+        Channel: "group:test",
+        From: "user:test",
+        To: "group:test",
+        ChatType: "group",
+        CurrentMessageId: "msg-1",
+        ReplyToId: "msg-0",
+        ReplyToIdFull: "thread-0",
+        MessageThreadId: "thread-0",
+        NativeChannelId: "native:test",
+      },
+      hasRepliedRef: repliedRef,
+    });
+
+    if (toolContext) {
+      expectThreadingToolContextShape(toolContext);
+      if (toolContext.hasRepliedRef) {
+        expect(toolContext.hasRepliedRef).toBe(repliedRef);
+      }
+    }
+
+    const autoThreadId = threading?.resolveAutoThreadId?.({
+      cfg: {} as RemoteClawConfig,
+      accountId: "default",
+      to: "group:test",
+      toolContext,
+      replyToId: null,
+    });
+    if (autoThreadId !== undefined) {
+      expect(typeof autoThreadId).toBe("string");
+      expect(autoThreadId.trim()).not.toBe("");
+    }
+
+    const replyTransport = threading?.resolveReplyTransport?.({
+      cfg: {} as RemoteClawConfig,
+      accountId: "default",
+      threadId: "thread-0",
+      replyToId: "msg-0",
+    });
+    if (replyTransport) {
+      expectReplyTransportShape(replyTransport);
+    }
+
+    const focusedBinding = threading?.resolveFocusedBinding?.({
+      cfg: {} as RemoteClawConfig,
+      accountId: "default",
+      context: {
+        Channel: "group:test",
+        From: "user:test",
+        To: "group:test",
+        ChatType: "group",
+        CurrentMessageId: "msg-1",
+        ReplyToId: "msg-0",
+        ReplyToIdFull: "thread-0",
+        MessageThreadId: "thread-0",
+        NativeChannelId: "native:test",
+      },
+    });
+    if (focusedBinding) {
+      expectFocusedBindingShape(focusedBinding);
+    }
+  });
+}
+
+export function installChannelDirectoryContractSuite(params: {
+  plugin: Pick<ChannelPlugin, "id" | "directory">;
+  invokeLookups?: boolean;
+}) {
+  it("exposes the base directory contract", async () => {
+    const directory = params.plugin.directory;
+    expect(directory).toBeDefined();
+
+    if (params.invokeLookups === false) {
+      return;
+    }
+
+    const self = await directory?.self?.({
+      cfg: {} as RemoteClawConfig,
+      accountId: "default",
+    });
+    if (self) {
+      expectDirectoryEntryShape(self);
+    }
+
+    const peers =
+      (await directory?.listPeers?.({
+        cfg: {} as RemoteClawConfig,
+        accountId: "default",
+        query: "",
+        limit: 5,
+      })) ?? [];
+    expect(Array.isArray(peers)).toBe(true);
+    for (const peer of peers) {
+      expectDirectoryEntryShape(peer);
+    }
+
+    const groups =
+      (await directory?.listGroups?.({
+        cfg: {} as RemoteClawConfig,
+        accountId: "default",
+        query: "",
+        limit: 5,
+      })) ?? [];
+    expect(Array.isArray(groups)).toBe(true);
+    for (const group of groups) {
+      expectDirectoryEntryShape(group);
+    }
+
+    if (directory?.listGroupMembers && groups[0]?.id) {
+      const members = await directory.listGroupMembers({
+        cfg: {} as RemoteClawConfig,
+        accountId: "default",
+        groupId: groups[0].id,
+        query: "",
+        limit: 5,
+      });
+      expect(Array.isArray(members)).toBe(true);
+      for (const member of members) {
+        expectDirectoryEntryShape(member);
+      }
+    }
+  });
+}
+
+export function installSessionBindingContractSuite(params: {
+  getCapabilities: () => SessionBindingCapabilities;
+  bindAndResolve: () => Promise<SessionBindingRecord>;
+  cleanup: () => Promise<void> | void;
+  expectedCapabilities: SessionBindingCapabilities;
+}) {
+  it("registers the expected session binding capabilities", () => {
+    expect(params.getCapabilities()).toEqual(params.expectedCapabilities);
+  });
+
+  it("binds and resolves a session binding through the shared service", async () => {
+    const binding = await params.bindAndResolve();
+    expect(typeof binding.bindingId).toBe("string");
+    expect(binding.bindingId.trim()).not.toBe("");
+    expect(typeof binding.targetSessionKey).toBe("string");
+    expect(binding.targetSessionKey.trim()).not.toBe("");
+    expect(["session", "subagent"]).toContain(binding.targetKind);
+    expect(typeof binding.conversation.channel).toBe("string");
+    expect(typeof binding.conversation.accountId).toBe("string");
+    expect(typeof binding.conversation.conversationId).toBe("string");
+    expect(["active", "ending", "ended"]).toContain(binding.status);
+    expect(typeof binding.boundAt).toBe("number");
+  });
+
+  it("cleans up registered bindings", async () => {
+    await params.cleanup();
+  });
 }
 
 type ChannelSetupContractCase<ResolvedAccount> = {
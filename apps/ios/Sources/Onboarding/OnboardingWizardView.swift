openclaw/openclaw

fix(ios): make pairing instructions generic

apps/ios/Sources/Onboarding/OnboardingWizardView.swift
@@ -536,7 +536,7 @@ struct OnboardingWizardView: View {
                     Text(
                         "Approve this device on the gateway.\n"
                             + "1) `openclaw devices approve` (or `openclaw devices approve <requestId>`)\n"
-                            + "2) `/pair approve` in Telegram\n"
+                            + "2) `/pair approve` in your OpenClaw chat\n"
                             + "\(requestLine)\n"
                             + "OpenClaw will also retry automatically when you return to this app.")
                 }

remoteclaw/remoteclaw

(cherry picked from commit c2e41c57c9d2a92919318d220adad8cbf5e0dd45)

apps/ios/Sources/Onboarding/OnboardingWizardView.swift
@@ -536,7 +536,7 @@ struct OnboardingWizardView: View {
                     Text(
                         "Approve this device on the gateway.\n"
                             + "1) `remoteclaw devices approve` (or `remoteclaw devices approve <requestId>`)\n"
-                            + "2) `/pair approve` in Telegram\n"
+                            + "2) `/pair approve` in your RemoteClaw chat\n"
                             + "\(requestLine)\n"
                             + "RemoteClaw will also retry automatically when you return to this app.")
                 }
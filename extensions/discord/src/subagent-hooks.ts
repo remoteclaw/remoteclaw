wonglaitung/openclaw_mini

refactor: untangle bundled channel sdk bridges

extensions/discord/src/subagent-hooks.ts
@@ -1,4 +1,4 @@
-import type { OpenClawPluginApi } from "openclaw/plugin-sdk/core";
+import type { OpenClawPluginApi } from "../../../src/plugin-sdk-internal/core.js";
 import { resolveDiscordAccount } from "./accounts.js";
 import {
   autoBindSpawnedDiscordSubagent,

remoteclaw/remoteclaw

(cherry picked from commit 7c2c20a62fa8898d97be57c27cd3109957158d87)
# Conflicts:
#	src/plugin-sdk-internal/accounts.ts
#	src/plugin-sdk-internal/telegram.ts

extensions/discord/src/subagent-hooks.ts
@@ -1,4 +1,4 @@
-import type { RemoteClawPluginApi } from "remoteclaw/plugin-sdk/core";
+import type { RemoteClawPluginApi } from "../../../src/plugin-sdk-internal/core.js";
 import { resolveDiscordAccount } from "./accounts.js";
 import {
   autoBindSpawnedDiscordSubagent,
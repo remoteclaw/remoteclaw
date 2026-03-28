Nachx639/clawdbot

refactor: consolidate plugin sdk surface

extensions/discord/src/probe.ts
@@ -1,4 +1,4 @@
-import type { BaseProbeResult } from "openclaw/plugin-sdk/channel-runtime";
+import type { BaseProbeResult } from "openclaw/plugin-sdk/channel-contract";
 import { resolveFetch } from "openclaw/plugin-sdk/infra-runtime";
 import { fetchWithTimeout } from "openclaw/plugin-sdk/text-runtime";
 import { normalizeDiscordToken } from "./token.js";

extensions/telegram/src/bot-access.ts
@@ -2,8 +2,8 @@ import {
   firstDefined,
   isSenderIdAllowed,
   mergeDmAllowFromSources,
-} from "openclaw/plugin-sdk/channel-runtime";
-import type { AllowlistMatch } from "openclaw/plugin-sdk/channel-runtime";
+  type AllowlistMatch,
+} from "openclaw/plugin-sdk/allow-from";
 import { createSubsystemLogger } from "openclaw/plugin-sdk/runtime-env";
 
 export type NormalizedAllowFrom = {

extensions/telegram/src/bot-native-commands.ts
@@ -1,8 +1,19 @@
 import type { Bot, Context } from "grammy";
 import { createChannelReplyPipeline } from "openclaw/plugin-sdk/channel-reply-pipeline";
-import { resolveCommandAuthorizedFromAuthorizers } from "openclaw/plugin-sdk/channel-runtime";
-import { resolveNativeCommandSessionTargets } from "openclaw/plugin-sdk/channel-runtime";
-import { recordInboundSessionMetaSafe } from "openclaw/plugin-sdk/channel-runtime";
+import {
+  resolveCommandAuthorization,
+  resolveCommandAuthorizedFromAuthorizers,
+  resolveNativeCommandSessionTargets,
+} from "openclaw/plugin-sdk/command-auth";
+import {
+  buildCommandTextFromArgs,
+  findCommandByNativeName,
+  listNativeCommandSpecs,
+  listNativeCommandSpecsForConfig,
+  parseCommandArgs,
+  resolveCommandArgMenu,
+  type CommandArgs,
+} from "openclaw/plugin-sdk/command-auth";
 import type { OpenClawConfig } from "openclaw/plugin-sdk/config-runtime";
 import type { ChannelGroupPolicy } from "openclaw/plugin-sdk/config-runtime";
 import { resolveMarkdownTableMode } from "openclaw/plugin-sdk/config-runtime";
@@ -18,7 +29,10 @@ import type {
   TelegramGroupConfig,
   TelegramTopicConfig,
 } from "openclaw/plugin-sdk/config-runtime";
-import { ensureConfiguredBindingRouteReady } from "openclaw/plugin-sdk/conversation-runtime";
+import {
+  ensureConfiguredBindingRouteReady,
+  recordInboundSessionMetaSafe,
+} from "openclaw/plugin-sdk/conversation-runtime";
 import { getAgentScopedMediaLocalRoots } from "openclaw/plugin-sdk/media-runtime";
 import {
   executePluginCommand,
@@ -26,16 +40,6 @@ import {
   matchPluginCommand,
 } from "openclaw/plugin-sdk/plugin-runtime";
 import { resolveChunkMode } from "openclaw/plugin-sdk/reply-runtime";
-import { resolveCommandAuthorization } from "openclaw/plugin-sdk/reply-runtime";
-import type { CommandArgs } from "openclaw/plugin-sdk/reply-runtime";
-import {
-  buildCommandTextFromArgs,
-  findCommandByNativeName,
-  listNativeCommandSpecs,
-  listNativeCommandSpecsForConfig,
-  parseCommandArgs,
-  resolveCommandArgMenu,
-} from "openclaw/plugin-sdk/reply-runtime";
 import { finalizeInboundContext } from "openclaw/plugin-sdk/reply-runtime";
 import { resolveAgentRoute } from "openclaw/plugin-sdk/routing";
 import { resolveThreadSessionKeys } from "openclaw/plugin-sdk/routing";

extensions/whatsapp/src/auto-reply/monitor.ts
@@ -1,13 +1,13 @@
+import { resolveInboundDebounceMs } from "openclaw/plugin-sdk/channel-inbound";
 import { formatCliCommand } from "openclaw/plugin-sdk/cli-runtime";
 import { waitForever } from "openclaw/plugin-sdk/cli-runtime";
+import { hasControlCommand } from "openclaw/plugin-sdk/command-auth";
 import { loadConfig } from "openclaw/plugin-sdk/config-runtime";
 import { createConnectedChannelStatusPatch } from "openclaw/plugin-sdk/gateway-runtime";
 import { formatDurationPrecise } from "openclaw/plugin-sdk/infra-runtime";
 import { enqueueSystemEvent } from "openclaw/plugin-sdk/infra-runtime";
-import { hasControlCommand } from "openclaw/plugin-sdk/reply-runtime";
-import { resolveInboundDebounceMs } from "openclaw/plugin-sdk/reply-runtime";
+import { DEFAULT_GROUP_HISTORY_LIMIT } from "openclaw/plugin-sdk/reply-history";
 import { getReplyFromConfig } from "openclaw/plugin-sdk/reply-runtime";
-import { DEFAULT_GROUP_HISTORY_LIMIT } from "openclaw/plugin-sdk/reply-runtime";
 import { resolveAgentRoute } from "openclaw/plugin-sdk/routing";
 import { logVerbose } from "openclaw/plugin-sdk/runtime-env";
 import { registerUnhandledRejectionHandler } from "openclaw/plugin-sdk/runtime-env";

remoteclaw/remoteclaw

(cherry picked from commit 62ddc9d9e0d9a167b513dd59845cdf0be47bdf3f)
# Conflicts:
#	extensions/chutes/index.ts
#	extensions/discord/src/config-schema.ts
#	extensions/discord/src/group-policy.ts
#	extensions/discord/src/monitor/agent-components-helpers.ts
#	extensions/discord/src/monitor/native-command-ui.ts
#	extensions/discord/src/runtime-api.ts
#	extensions/discord/src/setup-account-state.ts
#	extensions/feishu/runtime-api.ts
#	extensions/googlechat/runtime-api.ts
#	extensions/imessage/runtime-api.ts
#	extensions/imessage/src/channel.runtime.ts
#	extensions/imessage/src/config-schema.ts
#	extensions/irc/src/runtime-api.ts
#	extensions/line/runtime-api.ts
#	extensions/line/src/config-adapter.ts
#	extensions/line/src/group-policy.ts
#	extensions/matrix/src/actions.account-propagation.test.ts
#	extensions/matrix/src/actions.test.ts
#	extensions/matrix/src/channel.setup.test.ts
#	extensions/matrix/src/cli.test.ts
#	extensions/matrix/src/matrix/client/storage.test.ts
#	extensions/matrix/src/matrix/monitor/auto-join.test.ts
#	extensions/matrix/src/matrix/monitor/config.test.ts
#	extensions/matrix/src/matrix/monitor/handler.media-failure.test.ts
#	extensions/matrix/src/matrix/monitor/handler.thread-root-media.test.ts
#	extensions/matrix/src/matrix/monitor/legacy-crypto-restore.test.ts
#	extensions/matrix/src/matrix/thread-bindings-shared.ts
#	extensions/matrix/src/matrix/thread-bindings.test.ts
#	extensions/matrix/src/onboarding.resolve.test.ts
#	extensions/matrix/src/onboarding.test.ts
#	extensions/matrix/src/runtime-api.ts
#	extensions/mattermost/runtime-api.ts
#	extensions/mattermost/src/session-route.ts
#	extensions/minimax/oauth.ts
#	extensions/msteams/runtime-api.ts
#	extensions/nostr/runtime-api.ts
#	extensions/qwen-portal-auth/runtime-api.ts
#	extensions/signal/src/runtime-api.ts
#	extensions/slack/src/channel-actions.ts
#	extensions/slack/src/config-schema.ts
#	extensions/slack/src/group-policy.ts
#	extensions/slack/src/runtime-api.ts
#	extensions/tavily/src/con

extensions/discord/src/probe.ts
@@ -1,4 +1,4 @@
-import type { BaseProbeResult } from "remoteclaw/plugin-sdk/channel-runtime";
+import type { BaseProbeResult } from "remoteclaw/plugin-sdk/channel-contract";
 import { resolveFetch } from "remoteclaw/plugin-sdk/infra-runtime";
 import { fetchWithTimeout } from "remoteclaw/plugin-sdk/text-runtime";
 import { normalizeDiscordToken } from "./token.js";

extensions/telegram/src/bot-access.ts
@@ -2,8 +2,8 @@ import {
   firstDefined,
   isSenderIdAllowed,
   mergeDmAllowFromSources,
-} from "remoteclaw/plugin-sdk/channel-runtime";
-import type { AllowlistMatch } from "remoteclaw/plugin-sdk/channel-runtime";
+  type AllowlistMatch,
+} from "remoteclaw/plugin-sdk/allow-from";
 import { createSubsystemLogger } from "remoteclaw/plugin-sdk/runtime-env";
 
 export type NormalizedAllowFrom = {

extensions/telegram/src/bot-native-commands.ts
@@ -1,8 +1,19 @@
 import type { Bot, Context } from "grammy";
-import { resolveCommandAuthorizedFromAuthorizers } from "remoteclaw/plugin-sdk/channel-runtime";
-import { resolveNativeCommandSessionTargets } from "remoteclaw/plugin-sdk/channel-runtime";
-import { createReplyPrefixOptions } from "remoteclaw/plugin-sdk/channel-runtime";
-import { recordInboundSessionMetaSafe } from "remoteclaw/plugin-sdk/channel-runtime";
+import { createChannelReplyPipeline } from "remoteclaw/plugin-sdk/channel-reply-pipeline";
+import {
+  resolveCommandAuthorization,
+  resolveCommandAuthorizedFromAuthorizers,
+  resolveNativeCommandSessionTargets,
+} from "remoteclaw/plugin-sdk/command-auth";
+import {
+  buildCommandTextFromArgs,
+  findCommandByNativeName,
+  listNativeCommandSpecs,
+  listNativeCommandSpecsForConfig,
+  parseCommandArgs,
+  resolveCommandArgMenu,
+  type CommandArgs,
+} from "remoteclaw/plugin-sdk/command-auth";
 import type { RemoteClawConfig } from "remoteclaw/plugin-sdk/config-runtime";
 import type { ChannelGroupPolicy } from "remoteclaw/plugin-sdk/config-runtime";
 import { resolveMarkdownTableMode } from "remoteclaw/plugin-sdk/config-runtime";
@@ -18,7 +29,10 @@ import type {
   TelegramGroupConfig,
   TelegramTopicConfig,
 } from "remoteclaw/plugin-sdk/config-runtime";
-import { ensureConfiguredAcpRouteReady } from "remoteclaw/plugin-sdk/conversation-runtime";
+import {
+  ensureConfiguredBindingRouteReady,
+  recordInboundSessionMetaSafe,
+} from "remoteclaw/plugin-sdk/conversation-runtime";
 import { getAgentScopedMediaLocalRoots } from "remoteclaw/plugin-sdk/media-runtime";
 import {
   executePluginCommand,
@@ -26,24 +40,13 @@ import {
   matchPluginCommand,
 } from "remoteclaw/plugin-sdk/plugin-runtime";
 import { resolveChunkMode } from "remoteclaw/plugin-sdk/reply-runtime";
-import { resolveCommandAuthorization } from "remoteclaw/plugin-sdk/reply-runtime";
-import type { CommandArgs } from "remoteclaw/plugin-sdk/reply-runtime";
-import {
-  buildCommandTextFromArgs,
-  findCommandByNativeName,
-  listNativeCommandSpecs,
-  listNativeCommandSpecsForConfig,
-  parseCommandArgs,
-  resolveCommandArgMenu,
-} from "remoteclaw/plugin-sdk/reply-runtime";
 import { finalizeInboundContext } from "remoteclaw/plugin-sdk/reply-runtime";
-import { dispatchReplyWithBufferedBlockDispatcher } from "remoteclaw/plugin-sdk/reply-runtime";
-import { listSkillCommandsForAgents } from "remoteclaw/plugin-sdk/reply-runtime";
 import { resolveAgentRoute } from "remoteclaw/plugin-sdk/routing";
 import { resolveThreadSessionKeys } from "remoteclaw/plugin-sdk/routing";
 import { danger, logVerbose } from "remoteclaw/plugin-sdk/runtime-env";
 import { getChildLogger } from "remoteclaw/plugin-sdk/runtime-env";
 import type { RuntimeEnv } from "remoteclaw/plugin-sdk/runtime-env";
+import { resolveTelegramAccount } from "./accounts.js";
 import { withTelegramApiErrorLogging } from "./api-logging.js";
 import { isSenderAllowed, normalizeDmAllowFromWithStore } from "./bot-access.js";
 import type { TelegramMediaRef } from "./bot-message-context.js";

extensions/whatsapp/src/auto-reply/monitor.ts
@@ -1,13 +1,13 @@
+import { resolveInboundDebounceMs } from "remoteclaw/plugin-sdk/channel-inbound";
 import { formatCliCommand } from "remoteclaw/plugin-sdk/cli-runtime";
 import { waitForever } from "remoteclaw/plugin-sdk/cli-runtime";
+import { hasControlCommand } from "remoteclaw/plugin-sdk/command-auth";
 import { loadConfig } from "remoteclaw/plugin-sdk/config-runtime";
 import { createConnectedChannelStatusPatch } from "remoteclaw/plugin-sdk/gateway-runtime";
 import { formatDurationPrecise } from "remoteclaw/plugin-sdk/infra-runtime";
 import { enqueueSystemEvent } from "remoteclaw/plugin-sdk/infra-runtime";
-import { hasControlCommand } from "remoteclaw/plugin-sdk/reply-runtime";
-import { resolveInboundDebounceMs } from "remoteclaw/plugin-sdk/reply-runtime";
+import { DEFAULT_GROUP_HISTORY_LIMIT } from "remoteclaw/plugin-sdk/reply-history";
 import { getReplyFromConfig } from "remoteclaw/plugin-sdk/reply-runtime";
-import { DEFAULT_GROUP_HISTORY_LIMIT } from "remoteclaw/plugin-sdk/reply-runtime";
 import { resolveAgentRoute } from "remoteclaw/plugin-sdk/routing";
 import { logVerbose } from "remoteclaw/plugin-sdk/runtime-env";
 import { registerUnhandledRejectionHandler } from "remoteclaw/plugin-sdk/runtime-env";
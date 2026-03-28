wonglaitung/openclaw_mini

Tests: dedupe contract helper plumbing (#48760)
* Plugins: share contract test helpers

* Channels: collapse inbound contract testkit

src/plugins/contracts/testkit.ts
@@ -1,0 +1,26 @@
+import { createCapturedPluginRegistration } from "../../test-utils/plugin-registration.js";
+import type { OpenClawPluginApi, ProviderPlugin } from "../types.js";
+
+type RegistrablePlugin = {
+  register(api: OpenClawPluginApi): void;
+};
+
+export function registerProviders(...plugins: RegistrablePlugin[]) {
+  const captured = createCapturedPluginRegistration();
+  for (const plugin of plugins) {
+    plugin.register(captured.api);
+  }
+  return captured.providers;
+}
+
+export function requireProvider(providers: ProviderPlugin[], providerId: string) {
+  const provider = providers.find((entry) => entry.id === providerId);
+  if (!provider) {
+    throw new Error(`provider ${providerId} missing`);
+  }
+  return provider;
+}
+
+export function uniqueSortedStrings(values: readonly string[]) {
+  return [...new Set(values)].toSorted((left, right) => left.localeCompare(right));
+}

remoteclaw/remoteclaw

(cherry picked from commit 64c69c3fc963ea9e59b4387b105353ae72a4b8bd)
# Conflicts:
#	extensions/discord/src/monitor/message-handler.inbound-context.test.ts
#	src/plugins/contracts/auth.contract.test.ts
#	src/plugins/contracts/discovery.contract.test.ts
#	src/plugins/contracts/registry.ts
#	test/helpers/dispatch-inbound-capture.ts
#	test/helpers/inbound-contract-capture.ts
#	test/helpers/inbound-contract-dispatch-mock.ts

src/plugins/contracts/testkit.ts
@@ -1,0 +1,26 @@
+import { createCapturedPluginRegistration } from "../../test-utils/plugin-registration.js";
+import type { RemoteClawPluginApi, ProviderPlugin } from "../types.js";
+
+type RegistrablePlugin = {
+  register(api: RemoteClawPluginApi): void;
+};
+
+export function registerProviders(...plugins: RegistrablePlugin[]) {
+  const captured = createCapturedPluginRegistration();
+  for (const plugin of plugins) {
+    plugin.register(captured.api);
+  }
+  return captured.providers;
+}
+
+export function requireProvider(providers: ProviderPlugin[], providerId: string) {
+  const provider = providers.find((entry) => entry.id === providerId);
+  if (!provider) {
+    throw new Error(`provider ${providerId} missing`);
+  }
+  return provider;
+}
+
+export function uniqueSortedStrings(values: readonly string[]) {
+  return [...new Set(values)].toSorted((left, right) => left.localeCompare(right));
+}
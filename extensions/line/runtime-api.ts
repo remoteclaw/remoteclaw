Nachx639/clawdbot

refactor(plugins): move remaining channel and provider ownership out of src

extensions/line/runtime-api.ts
@@ -1,13 +1,39 @@
 // Private runtime barrel for the bundled LINE extension.
 // Keep this barrel thin and aligned with the local extension surface.
 
-export * from "openclaw/plugin-sdk/line";
+export type {
+  ChannelPlugin,
+  OpenClawConfig,
+  OpenClawPluginApi,
+  PluginRuntime,
+} from "openclaw/plugin-sdk/core";
+export { buildChannelConfigSchema, clearAccountEntryFields } from "openclaw/plugin-sdk/core";
+export type { ReplyPayload } from "openclaw/plugin-sdk/reply-runtime";
+export type { ChannelAccountSnapshot, ChannelGatewayContext } from "openclaw/plugin-sdk/testing";
+export type { ChannelStatusIssue } from "openclaw/plugin-sdk/channel-contract";
+export type { ChannelSetupDmPolicy, ChannelSetupWizard } from "openclaw/plugin-sdk/setup";
 export {
-  DEFAULT_ACCOUNT_ID,
-  formatDocsLink,
-  resolveExactLineGroupConfigKey,
-  setSetupChannelEnabled,
-  splitSetupEntries,
-  type ChannelSetupDmPolicy,
-  type ChannelSetupWizard,
-} from "openclaw/plugin-sdk/line-core";
+  buildComputedAccountStatusSnapshot,
+  buildTokenChannelStatusSummary,
+} from "openclaw/plugin-sdk/status-helpers";
+export { DEFAULT_ACCOUNT_ID, formatDocsLink, setSetupChannelEnabled, splitSetupEntries } from "openclaw/plugin-sdk/setup";
+export * from "../../src/plugin-sdk/line-runtime.js";
+
+export * from "./src/accounts.js";
+export * from "./src/actions.js";
+export * from "./src/bot-access.js";
+export * from "./src/channel-access-token.js";
+export * from "./src/config-schema.js";
+export * from "./src/download.js";
+export * from "./src/flex-templates.js";
+export * from "./src/group-keys.js";
+export * from "./src/markdown-to-line.js";
+export * from "./src/probe.js";
+export * from "./src/rich-menu.js";
+export * from "./src/send.js";
+export * from "./src/signature.js";
+export * from "./src/template-messages.js";
+export type { LineChannelData, LineConfig, ResolvedLineAccount } from "./src/types.js";
+export * from "./src/webhook-node.js";
+export * from "./src/webhook.js";
+export * from "./src/webhook-utils.js";

extensions/line/src/send.test.ts
@@ -1,0 +1,228 @@
+import { afterEach, beforeAll, beforeEach, describe, expect, it, vi } from "vitest";
+
+const {
+  pushMessageMock,
+  replyMessageMock,
+  showLoadingAnimationMock,
+  getProfileMock,
+  MessagingApiClientMock,
+  loadConfigMock,
+  resolveLineAccountMock,
+  resolveLineChannelAccessTokenMock,
+  recordChannelActivityMock,
+  logVerboseMock,
+} = vi.hoisted(() => {
+  const pushMessageMock = vi.fn();
+  const replyMessageMock = vi.fn();
+  const showLoadingAnimationMock = vi.fn();
+  const getProfileMock = vi.fn();
+  const MessagingApiClientMock = vi.fn(function () {
+    return {
+      pushMessage: pushMessageMock,
+      replyMessage: replyMessageMock,
+      showLoadingAnimation: showLoadingAnimationMock,
+      getProfile: getProfileMock,
+    };
+  });
+  const loadConfigMock = vi.fn(() => ({}));
+  const resolveLineAccountMock = vi.fn(() => ({ accountId: "default" }));
+  const resolveLineChannelAccessTokenMock = vi.fn(() => "line-token");
+  const recordChannelActivityMock = vi.fn();
+  const logVerboseMock = vi.fn();
+  return {
+    pushMessageMock,
+    replyMessageMock,
+    showLoadingAnimationMock,
+    getProfileMock,
+    MessagingApiClientMock,
+    loadConfigMock,
+    resolveLineAccountMock,
+    resolveLineChannelAccessTokenMock,
+    recordChannelActivityMock,
+    logVerboseMock,
+  };
+});
+
+vi.mock("@line/bot-sdk", () => ({
+  messagingApi: { MessagingApiClient: MessagingApiClientMock },
+}));
+
+vi.mock("openclaw/plugin-sdk/config-runtime", () => ({
+  loadConfig: loadConfigMock,
+}));
+
+vi.mock("./accounts.js", () => ({
+  resolveLineAccount: resolveLineAccountMock,
+}));
+
+vi.mock("./channel-access-token.js", () => ({
+  resolveLineChannelAccessToken: resolveLineChannelAccessTokenMock,
+}));
+
+vi.mock("openclaw/plugin-sdk/infra-runtime", () => ({
+  recordChannelActivity: recordChannelActivityMock,
+}));
+
+vi.mock("openclaw/plugin-sdk/runtime-env", () => ({
+  logVerbose: logVerboseMock,
+}));
+
+let sendModule: typeof import("./send.js");
+
+describe("LINE send helpers", () => {
+  beforeAll(async () => {
+    sendModule = await import("./send.js");
+  });
+
+  beforeEach(() => {
+    pushMessageMock.mockReset();
+    replyMessageMock.mockReset();
+    showLoadingAnimationMock.mockReset();
+    getProfileMock.mockReset();
+    MessagingApiClientMock.mockClear();
+    loadConfigMock.mockReset();
+    resolveLineAccountMock.mockReset();
+    resolveLineChannelAccessTokenMock.mockReset();
+    recordChannelActivityMock.mockReset();
+    logVerboseMock.mockReset();
+
+    loadConfigMock.mockReturnValue({});
+    resolveLineAccountMock.mockReturnValue({ accountId: "default" });
+    resolveLineChannelAccessTokenMock.mockReturnValue("line-token");
+    pushMessageMock.mockResolvedValue({});
+    replyMessageMock.mockResolvedValue({});
+    showLoadingAnimationMock.mockResolvedValue({});
+  });
+
+  afterEach(() => {
+    vi.useRealTimers();
+  });
+
+  it("limits quick reply items to 13", () => {
+    const labels = Array.from({ length: 20 }, (_, index) => `Option ${index + 1}`);
+    const quickReply = sendModule.createQuickReplyItems(labels);
+
+    expect(quickReply.items).toHaveLength(13);
+  });
+
+  it("pushes images via normalized LINE target", async () => {
+    const result = await sendModule.pushImageMessage(
+      "line:user:U123",
+      "https://example.com/original.jpg",
+      undefined,
+      { verbose: true },
+    );
+
+    expect(pushMessageMock).toHaveBeenCalledWith({
+      to: "U123",
+      messages: [
+        {
+          type: "image",
+          originalContentUrl: "https://example.com/original.jpg",
+          previewImageUrl: "https://example.com/original.jpg",
+        },
+      ],
+    });
+    expect(recordChannelActivityMock).toHaveBeenCalledWith({
+      channel: "line",
+      accountId: "default",
+      direction: "outbound",
+    });
+    expect(logVerboseMock).toHaveBeenCalledWith("line: pushed image to U123");
+    expect(result).toEqual({ messageId: "push", chatId: "U123" });
+  });
+
+  it("replies when reply token is provided", async () => {
+    const result = await sendModule.sendMessageLine("line:group:C1", "Hello", {
+      replyToken: "reply-token",
+      mediaUrl: "https://example.com/media.jpg",
+      verbose: true,
+    });
+
+    expect(replyMessageMock).toHaveBeenCalledTimes(1);
+    expect(pushMessageMock).not.toHaveBeenCalled();
+    expect(replyMessageMock).toHaveBeenCalledWith({
+      replyToken: "reply-token",
+      messages: [
+        {
+          type: "image",
+          originalContentUrl: "https://example.com/media.jpg",
+          previewImageUrl: "https://example.com/media.jpg",
+        },
+        {
+          type: "text",
+          text: "Hello",
+        },
+      ],
+    });
+    expect(logVerboseMock).toHaveBeenCalledWith("line: replied to C1");
+    expect(result).toEqual({ messageId: "reply", chatId: "C1" });
+  });
+
+  it("throws when push messages are empty", async () => {
+    await expect(sendModule.pushMessagesLine("U123", [])).rejects.toThrow(
+      "Message must be non-empty for LINE sends",
+    );
+  });
+
+  it("logs HTTP body when push fails", async () => {
+    const err = new Error("LINE push failed") as Error & {
+      status: number;
+      statusText: string;
+      body: string;
+    };
+    err.status = 400;
+    err.statusText = "Bad Request";
+    err.body = "invalid flex payload";
+    pushMessageMock.mockRejectedValueOnce(err);
+
+    await expect(
+      sendModule.pushMessagesLine("U999", [{ type: "text", text: "hello" }]),
+    ).rejects.toThrow("LINE push failed");
+
+    expect(logVerboseMock).toHaveBeenCalledWith(
+      "line: push message failed (400 Bad Request): invalid flex payload",
+    );
+  });
+
+  it("caches profile results by default", async () => {
+    getProfileMock.mockResolvedValue({
+      displayName: "Peter",
+      pictureUrl: "https://example.com/peter.jpg",
+    });
+
+    const first = await sendModule.getUserProfile("U-cache");
+    const second = await sendModule.getUserProfile("U-cache");
+
+    expect(first).toEqual({
+      displayName: "Peter",
+      pictureUrl: "https://example.com/peter.jpg",
+    });
+    expect(second).toEqual(first);
+    expect(getProfileMock).toHaveBeenCalledTimes(1);
+  });
+
+  it("continues when loading animation is unsupported", async () => {
+    showLoadingAnimationMock.mockRejectedValueOnce(new Error("unsupported"));
+
+    await expect(sendModule.showLoadingAnimation("line:room:R1")).resolves.toBeUndefined();
+
+    expect(logVerboseMock).toHaveBeenCalledWith(
+      expect.stringContaining("line: loading animation failed (non-fatal)"),
+    );
+  });
+
+  it("pushes quick-reply text and caps to 13 buttons", async () => {
+    await sendModule.pushTextMessageWithQuickReplies(
+      "U-quick",
+      "Pick one",
+      Array.from({ length: 20 }, (_, index) => `Choice ${index + 1}`),
+    );
+
+    expect(pushMessageMock).toHaveBeenCalledTimes(1);
+    const firstCall = pushMessageMock.mock.calls[0] as [
+      { messages: Array<{ quickReply?: { items: unknown[] } }> },
+    ];
+    expect(firstCall[0].messages[0].quickReply?.items).toHaveLength(13);
+  });
+});

extensions/line/src/webhook.ts
@@ -1,0 +1,111 @@
+import type { WebhookRequestBody } from "@line/bot-sdk";
+import type { NextFunction, Request, Response } from "express";
+import { danger, logVerbose, type RuntimeEnv } from "openclaw/plugin-sdk/runtime";
+import { parseLineWebhookBody, validateLineSignature } from "./webhook-utils.js";
+
+const LINE_WEBHOOK_MAX_RAW_BODY_BYTES = 64 * 1024;
+
+export interface LineWebhookOptions {
+  channelSecret: string;
+  onEvents: (body: WebhookRequestBody) => Promise<void>;
+  runtime?: RuntimeEnv;
+}
+
+function readRawBody(req: Request): string | null {
+  const rawBody =
+    (req as { rawBody?: string | Buffer }).rawBody ??
+    (typeof req.body === "string" || Buffer.isBuffer(req.body) ? req.body : null);
+  if (!rawBody) {
+    return null;
+  }
+  return Buffer.isBuffer(rawBody) ? rawBody.toString("utf-8") : rawBody;
+}
+
+function parseWebhookBody(rawBody?: string | null): WebhookRequestBody | null {
+  if (!rawBody) {
+    return null;
+  }
+  return parseLineWebhookBody(rawBody);
+}
+
+export function createLineWebhookMiddleware(
+  options: LineWebhookOptions,
+): (req: Request, res: Response, _next: NextFunction) => Promise<void> {
+  const { channelSecret, onEvents, runtime } = options;
+
+  return async (req: Request, res: Response, _next: NextFunction): Promise<void> => {
+    try {
+      const signature = req.headers["x-line-signature"];
+
+      if (!signature || typeof signature !== "string") {
+        res.status(400).json({ error: "Missing X-Line-Signature header" });
+        return;
+      }
+
+      const rawBody = readRawBody(req);
+
+      if (!rawBody) {
+        res.status(400).json({ error: "Missing raw request body for signature verification" });
+        return;
+      }
+      if (Buffer.byteLength(rawBody, "utf-8") > LINE_WEBHOOK_MAX_RAW_BODY_BYTES) {
+        res.status(413).json({ error: "Payload too large" });
+        return;
+      }
+
+      if (!validateLineSignature(rawBody, signature, channelSecret)) {
+        logVerbose("line: webhook signature validation failed");
+        res.status(401).json({ error: "Invalid signature" });
+        return;
+      }
+
+      const body = parseWebhookBody(rawBody);
+
+      if (!body) {
+        res.status(400).json({ error: "Invalid webhook payload" });
+        return;
+      }
+
+      if (body.events && body.events.length > 0) {
+        logVerbose(`line: received ${body.events.length} webhook events`);
+        await onEvents(body);
+      }
+
+      res.status(200).json({ status: "ok" });
+    } catch (err) {
+      runtime?.error?.(danger(`line webhook error: ${String(err)}`));
+      if (!res.headersSent) {
+        res.status(500).json({ error: "Internal server error" });
+      }
+    }
+  };
+}
+
+export interface StartLineWebhookOptions {
+  channelSecret: string;
+  onEvents: (body: WebhookRequestBody) => Promise<void>;
+  runtime?: RuntimeEnv;
+  path?: string;
+}
+
+export function startLineWebhook(options: StartLineWebhookOptions): {
+  path: string;
+  handler: (req: Request, res: Response, _next: NextFunction) => Promise<void>;
+} {
+  const channelSecret =
+    typeof options.channelSecret === "string" ? options.channelSecret.trim() : "";
+  if (!channelSecret) {
+    throw new Error(
+      "LINE webhook mode requires a non-empty channel secret. " +
+        "Set channels.line.channelSecret in your config.",
+    );
+  }
+  const path = options.path ?? "/line/webhook";
+  const middleware = createLineWebhookMiddleware({
+    channelSecret,
+    onEvents: options.onEvents,
+    runtime: options.runtime,
+  });
+
+  return { path, handler: middleware };
+}

remoteclaw/remoteclaw

(cherry picked from commit 213198123042a9b660c3540554882a3bdfca7d47)
# Conflicts:
#	extensions/deepgram/index.ts
#	extensions/deepgram/package.json
#	extensions/elevenlabs/speech-provider.ts
#	extensions/fal/image-generation-provider.test.ts
#	extensions/fal/image-generation-provider.ts
#	extensions/fal/index.ts
#	extensions/google/image-generation-provider.test.ts
#	extensions/google/image-generation-provider.ts
#	extensions/groq/index.ts
#	extensions/groq/media-understanding-provider.ts
#	extensions/line/src/config-schema.ts
#	extensions/microsoft/speech-provider.test.ts
#	extensions/microsoft/speech-provider.ts
#	extensions/openai/image-generation-provider.ts
#	extensions/openai/speech-provider.ts
#	src/agents/kilocode-models.ts
#	src/commands/models.ts
#	src/line/config-schema.ts
#	src/media-understanding/apply.echo-transcript.test.ts
#	src/media-understanding/apply.test.ts
#	src/media-understanding/attachments.cache.ts
#	src/media-understanding/providers/audio.test-helpers.ts
#	src/media-understanding/providers/openai-compatible-audio.ts
#	src/media-understanding/providers/shared.ts
#	src/media-understanding/resolve.ts
#	src/media-understanding/runner.entries.ts
#	src/media-understanding/runner.ts
#	src/media-understanding/runtime.ts
#	src/memory/embeddings-openai.ts
#	src/plugin-sdk/line-core.ts
#	src/plugin-sdk/media-understanding.ts
#	src/plugin-sdk/provider-auth-login.runtime.ts
#	src/plugin-sdk/whatsapp-core.ts
#	src/plugins/contracts/auth-choice.contract.test.ts
#	src/stt/audio.test-helpers.ts
#	src/stt/deepgram.audio.live.test.ts
#	src/stt/deepgram.audio.test.ts
#	src/stt/google.video.test.ts
#	src/stt/image-runtime.ts
#	src/stt/image.test.ts
#	src/stt/image.ts
#	src/stt/mistral.provider.test.ts
#	src/stt/moonshot.video.test.ts
#	src/stt/openai-compatible-audio.ts
#	src/stt/openai.audio.test.ts
#	src/stt/provider-registry.test.ts
#	src/stt/provider-registry.ts
#	src/stt/providers/audio.test-helpers.ts
#	src/stt/providers/openai/audio.ts
#	src/stt/provide

extensions/line/runtime-api.ts
@@ -1,13 +1,39 @@
 // Private runtime barrel for the bundled LINE extension.
 // Keep this barrel thin and aligned with the local extension surface.
 
-export * from "../../src/plugin-sdk/line.js";
+export type {
+  ChannelPlugin,
+  RemoteClawConfig,
+  RemoteClawPluginApi,
+  PluginRuntime,
+} from "remoteclaw/plugin-sdk/core";
+export { buildChannelConfigSchema, clearAccountEntryFields } from "remoteclaw/plugin-sdk/core";
+export type { ReplyPayload } from "remoteclaw/plugin-sdk/reply-runtime";
+export type { ChannelAccountSnapshot, ChannelGatewayContext } from "remoteclaw/plugin-sdk/testing";
+export type { ChannelStatusIssue } from "remoteclaw/plugin-sdk/channel-contract";
+export type { ChannelSetupDmPolicy, ChannelSetupWizard } from "remoteclaw/plugin-sdk/setup";
 export {
-  DEFAULT_ACCOUNT_ID,
-  formatDocsLink,
-  resolveExactLineGroupConfigKey,
-  setSetupChannelEnabled,
-  splitSetupEntries,
-  type ChannelSetupDmPolicy,
-  type ChannelSetupWizard,
-} from "../../src/plugin-sdk/line-core.js";
+  buildComputedAccountStatusSnapshot,
+  buildTokenChannelStatusSummary,
+} from "remoteclaw/plugin-sdk/status-helpers";
+export { DEFAULT_ACCOUNT_ID, formatDocsLink, setSetupChannelEnabled, splitSetupEntries } from "remoteclaw/plugin-sdk/setup";
+export * from "../../src/plugin-sdk/line-runtime.js";
+
+export * from "./src/accounts.js";
+export * from "./src/actions.js";
+export * from "./src/bot-access.js";
+export * from "./src/channel-access-token.js";
+export * from "./src/config-schema.js";
+export * from "./src/download.js";
+export * from "./src/flex-templates.js";
+export * from "./src/group-keys.js";
+export * from "./src/markdown-to-line.js";
+export * from "./src/probe.js";
+export * from "./src/rich-menu.js";
+export * from "./src/send.js";
+export * from "./src/signature.js";
+export * from "./src/template-messages.js";
+export type { LineChannelData, LineConfig, ResolvedLineAccount } from "./src/types.js";
+export * from "./src/webhook-node.js";
+export * from "./src/webhook.js";
+export * from "./src/webhook-utils.js";

extensions/line/src/send.test.ts
@@ -1,0 +1,228 @@
+import { afterEach, beforeAll, beforeEach, describe, expect, it, vi } from "vitest";
+
+const {
+  pushMessageMock,
+  replyMessageMock,
+  showLoadingAnimationMock,
+  getProfileMock,
+  MessagingApiClientMock,
+  loadConfigMock,
+  resolveLineAccountMock,
+  resolveLineChannelAccessTokenMock,
+  recordChannelActivityMock,
+  logVerboseMock,
+} = vi.hoisted(() => {
+  const pushMessageMock = vi.fn();
+  const replyMessageMock = vi.fn();
+  const showLoadingAnimationMock = vi.fn();
+  const getProfileMock = vi.fn();
+  const MessagingApiClientMock = vi.fn(function () {
+    return {
+      pushMessage: pushMessageMock,
+      replyMessage: replyMessageMock,
+      showLoadingAnimation: showLoadingAnimationMock,
+      getProfile: getProfileMock,
+    };
+  });
+  const loadConfigMock = vi.fn(() => ({}));
+  const resolveLineAccountMock = vi.fn(() => ({ accountId: "default" }));
+  const resolveLineChannelAccessTokenMock = vi.fn(() => "line-token");
+  const recordChannelActivityMock = vi.fn();
+  const logVerboseMock = vi.fn();
+  return {
+    pushMessageMock,
+    replyMessageMock,
+    showLoadingAnimationMock,
+    getProfileMock,
+    MessagingApiClientMock,
+    loadConfigMock,
+    resolveLineAccountMock,
+    resolveLineChannelAccessTokenMock,
+    recordChannelActivityMock,
+    logVerboseMock,
+  };
+});
+
+vi.mock("@line/bot-sdk", () => ({
+  messagingApi: { MessagingApiClient: MessagingApiClientMock },
+}));
+
+vi.mock("remoteclaw/plugin-sdk/config-runtime", () => ({
+  loadConfig: loadConfigMock,
+}));
+
+vi.mock("./accounts.js", () => ({
+  resolveLineAccount: resolveLineAccountMock,
+}));
+
+vi.mock("./channel-access-token.js", () => ({
+  resolveLineChannelAccessToken: resolveLineChannelAccessTokenMock,
+}));
+
+vi.mock("remoteclaw/plugin-sdk/infra-runtime", () => ({
+  recordChannelActivity: recordChannelActivityMock,
+}));
+
+vi.mock("remoteclaw/plugin-sdk/runtime-env", () => ({
+  logVerbose: logVerboseMock,
+}));
+
+let sendModule: typeof import("./send.js");
+
+describe("LINE send helpers", () => {
+  beforeAll(async () => {
+    sendModule = await import("./send.js");
+  });
+
+  beforeEach(() => {
+    pushMessageMock.mockReset();
+    replyMessageMock.mockReset();
+    showLoadingAnimationMock.mockReset();
+    getProfileMock.mockReset();
+    MessagingApiClientMock.mockClear();
+    loadConfigMock.mockReset();
+    resolveLineAccountMock.mockReset();
+    resolveLineChannelAccessTokenMock.mockReset();
+    recordChannelActivityMock.mockReset();
+    logVerboseMock.mockReset();
+
+    loadConfigMock.mockReturnValue({});
+    resolveLineAccountMock.mockReturnValue({ accountId: "default" });
+    resolveLineChannelAccessTokenMock.mockReturnValue("line-token");
+    pushMessageMock.mockResolvedValue({});
+    replyMessageMock.mockResolvedValue({});
+    showLoadingAnimationMock.mockResolvedValue({});
+  });
+
+  afterEach(() => {
+    vi.useRealTimers();
+  });
+
+  it("limits quick reply items to 13", () => {
+    const labels = Array.from({ length: 20 }, (_, index) => `Option ${index + 1}`);
+    const quickReply = sendModule.createQuickReplyItems(labels);
+
+    expect(quickReply.items).toHaveLength(13);
+  });
+
+  it("pushes images via normalized LINE target", async () => {
+    const result = await sendModule.pushImageMessage(
+      "line:user:U123",
+      "https://example.com/original.jpg",
+      undefined,
+      { verbose: true },
+    );
+
+    expect(pushMessageMock).toHaveBeenCalledWith({
+      to: "U123",
+      messages: [
+        {
+          type: "image",
+          originalContentUrl: "https://example.com/original.jpg",
+          previewImageUrl: "https://example.com/original.jpg",
+        },
+      ],
+    });
+    expect(recordChannelActivityMock).toHaveBeenCalledWith({
+      channel: "line",
+      accountId: "default",
+      direction: "outbound",
+    });
+    expect(logVerboseMock).toHaveBeenCalledWith("line: pushed image to U123");
+    expect(result).toEqual({ messageId: "push", chatId: "U123" });
+  });
+
+  it("replies when reply token is provided", async () => {
+    const result = await sendModule.sendMessageLine("line:group:C1", "Hello", {
+      replyToken: "reply-token",
+      mediaUrl: "https://example.com/media.jpg",
+      verbose: true,
+    });
+
+    expect(replyMessageMock).toHaveBeenCalledTimes(1);
+    expect(pushMessageMock).not.toHaveBeenCalled();
+    expect(replyMessageMock).toHaveBeenCalledWith({
+      replyToken: "reply-token",
+      messages: [
+        {
+          type: "image",
+          originalContentUrl: "https://example.com/media.jpg",
+          previewImageUrl: "https://example.com/media.jpg",
+        },
+        {
+          type: "text",
+          text: "Hello",
+        },
+      ],
+    });
+    expect(logVerboseMock).toHaveBeenCalledWith("line: replied to C1");
+    expect(result).toEqual({ messageId: "reply", chatId: "C1" });
+  });
+
+  it("throws when push messages are empty", async () => {
+    await expect(sendModule.pushMessagesLine("U123", [])).rejects.toThrow(
+      "Message must be non-empty for LINE sends",
+    );
+  });
+
+  it("logs HTTP body when push fails", async () => {
+    const err = new Error("LINE push failed") as Error & {
+      status: number;
+      statusText: string;
+      body: string;
+    };
+    err.status = 400;
+    err.statusText = "Bad Request";
+    err.body = "invalid flex payload";
+    pushMessageMock.mockRejectedValueOnce(err);
+
+    await expect(
+      sendModule.pushMessagesLine("U999", [{ type: "text", text: "hello" }]),
+    ).rejects.toThrow("LINE push failed");
+
+    expect(logVerboseMock).toHaveBeenCalledWith(
+      "line: push message failed (400 Bad Request): invalid flex payload",
+    );
+  });
+
+  it("caches profile results by default", async () => {
+    getProfileMock.mockResolvedValue({
+      displayName: "Peter",
+      pictureUrl: "https://example.com/peter.jpg",
+    });
+
+    const first = await sendModule.getUserProfile("U-cache");
+    const second = await sendModule.getUserProfile("U-cache");
+
+    expect(first).toEqual({
+      displayName: "Peter",
+      pictureUrl: "https://example.com/peter.jpg",
+    });
+    expect(second).toEqual(first);
+    expect(getProfileMock).toHaveBeenCalledTimes(1);
+  });
+
+  it("continues when loading animation is unsupported", async () => {
+    showLoadingAnimationMock.mockRejectedValueOnce(new Error("unsupported"));
+
+    await expect(sendModule.showLoadingAnimation("line:room:R1")).resolves.toBeUndefined();
+
+    expect(logVerboseMock).toHaveBeenCalledWith(
+      expect.stringContaining("line: loading animation failed (non-fatal)"),
+    );
+  });
+
+  it("pushes quick-reply text and caps to 13 buttons", async () => {
+    await sendModule.pushTextMessageWithQuickReplies(
+      "U-quick",
+      "Pick one",
+      Array.from({ length: 20 }, (_, index) => `Choice ${index + 1}`),
+    );
+
+    expect(pushMessageMock).toHaveBeenCalledTimes(1);
+    const firstCall = pushMessageMock.mock.calls[0] as [
+      { messages: Array<{ quickReply?: { items: unknown[] } }> },
+    ];
+    expect(firstCall[0].messages[0].quickReply?.items).toHaveLength(13);
+  });
+});

extensions/line/src/webhook.ts
@@ -1,0 +1,114 @@
+import type { WebhookRequestBody } from "@line/bot-sdk";
+import type { NextFunction, Request, Response } from "express";
+import { danger, logVerbose, type RuntimeEnv } from "remoteclaw/plugin-sdk/runtime";
+import { parseLineWebhookBody, validateLineSignature } from "./webhook-utils.js";
+
+const LINE_WEBHOOK_MAX_RAW_BODY_BYTES = 64 * 1024;
+
+export interface LineWebhookOptions {
+  channelSecret: string;
+  onEvents: (body: WebhookRequestBody) => Promise<void>;
+  runtime?: RuntimeEnv;
+}
+
+function readRawBody(req: Request): string | null {
+  const rawBody =
+    (req as { rawBody?: string | Buffer }).rawBody ??
+    (typeof req.body === "string" || Buffer.isBuffer(req.body) ? req.body : null);
+  if (!rawBody) {
+    return null;
+  }
+  return Buffer.isBuffer(rawBody) ? rawBody.toString("utf-8") : rawBody;
+}
+
+function parseWebhookBody(req: Request, rawBody?: string | null): WebhookRequestBody | null {
+  if (req.body && typeof req.body === "object" && !Buffer.isBuffer(req.body)) {
+    return req.body as WebhookRequestBody;
+  }
+  if (!rawBody) {
+    return null;
+  }
+  return parseLineWebhookBody(rawBody);
+}
+
+export function createLineWebhookMiddleware(
+  options: LineWebhookOptions,
+): (req: Request, res: Response, _next: NextFunction) => Promise<void> {
+  const { channelSecret, onEvents, runtime } = options;
+
+  return async (req: Request, res: Response, _next: NextFunction): Promise<void> => {
+    try {
+      const signature = req.headers["x-line-signature"];
+
+      if (!signature || typeof signature !== "string") {
+        res.status(400).json({ error: "Missing X-Line-Signature header" });
+        return;
+      }
+
+      const rawBody = readRawBody(req);
+
+      if (!rawBody) {
+        res.status(400).json({ error: "Missing raw request body for signature verification" });
+        return;
+      }
+      if (Buffer.byteLength(rawBody, "utf-8") > LINE_WEBHOOK_MAX_RAW_BODY_BYTES) {
+        res.status(413).json({ error: "Payload too large" });
+        return;
+      }
+
+      if (!validateLineSignature(rawBody, signature, channelSecret)) {
+        logVerbose("line: webhook signature validation failed");
+        res.status(401).json({ error: "Invalid signature" });
+        return;
+      }
+
+      const body = parseWebhookBody(rawBody);
+
+      if (!body) {
+        res.status(400).json({ error: "Invalid webhook payload" });
+        return;
+      }
+
+      if (body.events && body.events.length > 0) {
+        logVerbose(`line: received ${body.events.length} webhook events`);
+        await onEvents(body);
+      }
+
+      res.status(200).json({ status: "ok" });
+    } catch (err) {
+      runtime?.error?.(danger(`line webhook error: ${String(err)}`));
+      if (!res.headersSent) {
+        res.status(500).json({ error: "Internal server error" });
+      }
+    }
+  };
+}
+
+export interface StartLineWebhookOptions {
+  channelSecret: string;
+  onEvents: (body: WebhookRequestBody) => Promise<void>;
+  runtime?: RuntimeEnv;
+  path?: string;
+}
+
+export function startLineWebhook(options: StartLineWebhookOptions): {
+  path: string;
+  handler: (req: Request, res: Response, _next: NextFunction) => Promise<void>;
+} {
+  const channelSecret =
+    typeof options.channelSecret === "string" ? options.channelSecret.trim() : "";
+  if (!channelSecret) {
+    throw new Error(
+      "LINE webhook mode requires a non-empty channel secret. " +
+        "Set channels.line.channelSecret in your config.",
+    );
+  }
+  const path = options.path ?? "/line/webhook";
+  const middleware = createLineWebhookMiddleware({
+    channelSecret,
+    onEvents: options.onEvents,
+    runtime: options.runtime,
+  });
+
+  return { path, handler: middleware };
+}
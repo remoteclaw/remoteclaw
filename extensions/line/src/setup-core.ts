wonglaitung/openclaw_mini

refactor: enforce plugin boundary seams

extensions/line/src/setup-core.ts
@@ -1,7 +1,11 @@
-import type { ChannelSetupAdapter, OpenClawConfig } from "openclaw/plugin-sdk/setup";
-import { DEFAULT_ACCOUNT_ID } from "openclaw/plugin-sdk/setup";
-import { normalizeAccountId, resolveLineAccount } from "../../../src/line/accounts.js";
-import type { LineConfig } from "../../../src/line/types.js";
+import {
+  DEFAULT_ACCOUNT_ID,
+  normalizeAccountId,
+  resolveLineAccount,
+  type ChannelSetupAdapter,
+  type LineConfig,
+  type OpenClawConfig,
+} from "openclaw/plugin-sdk/line-core";
 
 const channel = "line" as const;
 
@@ -154,4 +158,4 @@ export const lineSetupAdapter: ChannelSetupAdapter = {
   },
 };
 
-export { listLineAccountIds } from "../../../src/line/accounts.js";
+export { listLineAccountIds } from "openclaw/plugin-sdk/line-core";

extensions/line/src/setup-surface.ts
@@ -1,13 +1,13 @@
 import {
   DEFAULT_ACCOUNT_ID,
   formatDocsLink,
+  resolveLineAccount,
   setSetupChannelEnabled,
   setTopLevelChannelDmPolicyWithAllowFrom,
   splitSetupEntries,
   type ChannelSetupDmPolicy,
   type ChannelSetupWizard,
-} from "openclaw/plugin-sdk/setup";
-import { resolveLineAccount } from "../../../src/line/accounts.js";
+} from "openclaw/plugin-sdk/line-core";
 import {
   isLineConfigured,
   listLineAccountIds,

remoteclaw/remoteclaw

(cherry picked from commit 1313767825cb06f29305e53bc89ff91ee4ddf28f)
# Conflicts:
#	extensions/line/api.ts
#	extensions/nostr/api.ts
#	extensions/synology-chat/api.ts
#	extensions/tlon/api.ts
#	src/plugin-sdk/discord-core.ts
#	src/plugin-sdk/slack-core.ts
#	src/plugin-sdk/telegram-core.ts

extensions/line/src/setup-core.ts
@@ -1,10 +1,11 @@
-import type { ChannelSetupAdapter, RemoteClawConfig } from "remoteclaw/plugin-sdk/setup";
 import {
-  listLineAccountIds,
+  DEFAULT_ACCOUNT_ID,
   normalizeAccountId,
   resolveLineAccount,
+  type ChannelSetupAdapter,
   type LineConfig,
-} from "../../../src/plugin-sdk/line-core.js";
+  type RemoteClawConfig,
+} from "remoteclaw/plugin-sdk/line-core";
 
 const channel = "line" as const;
 
@@ -157,4 +158,4 @@ export const lineSetupAdapter: ChannelSetupAdapter = {
   },
 };
 
-export { listLineAccountIds };
+export { listLineAccountIds } from "remoteclaw/plugin-sdk/line-core";

extensions/line/src/setup-surface.ts
@@ -2,12 +2,13 @@ import { createAllowFromSection, createTopLevelChannelDmPolicy } from "remotecla
 import {
   DEFAULT_ACCOUNT_ID,
   formatDocsLink,
+  resolveLineAccount,
   setSetupChannelEnabled,
   setTopLevelChannelDmPolicyWithAllowFrom,
   splitSetupEntries,
   type ChannelSetupDmPolicy,
   type ChannelSetupWizard,
-} from "../../../src/plugin-sdk/line-core.js";
+} from "remoteclaw/plugin-sdk/line-core";
 import {
   isLineConfigured,
   listLineAccountIds,
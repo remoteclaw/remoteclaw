wonglaitung/openclaw_mini

refactor: finalize plugin sdk legacy boundary cleanup

extensions/discord/src/channel.ts
@@ -11,11 +11,6 @@ import {
 import { resolveOutboundSendDep } from "openclaw/plugin-sdk/channel-runtime";
 import { normalizeMessageChannel } from "openclaw/plugin-sdk/channel-runtime";
 import {
-  buildAgentSessionKey,
-  resolveThreadSessionKeys,
-  type RoutePeer,
-} from "openclaw/plugin-sdk/core";
-import {
   buildComputedAccountStatusSnapshot,
   buildChannelConfigSchema,
   buildTokenChannelStatusSummary,
@@ -33,6 +28,11 @@ import {
   type ChannelPlugin,
   type OpenClawConfig,
 } from "openclaw/plugin-sdk/discord";
+import {
+  buildAgentSessionKey,
+  resolveThreadSessionKeys,
+  type RoutePeer,
+} from "openclaw/plugin-sdk/routing";
 import {
   listDiscordAccountIds,
   resolveDiscordAccount,

extensions/matrix/src/resolve-targets.ts
@@ -1,4 +1,4 @@
-import { mapAllowlistResolutionInputs } from "openclaw/plugin-sdk/compat";
+import { mapAllowlistResolutionInputs } from "openclaw/plugin-sdk/allowlist-resolution";
 import type {
   ChannelDirectoryEntry,
   ChannelResolveKind,

extensions/openai/openai-codex-provider.ts
@@ -4,7 +4,7 @@ import type {
   ProviderResolveDynamicModelContext,
   ProviderRuntimeModel,
 } from "openclaw/plugin-sdk/core";
-import { buildOauthProviderAuthResult } from "openclaw/plugin-sdk/core";
+import { buildOauthProviderAuthResult } from "openclaw/plugin-sdk/provider-auth";
 import {
   CODEX_CLI_PROFILE_ID,
   ensureAuthProfileStore,

extensions/signal/src/channel.ts
@@ -5,8 +5,8 @@ import {
 } from "openclaw/plugin-sdk/channel-config-helpers";
 import { resolveOutboundSendDep } from "openclaw/plugin-sdk/channel-runtime";
 import { resolveMarkdownTableMode } from "openclaw/plugin-sdk/config-runtime";
-import { buildAgentSessionKey, type RoutePeer } from "openclaw/plugin-sdk/core";
 import { resolveTextChunkLimit } from "openclaw/plugin-sdk/reply-runtime";
+import { buildAgentSessionKey, type RoutePeer } from "openclaw/plugin-sdk/routing";
 import {
   buildBaseAccountStatusSnapshot,
   buildBaseChannelStatusSummary,

extensions/telegram/src/channel.ts
@@ -6,13 +6,13 @@ import {
 } from "openclaw/plugin-sdk/channel-config-helpers";
 import { type OutboundSendDeps, resolveOutboundSendDep } from "openclaw/plugin-sdk/channel-runtime";
 import { normalizeMessageChannel } from "openclaw/plugin-sdk/channel-runtime";
+import { resolveExecApprovalCommandDisplay } from "openclaw/plugin-sdk/infra-runtime";
+import { buildExecApprovalPendingReplyPayload } from "openclaw/plugin-sdk/infra-runtime";
 import {
   buildAgentSessionKey,
   resolveThreadSessionKeys,
   type RoutePeer,
-} from "openclaw/plugin-sdk/core";
-import { resolveExecApprovalCommandDisplay } from "openclaw/plugin-sdk/infra-runtime";
-import { buildExecApprovalPendingReplyPayload } from "openclaw/plugin-sdk/infra-runtime";
+} from "openclaw/plugin-sdk/routing";
 import { parseTelegramTopicConversation } from "openclaw/plugin-sdk/telegram";
 import {
   buildTokenChannelStatusSummary,

extensions/zalo/src/runtime.ts
@@ -1,4 +1,4 @@
-import { createPluginRuntimeStore } from "openclaw/plugin-sdk/compat";
+import { createPluginRuntimeStore } from "openclaw/plugin-sdk/runtime-store";
 import type { PluginRuntime } from "openclaw/plugin-sdk/zalo";
 
 const { setRuntime: setZaloRuntime, getRuntime: getZaloRuntime } =

remoteclaw/remoteclaw

(cherry picked from commit f2bd76cd1a4b463799a452e24f470a0db87a44b2)
# Conflicts:
#	extensions/discord/src/shared.ts
#	extensions/feishu/src/directory.static.ts
#	extensions/imessage/src/shared.ts
#	extensions/shared/passive-monitor.ts
#	extensions/signal/src/shared.ts
#	extensions/telegram/src/shared.ts
#	extensions/whatsapp/src/shared.ts
#	extensions/zalo/src/channel.runtime.ts
#	src/agents/models-config.providers.moonshot.test.ts
#	src/auto-reply/reply/elevated-allowlist-matcher.ts
#	src/commands/auth-choice.test.ts

extensions/discord/src/channel.ts
@@ -8,12 +8,6 @@ import {
 import { resolveOutboundSendDep } from "remoteclaw/plugin-sdk/channel-runtime";
 import { normalizeMessageChannel } from "remoteclaw/plugin-sdk/channel-runtime";
 import {
-  buildAgentSessionKey,
-  resolveThreadSessionKeys,
-  type RoutePeer,
-} from "remoteclaw/plugin-sdk/core";
-import {
-  applyAccountNameToChannelSection,
   buildComputedAccountStatusSnapshot,
   buildChannelConfigSchema,
   buildTokenChannelStatusSummary,
@@ -33,6 +27,11 @@ import {
   type ChannelPlugin,
   type RemoteClawConfig,
 } from "remoteclaw/plugin-sdk/discord";
+import {
+  buildAgentSessionKey,
+  resolveThreadSessionKeys,
+  type RoutePeer,
+} from "remoteclaw/plugin-sdk/routing";
 import {
   listDiscordAccountIds,
   resolveDiscordAccount,

extensions/matrix/src/resolve-targets.ts
@@ -1,4 +1,4 @@
-import { mapAllowlistResolutionInputs } from "remoteclaw/plugin-sdk";
+import { mapAllowlistResolutionInputs } from "remoteclaw/plugin-sdk/allowlist-resolution";
 import type {
   ChannelDirectoryEntry,
   ChannelResolveKind,

extensions/openai/openai-codex-provider.ts
@@ -4,7 +4,7 @@ import type {
   ProviderResolveDynamicModelContext,
   ProviderRuntimeModel,
 } from "remoteclaw/plugin-sdk/core";
-import { buildOauthProviderAuthResult } from "remoteclaw/plugin-sdk/core";
+import { buildOauthProviderAuthResult } from "remoteclaw/plugin-sdk/provider-auth";
 import {
   CODEX_CLI_PROFILE_ID,
   ensureAuthProfileStore,

extensions/signal/src/channel.ts
@@ -5,8 +5,8 @@ import {
 } from "remoteclaw/plugin-sdk/channel-config-helpers";
 import { resolveOutboundSendDep } from "remoteclaw/plugin-sdk/channel-runtime";
 import { resolveMarkdownTableMode } from "remoteclaw/plugin-sdk/config-runtime";
-import { buildAgentSessionKey, type RoutePeer } from "remoteclaw/plugin-sdk/core";
 import { resolveTextChunkLimit } from "remoteclaw/plugin-sdk/reply-runtime";
+import { buildAgentSessionKey, type RoutePeer } from "remoteclaw/plugin-sdk/routing";
 import {
   buildAccountScopedDmSecurityPolicy,
   createScopedAccountConfigAccessors,

extensions/telegram/src/channel.ts
@@ -7,13 +7,13 @@ import {
 } from "remoteclaw/plugin-sdk/channel-config-helpers";
 import { type OutboundSendDeps, resolveOutboundSendDep } from "remoteclaw/plugin-sdk/channel-runtime";
 import { normalizeMessageChannel } from "remoteclaw/plugin-sdk/channel-runtime";
+import { resolveExecApprovalCommandDisplay } from "remoteclaw/plugin-sdk/infra-runtime";
+import { buildExecApprovalPendingReplyPayload } from "remoteclaw/plugin-sdk/infra-runtime";
 import {
   buildAgentSessionKey,
   resolveThreadSessionKeys,
   type RoutePeer,
-} from "remoteclaw/plugin-sdk/core";
-import { resolveExecApprovalCommandDisplay } from "remoteclaw/plugin-sdk/infra-runtime";
-import { buildExecApprovalPendingReplyPayload } from "remoteclaw/plugin-sdk/infra-runtime";
+} from "remoteclaw/plugin-sdk/routing";
 import { parseTelegramTopicConversation } from "remoteclaw/plugin-sdk/telegram";
 import {
   buildChannelConfigSchema,

extensions/zalo/src/runtime.ts
@@ -1,5 +1,5 @@
-import { createPluginRuntimeStore } from "remoteclaw/plugin-sdk";
-import type { PluginRuntime } from "remoteclaw/plugin-sdk";
+import { createPluginRuntimeStore } from "remoteclaw/plugin-sdk/runtime-store";
+import type { PluginRuntime } from "remoteclaw/plugin-sdk/zalo";
 
 const { setRuntime: setZaloRuntime, getRuntime: getZaloRuntime } =
   createPluginRuntimeStore<PluginRuntime>("Zalo runtime not initialized");
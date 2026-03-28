wonglaitung/openclaw_mini

refactor: bundle lazy runtime surfaces

src/commands/status.scan.deps.runtime.ts
@@ -1,2 +1,34 @@
-export { getTailnetHostname } from "../infra/tailscale.js";
-export { getMemorySearchManager } from "../memory/index.js";
+import type { OpenClawConfig } from "../config/config.js";
+import { getTailnetHostname } from "../infra/tailscale.js";
+import { getMemorySearchManager as getMemorySearchManagerImpl } from "../memory/index.js";
+import type { MemoryProviderStatus } from "../memory/types.js";
+
+export { getTailnetHostname };
+
+type StatusMemoryManager = {
+  probeVectorAvailability(): Promise<void>;
+  status(): MemoryProviderStatus;
+  close?(): Promise<void>;
+};
+
+export async function getMemorySearchManager(params: {
+  cfg: OpenClawConfig;
+  agentId: string;
+  purpose: "status";
+}): Promise<{ manager: StatusMemoryManager | null }> {
+  const { manager } = await getMemorySearchManagerImpl(params);
+  if (!manager) {
+    return { manager: null };
+  }
+  return {
+    manager: {
+      async probeVectorAvailability() {
+        await manager.probeVectorAvailability();
+      },
+      status() {
+        return manager.status();
+      },
+      close: manager.close ? async () => await manager.close?.() : undefined,
+    },
+  };
+}

remoteclaw/remoteclaw

(cherry picked from commit 3dec814fda938c44cafbcd73ba8525d54cf5c469)
# Conflicts:
#	extensions/bluebubbles/src/channel.runtime.ts
#	extensions/googlechat/src/channel.runtime.ts
#	extensions/matrix/src/channel.runtime.ts
#	extensions/signal/src/channel.runtime.ts
#	extensions/slack/src/channel.runtime.ts
#	src/plugins/runtime/runtime-slack.ts

src/commands/status.scan.deps.runtime.ts
@@ -1,1 +1,34 @@
-export { getTailnetHostname } from "../infra/tailscale.js";
+import type { RemoteClawConfig } from "../config/config.js";
+import { getTailnetHostname } from "../infra/tailscale.js";
+import { getMemorySearchManager as getMemorySearchManagerImpl } from "../memory/index.js";
+import type { MemoryProviderStatus } from "../memory/types.js";
+
+export { getTailnetHostname };
+
+type StatusMemoryManager = {
+  probeVectorAvailability(): Promise<void>;
+  status(): MemoryProviderStatus;
+  close?(): Promise<void>;
+};
+
+export async function getMemorySearchManager(params: {
+  cfg: RemoteClawConfig;
+  agentId: string;
+  purpose: "status";
+}): Promise<{ manager: StatusMemoryManager | null }> {
+  const { manager } = await getMemorySearchManagerImpl(params);
+  if (!manager) {
+    return { manager: null };
+  }
+  return {
+    manager: {
+      async probeVectorAvailability() {
+        await manager.probeVectorAvailability();
+      },
+      status() {
+        return manager.status();
+      },
+      close: manager.close ? async () => await manager.close?.() : undefined,
+    },
+  };
+}
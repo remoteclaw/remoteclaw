wonglaitung/openclaw_mini

refactor: split plugin testing seam from bundled extension helpers

extensions/discord/src/send.test-harness.ts
@@ -1,4 +1,4 @@
-import type { MockFn } from "openclaw/plugin-sdk/test-utils";
+import type { MockFn } from "openclaw/plugin-sdk/testing";
 import { vi } from "vitest";
 
 type DiscordWebMediaMockFactoryResult = {

remoteclaw/remoteclaw

(cherry picked from commit f9588da3e01ccabe5eec09b65a61afc51300031f)
# Conflicts:
#	extensions/amazon-bedrock/index.test.ts
#	extensions/diffs/index.test.ts
#	extensions/diffs/src/http.test.ts
#	extensions/diffs/src/tool.test.ts
#	extensions/discord/src/monitor/provider.test-support.ts
#	extensions/feishu/src/monitor.bot-menu.test.ts
#	extensions/googlechat/src/setup-surface.test.ts
#	extensions/irc/src/channel.startup.test.ts
#	extensions/irc/src/setup-surface.test.ts
#	extensions/line/src/setup-surface.test.ts
#	extensions/mattermost/index.test.ts
#	extensions/nostr/src/setup-surface.test.ts
#	extensions/synology-chat/src/setup-surface.test.ts
#	extensions/talk-voice/index.test.ts
#	extensions/tlon/src/setup-surface.test.ts
#	extensions/zalo/src/setup-surface.test.ts
#	extensions/zalouser/src/setup-surface.test.ts
#	scripts/check-no-extension-test-core-imports.ts
#	test/helpers/extensions/discord-provider.test-support.ts
#	test/helpers/extensions/plugin-command.ts
#	test/helpers/extensions/setup-wizard.ts

extensions/discord/src/send.test-harness.ts
@@ -1,4 +1,4 @@
-import type { MockFn } from "remoteclaw/plugin-sdk/test-utils";
+import type { MockFn } from "remoteclaw/plugin-sdk/testing";
 import { vi } from "vitest";
 
 type DiscordWebMediaMockFactoryResult = {
wonglaitung/openclaw_mini

Setup: trim channel setup import cycles

extensions/imessage/src/accounts.ts
@@ -1,10 +1,10 @@
+import type { OpenClawConfig } from "../../../src/config/config.js";
+import type { IMessageAccountConfig } from "../../../src/config/types.js";
 import {
   createAccountListHelpers,
   normalizeAccountId,
   resolveAccountEntry,
-  type OpenClawConfig,
-} from "openclaw/plugin-sdk/account-resolution";
-import type { IMessageAccountConfig } from "openclaw/plugin-sdk/imessage";
+} from "../../../src/plugin-sdk-internal/accounts.js";
 
 export type ResolvedIMessageAccount = {
   accountId: string;

extensions/signal/src/accounts.ts
@@ -1,10 +1,10 @@
+import type { OpenClawConfig } from "../../../src/config/config.js";
+import type { SignalAccountConfig } from "../../../src/config/types.js";
 import {
   createAccountListHelpers,
   normalizeAccountId,
   resolveAccountEntry,
-  type OpenClawConfig,
-} from "openclaw/plugin-sdk/account-resolution";
-import type { SignalAccountConfig } from "openclaw/plugin-sdk/signal";
+} from "../../../src/plugin-sdk-internal/accounts.js";
 
 export type ResolvedSignalAccount = {
   accountId: string;

extensions/slack/src/accounts.ts
@@ -1,12 +1,12 @@
+import type { OpenClawConfig } from "../../../src/config/config.js";
+import type { SlackAccountConfig } from "../../../src/config/types.slack.js";
 import {
   createAccountListHelpers,
   DEFAULT_ACCOUNT_ID,
   normalizeAccountId,
   normalizeChatType,
   resolveAccountEntry,
-  type OpenClawConfig,
-} from "openclaw/plugin-sdk/account-resolution";
-import type { SlackAccountConfig } from "openclaw/plugin-sdk/slack";
+} from "../../../src/plugin-sdk-internal/accounts.js";
 import type { SlackAccountSurfaceFields } from "./account-surface-fields.js";
 import { resolveSlackAppToken, resolveSlackBotToken, resolveSlackUserToken } from "./token.js";
 

remoteclaw/remoteclaw

(cherry picked from commit 21f5675f031556eb4db447a5c937754c1bf1c8f1)
# Conflicts:
#	src/plugin-sdk-internal/discord.ts
#	src/plugin-sdk-internal/imessage.ts
#	src/plugin-sdk-internal/setup.ts
#	src/plugin-sdk-internal/signal.ts
#	src/plugin-sdk-internal/slack.ts

extensions/imessage/src/accounts.ts
@@ -1,10 +1,10 @@
+import type { RemoteClawConfig } from "../../../src/config/config.js";
+import type { IMessageAccountConfig } from "../../../src/config/types.js";
 import {
   createAccountListHelpers,
   normalizeAccountId,
   resolveAccountEntry,
-  type RemoteClawConfig,
-} from "remoteclaw/plugin-sdk/account-resolution";
-import type { IMessageAccountConfig } from "remoteclaw/plugin-sdk/imessage";
+} from "../../../src/plugin-sdk-internal/accounts.js";
 
 export type ResolvedIMessageAccount = {
   accountId: string;

extensions/signal/src/accounts.ts
@@ -1,10 +1,10 @@
+import type { RemoteClawConfig } from "../../../src/config/config.js";
+import type { SignalAccountConfig } from "../../../src/config/types.js";
 import {
   createAccountListHelpers,
   normalizeAccountId,
   resolveAccountEntry,
-  type RemoteClawConfig,
-} from "remoteclaw/plugin-sdk/account-resolution";
-import type { SignalAccountConfig } from "remoteclaw/plugin-sdk/signal";
+} from "../../../src/plugin-sdk-internal/accounts.js";
 
 export type ResolvedSignalAccount = {
   accountId: string;

extensions/slack/src/accounts.ts
@@ -1,12 +1,12 @@
+import type { RemoteClawConfig } from "../../../src/config/config.js";
+import type { SlackAccountConfig } from "../../../src/config/types.slack.js";
 import {
   createAccountListHelpers,
   DEFAULT_ACCOUNT_ID,
   normalizeAccountId,
   normalizeChatType,
   resolveAccountEntry,
-  type RemoteClawConfig,
-} from "remoteclaw/plugin-sdk/account-resolution";
-import type { SlackAccountConfig } from "remoteclaw/plugin-sdk/slack";
+} from "../../../src/plugin-sdk-internal/accounts.js";
 import type { SlackAccountSurfaceFields } from "./account-surface-fields.js";
 import { resolveSlackAppToken, resolveSlackBotToken, resolveSlackUserToken } from "./token.js";
 
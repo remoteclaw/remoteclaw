wonglaitung/openclaw_mini

refactor: remove channel shim directories, point all imports to extensions (#45967)
* refactor: remove channel shim directories, point all imports to extensions

Delete the 6 backward-compat shim directories (src/telegram, src/discord,
src/slack, src/signal, src/imessage, src/web) that were re-exporting from
extensions. Update all 112+ source files to import directly from
extensions/{channel}/src/ instead of through the shims.

Also:
- Move src/channels/telegram/ (allow-from, api) to extensions/telegram/src/
- Fix outbound adapters to use resolveOutboundSendDep (fixes 5 pre-existing TS errors)
- Update cross-extension imports (src/web/media.js → extensions/whatsapp/src/media.js)
- Update vitest, tsdown, knip, labeler, and script configs for new paths
- Update guard test allowlists for extension paths

After this, src/ has zero channel-specific implementation code — only the
generic plugin framework remains.

* fix: update raw-fetch guard allowlist line numbers after shim removal

* refactor: document direct extension channel imports

* test: mock transcript module in delivery helpers

src/agents/tools/slack-actions.ts
@@ -1,6 +1,5 @@
 import type { AgentToolResult } from "@mariozechner/pi-agent-core";
-import type { OpenClawConfig } from "../../config/config.js";
-import { resolveSlackAccount } from "../../slack/accounts.js";
+import { resolveSlackAccount } from "../../../extensions/slack/src/accounts.js";
 import {
   deleteSlackMessage,
   downloadSlackFile,
@@ -16,10 +15,11 @@ import {
   removeSlackReaction,
   sendSlackMessage,
   unpinSlackMessage,
-} from "../../slack/actions.js";
-import { parseSlackBlocksInput } from "../../slack/blocks-input.js";
-import { recordSlackThreadParticipation } from "../../slack/sent-thread-cache.js";
-import { parseSlackTarget, resolveSlackChannelId } from "../../slack/targets.js";
+} from "../../../extensions/slack/src/actions.js";
+import { parseSlackBlocksInput } from "../../../extensions/slack/src/blocks-input.js";
+import { recordSlackThreadParticipation } from "../../../extensions/slack/src/sent-thread-cache.js";
+import { parseSlackTarget, resolveSlackChannelId } from "../../../extensions/slack/src/targets.js";
+import type { OpenClawConfig } from "../../config/config.js";
 import { withNormalizedTimestamp } from "../date-time.js";
 import {
   createActionGate,

remoteclaw/remoteclaw

(cherry picked from commit 439c21e078f168a3e774013c390e4d97b5404174)
# Conflicts:
#	.github/labeler.yml
#	CHANGELOG.md
#	src/agents/pi-embedded-runner/compact.ts
#	src/agents/pi-embedded-runner/run/attempt.ts
#	src/agents/pi-embedded-runner/run/images.ts
#	src/agents/tools/image-tool.ts
#	src/agents/tools/media-tool-shared.ts
#	src/agents/tools/pdf-tool.test.ts
#	src/agents/tools/pdf-tool.ts
#	src/auto-reply/reply/commands-acp.test.ts
#	src/auto-reply/reply/commands-approve.ts
#	src/auto-reply/reply/commands-models.ts
#	src/auto-reply/reply/directive-handling.model.ts
#	src/auto-reply/reply/slack-directives.ts
#	src/channels/read-only-account-inspect.ts
#	src/infra/exec-approval-forwarder.ts
#	src/infra/exec-approval-surface.test.ts
#	src/infra/exec-approval-surface.ts
#	src/infra/outbound/deliver.test-helpers.ts
#	src/infra/outbound/message-action-runner.media.test.ts
#	src/slack/send.upload.test.ts

src/agents/tools/slack-actions.ts
@@ -1,5 +1,5 @@
-import type { RemoteClawConfig } from "../../config/config.js";
-import { resolveSlackAccount } from "../../slack/accounts.js";
+import type { AgentToolResult } from "@mariozechner/pi-agent-core";
+import { resolveSlackAccount } from "../../../extensions/slack/src/accounts.js";
 import {
   deleteSlackMessage,
   downloadSlackFile,
@@ -15,11 +15,11 @@ import {
   removeSlackReaction,
   sendSlackMessage,
   unpinSlackMessage,
-} from "../../slack/actions.js";
-import { parseSlackBlocksInput } from "../../slack/blocks-input.js";
-import { recordSlackThreadParticipation } from "../../slack/sent-thread-cache.js";
-import { parseSlackTarget, resolveSlackChannelId } from "../../slack/targets.js";
-import type { AgentToolResult } from "../agent-types.js";
+} from "../../../extensions/slack/src/actions.js";
+import { parseSlackBlocksInput } from "../../../extensions/slack/src/blocks-input.js";
+import { recordSlackThreadParticipation } from "../../../extensions/slack/src/sent-thread-cache.js";
+import { parseSlackTarget, resolveSlackChannelId } from "../../../extensions/slack/src/targets.js";
+import type { RemoteClawConfig } from "../../config/config.js";
 import { withNormalizedTimestamp } from "../date-time.js";
 import {
   createActionGate,
wonglaitung/openclaw_mini

Discord: stabilize provider registry coverage

extensions/discord/src/send.creates-thread.test.ts
@@ -1,5 +1,6 @@
 import { RateLimitError } from "@buape/carbon";
 import { ChannelType, Routes } from "discord-api-types/v10";
+import { loadWebMediaRaw } from "openclaw/plugin-sdk/web-media";
 import { beforeEach, describe, expect, it, vi } from "vitest";
 import {
   addRoleDiscord,
@@ -18,7 +19,7 @@ import {
 } from "./send.js";
 import { makeDiscordRest } from "./send.test-harness.js";
 
-vi.mock("../../whatsapp/src/media.js", async () => {
+vi.mock("openclaw/plugin-sdk/web-media", async () => {
   const { discordWebMediaMockFactory } = await import("./send.test-harness.js");
   return discordWebMediaMockFactory();
 });
@@ -288,6 +289,7 @@ describe("uploadEmojiDiscord", () => {
         },
       }),
     );
+    expect(loadWebMediaRaw).toHaveBeenCalledWith("file:///tmp/party.png", 256 * 1024);
   });
 });
 
@@ -325,6 +327,7 @@ describe("uploadStickerDiscord", () => {
         },
       }),
     );
+    expect(loadWebMediaRaw).toHaveBeenCalledWith("file:///tmp/wave.png", 512 * 1024);
   });
 });
 

extensions/discord/src/send.sends-basic-channel-messages.test.ts
@@ -1,6 +1,6 @@
 import { ChannelType, PermissionFlagsBits, Routes } from "discord-api-types/v10";
+import { loadWebMedia } from "openclaw/plugin-sdk/web-media";
 import { beforeEach, describe, expect, it, vi } from "vitest";
-import { loadWebMedia } from "../../whatsapp/src/media.js";
 import {
   __resetDiscordDirectoryCacheForTest,
   rememberDiscordDirectoryUser,
@@ -21,7 +21,7 @@ import {
 } from "./send.js";
 import { makeDiscordRest } from "./send.test-harness.js";
 
-vi.mock("../../whatsapp/src/media.js", async () => {
+vi.mock("openclaw/plugin-sdk/web-media", async () => {
   const { discordWebMediaMockFactory } = await import("./send.test-harness.js");
   return discordWebMediaMockFactory();
 });

remoteclaw/remoteclaw

(cherry picked from commit 513b4869d8de1a69cc371ae5920f18b19d673f51)
# Conflicts:
#	extensions/discord/src/monitor/provider.registry.test.ts
#	src/plugin-sdk/account-helpers.ts

extensions/discord/src/send.creates-thread.test.ts
@@ -1,5 +1,6 @@
 import { RateLimitError } from "@buape/carbon";
 import { ChannelType, Routes } from "discord-api-types/v10";
+import { loadWebMediaRaw } from "remoteclaw/plugin-sdk/web-media";
 import { beforeEach, describe, expect, it, vi } from "vitest";
 import {
   addRoleDiscord,
@@ -18,7 +19,7 @@ import {
 } from "./send.js";
 import { makeDiscordRest } from "./send.test-harness.js";
 
-vi.mock("../../extensions/whatsapp/src/media.js", async () => {
+vi.mock("remoteclaw/plugin-sdk/web-media", async () => {
   const { discordWebMediaMockFactory } = await import("./send.test-harness.js");
   return discordWebMediaMockFactory();
 });
@@ -288,6 +289,7 @@ describe("uploadEmojiDiscord", () => {
         },
       }),
     );
+    expect(loadWebMediaRaw).toHaveBeenCalledWith("file:///tmp/party.png", 256 * 1024);
   });
 });
 
@@ -325,6 +327,7 @@ describe("uploadStickerDiscord", () => {
         },
       }),
     );
+    expect(loadWebMediaRaw).toHaveBeenCalledWith("file:///tmp/wave.png", 512 * 1024);
   });
 });
 

extensions/discord/src/send.sends-basic-channel-messages.test.ts
@@ -1,6 +1,6 @@
 import { ChannelType, PermissionFlagsBits, Routes } from "discord-api-types/v10";
+import { loadWebMedia } from "remoteclaw/plugin-sdk/web-media";
 import { beforeEach, describe, expect, it, vi } from "vitest";
-import { loadWebMedia } from "../../whatsapp/src/media.js";
 import {
   __resetDiscordDirectoryCacheForTest,
   rememberDiscordDirectoryUser,
@@ -21,7 +21,7 @@ import {
 } from "./send.js";
 import { makeDiscordRest } from "./send.test-harness.js";
 
-vi.mock("../../extensions/whatsapp/src/media.js", async () => {
+vi.mock("remoteclaw/plugin-sdk/web-media", async () => {
   const { discordWebMediaMockFactory } = await import("./send.test-harness.js");
   return discordWebMediaMockFactory();
 });
wonglaitung/openclaw_mini

fix(plugin-sdk): isolate provider entry surfaces

extensions/google/gemini-cli-provider.ts
@@ -2,7 +2,7 @@ import type {
   OpenClawPluginApi,
   ProviderAuthContext,
   ProviderFetchUsageSnapshotContext,
-} from "openclaw/plugin-sdk/core";
+} from "openclaw/plugin-sdk/plugin-entry";
 import { buildOauthProviderAuthResult } from "openclaw/plugin-sdk/provider-auth";
 import { fetchGeminiUsage } from "openclaw/plugin-sdk/provider-usage";
 import { loginGeminiCliOAuth } from "./oauth.js";

extensions/google/index.ts
@@ -1,5 +1,5 @@
-import { definePluginEntry } from "openclaw/plugin-sdk/core";
 import { buildGoogleImageGenerationProvider } from "openclaw/plugin-sdk/image-generation";
+import { definePluginEntry } from "openclaw/plugin-sdk/plugin-entry";
 import { createProviderApiKeyAuthMethod } from "openclaw/plugin-sdk/provider-auth";
 import {
   GOOGLE_GEMINI_DEFAULT_MODEL,

extensions/moonshot/index.ts
@@ -1,4 +1,4 @@
-import { definePluginEntry } from "openclaw/plugin-sdk/core";
+import { definePluginEntry } from "openclaw/plugin-sdk/plugin-entry";
 import { createProviderApiKeyAuthMethod } from "openclaw/plugin-sdk/provider-auth";
 import { buildSingleProviderApiKeyCatalog } from "openclaw/plugin-sdk/provider-catalog";
 import {

extensions/openai/index.ts
@@ -1,5 +1,5 @@
-import { definePluginEntry } from "openclaw/plugin-sdk/core";
 import { buildOpenAIImageGenerationProvider } from "openclaw/plugin-sdk/image-generation";
+import { definePluginEntry } from "openclaw/plugin-sdk/plugin-entry";
 import { buildOpenAISpeechProvider } from "openclaw/plugin-sdk/speech";
 import { openaiMediaUnderstandingProvider } from "./media-understanding-provider.js";
 import { buildOpenAICodexProviderPlugin } from "./openai-codex-provider.js";

extensions/openai/openai-provider.ts
@@ -1,7 +1,7 @@
 import {
   type ProviderResolveDynamicModelContext,
   type ProviderRuntimeModel,
-} from "openclaw/plugin-sdk/core";
+} from "openclaw/plugin-sdk/plugin-entry";
 import { createProviderApiKeyAuthMethod } from "openclaw/plugin-sdk/provider-auth";
 import {
   applyOpenAIConfig,

extensions/xai/index.ts
@@ -1,4 +1,4 @@
-import { definePluginEntry } from "openclaw/plugin-sdk/core";
+import { definePluginEntry } from "openclaw/plugin-sdk/plugin-entry";
 import { createProviderApiKeyAuthMethod } from "openclaw/plugin-sdk/provider-auth-api-key";
 import { buildSingleProviderApiKeyCatalog } from "openclaw/plugin-sdk/provider-catalog";
 import { applyXaiModelCompat } from "openclaw/plugin-sdk/provider-models";

remoteclaw/remoteclaw

(cherry picked from commit 6ebcd853be0196277f74146f10dc0470e363af3e)
# Conflicts:
#	extensions/amazon-bedrock/index.ts

extensions/google/gemini-cli-provider.ts
@@ -2,7 +2,7 @@ import type {
   RemoteClawPluginApi,
   ProviderAuthContext,
   ProviderFetchUsageSnapshotContext,
-} from "remoteclaw/plugin-sdk/core";
+} from "remoteclaw/plugin-sdk/plugin-entry";
 import { buildOauthProviderAuthResult } from "remoteclaw/plugin-sdk/provider-auth";
 import { fetchGeminiUsage } from "remoteclaw/plugin-sdk/provider-usage";
 import { loginGeminiCliOAuth } from "./oauth.js";

extensions/google/index.ts
@@ -1,4 +1,5 @@
-import { emptyPluginConfigSchema, type RemoteClawPluginApi } from "remoteclaw/plugin-sdk/core";
+import { buildGoogleImageGenerationProvider } from "remoteclaw/plugin-sdk/image-generation";
+import { definePluginEntry } from "remoteclaw/plugin-sdk/plugin-entry";
 import { createProviderApiKeyAuthMethod } from "remoteclaw/plugin-sdk/provider-auth";
 import {
   GOOGLE_GEMINI_DEFAULT_MODEL,

extensions/moonshot/index.ts
@@ -1,5 +1,6 @@
-import { emptyPluginConfigSchema, type RemoteClawPluginApi } from "remoteclaw/plugin-sdk/core";
+import { definePluginEntry } from "remoteclaw/plugin-sdk/plugin-entry";
 import { createProviderApiKeyAuthMethod } from "remoteclaw/plugin-sdk/provider-auth";
+import { buildSingleProviderApiKeyCatalog } from "remoteclaw/plugin-sdk/provider-catalog";
 import {
   createMoonshotThinkingWrapper,
   resolveMoonshotThinkingType,

extensions/openai/index.ts
@@ -1,5 +1,7 @@
-import { emptyPluginConfigSchema, type RemoteClawPluginApi } from "remoteclaw/plugin-sdk/core";
+import { buildOpenAIImageGenerationProvider } from "remoteclaw/plugin-sdk/image-generation";
+import { definePluginEntry } from "remoteclaw/plugin-sdk/plugin-entry";
 import { buildOpenAISpeechProvider } from "remoteclaw/plugin-sdk/speech";
+import { openaiMediaUnderstandingProvider } from "./media-understanding-provider.js";
 import { buildOpenAICodexProviderPlugin } from "./openai-codex-provider.js";
 import { buildOpenAIProvider } from "./openai-provider.js";
 

extensions/openai/openai-provider.ts
@@ -1,7 +1,7 @@
 import {
   type ProviderResolveDynamicModelContext,
   type ProviderRuntimeModel,
-} from "remoteclaw/plugin-sdk/core";
+} from "remoteclaw/plugin-sdk/plugin-entry";
 import { createProviderApiKeyAuthMethod } from "remoteclaw/plugin-sdk/provider-auth";
 import {
   applyOpenAIConfig,

extensions/xai/index.ts
@@ -1,6 +1,11 @@
-import { emptyPluginConfigSchema, type RemoteClawPluginApi } from "remoteclaw/plugin-sdk/core";
-import { createProviderApiKeyAuthMethod } from "remoteclaw/plugin-sdk/provider-auth";
-import { normalizeProviderId } from "remoteclaw/plugin-sdk/provider-models";
+import { definePluginEntry } from "remoteclaw/plugin-sdk/plugin-entry";
+import { createProviderApiKeyAuthMethod } from "remoteclaw/plugin-sdk/provider-auth-api-key";
+import { buildSingleProviderApiKeyCatalog } from "remoteclaw/plugin-sdk/provider-catalog";
+import { applyXaiModelCompat } from "remoteclaw/plugin-sdk/provider-models";
+import { createToolStreamWrapper } from "remoteclaw/plugin-sdk/provider-stream";
+import { applyXaiConfig, XAI_DEFAULT_MODEL_REF } from "./onboard.js";
+import { buildXaiProvider } from "./provider-catalog.js";
+import { isModernXaiModel, resolveXaiForwardCompatModel } from "./provider-models.js";
 import {
   createPluginBackedWebSearchProvider,
   getScopedCredentialValue,
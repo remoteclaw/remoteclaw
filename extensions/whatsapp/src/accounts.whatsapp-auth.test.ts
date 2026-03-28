wonglaitung/openclaw_mini

refactor: move WhatsApp channel implementation to extensions/ (#45725)
* refactor: move WhatsApp channel from src/web/ to extensions/whatsapp/

Move all WhatsApp implementation code (77 source/test files + 9 channel
plugin files) from src/web/ and src/channels/plugins/*/whatsapp* to
extensions/whatsapp/src/.

- Leave thin re-export shims at all original locations so cross-cutting
  imports continue to resolve
- Update plugin-sdk/whatsapp.ts to only re-export generic framework
  utilities; channel-specific functions imported locally by the extension
- Update vi.mock paths in 15 cross-cutting test files
- Rename outbound.ts -> send.ts to match extension naming conventions
  and avoid false positive in cfg-threading guard test
- Widen tsconfig.plugin-sdk.dts.json rootDir to support shim->extension
  cross-directory references

Part of the core-channels-to-extensions migration (PR 6/10).

* style: format WhatsApp extension files

* fix: correct stale import paths in WhatsApp extension tests

Fix vi.importActual, test mock, and hardcoded source paths that weren't
updated during the file move:
- media.test.ts: vi.importActual path
- onboarding.test.ts: vi.importActual path
- test-helpers.ts: test/mocks/baileys.js path
- monitor-inbox.test-harness.ts: incomplete media/store mock
- login.test.ts: hardcoded source file path
- message-action-runner.media.test.ts: vi.mock/importActual path

extensions/whatsapp/src/accounts.whatsapp-auth.test.ts
@@ -1,0 +1,59 @@
+import fs from "node:fs";
+import os from "node:os";
+import path from "node:path";
+import { afterEach, beforeEach, describe, expect, it } from "vitest";
+import { captureEnv } from "../../../src/test-utils/env.js";
+import { hasAnyWhatsAppAuth, listWhatsAppAuthDirs } from "./accounts.js";
+
+describe("hasAnyWhatsAppAuth", () => {
+  let envSnapshot: ReturnType<typeof captureEnv>;
+  let tempOauthDir: string | undefined;
+
+  const writeCreds = (dir: string) => {
+    fs.mkdirSync(dir, { recursive: true });
+    fs.writeFileSync(path.join(dir, "creds.json"), JSON.stringify({ me: {} }));
+  };
+
+  beforeEach(() => {
+    envSnapshot = captureEnv(["OPENCLAW_OAUTH_DIR"]);
+    tempOauthDir = fs.mkdtempSync(path.join(os.tmpdir(), "openclaw-oauth-"));
+    process.env.OPENCLAW_OAUTH_DIR = tempOauthDir;
+  });
+
+  afterEach(() => {
+    envSnapshot.restore();
+    if (tempOauthDir) {
+      fs.rmSync(tempOauthDir, { recursive: true, force: true });
+      tempOauthDir = undefined;
+    }
+  });
+
+  it("returns false when no auth exists", () => {
+    expect(hasAnyWhatsAppAuth({})).toBe(false);
+  });
+
+  it("returns true when legacy auth exists", () => {
+    fs.writeFileSync(path.join(tempOauthDir ?? "", "creds.json"), JSON.stringify({ me: {} }));
+    expect(hasAnyWhatsAppAuth({})).toBe(true);
+  });
+
+  it("returns true when non-default auth exists", () => {
+    writeCreds(path.join(tempOauthDir ?? "", "whatsapp", "work"));
+    expect(hasAnyWhatsAppAuth({})).toBe(true);
+  });
+
+  it("includes authDir overrides", () => {
+    const customDir = fs.mkdtempSync(path.join(os.tmpdir(), "openclaw-wa-auth-"));
+    try {
+      writeCreds(customDir);
+      const cfg = {
+        channels: { whatsapp: { accounts: { work: { authDir: customDir } } } },
+      };
+
+      expect(listWhatsAppAuthDirs(cfg)).toContain(customDir);
+      expect(hasAnyWhatsAppAuth(cfg)).toBe(true);
+    } finally {
+      fs.rmSync(customDir, { recursive: true, force: true });
+    }
+  });
+});

remoteclaw/remoteclaw

(cherry picked from commit 16505718e8278e6c8dff0e5227a5cb5a9f7c56df)

extensions/whatsapp/src/accounts.whatsapp-auth.test.ts
@@ -1,0 +1,59 @@
+import fs from "node:fs";
+import os from "node:os";
+import path from "node:path";
+import { afterEach, beforeEach, describe, expect, it } from "vitest";
+import { captureEnv } from "../../../src/test-utils/env.js";
+import { hasAnyWhatsAppAuth, listWhatsAppAuthDirs } from "./accounts.js";
+
+describe("hasAnyWhatsAppAuth", () => {
+  let envSnapshot: ReturnType<typeof captureEnv>;
+  let tempOauthDir: string | undefined;
+
+  const writeCreds = (dir: string) => {
+    fs.mkdirSync(dir, { recursive: true });
+    fs.writeFileSync(path.join(dir, "creds.json"), JSON.stringify({ me: {} }));
+  };
+
+  beforeEach(() => {
+    envSnapshot = captureEnv(["REMOTECLAW_OAUTH_DIR"]);
+    tempOauthDir = fs.mkdtempSync(path.join(os.tmpdir(), "remoteclaw-oauth-"));
+    process.env.REMOTECLAW_OAUTH_DIR = tempOauthDir;
+  });
+
+  afterEach(() => {
+    envSnapshot.restore();
+    if (tempOauthDir) {
+      fs.rmSync(tempOauthDir, { recursive: true, force: true });
+      tempOauthDir = undefined;
+    }
+  });
+
+  it("returns false when no auth exists", () => {
+    expect(hasAnyWhatsAppAuth({})).toBe(false);
+  });
+
+  it("returns true when legacy auth exists", () => {
+    fs.writeFileSync(path.join(tempOauthDir ?? "", "creds.json"), JSON.stringify({ me: {} }));
+    expect(hasAnyWhatsAppAuth({})).toBe(true);
+  });
+
+  it("returns true when non-default auth exists", () => {
+    writeCreds(path.join(tempOauthDir ?? "", "whatsapp", "work"));
+    expect(hasAnyWhatsAppAuth({})).toBe(true);
+  });
+
+  it("includes authDir overrides", () => {
+    const customDir = fs.mkdtempSync(path.join(os.tmpdir(), "remoteclaw-wa-auth-"));
+    try {
+      writeCreds(customDir);
+      const cfg = {
+        channels: { whatsapp: { accounts: { work: { authDir: customDir } } } },
+      };
+
+      expect(listWhatsAppAuthDirs(cfg)).toContain(customDir);
+      expect(hasAnyWhatsAppAuth(cfg)).toBe(true);
+    } finally {
+      fs.rmSync(customDir, { recursive: true, force: true });
+    }
+  });
+});
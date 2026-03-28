Nachx639/clawdbot

test: align extension runtime mocks with plugin-sdk (#51289)
* test: align extension runtime mocks with plugin-sdk

Update stale extension tests to mock the plugin-sdk runtime barrels that production code now imports, and harden the Signal tool-result harness around system-event assertions so the channels lane matches current extension boundaries.

Regeneration-Prompt: |
  Verify the failing channels-lane tests against current origin/main in an isolated worktree before changing anything. If the failures reproduce on main, keep the fix test-only unless production behavior is clearly wrong. Recent extension refactors moved Telegram, WhatsApp, and Signal code onto plugin-sdk runtime barrels, so update stale tests that still mock old core module paths to intercept the seams production code now uses. For Signal reaction notifications, avoid brittle assertions that depend on shared queued system-event state when a direct harness spy on enqueue behavior is sufficient. Preserve scope: only touch the failing tests and their local harness, then rerun the reproduced targeted tests plus the full channels lane and repo check gate.

* test: fix extension test drift on main

* fix: lazy-load bundled web search plugin registry

* test: make matrix sweeper failure injection portable

* fix: split heavy matrix runtime-api seams

* fix: simplify bundled web search id lookup

* test: tolerate windows env key casing

extensions/matrix/runtime-api.ts
@@ -11,7 +11,7 @@ export {
   ssrfPolicyFromAllowPrivateNetwork,
   type LookupFn,
   type SsrFPolicy,
-} from "openclaw/plugin-sdk/infra-runtime";
+} from "openclaw/plugin-sdk/ssrf-runtime";
 export {
   setMatrixThreadBindingIdleTimeoutBySessionKey,
   setMatrixThreadBindingMaxAgeBySessionKey,

extensions/matrix/src/runtime-api.ts
@@ -8,6 +8,12 @@ export {
   type LookupFn,
   type SsrFPolicy,
 } from "openclaw/plugin-sdk/infra-runtime";
+export {
+  dispatchReplyFromConfigWithSettledDispatcher,
+  ensureConfiguredAcpBindingReady,
+  maybeCreateMatrixMigrationSnapshot,
+  resolveConfiguredAcpBindingRecord,
+} from "openclaw/plugin-sdk/matrix-runtime-heavy";
 // Keep auth-precedence available internally without re-exporting helper-api
 // twice through both plugin-sdk/matrix and ../runtime-api.js.
 export * from "./auth-precedence.js";

extensions/telegram/src/send.proxy.test.ts
@@ -21,8 +21,10 @@ const { resolveTelegramFetch } = vi.hoisted(() => ({
   resolveTelegramFetch: vi.fn(),
 }));
 
-vi.mock("../../../src/config/config.js", async (importOriginal) => {
-  const actual = await importOriginal<typeof import("../../../src/config/config.js")>();
+vi.mock("openclaw/plugin-sdk/config-runtime", async () => {
+  const actual = await vi.importActual<typeof import("openclaw/plugin-sdk/config-runtime")>(
+    "openclaw/plugin-sdk/config-runtime",
+  );
   return {
     ...actual,
     loadConfig,

src/plugin-sdk/matrix-runtime-shared.ts
@@ -1,0 +1,11 @@
+// Narrow shared Matrix runtime exports for light runtime-api consumers.
+
+export type {
+  ChannelDirectoryEntry,
+  ChannelMessageActionContext,
+} from "../channels/plugins/types.js";
+export type { OpenClawConfig } from "../config/config.js";
+export { formatZonedTimestamp } from "../infra/format-time/format-datetime.js";
+export type { PluginRuntime, RuntimeLogger } from "../plugins/runtime/types.js";
+export type { RuntimeEnv } from "../runtime.js";
+export type { WizardPrompter } from "../wizard/prompts.js";

src/plugin-sdk/subpaths.test.ts
@@ -36,6 +36,7 @@ import type {
 import * as directoryRuntimeSdk from "openclaw/plugin-sdk/directory-runtime";
 import * as infraRuntimeSdk from "openclaw/plugin-sdk/infra-runtime";
 import * as lazyRuntimeSdk from "openclaw/plugin-sdk/lazy-runtime";
+import * as matrixRuntimeSharedSdk from "openclaw/plugin-sdk/matrix-runtime-shared";
 import * as mediaRuntimeSdk from "openclaw/plugin-sdk/media-runtime";
 import * as ollamaSetupSdk from "openclaw/plugin-sdk/ollama-setup";
 import * as providerAuthSdk from "openclaw/plugin-sdk/provider-auth";
@@ -50,7 +51,9 @@ import * as sandboxSdk from "openclaw/plugin-sdk/sandbox";
 import * as secretInputSdk from "openclaw/plugin-sdk/secret-input";
 import * as selfHostedProviderSetupSdk from "openclaw/plugin-sdk/self-hosted-provider-setup";
 import * as setupSdk from "openclaw/plugin-sdk/setup";
+import * as ssrfRuntimeSdk from "openclaw/plugin-sdk/ssrf-runtime";
 import * as testingSdk from "openclaw/plugin-sdk/testing";
+import * as threadBindingsRuntimeSdk from "openclaw/plugin-sdk/thread-bindings-runtime";
 import * as webhookIngressSdk from "openclaw/plugin-sdk/webhook-ingress";
 import { describe, expect, expectTypeOf, it } from "vitest";
 import type { ChannelMessageActionContext } from "../channels/plugins/types.js";
@@ -521,6 +524,22 @@ describe("plugin-sdk subpath exports", () => {
     expect(typeof conversationRuntimeSdk.createScopedAccountReplyToModeResolver).toBe("function");
     expect(typeof conversationRuntimeSdk.createStaticReplyToModeResolver).toBe("function");
     expect(typeof conversationRuntimeSdk.createTopLevelChannelReplyToModeResolver).toBe("function");
+  });
+
+  it("exports narrow binding lifecycle helpers from the dedicated subpath", () => {
+    expect(typeof threadBindingsRuntimeSdk.resolveThreadBindingLifecycle).toBe("function");
+  });
+
+  it("exports narrow matrix runtime helpers from the dedicated subpath", () => {
+    expect(typeof matrixRuntimeSharedSdk.formatZonedTimestamp).toBe("function");
+  });
+
+  it("exports narrow ssrf helpers from the dedicated subpath", () => {
+    expect(typeof ssrfRuntimeSdk.closeDispatcher).toBe("function");
+    expect(typeof ssrfRuntimeSdk.createPinnedDispatcher).toBe("function");
+    expect(typeof ssrfRuntimeSdk.resolvePinnedHostnameWithPolicy).toBe("function");
+    expect(typeof ssrfRuntimeSdk.assertHttpUrlTargetsPrivateNetwork).toBe("function");
+    expect(typeof ssrfRuntimeSdk.ssrfPolicyFromAllowPrivateNetwork).toBe("function");
   });
 
   it("exports provider setup helpers from the dedicated subpath", () => {

remoteclaw/remoteclaw

(cherry picked from commit 2364e45fe4a1900a98982cc758f588843224a3b3)
# Conflicts:
#	src/bundled-web-search-registry.ts
#	src/plugins/bundled-web-search.ts

extensions/matrix/runtime-api.ts
@@ -11,8 +11,7 @@ export {
   ssrfPolicyFromAllowPrivateNetwork,
   type LookupFn,
   type SsrFPolicy,
-} from "remoteclaw/plugin-sdk/infra-runtime";
-export { formatZonedTimestamp } from "../../src/infra/format-time/format-datetime.js";
+} from "remoteclaw/plugin-sdk/ssrf-runtime";
 export {
   setMatrixThreadBindingIdleTimeoutBySessionKey,
   setMatrixThreadBindingMaxAgeBySessionKey,

extensions/matrix/src/runtime-api.ts
@@ -8,6 +8,12 @@ export {
   type LookupFn,
   type SsrFPolicy,
 } from "remoteclaw/plugin-sdk/infra-runtime";
+export {
+  dispatchReplyFromConfigWithSettledDispatcher,
+  ensureConfiguredAcpBindingReady,
+  maybeCreateMatrixMigrationSnapshot,
+  resolveConfiguredAcpBindingRecord,
+} from "remoteclaw/plugin-sdk/matrix-runtime-heavy";
 // Keep auth-precedence available internally without re-exporting helper-api
 // twice through both plugin-sdk/matrix and ../runtime-api.js.
 export * from "./auth-precedence.js";

extensions/telegram/src/send.proxy.test.ts
@@ -21,8 +21,10 @@ const { resolveTelegramFetch } = vi.hoisted(() => ({
   resolveTelegramFetch: vi.fn(),
 }));
 
-vi.mock("../../../src/config/config.js", async (importOriginal) => {
-  const actual = await importOriginal<typeof import("../../../src/config/config.js")>();
+vi.mock("remoteclaw/plugin-sdk/config-runtime", async () => {
+  const actual = await vi.importActual<typeof import("remoteclaw/plugin-sdk/config-runtime")>(
+    "remoteclaw/plugin-sdk/config-runtime",
+  );
   return {
     ...actual,
     loadConfig,

src/plugin-sdk/matrix-runtime-shared.ts
@@ -1,0 +1,11 @@
+// Narrow shared Matrix runtime exports for light runtime-api consumers.
+
+export type {
+  ChannelDirectoryEntry,
+  ChannelMessageActionContext,
+} from "../channels/plugins/types.js";
+export type { RemoteClawConfig } from "../config/config.js";
+export { formatZonedTimestamp } from "../infra/format-time/format-datetime.js";
+export type { PluginRuntime, RuntimeLogger } from "../plugins/runtime/types.js";
+export type { RuntimeEnv } from "../runtime.js";
+export type { WizardPrompter } from "../wizard/prompts.js";

src/plugin-sdk/subpaths.test.ts
@@ -36,6 +36,7 @@ import type {
 import * as directoryRuntimeSdk from "remoteclaw/plugin-sdk/directory-runtime";
 import * as infraRuntimeSdk from "remoteclaw/plugin-sdk/infra-runtime";
 import * as lazyRuntimeSdk from "remoteclaw/plugin-sdk/lazy-runtime";
+import * as matrixRuntimeSharedSdk from "remoteclaw/plugin-sdk/matrix-runtime-shared";
 import * as mediaRuntimeSdk from "remoteclaw/plugin-sdk/media-runtime";
 import * as ollamaSetupSdk from "remoteclaw/plugin-sdk/ollama-setup";
 import * as providerAuthSdk from "remoteclaw/plugin-sdk/provider-auth";
@@ -50,7 +51,9 @@ import * as sandboxSdk from "remoteclaw/plugin-sdk/sandbox";
 import * as secretInputSdk from "remoteclaw/plugin-sdk/secret-input";
 import * as selfHostedProviderSetupSdk from "remoteclaw/plugin-sdk/self-hosted-provider-setup";
 import * as setupSdk from "remoteclaw/plugin-sdk/setup";
+import * as ssrfRuntimeSdk from "remoteclaw/plugin-sdk/ssrf-runtime";
 import * as testingSdk from "remoteclaw/plugin-sdk/testing";
+import * as threadBindingsRuntimeSdk from "remoteclaw/plugin-sdk/thread-bindings-runtime";
 import * as webhookIngressSdk from "remoteclaw/plugin-sdk/webhook-ingress";
 import { describe, expect, expectTypeOf, it } from "vitest";
 import type { ChannelMessageActionContext } from "../channels/plugins/types.js";
@@ -484,6 +487,22 @@ describe("plugin-sdk subpath exports", () => {
     expect(typeof conversationRuntimeSdk.createScopedAccountReplyToModeResolver).toBe("function");
     expect(typeof conversationRuntimeSdk.createStaticReplyToModeResolver).toBe("function");
     expect(typeof conversationRuntimeSdk.createTopLevelChannelReplyToModeResolver).toBe("function");
+  });
+
+  it("exports narrow binding lifecycle helpers from the dedicated subpath", () => {
+    expect(typeof threadBindingsRuntimeSdk.resolveThreadBindingLifecycle).toBe("function");
+  });
+
+  it("exports narrow matrix runtime helpers from the dedicated subpath", () => {
+    expect(typeof matrixRuntimeSharedSdk.formatZonedTimestamp).toBe("function");
+  });
+
+  it("exports narrow ssrf helpers from the dedicated subpath", () => {
+    expect(typeof ssrfRuntimeSdk.closeDispatcher).toBe("function");
+    expect(typeof ssrfRuntimeSdk.createPinnedDispatcher).toBe("function");
+    expect(typeof ssrfRuntimeSdk.resolvePinnedHostnameWithPolicy).toBe("function");
+    expect(typeof ssrfRuntimeSdk.assertHttpUrlTargetsPrivateNetwork).toBe("function");
+    expect(typeof ssrfRuntimeSdk.ssrfPolicyFromAllowPrivateNetwork).toBe("function");
   });
 
   it("exports provider setup helpers from the dedicated subpath", () => {
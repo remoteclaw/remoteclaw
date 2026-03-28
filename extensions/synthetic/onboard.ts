wonglaitung/openclaw_mini

refactor: unify plugin sdk primitives

extensions/synthetic/onboard.ts
@@ -5,32 +5,27 @@ import {
   SYNTHETIC_MODEL_CATALOG,
 } from "openclaw/plugin-sdk/provider-models";
 import {
-  applyAgentDefaultModelPrimary,
-  applyProviderConfigWithModelCatalog,
+  applyProviderConfigWithModelCatalogPreset,
   type OpenClawConfig,
 } from "openclaw/plugin-sdk/provider-onboard";
 
 export { SYNTHETIC_DEFAULT_MODEL_REF };
 
-export function applySyntheticProviderConfig(cfg: OpenClawConfig): OpenClawConfig {
-  const models = { ...cfg.agents?.defaults?.models };
-  models[SYNTHETIC_DEFAULT_MODEL_REF] = {
-    ...models[SYNTHETIC_DEFAULT_MODEL_REF],
-    alias: models[SYNTHETIC_DEFAULT_MODEL_REF]?.alias ?? "MiniMax M2.5",
-  };
-
-  return applyProviderConfigWithModelCatalog(cfg, {
-    agentModels: models,
+function applySyntheticPreset(cfg: OpenClawConfig, primaryModelRef?: string): OpenClawConfig {
+  return applyProviderConfigWithModelCatalogPreset(cfg, {
     providerId: "synthetic",
     api: "anthropic-messages",
     baseUrl: SYNTHETIC_BASE_URL,
     catalogModels: SYNTHETIC_MODEL_CATALOG.map(buildSyntheticModelDefinition),
+    aliases: [{ modelRef: SYNTHETIC_DEFAULT_MODEL_REF, alias: "MiniMax M2.5" }],
+    primaryModelRef,
   });
 }
 
+export function applySyntheticProviderConfig(cfg: OpenClawConfig): OpenClawConfig {
+  return applySyntheticPreset(cfg);
+}
+
 export function applySyntheticConfig(cfg: OpenClawConfig): OpenClawConfig {
-  return applyAgentDefaultModelPrimary(
-    applySyntheticProviderConfig(cfg),
-    SYNTHETIC_DEFAULT_MODEL_REF,
-  );
+  return applySyntheticPreset(cfg, SYNTHETIC_DEFAULT_MODEL_REF);
 }

extensions/venice/onboard.ts
@@ -5,29 +5,27 @@ import {
   VENICE_MODEL_CATALOG,
 } from "openclaw/plugin-sdk/provider-models";
 import {
-  applyAgentDefaultModelPrimary,
-  applyProviderConfigWithModelCatalog,
+  applyProviderConfigWithModelCatalogPreset,
   type OpenClawConfig,
 } from "openclaw/plugin-sdk/provider-onboard";
 
 export { VENICE_DEFAULT_MODEL_REF };
 
-export function applyVeniceProviderConfig(cfg: OpenClawConfig): OpenClawConfig {
-  const models = { ...cfg.agents?.defaults?.models };
-  models[VENICE_DEFAULT_MODEL_REF] = {
-    ...models[VENICE_DEFAULT_MODEL_REF],
-    alias: models[VENICE_DEFAULT_MODEL_REF]?.alias ?? "Kimi K2.5",
-  };
-
-  return applyProviderConfigWithModelCatalog(cfg, {
-    agentModels: models,
+function applyVenicePreset(cfg: OpenClawConfig, primaryModelRef?: string): OpenClawConfig {
+  return applyProviderConfigWithModelCatalogPreset(cfg, {
     providerId: "venice",
     api: "openai-completions",
     baseUrl: VENICE_BASE_URL,
     catalogModels: VENICE_MODEL_CATALOG.map(buildVeniceModelDefinition),
+    aliases: [{ modelRef: VENICE_DEFAULT_MODEL_REF, alias: "Kimi K2.5" }],
+    primaryModelRef,
   });
 }
 
+export function applyVeniceProviderConfig(cfg: OpenClawConfig): OpenClawConfig {
+  return applyVenicePreset(cfg);
+}
+
 export function applyVeniceConfig(cfg: OpenClawConfig): OpenClawConfig {
-  return applyAgentDefaultModelPrimary(applyVeniceProviderConfig(cfg), VENICE_DEFAULT_MODEL_REF);
+  return applyVenicePreset(cfg, VENICE_DEFAULT_MODEL_REF);
 }

src/plugin-sdk/subpaths.test.ts
@@ -1,6 +1,9 @@
 import * as bluebubblesSdk from "openclaw/plugin-sdk/bluebubbles";
+import * as channelPairingSdk from "openclaw/plugin-sdk/channel-pairing";
+import * as channelReplyPipelineSdk from "openclaw/plugin-sdk/channel-reply-pipeline";
 import * as channelRuntimeSdk from "openclaw/plugin-sdk/channel-runtime";
 import * as channelSendResultSdk from "openclaw/plugin-sdk/channel-send-result";
+import * as channelSetupSdk from "openclaw/plugin-sdk/channel-setup";
 import * as coreSdk from "openclaw/plugin-sdk/core";
 import type {
   ChannelMessageActionContext as CoreChannelMessageActionContext,
@@ -18,11 +21,13 @@ import * as replyPayloadSdk from "openclaw/plugin-sdk/reply-payload";
 import * as routingSdk from "openclaw/plugin-sdk/routing";
 import * as runtimeSdk from "openclaw/plugin-sdk/runtime";
 import * as sandboxSdk from "openclaw/plugin-sdk/sandbox";
+import * as secretInputSdk from "openclaw/plugin-sdk/secret-input";
 import * as selfHostedProviderSetupSdk from "openclaw/plugin-sdk/self-hosted-provider-setup";
 import * as setupSdk from "openclaw/plugin-sdk/setup";
 import * as slackSdk from "openclaw/plugin-sdk/slack";
 import * as telegramSdk from "openclaw/plugin-sdk/telegram";
 import * as testingSdk from "openclaw/plugin-sdk/testing";
+import * as webhookIngressSdk from "openclaw/plugin-sdk/webhook-ingress";
 import * as whatsappSdk from "openclaw/plugin-sdk/whatsapp";
 import * as whatsappActionRuntimeSdk from "openclaw/plugin-sdk/whatsapp-action-runtime";
 import * as whatsappLoginQrSdk from "openclaw/plugin-sdk/whatsapp-login-qr";
@@ -111,6 +116,21 @@ describe("plugin-sdk subpath exports", () => {
     expect(typeof channelRuntimeSdk.sendPayloadMediaSequenceOrFallback).toBe("function");
   });
 
+  it("exports channel setup helpers from the dedicated subpath", () => {
+    expect(typeof channelSetupSdk.createOptionalChannelSetupSurface).toBe("function");
+    expect(typeof channelSetupSdk.createTopLevelChannelDmPolicy).toBe("function");
+  });
+
+  it("exports channel pairing helpers from the dedicated subpath", () => {
+    expect(typeof channelPairingSdk.createChannelPairingController).toBe("function");
+    expect(typeof channelPairingSdk.createScopedPairingAccess).toBe("function");
+  });
+
+  it("exports channel reply pipeline helpers from the dedicated subpath", () => {
+    expect(typeof channelReplyPipelineSdk.createChannelReplyPipeline).toBe("function");
+    expect(typeof channelReplyPipelineSdk.createTypingCallbacks).toBe("function");
+  });
+
   it("exports channel send-result helpers from the dedicated subpath", () => {
     expect(typeof channelSendResultSdk.attachChannelToResult).toBe("function");
     expect(typeof channelSendResultSdk.buildChannelSendResult).toBe("function");
@@ -160,6 +180,18 @@ describe("plugin-sdk subpath exports", () => {
   it("exports sandbox helpers from the dedicated subpath", () => {
     expect(typeof sandboxSdk.registerSandboxBackend).toBe("function");
     expect(typeof sandboxSdk.runPluginCommandWithTimeout).toBe("function");
+  });
+
+  it("exports secret input helpers from the dedicated subpath", () => {
+    expect(typeof secretInputSdk.buildSecretInputSchema).toBe("function");
+    expect(typeof secretInputSdk.buildOptionalSecretInputSchema).toBe("function");
+    expect(typeof secretInputSdk.normalizeSecretInputString).toBe("function");
+  });
+
+  it("exports webhook ingress helpers from the dedicated subpath", () => {
+    expect(typeof webhookIngressSdk.resolveWebhookPath).toBe("function");
+    expect(typeof webhookIngressSdk.readJsonWebhookBodyOrReject).toBe("function");
+    expect(typeof webhookIngressSdk.withResolvedWebhookRequestPipeline).toBe("function");
   });
 
   it("exports shared core types used by bundled channels", () => {

remoteclaw/remoteclaw

(cherry picked from commit 07d9f725b618bd676b791f6d1949ecb2bff759c1)
# Conflicts:
#	docs/plugins/architecture.md
#	extensions/bluebubbles/src/secret-input.ts
#	extensions/chutes/onboard.ts
#	extensions/googlechat/src/monitor-access.ts
#	extensions/matrix/src/secret-input.ts
#	extensions/mattermost/src/secret-input.ts
#	extensions/nextcloud-talk/src/secret-input.ts
#	extensions/zalo/src/secret-input.ts
#	src/plugins/provider-onboarding-config.ts

extensions/synthetic/onboard.ts
@@ -5,32 +5,27 @@ import {
   SYNTHETIC_MODEL_CATALOG,
 } from "remoteclaw/plugin-sdk/provider-models";
 import {
-  applyAgentDefaultModelPrimary,
-  applyProviderConfigWithModelCatalog,
+  applyProviderConfigWithModelCatalogPreset,
   type RemoteClawConfig,
 } from "remoteclaw/plugin-sdk/provider-onboard";
 
 export { SYNTHETIC_DEFAULT_MODEL_REF };
 
-export function applySyntheticProviderConfig(cfg: RemoteClawConfig): RemoteClawConfig {
-  const models = { ...cfg.agents?.defaults?.models };
-  models[SYNTHETIC_DEFAULT_MODEL_REF] = {
-    ...models[SYNTHETIC_DEFAULT_MODEL_REF],
-    alias: models[SYNTHETIC_DEFAULT_MODEL_REF]?.alias ?? "MiniMax M2.5",
-  };
-
-  return applyProviderConfigWithModelCatalog(cfg, {
-    agentModels: models,
+function applySyntheticPreset(cfg: RemoteClawConfig, primaryModelRef?: string): RemoteClawConfig {
+  return applyProviderConfigWithModelCatalogPreset(cfg, {
     providerId: "synthetic",
     api: "anthropic-messages",
     baseUrl: SYNTHETIC_BASE_URL,
     catalogModels: SYNTHETIC_MODEL_CATALOG.map(buildSyntheticModelDefinition),
+    aliases: [{ modelRef: SYNTHETIC_DEFAULT_MODEL_REF, alias: "MiniMax M2.5" }],
+    primaryModelRef,
   });
 }
 
+export function applySyntheticProviderConfig(cfg: RemoteClawConfig): RemoteClawConfig {
+  return applySyntheticPreset(cfg);
+}
+
 export function applySyntheticConfig(cfg: RemoteClawConfig): RemoteClawConfig {
-  return applyAgentDefaultModelPrimary(
-    applySyntheticProviderConfig(cfg),
-    SYNTHETIC_DEFAULT_MODEL_REF,
-  );
+  return applySyntheticPreset(cfg, SYNTHETIC_DEFAULT_MODEL_REF);
 }

extensions/venice/onboard.ts
@@ -5,29 +5,27 @@ import {
   VENICE_MODEL_CATALOG,
 } from "remoteclaw/plugin-sdk/provider-models";
 import {
-  applyAgentDefaultModelPrimary,
-  applyProviderConfigWithModelCatalog,
+  applyProviderConfigWithModelCatalogPreset,
   type RemoteClawConfig,
 } from "remoteclaw/plugin-sdk/provider-onboard";
 
 export { VENICE_DEFAULT_MODEL_REF };
 
-export function applyVeniceProviderConfig(cfg: RemoteClawConfig): RemoteClawConfig {
-  const models = { ...cfg.agents?.defaults?.models };
-  models[VENICE_DEFAULT_MODEL_REF] = {
-    ...models[VENICE_DEFAULT_MODEL_REF],
-    alias: models[VENICE_DEFAULT_MODEL_REF]?.alias ?? "Kimi K2.5",
-  };
-
-  return applyProviderConfigWithModelCatalog(cfg, {
-    agentModels: models,
+function applyVenicePreset(cfg: RemoteClawConfig, primaryModelRef?: string): RemoteClawConfig {
+  return applyProviderConfigWithModelCatalogPreset(cfg, {
     providerId: "venice",
     api: "openai-completions",
     baseUrl: VENICE_BASE_URL,
     catalogModels: VENICE_MODEL_CATALOG.map(buildVeniceModelDefinition),
+    aliases: [{ modelRef: VENICE_DEFAULT_MODEL_REF, alias: "Kimi K2.5" }],
+    primaryModelRef,
   });
 }
 
+export function applyVeniceProviderConfig(cfg: RemoteClawConfig): RemoteClawConfig {
+  return applyVenicePreset(cfg);
+}
+
 export function applyVeniceConfig(cfg: RemoteClawConfig): RemoteClawConfig {
-  return applyAgentDefaultModelPrimary(applyVeniceProviderConfig(cfg), VENICE_DEFAULT_MODEL_REF);
+  return applyVenicePreset(cfg, VENICE_DEFAULT_MODEL_REF);
 }

src/plugin-sdk/subpaths.test.ts
@@ -1,26 +1,36 @@
-import * as compatSdk from "remoteclaw/plugin-sdk/compat";
+import * as bluebubblesSdk from "remoteclaw/plugin-sdk/bluebubbles";
+import * as channelPairingSdk from "remoteclaw/plugin-sdk/channel-pairing";
+import * as channelReplyPipelineSdk from "remoteclaw/plugin-sdk/channel-reply-pipeline";
+import * as channelRuntimeSdk from "remoteclaw/plugin-sdk/channel-runtime";
+import * as channelSendResultSdk from "remoteclaw/plugin-sdk/channel-send-result";
+import * as channelSetupSdk from "remoteclaw/plugin-sdk/channel-setup";
 import * as coreSdk from "remoteclaw/plugin-sdk/core";
 import type {
   ChannelMessageActionContext as CoreChannelMessageActionContext,
   RemoteClawPluginApi as CoreRemoteClawPluginApi,
   PluginRuntime as CorePluginRuntime,
 } from "remoteclaw/plugin-sdk/core";
+import * as directoryRuntimeSdk from "remoteclaw/plugin-sdk/directory-runtime";
 import * as discordSdk from "remoteclaw/plugin-sdk/discord";
 import * as imessageSdk from "remoteclaw/plugin-sdk/imessage";
-import * as lineSdk from "remoteclaw/plugin-sdk/line";
-import * as msteamsSdk from "remoteclaw/plugin-sdk/msteams";
-import * as nostrSdk from "remoteclaw/plugin-sdk/nostr";
+import * as lazyRuntimeSdk from "remoteclaw/plugin-sdk/lazy-runtime";
 import * as ollamaSetupSdk from "remoteclaw/plugin-sdk/ollama-setup";
+import * as providerModelsSdk from "remoteclaw/plugin-sdk/provider-models";
 import * as providerSetupSdk from "remoteclaw/plugin-sdk/provider-setup";
+import * as replyPayloadSdk from "remoteclaw/plugin-sdk/reply-payload";
 import * as routingSdk from "remoteclaw/plugin-sdk/routing";
 import * as runtimeSdk from "remoteclaw/plugin-sdk/runtime";
 import * as sandboxSdk from "remoteclaw/plugin-sdk/sandbox";
+import * as secretInputSdk from "remoteclaw/plugin-sdk/secret-input";
 import * as selfHostedProviderSetupSdk from "remoteclaw/plugin-sdk/self-hosted-provider-setup";
 import * as setupSdk from "remoteclaw/plugin-sdk/setup";
-import * as signalSdk from "remoteclaw/plugin-sdk/signal";
 import * as slackSdk from "remoteclaw/plugin-sdk/slack";
 import * as telegramSdk from "remoteclaw/plugin-sdk/telegram";
+import * as testingSdk from "remoteclaw/plugin-sdk/testing";
+import * as webhookIngressSdk from "remoteclaw/plugin-sdk/webhook-ingress";
 import * as whatsappSdk from "remoteclaw/plugin-sdk/whatsapp";
+import * as whatsappActionRuntimeSdk from "remoteclaw/plugin-sdk/whatsapp-action-runtime";
+import * as whatsappLoginQrSdk from "remoteclaw/plugin-sdk/whatsapp-login-qr";
 import { describe, expect, expectTypeOf, it } from "vitest";
 import type { ChannelMessageActionContext } from "../channels/plugins/types.js";
 import type { PluginRuntime } from "../plugins/runtime/types.js";
@@ -95,6 +105,37 @@ describe("plugin-sdk subpath exports", () => {
     expect(typeof runtimeSdk.createLoggerBackedRuntime).toBe("function");
   });
 
+  it("exports directory runtime helpers from the dedicated subpath", () => {
+    expect(typeof directoryRuntimeSdk.listDirectoryEntriesFromSources).toBe("function");
+    expect(typeof directoryRuntimeSdk.listResolvedDirectoryEntriesFromSources).toBe("function");
+  });
+
+  it("exports channel runtime helpers from the dedicated subpath", () => {
+    expect(typeof channelRuntimeSdk.createChannelDirectoryAdapter).toBe("function");
+    expect(typeof channelRuntimeSdk.createRuntimeOutboundDelegates).toBe("function");
+    expect(typeof channelRuntimeSdk.sendPayloadMediaSequenceOrFallback).toBe("function");
+  });
+
+  it("exports channel setup helpers from the dedicated subpath", () => {
+    expect(typeof channelSetupSdk.createOptionalChannelSetupSurface).toBe("function");
+    expect(typeof channelSetupSdk.createTopLevelChannelDmPolicy).toBe("function");
+  });
+
+  it("exports channel pairing helpers from the dedicated subpath", () => {
+    expect(typeof channelPairingSdk.createChannelPairingController).toBe("function");
+    expect(typeof channelPairingSdk.createScopedPairingAccess).toBe("function");
+  });
+
+  it("exports channel reply pipeline helpers from the dedicated subpath", () => {
+    expect(typeof channelReplyPipelineSdk.createChannelReplyPipeline).toBe("function");
+    expect(typeof channelReplyPipelineSdk.createTypingCallbacks).toBe("function");
+  });
+
+  it("exports channel send-result helpers from the dedicated subpath", () => {
+    expect(typeof channelSendResultSdk.attachChannelToResult).toBe("function");
+    expect(typeof channelSendResultSdk.buildChannelSendResult).toBe("function");
+  });
+
   it("exports provider setup helpers from the dedicated subpath", () => {
     expect(typeof providerSetupSdk.buildVllmProvider).toBe("function");
     expect(typeof providerSetupSdk.discoverOpenAICompatibleSelfHostedProvider).toBe("function");
@@ -132,6 +173,18 @@ describe("plugin-sdk subpath exports", () => {
     expect(typeof sandboxSdk.registerSandboxBackend).toBe("function");
     expect(typeof sandboxSdk.runPluginCommandWithTimeout).toBe("function");
     expect(typeof sandboxSdk.createRemoteShellSandboxFsBridge).toBe("function");
+  });
+
+  it("exports secret input helpers from the dedicated subpath", () => {
+    expect(typeof secretInputSdk.buildSecretInputSchema).toBe("function");
+    expect(typeof secretInputSdk.buildOptionalSecretInputSchema).toBe("function");
+    expect(typeof secretInputSdk.normalizeSecretInputString).toBe("function");
+  });
+
+  it("exports webhook ingress helpers from the dedicated subpath", () => {
+    expect(typeof webhookIngressSdk.resolveWebhookPath).toBe("function");
+    expect(typeof webhookIngressSdk.readJsonWebhookBodyOrReject).toBe("function");
+    expect(typeof webhookIngressSdk.withResolvedWebhookRequestPipeline).toBe("function");
   });
 
   it("exports shared core types used by bundled channels", () => {
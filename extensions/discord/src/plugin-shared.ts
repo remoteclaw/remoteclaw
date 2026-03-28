wonglaitung/openclaw_mini

Tests: guard channel setup import seams

extensions/discord/src/plugin-shared.ts
@@ -3,7 +3,7 @@ import {
   createScopedAccountConfigAccessors,
   createScopedChannelConfigBase,
 } from "openclaw/plugin-sdk/channel-config-helpers";
-import type { OpenClawConfig } from "openclaw/plugin-sdk/discord";
+import type { OpenClawConfig } from "../../../src/config/config.js";
 import { inspectDiscordAccount } from "./account-inspect.js";
 import {
   listDiscordAccountIds,

extensions/whatsapp/src/shared.ts
@@ -1,20 +1,24 @@
 import {
   buildAccountScopedDmSecurityPolicy,
-  buildChannelConfigSchema,
   collectAllowlistProviderGroupPolicyWarnings,
   collectOpenGroupPolicyRouteAllowlistWarnings,
-  DEFAULT_ACCOUNT_ID,
+} from "openclaw/plugin-sdk/compat";
+import {
   formatWhatsAppConfigAllowFromEntries,
-  getChatChannelMeta,
-  normalizeE164,
   resolveWhatsAppConfigAllowFrom,
   resolveWhatsAppConfigDefaultTo,
-  resolveWhatsAppGroupIntroHint,
+} from "../../../src/plugin-sdk/channel-config-helpers.js";
+import { buildChannelConfigSchema } from "../../../src/channels/plugins/config-schema.js";
+import type { ChannelPlugin } from "../../../src/channels/plugins/types.plugin.js";
+import {
   resolveWhatsAppGroupRequireMention,
   resolveWhatsAppGroupToolPolicy,
-  WhatsAppConfigSchema,
-  type ChannelPlugin,
-} from "openclaw/plugin-sdk/whatsapp";
+} from "../../../src/channels/plugins/group-mentions.js";
+import { resolveWhatsAppGroupIntroHint } from "../../../src/channels/plugins/whatsapp-shared.js";
+import { getChatChannelMeta } from "../../../src/channels/registry.js";
+import { WhatsAppConfigSchema } from "../../../src/config/zod-schema.providers-whatsapp.js";
+import { DEFAULT_ACCOUNT_ID } from "../../../src/routing/session-key.js";
+import { normalizeE164 } from "../../../src/utils.js";
 import {
   listWhatsAppAccountIds,
   resolveDefaultWhatsAppAccountId,

remoteclaw/remoteclaw

(cherry picked from commit 0bf11c1d69fbcfdaba564d5e6d88ee2210be7501)
# Conflicts:
#	extensions/slack/src/shared.ts

extensions/discord/src/plugin-shared.ts
@@ -3,7 +3,7 @@ import {
   createScopedAccountConfigAccessors,
   createScopedChannelConfigBase,
 } from "remoteclaw/plugin-sdk/channel-config-helpers";
-import type { RemoteClawConfig } from "remoteclaw/plugin-sdk/discord";
+import type { RemoteClawConfig } from "../../../src/config/config.js";
 import { inspectDiscordAccount } from "./account-inspect.js";
 import {
   listDiscordAccountIds,

extensions/whatsapp/src/shared.ts
@@ -1,32 +1,30 @@
 import {
-  createScopedChannelConfigAdapter,
-  createScopedDmSecurityResolver,
-} from "remoteclaw/plugin-sdk/channel-config-helpers";
-import { createAllowlistProviderRouteAllowlistWarningCollector } from "remoteclaw/plugin-sdk/channel-policy";
-import { createChannelPluginBase } from "remoteclaw/plugin-sdk/core";
-import { createDelegatedSetupWizardProxy } from "remoteclaw/plugin-sdk/setup";
+  buildAccountScopedDmSecurityPolicy,
+  collectAllowlistProviderGroupPolicyWarnings,
+  collectOpenGroupPolicyRouteAllowlistWarnings,
+} from "remoteclaw/plugin-sdk/compat";
+import {
+  formatWhatsAppConfigAllowFromEntries,
+  resolveWhatsAppConfigAllowFrom,
+  resolveWhatsAppConfigDefaultTo,
+} from "../../../src/plugin-sdk/channel-config-helpers.js";
+import { buildChannelConfigSchema } from "../../../src/channels/plugins/config-schema.js";
+import type { ChannelPlugin } from "../../../src/channels/plugins/types.plugin.js";
+import {
+  resolveWhatsAppGroupRequireMention,
+  resolveWhatsAppGroupToolPolicy,
+} from "../../../src/channels/plugins/group-mentions.js";
+import { resolveWhatsAppGroupIntroHint } from "../../../src/channels/plugins/whatsapp-shared.js";
+import { getChatChannelMeta } from "../../../src/channels/registry.js";
+import { WhatsAppConfigSchema } from "../../../src/config/zod-schema.providers-whatsapp.js";
+import { DEFAULT_ACCOUNT_ID } from "../../../src/routing/session-key.js";
+import { normalizeE164 } from "../../../src/utils.js";
 import {
   listWhatsAppAccountIds,
   resolveDefaultWhatsAppAccountId,
   resolveWhatsAppAccount,
   type ResolvedWhatsAppAccount,
 } from "./accounts.js";
-import {
-  buildChannelConfigSchema,
-  collectAllowlistProviderGroupPolicyWarnings,
-  collectOpenGroupPolicyRouteAllowlistWarnings,
-  DEFAULT_ACCOUNT_ID,
-  formatWhatsAppConfigAllowFromEntries,
-  getChatChannelMeta,
-  normalizeE164,
-  resolveWhatsAppConfigAllowFrom,
-  resolveWhatsAppConfigDefaultTo,
-  resolveWhatsAppGroupIntroHint,
-  resolveWhatsAppGroupRequireMention,
-  resolveWhatsAppGroupToolPolicy,
-  WhatsAppConfigSchema,
-  type ChannelPlugin,
-} from "./runtime-api.js";
 
 export const WHATSAPP_CHANNEL = "whatsapp" as const;
 
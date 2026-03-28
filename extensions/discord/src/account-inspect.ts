wonglaitung/openclaw_mini

fix(runtime): lazy-load setup shims and align contracts

extensions/discord/src/account-inspect.ts
@@ -3,12 +3,12 @@ import {
   hasConfiguredSecretInput,
   normalizeSecretInputString,
 } from "openclaw/plugin-sdk/config-runtime";
-import type { DiscordAccountConfig, OpenClawConfig } from "openclaw/plugin-sdk/discord-core";
 import {
   mergeDiscordAccountConfig,
   resolveDefaultDiscordAccountId,
   resolveDiscordAccountConfig,
 } from "./accounts.js";
+import type { DiscordAccountConfig, OpenClawConfig } from "./runtime-api.js";
 
 export type DiscordCredentialStatus = "available" | "configured_unavailable" | "missing";
 

extensions/discord/src/setup-core.ts
@@ -1,24 +1,27 @@
+import { DEFAULT_ACCOUNT_ID } from "openclaw/plugin-sdk/account-id";
 import type { DiscordGuildEntry } from "openclaw/plugin-sdk/config-runtime";
+import type { OpenClawConfig } from "openclaw/plugin-sdk/config-runtime";
+import { createEnvPatchedAccountSetupAdapter } from "openclaw/plugin-sdk/setup-adapter-runtime";
+import type {
+  ChannelSetupAdapter,
+  ChannelSetupDmPolicy,
+  ChannelSetupWizard,
+} from "openclaw/plugin-sdk/setup-runtime";
+import { formatDocsLink } from "openclaw/plugin-sdk/setup-tools";
+import {
+  inspectDiscordSetupAccount,
+  listDiscordSetupAccountIds,
+  resolveDiscordSetupAccountConfig,
+} from "./setup-account-state.js";
 import {
   createAccountScopedAllowFromSection,
   createAccountScopedGroupAccessSection,
+  createAllowlistSetupWizardProxy,
   createLegacyCompatChannelDmPolicy,
-  DEFAULT_ACCOUNT_ID,
-  createEnvPatchedAccountSetupAdapter,
   parseMentionOrPrefixedId,
   patchChannelConfigForAccount,
   setSetupChannelEnabled,
-  type OpenClawConfig,
-} from "openclaw/plugin-sdk/setup";
-import {
-  createAllowlistSetupWizardProxy,
-  type ChannelSetupAdapter,
-  type ChannelSetupDmPolicy,
-  type ChannelSetupWizard,
-} from "openclaw/plugin-sdk/setup";
-import { formatDocsLink } from "openclaw/plugin-sdk/setup-tools";
-import { inspectDiscordAccount } from "./account-inspect.js";
-import { listDiscordAccountIds, resolveDiscordAccount } from "./accounts.js";
+} from "./setup-runtime-helpers.js";
 
 const channel = "discord" as const;
 
@@ -104,8 +107,8 @@ export function createDiscordSetupWizardBase(handlers: {
       configuredScore: 2,
       unconfiguredScore: 1,
       resolveConfigured: ({ cfg }) =>
-        listDiscordAccountIds(cfg).some((accountId) => {
-          const account = inspectDiscordAccount({ cfg, accountId });
+        listDiscordSetupAccountIds(cfg).some((accountId) => {
+          const account = inspectDiscordSetupAccount({ cfg, accountId });
           return account.configured;
         }),
     },
@@ -122,7 +125,7 @@ export function createDiscordSetupWizardBase(handlers: {
         inputPrompt: "Enter Discord bot token",
         allowEnv: ({ accountId }: { accountId: string }) => accountId === DEFAULT_ACCOUNT_ID,
         inspect: ({ cfg, accountId }: { cfg: OpenClawConfig; accountId: string }) => {
-          const account = inspectDiscordAccount({ cfg, accountId });
+          const account = inspectDiscordSetupAccount({ cfg, accountId });
           return {
             accountConfigured: account.configured,
             hasConfiguredValue: account.tokenStatus !== "missing",
@@ -136,25 +139,24 @@ export function createDiscordSetupWizardBase(handlers: {
       },
     ],
     groupAccess: createAccountScopedGroupAccessSection({
-      channel,
       label: "Discord channels",
       placeholder: "My Server/#general, guildId/channelId, #support",
       currentPolicy: ({ cfg, accountId }: { cfg: OpenClawConfig; accountId: string }) =>
-        resolveDiscordAccount({ cfg, accountId }).config.groupPolicy ?? "allowlist",
+        resolveDiscordSetupAccountConfig({ cfg, accountId }).config.groupPolicy ?? "allowlist",
       currentEntries: ({ cfg, accountId }: { cfg: OpenClawConfig; accountId: string }) =>
-        Object.entries(resolveDiscordAccount({ cfg, accountId }).config.guilds ?? {}).flatMap(
-          ([guildKey, value]) => {
-            const channels = value?.channels ?? {};
-            const channelKeys = Object.keys(channels);
-            if (channelKeys.length === 0) {
-              const input = /^\d+$/.test(guildKey) ? `guild:${guildKey}` : guildKey;
-              return [input];
-            }
-            return channelKeys.map((channelKey) => `${guildKey}/${channelKey}`);
-          },
-        ),
+        Object.entries(
+          resolveDiscordSetupAccountConfig({ cfg, accountId }).config.guilds ?? {},
+        ).flatMap(([guildKey, value]) => {
+          const channels = value?.channels ?? {};
+          const channelKeys = Object.keys(channels);
+          if (channelKeys.length === 0) {
+            const input = /^\d+$/.test(guildKey) ? `guild:${guildKey}` : guildKey;
+            return [input];
+          }
+          return channelKeys.map((channelKey) => `${guildKey}/${channelKey}`);
+        }),
       updatePrompt: ({ cfg, accountId }: { cfg: OpenClawConfig; accountId: string }) =>
-        Boolean(resolveDiscordAccount({ cfg, accountId }).config.guilds),
+        Boolean(resolveDiscordSetupAccountConfig({ cfg, accountId }).config.guilds),
       resolveAllowlist: handlers.resolveGroupAllowlist,
       fallbackResolved: (entries) => entries.map((input) => ({ input, resolved: false })),
       applyAllowlist: ({
@@ -168,7 +170,6 @@ export function createDiscordSetupWizardBase(handlers: {
       }) => setDiscordGuildChannelAllowlist(cfg, accountId, resolved as never),
     }),
     allowFrom: createAccountScopedAllowFromSection({
-      channel,
       credentialInputKey: "token",
       helpTitle: "Discord allowlist",
       helpLines: [

src/infra/outbound/message-action-runner.context.test.ts
@@ -1,8 +1,17 @@
 import { afterEach, beforeEach, describe, expect, it } from "vitest";
-import type { ChannelPlugin } from "../../channels/plugins/types.js";
+import type {
+  ChannelDirectoryEntryKind,
+  ChannelMessagingAdapter,
+  ChannelOutboundAdapter,
+  ChannelPlugin,
+} from "../../channels/plugins/types.js";
 import type { OpenClawConfig } from "../../config/config.js";
 import { setActivePluginRegistry } from "../../plugins/runtime.js";
-import { createOutboundTestPlugin, createTestRegistry } from "../../test-utils/channel-plugins.js";
+import {
+  createChannelTestPluginBase,
+  createTestRegistry,
+} from "../../test-utils/channel-plugins.js";
+import { createIMessageTestPlugin } from "../../test-utils/imessage-test-plugin.js";
 import { runMessageAction } from "./message-action-runner.js";
 
 const slackConfig = {
@@ -52,48 +61,112 @@ const runDrySend = (params: {
     action: "send",
   });
 
-const createDryRunPlugin = (id: "slack" | "whatsapp" | "telegram" | "imessage"): ChannelPlugin => {
-  const plugin = createOutboundTestPlugin({
-    id,
-    outbound: {} as never,
-  });
+type ResolvedTestTarget = { to: string; kind: ChannelDirectoryEntryKind };
 
-  const resolveTarget: NonNullable<
-    NonNullable<ChannelPlugin["messaging"]>["targetResolver"]
-  >["resolveTarget"] = async ({ input, normalized }) => {
-    if (id === "slack") {
-      const raw = input.replace(/^#/, "");
-      return { to: `channel:${raw}`, kind: "group", source: "normalized" };
-    }
-    if (id === "telegram") {
-      return { to: `group:${normalized || input}`, kind: "group", source: "normalized" };
-    }
-    if (id === "whatsapp") {
-      return { to: `group:${normalized || input}`, kind: "group", source: "normalized" };
-    }
-    return { to: normalized || input, kind: "user", source: "normalized" };
+const directOutbound: ChannelOutboundAdapter = { deliveryMode: "direct" };
+
+function normalizeSlackTarget(raw: string): string {
+  const trimmed = raw.trim();
+  if (!trimmed) {
+    return trimmed;
+  }
+  if (trimmed.startsWith("#")) {
+    return trimmed.slice(1).trim();
+  }
+  if (/^channel:/i.test(trimmed)) {
+    return trimmed.replace(/^channel:/i, "").trim();
+  }
+  if (/^user:/i.test(trimmed)) {
+    return trimmed.replace(/^user:/i, "").trim();
+  }
+  const mention = trimmed.match(/^<@([A-Z0-9]+)>$/i);
+  if (mention?.[1]) {
+    return mention[1];
+  }
+  return trimmed;
+}
+
+function createConfiguredTestPlugin(params: {
+  id: "slack" | "telegram" | "whatsapp";
+  isConfigured: (cfg: OpenClawConfig) => boolean;
+  normalizeTarget: (raw: string) => string | undefined;
+  resolveTarget: (input: string) => ResolvedTestTarget | null;
+}): ChannelPlugin {
+  const messaging: ChannelMessagingAdapter = {
+    normalizeTarget: params.normalizeTarget,
+    targetResolver: {
+      looksLikeId: (raw) => Boolean(params.resolveTarget(raw.trim())),
+      hint: "<id>",
+      resolveTarget: async (resolverParams) => {
+        const resolved = params.resolveTarget(resolverParams.input);
+        return resolved ? { ...resolved, source: "normalized" } : null;
+      },
+    },
+    inferTargetChatType: (inferParams) =>
+      params.resolveTarget(inferParams.to)?.kind === "user" ? "direct" : "group",
   };
-
   return {
-    ...plugin,
-    config: {
-      listAccountIds: () => ["default"],
-      resolveAccount: () => ({}),
-    },
-    messaging: {
-      inferTargetChatType: ({ to }) => {
-        if (id === "imessage" && to.startsWith("imessage:")) {
-          return "direct";
-        }
-        return "group";
+    ...createChannelTestPluginBase({
+      id: params.id,
+      config: {
+        listAccountIds: () => ["default"],
+        resolveAccount: () => ({ enabled: true }),
+        isConfigured: (_account, cfg) => params.isConfigured(cfg),
       },
-      targetResolver: {
-        looksLikeId: () => true,
-        resolveTarget,
-      },
-    },
+    }),
+    outbound: directOutbound,
+    messaging,
   };
-};
+}
+
+const slackTestPlugin = createConfiguredTestPlugin({
+  id: "slack",
+  isConfigured: (cfg) => Boolean(cfg.channels?.slack?.botToken?.trim()),
+  normalizeTarget: (raw) => normalizeSlackTarget(raw) || undefined,
+  resolveTarget: (input) => {
+    const normalized = normalizeSlackTarget(input);
+    if (!normalized) {
+      return null;
+    }
+    if (/^[A-Z0-9]+$/i.test(normalized)) {
+      const kind = /^U/i.test(normalized) ? "user" : "group";
+      return { to: normalized, kind };
+    }
+    return null;
+  },
+});
+
+const telegramTestPlugin = createConfiguredTestPlugin({
+  id: "telegram",
+  isConfigured: (cfg) => Boolean(cfg.channels?.telegram?.botToken?.trim()),
+  normalizeTarget: (raw) => raw.trim() || undefined,
+  resolveTarget: (input) => {
+    const normalized = input.trim();
+    if (!normalized) {
+      return null;
+    }
+    return {
+      to: normalized.replace(/^telegram:/i, ""),
+      kind: normalized.startsWith("@") ? "user" : "group",
+    };
+  },
+});
+
+const whatsappTestPlugin = createConfiguredTestPlugin({
+  id: "whatsapp",
+  isConfigured: (cfg) => Boolean(cfg.channels?.whatsapp),
+  normalizeTarget: (raw) => raw.trim() || undefined,
+  resolveTarget: (input) => {
+    const normalized = input.trim();
+    if (!normalized) {
+      return null;
+    }
+    return {
+      to: normalized,
+      kind: normalized.endsWith("@g.us") ? "group" : "user",
+    };
+  },
+});
 
 describe("runMessageAction context isolation", () => {
   beforeEach(() => {
@@ -102,22 +175,22 @@ describe("runMessageAction context isolation", () => {
         {
           pluginId: "slack",
           source: "test",
-          plugin: createDryRunPlugin("slack"),
+          plugin: slackTestPlugin,
         },
         {
           pluginId: "whatsapp",
           source: "test",
-          plugin: createDryRunPlugin("whatsapp"),
+          plugin: whatsappTestPlugin,
         },
         {
           pluginId: "telegram",
           source: "test",
-          plugin: createDryRunPlugin("telegram"),
+          plugin: telegramTestPlugin,
         },
         {
           pluginId: "imessage",
           source: "test",
-          plugin: createDryRunPlugin("imessage"),
+          plugin: createIMessageTestPlugin(),
         },
       ]),
     );

remoteclaw/remoteclaw

(cherry picked from commit 3b79494cbf72d06feb55591d28f03154e65451ca)
# Conflicts:
#	extensions/discord/src/channel.runtime.ts
#	extensions/matrix/src/matrix/monitor/handler.test.ts
#	src/agents/acp-spawn.test.ts
#	src/cli/command-secret-gateway.ts

extensions/discord/src/account-inspect.ts
@@ -4,15 +4,11 @@ import {
   normalizeSecretInputString,
 } from "remoteclaw/plugin-sdk/config-runtime";
 import {
-  DEFAULT_ACCOUNT_ID,
-  normalizeAccountId,
-  type RemoteClawConfig,
-} from "../../../src/plugin-sdk-internal/accounts.js";
-import {
   mergeDiscordAccountConfig,
   resolveDefaultDiscordAccountId,
   resolveDiscordAccountConfig,
 } from "./accounts.js";
+import type { DiscordAccountConfig, RemoteClawConfig } from "./runtime-api.js";
 
 export type DiscordCredentialStatus = "available" | "configured_unavailable" | "missing";
 

extensions/discord/src/setup-core.ts
@@ -1,23 +1,28 @@
-import { createPatchedAccountSetupAdapter } from "../../../src/channels/plugins/setup-helpers.js";
-import type { DiscordGuildEntry } from "../../../src/config/types.discord.js";
+import { DEFAULT_ACCOUNT_ID } from "remoteclaw/plugin-sdk/account-id";
+import type { DiscordGuildEntry } from "remoteclaw/plugin-sdk/config-runtime";
+import type { RemoteClawConfig } from "remoteclaw/plugin-sdk/config-runtime";
+import { createEnvPatchedAccountSetupAdapter } from "remoteclaw/plugin-sdk/setup-adapter-runtime";
+import type {
+  ChannelSetupAdapter,
+  ChannelSetupDmPolicy,
+  ChannelSetupWizard,
+} from "remoteclaw/plugin-sdk/setup-runtime";
+import { formatDocsLink } from "remoteclaw/plugin-sdk/setup-tools";
 import {
-  DEFAULT_ACCOUNT_ID,
-  noteChannelLookupFailure,
-  noteChannelLookupSummary,
+  inspectDiscordSetupAccount,
+  listDiscordSetupAccountIds,
+  resolveDiscordSetupAccountConfig,
+} from "./setup-account-state.js";
+import {
+  createAccountScopedAllowFromSection,
+  createAccountScopedGroupAccessSection,
+  createAllowlistSetupWizardProxy,
+  createLegacyCompatChannelDmPolicy,
   parseMentionOrPrefixedId,
   patchChannelConfigForAccount,
   setLegacyChannelDmPolicyWithAllowFrom,
   setSetupChannelEnabled,
-  type RemoteClawConfig,
-} from "remoteclaw/plugin-sdk/setup";
-import {
-  type ChannelSetupAdapter,
-  type ChannelSetupDmPolicy,
-  type ChannelSetupWizard,
-} from "remoteclaw/plugin-sdk/setup";
-import { formatDocsLink } from "../../../src/terminal/links.js";
-import { inspectDiscordAccount } from "./account-inspect.js";
-import { listDiscordAccountIds, resolveDiscordAccount } from "./accounts.js";
+} from "./setup-runtime-helpers.js";
 
 const channel = "discord" as const;
 
@@ -119,8 +124,8 @@ export function createDiscordSetupWizardProxy(
       configuredScore: 2,
       unconfiguredScore: 1,
       resolveConfigured: ({ cfg }) =>
-        listDiscordAccountIds(cfg).some((accountId) => {
-          const account = inspectDiscordAccount({ cfg, accountId });
+        listDiscordSetupAccountIds(cfg).some((accountId) => {
+          const account = inspectDiscordSetupAccount({ cfg, accountId });
           return account.configured;
         }),
     },
@@ -137,7 +142,7 @@ export function createDiscordSetupWizardProxy(
         inputPrompt: "Enter Discord bot token",
         allowEnv: ({ accountId }: { accountId: string }) => accountId === DEFAULT_ACCOUNT_ID,
         inspect: ({ cfg, accountId }: { cfg: RemoteClawConfig; accountId: string }) => {
-          const account = inspectDiscordAccount({ cfg, accountId });
+          const account = inspectDiscordSetupAccount({ cfg, accountId });
           return {
             accountConfigured: account.configured,
             hasConfiguredValue: account.tokenStatus !== "missing",
@@ -150,80 +155,27 @@ export function createDiscordSetupWizardProxy(
         },
       },
     ],
-    groupAccess: {
+    groupAccess: createAccountScopedGroupAccessSection({
       label: "Discord channels",
       placeholder: "My Server/#general, guildId/channelId, #support",
       currentPolicy: ({ cfg, accountId }: { cfg: RemoteClawConfig; accountId: string }) =>
-        resolveDiscordAccount({ cfg, accountId }).config.groupPolicy ?? "allowlist",
+        resolveDiscordSetupAccountConfig({ cfg, accountId }).config.groupPolicy ?? "allowlist",
       currentEntries: ({ cfg, accountId }: { cfg: RemoteClawConfig; accountId: string }) =>
-        Object.entries(resolveDiscordAccount({ cfg, accountId }).config.guilds ?? {}).flatMap(
-          ([guildKey, value]) => {
-            const channels = value?.channels ?? {};
-            const channelKeys = Object.keys(channels);
-            if (channelKeys.length === 0) {
-              const input = /^\d+$/.test(guildKey) ? `guild:${guildKey}` : guildKey;
-              return [input];
-            }
-            return channelKeys.map((channelKey) => `${guildKey}/${channelKey}`);
-          },
-        ),
-      updatePrompt: ({ cfg, accountId }: { cfg: RemoteClawConfig; accountId: string }) =>
-        Boolean(resolveDiscordAccount({ cfg, accountId }).config.guilds),
-      setPolicy: ({
-        cfg,
-        accountId,
-        policy,
-      }: {
-        cfg: RemoteClawConfig;
-        accountId: string;
-        policy: "open" | "allowlist" | "disabled";
-      }) =>
-        patchChannelConfigForAccount({
-          cfg,
-          channel,
-          accountId,
-          patch: { groupPolicy: policy },
+        Object.entries(
+          resolveDiscordSetupAccountConfig({ cfg, accountId }).config.guilds ?? {},
+        ).flatMap(([guildKey, value]) => {
+          const channels = value?.channels ?? {};
+          const channelKeys = Object.keys(channels);
+          if (channelKeys.length === 0) {
+            const input = /^\d+$/.test(guildKey) ? `guild:${guildKey}` : guildKey;
+            return [input];
+          }
+          return channelKeys.map((channelKey) => `${guildKey}/${channelKey}`);
         }),
-      resolveAllowlist: async ({
-        cfg,
-        accountId,
-        credentialValues,
-        entries,
-        prompter,
-      }: {
-        cfg: RemoteClawConfig;
-        accountId: string;
-        credentialValues: { token?: string };
-        entries: string[];
-        prompter: { note: (message: string, title?: string) => Promise<void> };
-      }) => {
-        const wizard = (await loadWizard()).discordSetupWizard;
-        if (!wizard.groupAccess?.resolveAllowlist) {
-          return entries.map((input) => ({ input, resolved: false }));
-        }
-        try {
-          return await wizard.groupAccess.resolveAllowlist({
-            cfg,
-            accountId,
-            credentialValues,
-            entries,
-            prompter,
-          });
-        } catch (error) {
-          await noteChannelLookupFailure({
-            prompter,
-            label: "Discord channels",
-            error,
-          });
-          await noteChannelLookupSummary({
-            prompter,
-            label: "Discord channels",
-            resolvedSections: [],
-            unresolved: entries,
-          });
-          return entries.map((input) => ({ input, resolved: false }));
-        }
-      },
+      updatePrompt: ({ cfg, accountId }: { cfg: RemoteClawConfig; accountId: string }) =>
+        Boolean(resolveDiscordSetupAccountConfig({ cfg, accountId }).config.guilds),
+      resolveAllowlist: handlers.resolveGroupAllowlist,
+      fallbackResolved: (entries) => entries.map((input) => ({ input, resolved: false })),
       applyAllowlist: ({
         cfg,
         accountId,
@@ -233,8 +185,8 @@ export function createDiscordSetupWizardProxy(
         accountId: string;
         resolved: unknown;
       }) => setDiscordGuildChannelAllowlist(cfg, accountId, resolved as never),
-    },
-    allowFrom: {
+    }),
+    allowFrom: createAccountScopedAllowFromSection({
       credentialInputKey: "token",
       helpTitle: "Discord allowlist",
       helpLines: [

src/infra/outbound/message-action-runner.context.test.ts
@@ -1,17 +1,17 @@
-import fs from "node:fs/promises";
-import os from "node:os";
-import path from "node:path";
-import { afterEach, beforeAll, beforeEach, describe, expect, it, vi } from "vitest";
-import { slackPlugin } from "../../../extensions/slack/src/channel.js";
-import { telegramPlugin } from "../../../extensions/telegram/src/channel.js";
-import { whatsappPlugin } from "../../../extensions/whatsapp/src/channel.js";
-import { jsonResult } from "../../agents/tools/common.js";
-import type { ChannelPlugin } from "../../channels/plugins/types.js";
+import { afterEach, beforeEach, describe, expect, it } from "vitest";
+import type {
+  ChannelDirectoryEntryKind,
+  ChannelMessagingAdapter,
+  ChannelOutboundAdapter,
+  ChannelPlugin,
+} from "../../channels/plugins/types.js";
 import type { RemoteClawConfig } from "../../config/config.js";
 import { setActivePluginRegistry } from "../../plugins/runtime.js";
-import { createOutboundTestPlugin, createTestRegistry } from "../../test-utils/channel-plugins.js";
+import {
+  createChannelTestPluginBase,
+  createTestRegistry,
+} from "../../test-utils/channel-plugins.js";
 import { createIMessageTestPlugin } from "../../test-utils/imessage-test-plugin.js";
-import { loadWebMedia } from "../../web/media.js";
 import { runMessageAction } from "./message-action-runner.js";
 
 vi.mock("../../web/media.js", async () => {
@@ -69,29 +69,112 @@ const runDrySend = (params: {
     action: "send",
   });
 
-function createAlwaysConfiguredPluginConfig(account: Record<string, unknown> = { enabled: true }) {
+type ResolvedTestTarget = { to: string; kind: ChannelDirectoryEntryKind };
+
+const directOutbound: ChannelOutboundAdapter = { deliveryMode: "direct" };
+
+function normalizeSlackTarget(raw: string): string {
+  const trimmed = raw.trim();
+  if (!trimmed) {
+    return trimmed;
+  }
+  if (trimmed.startsWith("#")) {
+    return trimmed.slice(1).trim();
+  }
+  if (/^channel:/i.test(trimmed)) {
+    return trimmed.replace(/^channel:/i, "").trim();
+  }
+  if (/^user:/i.test(trimmed)) {
+    return trimmed.replace(/^user:/i, "").trim();
+  }
+  const mention = trimmed.match(/^<@([A-Z0-9]+)>$/i);
+  if (mention?.[1]) {
+    return mention[1];
+  }
+  return trimmed;
+}
+
+function createConfiguredTestPlugin(params: {
+  id: "slack" | "telegram" | "whatsapp";
+  isConfigured: (cfg: RemoteClawConfig) => boolean;
+  normalizeTarget: (raw: string) => string | undefined;
+  resolveTarget: (input: string) => ResolvedTestTarget | null;
+}): ChannelPlugin {
+  const messaging: ChannelMessagingAdapter = {
+    normalizeTarget: params.normalizeTarget,
+    targetResolver: {
+      looksLikeId: (raw) => Boolean(params.resolveTarget(raw.trim())),
+      hint: "<id>",
+      resolveTarget: async (resolverParams) => {
+        const resolved = params.resolveTarget(resolverParams.input);
+        return resolved ? { ...resolved, source: "normalized" } : null;
+      },
+    },
+    inferTargetChatType: (inferParams) =>
+      params.resolveTarget(inferParams.to)?.kind === "user" ? "direct" : "group",
+  };
   return {
-    listAccountIds: () => ["default"],
-    resolveAccount: () => account,
-    isConfigured: () => true,
+    ...createChannelTestPluginBase({
+      id: params.id,
+      config: {
+        listAccountIds: () => ["default"],
+        resolveAccount: () => ({ enabled: true }),
+        isConfigured: (_account, cfg) => params.isConfigured(cfg),
+      },
+    }),
+    outbound: directOutbound,
+    messaging,
   };
 }
 
-let createPluginRuntime: typeof import("../../plugins/runtime/index.js").createPluginRuntime;
-let setSlackRuntime: typeof import("../../../extensions/slack/src/runtime.js").setSlackRuntime;
-let setTelegramRuntime: typeof import("../../../extensions/telegram/src/runtime.js").setTelegramRuntime;
-let setWhatsAppRuntime: typeof import("../../../extensions/whatsapp/src/runtime.js").setWhatsAppRuntime;
+const slackTestPlugin = createConfiguredTestPlugin({
+  id: "slack",
+  isConfigured: (cfg) => Boolean(cfg.channels?.slack?.botToken?.trim()),
+  normalizeTarget: (raw) => normalizeSlackTarget(raw) || undefined,
+  resolveTarget: (input) => {
+    const normalized = normalizeSlackTarget(input);
+    if (!normalized) {
+      return null;
+    }
+    if (/^[A-Z0-9]+$/i.test(normalized)) {
+      const kind = /^U/i.test(normalized) ? "user" : "group";
+      return { to: normalized, kind };
+    }
+    return null;
+  },
+});
 
-function installChannelRuntimes(params?: { includeTelegram?: boolean; includeWhatsApp?: boolean }) {
-  const runtime = createPluginRuntime();
-  setSlackRuntime(runtime);
-  if (params?.includeTelegram !== false) {
-    setTelegramRuntime(runtime);
-  }
-  if (params?.includeWhatsApp !== false) {
-    setWhatsAppRuntime(runtime);
-  }
-}
+const telegramTestPlugin = createConfiguredTestPlugin({
+  id: "telegram",
+  isConfigured: (cfg) => Boolean(cfg.channels?.telegram?.botToken?.trim()),
+  normalizeTarget: (raw) => raw.trim() || undefined,
+  resolveTarget: (input) => {
+    const normalized = input.trim();
+    if (!normalized) {
+      return null;
+    }
+    return {
+      to: normalized.replace(/^telegram:/i, ""),
+      kind: normalized.startsWith("@") ? "user" : "group",
+    };
+  },
+});
+
+const whatsappTestPlugin = createConfiguredTestPlugin({
+  id: "whatsapp",
+  isConfigured: (cfg) => Boolean(cfg.channels?.whatsapp),
+  normalizeTarget: (raw) => raw.trim() || undefined,
+  resolveTarget: (input) => {
+    const normalized = input.trim();
+    if (!normalized) {
+      return null;
+    }
+    return {
+      to: normalized,
+      kind: normalized.endsWith("@g.us") ? "group" : "user",
+    };
+  },
+});
 
 describe("runMessageAction context isolation", () => {
   beforeAll(async () => {
@@ -108,17 +191,17 @@ describe("runMessageAction context isolation", () => {
         {
           pluginId: "slack",
           source: "test",
-          plugin: slackPlugin,
+          plugin: slackTestPlugin,
         },
         {
           pluginId: "whatsapp",
           source: "test",
-          plugin: whatsappPlugin,
+          plugin: whatsappTestPlugin,
         },
         {
           pluginId: "telegram",
           source: "test",
-          plugin: telegramPlugin,
+          plugin: telegramTestPlugin,
         },
         {
           pluginId: "imessage",
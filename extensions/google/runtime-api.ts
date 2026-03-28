wonglaitung/openclaw_mini

Plugins: internalize medium extension SDK imports

extensions/google/runtime-api.ts
@@ -1,0 +1,1 @@
+export { normalizeGoogleModelId, parseGeminiAuth } from "openclaw/plugin-sdk/google";

extensions/line/runtime-api.ts
@@ -1,0 +1,1 @@
+export * from "openclaw/plugin-sdk/line-core";

remoteclaw/remoteclaw

(cherry picked from commit 6e723dfd6928772f5da0146ffdd60860981963e2)
# Conflicts:
#	extensions/bluebubbles/src/group-policy.ts
#	extensions/google/media-understanding-provider.ts

extensions/google/runtime-api.ts
@@ -1,1 +1,1 @@
-export { normalizeGoogleModelId, parseGeminiAuth } from "remoteclaw/plugin-sdk/provider-google";
+export { normalizeGoogleModelId, parseGeminiAuth } from "remoteclaw/plugin-sdk/google";

extensions/line/runtime-api.ts
@@ -1,11 +1,1 @@
-// Private runtime barrel for the bundled LINE extension.
-// Keep this barrel thin and aligned with the local extension surface.
-
-export * from "remoteclaw/plugin-sdk/line";
-export {
-  formatDocsLink,
-  setSetupChannelEnabled,
-  splitSetupEntries,
-  type ChannelSetupDmPolicy,
-  type ChannelSetupWizard,
-} from "remoteclaw/plugin-sdk/line-core";
+export * from "remoteclaw/plugin-sdk/line-core";
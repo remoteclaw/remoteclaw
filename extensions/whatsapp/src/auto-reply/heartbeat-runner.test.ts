wonglaitung/openclaw_mini

fix: restore full gate stability

extensions/whatsapp/src/auto-reply/heartbeat-runner.test.ts
@@ -41,9 +41,13 @@ vi.mock("../../../../src/config/config.js", () => ({
   loadConfig: () => ({ agents: { defaults: {} }, session: {} }),
 }));
 
-vi.mock("../../../../src/routing/session-key.js", () => ({
-  normalizeMainKey: () => null,
-}));
+vi.mock("../../../../src/routing/session-key.js", async (importOriginal) => {
+  const actual = await importOriginal<typeof import("../../../../src/routing/session-key.js")>();
+  return {
+    ...actual,
+    normalizeMainKey: () => null,
+  };
+});
 
 vi.mock("../../../../src/infra/heartbeat-visibility.js", () => ({
   resolveHeartbeatVisibility: () => state.visibility,
@@ -74,6 +78,42 @@ vi.mock("../../../../src/logging.js", () => ({
   }),
 }));
 
+vi.mock("openclaw/plugin-sdk/state-paths", async (importOriginal) => {
+  const actual = await importOriginal<typeof import("openclaw/plugin-sdk/state-paths")>();
+  return {
+    ...actual,
+    resolveOAuthDir: () => "/tmp/openclaw-oauth",
+  };
+});
+
+vi.mock("openclaw/plugin-sdk/runtime-env", async (importOriginal) => {
+  const actual = await importOriginal<typeof import("openclaw/plugin-sdk/runtime-env")>();
+  const logger = {
+    child: () => logger,
+    info: vi.fn(),
+    warn: vi.fn(),
+    error: vi.fn(),
+    debug: vi.fn(),
+  };
+  return {
+    ...actual,
+    createSubsystemLogger: () => logger,
+  };
+});
+
+vi.mock("../auth-store.js", () => ({
+  WA_WEB_AUTH_DIR: "/tmp/openclaw-oauth/whatsapp/default",
+  resolveDefaultWebAuthDir: () => "/tmp/openclaw-oauth/whatsapp/default",
+  hasWebCredsSync: () => false,
+  maybeRestoreCredsFromBackup: () => undefined,
+  webAuthExists: async () => false,
+  logoutWeb: async () => undefined,
+  readWebSelfId: () => null,
+  getWebAuthAgeMs: () => null,
+  logWebSelfId: () => undefined,
+  pickWebChannel: async () => undefined,
+}));
+
 vi.mock("./loggers.js", () => ({
   whatsappHeartbeatLog: {
     info: (msg: string) => state.heartbeatInfoLogs.push(msg),
@@ -87,6 +127,7 @@ vi.mock("../reconnect.js", () => ({
 
 vi.mock("../send.js", () => ({
   sendMessageWhatsApp: vi.fn(async () => ({ messageId: "m1" })),
+  sendReactionWhatsApp: vi.fn(async () => undefined),
 }));
 
 vi.mock("../session.js", () => ({

extensions/whatsapp/src/test-helpers.ts
@@ -1,3 +1,5 @@
+import fsSync from "node:fs";
+import fs from "node:fs/promises";
 import { vi } from "vitest";
 import type { MockBaileysSocket } from "../../../test/mocks/baileys.js";
 import { createMockBaileys } from "../../../test/mocks/baileys.js";
@@ -41,6 +43,31 @@ vi.mock("openclaw/plugin-sdk/config-runtime", async (importOriginal) => {
       }
       return DEFAULT_CONFIG;
     },
+    updateLastRoute: async (params: {
+      storePath: string;
+      sessionKey: string;
+      deliveryContext: { channel: string; to: string; accountId?: string };
+    }) => {
+      const raw = await fs.readFile(params.storePath, "utf8").catch(() => "{}");
+      const store = JSON.parse(raw) as Record<string, Record<string, unknown>>;
+      const current = store[params.sessionKey] ?? {};
+      store[params.sessionKey] = {
+        ...current,
+        lastChannel: params.deliveryContext.channel,
+        lastTo: params.deliveryContext.to,
+        lastAccountId: params.deliveryContext.accountId,
+      };
+      await fs.writeFile(params.storePath, JSON.stringify(store));
+    },
+    loadSessionStore: (storePath: string) => {
+      try {
+        return JSON.parse(fsSync.readFileSync(storePath, "utf8")) as Record<string, unknown>;
+      } catch {
+        return {};
+      }
+    },
+    recordSessionMetaFromInbound: async () => undefined,
+    resolveStorePath: actual.resolveStorePath,
   };
 });
 
@@ -80,6 +107,14 @@ vi.mock("openclaw/plugin-sdk/media-runtime", async (importOriginal) => {
     })),
   });
   return mockModule;
+});
+
+vi.mock("openclaw/plugin-sdk/state-paths", async (importOriginal) => {
+  const actual = await importOriginal<typeof import("openclaw/plugin-sdk/state-paths")>();
+  return {
+    ...actual,
+    resolveOAuthDir: () => "/tmp/openclaw-oauth",
+  };
 });
 
 vi.mock("@whiskeysockets/baileys", () => {

src/plugin-sdk/runtime-api-guardrails.test.ts
@@ -34,9 +34,9 @@ const RUNTIME_API_EXPORT_GUARDS: Record<string, readonly string[]> = {
     'export { probeIMessage } from "./src/probe.js";',
     'export { sendMessageIMessage } from "./src/send.js";',
   ],
-  "extensions/googlechat/runtime-api.ts": ['export * from "../../src/plugin-sdk/googlechat.js";'],
+  "extensions/googlechat/runtime-api.ts": ['export * from "openclaw/plugin-sdk/googlechat";'],
   "extensions/nextcloud-talk/runtime-api.ts": [
-    'export * from "../../src/plugin-sdk/nextcloud-talk.js";',
+    'export * from "openclaw/plugin-sdk/nextcloud-talk";',
   ],
   "extensions/signal/runtime-api.ts": ['export * from "./src/runtime-api.js";'],
   "extensions/slack/runtime-api.ts": [

remoteclaw/remoteclaw

(cherry picked from commit 83c5bc946df187aa5cb325cdb3326fbaf2502ea3)
# Conflicts:
#	extensions/llm-task/src/llm-task-tool.ts
#	extensions/matrix/src/runtime-api.test.ts
#	src/cli/command-secret-gateway.test.ts
#	src/secrets/runtime-web-tools.ts

extensions/whatsapp/src/auto-reply/heartbeat-runner.test.ts
@@ -41,15 +41,13 @@ vi.mock("../../../../src/config/config.js", () => ({
   loadConfig: () => ({ agents: { defaults: {} }, session: {} }),
 }));
 
-vi.mock("../../../../src/routing/session-key.js", () => ({
-  normalizeMainKey: () => null,
-  DEFAULT_AGENT_ID: "default",
-}));
-
-vi.mock("../../agents/agent-scope.js", () => ({
-  resolveAgentWorkspaceDir: () => "/tmp/workspace",
-  resolveDefaultAgentId: () => "default",
-}));
+vi.mock("../../../../src/routing/session-key.js", async (importOriginal) => {
+  const actual = await importOriginal<typeof import("../../../../src/routing/session-key.js")>();
+  return {
+    ...actual,
+    normalizeMainKey: () => null,
+  };
+});
 
 vi.mock("../../../../src/infra/heartbeat-visibility.js", () => ({
   resolveHeartbeatVisibility: () => state.visibility,
@@ -80,6 +78,42 @@ vi.mock("../../../../src/logging.js", () => ({
   }),
 }));
 
+vi.mock("remoteclaw/plugin-sdk/state-paths", async (importOriginal) => {
+  const actual = await importOriginal<typeof import("remoteclaw/plugin-sdk/state-paths")>();
+  return {
+    ...actual,
+    resolveOAuthDir: () => "/tmp/openclaw-oauth",
+  };
+});
+
+vi.mock("remoteclaw/plugin-sdk/runtime-env", async (importOriginal) => {
+  const actual = await importOriginal<typeof import("remoteclaw/plugin-sdk/runtime-env")>();
+  const logger = {
+    child: () => logger,
+    info: vi.fn(),
+    warn: vi.fn(),
+    error: vi.fn(),
+    debug: vi.fn(),
+  };
+  return {
+    ...actual,
+    createSubsystemLogger: () => logger,
+  };
+});
+
+vi.mock("../auth-store.js", () => ({
+  WA_WEB_AUTH_DIR: "/tmp/openclaw-oauth/whatsapp/default",
+  resolveDefaultWebAuthDir: () => "/tmp/openclaw-oauth/whatsapp/default",
+  hasWebCredsSync: () => false,
+  maybeRestoreCredsFromBackup: () => undefined,
+  webAuthExists: async () => false,
+  logoutWeb: async () => undefined,
+  readWebSelfId: () => null,
+  getWebAuthAgeMs: () => null,
+  logWebSelfId: () => undefined,
+  pickWebChannel: async () => undefined,
+}));
+
 vi.mock("./loggers.js", () => ({
   whatsappHeartbeatLog: {
     info: (msg: string) => state.heartbeatInfoLogs.push(msg),
@@ -93,6 +127,7 @@ vi.mock("../reconnect.js", () => ({
 
 vi.mock("../send.js", () => ({
   sendMessageWhatsApp: vi.fn(async () => ({ messageId: "m1" })),
+  sendReactionWhatsApp: vi.fn(async () => undefined),
 }));
 
 vi.mock("../session.js", () => ({

extensions/whatsapp/src/test-helpers.ts
@@ -1,3 +1,5 @@
+import fsSync from "node:fs";
+import fs from "node:fs/promises";
 import { vi } from "vitest";
 import type { MockBaileysSocket } from "../../../test/mocks/baileys.js";
 import { createMockBaileys } from "../../../test/mocks/baileys.js";
@@ -41,6 +43,31 @@ vi.mock("remoteclaw/plugin-sdk/config-runtime", async (importOriginal) => {
       }
       return DEFAULT_CONFIG;
     },
+    updateLastRoute: async (params: {
+      storePath: string;
+      sessionKey: string;
+      deliveryContext: { channel: string; to: string; accountId?: string };
+    }) => {
+      const raw = await fs.readFile(params.storePath, "utf8").catch(() => "{}");
+      const store = JSON.parse(raw) as Record<string, Record<string, unknown>>;
+      const current = store[params.sessionKey] ?? {};
+      store[params.sessionKey] = {
+        ...current,
+        lastChannel: params.deliveryContext.channel,
+        lastTo: params.deliveryContext.to,
+        lastAccountId: params.deliveryContext.accountId,
+      };
+      await fs.writeFile(params.storePath, JSON.stringify(store));
+    },
+    loadSessionStore: (storePath: string) => {
+      try {
+        return JSON.parse(fsSync.readFileSync(storePath, "utf8")) as Record<string, unknown>;
+      } catch {
+        return {};
+      }
+    },
+    recordSessionMetaFromInbound: async () => undefined,
+    resolveStorePath: actual.resolveStorePath,
   };
 });
 
@@ -80,6 +107,14 @@ vi.mock("remoteclaw/plugin-sdk/media-runtime", async (importOriginal) => {
     })),
   });
   return mockModule;
+});
+
+vi.mock("remoteclaw/plugin-sdk/state-paths", async (importOriginal) => {
+  const actual = await importOriginal<typeof import("remoteclaw/plugin-sdk/state-paths")>();
+  return {
+    ...actual,
+    resolveOAuthDir: () => "/tmp/openclaw-oauth",
+  };
 });
 
 vi.mock("@whiskeysockets/baileys", () => {

src/plugin-sdk/runtime-api-guardrails.test.ts
@@ -44,8 +44,9 @@ const RUNTIME_API_EXPORT_GUARDS: Record<string, readonly string[]> = {
     'export { probeIMessage } from "./src/probe.js";',
     'export { sendMessageIMessage } from "./src/send.js";',
   ],
+  "extensions/googlechat/runtime-api.ts": ['export * from "remoteclaw/plugin-sdk/googlechat";'],
   "extensions/nextcloud-talk/runtime-api.ts": [
-    'export * from "../../src/plugin-sdk/nextcloud-talk.js";',
+    'export * from "remoteclaw/plugin-sdk/nextcloud-talk";',
   ],
   "extensions/signal/runtime-api.ts": ['export * from "./src/runtime-api.js";'],
   "extensions/slack/runtime-api.ts": [
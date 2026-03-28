wonglaitung/openclaw_mini

refactor: move Telegram channel implementation to extensions/ (#45635)
* refactor: move Telegram channel implementation to extensions/telegram/src/

Move all Telegram channel code (123 files + 10 bot/ files + 8 channel plugin
files) from src/telegram/ and src/channels/plugins/*/telegram.ts to
extensions/telegram/src/. Leave thin re-export shims at original locations so
cross-cutting src/ imports continue to resolve.

- Fix all relative import paths in moved files (../X/ -> ../../../src/X/)
- Fix vi.mock paths in 60 test files
- Fix inline typeof import() expressions
- Update tsconfig.plugin-sdk.dts.json rootDir to "." for cross-directory DTS
- Update write-plugin-sdk-entry-dts.ts for new rootDir structure
- Move channel plugin files with correct path remapping

* fix: support keyed telegram send deps

* fix: sync telegram extension copies with latest main

* fix: correct import paths and remove misplaced files in telegram extension

* fix: sync outbound-adapter with main (add sendTelegramPayloadMessages) and fix delivery.test import path

extensions/telegram/src/draft-chunking.test.ts
@@ -1,0 +1,52 @@
+import { describe, expect, it } from "vitest";
+import type { OpenClawConfig } from "../../../src/config/config.js";
+import { resolveTelegramDraftStreamingChunking } from "./draft-chunking.js";
+
+describe("resolveTelegramDraftStreamingChunking", () => {
+  it("uses smaller defaults than block streaming", () => {
+    const chunking = resolveTelegramDraftStreamingChunking(undefined, "default");
+    expect(chunking).toEqual({
+      minChars: 200,
+      maxChars: 800,
+      breakPreference: "paragraph",
+    });
+  });
+
+  it("clamps to telegram.textChunkLimit", () => {
+    const cfg: OpenClawConfig = {
+      channels: { telegram: { allowFrom: ["*"], textChunkLimit: 150 } },
+    };
+    const chunking = resolveTelegramDraftStreamingChunking(cfg, "default");
+    expect(chunking).toEqual({
+      minChars: 150,
+      maxChars: 150,
+      breakPreference: "paragraph",
+    });
+  });
+
+  it("supports per-account overrides", () => {
+    const cfg: OpenClawConfig = {
+      channels: {
+        telegram: {
+          allowFrom: ["*"],
+          accounts: {
+            default: {
+              allowFrom: ["*"],
+              draftChunk: {
+                minChars: 10,
+                maxChars: 20,
+                breakPreference: "sentence",
+              },
+            },
+          },
+        },
+      },
+    };
+    const chunking = resolveTelegramDraftStreamingChunking(cfg, "default");
+    expect(chunking).toEqual({
+      minChars: 10,
+      maxChars: 20,
+      breakPreference: "sentence",
+    });
+  });
+});

extensions/telegram/src/draft-chunking.ts
@@ -1,0 +1,41 @@
+import { resolveTextChunkLimit } from "../../../src/auto-reply/chunk.js";
+import { getChannelDock } from "../../../src/channels/dock.js";
+import type { OpenClawConfig } from "../../../src/config/config.js";
+import { resolveAccountEntry } from "../../../src/routing/account-lookup.js";
+import { normalizeAccountId } from "../../../src/routing/session-key.js";
+
+const DEFAULT_TELEGRAM_DRAFT_STREAM_MIN = 200;
+const DEFAULT_TELEGRAM_DRAFT_STREAM_MAX = 800;
+
+export function resolveTelegramDraftStreamingChunking(
+  cfg: OpenClawConfig | undefined,
+  accountId?: string | null,
+): {
+  minChars: number;
+  maxChars: number;
+  breakPreference: "paragraph" | "newline" | "sentence";
+} {
+  const providerChunkLimit = getChannelDock("telegram")?.outbound?.textChunkLimit;
+  const textLimit = resolveTextChunkLimit(cfg, "telegram", accountId, {
+    fallbackLimit: providerChunkLimit,
+  });
+  const normalizedAccountId = normalizeAccountId(accountId);
+  const accountCfg = resolveAccountEntry(cfg?.channels?.telegram?.accounts, normalizedAccountId);
+  const draftCfg = accountCfg?.draftChunk ?? cfg?.channels?.telegram?.draftChunk;
+
+  const maxRequested = Math.max(
+    1,
+    Math.floor(draftCfg?.maxChars ?? DEFAULT_TELEGRAM_DRAFT_STREAM_MAX),
+  );
+  const maxChars = Math.max(1, Math.min(maxRequested, textLimit));
+  const minRequested = Math.max(
+    1,
+    Math.floor(draftCfg?.minChars ?? DEFAULT_TELEGRAM_DRAFT_STREAM_MIN),
+  );
+  const minChars = Math.min(minRequested, maxChars);
+  const breakPreference =
+    draftCfg?.breakPreference === "newline" || draftCfg?.breakPreference === "sentence"
+      ? draftCfg.breakPreference
+      : "paragraph";
+  return { minChars, maxChars, breakPreference };
+}

extensions/telegram/src/exec-approvals.test.ts
@@ -1,0 +1,92 @@
+import { describe, expect, it } from "vitest";
+import type { OpenClawConfig } from "../../../src/config/config.js";
+import {
+  isTelegramExecApprovalApprover,
+  isTelegramExecApprovalClientEnabled,
+  resolveTelegramExecApprovalTarget,
+  shouldEnableTelegramExecApprovalButtons,
+  shouldInjectTelegramExecApprovalButtons,
+} from "./exec-approvals.js";
+
+function buildConfig(
+  execApprovals?: NonNullable<NonNullable<OpenClawConfig["channels"]>["telegram"]>["execApprovals"],
+): OpenClawConfig {
+  return {
+    channels: {
+      telegram: {
+        botToken: "tok",
+        execApprovals,
+      },
+    },
+  } as OpenClawConfig;
+}
+
+describe("telegram exec approvals", () => {
+  it("requires enablement and at least one approver", () => {
+    expect(isTelegramExecApprovalClientEnabled({ cfg: buildConfig() })).toBe(false);
+    expect(
+      isTelegramExecApprovalClientEnabled({
+        cfg: buildConfig({ enabled: true }),
+      }),
+    ).toBe(false);
+    expect(
+      isTelegramExecApprovalClientEnabled({
+        cfg: buildConfig({ enabled: true, approvers: ["123"] }),
+      }),
+    ).toBe(true);
+  });
+
+  it("matches approvers by normalized sender id", () => {
+    const cfg = buildConfig({ enabled: true, approvers: [123, "456"] });
+    expect(isTelegramExecApprovalApprover({ cfg, senderId: "123" })).toBe(true);
+    expect(isTelegramExecApprovalApprover({ cfg, senderId: "456" })).toBe(true);
+    expect(isTelegramExecApprovalApprover({ cfg, senderId: "789" })).toBe(false);
+  });
+
+  it("defaults target to dm", () => {
+    expect(
+      resolveTelegramExecApprovalTarget({ cfg: buildConfig({ enabled: true, approvers: ["1"] }) }),
+    ).toBe("dm");
+  });
+
+  it("only injects approval buttons on eligible telegram targets", () => {
+    const dmCfg = buildConfig({ enabled: true, approvers: ["123"], target: "dm" });
+    const channelCfg = buildConfig({ enabled: true, approvers: ["123"], target: "channel" });
+    const bothCfg = buildConfig({ enabled: true, approvers: ["123"], target: "both" });
+
+    expect(shouldInjectTelegramExecApprovalButtons({ cfg: dmCfg, to: "123" })).toBe(true);
+    expect(shouldInjectTelegramExecApprovalButtons({ cfg: dmCfg, to: "-100123" })).toBe(false);
+    expect(shouldInjectTelegramExecApprovalButtons({ cfg: channelCfg, to: "-100123" })).toBe(true);
+    expect(shouldInjectTelegramExecApprovalButtons({ cfg: channelCfg, to: "123" })).toBe(false);
+    expect(shouldInjectTelegramExecApprovalButtons({ cfg: bothCfg, to: "123" })).toBe(true);
+    expect(shouldInjectTelegramExecApprovalButtons({ cfg: bothCfg, to: "-100123" })).toBe(true);
+  });
+
+  it("does not require generic inlineButtons capability to enable exec approval buttons", () => {
+    const cfg = {
+      channels: {
+        telegram: {
+          botToken: "tok",
+          capabilities: ["vision"],
+          execApprovals: { enabled: true, approvers: ["123"], target: "dm" },
+        },
+      },
+    } as OpenClawConfig;
+
+    expect(shouldEnableTelegramExecApprovalButtons({ cfg, to: "123" })).toBe(true);
+  });
+
+  it("still respects explicit inlineButtons off for exec approval buttons", () => {
+    const cfg = {
+      channels: {
+        telegram: {
+          botToken: "tok",
+          capabilities: { inlineButtons: "off" },
+          execApprovals: { enabled: true, approvers: ["123"], target: "dm" },
+        },
+      },
+    } as OpenClawConfig;
+
+    expect(shouldEnableTelegramExecApprovalButtons({ cfg, to: "123" })).toBe(false);
+  });
+});

extensions/telegram/src/inline-buttons.ts
@@ -1,0 +1,67 @@
+import type { OpenClawConfig } from "../../../src/config/config.js";
+import type { TelegramInlineButtonsScope } from "../../../src/config/types.telegram.js";
+import { listTelegramAccountIds, resolveTelegramAccount } from "./accounts.js";
+
+const DEFAULT_INLINE_BUTTONS_SCOPE: TelegramInlineButtonsScope = "allowlist";
+
+function normalizeInlineButtonsScope(value: unknown): TelegramInlineButtonsScope | undefined {
+  if (typeof value !== "string") {
+    return undefined;
+  }
+  const trimmed = value.trim().toLowerCase();
+  if (
+    trimmed === "off" ||
+    trimmed === "dm" ||
+    trimmed === "group" ||
+    trimmed === "all" ||
+    trimmed === "allowlist"
+  ) {
+    return trimmed as TelegramInlineButtonsScope;
+  }
+  return undefined;
+}
+
+function resolveInlineButtonsScopeFromCapabilities(
+  capabilities: unknown,
+): TelegramInlineButtonsScope {
+  if (!capabilities) {
+    return DEFAULT_INLINE_BUTTONS_SCOPE;
+  }
+  if (Array.isArray(capabilities)) {
+    const enabled = capabilities.some(
+      (entry) => String(entry).trim().toLowerCase() === "inlinebuttons",
+    );
+    return enabled ? "all" : "off";
+  }
+  if (typeof capabilities === "object") {
+    const inlineButtons = (capabilities as { inlineButtons?: unknown }).inlineButtons;
+    return normalizeInlineButtonsScope(inlineButtons) ?? DEFAULT_INLINE_BUTTONS_SCOPE;
+  }
+  return DEFAULT_INLINE_BUTTONS_SCOPE;
+}
+
+export function resolveTelegramInlineButtonsScope(params: {
+  cfg: OpenClawConfig;
+  accountId?: string | null;
+}): TelegramInlineButtonsScope {
+  const account = resolveTelegramAccount({ cfg: params.cfg, accountId: params.accountId });
+  return resolveInlineButtonsScopeFromCapabilities(account.config.capabilities);
+}
+
+export function isTelegramInlineButtonsEnabled(params: {
+  cfg: OpenClawConfig;
+  accountId?: string | null;
+}): boolean {
+  if (params.accountId) {
+    return resolveTelegramInlineButtonsScope(params) !== "off";
+  }
+  const accountIds = listTelegramAccountIds(params.cfg);
+  if (accountIds.length === 0) {
+    return resolveTelegramInlineButtonsScope(params) !== "off";
+  }
+  return accountIds.some(
+    (accountId) => resolveTelegramInlineButtonsScope({ cfg: params.cfg, accountId }) !== "off",
+  );
+}
+
+export { resolveTelegramTargetChatType } from "./targets.js";

remoteclaw/remoteclaw

(cherry picked from commit e5bca0832fbd01b98eeede548d2f1cf94166f149)
# Conflicts:
#	extensions/telegram/src/bot-native-commands.skills-allowlist.test.ts
#	extensions/telegram/src/model-buttons.test.ts
#	extensions/telegram/src/reasoning-lane-coordinator.test.ts
#	src/channels/plugins/outbound/telegram.test.ts
#	src/telegram/account-inspect.test.ts
#	src/telegram/approval-buttons.test.ts
#	src/telegram/approval-buttons.ts
#	src/telegram/exec-approvals-handler.test.ts
#	src/telegram/exec-approvals-handler.ts
#	src/telegram/exec-approvals.test.ts
#	src/telegram/exec-approvals.ts
#	src/telegram/model-buttons.ts
#	src/telegram/reasoning-lane-coordinator.ts

extensions/telegram/src/draft-chunking.test.ts
@@ -1,0 +1,52 @@
+import { describe, expect, it } from "vitest";
+import type { RemoteClawConfig } from "../../../src/config/config.js";
+import { resolveTelegramDraftStreamingChunking } from "./draft-chunking.js";
+
+describe("resolveTelegramDraftStreamingChunking", () => {
+  it("uses smaller defaults than block streaming", () => {
+    const chunking = resolveTelegramDraftStreamingChunking(undefined, "default");
+    expect(chunking).toEqual({
+      minChars: 200,
+      maxChars: 800,
+      breakPreference: "paragraph",
+    });
+  });
+
+  it("clamps to telegram.textChunkLimit", () => {
+    const cfg: RemoteClawConfig = {
+      channels: { telegram: { allowFrom: ["*"], textChunkLimit: 150 } },
+    };
+    const chunking = resolveTelegramDraftStreamingChunking(cfg, "default");
+    expect(chunking).toEqual({
+      minChars: 150,
+      maxChars: 150,
+      breakPreference: "paragraph",
+    });
+  });
+
+  it("supports per-account overrides", () => {
+    const cfg: RemoteClawConfig = {
+      channels: {
+        telegram: {
+          allowFrom: ["*"],
+          accounts: {
+            default: {
+              allowFrom: ["*"],
+              draftChunk: {
+                minChars: 10,
+                maxChars: 20,
+                breakPreference: "sentence",
+              },
+            },
+          },
+        },
+      },
+    };
+    const chunking = resolveTelegramDraftStreamingChunking(cfg, "default");
+    expect(chunking).toEqual({
+      minChars: 10,
+      maxChars: 20,
+      breakPreference: "sentence",
+    });
+  });
+});

extensions/telegram/src/draft-chunking.ts
@@ -1,0 +1,41 @@
+import { resolveTextChunkLimit } from "../../../src/auto-reply/chunk.js";
+import { getChannelDock } from "../../../src/channels/dock.js";
+import type { RemoteClawConfig } from "../../../src/config/config.js";
+import { resolveAccountEntry } from "../../../src/routing/account-lookup.js";
+import { normalizeAccountId } from "../../../src/routing/session-key.js";
+
+const DEFAULT_TELEGRAM_DRAFT_STREAM_MIN = 200;
+const DEFAULT_TELEGRAM_DRAFT_STREAM_MAX = 800;
+
+export function resolveTelegramDraftStreamingChunking(
+  cfg: RemoteClawConfig | undefined,
+  accountId?: string | null,
+): {
+  minChars: number;
+  maxChars: number;
+  breakPreference: "paragraph" | "newline" | "sentence";
+} {
+  const providerChunkLimit = getChannelDock("telegram")?.outbound?.textChunkLimit;
+  const textLimit = resolveTextChunkLimit(cfg, "telegram", accountId, {
+    fallbackLimit: providerChunkLimit,
+  });
+  const normalizedAccountId = normalizeAccountId(accountId);
+  const accountCfg = resolveAccountEntry(cfg?.channels?.telegram?.accounts, normalizedAccountId);
+  const draftCfg = accountCfg?.draftChunk ?? cfg?.channels?.telegram?.draftChunk;
+
+  const maxRequested = Math.max(
+    1,
+    Math.floor(draftCfg?.maxChars ?? DEFAULT_TELEGRAM_DRAFT_STREAM_MAX),
+  );
+  const maxChars = Math.max(1, Math.min(maxRequested, textLimit));
+  const minRequested = Math.max(
+    1,
+    Math.floor(draftCfg?.minChars ?? DEFAULT_TELEGRAM_DRAFT_STREAM_MIN),
+  );
+  const minChars = Math.min(minRequested, maxChars);
+  const breakPreference =
+    draftCfg?.breakPreference === "newline" || draftCfg?.breakPreference === "sentence"
+      ? draftCfg.breakPreference
+      : "paragraph";
+  return { minChars, maxChars, breakPreference };
+}

extensions/telegram/src/exec-approvals.test.ts
@@ -1,0 +1,92 @@
+import { describe, expect, it } from "vitest";
+import type { RemoteClawConfig } from "../../../src/config/config.js";
+import {
+  isTelegramExecApprovalApprover,
+  isTelegramExecApprovalClientEnabled,
+  resolveTelegramExecApprovalTarget,
+  shouldEnableTelegramExecApprovalButtons,
+  shouldInjectTelegramExecApprovalButtons,
+} from "./exec-approvals.js";
+
+function buildConfig(
+  execApprovals?: NonNullable<NonNullable<RemoteClawConfig["channels"]>["telegram"]>["execApprovals"],
+): RemoteClawConfig {
+  return {
+    channels: {
+      telegram: {
+        botToken: "tok",
+        execApprovals,
+      },
+    },
+  } as RemoteClawConfig;
+}
+
+describe("telegram exec approvals", () => {
+  it("requires enablement and at least one approver", () => {
+    expect(isTelegramExecApprovalClientEnabled({ cfg: buildConfig() })).toBe(false);
+    expect(
+      isTelegramExecApprovalClientEnabled({
+        cfg: buildConfig({ enabled: true }),
+      }),
+    ).toBe(false);
+    expect(
+      isTelegramExecApprovalClientEnabled({
+        cfg: buildConfig({ enabled: true, approvers: ["123"] }),
+      }),
+    ).toBe(true);
+  });
+
+  it("matches approvers by normalized sender id", () => {
+    const cfg = buildConfig({ enabled: true, approvers: [123, "456"] });
+    expect(isTelegramExecApprovalApprover({ cfg, senderId: "123" })).toBe(true);
+    expect(isTelegramExecApprovalApprover({ cfg, senderId: "456" })).toBe(true);
+    expect(isTelegramExecApprovalApprover({ cfg, senderId: "789" })).toBe(false);
+  });
+
+  it("defaults target to dm", () => {
+    expect(
+      resolveTelegramExecApprovalTarget({ cfg: buildConfig({ enabled: true, approvers: ["1"] }) }),
+    ).toBe("dm");
+  });
+
+  it("only injects approval buttons on eligible telegram targets", () => {
+    const dmCfg = buildConfig({ enabled: true, approvers: ["123"], target: "dm" });
+    const channelCfg = buildConfig({ enabled: true, approvers: ["123"], target: "channel" });
+    const bothCfg = buildConfig({ enabled: true, approvers: ["123"], target: "both" });
+
+    expect(shouldInjectTelegramExecApprovalButtons({ cfg: dmCfg, to: "123" })).toBe(true);
+    expect(shouldInjectTelegramExecApprovalButtons({ cfg: dmCfg, to: "-100123" })).toBe(false);
+    expect(shouldInjectTelegramExecApprovalButtons({ cfg: channelCfg, to: "-100123" })).toBe(true);
+    expect(shouldInjectTelegramExecApprovalButtons({ cfg: channelCfg, to: "123" })).toBe(false);
+    expect(shouldInjectTelegramExecApprovalButtons({ cfg: bothCfg, to: "123" })).toBe(true);
+    expect(shouldInjectTelegramExecApprovalButtons({ cfg: bothCfg, to: "-100123" })).toBe(true);
+  });
+
+  it("does not require generic inlineButtons capability to enable exec approval buttons", () => {
+    const cfg = {
+      channels: {
+        telegram: {
+          botToken: "tok",
+          capabilities: ["vision"],
+          execApprovals: { enabled: true, approvers: ["123"], target: "dm" },
+        },
+      },
+    } as RemoteClawConfig;
+
+    expect(shouldEnableTelegramExecApprovalButtons({ cfg, to: "123" })).toBe(true);
+  });
+
+  it("still respects explicit inlineButtons off for exec approval buttons", () => {
+    const cfg = {
+      channels: {
+        telegram: {
+          botToken: "tok",
+          capabilities: { inlineButtons: "off" },
+          execApprovals: { enabled: true, approvers: ["123"], target: "dm" },
+        },
+      },
+    } as RemoteClawConfig;
+
+    expect(shouldEnableTelegramExecApprovalButtons({ cfg, to: "123" })).toBe(false);
+  });
+});

extensions/telegram/src/inline-buttons.ts
@@ -1,0 +1,67 @@
+import type { RemoteClawConfig } from "../../../src/config/config.js";
+import type { TelegramInlineButtonsScope } from "../../../src/config/types.telegram.js";
+import { listTelegramAccountIds, resolveTelegramAccount } from "./accounts.js";
+
+const DEFAULT_INLINE_BUTTONS_SCOPE: TelegramInlineButtonsScope = "allowlist";
+
+function normalizeInlineButtonsScope(value: unknown): TelegramInlineButtonsScope | undefined {
+  if (typeof value !== "string") {
+    return undefined;
+  }
+  const trimmed = value.trim().toLowerCase();
+  if (
+    trimmed === "off" ||
+    trimmed === "dm" ||
+    trimmed === "group" ||
+    trimmed === "all" ||
+    trimmed === "allowlist"
+  ) {
+    return trimmed as TelegramInlineButtonsScope;
+  }
+  return undefined;
+}
+
+function resolveInlineButtonsScopeFromCapabilities(
+  capabilities: unknown,
+): TelegramInlineButtonsScope {
+  if (!capabilities) {
+    return DEFAULT_INLINE_BUTTONS_SCOPE;
+  }
+  if (Array.isArray(capabilities)) {
+    const enabled = capabilities.some(
+      (entry) => String(entry).trim().toLowerCase() === "inlinebuttons",
+    );
+    return enabled ? "all" : "off";
+  }
+  if (typeof capabilities === "object") {
+    const inlineButtons = (capabilities as { inlineButtons?: unknown }).inlineButtons;
+    return normalizeInlineButtonsScope(inlineButtons) ?? DEFAULT_INLINE_BUTTONS_SCOPE;
+  }
+  return DEFAULT_INLINE_BUTTONS_SCOPE;
+}
+
+export function resolveTelegramInlineButtonsScope(params: {
+  cfg: RemoteClawConfig;
+  accountId?: string | null;
+}): TelegramInlineButtonsScope {
+  const account = resolveTelegramAccount({ cfg: params.cfg, accountId: params.accountId });
+  return resolveInlineButtonsScopeFromCapabilities(account.config.capabilities);
+}
+
+export function isTelegramInlineButtonsEnabled(params: {
+  cfg: RemoteClawConfig;
+  accountId?: string | null;
+}): boolean {
+  if (params.accountId) {
+    return resolveTelegramInlineButtonsScope(params) !== "off";
+  }
+  const accountIds = listTelegramAccountIds(params.cfg);
+  if (accountIds.length === 0) {
+    return resolveTelegramInlineButtonsScope(params) !== "off";
+  }
+  return accountIds.some(
+    (accountId) => resolveTelegramInlineButtonsScope({ cfg: params.cfg, accountId }) !== "off",
+  );
+}
+
+export { resolveTelegramTargetChatType } from "./targets.js";
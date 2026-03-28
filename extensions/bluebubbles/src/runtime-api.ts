Nachx639/clawdbot

fix(plugin-sdk): remove relative extension boundary escapes (#51939)
* fix(plugin-sdk): remove relative extension boundary escapes

* Gate new plugin-sdk subpaths on host version

* Add changelog entry for #51939

* Fix local staging for plugin-sdk host version gate

* Raise host floor for line and googlechat plugins

---------

Co-authored-by: Tak Hoffman <781889+Takhoffman@users.noreply.github.com>

extensions/bluebubbles/src/runtime-api.ts
@@ -1,1 +1,1 @@
-export * from "../../../src/plugin-sdk/bluebubbles.js";
+export * from "openclaw/plugin-sdk/bluebubbles";

extensions/matrix/src/runtime-api.ts
@@ -1,4 +1,4 @@
-export * from "../../../src/plugin-sdk/matrix.js";
+export * from "openclaw/plugin-sdk/matrix";
 export {
   assertHttpUrlTargetsPrivateNetwork,
   closeDispatcher,

extensions/whatsapp/src/runtime-api.ts
@@ -16,7 +16,7 @@ export {
   WhatsAppConfigSchema,
   type ChannelPlugin,
   type OpenClawConfig,
-} from "../../../src/plugin-sdk/whatsapp-core.js";
+} from "openclaw/plugin-sdk/whatsapp-core";
 
 export {
   createWhatsAppOutboundBase,
@@ -31,6 +31,6 @@ export {
   type DmPolicy,
   type GroupPolicy,
   type WhatsAppAccountConfig,
-} from "../../../src/plugin-sdk/whatsapp-shared.js";
+} from "openclaw/plugin-sdk/whatsapp-shared";
 
 export { monitorWebChannel } from "./channel.runtime.js";

remoteclaw/remoteclaw

(cherry picked from commit 2b4c3c2057273d3d2f910c372b4b145e8312e09a)
# Conflicts:
#	scripts/stage-bundled-plugin-runtime-deps.mjs

extensions/bluebubbles/src/runtime-api.ts
@@ -1,1 +1,1 @@
-export * from "../../../src/plugin-sdk/bluebubbles.js";
+export * from "remoteclaw/plugin-sdk/bluebubbles";

extensions/matrix/src/runtime-api.ts
@@ -1,4 +1,4 @@
-export * from "../../../src/plugin-sdk/matrix.js";
+export * from "remoteclaw/plugin-sdk/matrix";
 export {
   assertHttpUrlTargetsPrivateNetwork,
   closeDispatcher,

extensions/whatsapp/src/runtime-api.ts
@@ -15,7 +15,7 @@ export {
   WhatsAppConfigSchema,
   type ChannelPlugin,
   type RemoteClawConfig,
-} from "../../../src/plugin-sdk/whatsapp-core.js";
+} from "remoteclaw/plugin-sdk/whatsapp-core";
 
 export {
   createWhatsAppOutboundBase,
@@ -28,12 +28,6 @@ export {
   type DmPolicy,
   type GroupPolicy,
   type WhatsAppAccountConfig,
-} from "../../../src/plugin-sdk/whatsapp-shared.js";
-export {
-  isWhatsAppGroupJid,
-  isWhatsAppUserTarget,
-  normalizeWhatsAppTarget,
-} from "./normalize-target.js";
-export { resolveWhatsAppOutboundTarget } from "./resolve-outbound-target.js";
+} from "remoteclaw/plugin-sdk/whatsapp-shared";
 
 export { monitorWebChannel } from "./channel.runtime.js";
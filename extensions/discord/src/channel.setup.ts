wonglaitung/openclaw_mini

refactor(discord): use shared plugin base

extensions/discord/src/channel.setup.ts
@@ -1,43 +1,10 @@
-import {
-  buildChannelConfigSchema,
-  DiscordConfigSchema,
-  getChatChannelMeta,
-  type ChannelPlugin,
-} from "openclaw/plugin-sdk/discord";
+import { type ChannelPlugin } from "openclaw/plugin-sdk/discord";
 import { type ResolvedDiscordAccount } from "./accounts.js";
-import { discordConfigAccessors, discordConfigBase, discordSetupWizard } from "./plugin-shared.js";
 import { discordSetupAdapter } from "./setup-core.js";
+import { createDiscordPluginBase } from "./shared.js";
 
 export const discordSetupPlugin: ChannelPlugin<ResolvedDiscordAccount> = {
-  id: "discord",
-  meta: {
-    ...getChatChannelMeta("discord"),
-  },
-  setupWizard: discordSetupWizard,
-  capabilities: {
-    chatTypes: ["direct", "channel", "thread"],
-    polls: true,
-    reactions: true,
-    threads: true,
-    media: true,
-    nativeCommands: true,
-  },
-  streaming: {
-    blockStreamingCoalesceDefaults: { minChars: 1500, idleMs: 1000 },
-  },
-  reload: { configPrefixes: ["channels.discord"] },
-  configSchema: buildChannelConfigSchema(DiscordConfigSchema),
-  config: {
-    ...discordConfigBase,
-    isConfigured: (account) => Boolean(account.token?.trim()),
-    describeAccount: (account) => ({
-      accountId: account.accountId,
-      name: account.name,
-      enabled: account.enabled,
-      configured: Boolean(account.token?.trim()),
-      tokenSource: account.tokenSource,
-    }),
-    ...discordConfigAccessors,
-  },
-  setup: discordSetupAdapter,
+  ...createDiscordPluginBase({
+    setup: discordSetupAdapter,
+  }),
 };

remoteclaw/remoteclaw

(cherry picked from commit 4b001c793448c0326b902b6ea02cebf22c9d183c)
# Conflicts:
#	extensions/discord/src/plugin-shared.ts

extensions/discord/src/channel.setup.ts
@@ -1,43 +1,10 @@
-import {
-  buildChannelConfigSchema,
-  DiscordConfigSchema,
-  getChatChannelMeta,
-  type ChannelPlugin,
-} from "remoteclaw/plugin-sdk/discord";
+import { type ChannelPlugin } from "remoteclaw/plugin-sdk/discord";
 import { type ResolvedDiscordAccount } from "./accounts.js";
-import { discordConfigAccessors, discordConfigBase, discordSetupWizard } from "./plugin-shared.js";
 import { discordSetupAdapter } from "./setup-core.js";
+import { createDiscordPluginBase } from "./shared.js";
 
 export const discordSetupPlugin: ChannelPlugin<ResolvedDiscordAccount> = {
-  id: "discord",
-  meta: {
-    ...getChatChannelMeta("discord"),
-  },
-  setupWizard: discordSetupWizard,
-  capabilities: {
-    chatTypes: ["direct", "channel", "thread"],
-    polls: true,
-    reactions: true,
-    threads: true,
-    media: true,
-    nativeCommands: true,
-  },
-  streaming: {
-    blockStreamingCoalesceDefaults: { minChars: 1500, idleMs: 1000 },
-  },
-  reload: { configPrefixes: ["channels.discord"] },
-  configSchema: buildChannelConfigSchema(DiscordConfigSchema),
-  config: {
-    ...discordConfigBase,
-    isConfigured: (account) => Boolean(account.token?.trim()),
-    describeAccount: (account) => ({
-      accountId: account.accountId,
-      name: account.name,
-      enabled: account.enabled,
-      configured: Boolean(account.token?.trim()),
-      tokenSource: account.tokenSource,
-    }),
-    ...discordConfigAccessors,
-  },
-  setup: discordSetupAdapter,
+  ...createDiscordPluginBase({
+    setup: discordSetupAdapter,
+  }),
 };
wonglaitung/openclaw_mini

refactor: untangle remaining plugin sdk boundaries

extensions/discord/src/monitor/provider.allowlist.ts
@@ -4,11 +4,11 @@ import {
   canonicalizeAllowlistWithResolvedIds,
   patchAllowlistUsersInConfigEntries,
   summarizeMapping,
-} from "../../../../src/channels/allowlists/resolve-utils.js";
-import type { DiscordGuildEntry } from "../../../../src/config/types.discord.js";
-import { formatErrorMessage } from "../../../../src/infra/errors.js";
-import type { RuntimeEnv } from "../../../../src/runtime.js";
-import { normalizeStringEntries } from "../../../../src/shared/string-normalization.js";
+} from "openclaw/plugin-sdk/channel-runtime";
+import type { DiscordGuildEntry } from "openclaw/plugin-sdk/config-runtime";
+import { formatErrorMessage } from "openclaw/plugin-sdk/infra-runtime";
+import type { RuntimeEnv } from "openclaw/plugin-sdk/runtime-env";
+import { normalizeStringEntries } from "openclaw/plugin-sdk/text-runtime";
 import { resolveDiscordChannelAllowlist } from "../resolve-channels.js";
 import { resolveDiscordUserAllowlist } from "../resolve-users.js";
 

extensions/signal/src/runtime.ts
@@ -1,7 +1,5 @@
-import {
-  createPluginRuntimeStore,
-  type PluginRuntime,
-} from "../../../src/plugin-sdk-internal/core.js";
+import type { PluginRuntime } from "openclaw/plugin-sdk/core";
+import { createPluginRuntimeStore } from "openclaw/plugin-sdk/runtime-store";
 
 const { setRuntime: setSignalRuntime, getRuntime: getSignalRuntime } =
   createPluginRuntimeStore<PluginRuntime>("Signal runtime not initialized");

extensions/telegram/src/api-logging.ts
@@ -1,7 +1,7 @@
-import { danger } from "../../../src/globals.js";
-import { formatErrorMessage } from "../../../src/infra/errors.js";
-import { createSubsystemLogger } from "../../../src/logging/subsystem.js";
-import type { RuntimeEnv } from "../../../src/runtime.js";
+import { formatErrorMessage } from "openclaw/plugin-sdk/infra-runtime";
+import { danger } from "openclaw/plugin-sdk/runtime-env";
+import { createSubsystemLogger } from "openclaw/plugin-sdk/runtime-env";
+import type { RuntimeEnv } from "openclaw/plugin-sdk/runtime-env";
 
 export type TelegramApiLogger = (message: string) => void;
 

extensions/test-utils/directory.ts
@@ -1,4 +1,4 @@
-import type { ChannelDirectoryAdapter } from "../../src/channels/plugins/types.js";
+import type { ChannelDirectoryAdapter } from "openclaw/plugin-sdk/channel-runtime";
 
 export function createDirectoryTestRuntime() {
   return {

remoteclaw/remoteclaw

(cherry picked from commit 9ebe38b6e36b6128b194e17892e95920cafee42b)
# Conflicts:
#	extensions/acpx/src/test-utils/runtime-fixtures.ts
#	extensions/anthropic/index.ts
#	extensions/brave/index.ts
#	extensions/byteplus/index.ts
#	extensions/byteplus/provider-catalog.ts
#	extensions/cloudflare-ai-gateway/index.ts
#	extensions/cloudflare-ai-gateway/onboard.ts
#	extensions/discord/src/channel.setup.ts
#	extensions/discord/src/plugin-shared.ts
#	extensions/discord/src/setup-core.ts
#	extensions/discord/src/setup-surface.ts
#	extensions/discord/src/shared-interactive.ts
#	extensions/elevenlabs/index.ts
#	extensions/feishu/src/thread-bindings.ts
#	extensions/firecrawl/index.ts
#	extensions/firecrawl/src/config.ts
#	extensions/firecrawl/src/firecrawl-client.ts
#	extensions/firecrawl/src/firecrawl-scrape-tool.ts
#	extensions/firecrawl/src/firecrawl-search-provider.ts
#	extensions/firecrawl/src/firecrawl-search-tool.ts
#	extensions/github-copilot/index.ts
#	extensions/github-copilot/token.ts
#	extensions/google/gemini-cli-provider.ts
#	extensions/google/index.ts
#	extensions/google/oauth.flow.ts
#	extensions/google/oauth.http.ts
#	extensions/google/provider-models.ts
#	extensions/huggingface/index.ts
#	extensions/huggingface/onboard.ts
#	extensions/huggingface/provider-catalog.ts
#	extensions/imessage/src/channel.setup.ts
#	extensions/imessage/src/plugin-shared.ts
#	extensions/imessage/src/setup-core.ts
#	extensions/imessage/src/setup-surface.ts
#	extensions/irc/src/setup-core.ts
#	extensions/irc/src/setup-surface.ts
#	extensions/kilocode/index.ts
#	extensions/kilocode/onboard.ts
#	extensions/kilocode/provider-catalog.ts
#	extensions/kimi-coding/index.ts
#	extensions/kimi-coding/onboard.ts
#	extensions/kimi-coding/provider-catalog.ts
#	extensions/line/src/channel.setup.ts
#	extensions/line/src/setup-surface.ts
#	extensions/mattermost/src/setup-surface.ts
#	extensions/microsoft/index.ts
#	extensions/minimax/index.ts
#	extensions/minimax/onboard.ts
#	extensions/minimax/provider-

extensions/discord/src/monitor/provider.allowlist.ts
@@ -4,11 +4,11 @@ import {
   canonicalizeAllowlistWithResolvedIds,
   patchAllowlistUsersInConfigEntries,
   summarizeMapping,
-} from "../../../../src/channels/allowlists/resolve-utils.js";
-import type { DiscordGuildEntry } from "../../../../src/config/types.discord.js";
-import { formatErrorMessage } from "../../../../src/infra/errors.js";
-import type { RuntimeEnv } from "../../../../src/runtime.js";
-import { normalizeStringEntries } from "../../../../src/shared/string-normalization.js";
+} from "remoteclaw/plugin-sdk/channel-runtime";
+import type { DiscordGuildEntry } from "remoteclaw/plugin-sdk/config-runtime";
+import { formatErrorMessage } from "remoteclaw/plugin-sdk/infra-runtime";
+import type { RuntimeEnv } from "remoteclaw/plugin-sdk/runtime-env";
+import { normalizeStringEntries } from "remoteclaw/plugin-sdk/text-runtime";
 import { resolveDiscordChannelAllowlist } from "../resolve-channels.js";
 import { resolveDiscordUserAllowlist } from "../resolve-users.js";
 

extensions/signal/src/runtime.ts
@@ -1,4 +1,5 @@
-import type { PluginRuntime } from "remoteclaw/plugin-sdk/signal";
+import type { PluginRuntime } from "remoteclaw/plugin-sdk/core";
+import { createPluginRuntimeStore } from "remoteclaw/plugin-sdk/runtime-store";
 
 let runtime: PluginRuntime | null = null;
 

extensions/telegram/src/api-logging.ts
@@ -1,7 +1,7 @@
-import { danger } from "../../../src/globals.js";
-import { formatErrorMessage } from "../../../src/infra/errors.js";
-import { createSubsystemLogger } from "../../../src/logging/subsystem.js";
-import type { RuntimeEnv } from "../../../src/runtime.js";
+import { formatErrorMessage } from "remoteclaw/plugin-sdk/infra-runtime";
+import { danger } from "remoteclaw/plugin-sdk/runtime-env";
+import { createSubsystemLogger } from "remoteclaw/plugin-sdk/runtime-env";
+import type { RuntimeEnv } from "remoteclaw/plugin-sdk/runtime-env";
 
 export type TelegramApiLogger = (message: string) => void;
 

extensions/test-utils/directory.ts
@@ -1,3 +1,5 @@
+import type { ChannelDirectoryAdapter } from "remoteclaw/plugin-sdk/channel-runtime";
+
 export function createDirectoryTestRuntime() {
   return {
     log: () => {},
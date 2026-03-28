wonglaitung/openclaw_mini

refactor: add shared setup sdk subpath

extensions/googlechat/src/setup-core.ts
@@ -1,10 +1,11 @@
 import {
   applyAccountNameToChannelSection,
   applySetupAccountConfigPatch,
+  DEFAULT_ACCOUNT_ID,
   migrateBaseNameToDefaultAccount,
-} from "../../../src/channels/plugins/setup-helpers.js";
-import type { ChannelSetupAdapter } from "../../../src/channels/plugins/types.adapters.js";
-import { DEFAULT_ACCOUNT_ID, normalizeAccountId } from "../../../src/routing/session-key.js";
+  normalizeAccountId,
+  type ChannelSetupAdapter,
+} from "openclaw/plugin-sdk/setup";
 
 const channel = "googlechat" as const;
 

extensions/slack/src/monitor/policy.ts
@@ -1,4 +1,4 @@
-import { evaluateGroupRouteAccessForPolicy } from "../../../../src/plugin-sdk/group-access.js";
+import { evaluateGroupRouteAccessForPolicy } from "openclaw/plugin-sdk/group-access";
 
 export function isSlackChannelAllowedByPolicy(params: {
   groupPolicy: "open" | "disabled" | "allowlist";

extensions/zalouser/src/setup-surface.ts
@@ -1,14 +1,15 @@
-import { patchScopedAccountConfig } from "../../../src/channels/plugins/setup-helpers.js";
 import {
+  DEFAULT_ACCOUNT_ID,
+  formatDocsLink,
+  formatResolvedUnresolvedNote,
   mergeAllowFromEntries,
+  normalizeAccountId,
+  patchScopedAccountConfig,
   setTopLevelChannelDmPolicyWithAllowFrom,
-} from "../../../src/channels/plugins/setup-wizard-helpers.js";
-import type { ChannelSetupDmPolicy } from "../../../src/channels/plugins/setup-wizard-types.js";
-import type { ChannelSetupWizard } from "../../../src/channels/plugins/setup-wizard.js";
-import type { OpenClawConfig } from "../../../src/config/config.js";
-import { formatResolvedUnresolvedNote } from "../../../src/plugin-sdk/resolution-notes.js";
-import { DEFAULT_ACCOUNT_ID, normalizeAccountId } from "../../../src/routing/session-key.js";
-import { formatDocsLink } from "../../../src/terminal/links.js";
+  type ChannelSetupDmPolicy,
+  type ChannelSetupWizard,
+  type OpenClawConfig,
+} from "openclaw/plugin-sdk/setup";
 import {
   listZalouserAccountIds,
   resolveDefaultZalouserAccountId,

src/plugin-sdk/subpaths.test.ts
@@ -14,6 +14,7 @@ import * as ollamaSetupSdk from "openclaw/plugin-sdk/ollama-setup";
 import * as providerSetupSdk from "openclaw/plugin-sdk/provider-setup";
 import * as sandboxSdk from "openclaw/plugin-sdk/sandbox";
 import * as selfHostedProviderSetupSdk from "openclaw/plugin-sdk/self-hosted-provider-setup";
+import * as setupSdk from "openclaw/plugin-sdk/setup";
 import * as signalSdk from "openclaw/plugin-sdk/signal";
 import * as slackSdk from "openclaw/plugin-sdk/slack";
 import * as telegramSdk from "openclaw/plugin-sdk/telegram";
@@ -61,6 +62,14 @@ describe("plugin-sdk subpath exports", () => {
     expect(typeof providerSetupSdk.promptAndConfigureOpenAICompatibleSelfHostedProviderAuth).toBe(
       "function",
     );
+  });
+
+  it("exports shared setup helpers from the dedicated subpath", () => {
+    expect(typeof setupSdk.DEFAULT_ACCOUNT_ID).toBe("string");
+    expect(typeof setupSdk.formatDocsLink).toBe("function");
+    expect(typeof setupSdk.mergeAllowFromEntries).toBe("function");
+    expect(typeof setupSdk.setTopLevelChannelDmPolicyWithAllowFrom).toBe("function");
+    expect(typeof setupSdk.formatResolvedUnresolvedNote).toBe("function");
   });
 
   it("exports narrow self-hosted provider setup helpers", () => {

remoteclaw/remoteclaw

(cherry picked from commit 2497b8147e85ca9e842ae2ef4c3626c07caa790c)
# Conflicts:
#	extensions/bluebubbles/src/setup-core.ts
#	extensions/feishu/src/setup-core.ts
#	extensions/googlechat/src/setup-surface.ts
#	extensions/matrix/src/setup-core.ts
#	extensions/msteams/src/setup-core.ts
#	extensions/tlon/src/setup-surface.ts

extensions/googlechat/src/setup-core.ts
@@ -1,10 +1,11 @@
 import {
   applyAccountNameToChannelSection,
   applySetupAccountConfigPatch,
+  DEFAULT_ACCOUNT_ID,
   migrateBaseNameToDefaultAccount,
-} from "../../../src/channels/plugins/setup-helpers.js";
-import type { ChannelSetupAdapter } from "../../../src/channels/plugins/types.adapters.js";
-import { DEFAULT_ACCOUNT_ID, normalizeAccountId } from "../../../src/routing/session-key.js";
+  normalizeAccountId,
+  type ChannelSetupAdapter,
+} from "remoteclaw/plugin-sdk/setup";
 
 const channel = "googlechat" as const;
 

extensions/slack/src/monitor/policy.ts
@@ -1,4 +1,4 @@
-import { evaluateGroupRouteAccessForPolicy } from "../../../../src/plugin-sdk/group-access.js";
+import { evaluateGroupRouteAccessForPolicy } from "remoteclaw/plugin-sdk/group-access";
 
 export function isSlackChannelAllowedByPolicy(params: {
   groupPolicy: "open" | "disabled" | "allowlist";

extensions/zalouser/src/setup-surface.ts
@@ -1,18 +1,15 @@
-import type {
-  ChannelOnboardingAdapter,
-  ChannelOnboardingDmPolicy,
-  RemoteClawConfig,
-  WizardPrompter,
-} from "remoteclaw/plugin-sdk";
 import {
   DEFAULT_ACCOUNT_ID,
+  formatDocsLink,
   formatResolvedUnresolvedNote,
   mergeAllowFromEntries,
   normalizeAccountId,
-  promptChannelAccessConfig,
-  resolveAccountIdForConfigure,
+  patchScopedAccountConfig,
   setTopLevelChannelDmPolicyWithAllowFrom,
-} from "remoteclaw/plugin-sdk";
+  type ChannelSetupDmPolicy,
+  type ChannelSetupWizard,
+  type RemoteClawConfig,
+} from "remoteclaw/plugin-sdk/setup";
 import {
   listZalouserAccountIds,
   resolveDefaultZalouserAccountId,

src/plugin-sdk/subpaths.test.ts
@@ -33,28 +33,20 @@ import type {
   RemoteClawPluginApi as CoreRemoteClawPluginApi,
   PluginRuntime as CorePluginRuntime,
 } from "remoteclaw/plugin-sdk/core";
-import * as directoryRuntimeSdk from "remoteclaw/plugin-sdk/directory-runtime";
-import * as infraRuntimeSdk from "remoteclaw/plugin-sdk/infra-runtime";
-import * as lazyRuntimeSdk from "remoteclaw/plugin-sdk/lazy-runtime";
-import * as matrixRuntimeSharedSdk from "remoteclaw/plugin-sdk/matrix-runtime-shared";
-import * as mediaRuntimeSdk from "remoteclaw/plugin-sdk/media-runtime";
+import * as discordSdk from "remoteclaw/plugin-sdk/discord";
+import * as imessageSdk from "remoteclaw/plugin-sdk/imessage";
+import * as lineSdk from "remoteclaw/plugin-sdk/line";
+import * as msteamsSdk from "remoteclaw/plugin-sdk/msteams";
+import * as nostrSdk from "remoteclaw/plugin-sdk/nostr";
 import * as ollamaSetupSdk from "remoteclaw/plugin-sdk/ollama-setup";
-import * as providerAuthSdk from "remoteclaw/plugin-sdk/provider-auth";
-import * as providerModelsSdk from "remoteclaw/plugin-sdk/provider-models";
 import * as providerSetupSdk from "remoteclaw/plugin-sdk/provider-setup";
-import * as replyHistorySdk from "remoteclaw/plugin-sdk/reply-history";
-import * as replyPayloadSdk from "remoteclaw/plugin-sdk/reply-payload";
-import * as replyRuntimeSdk from "remoteclaw/plugin-sdk/reply-runtime";
-import * as routingSdk from "remoteclaw/plugin-sdk/routing";
-import * as runtimeSdk from "remoteclaw/plugin-sdk/runtime";
 import * as sandboxSdk from "remoteclaw/plugin-sdk/sandbox";
-import * as secretInputSdk from "remoteclaw/plugin-sdk/secret-input";
 import * as selfHostedProviderSetupSdk from "remoteclaw/plugin-sdk/self-hosted-provider-setup";
 import * as setupSdk from "remoteclaw/plugin-sdk/setup";
-import * as ssrfRuntimeSdk from "remoteclaw/plugin-sdk/ssrf-runtime";
-import * as testingSdk from "remoteclaw/plugin-sdk/testing";
-import * as threadBindingsRuntimeSdk from "remoteclaw/plugin-sdk/thread-bindings-runtime";
-import * as webhookIngressSdk from "remoteclaw/plugin-sdk/webhook-ingress";
+import * as signalSdk from "remoteclaw/plugin-sdk/signal";
+import * as slackSdk from "remoteclaw/plugin-sdk/slack";
+import * as telegramSdk from "remoteclaw/plugin-sdk/telegram";
+import * as whatsappSdk from "remoteclaw/plugin-sdk/whatsapp";
 import { describe, expect, expectTypeOf, it } from "vitest";
 import type { ChannelMessageActionContext } from "../channels/plugins/types.js";
 import type {
@@ -553,6 +545,14 @@ describe("plugin-sdk subpath exports", () => {
     expect(typeof setupSdk.resolveGroupAllowlistWithLookupNotes).toBe("function");
     expect(typeof setupSdk.setAccountAllowFromForChannel).toBe("function");
     expect(typeof setupSdk.setAccountDmAllowFromForChannel).toBe("function");
+    expect(typeof setupSdk.setTopLevelChannelDmPolicyWithAllowFrom).toBe("function");
+    expect(typeof setupSdk.formatResolvedUnresolvedNote).toBe("function");
+  });
+
+  it("exports shared setup helpers from the dedicated subpath", () => {
+    expect(typeof setupSdk.DEFAULT_ACCOUNT_ID).toBe("string");
+    expect(typeof setupSdk.formatDocsLink).toBe("function");
+    expect(typeof setupSdk.mergeAllowFromEntries).toBe("function");
     expect(typeof setupSdk.setTopLevelChannelDmPolicyWithAllowFrom).toBe("function");
     expect(typeof setupSdk.formatResolvedUnresolvedNote).toBe("function");
   });
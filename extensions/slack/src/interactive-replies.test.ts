wonglaitung/openclaw_mini

refactor(slack): move Slack channel code to extensions/slack/src/ (#45621)
Move all Slack channel implementation files from src/slack/ to
extensions/slack/src/ and replace originals with shim re-exports.
This follows the extension migration pattern for channel plugins.

- Copy all .ts files to extensions/slack/src/ (preserving directory
  structure: monitor/, http/, monitor/events/, monitor/message-handler/)
- Transform import paths: external src/ imports use relative paths
  back to src/, internal slack imports stay relative within extension
- Replace all src/slack/ files with shim re-exports pointing to
  the extension copies
- Update tsconfig.plugin-sdk.dts.json rootDir from "src" to "." so
  the DTS build can follow shim chains into extensions/
- Update write-plugin-sdk-entry-dts.ts re-export path accordingly
- Preserve extensions/slack/index.ts, package.json, openclaw.plugin.json,
  src/channel.ts, src/runtime.ts, src/channel.test.ts (untouched)

extensions/slack/src/interactive-replies.test.ts
@@ -1,0 +1,38 @@
+import { describe, expect, it } from "vitest";
+import type { OpenClawConfig } from "../../../src/config/config.js";
+import { isSlackInteractiveRepliesEnabled } from "./interactive-replies.js";
+
+describe("isSlackInteractiveRepliesEnabled", () => {
+  it("fails closed when accountId is unknown and multiple accounts exist", () => {
+    const cfg = {
+      channels: {
+        slack: {
+          accounts: {
+            one: {
+              capabilities: { interactiveReplies: true },
+            },
+            two: {},
+          },
+        },
+      },
+    } as OpenClawConfig;
+
+    expect(isSlackInteractiveRepliesEnabled({ cfg, accountId: undefined })).toBe(false);
+  });
+
+  it("uses the only configured account when accountId is unknown", () => {
+    const cfg = {
+      channels: {
+        slack: {
+          accounts: {
+            only: {
+              capabilities: { interactiveReplies: true },
+            },
+          },
+        },
+      },
+    } as OpenClawConfig;
+
+    expect(isSlackInteractiveRepliesEnabled({ cfg, accountId: undefined })).toBe(true);
+  });
+});

extensions/slack/src/monitor/context.test.ts
@@ -1,0 +1,83 @@
+import type { App } from "@slack/bolt";
+import { describe, expect, it } from "vitest";
+import type { OpenClawConfig } from "../../../../src/config/config.js";
+import type { RuntimeEnv } from "../../../../src/runtime.js";
+import { createSlackMonitorContext } from "./context.js";
+
+function createTestContext() {
+  return createSlackMonitorContext({
+    cfg: {
+      channels: { slack: { enabled: true } },
+      session: { dmScope: "main" },
+    } as OpenClawConfig,
+    accountId: "default",
+    botToken: "xoxb-test",
+    app: { client: {} } as App,
+    runtime: {} as RuntimeEnv,
+    botUserId: "U_BOT",
+    teamId: "T_EXPECTED",
+    apiAppId: "A_EXPECTED",
+    historyLimit: 0,
+    sessionScope: "per-sender",
+    mainKey: "main",
+    dmEnabled: true,
+    dmPolicy: "open",
+    allowFrom: [],
+    allowNameMatching: false,
+    groupDmEnabled: false,
+    groupDmChannels: [],
+    defaultRequireMention: true,
+    groupPolicy: "allowlist",
+    useAccessGroups: true,
+    reactionMode: "off",
+    reactionAllowlist: [],
+    replyToMode: "off",
+    threadHistoryScope: "thread",
+    threadInheritParent: false,
+    slashCommand: {
+      enabled: true,
+      name: "openclaw",
+      ephemeral: true,
+      sessionPrefix: "slack:slash",
+    },
+    textLimit: 4000,
+    typingReaction: "",
+    ackReactionScope: "group-mentions",
+    mediaMaxBytes: 20 * 1024 * 1024,
+    removeAckAfterReply: false,
+  });
+}
+
+describe("createSlackMonitorContext shouldDropMismatchedSlackEvent", () => {
+  it("drops mismatched top-level app/team identifiers", () => {
+    const ctx = createTestContext();
+    expect(
+      ctx.shouldDropMismatchedSlackEvent({
+        api_app_id: "A_WRONG",
+        team_id: "T_EXPECTED",
+      }),
+    ).toBe(true);
+    expect(
+      ctx.shouldDropMismatchedSlackEvent({
+        api_app_id: "A_EXPECTED",
+        team_id: "T_WRONG",
+      }),
+    ).toBe(true);
+  });
+
+  it("drops mismatched nested team.id payloads used by interaction bodies", () => {
+    const ctx = createTestContext();
+    expect(
+      ctx.shouldDropMismatchedSlackEvent({
+        api_app_id: "A_EXPECTED",
+        team: { id: "T_WRONG" },
+      }),
+    ).toBe(true);
+    expect(
+      ctx.shouldDropMismatchedSlackEvent({
+        api_app_id: "A_EXPECTED",
+        team: { id: "T_EXPECTED" },
+      }),
+    ).toBe(false);
+  });
+});

extensions/slack/src/monitor/message-handler/prepare.test-helpers.ts
@@ -1,0 +1,69 @@
+import type { App } from "@slack/bolt";
+import type { OpenClawConfig } from "../../../../../src/config/config.js";
+import type { RuntimeEnv } from "../../../../../src/runtime.js";
+import type { ResolvedSlackAccount } from "../../accounts.js";
+import { createSlackMonitorContext } from "../context.js";
+
+export function createInboundSlackTestContext(params: {
+  cfg: OpenClawConfig;
+  appClient?: App["client"];
+  defaultRequireMention?: boolean;
+  replyToMode?: "off" | "all" | "first";
+  channelsConfig?: Record<string, { systemPrompt: string }>;
+}) {
+  return createSlackMonitorContext({
+    cfg: params.cfg,
+    accountId: "default",
+    botToken: "token",
+    app: { client: params.appClient ?? {} } as App,
+    runtime: {} as RuntimeEnv,
+    botUserId: "B1",
+    teamId: "T1",
+    apiAppId: "A1",
+    historyLimit: 0,
+    sessionScope: "per-sender",
+    mainKey: "main",
+    dmEnabled: true,
+    dmPolicy: "open",
+    allowFrom: [],
+    allowNameMatching: false,
+    groupDmEnabled: true,
+    groupDmChannels: [],
+    defaultRequireMention: params.defaultRequireMention ?? true,
+    channelsConfig: params.channelsConfig,
+    groupPolicy: "open",
+    useAccessGroups: false,
+    reactionMode: "off",
+    reactionAllowlist: [],
+    replyToMode: params.replyToMode ?? "off",
+    threadHistoryScope: "thread",
+    threadInheritParent: false,
+    slashCommand: {
+      enabled: false,
+      name: "openclaw",
+      sessionPrefix: "slack:slash",
+      ephemeral: true,
+    },
+    textLimit: 4000,
+    ackReactionScope: "group-mentions",
+    typingReaction: "",
+    mediaMaxBytes: 1024,
+    removeAckAfterReply: false,
+  });
+}
+
+export function createSlackTestAccount(
+  config: ResolvedSlackAccount["config"] = {},
+): ResolvedSlackAccount {
+  return {
+    accountId: "default",
+    enabled: true,
+    botTokenSource: "config",
+    appTokenSource: "config",
+    userTokenSource: "none",
+    config,
+    replyToMode: config.replyToMode,
+    replyToModeByChatType: config.replyToModeByChatType,
+    dm: config.dm,
+  };
+}

remoteclaw/remoteclaw

(cherry picked from commit 8746362f5ebfe8de4d3633b424595b3b47f58af5)
# Conflicts:
#	src/slack/account-inspect.ts
#	src/slack/interactive-replies.test.ts
#	src/slack/interactive-replies.ts
#	src/slack/monitor/events/channels.test.ts
#	src/slack/monitor/slash-skill-commands.runtime.ts
#	src/slack/probe.test.ts

extensions/slack/src/interactive-replies.test.ts
@@ -1,0 +1,38 @@
+import { describe, expect, it } from "vitest";
+import type { RemoteClawConfig } from "../../../src/config/config.js";
+import { isSlackInteractiveRepliesEnabled } from "./interactive-replies.js";
+
+describe("isSlackInteractiveRepliesEnabled", () => {
+  it("fails closed when accountId is unknown and multiple accounts exist", () => {
+    const cfg = {
+      channels: {
+        slack: {
+          accounts: {
+            one: {
+              capabilities: { interactiveReplies: true },
+            },
+            two: {},
+          },
+        },
+      },
+    } as RemoteClawConfig;
+
+    expect(isSlackInteractiveRepliesEnabled({ cfg, accountId: undefined })).toBe(false);
+  });
+
+  it("uses the only configured account when accountId is unknown", () => {
+    const cfg = {
+      channels: {
+        slack: {
+          accounts: {
+            only: {
+              capabilities: { interactiveReplies: true },
+            },
+          },
+        },
+      },
+    } as RemoteClawConfig;
+
+    expect(isSlackInteractiveRepliesEnabled({ cfg, accountId: undefined })).toBe(true);
+  });
+});

extensions/slack/src/monitor/context.test.ts
@@ -1,0 +1,83 @@
+import type { App } from "@slack/bolt";
+import { describe, expect, it } from "vitest";
+import type { RemoteClawConfig } from "../../../../src/config/config.js";
+import type { RuntimeEnv } from "../../../../src/runtime.js";
+import { createSlackMonitorContext } from "./context.js";
+
+function createTestContext() {
+  return createSlackMonitorContext({
+    cfg: {
+      channels: { slack: { enabled: true } },
+      session: { dmScope: "main" },
+    } as RemoteClawConfig,
+    accountId: "default",
+    botToken: "xoxb-test",
+    app: { client: {} } as App,
+    runtime: {} as RuntimeEnv,
+    botUserId: "U_BOT",
+    teamId: "T_EXPECTED",
+    apiAppId: "A_EXPECTED",
+    historyLimit: 0,
+    sessionScope: "per-sender",
+    mainKey: "main",
+    dmEnabled: true,
+    dmPolicy: "open",
+    allowFrom: [],
+    allowNameMatching: false,
+    groupDmEnabled: false,
+    groupDmChannels: [],
+    defaultRequireMention: true,
+    groupPolicy: "allowlist",
+    useAccessGroups: true,
+    reactionMode: "off",
+    reactionAllowlist: [],
+    replyToMode: "off",
+    threadHistoryScope: "thread",
+    threadInheritParent: false,
+    slashCommand: {
+      enabled: true,
+      name: "openclaw",
+      ephemeral: true,
+      sessionPrefix: "slack:slash",
+    },
+    textLimit: 4000,
+    typingReaction: "",
+    ackReactionScope: "group-mentions",
+    mediaMaxBytes: 20 * 1024 * 1024,
+    removeAckAfterReply: false,
+  });
+}
+
+describe("createSlackMonitorContext shouldDropMismatchedSlackEvent", () => {
+  it("drops mismatched top-level app/team identifiers", () => {
+    const ctx = createTestContext();
+    expect(
+      ctx.shouldDropMismatchedSlackEvent({
+        api_app_id: "A_WRONG",
+        team_id: "T_EXPECTED",
+      }),
+    ).toBe(true);
+    expect(
+      ctx.shouldDropMismatchedSlackEvent({
+        api_app_id: "A_EXPECTED",
+        team_id: "T_WRONG",
+      }),
+    ).toBe(true);
+  });
+
+  it("drops mismatched nested team.id payloads used by interaction bodies", () => {
+    const ctx = createTestContext();
+    expect(
+      ctx.shouldDropMismatchedSlackEvent({
+        api_app_id: "A_EXPECTED",
+        team: { id: "T_WRONG" },
+      }),
+    ).toBe(true);
+    expect(
+      ctx.shouldDropMismatchedSlackEvent({
+        api_app_id: "A_EXPECTED",
+        team: { id: "T_EXPECTED" },
+      }),
+    ).toBe(false);
+  });
+});

extensions/slack/src/monitor/message-handler/prepare.test-helpers.ts
@@ -1,0 +1,69 @@
+import type { App } from "@slack/bolt";
+import type { RemoteClawConfig } from "../../../../../src/config/config.js";
+import type { RuntimeEnv } from "../../../../../src/runtime.js";
+import type { ResolvedSlackAccount } from "../../accounts.js";
+import { createSlackMonitorContext } from "../context.js";
+
+export function createInboundSlackTestContext(params: {
+  cfg: RemoteClawConfig;
+  appClient?: App["client"];
+  defaultRequireMention?: boolean;
+  replyToMode?: "off" | "all" | "first";
+  channelsConfig?: Record<string, { systemPrompt: string }>;
+}) {
+  return createSlackMonitorContext({
+    cfg: params.cfg,
+    accountId: "default",
+    botToken: "token",
+    app: { client: params.appClient ?? {} } as App,
+    runtime: {} as RuntimeEnv,
+    botUserId: "B1",
+    teamId: "T1",
+    apiAppId: "A1",
+    historyLimit: 0,
+    sessionScope: "per-sender",
+    mainKey: "main",
+    dmEnabled: true,
+    dmPolicy: "open",
+    allowFrom: [],
+    allowNameMatching: false,
+    groupDmEnabled: true,
+    groupDmChannels: [],
+    defaultRequireMention: params.defaultRequireMention ?? true,
+    channelsConfig: params.channelsConfig,
+    groupPolicy: "open",
+    useAccessGroups: false,
+    reactionMode: "off",
+    reactionAllowlist: [],
+    replyToMode: params.replyToMode ?? "off",
+    threadHistoryScope: "thread",
+    threadInheritParent: false,
+    slashCommand: {
+      enabled: false,
+      name: "openclaw",
+      sessionPrefix: "slack:slash",
+      ephemeral: true,
+    },
+    textLimit: 4000,
+    ackReactionScope: "group-mentions",
+    typingReaction: "",
+    mediaMaxBytes: 1024,
+    removeAckAfterReply: false,
+  });
+}
+
+export function createSlackTestAccount(
+  config: ResolvedSlackAccount["config"] = {},
+): ResolvedSlackAccount {
+  return {
+    accountId: "default",
+    enabled: true,
+    botTokenSource: "config",
+    appTokenSource: "config",
+    userTokenSource: "none",
+    config,
+    replyToMode: config.replyToMode,
+    replyToModeByChatType: config.replyToModeByChatType,
+    dm: config.dm,
+  };
+}
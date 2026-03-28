wonglaitung/openclaw_mini

refactor: deduplicate setup wizard helpers

extensions/signal/src/shared.ts
@@ -1,16 +1,14 @@
-import { createScopedAccountConfigAccessors } from "openclaw/plugin-sdk/channel-config-helpers";
 import {
-  buildAccountScopedDmSecurityPolicy,
   collectAllowlistProviderRestrictSendersWarnings,
-} from "openclaw/plugin-sdk/channel-policy";
+  createScopedAccountConfigAccessors,
+  createScopedChannelConfigBase,
+  createScopedDmSecurityResolver,
+} from "openclaw/plugin-sdk/channel-config-helpers";
 import { createChannelPluginBase } from "openclaw/plugin-sdk/core";
 import {
   buildChannelConfigSchema,
-  DEFAULT_ACCOUNT_ID,
-  deleteAccountFromConfigSection,
   getChatChannelMeta,
   normalizeE164,
-  setAccountEnabledInConfigSection,
   SignalConfigSchema,
   type ChannelPlugin,
 } from "openclaw/plugin-sdk/signal-core";
@@ -28,9 +26,9 @@ async function loadSignalChannelRuntime() {
   return await import("./channel.runtime.js");
 }
 
-export const signalSetupWizard = createSignalSetupWizardProxy(async () => ({
-  signalSetupWizard: (await loadSignalChannelRuntime()).signalSetupWizard,
-}));
+export const signalSetupWizard = createSignalSetupWizardProxy(
+  async () => (await loadSignalChannelRuntime()).signalSetupWizard,
+);
 
 export const signalConfigAccessors = createScopedAccountConfigAccessors({
   resolveAccount: ({ cfg, accountId }) => resolveSignalAccount({ cfg, accountId }),
@@ -43,6 +41,38 @@ export const signalConfigAccessors = createScopedAccountConfigAccessors({
       .filter(Boolean),
   resolveDefaultTo: (account: ResolvedSignalAccount) => account.config.defaultTo,
 });
+
+export const signalConfigBase = createScopedChannelConfigBase<ResolvedSignalAccount>({
+  sectionKey: SIGNAL_CHANNEL,
+  listAccountIds: listSignalAccountIds,
+  resolveAccount: (cfg, accountId) => resolveSignalAccount({ cfg, accountId }),
+  defaultAccountId: resolveDefaultSignalAccountId,
+  clearBaseFields: ["account", "httpUrl", "httpHost", "httpPort", "cliPath", "name"],
+});
+
+export const signalResolveDmPolicy = createScopedDmSecurityResolver<ResolvedSignalAccount>({
+  channelKey: SIGNAL_CHANNEL,
+  resolvePolicy: (account) => account.config.dmPolicy,
+  resolveAllowFrom: (account) => account.config.allowFrom,
+  policyPathSuffix: "dmPolicy",
+  normalizeEntry: (raw) => normalizeE164(raw.replace(/^signal:/i, "").trim()),
+});
+
+export function collectSignalSecurityWarnings(params: {
+  account: ResolvedSignalAccount;
+  cfg: Parameters<typeof resolveSignalAccount>[0]["cfg"];
+}) {
+  return collectAllowlistProviderRestrictSendersWarnings({
+    cfg: params.cfg,
+    providerConfigPresent: params.cfg.channels?.signal !== undefined,
+    configuredGroupPolicy: params.account.config.groupPolicy,
+    surface: "Signal groups",
+    openScope: "any member",
+    groupPolicyPath: "channels.signal.groupPolicy",
+    groupAllowFromPath: "channels.signal.groupAllowFrom",
+    mentionGated: false,
+  });
+}
 
 export function createSignalPluginBase(params: {
   setupWizard?: NonNullable<ChannelPlugin<ResolvedSignalAccount>["setupWizard"]>;
@@ -77,24 +107,7 @@ export function createSignalPluginBase(params: {
     reload: { configPrefixes: ["channels.signal"] },
     configSchema: buildChannelConfigSchema(SignalConfigSchema),
     config: {
-      listAccountIds: (cfg) => listSignalAccountIds(cfg),
-      resolveAccount: (cfg, accountId) => resolveSignalAccount({ cfg, accountId }),
-      defaultAccountId: (cfg) => resolveDefaultSignalAccountId(cfg),
-      setAccountEnabled: ({ cfg, accountId, enabled }) =>
-        setAccountEnabledInConfigSection({
-          cfg,
-          sectionKey: SIGNAL_CHANNEL,
-          accountId,
-          enabled,
-          allowTopLevel: true,
-        }),
-      deleteAccount: ({ cfg, accountId }) =>
-        deleteAccountFromConfigSection({
-          cfg,
-          sectionKey: SIGNAL_CHANNEL,
-          accountId,
-          clearBaseFields: ["account", "httpUrl", "httpHost", "httpPort", "cliPath", "name"],
-        }),
+      ...signalConfigBase,
       isConfigured: (account) => account.configured,
       describeAccount: (account) => ({
         accountId: account.accountId,
@@ -106,28 +119,8 @@ export function createSignalPluginBase(params: {
       ...signalConfigAccessors,
     },
     security: {
-      resolveDmPolicy: ({ cfg, accountId, account }) =>
-        buildAccountScopedDmSecurityPolicy({
-          cfg,
-          channelKey: SIGNAL_CHANNEL,
-          accountId,
-          fallbackAccountId: account.accountId ?? DEFAULT_ACCOUNT_ID,
-          policy: account.config.dmPolicy,
-          allowFrom: account.config.allowFrom ?? [],
-          policyPathSuffix: "dmPolicy",
-          normalizeEntry: (raw) => normalizeE164(raw.replace(/^signal:/i, "").trim()),
-        }),
-      collectWarnings: ({ account, cfg }) =>
-        collectAllowlistProviderRestrictSendersWarnings({
-          cfg,
-          providerConfigPresent: cfg.channels?.signal !== undefined,
-          configuredGroupPolicy: account.config.groupPolicy,
-          surface: "Signal groups",
-          openScope: "any member",
-          groupPolicyPath: "channels.signal.groupPolicy",
-          groupAllowFromPath: "channels.signal.groupAllowFrom",
-          mentionGated: false,
-        }),
+      resolveDmPolicy: signalResolveDmPolicy,
+      collectWarnings: collectSignalSecurityWarnings,
     },
     setup: params.setup,
   }) as Pick<

src/channels/plugins/setup-wizard-binary.ts
@@ -1,0 +1,100 @@
+import type { OpenClawConfig } from "../../config/config.js";
+import { detectBinary as defaultDetectBinary } from "../../plugins/setup-binary.js";
+import type {
+  ChannelSetupWizard,
+  ChannelSetupWizardStatus,
+  ChannelSetupWizardTextInput,
+} from "./setup-wizard.js";
+
+type SetupTextInputParams = Parameters<NonNullable<ChannelSetupWizardTextInput["currentValue"]>>[0];
+type SetupStatusParams = Parameters<NonNullable<ChannelSetupWizardStatus["resolveStatusLines"]>>[0];
+
+export function createDetectedBinaryStatus(params: {
+  channelLabel: string;
+  binaryLabel: string;
+  configuredLabel: string;
+  unconfiguredLabel: string;
+  configuredHint: string;
+  unconfiguredHint: string;
+  configuredScore: number;
+  unconfiguredScore: number;
+  resolveConfigured: (params: { cfg: OpenClawConfig }) => boolean | Promise<boolean>;
+  resolveBinaryPath: (params: { cfg: OpenClawConfig }) => string;
+  detectBinary?: (path: string) => Promise<boolean>;
+}): ChannelSetupWizardStatus {
+  const detectBinary = params.detectBinary ?? defaultDetectBinary;
+  return {
+    configuredLabel: params.configuredLabel,
+    unconfiguredLabel: params.unconfiguredLabel,
+    configuredHint: params.configuredHint,
+    unconfiguredHint: params.unconfiguredHint,
+    configuredScore: params.configuredScore,
+    unconfiguredScore: params.unconfiguredScore,
+    resolveConfigured: params.resolveConfigured,
+    resolveStatusLines: async ({ cfg, configured }: SetupStatusParams) => {
+      const binaryPath = params.resolveBinaryPath({ cfg });
+      const detected = await detectBinary(binaryPath);
+      return [
+        `${params.channelLabel}: ${configured ? params.configuredLabel : params.unconfiguredLabel}`,
+        `${params.binaryLabel}: ${detected ? "found" : "missing"} (${binaryPath})`,
+      ];
+    },
+    resolveSelectionHint: async ({ cfg }) =>
+      (await detectBinary(params.resolveBinaryPath({ cfg })))
+        ? params.configuredHint
+        : params.unconfiguredHint,
+    resolveQuickstartScore: async ({ cfg }) =>
+      (await detectBinary(params.resolveBinaryPath({ cfg })))
+        ? params.configuredScore
+        : params.unconfiguredScore,
+  };
+}
+
+export function createCliPathTextInput(params: {
+  inputKey: ChannelSetupWizardTextInput["inputKey"];
+  message: string;
+  resolvePath: (params: SetupTextInputParams) => string | undefined;
+  shouldPrompt: NonNullable<ChannelSetupWizardTextInput["shouldPrompt"]>;
+  helpTitle?: string;
+  helpLines?: string[];
+}): ChannelSetupWizardTextInput {
+  return {
+    inputKey: params.inputKey,
+    message: params.message,
+    currentValue: params.resolvePath,
+    initialValue: params.resolvePath,
+    shouldPrompt: params.shouldPrompt,
+    confirmCurrentValue: false,
+    applyCurrentValue: true,
+    ...(params.helpTitle ? { helpTitle: params.helpTitle } : {}),
+    ...(params.helpLines ? { helpLines: params.helpLines } : {}),
+  };
+}
+
+export function createDelegatedSetupWizardStatusResolvers(
+  loadWizard: () => Promise<ChannelSetupWizard>,
+): Pick<
+  ChannelSetupWizardStatus,
+  "resolveStatusLines" | "resolveSelectionHint" | "resolveQuickstartScore"
+> {
+  return {
+    resolveStatusLines: async (params) =>
+      (await loadWizard()).status.resolveStatusLines?.(params) ?? [],
+    resolveSelectionHint: async (params) =>
+      await (await loadWizard()).status.resolveSelectionHint?.(params),
+    resolveQuickstartScore: async (params) =>
+      await (await loadWizard()).status.resolveQuickstartScore?.(params),
+  };
+}
+
+export function createDelegatedTextInputShouldPrompt(params: {
+  loadWizard: () => Promise<ChannelSetupWizard>;
+  inputKey: ChannelSetupWizardTextInput["inputKey"];
+}): NonNullable<ChannelSetupWizardTextInput["shouldPrompt"]> {
+  return async (inputParams) => {
+    const input = (await params.loadWizard()).textInputs?.find(
+      (entry) => entry.inputKey === params.inputKey,
+    );
+    return (await input?.shouldPrompt?.(inputParams)) ?? false;
+  };
+}

remoteclaw/remoteclaw

(cherry picked from commit 1a9114a169ea52a15458ba6c73bbcbe93f457b8e)
# Conflicts:
#	extensions/bluebubbles/src/setup-surface.ts
#	extensions/synology-chat/src/setup-surface.ts

extensions/signal/src/shared.ts
@@ -1,18 +1,17 @@
-import { createScopedAccountConfigAccessors } from "remoteclaw/plugin-sdk/channel-config-helpers";
 import {
-  buildAccountScopedDmSecurityPolicy,
   collectAllowlistProviderRestrictSendersWarnings,
-} from "remoteclaw/plugin-sdk/channel-policy";
+  createScopedAccountConfigAccessors,
+  createScopedChannelConfigBase,
+  createScopedDmSecurityResolver,
+} from "remoteclaw/plugin-sdk/channel-config-helpers";
+import { createChannelPluginBase } from "remoteclaw/plugin-sdk/core";
 import {
-  deleteAccountFromConfigSection,
-  setAccountEnabledInConfigSection,
-} from "../../../src/channels/plugins/config-helpers.js";
-import { buildChannelConfigSchema } from "../../../src/channels/plugins/config-schema.js";
-import type { ChannelPlugin } from "../../../src/channels/plugins/types.plugin.js";
-import { getChatChannelMeta } from "../../../src/channels/registry.js";
-import { SignalConfigSchema } from "../../../src/config/zod-schema.providers-core.js";
-import { DEFAULT_ACCOUNT_ID } from "../../../src/routing/session-key.js";
-import { normalizeE164 } from "../../../src/utils.js";
+  buildChannelConfigSchema,
+  getChatChannelMeta,
+  normalizeE164,
+  SignalConfigSchema,
+  type ChannelPlugin,
+} from "remoteclaw/plugin-sdk/signal-core";
 import {
   listSignalAccountIds,
   resolveDefaultSignalAccountId,
@@ -27,9 +26,9 @@ async function loadSignalChannelRuntime() {
   return await import("./channel.runtime.js");
 }
 
-export const signalSetupWizard = createSignalSetupWizardProxy(async () => ({
-  signalSetupWizard: (await loadSignalChannelRuntime()).signalSetupWizard,
-}));
+export const signalSetupWizard = createSignalSetupWizardProxy(
+  async () => (await loadSignalChannelRuntime()).signalSetupWizard,
+);
 
 export const signalConfigAccessors = createScopedAccountConfigAccessors({
   resolveAccount: ({ cfg, accountId }) => resolveSignalAccount({ cfg, accountId }),
@@ -42,6 +41,38 @@ export const signalConfigAccessors = createScopedAccountConfigAccessors({
       .filter(Boolean),
   resolveDefaultTo: (account: ResolvedSignalAccount) => account.config.defaultTo,
 });
+
+export const signalConfigBase = createScopedChannelConfigBase<ResolvedSignalAccount>({
+  sectionKey: SIGNAL_CHANNEL,
+  listAccountIds: listSignalAccountIds,
+  resolveAccount: (cfg, accountId) => resolveSignalAccount({ cfg, accountId }),
+  defaultAccountId: resolveDefaultSignalAccountId,
+  clearBaseFields: ["account", "httpUrl", "httpHost", "httpPort", "cliPath", "name"],
+});
+
+export const signalResolveDmPolicy = createScopedDmSecurityResolver<ResolvedSignalAccount>({
+  channelKey: SIGNAL_CHANNEL,
+  resolvePolicy: (account) => account.config.dmPolicy,
+  resolveAllowFrom: (account) => account.config.allowFrom,
+  policyPathSuffix: "dmPolicy",
+  normalizeEntry: (raw) => normalizeE164(raw.replace(/^signal:/i, "").trim()),
+});
+
+export function collectSignalSecurityWarnings(params: {
+  account: ResolvedSignalAccount;
+  cfg: Parameters<typeof resolveSignalAccount>[0]["cfg"];
+}) {
+  return collectAllowlistProviderRestrictSendersWarnings({
+    cfg: params.cfg,
+    providerConfigPresent: params.cfg.channels?.signal !== undefined,
+    configuredGroupPolicy: params.account.config.groupPolicy,
+    surface: "Signal groups",
+    openScope: "any member",
+    groupPolicyPath: "channels.signal.groupPolicy",
+    groupAllowFromPath: "channels.signal.groupAllowFrom",
+    mentionGated: false,
+  });
+}
 
 export function createSignalPluginBase(params: {
   setupWizard?: NonNullable<ChannelPlugin<ResolvedSignalAccount>["setupWizard"]>;
@@ -76,24 +107,7 @@ export function createSignalPluginBase(params: {
     reload: { configPrefixes: ["channels.signal"] },
     configSchema: buildChannelConfigSchema(SignalConfigSchema),
     config: {
-      listAccountIds: (cfg) => listSignalAccountIds(cfg),
-      resolveAccount: (cfg, accountId) => resolveSignalAccount({ cfg, accountId }),
-      defaultAccountId: (cfg) => resolveDefaultSignalAccountId(cfg),
-      setAccountEnabled: ({ cfg, accountId, enabled }) =>
-        setAccountEnabledInConfigSection({
-          cfg,
-          sectionKey: SIGNAL_CHANNEL,
-          accountId,
-          enabled,
-          allowTopLevel: true,
-        }),
-      deleteAccount: ({ cfg, accountId }) =>
-        deleteAccountFromConfigSection({
-          cfg,
-          sectionKey: SIGNAL_CHANNEL,
-          accountId,
-          clearBaseFields: ["account", "httpUrl", "httpHost", "httpPort", "cliPath", "name"],
-        }),
+      ...signalConfigBase,
       isConfigured: (account) => account.configured,
       describeAccount: (account) => ({
         accountId: account.accountId,
@@ -105,28 +119,8 @@ export function createSignalPluginBase(params: {
       ...signalConfigAccessors,
     },
     security: {
-      resolveDmPolicy: ({ cfg, accountId, account }) =>
-        buildAccountScopedDmSecurityPolicy({
-          cfg,
-          channelKey: SIGNAL_CHANNEL,
-          accountId,
-          fallbackAccountId: account.accountId ?? DEFAULT_ACCOUNT_ID,
-          policy: account.config.dmPolicy,
-          allowFrom: account.config.allowFrom ?? [],
-          policyPathSuffix: "dmPolicy",
-          normalizeEntry: (raw) => normalizeE164(raw.replace(/^signal:/i, "").trim()),
-        }),
-      collectWarnings: ({ account, cfg }) =>
-        collectAllowlistProviderRestrictSendersWarnings({
-          cfg,
-          providerConfigPresent: cfg.channels?.signal !== undefined,
-          configuredGroupPolicy: account.config.groupPolicy,
-          surface: "Signal groups",
-          openScope: "any member",
-          groupPolicyPath: "channels.signal.groupPolicy",
-          groupAllowFromPath: "channels.signal.groupAllowFrom",
-          mentionGated: false,
-        }),
+      resolveDmPolicy: signalResolveDmPolicy,
+      collectWarnings: collectSignalSecurityWarnings,
     },
     setup: params.setup,
   };

src/channels/plugins/setup-wizard-binary.ts
@@ -1,0 +1,100 @@
+import type { RemoteClawConfig } from "../../config/config.js";
+import { detectBinary as defaultDetectBinary } from "../../plugins/setup-binary.js";
+import type {
+  ChannelSetupWizard,
+  ChannelSetupWizardStatus,
+  ChannelSetupWizardTextInput,
+} from "./setup-wizard.js";
+
+type SetupTextInputParams = Parameters<NonNullable<ChannelSetupWizardTextInput["currentValue"]>>[0];
+type SetupStatusParams = Parameters<NonNullable<ChannelSetupWizardStatus["resolveStatusLines"]>>[0];
+
+export function createDetectedBinaryStatus(params: {
+  channelLabel: string;
+  binaryLabel: string;
+  configuredLabel: string;
+  unconfiguredLabel: string;
+  configuredHint: string;
+  unconfiguredHint: string;
+  configuredScore: number;
+  unconfiguredScore: number;
+  resolveConfigured: (params: { cfg: RemoteClawConfig }) => boolean | Promise<boolean>;
+  resolveBinaryPath: (params: { cfg: RemoteClawConfig }) => string;
+  detectBinary?: (path: string) => Promise<boolean>;
+}): ChannelSetupWizardStatus {
+  const detectBinary = params.detectBinary ?? defaultDetectBinary;
+  return {
+    configuredLabel: params.configuredLabel,
+    unconfiguredLabel: params.unconfiguredLabel,
+    configuredHint: params.configuredHint,
+    unconfiguredHint: params.unconfiguredHint,
+    configuredScore: params.configuredScore,
+    unconfiguredScore: params.unconfiguredScore,
+    resolveConfigured: params.resolveConfigured,
+    resolveStatusLines: async ({ cfg, configured }: SetupStatusParams) => {
+      const binaryPath = params.resolveBinaryPath({ cfg });
+      const detected = await detectBinary(binaryPath);
+      return [
+        `${params.channelLabel}: ${configured ? params.configuredLabel : params.unconfiguredLabel}`,
+        `${params.binaryLabel}: ${detected ? "found" : "missing"} (${binaryPath})`,
+      ];
+    },
+    resolveSelectionHint: async ({ cfg }) =>
+      (await detectBinary(params.resolveBinaryPath({ cfg })))
+        ? params.configuredHint
+        : params.unconfiguredHint,
+    resolveQuickstartScore: async ({ cfg }) =>
+      (await detectBinary(params.resolveBinaryPath({ cfg })))
+        ? params.configuredScore
+        : params.unconfiguredScore,
+  };
+}
+
+export function createCliPathTextInput(params: {
+  inputKey: ChannelSetupWizardTextInput["inputKey"];
+  message: string;
+  resolvePath: (params: SetupTextInputParams) => string | undefined;
+  shouldPrompt: NonNullable<ChannelSetupWizardTextInput["shouldPrompt"]>;
+  helpTitle?: string;
+  helpLines?: string[];
+}): ChannelSetupWizardTextInput {
+  return {
+    inputKey: params.inputKey,
+    message: params.message,
+    currentValue: params.resolvePath,
+    initialValue: params.resolvePath,
+    shouldPrompt: params.shouldPrompt,
+    confirmCurrentValue: false,
+    applyCurrentValue: true,
+    ...(params.helpTitle ? { helpTitle: params.helpTitle } : {}),
+    ...(params.helpLines ? { helpLines: params.helpLines } : {}),
+  };
+}
+
+export function createDelegatedSetupWizardStatusResolvers(
+  loadWizard: () => Promise<ChannelSetupWizard>,
+): Pick<
+  ChannelSetupWizardStatus,
+  "resolveStatusLines" | "resolveSelectionHint" | "resolveQuickstartScore"
+> {
+  return {
+    resolveStatusLines: async (params) =>
+      (await loadWizard()).status.resolveStatusLines?.(params) ?? [],
+    resolveSelectionHint: async (params) =>
+      await (await loadWizard()).status.resolveSelectionHint?.(params),
+    resolveQuickstartScore: async (params) =>
+      await (await loadWizard()).status.resolveQuickstartScore?.(params),
+  };
+}
+
+export function createDelegatedTextInputShouldPrompt(params: {
+  loadWizard: () => Promise<ChannelSetupWizard>;
+  inputKey: ChannelSetupWizardTextInput["inputKey"];
+}): NonNullable<ChannelSetupWizardTextInput["shouldPrompt"]> {
+  return async (inputParams) => {
+    const input = (await params.loadWizard()).textInputs?.find(
+      (entry) => entry.inputKey === params.inputKey,
+    );
+    return (await input?.shouldPrompt?.(inputParams)) ?? false;
+  };
+}
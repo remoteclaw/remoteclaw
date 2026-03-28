wonglaitung/openclaw_mini

refactor: move Discord channel implementation to extensions/ (#45660)
* refactor: move Discord channel implementation to extensions/discord/src/

Move all Discord source files from src/discord/ to extensions/discord/src/,
following the extension migration pattern. Source files in src/discord/ are
replaced with re-export shims. Channel-plugin files from
src/channels/plugins/*/discord* are similarly moved and shimmed.

- Copy all .ts source files preserving subdirectory structure (monitor/, voice/)
- Move channel-plugin files (actions, normalize, onboarding, outbound, status-issues)
- Fix all relative imports to use correct paths from new location
- Create re-export shims at original locations for backward compatibility
- Delete test files from shim locations (tests live in extension now)
- Update tsconfig.plugin-sdk.dts.json rootDir from "src" to "." to accommodate
  extension files outside src/
- Update write-plugin-sdk-entry-dts.ts to match new declaration output paths

* fix: add importOriginal to thread-bindings session-meta mock for extensions test

* style: fix formatting in thread-bindings lifecycle test

extensions/discord/src/accounts.ts
@@ -1,0 +1,89 @@
+import { createAccountActionGate } from "../../../src/channels/plugins/account-action-gate.js";
+import { createAccountListHelpers } from "../../../src/channels/plugins/account-helpers.js";
+import type { OpenClawConfig } from "../../../src/config/config.js";
+import type { DiscordAccountConfig, DiscordActionConfig } from "../../../src/config/types.js";
+import { resolveAccountEntry } from "../../../src/routing/account-lookup.js";
+import { normalizeAccountId } from "../../../src/routing/session-key.js";
+import { resolveDiscordToken } from "./token.js";
+
+export type ResolvedDiscordAccount = {
+  accountId: string;
+  enabled: boolean;
+  name?: string;
+  token: string;
+  tokenSource: "env" | "config" | "none";
+  config: DiscordAccountConfig;
+};
+
+const { listAccountIds, resolveDefaultAccountId } = createAccountListHelpers("discord");
+export const listDiscordAccountIds = listAccountIds;
+export const resolveDefaultDiscordAccountId = resolveDefaultAccountId;
+
+export function resolveDiscordAccountConfig(
+  cfg: OpenClawConfig,
+  accountId: string,
+): DiscordAccountConfig | undefined {
+  return resolveAccountEntry(cfg.channels?.discord?.accounts, accountId);
+}
+
+export function mergeDiscordAccountConfig(
+  cfg: OpenClawConfig,
+  accountId: string,
+): DiscordAccountConfig {
+  const { accounts: _ignored, ...base } = (cfg.channels?.discord ?? {}) as DiscordAccountConfig & {
+    accounts?: unknown;
+  };
+  const account = resolveDiscordAccountConfig(cfg, accountId) ?? {};
+  return { ...base, ...account };
+}
+
+export function createDiscordActionGate(params: {
+  cfg: OpenClawConfig;
+  accountId?: string | null;
+}): (key: keyof DiscordActionConfig, defaultValue?: boolean) => boolean {
+  const accountId = normalizeAccountId(params.accountId);
+  return createAccountActionGate({
+    baseActions: params.cfg.channels?.discord?.actions,
+    accountActions: resolveDiscordAccountConfig(params.cfg, accountId)?.actions,
+  });
+}
+
+export function resolveDiscordAccount(params: {
+  cfg: OpenClawConfig;
+  accountId?: string | null;
+}): ResolvedDiscordAccount {
+  const accountId = normalizeAccountId(params.accountId);
+  const baseEnabled = params.cfg.channels?.discord?.enabled !== false;
+  const merged = mergeDiscordAccountConfig(params.cfg, accountId);
+  const accountEnabled = merged.enabled !== false;
+  const enabled = baseEnabled && accountEnabled;
+  const tokenResolution = resolveDiscordToken(params.cfg, { accountId });
+  return {
+    accountId,
+    enabled,
+    name: merged.name?.trim() || undefined,
+    token: tokenResolution.token,
+    tokenSource: tokenResolution.source,
+    config: merged,
+  };
+}
+
+export function resolveDiscordMaxLinesPerMessage(params: {
+  cfg: OpenClawConfig;
+  discordConfig?: DiscordAccountConfig | null;
+  accountId?: string | null;
+}): number | undefined {
+  if (typeof params.discordConfig?.maxLinesPerMessage === "number") {
+    return params.discordConfig.maxLinesPerMessage;
+  }
+  return resolveDiscordAccount({
+    cfg: params.cfg,
+    accountId: params.accountId,
+  }).config.maxLinesPerMessage;
+}
+
+export function listEnabledDiscordAccounts(cfg: OpenClawConfig): ResolvedDiscordAccount[] {
+  return listDiscordAccountIds(cfg)
+    .map((accountId) => resolveDiscordAccount({ cfg, accountId }))
+    .filter((account) => account.enabled);
+}

extensions/discord/src/monitor/thread-bindings.config.ts
@@ -1,0 +1,39 @@
+import {
+  resolveThreadBindingIdleTimeoutMs,
+  resolveThreadBindingMaxAgeMs,
+  resolveThreadBindingsEnabled,
+} from "../../../../src/channels/thread-bindings-policy.js";
+import type { OpenClawConfig } from "../../../../src/config/config.js";
+import { normalizeAccountId } from "../../../../src/routing/session-key.js";
+
+export {
+  resolveThreadBindingIdleTimeoutMs,
+  resolveThreadBindingMaxAgeMs,
+  resolveThreadBindingsEnabled,
+};
+
+export function resolveDiscordThreadBindingIdleTimeoutMs(params: {
+  cfg: OpenClawConfig;
+  accountId?: string;
+}): number {
+  const accountId = normalizeAccountId(params.accountId);
+  const root = params.cfg.channels?.discord?.threadBindings;
+  const account = params.cfg.channels?.discord?.accounts?.[accountId]?.threadBindings;
+  return resolveThreadBindingIdleTimeoutMs({
+    channelIdleHoursRaw: account?.idleHours ?? root?.idleHours,
+    sessionIdleHoursRaw: params.cfg.session?.threadBindings?.idleHours,
+  });
+}
+
+export function resolveDiscordThreadBindingMaxAgeMs(params: {
+  cfg: OpenClawConfig;
+  accountId?: string;
+}): number {
+  const accountId = normalizeAccountId(params.accountId);
+  const root = params.cfg.channels?.discord?.threadBindings;
+  const account = params.cfg.channels?.discord?.accounts?.[accountId]?.threadBindings;
+  return resolveThreadBindingMaxAgeMs({
+    channelMaxAgeHoursRaw: account?.maxAgeHours ?? root?.maxAgeHours,
+    sessionMaxAgeHoursRaw: params.cfg.session?.threadBindings?.maxAgeHours,
+  });
+}

extensions/discord/src/monitor/thread-bindings.discord-api.test.ts
@@ -1,0 +1,161 @@
+import { ChannelType } from "discord-api-types/v10";
+import { beforeEach, describe, expect, it, vi } from "vitest";
+import type { OpenClawConfig } from "../../../../src/config/config.js";
+import type { ThreadBindingRecord } from "./thread-bindings.types.js";
+
+const hoisted = vi.hoisted(() => {
+  const restGet = vi.fn();
+  const sendMessageDiscord = vi.fn();
+  const sendWebhookMessageDiscord = vi.fn();
+  const createDiscordRestClient = vi.fn(() => ({
+    rest: {
+      get: restGet,
+    },
+  }));
+  return {
+    restGet,
+    sendMessageDiscord,
+    sendWebhookMessageDiscord,
+    createDiscordRestClient,
+  };
+});
+
+vi.mock("../client.js", () => ({
+  createDiscordRestClient: hoisted.createDiscordRestClient,
+}));
+
+vi.mock("../send.js", () => ({
+  sendMessageDiscord: (...args: unknown[]) => hoisted.sendMessageDiscord(...args),
+  sendWebhookMessageDiscord: (...args: unknown[]) => hoisted.sendWebhookMessageDiscord(...args),
+}));
+
+const { maybeSendBindingMessage, resolveChannelIdForBinding } =
+  await import("./thread-bindings.discord-api.js");
+
+describe("resolveChannelIdForBinding", () => {
+  beforeEach(() => {
+    hoisted.restGet.mockClear();
+    hoisted.createDiscordRestClient.mockClear();
+    hoisted.sendMessageDiscord.mockClear().mockResolvedValue({});
+    hoisted.sendWebhookMessageDiscord.mockClear().mockResolvedValue({});
+  });
+
+  it("returns explicit channelId without resolving route", async () => {
+    const resolved = await resolveChannelIdForBinding({
+      accountId: "default",
+      threadId: "thread-1",
+      channelId: "channel-explicit",
+    });
+
+    expect(resolved).toBe("channel-explicit");
+    expect(hoisted.createDiscordRestClient).not.toHaveBeenCalled();
+    expect(hoisted.restGet).not.toHaveBeenCalled();
+  });
+
+  it("returns parent channel for thread channels", async () => {
+    hoisted.restGet.mockResolvedValueOnce({
+      id: "thread-1",
+      type: ChannelType.PublicThread,
+      parent_id: "channel-parent",
+    });
+
+    const resolved = await resolveChannelIdForBinding({
+      accountId: "default",
+      threadId: "thread-1",
+    });
+
+    expect(resolved).toBe("channel-parent");
+  });
+
+  it("forwards cfg when resolving channel id through Discord client", async () => {
+    const cfg = {
+      channels: { discord: { token: "tok" } },
+    } as OpenClawConfig;
+    hoisted.restGet.mockResolvedValueOnce({
+      id: "thread-1",
+      type: ChannelType.PublicThread,
+      parent_id: "channel-parent",
+    });
+
+    await resolveChannelIdForBinding({
+      cfg,
+      accountId: "default",
+      threadId: "thread-1",
+    });
+
+    const createDiscordRestClientCalls = hoisted.createDiscordRestClient.mock.calls as unknown[][];
+    expect(createDiscordRestClientCalls[0]?.[1]).toBe(cfg);
+  });
+
+  it("keeps non-thread channel id even when parent_id exists", async () => {
+    hoisted.restGet.mockResolvedValueOnce({
+      id: "channel-text",
+      type: ChannelType.GuildText,
+      parent_id: "category-1",
+    });
+
+    const resolved = await resolveChannelIdForBinding({
+      accountId: "default",
+      threadId: "channel-text",
+    });
+
+    expect(resolved).toBe("channel-text");
+  });
+
+  it("keeps forum channel id instead of parent category", async () => {
+    hoisted.restGet.mockResolvedValueOnce({
+      id: "forum-1",
+      type: ChannelType.GuildForum,
+      parent_id: "category-1",
+    });
+
+    const resolved = await resolveChannelIdForBinding({
+      accountId: "default",
+      threadId: "forum-1",
+    });
+
+    expect(resolved).toBe("forum-1");
+  });
+});
+
+describe("maybeSendBindingMessage", () => {
+  beforeEach(() => {
+    hoisted.sendMessageDiscord.mockClear().mockResolvedValue({});
+    hoisted.sendWebhookMessageDiscord.mockClear().mockResolvedValue({});
+  });
+
+  it("forwards cfg to webhook send path", async () => {
+    const cfg = {
+      channels: { discord: { token: "tok" } },
+    } as OpenClawConfig;
+    const record = {
+      accountId: "default",
+      channelId: "parent-1",
+      threadId: "thread-1",
+      targetKind: "subagent",
+      targetSessionKey: "agent:main:subagent:test",
+      agentId: "main",
+      boundBy: "test",
+      boundAt: Date.now(),
+      lastActivityAt: Date.now(),
+      webhookId: "wh_1",
+      webhookToken: "tok_1",
+    } satisfies ThreadBindingRecord;
+
+    await maybeSendBindingMessage({
+      cfg,
+      record,
+      text: "hello webhook",
+    });
+
+    expect(hoisted.sendWebhookMessageDiscord).toHaveBeenCalledTimes(1);
+    expect(hoisted.sendWebhookMessageDiscord.mock.calls[0]?.[1]).toMatchObject({
+      cfg,
+      webhookId: "wh_1",
+      webhookToken: "tok_1",
+      accountId: "default",
+      threadId: "thread-1",
+    });
+    expect(hoisted.sendMessageDiscord).not.toHaveBeenCalled();
+  });
+});

remoteclaw/remoteclaw

(cherry picked from commit 5682ec37fada89205821ee16a03f1e0d0948efb7)
# Conflicts:
#	extensions/discord/src/account-inspect.test.ts
#	extensions/discord/src/accounts.test.ts
#	extensions/discord/src/api.test.ts
#	extensions/discord/src/audit.test.ts
#	extensions/discord/src/chunk.test.ts
#	extensions/discord/src/client.test.ts
#	extensions/discord/src/components.test.ts
#	extensions/discord/src/directory-live.test.ts
#	extensions/discord/src/gateway-logging.test.ts
#	extensions/discord/src/mentions.test.ts
#	extensions/discord/src/monitor.gateway.test.ts
#	extensions/discord/src/monitor.test.ts
#	extensions/discord/src/monitor.tool-result.accepts-guild-messages-mentionpatterns-match.e2e.test.ts
#	extensions/discord/src/monitor.tool-result.sends-status-replies-responseprefix.test.ts
#	extensions/discord/src/monitor/agent-components.wildcard.test.ts
#	extensions/discord/src/monitor/auto-presence.test.ts
#	extensions/discord/src/monitor/commands.test.ts
#	extensions/discord/src/monitor/dm-command-auth.test.ts
#	extensions/discord/src/monitor/dm-command-decision.test.ts
#	extensions/discord/src/monitor/exec-approvals.test.ts
#	extensions/discord/src/monitor/gateway-error-guard.test.ts
#	extensions/discord/src/monitor/inbound-context.test.ts
#	extensions/discord/src/monitor/inbound-job.test.ts
#	extensions/discord/src/monitor/listeners.test.ts
#	extensions/discord/src/monitor/message-handler.bot-self-filter.test.ts
#	extensions/discord/src/monitor/message-handler.inbound-contract.test.ts
#	extensions/discord/src/monitor/message-handler.preflight.acp-bindings.test.ts
#	extensions/discord/src/monitor/message-handler.preflight.test.ts
#	extensions/discord/src/monitor/message-handler.process.test.ts
#	extensions/discord/src/monitor/message-handler.queue.test.ts
#	extensions/discord/src/monitor/message-handler.test-harness.ts
#	extensions/discord/src/monitor/message-handler.test-helpers.ts
#	extensions/discord/src/monitor/message-utils.test.ts
#	extensions/discord/src/monitor/

extensions/discord/src/accounts.ts
@@ -1,0 +1,89 @@
+import { createAccountActionGate } from "../../../src/channels/plugins/account-action-gate.js";
+import { createAccountListHelpers } from "../../../src/channels/plugins/account-helpers.js";
+import type { RemoteClawConfig } from "../../../src/config/config.js";
+import type { DiscordAccountConfig, DiscordActionConfig } from "../../../src/config/types.js";
+import { resolveAccountEntry } from "../../../src/routing/account-lookup.js";
+import { normalizeAccountId } from "../../../src/routing/session-key.js";
+import { resolveDiscordToken } from "./token.js";
+
+export type ResolvedDiscordAccount = {
+  accountId: string;
+  enabled: boolean;
+  name?: string;
+  token: string;
+  tokenSource: "env" | "config" | "none";
+  config: DiscordAccountConfig;
+};
+
+const { listAccountIds, resolveDefaultAccountId } = createAccountListHelpers("discord");
+export const listDiscordAccountIds = listAccountIds;
+export const resolveDefaultDiscordAccountId = resolveDefaultAccountId;
+
+export function resolveDiscordAccountConfig(
+  cfg: RemoteClawConfig,
+  accountId: string,
+): DiscordAccountConfig | undefined {
+  return resolveAccountEntry(cfg.channels?.discord?.accounts, accountId);
+}
+
+export function mergeDiscordAccountConfig(
+  cfg: RemoteClawConfig,
+  accountId: string,
+): DiscordAccountConfig {
+  const { accounts: _ignored, ...base } = (cfg.channels?.discord ?? {}) as DiscordAccountConfig & {
+    accounts?: unknown;
+  };
+  const account = resolveDiscordAccountConfig(cfg, accountId) ?? {};
+  return { ...base, ...account };
+}
+
+export function createDiscordActionGate(params: {
+  cfg: RemoteClawConfig;
+  accountId?: string | null;
+}): (key: keyof DiscordActionConfig, defaultValue?: boolean) => boolean {
+  const accountId = normalizeAccountId(params.accountId);
+  return createAccountActionGate({
+    baseActions: params.cfg.channels?.discord?.actions,
+    accountActions: resolveDiscordAccountConfig(params.cfg, accountId)?.actions,
+  });
+}
+
+export function resolveDiscordAccount(params: {
+  cfg: RemoteClawConfig;
+  accountId?: string | null;
+}): ResolvedDiscordAccount {
+  const accountId = normalizeAccountId(params.accountId);
+  const baseEnabled = params.cfg.channels?.discord?.enabled !== false;
+  const merged = mergeDiscordAccountConfig(params.cfg, accountId);
+  const accountEnabled = merged.enabled !== false;
+  const enabled = baseEnabled && accountEnabled;
+  const tokenResolution = resolveDiscordToken(params.cfg, { accountId });
+  return {
+    accountId,
+    enabled,
+    name: merged.name?.trim() || undefined,
+    token: tokenResolution.token,
+    tokenSource: tokenResolution.source,
+    config: merged,
+  };
+}
+
+export function resolveDiscordMaxLinesPerMessage(params: {
+  cfg: RemoteClawConfig;
+  discordConfig?: DiscordAccountConfig | null;
+  accountId?: string | null;
+}): number | undefined {
+  if (typeof params.discordConfig?.maxLinesPerMessage === "number") {
+    return params.discordConfig.maxLinesPerMessage;
+  }
+  return resolveDiscordAccount({
+    cfg: params.cfg,
+    accountId: params.accountId,
+  }).config.maxLinesPerMessage;
+}
+
+export function listEnabledDiscordAccounts(cfg: RemoteClawConfig): ResolvedDiscordAccount[] {
+  return listDiscordAccountIds(cfg)
+    .map((accountId) => resolveDiscordAccount({ cfg, accountId }))
+    .filter((account) => account.enabled);
+}

extensions/discord/src/monitor/thread-bindings.config.ts
@@ -1,0 +1,39 @@
+import {
+  resolveThreadBindingIdleTimeoutMs,
+  resolveThreadBindingMaxAgeMs,
+  resolveThreadBindingsEnabled,
+} from "../../../../src/channels/thread-bindings-policy.js";
+import type { RemoteClawConfig } from "../../../../src/config/config.js";
+import { normalizeAccountId } from "../../../../src/routing/session-key.js";
+
+export {
+  resolveThreadBindingIdleTimeoutMs,
+  resolveThreadBindingMaxAgeMs,
+  resolveThreadBindingsEnabled,
+};
+
+export function resolveDiscordThreadBindingIdleTimeoutMs(params: {
+  cfg: RemoteClawConfig;
+  accountId?: string;
+}): number {
+  const accountId = normalizeAccountId(params.accountId);
+  const root = params.cfg.channels?.discord?.threadBindings;
+  const account = params.cfg.channels?.discord?.accounts?.[accountId]?.threadBindings;
+  return resolveThreadBindingIdleTimeoutMs({
+    channelIdleHoursRaw: account?.idleHours ?? root?.idleHours,
+    sessionIdleHoursRaw: params.cfg.session?.threadBindings?.idleHours,
+  });
+}
+
+export function resolveDiscordThreadBindingMaxAgeMs(params: {
+  cfg: RemoteClawConfig;
+  accountId?: string;
+}): number {
+  const accountId = normalizeAccountId(params.accountId);
+  const root = params.cfg.channels?.discord?.threadBindings;
+  const account = params.cfg.channels?.discord?.accounts?.[accountId]?.threadBindings;
+  return resolveThreadBindingMaxAgeMs({
+    channelMaxAgeHoursRaw: account?.maxAgeHours ?? root?.maxAgeHours,
+    sessionMaxAgeHoursRaw: params.cfg.session?.threadBindings?.maxAgeHours,
+  });
+}

extensions/discord/src/monitor/thread-bindings.discord-api.test.ts
@@ -1,0 +1,161 @@
+import { ChannelType } from "discord-api-types/v10";
+import { beforeEach, describe, expect, it, vi } from "vitest";
+import type { RemoteClawConfig } from "../../../../src/config/config.js";
+import type { ThreadBindingRecord } from "./thread-bindings.types.js";
+
+const hoisted = vi.hoisted(() => {
+  const restGet = vi.fn();
+  const sendMessageDiscord = vi.fn();
+  const sendWebhookMessageDiscord = vi.fn();
+  const createDiscordRestClient = vi.fn(() => ({
+    rest: {
+      get: restGet,
+    },
+  }));
+  return {
+    restGet,
+    sendMessageDiscord,
+    sendWebhookMessageDiscord,
+    createDiscordRestClient,
+  };
+});
+
+vi.mock("../client.js", () => ({
+  createDiscordRestClient: hoisted.createDiscordRestClient,
+}));
+
+vi.mock("../send.js", () => ({
+  sendMessageDiscord: (...args: unknown[]) => hoisted.sendMessageDiscord(...args),
+  sendWebhookMessageDiscord: (...args: unknown[]) => hoisted.sendWebhookMessageDiscord(...args),
+}));
+
+const { maybeSendBindingMessage, resolveChannelIdForBinding } =
+  await import("./thread-bindings.discord-api.js");
+
+describe("resolveChannelIdForBinding", () => {
+  beforeEach(() => {
+    hoisted.restGet.mockClear();
+    hoisted.createDiscordRestClient.mockClear();
+    hoisted.sendMessageDiscord.mockClear().mockResolvedValue({});
+    hoisted.sendWebhookMessageDiscord.mockClear().mockResolvedValue({});
+  });
+
+  it("returns explicit channelId without resolving route", async () => {
+    const resolved = await resolveChannelIdForBinding({
+      accountId: "default",
+      threadId: "thread-1",
+      channelId: "channel-explicit",
+    });
+
+    expect(resolved).toBe("channel-explicit");
+    expect(hoisted.createDiscordRestClient).not.toHaveBeenCalled();
+    expect(hoisted.restGet).not.toHaveBeenCalled();
+  });
+
+  it("returns parent channel for thread channels", async () => {
+    hoisted.restGet.mockResolvedValueOnce({
+      id: "thread-1",
+      type: ChannelType.PublicThread,
+      parent_id: "channel-parent",
+    });
+
+    const resolved = await resolveChannelIdForBinding({
+      accountId: "default",
+      threadId: "thread-1",
+    });
+
+    expect(resolved).toBe("channel-parent");
+  });
+
+  it("forwards cfg when resolving channel id through Discord client", async () => {
+    const cfg = {
+      channels: { discord: { token: "tok" } },
+    } as RemoteClawConfig;
+    hoisted.restGet.mockResolvedValueOnce({
+      id: "thread-1",
+      type: ChannelType.PublicThread,
+      parent_id: "channel-parent",
+    });
+
+    await resolveChannelIdForBinding({
+      cfg,
+      accountId: "default",
+      threadId: "thread-1",
+    });
+
+    const createDiscordRestClientCalls = hoisted.createDiscordRestClient.mock.calls as unknown[][];
+    expect(createDiscordRestClientCalls[0]?.[1]).toBe(cfg);
+  });
+
+  it("keeps non-thread channel id even when parent_id exists", async () => {
+    hoisted.restGet.mockResolvedValueOnce({
+      id: "channel-text",
+      type: ChannelType.GuildText,
+      parent_id: "category-1",
+    });
+
+    const resolved = await resolveChannelIdForBinding({
+      accountId: "default",
+      threadId: "channel-text",
+    });
+
+    expect(resolved).toBe("channel-text");
+  });
+
+  it("keeps forum channel id instead of parent category", async () => {
+    hoisted.restGet.mockResolvedValueOnce({
+      id: "forum-1",
+      type: ChannelType.GuildForum,
+      parent_id: "category-1",
+    });
+
+    const resolved = await resolveChannelIdForBinding({
+      accountId: "default",
+      threadId: "forum-1",
+    });
+
+    expect(resolved).toBe("forum-1");
+  });
+});
+
+describe("maybeSendBindingMessage", () => {
+  beforeEach(() => {
+    hoisted.sendMessageDiscord.mockClear().mockResolvedValue({});
+    hoisted.sendWebhookMessageDiscord.mockClear().mockResolvedValue({});
+  });
+
+  it("forwards cfg to webhook send path", async () => {
+    const cfg = {
+      channels: { discord: { token: "tok" } },
+    } as RemoteClawConfig;
+    const record = {
+      accountId: "default",
+      channelId: "parent-1",
+      threadId: "thread-1",
+      targetKind: "subagent",
+      targetSessionKey: "agent:main:subagent:test",
+      agentId: "main",
+      boundBy: "test",
+      boundAt: Date.now(),
+      lastActivityAt: Date.now(),
+      webhookId: "wh_1",
+      webhookToken: "tok_1",
+    } satisfies ThreadBindingRecord;
+
+    await maybeSendBindingMessage({
+      cfg,
+      record,
+      text: "hello webhook",
+    });
+
+    expect(hoisted.sendWebhookMessageDiscord).toHaveBeenCalledTimes(1);
+    expect(hoisted.sendWebhookMessageDiscord.mock.calls[0]?.[1]).toMatchObject({
+      cfg,
+      webhookId: "wh_1",
+      webhookToken: "tok_1",
+      accountId: "default",
+      threadId: "thread-1",
+    });
+    expect(hoisted.sendMessageDiscord).not.toHaveBeenCalled();
+  });
+});
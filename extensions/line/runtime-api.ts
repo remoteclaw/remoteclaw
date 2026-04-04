Nachx639/clawdbot

fix: restore plugin sdk runtime barrels

extensions/line/runtime-api.ts
@@ -1,12 +1,12 @@
 // Private runtime barrel for the bundled LINE extension.
 // Keep this barrel thin and aligned with the local extension surface.
 
-export * from "../../src/plugin-sdk/line.js";
-export { resolveExactLineGroupConfigKey } from "../../src/plugin-sdk/line-core.js";
+export * from "openclaw/plugin-sdk/line";
+export { resolveExactLineGroupConfigKey } from "openclaw/plugin-sdk/line-core";
 export {
   formatDocsLink,
   setSetupChannelEnabled,
   splitSetupEntries,
   type ChannelSetupDmPolicy,
   type ChannelSetupWizard,
-} from "../../src/plugin-sdk/line-core.js";
+} from "openclaw/plugin-sdk/line-core";

extensions/line/src/group-policy.ts
@@ -1,5 +1,5 @@
 import { resolveChannelGroupRequireMention } from "openclaw/plugin-sdk/channel-policy";
-import { resolveExactLineGroupConfigKey, type OpenClawConfig } from "../runtime-api.js";
+import { resolveExactLineGroupConfigKey, type OpenClawConfig } from "openclaw/plugin-sdk/line-core";
 
 type LineGroupContext = {
   cfg: OpenClawConfig;

remoteclaw/remoteclaw

extensions/line/runtime-api.ts
@@ -1,74 +1,12 @@
 // Private runtime barrel for the bundled LINE extension.
 // Keep this barrel thin and aligned with the local extension surface.
 
-export type {
-  ChannelPlugin,
-  RemoteClawConfig,
-  RemoteClawPluginApi,
-  PluginRuntime,
-} from "remoteclaw/plugin-sdk/core";
-export { buildChannelConfigSchema, clearAccountEntryFields } from "remoteclaw/plugin-sdk/core";
-export type { ReplyPayload } from "remoteclaw/plugin-sdk/reply-runtime";
-export type { ChannelAccountSnapshot, ChannelGatewayContext } from "remoteclaw/plugin-sdk/testing";
-export type { ChannelStatusIssue } from "remoteclaw/plugin-sdk/channel-contract";
-export type { ChannelSetupDmPolicy, ChannelSetupWizard } from "remoteclaw/plugin-sdk/setup";
+export * from "remoteclaw/plugin-sdk/line";
+export { resolveExactLineGroupConfigKey } from "remoteclaw/plugin-sdk/line-core";
 export {
-  buildComputedAccountStatusSnapshot,
-  buildTokenChannelStatusSummary,
-} from "remoteclaw/plugin-sdk/status-helpers";
-export {
-  DEFAULT_ACCOUNT_ID,
   formatDocsLink,
   setSetupChannelEnabled,
   splitSetupEntries,
-} from "remoteclaw/plugin-sdk/setup";
-export * from "remoteclaw/plugin-sdk/line-runtime";
-
-export * from "./src/accounts.js";
-export * from "./src/bot-access.js";
-export * from "./src/channel-access-token.js";
-export * from "./src/config-schema.js";
-export * from "./src/download.js";
-export * from "./src/flex-templates.js";
-export * from "./src/group-keys.js";
-export * from "./src/markdown-to-line.js";
-export * from "./src/probe.js";
-export * from "./src/send.js";
-export * from "./src/signature.js";
-export { datetimePickerAction, messageAction, postbackAction, uriAction } from "./src/rich-menu.js";
-export {
-  createDefaultMenuConfig,
-  createGridLayout,
-  type RichMenuArea,
-  type RichMenuRequest,
-  type RichMenuResponse,
-} from "./src/rich-menu.js";
-export {
-  createButtonMenu,
-  createButtonTemplate,
-  createCarouselColumn,
-  createConfirmTemplate,
-  createImageCarousel,
-  createImageCarouselColumn,
-  createLinkMenu,
-  createProductCarousel,
-  createTemplateCarousel,
-  createYesNoConfirm,
-  buildTemplateMessageFromPayload,
-  type ButtonsTemplate,
-  type CarouselColumn,
-  type CarouselTemplate,
-  type ConfirmTemplate,
-  type ImageCarouselColumn,
-  type ImageCarouselTemplate,
-  type TemplateMessage,
-} from "./src/template-messages.js";
-export type {
-  LineChannelData,
-  LineConfig,
-  LineProbeResult,
-  ResolvedLineAccount,
-} from "./src/types.js";
-export * from "./src/webhook-node.js";
-export * from "./src/webhook.js";
-export * from "./src/webhook-utils.js";
+  type ChannelSetupDmPolicy,
+  type ChannelSetupWizard,
+} from "remoteclaw/plugin-sdk/line-core";

extensions/line/src/group-policy.ts
@@ -1,5 +1,5 @@
 import { resolveChannelGroupRequireMention } from "remoteclaw/plugin-sdk/channel-policy";
-import { resolveExactLineGroupConfigKey, type RemoteClawConfig } from "../runtime-api.js";
+import { resolveExactLineGroupConfigKey, type RemoteClawConfig } from "remoteclaw/plugin-sdk/line-core";
 
 type LineGroupContext = {
   cfg: RemoteClawConfig;
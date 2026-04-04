wonglaitung/openclaw_mini

refactor: move group access into setup wizard

src/channels/plugins/setup-group-access-configure.test.ts
@@ -1,7 +1,7 @@
 import { describe, expect, it, vi } from "vitest";
-import type { OpenClawConfig } from "../../../config/config.js";
-import { configureChannelAccessWithAllowlist } from "./channel-access-configure.js";
-import type { ChannelAccessPolicy } from "./channel-access.js";
+import type { OpenClawConfig } from "../../config/config.js";
+import { configureChannelAccessWithAllowlist } from "./setup-group-access-configure.js";
+import type { ChannelAccessPolicy } from "./setup-group-access.js";
 
 function createPrompter(params: { confirm: boolean; policy?: ChannelAccessPolicy; text?: string }) {
   return {
@@ -85,6 +85,41 @@ describe("configureChannelAccessWithAllowlist", () => {
 
     expect(next.channels?.discord?.groupPolicy).toBe("open");
     expect(setPolicy).toHaveBeenCalledWith(cfg, "open");
+    expect(resolveAllowlist).not.toHaveBeenCalled();
+    expect(applyAllowlist).not.toHaveBeenCalled();
+  });
+
+  it("supports allowlist policies without prompting for entries", async () => {
+    const cfg: OpenClawConfig = {};
+    const prompter = createPrompter({
+      confirm: true,
+      policy: "allowlist",
+    });
+    const setPolicy = vi.fn(
+      (next: OpenClawConfig, policy: ChannelAccessPolicy): OpenClawConfig => ({
+        ...next,
+        channels: { twitch: { groupPolicy: policy } },
+      }),
+    );
+    const resolveAllowlist = vi.fn(async () => ["ignored"]);
+    const applyAllowlist = vi.fn((params: { cfg: OpenClawConfig }) => params.cfg);
+
+    const next = await configureChannelAccessWithAllowlist({
+      cfg,
+      // oxlint-disable-next-line typescript/no-explicit-any
+      prompter: prompter as any,
+      label: "Twitch chat",
+      currentPolicy: "disabled",
+      currentEntries: [],
+      placeholder: "",
+      updatePrompt: false,
+      skipAllowlistEntries: true,
+      setPolicy,
+      resolveAllowlist,
+      applyAllowlist,
+    });
+
+    expect(next.channels).toEqual({ twitch: { groupPolicy: "allowlist" } });
     expect(resolveAllowlist).not.toHaveBeenCalled();
     expect(applyAllowlist).not.toHaveBeenCalled();
   });

src/channels/plugins/setup-group-access-configure.ts
@@ -1,6 +1,6 @@
-import type { OpenClawConfig } from "../../../config/config.js";
-import type { WizardPrompter } from "../../../wizard/prompts.js";
-import { promptChannelAccessConfig, type ChannelAccessPolicy } from "./channel-access.js";
+import type { OpenClawConfig } from "../../config/config.js";
+import type { WizardPrompter } from "../../wizard/prompts.js";
+import { promptChannelAccessConfig, type ChannelAccessPolicy } from "./setup-group-access.js";
 
 export async function configureChannelAccessWithAllowlist<TResolved>(params: {
   cfg: OpenClawConfig;
@@ -10,9 +10,10 @@ export async function configureChannelAccessWithAllowlist<TResolved>(params: {
   currentEntries: string[];
   placeholder: string;
   updatePrompt: boolean;
+  skipAllowlistEntries?: boolean;
   setPolicy: (cfg: OpenClawConfig, policy: ChannelAccessPolicy) => OpenClawConfig;
-  resolveAllowlist: (params: { cfg: OpenClawConfig; entries: string[] }) => Promise<TResolved>;
-  applyAllowlist: (params: { cfg: OpenClawConfig; resolved: TResolved }) => OpenClawConfig;
+  resolveAllowlist?: (params: { cfg: OpenClawConfig; entries: string[] }) => Promise<TResolved>;
+  applyAllowlist?: (params: { cfg: OpenClawConfig; resolved: TResolved }) => OpenClawConfig;
 }): Promise<OpenClawConfig> {
   let next = params.cfg;
   const accessConfig = await promptChannelAccessConfig({
@@ -22,12 +23,16 @@ export async function configureChannelAccessWithAllowlist<TResolved>(params: {
     currentEntries: params.currentEntries,
     placeholder: params.placeholder,
     updatePrompt: params.updatePrompt,
+    skipAllowlistEntries: params.skipAllowlistEntries,
   });
   if (!accessConfig) {
     return next;
   }
   if (accessConfig.policy !== "allowlist") {
     return params.setPolicy(next, accessConfig.policy);
+  }
+  if (params.skipAllowlistEntries || !params.resolveAllowlist || !params.applyAllowlist) {
+    return params.setPolicy(next, "allowlist");
   }
   const resolved = await params.resolveAllowlist({
     cfg: next,

remoteclaw/remoteclaw

src/channels/plugins/setup-group-access-configure.test.ts
@@ -1,7 +1,7 @@
 import { describe, expect, it, vi } from "vitest";
-import type { RemoteClawConfig } from "../../../config/config.js";
-import { configureChannelAccessWithAllowlist } from "./channel-access-configure.js";
-import type { ChannelAccessPolicy } from "./channel-access.js";
+import type { RemoteClawConfig } from "../../config/config.js";
+import { configureChannelAccessWithAllowlist } from "./setup-group-access-configure.js";
+import type { ChannelAccessPolicy } from "./setup-group-access.js";
 
 function createPrompter(params: { confirm: boolean; policy?: ChannelAccessPolicy; text?: string }) {
   return {
@@ -89,6 +89,41 @@ describe("configureChannelAccessWithAllowlist", () => {
     expect(applyAllowlist).not.toHaveBeenCalled();
   });
 
+  it("supports allowlist policies without prompting for entries", async () => {
+    const cfg: RemoteClawConfig = {};
+    const prompter = createPrompter({
+      confirm: true,
+      policy: "allowlist",
+    });
+    const setPolicy = vi.fn(
+      (next: RemoteClawConfig, policy: ChannelAccessPolicy): RemoteClawConfig => ({
+        ...next,
+        channels: { twitch: { groupPolicy: policy } },
+      }),
+    );
+    const resolveAllowlist = vi.fn(async () => ["ignored"]);
+    const applyAllowlist = vi.fn((params: { cfg: RemoteClawConfig }) => params.cfg);
+
+    const next = await configureChannelAccessWithAllowlist({
+      cfg,
+      // oxlint-disable-next-line typescript/no-explicit-any
+      prompter: prompter as any,
+      label: "Twitch chat",
+      currentPolicy: "disabled",
+      currentEntries: [],
+      placeholder: "",
+      updatePrompt: false,
+      skipAllowlistEntries: true,
+      setPolicy,
+      resolveAllowlist,
+      applyAllowlist,
+    });
+
+    expect(next.channels).toEqual({ twitch: { groupPolicy: "allowlist" } });
+    expect(resolveAllowlist).not.toHaveBeenCalled();
+    expect(applyAllowlist).not.toHaveBeenCalled();
+  });
+
   it("resolves allowlist entries and applies them after forcing allowlist policy", async () => {
     const cfg: RemoteClawConfig = {};
     const prompter = createPrompter({
@@ -97,15 +132,13 @@ describe("configureChannelAccessWithAllowlist", () => {
       text: "#general, #support",
     });
     const calls: string[] = [];
-    const setPolicy = vi.fn(
-      (next: RemoteClawConfig, policy: ChannelAccessPolicy): RemoteClawConfig => {
-        calls.push("setPolicy");
-        return {
-          ...next,
-          channels: { slack: { groupPolicy: policy } },
-        };
-      },
-    );
+    const setPolicy = vi.fn((next: RemoteClawConfig, policy: ChannelAccessPolicy): RemoteClawConfig => {
+      calls.push("setPolicy");
+      return {
+        ...next,
+        channels: { slack: { groupPolicy: policy } },
+      };
+    });
     const resolveAllowlist = vi.fn(async (params: { cfg: RemoteClawConfig; entries: string[] }) => {
       calls.push("resolve");
       expect(params.cfg).toBe(cfg);

src/channels/plugins/setup-group-access-configure.ts
@@ -1,6 +1,6 @@
-import type { RemoteClawConfig } from "../../../config/config.js";
-import type { WizardPrompter } from "../../../wizard/prompts.js";
-import { promptChannelAccessConfig, type ChannelAccessPolicy } from "./channel-access.js";
+import type { RemoteClawConfig } from "../../config/config.js";
+import type { WizardPrompter } from "../../wizard/prompts.js";
+import { promptChannelAccessConfig, type ChannelAccessPolicy } from "./setup-group-access.js";
 
 export async function configureChannelAccessWithAllowlist<TResolved>(params: {
   cfg: RemoteClawConfig;
@@ -10,9 +10,10 @@ export async function configureChannelAccessWithAllowlist<TResolved>(params: {
   currentEntries: string[];
   placeholder: string;
   updatePrompt: boolean;
+  skipAllowlistEntries?: boolean;
   setPolicy: (cfg: RemoteClawConfig, policy: ChannelAccessPolicy) => RemoteClawConfig;
-  resolveAllowlist: (params: { cfg: RemoteClawConfig; entries: string[] }) => Promise<TResolved>;
-  applyAllowlist: (params: { cfg: RemoteClawConfig; resolved: TResolved }) => RemoteClawConfig;
+  resolveAllowlist?: (params: { cfg: RemoteClawConfig; entries: string[] }) => Promise<TResolved>;
+  applyAllowlist?: (params: { cfg: RemoteClawConfig; resolved: TResolved }) => RemoteClawConfig;
 }): Promise<RemoteClawConfig> {
   let next = params.cfg;
   const accessConfig = await promptChannelAccessConfig({
@@ -22,12 +23,16 @@ export async function configureChannelAccessWithAllowlist<TResolved>(params: {
     currentEntries: params.currentEntries,
     placeholder: params.placeholder,
     updatePrompt: params.updatePrompt,
+    skipAllowlistEntries: params.skipAllowlistEntries,
   });
   if (!accessConfig) {
     return next;
   }
   if (accessConfig.policy !== "allowlist") {
     return params.setPolicy(next, accessConfig.policy);
+  }
+  if (params.skipAllowlistEntries || !params.resolveAllowlist || !params.applyAllowlist) {
+    return params.setPolicy(next, "allowlist");
   }
   const resolved = await params.resolveAllowlist({
     cfg: next,
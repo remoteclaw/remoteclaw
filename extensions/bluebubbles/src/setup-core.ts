wonglaitung/openclaw_mini

refactor(setup): share scoped config prelude

extensions/bluebubbles/src/setup-core.ts
@@ -1,14 +1,12 @@
 import {
-  applyAccountNameToChannelSection,
-  DEFAULT_ACCOUNT_ID,
-  migrateBaseNameToDefaultAccount,
   patchScopedAccountConfig,
-  normalizeAccountId,
-  setTopLevelChannelDmPolicyWithAllowFrom,
-  type ChannelSetupAdapter,
-  type DmPolicy,
-  type OpenClawConfig,
-} from "openclaw/plugin-sdk/setup";
+  prepareScopedSetupConfig,
+} from "../../../src/channels/plugins/setup-helpers.js";
+import { setTopLevelChannelDmPolicyWithAllowFrom } from "../../../src/channels/plugins/setup-wizard-helpers.js";
+import type { ChannelSetupAdapter } from "../../../src/channels/plugins/types.adapters.js";
+import type { OpenClawConfig } from "../../../src/config/config.js";
+import type { DmPolicy } from "../../../src/config/types.js";
+import { normalizeAccountId } from "../../../src/routing/session-key.js";
 import { applyBlueBubblesConnectionConfig } from "./config-apply.js";
 
 const channel = "bluebubbles" as const;
@@ -39,7 +37,7 @@ export function setBlueBubblesAllowFrom(
 export const blueBubblesSetupAdapter: ChannelSetupAdapter = {
   resolveAccountId: ({ accountId }) => normalizeAccountId(accountId),
   applyAccountName: ({ cfg, accountId, name }) =>
-    applyAccountNameToChannelSection({
+    prepareScopedSetupConfig({
       cfg,
       channelKey: channel,
       accountId,
@@ -58,19 +56,13 @@ export const blueBubblesSetupAdapter: ChannelSetupAdapter = {
     return null;
   },
   applyAccountConfig: ({ cfg, accountId, input }) => {
-    const namedConfig = applyAccountNameToChannelSection({
+    const next = prepareScopedSetupConfig({
       cfg,
       channelKey: channel,
       accountId,
       name: input.name,
+      migrateBaseName: true,
     });
-    const next =
-      accountId !== DEFAULT_ACCOUNT_ID
-        ? migrateBaseNameToDefaultAccount({
-            cfg: namedConfig,
-            channelKey: channel,
-          })
-        : namedConfig;
     return applyBlueBubblesConnectionConfig({
       cfg: next,
       accountId,

extensions/tlon/src/setup-core.ts
@@ -1,12 +1,11 @@
 import {
-  applyAccountNameToChannelSection,
-  DEFAULT_ACCOUNT_ID,
-  normalizeAccountId,
   patchScopedAccountConfig,
-  type ChannelSetupAdapter,
-  type ChannelSetupInput,
-  type OpenClawConfig,
-} from "openclaw/plugin-sdk/setup";
+  prepareScopedSetupConfig,
+} from "../../../src/channels/plugins/setup-helpers.js";
+import type { ChannelSetupAdapter } from "../../../src/channels/plugins/types.adapters.js";
+import type { ChannelSetupInput } from "../../../src/channels/plugins/types.core.js";
+import type { OpenClawConfig } from "../../../src/config/config.js";
+import { DEFAULT_ACCOUNT_ID, normalizeAccountId } from "../../../src/routing/session-key.js";
 import { buildTlonAccountFields } from "./account-fields.js";
 import { resolveTlonAccount } from "./types.js";
 
@@ -30,7 +29,7 @@ export function applyTlonSetupConfig(params: {
 }): OpenClawConfig {
   const { cfg, accountId, input } = params;
   const useDefault = accountId === DEFAULT_ACCOUNT_ID;
-  const namedConfig = applyAccountNameToChannelSection({
+  const namedConfig = prepareScopedSetupConfig({
     cfg,
     channelKey: channel,
     accountId,
@@ -70,7 +69,7 @@ export function applyTlonSetupConfig(params: {
 export const tlonSetupAdapter: ChannelSetupAdapter = {
   resolveAccountId: ({ accountId }) => normalizeAccountId(accountId),
   applyAccountName: ({ cfg, accountId, name }) =>
-    applyAccountNameToChannelSection({
+    prepareScopedSetupConfig({
       cfg,
       channelKey: channel,
       accountId,

remoteclaw/remoteclaw

extensions/bluebubbles/src/setup-core.ts
@@ -1,19 +1,20 @@
 import {
-  applyAccountNameToChannelSection,
-  DEFAULT_ACCOUNT_ID,
-  migrateBaseNameToDefaultAccount,
   patchScopedAccountConfig,
-  normalizeAccountId,
-  setTopLevelChannelDmPolicyWithAllowFrom,
-  type ChannelSetupAdapter,
-  type DmPolicy,
-  type RemoteClawConfig,
-} from "remoteclaw/plugin-sdk/setup";
+  prepareScopedSetupConfig,
+} from "../../../src/channels/plugins/setup-helpers.js";
+import { setTopLevelChannelDmPolicyWithAllowFrom } from "../../../src/channels/plugins/setup-wizard-helpers.js";
+import type { ChannelSetupAdapter } from "../../../src/channels/plugins/types.adapters.js";
+import type { RemoteClawConfig } from "../../../src/config/config.js";
+import type { DmPolicy } from "../../../src/config/types.js";
+import { normalizeAccountId } from "../../../src/routing/session-key.js";
 import { applyBlueBubblesConnectionConfig } from "./config-apply.js";
 
 const channel = "bluebubbles" as const;
 
-export function setBlueBubblesDmPolicy(cfg: RemoteClawConfig, dmPolicy: DmPolicy): RemoteClawConfig {
+export function setBlueBubblesDmPolicy(
+  cfg: RemoteClawConfig,
+  dmPolicy: DmPolicy,
+): RemoteClawConfig {
   return setTopLevelChannelDmPolicyWithAllowFrom({
     cfg,
     channel,
@@ -39,7 +40,7 @@ export function setBlueBubblesAllowFrom(
 export const blueBubblesSetupAdapter: ChannelSetupAdapter = {
   resolveAccountId: ({ accountId }) => normalizeAccountId(accountId),
   applyAccountName: ({ cfg, accountId, name }) =>
-    applyAccountNameToChannelSection({
+    prepareScopedSetupConfig({
       cfg,
       channelKey: channel,
       accountId,
@@ -58,19 +59,13 @@ export const blueBubblesSetupAdapter: ChannelSetupAdapter = {
     return null;
   },
   applyAccountConfig: ({ cfg, accountId, input }) => {
-    const namedConfig = applyAccountNameToChannelSection({
+    const next = prepareScopedSetupConfig({
       cfg,
       channelKey: channel,
       accountId,
       name: input.name,
+      migrateBaseName: true,
     });
-    const next =
-      accountId !== DEFAULT_ACCOUNT_ID
-        ? migrateBaseNameToDefaultAccount({
-            cfg: namedConfig,
-            channelKey: channel,
-          })
-        : namedConfig;
     return applyBlueBubblesConnectionConfig({
       cfg: next,
       accountId,

extensions/tlon/src/setup-core.ts
@@ -1,12 +1,11 @@
 import {
-  applyAccountNameToChannelSection,
-  DEFAULT_ACCOUNT_ID,
-  normalizeAccountId,
   patchScopedAccountConfig,
-  type ChannelSetupAdapter,
-  type ChannelSetupInput,
-  type RemoteClawConfig,
-} from "remoteclaw/plugin-sdk/setup";
+  prepareScopedSetupConfig,
+} from "../../../src/channels/plugins/setup-helpers.js";
+import type { ChannelSetupAdapter } from "../../../src/channels/plugins/types.adapters.js";
+import type { ChannelSetupInput } from "../../../src/channels/plugins/types.core.js";
+import type { RemoteClawConfig } from "../../../src/config/config.js";
+import { DEFAULT_ACCOUNT_ID, normalizeAccountId } from "../../../src/routing/session-key.js";
 import { buildTlonAccountFields } from "./account-fields.js";
 import { resolveTlonAccount } from "./types.js";
 
@@ -30,7 +29,7 @@ export function applyTlonSetupConfig(params: {
 }): RemoteClawConfig {
   const { cfg, accountId, input } = params;
   const useDefault = accountId === DEFAULT_ACCOUNT_ID;
-  const namedConfig = applyAccountNameToChannelSection({
+  const namedConfig = prepareScopedSetupConfig({
     cfg,
     channelKey: channel,
     accountId,
@@ -70,7 +69,7 @@ export function applyTlonSetupConfig(params: {
 export const tlonSetupAdapter: ChannelSetupAdapter = {
   resolveAccountId: ({ accountId }) => normalizeAccountId(accountId),
   applyAccountName: ({ cfg, accountId, name }) =>
-    applyAccountNameToChannelSection({
+    prepareScopedSetupConfig({
       cfg,
       channelKey: channel,
       accountId,
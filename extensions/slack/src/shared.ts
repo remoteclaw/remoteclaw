wonglaitung/openclaw_mini

Tests: stabilize bundled native command regressions

extensions/slack/src/shared.ts
@@ -3,18 +3,14 @@ import {
   createScopedAccountConfigAccessors,
   createScopedChannelConfigBase,
 } from "openclaw/plugin-sdk/channel-config-helpers";
-import {
-  formatDocsLink,
-  hasConfiguredSecretInput,
-  patchChannelConfigForAccount,
-} from "openclaw/plugin-sdk/setup";
-import {
-  buildChannelConfigSchema,
-  getChatChannelMeta,
-  SlackConfigSchema,
-  type ChannelPlugin,
-  type OpenClawConfig,
-} from "openclaw/plugin-sdk/slack";
+import { buildChannelConfigSchema } from "../../../src/channels/plugins/config-schema.js";
+import { patchChannelConfigForAccount } from "../../../src/channels/plugins/setup-wizard-helpers.js";
+import type { ChannelPlugin } from "../../../src/channels/plugins/types.plugin.js";
+import { getChatChannelMeta } from "../../../src/channels/registry.js";
+import type { OpenClawConfig } from "../../../src/config/config.js";
+import { hasConfiguredSecretInput } from "../../../src/config/types.secrets.js";
+import { SlackConfigSchema } from "../../../src/config/zod-schema.providers-core.js";
+import { formatDocsLink } from "../../../src/terminal/links.js";
 import { inspectSlackAccount } from "./account-inspect.js";
 import {
   listSlackAccountIds,

extensions/telegram/src/shared.ts
@@ -3,14 +3,12 @@ import {
   createScopedAccountConfigAccessors,
   createScopedChannelConfigBase,
 } from "openclaw/plugin-sdk/channel-config-helpers";
-import {
-  buildChannelConfigSchema,
-  getChatChannelMeta,
-  normalizeAccountId,
-  TelegramConfigSchema,
-  type ChannelPlugin,
-  type OpenClawConfig,
-} from "openclaw/plugin-sdk/telegram";
+import { buildChannelConfigSchema } from "../../../src/channels/plugins/config-schema.js";
+import type { ChannelPlugin } from "../../../src/channels/plugins/types.plugin.js";
+import { getChatChannelMeta } from "../../../src/channels/registry.js";
+import type { OpenClawConfig } from "../../../src/config/config.js";
+import { TelegramConfigSchema } from "../../../src/config/zod-schema.providers-core.js";
+import { normalizeAccountId } from "../../../src/routing/session-key.js";
 import { inspectTelegramAccount } from "./account-inspect.js";
 import {
   listTelegramAccountIds,

remoteclaw/remoteclaw

extensions/slack/src/shared.ts
@@ -3,18 +3,14 @@ import {
   createScopedAccountConfigAccessors,
   createScopedChannelConfigBase,
 } from "remoteclaw/plugin-sdk/channel-config-helpers";
-import {
-  formatDocsLink,
-  hasConfiguredSecretInput,
-  patchChannelConfigForAccount,
-} from "remoteclaw/plugin-sdk/setup";
-import {
-  buildChannelConfigSchema,
-  getChatChannelMeta,
-  SlackConfigSchema,
-  type ChannelPlugin,
-  type RemoteClawConfig,
-} from "remoteclaw/plugin-sdk/slack";
+import { buildChannelConfigSchema } from "../../../src/channels/plugins/config-schema.js";
+import { patchChannelConfigForAccount } from "../../../src/channels/plugins/setup-wizard-helpers.js";
+import type { ChannelPlugin } from "../../../src/channels/plugins/types.plugin.js";
+import { getChatChannelMeta } from "../../../src/channels/registry.js";
+import type { RemoteClawConfig } from "../../../src/config/config.js";
+import { hasConfiguredSecretInput } from "../../../src/config/types.secrets.js";
+import { SlackConfigSchema } from "../../../src/config/zod-schema.providers-core.js";
+import { formatDocsLink } from "../../../src/terminal/links.js";
 import { inspectSlackAccount } from "./account-inspect.js";
 import {
   listSlackAccountIds,

extensions/telegram/src/shared.ts
@@ -3,14 +3,12 @@ import {
   createScopedAccountConfigAccessors,
   createScopedChannelConfigBase,
 } from "remoteclaw/plugin-sdk/channel-config-helpers";
-import {
-  buildChannelConfigSchema,
-  getChatChannelMeta,
-  normalizeAccountId,
-  TelegramConfigSchema,
-  type ChannelPlugin,
-  type RemoteClawConfig,
-} from "remoteclaw/plugin-sdk/telegram";
+import { buildChannelConfigSchema } from "../../../src/channels/plugins/config-schema.js";
+import type { ChannelPlugin } from "../../../src/channels/plugins/types.plugin.js";
+import { getChatChannelMeta } from "../../../src/channels/registry.js";
+import type { RemoteClawConfig } from "../../../src/config/config.js";
+import { TelegramConfigSchema } from "../../../src/config/zod-schema.providers-core.js";
+import { normalizeAccountId } from "../../../src/routing/session-key.js";
 import { inspectTelegramAccount } from "./account-inspect.js";
 import {
   listTelegramAccountIds,
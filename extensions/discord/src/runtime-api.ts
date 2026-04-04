wonglaitung/openclaw_mini

Discord: stabilize provider registry coverage

extensions/discord/src/runtime-api.ts
@@ -1,12 +1,10 @@
 export {
   buildComputedAccountStatusSnapshot,
   buildTokenChannelStatusSummary,
-  listDiscordDirectoryGroupsFromConfig,
-  listDiscordDirectoryPeersFromConfig,
   PAIRING_APPROVED_MESSAGE,
   projectCredentialSnapshotFields,
   resolveConfiguredFromCredentialStatuses,
-} from "openclaw/plugin-sdk/discord";
+} from "openclaw/plugin-sdk/channel-runtime";
 export {
   buildChannelConfigSchema,
   getChatChannelMeta,
@@ -37,10 +35,9 @@ export {
 export {
   createAccountActionGate,
   createAccountListHelpers,
-  DEFAULT_ACCOUNT_ID,
-  normalizeAccountId,
-  resolveAccountEntry,
-} from "openclaw/plugin-sdk/account-resolution";
+} from "openclaw/plugin-sdk/account-helpers";
+export { DEFAULT_ACCOUNT_ID, normalizeAccountId } from "openclaw/plugin-sdk/account-id";
+export { resolveAccountEntry } from "openclaw/plugin-sdk/routing";
 export type {
   ChannelMessageActionAdapter,
   ChannelMessageActionName,

remoteclaw/remoteclaw

extensions/discord/src/runtime-api.ts
@@ -4,7 +4,7 @@ export {
   PAIRING_APPROVED_MESSAGE,
   projectCredentialSnapshotFields,
   resolveConfiguredFromCredentialStatuses,
-} from "remoteclaw/plugin-sdk/channel-status";
+} from "remoteclaw/plugin-sdk/channel-runtime";
 export {
   buildChannelConfigSchema,
   getChatChannelMeta,
@@ -15,9 +15,6 @@ export {
   resolvePollMaxSelections,
   type ActionGate,
   type ChannelPlugin,
-  type DiscordAccountConfig,
-  type DiscordActionConfig,
-  type DiscordConfig,
   type RemoteClawConfig,
 } from "remoteclaw/plugin-sdk/discord-core";
 export { DiscordConfigSchema } from "remoteclaw/plugin-sdk/discord-core";
@@ -44,9 +41,11 @@ export { resolveAccountEntry } from "remoteclaw/plugin-sdk/routing";
 export type {
   ChannelMessageActionAdapter,
   ChannelMessageActionName,
-} from "remoteclaw/plugin-sdk/channel-contract";
+} from "remoteclaw/plugin-sdk/channel-runtime";
+export type { DiscordConfig } from "remoteclaw/plugin-sdk/discord";
+export type { DiscordAccountConfig, DiscordActionConfig } from "remoteclaw/plugin-sdk/discord";
 export {
   hasConfiguredSecretInput,
   normalizeResolvedSecretInputString,
   normalizeSecretInputString,
-} from "remoteclaw/plugin-sdk/secret-input";
+} from "remoteclaw/plugin-sdk/config-runtime";
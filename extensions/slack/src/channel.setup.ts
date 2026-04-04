wonglaitung/openclaw_mini

refactor(slack): share plugin base config

extensions/slack/src/channel.setup.ts
@@ -1,61 +1,19 @@
-import {
-  buildChannelConfigSchema,
-  getChatChannelMeta,
-  SlackConfigSchema,
-  type ChannelPlugin,
-} from "../../../src/plugin-sdk-internal/slack.js";
+import { type ChannelPlugin } from "openclaw/plugin-sdk/slack";
 import { type ResolvedSlackAccount } from "./accounts.js";
-import { isSlackInteractiveRepliesEnabled } from "./interactive-replies.js";
-import {
-  isSlackPluginAccountConfigured,
-  slackConfigAccessors,
-  slackConfigBase,
-  slackSetupWizard,
-} from "./plugin-shared.js";
-import { slackSetupAdapter } from "./setup-core.js";
+import { createSlackSetupWizardProxy, slackSetupAdapter } from "./setup-core.js";
+import { createSlackPluginBase } from "./shared.js";
+
+async function loadSlackChannelRuntime() {
+  return await import("./channel.runtime.js");
+}
+
+const slackSetupWizard = createSlackSetupWizardProxy(async () => ({
+  slackSetupWizard: (await loadSlackChannelRuntime()).slackSetupWizard,
+}));
 
 export const slackSetupPlugin: ChannelPlugin<ResolvedSlackAccount> = {
-  id: "slack",
-  meta: {
-    ...getChatChannelMeta("slack"),
-    preferSessionLookupForAnnounceTarget: true,
-  },
-  setupWizard: slackSetupWizard,
-  capabilities: {
-    chatTypes: ["direct", "channel", "thread"],
-    reactions: true,
-    threads: true,
-    media: true,
-    nativeCommands: true,
-  },
-  agentPrompt: {
-    messageToolHints: ({ cfg, accountId }) =>
-      isSlackInteractiveRepliesEnabled({ cfg, accountId })
-        ? [
-            "- Slack interactive replies: use `[[slack_buttons: Label:value, Other:other]]` to add action buttons that route clicks back as Slack interaction system events.",
-            "- Slack selects: use `[[slack_select: Placeholder | Label:value, Other:other]]` to add a static select menu that routes the chosen value back as a Slack interaction system event.",
-          ]
-        : [
-            "- Slack interactive replies are disabled. If needed, ask to set `channels.slack.capabilities.interactiveReplies=true` (or the same under `channels.slack.accounts.<account>.capabilities`).",
-          ],
-  },
-  streaming: {
-    blockStreamingCoalesceDefaults: { minChars: 1500, idleMs: 1000 },
-  },
-  reload: { configPrefixes: ["channels.slack"] },
-  configSchema: buildChannelConfigSchema(SlackConfigSchema),
-  config: {
-    ...slackConfigBase,
-    isConfigured: (account) => isSlackPluginAccountConfigured(account),
-    describeAccount: (account) => ({
-      accountId: account.accountId,
-      name: account.name,
-      enabled: account.enabled,
-      configured: isSlackPluginAccountConfigured(account),
-      botTokenSource: account.botTokenSource,
-      appTokenSource: account.appTokenSource,
-    }),
-    ...slackConfigAccessors,
-  },
-  setup: slackSetupAdapter,
+  ...createSlackPluginBase({
+    setupWizard: slackSetupWizard,
+    setup: slackSetupAdapter,
+  }),
 };

extensions/slack/src/shared.ts
@@ -1,14 +1,18 @@
-import { formatAllowFromLowercase } from "openclaw/plugin-sdk/allow-from";
+import {
+  buildChannelConfigSchema,
+  getChatChannelMeta,
+  SlackConfigSchema,
+  type ChannelPlugin,
+} from "openclaw/plugin-sdk/slack";
+import { patchChannelConfigForAccount } from "../../../src/channels/plugins/setup-wizard-helpers.js";
+import type { OpenClawConfig } from "../../../src/config/config.js";
+import { hasConfiguredSecretInput } from "../../../src/config/types.secrets.js";
+import { formatAllowFromLowercase } from "../../../src/plugin-sdk/allow-from.js";
 import {
   createScopedAccountConfigAccessors,
   createScopedChannelConfigBase,
-} from "openclaw/plugin-sdk/channel-config-helpers";
-import {
-  formatDocsLink,
-  hasConfiguredSecretInput,
-  patchChannelConfigForAccount,
-  type OpenClawConfig,
-} from "openclaw/plugin-sdk/setup";
+} from "../../../src/plugin-sdk/channel-config-helpers.js";
+import { formatDocsLink } from "../../../src/terminal/links.js";
 import { inspectSlackAccount } from "./account-inspect.js";
 import {
   listSlackAccountIds,
@@ -16,6 +20,7 @@ import {
   resolveSlackAccount,
   type ResolvedSlackAccount,
 } from "./accounts.js";
+import { isSlackInteractiveRepliesEnabled } from "./interactive-replies.js";
 
 export const SLACK_CHANNEL = "slack" as const;
 
@@ -152,3 +157,66 @@ export const slackConfigBase = createScopedChannelConfigBase({
   defaultAccountId: resolveDefaultSlackAccountId,
   clearBaseFields: ["botToken", "appToken", "name"],
 });
+
+export function createSlackPluginBase(params: {
+  setupWizard: NonNullable<ChannelPlugin<ResolvedSlackAccount>["setupWizard"]>;
+  setup: NonNullable<ChannelPlugin<ResolvedSlackAccount>["setup"]>;
+}): Pick<
+  ChannelPlugin<ResolvedSlackAccount>,
+  | "id"
+  | "meta"
+  | "setupWizard"
+  | "capabilities"
+  | "agentPrompt"
+  | "streaming"
+  | "reload"
+  | "configSchema"
+  | "config"
+  | "setup"
+> {
+  return {
+    id: SLACK_CHANNEL,
+    meta: {
+      ...getChatChannelMeta(SLACK_CHANNEL),
+      preferSessionLookupForAnnounceTarget: true,
+    },
+    setupWizard: params.setupWizard,
+    capabilities: {
+      chatTypes: ["direct", "channel", "thread"],
+      reactions: true,
+      threads: true,
+      media: true,
+      nativeCommands: true,
+    },
+    agentPrompt: {
+      messageToolHints: ({ cfg, accountId }) =>
+        isSlackInteractiveRepliesEnabled({ cfg, accountId })
+          ? [
+              "- Slack interactive replies: use `[[slack_buttons: Label:value, Other:other]]` to add action buttons that route clicks back as Slack interaction system events.",
+              "- Slack selects: use `[[slack_select: Placeholder | Label:value, Other:other]]` to add a static select menu that routes the chosen value back as a Slack interaction system event.",
+            ]
+          : [
+              "- Slack interactive replies are disabled. If needed, ask to set `channels.slack.capabilities.interactiveReplies=true` (or the same under `channels.slack.accounts.<account>.capabilities`).",
+            ],
+    },
+    streaming: {
+      blockStreamingCoalesceDefaults: { minChars: 1500, idleMs: 1000 },
+    },
+    reload: { configPrefixes: ["channels.slack"] },
+    configSchema: buildChannelConfigSchema(SlackConfigSchema),
+    config: {
+      ...slackConfigBase,
+      isConfigured: (account) => isSlackPluginAccountConfigured(account),
+      describeAccount: (account) => ({
+        accountId: account.accountId,
+        name: account.name,
+        enabled: account.enabled,
+        configured: isSlackPluginAccountConfigured(account),
+        botTokenSource: account.botTokenSource,
+        appTokenSource: account.appTokenSource,
+      }),
+      ...slackConfigAccessors,
+    },
+    setup: params.setup,
+  };
+}

remoteclaw/remoteclaw

extensions/slack/src/channel.setup.ts
@@ -1,13 +1,7 @@
-import {
-  buildChannelConfigSchema,
-  getChatChannelMeta,
-  SlackConfigSchema,
-  type ChannelPlugin,
-} from "remoteclaw/plugin-sdk/slack";
+import { type ChannelPlugin } from "remoteclaw/plugin-sdk/slack";
 import { type ResolvedSlackAccount } from "./accounts.js";
-import { isSlackInteractiveRepliesEnabled } from "./interactive-replies.js";
 import { createSlackSetupWizardProxy, slackSetupAdapter } from "./setup-core.js";
-import { isSlackPluginAccountConfigured, slackConfigAccessors, slackConfigBase } from "./shared.js";
+import { createSlackPluginBase } from "./shared.js";
 
 async function loadSlackChannelRuntime() {
   return await import("./channel.runtime.js");
@@ -18,47 +12,8 @@ const slackSetupWizard = createSlackSetupWizardProxy(async () => ({
 }));
 
 export const slackSetupPlugin: ChannelPlugin<ResolvedSlackAccount> = {
-  id: "slack",
-  meta: {
-    ...getChatChannelMeta("slack"),
-    preferSessionLookupForAnnounceTarget: true,
-  },
-  setupWizard: slackSetupWizard,
-  capabilities: {
-    chatTypes: ["direct", "channel", "thread"],
-    reactions: true,
-    threads: true,
-    media: true,
-    nativeCommands: true,
-  },
-  agentPrompt: {
-    messageToolHints: ({ cfg, accountId }) =>
-      isSlackInteractiveRepliesEnabled({ cfg, accountId })
-        ? [
-            "- Slack interactive replies: use `[[slack_buttons: Label:value, Other:other]]` to add action buttons that route clicks back as Slack interaction system events.",
-            "- Slack selects: use `[[slack_select: Placeholder | Label:value, Other:other]]` to add a static select menu that routes the chosen value back as a Slack interaction system event.",
-          ]
-        : [
-            "- Slack interactive replies are disabled. If needed, ask to set `channels.slack.capabilities.interactiveReplies=true` (or the same under `channels.slack.accounts.<account>.capabilities`).",
-          ],
-  },
-  streaming: {
-    blockStreamingCoalesceDefaults: { minChars: 1500, idleMs: 1000 },
-  },
-  reload: { configPrefixes: ["channels.slack"] },
-  configSchema: buildChannelConfigSchema(SlackConfigSchema),
-  config: {
-    ...slackConfigBase,
-    isConfigured: (account) => isSlackPluginAccountConfigured(account),
-    describeAccount: (account) => ({
-      accountId: account.accountId,
-      name: account.name,
-      enabled: account.enabled,
-      configured: isSlackPluginAccountConfigured(account),
-      botTokenSource: account.botTokenSource,
-      appTokenSource: account.appTokenSource,
-    }),
-    ...slackConfigAccessors,
-  },
-  setup: slackSetupAdapter,
+  ...createSlackPluginBase({
+    setupWizard: slackSetupWizard,
+    setup: slackSetupAdapter,
+  }),
 };

extensions/slack/src/shared.ts
@@ -1,3 +1,9 @@
+import {
+  buildChannelConfigSchema,
+  getChatChannelMeta,
+  SlackConfigSchema,
+  type ChannelPlugin,
+} from "remoteclaw/plugin-sdk/slack";
 import { patchChannelConfigForAccount } from "../../../src/channels/plugins/setup-wizard-helpers.js";
 import type { RemoteClawConfig } from "../../../src/config/config.js";
 import { hasConfiguredSecretInput } from "../../../src/config/types.secrets.js";
@@ -14,6 +20,7 @@ import {
   resolveSlackAccount,
   type ResolvedSlackAccount,
 } from "./accounts.js";
+import { isSlackInteractiveRepliesEnabled } from "./interactive-replies.js";
 
 export const SLACK_CHANNEL = "slack" as const;
 
@@ -150,3 +157,66 @@ export const slackConfigBase = createScopedChannelConfigBase({
   defaultAccountId: resolveDefaultSlackAccountId,
   clearBaseFields: ["botToken", "appToken", "name"],
 });
+
+export function createSlackPluginBase(params: {
+  setupWizard: NonNullable<ChannelPlugin<ResolvedSlackAccount>["setupWizard"]>;
+  setup: NonNullable<ChannelPlugin<ResolvedSlackAccount>["setup"]>;
+}): Pick<
+  ChannelPlugin<ResolvedSlackAccount>,
+  | "id"
+  | "meta"
+  | "setupWizard"
+  | "capabilities"
+  | "agentPrompt"
+  | "streaming"
+  | "reload"
+  | "configSchema"
+  | "config"
+  | "setup"
+> {
+  return {
+    id: SLACK_CHANNEL,
+    meta: {
+      ...getChatChannelMeta(SLACK_CHANNEL),
+      preferSessionLookupForAnnounceTarget: true,
+    },
+    setupWizard: params.setupWizard,
+    capabilities: {
+      chatTypes: ["direct", "channel", "thread"],
+      reactions: true,
+      threads: true,
+      media: true,
+      nativeCommands: true,
+    },
+    agentPrompt: {
+      messageToolHints: ({ cfg, accountId }) =>
+        isSlackInteractiveRepliesEnabled({ cfg, accountId })
+          ? [
+              "- Slack interactive replies: use `[[slack_buttons: Label:value, Other:other]]` to add action buttons that route clicks back as Slack interaction system events.",
+              "- Slack selects: use `[[slack_select: Placeholder | Label:value, Other:other]]` to add a static select menu that routes the chosen value back as a Slack interaction system event.",
+            ]
+          : [
+              "- Slack interactive replies are disabled. If needed, ask to set `channels.slack.capabilities.interactiveReplies=true` (or the same under `channels.slack.accounts.<account>.capabilities`).",
+            ],
+    },
+    streaming: {
+      blockStreamingCoalesceDefaults: { minChars: 1500, idleMs: 1000 },
+    },
+    reload: { configPrefixes: ["channels.slack"] },
+    configSchema: buildChannelConfigSchema(SlackConfigSchema),
+    config: {
+      ...slackConfigBase,
+      isConfigured: (account) => isSlackPluginAccountConfigured(account),
+      describeAccount: (account) => ({
+        accountId: account.accountId,
+        name: account.name,
+        enabled: account.enabled,
+        configured: isSlackPluginAccountConfigured(account),
+        botTokenSource: account.botTokenSource,
+        appTokenSource: account.appTokenSource,
+      }),
+      ...slackConfigAccessors,
+    },
+    setup: params.setup,
+  };
+}
wonglaitung/openclaw_mini

Plugins: finish signal SDK internalization

extensions/signal/src/runtime-api.ts
@@ -1,0 +1,1 @@
+export * from "openclaw/plugin-sdk/signal";

remoteclaw/remoteclaw

extensions/signal/src/runtime-api.ts
@@ -1,4 +1,1 @@
-// Private runtime barrel for the bundled Signal extension.
-// Keep this barrel thin and aligned with the local extension surface.
-
-export * from "../../../src/plugin-sdk/signal.js";
+export * from "remoteclaw/plugin-sdk/signal";
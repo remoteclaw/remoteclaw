d3588ee36feeb55c6862281068dcc19d4a8111c7
Vincent Koc <vincentkoc@ieee.org>
2026-03-17 23:05:40 -0700

    Plugins: finish signal SDK internalization

--- extensions/signal/src/runtime-api.ts
+++ extensions/signal/src/runtime-api.ts
@@ -1,4 +1,1 @@
-// Private runtime barrel for the bundled Signal extension.
-// Keep this barrel thin and aligned with the local extension surface.
-
-export * from "../../../src/plugin-sdk/signal.js";
+export * from "remoteclaw/plugin-sdk/signal";

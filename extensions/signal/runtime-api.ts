wonglaitung/openclaw_mini

Plugins: internalize signal SDK imports

extensions/signal/runtime-api.ts
@@ -1,1 +1,2 @@
-export * from "./src/index.js";
+export * from "openclaw/plugin-sdk/signal";
+export * from "openclaw/plugin-sdk/signal-core";

remoteclaw/remoteclaw

extensions/signal/runtime-api.ts
@@ -1,1 +1,2 @@
-export * from "./src/runtime-api.js";
+export * from "remoteclaw/plugin-sdk/signal";
+export * from "remoteclaw/plugin-sdk/signal-core";
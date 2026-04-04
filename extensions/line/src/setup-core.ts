wonglaitung/openclaw_mini

refactor: enforce plugin boundary seams

extensions/line/src/setup-core.ts
@@ -1,7 +1,11 @@
-import type { ChannelSetupAdapter, OpenClawConfig } from "openclaw/plugin-sdk/setup";
-import { DEFAULT_ACCOUNT_ID } from "openclaw/plugin-sdk/setup";
-import { normalizeAccountId, resolveLineAccount } from "../../../src/line/accounts.js";
-import type { LineConfig } from "../../../src/line/types.js";
+import {
+  DEFAULT_ACCOUNT_ID,
+  normalizeAccountId,
+  resolveLineAccount,
+  type ChannelSetupAdapter,
+  type LineConfig,
+  type OpenClawConfig,
+} from "openclaw/plugin-sdk/line-core";
 
 const channel = "line" as const;
 
@@ -154,4 +158,4 @@ export const lineSetupAdapter: ChannelSetupAdapter = {
   },
 };
 
-export { listLineAccountIds } from "../../../src/line/accounts.js";
+export { listLineAccountIds } from "openclaw/plugin-sdk/line-core";

remoteclaw/remoteclaw

extensions/line/src/setup-core.ts
@@ -1,11 +1,11 @@
-import type { ChannelSetupAdapter, RemoteClawConfig } from "remoteclaw/plugin-sdk/setup";
 import {
   DEFAULT_ACCOUNT_ID,
-  listLineAccountIds,
   normalizeAccountId,
   resolveLineAccount,
+  type ChannelSetupAdapter,
   type LineConfig,
-} from "../runtime-api.js";
+  type RemoteClawConfig,
+} from "remoteclaw/plugin-sdk/line-core";
 
 const channel = "line" as const;
 
@@ -158,4 +158,4 @@ export const lineSetupAdapter: ChannelSetupAdapter = {
   },
 };
 
-export { listLineAccountIds };
+export { listLineAccountIds } from "remoteclaw/plugin-sdk/line-core";
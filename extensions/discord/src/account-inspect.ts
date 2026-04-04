wonglaitung/openclaw_mini

Plugins: internalize discord SDK imports

extensions/discord/src/account-inspect.ts
@@ -1,18 +1,16 @@
 import {
-  DEFAULT_ACCOUNT_ID,
-  normalizeAccountId,
-  type OpenClawConfig,
-} from "openclaw/plugin-sdk/account-resolution";
-import {
-  hasConfiguredSecretInput,
-  normalizeSecretInputString,
-} from "openclaw/plugin-sdk/config-runtime";
-import type { DiscordAccountConfig } from "openclaw/plugin-sdk/discord";
-import {
   mergeDiscordAccountConfig,
   resolveDefaultDiscordAccountId,
   resolveDiscordAccountConfig,
 } from "./accounts.js";
+import {
+  DEFAULT_ACCOUNT_ID,
+  normalizeAccountId,
+  hasConfiguredSecretInput,
+  normalizeSecretInputString,
+  type OpenClawConfig,
+  type DiscordAccountConfig,
+} from "./runtime-api.js";
 
 export type DiscordCredentialStatus = "available" | "configured_unavailable" | "missing";
 

extensions/discord/src/runtime-api.ts
@@ -1,0 +1,43 @@
+export {
+  buildComputedAccountStatusSnapshot,
+  buildTokenChannelStatusSummary,
+  listDiscordDirectoryGroupsFromConfig,
+  listDiscordDirectoryPeersFromConfig,
+  PAIRING_APPROVED_MESSAGE,
+  projectCredentialSnapshotFields,
+  resolveConfiguredFromCredentialStatuses,
+} from "openclaw/plugin-sdk/discord";
+export {
+  buildChannelConfigSchema,
+  getChatChannelMeta,
+  jsonResult,
+  readNumberParam,
+  readStringArrayParam,
+  readStringParam,
+  type ActionGate,
+  type ChannelPlugin,
+  type OpenClawConfig,
+} from "openclaw/plugin-sdk/discord-core";
+export { DiscordConfigSchema } from "openclaw/plugin-sdk/discord-core";
+export { readBooleanParam } from "openclaw/plugin-sdk/boolean-param";
+export {
+  createAccountActionGate,
+  createAccountListHelpers,
+  DEFAULT_ACCOUNT_ID,
+  normalizeAccountId,
+  resolveAccountEntry,
+} from "openclaw/plugin-sdk/account-resolution";
+export type {
+  ChannelMessageActionAdapter,
+  ChannelMessageActionName,
+} from "openclaw/plugin-sdk/channel-runtime";
+export { withNormalizedTimestamp } from "../../../src/agents/date-time.js";
+export { assertMediaNotDataUrl } from "../../../src/agents/sandbox-paths.js";
+export { parseAvailableTags, readReactionParams } from "openclaw/plugin-sdk/discord-core";
+export { resolvePollMaxSelections } from "../../../src/polls.js";
+export type { DiscordAccountConfig, DiscordActionConfig } from "../config/types.js";
+export {
+  hasConfiguredSecretInput,
+  normalizeResolvedSecretInputString,
+  normalizeSecretInputString,
+} from "../../../src/config/types.secrets.js";

remoteclaw/remoteclaw

extensions/discord/src/account-inspect.ts
@@ -1,18 +1,16 @@
 import {
-  hasConfiguredSecretInput,
-  normalizeSecretInputString,
-} from "../../../src/config/types.secrets.js";
-import {
-  DEFAULT_ACCOUNT_ID,
-  normalizeAccountId,
-  type RemoteClawConfig,
-  type DiscordAccountConfig,
-} from "../../../src/plugin-sdk-internal/discord.js";
-import {
   mergeDiscordAccountConfig,
   resolveDefaultDiscordAccountId,
   resolveDiscordAccountConfig,
 } from "./accounts.js";
+import {
+  DEFAULT_ACCOUNT_ID,
+  normalizeAccountId,
+  hasConfiguredSecretInput,
+  normalizeSecretInputString,
+  type RemoteClawConfig,
+  type DiscordAccountConfig,
+} from "./runtime-api.js";
 
 export type DiscordCredentialStatus = "available" | "configured_unavailable" | "missing";
 

extensions/discord/src/runtime-api.ts
@@ -1,10 +1,12 @@
 export {
   buildComputedAccountStatusSnapshot,
   buildTokenChannelStatusSummary,
+  listDiscordDirectoryGroupsFromConfig,
+  listDiscordDirectoryPeersFromConfig,
   PAIRING_APPROVED_MESSAGE,
   projectCredentialSnapshotFields,
   resolveConfiguredFromCredentialStatuses,
-} from "remoteclaw/plugin-sdk/channel-runtime";
+} from "remoteclaw/plugin-sdk/discord";
 export {
   buildChannelConfigSchema,
   getChatChannelMeta,
@@ -12,7 +14,6 @@ export {
   readNumberParam,
   readStringArrayParam,
   readStringParam,
-  resolvePollMaxSelections,
   type ActionGate,
   type ChannelPlugin,
   type RemoteClawConfig,
@@ -20,32 +21,23 @@ export {
 export { DiscordConfigSchema } from "remoteclaw/plugin-sdk/discord-core";
 export { readBooleanParam } from "remoteclaw/plugin-sdk/boolean-param";
 export {
-  assertMediaNotDataUrl,
-  parseAvailableTags,
-  readReactionParams,
-  withNormalizedTimestamp,
-} from "remoteclaw/plugin-sdk/discord-core";
-export {
-  createHybridChannelConfigAdapter,
-  createScopedChannelConfigAdapter,
-  createScopedAccountConfigAccessors,
-  createScopedChannelConfigBase,
-  createTopLevelChannelConfigAdapter,
-} from "remoteclaw/plugin-sdk/channel-config-helpers";
-export {
   createAccountActionGate,
   createAccountListHelpers,
-} from "remoteclaw/plugin-sdk/account-helpers";
-export { DEFAULT_ACCOUNT_ID, normalizeAccountId } from "remoteclaw/plugin-sdk/account-id";
-export { resolveAccountEntry } from "remoteclaw/plugin-sdk/routing";
+  DEFAULT_ACCOUNT_ID,
+  normalizeAccountId,
+  resolveAccountEntry,
+} from "remoteclaw/plugin-sdk/account-resolution";
 export type {
   ChannelMessageActionAdapter,
   ChannelMessageActionName,
 } from "remoteclaw/plugin-sdk/channel-runtime";
-export type { DiscordConfig } from "remoteclaw/plugin-sdk/discord";
-export type { DiscordAccountConfig, DiscordActionConfig } from "remoteclaw/plugin-sdk/discord";
+export { withNormalizedTimestamp } from "../../../src/agents/date-time.js";
+export { assertMediaNotDataUrl } from "../../../src/agents/sandbox-paths.js";
+export { parseAvailableTags, readReactionParams } from "remoteclaw/plugin-sdk/discord-core";
+export { resolvePollMaxSelections } from "../../../src/polls.js";
+export type { DiscordAccountConfig, DiscordActionConfig } from "../config/types.js";
 export {
   hasConfiguredSecretInput,
   normalizeResolvedSecretInputString,
   normalizeSecretInputString,
-} from "remoteclaw/plugin-sdk/config-runtime";
+} from "../../../src/config/types.secrets.js";
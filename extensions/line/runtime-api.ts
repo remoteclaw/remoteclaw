wonglaitung/openclaw_mini

Plugins: internalize medium extension SDK imports

extensions/line/runtime-api.ts
@@ -1,0 +1,1 @@
+export * from "openclaw/plugin-sdk/line-core";

extensions/line/src/group-policy.ts
@@ -1,5 +1,5 @@
 import { resolveChannelGroupRequireMention } from "openclaw/plugin-sdk/channel-policy";
-import { resolveExactLineGroupConfigKey, type OpenClawConfig } from "openclaw/plugin-sdk/line-core";
+import { resolveExactLineGroupConfigKey, type OpenClawConfig } from "../runtime-api.js";
 
 type LineGroupContext = {
   cfg: OpenClawConfig;

remoteclaw/remoteclaw

extensions/line/runtime-api.ts
@@ -1,13 +1,1 @@
-// Private runtime barrel for the bundled LINE extension.
-// Keep this barrel thin and aligned with the local extension surface.
-
-export * from "remoteclaw/plugin-sdk/line";
-export {
-  DEFAULT_ACCOUNT_ID,
-  formatDocsLink,
-  resolveExactLineGroupConfigKey,
-  setSetupChannelEnabled,
-  splitSetupEntries,
-  type ChannelSetupDmPolicy,
-  type ChannelSetupWizard,
-} from "remoteclaw/plugin-sdk/line-core";
+export * from "remoteclaw/plugin-sdk/line-core";

extensions/line/src/group-policy.ts
@@ -1,5 +1,5 @@
 import { resolveChannelGroupRequireMention } from "remoteclaw/plugin-sdk/channel-policy";
-import { resolveExactLineGroupConfigKey, type RemoteClawConfig } from "remoteclaw/plugin-sdk/line-core";
+import { resolveExactLineGroupConfigKey, type RemoteClawConfig } from "../runtime-api.js";
 
 type LineGroupContext = {
   cfg: RemoteClawConfig;
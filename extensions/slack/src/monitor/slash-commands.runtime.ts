wonglaitung/openclaw_mini

build: tighten lazy runtime boundaries

extensions/slack/src/monitor/slash-commands.runtime.ts
@@ -1,7 +1,47 @@
-export {
-  buildCommandTextFromArgs,
-  findCommandByNativeName,
-  listNativeCommandSpecsForConfig,
-  parseCommandArgs,
-  resolveCommandArgMenu,
+import {
+  buildCommandTextFromArgs as buildCommandTextFromArgsImpl,
+  findCommandByNativeName as findCommandByNativeNameImpl,
+  listNativeCommandSpecsForConfig as listNativeCommandSpecsForConfigImpl,
+  parseCommandArgs as parseCommandArgsImpl,
+  resolveCommandArgMenu as resolveCommandArgMenuImpl,
 } from "openclaw/plugin-sdk/reply-runtime";
+
+type BuildCommandTextFromArgs =
+  typeof import("openclaw/plugin-sdk/reply-runtime").buildCommandTextFromArgs;
+type FindCommandByNativeName =
+  typeof import("openclaw/plugin-sdk/reply-runtime").findCommandByNativeName;
+type ListNativeCommandSpecsForConfig =
+  typeof import("openclaw/plugin-sdk/reply-runtime").listNativeCommandSpecsForConfig;
+type ParseCommandArgs = typeof import("openclaw/plugin-sdk/reply-runtime").parseCommandArgs;
+type ResolveCommandArgMenu =
+  typeof import("openclaw/plugin-sdk/reply-runtime").resolveCommandArgMenu;
+
+export function buildCommandTextFromArgs(
+  ...args: Parameters<BuildCommandTextFromArgs>
+): ReturnType<BuildCommandTextFromArgs> {
+  return buildCommandTextFromArgsImpl(...args);
+}
+
+export function findCommandByNativeName(
+  ...args: Parameters<FindCommandByNativeName>
+): ReturnType<FindCommandByNativeName> {
+  return findCommandByNativeNameImpl(...args);
+}
+
+export function listNativeCommandSpecsForConfig(
+  ...args: Parameters<ListNativeCommandSpecsForConfig>
+): ReturnType<ListNativeCommandSpecsForConfig> {
+  return listNativeCommandSpecsForConfigImpl(...args);
+}
+
+export function parseCommandArgs(
+  ...args: Parameters<ParseCommandArgs>
+): ReturnType<ParseCommandArgs> {
+  return parseCommandArgsImpl(...args);
+}
+
+export function resolveCommandArgMenu(
+  ...args: Parameters<ResolveCommandArgMenu>
+): ReturnType<ResolveCommandArgMenu> {
+  return resolveCommandArgMenuImpl(...args);
+}

extensions/slack/src/monitor/slash-skill-commands.runtime.ts
@@ -1,1 +1,10 @@
-export { listSkillCommandsForAgents } from "openclaw/plugin-sdk/reply-runtime";
+import { listSkillCommandsForAgents as listSkillCommandsForAgentsImpl } from "openclaw/plugin-sdk/reply-runtime";
+
+type ListSkillCommandsForAgents =
+  typeof import("openclaw/plugin-sdk/reply-runtime").listSkillCommandsForAgents;
+
+export function listSkillCommandsForAgents(
+  ...args: Parameters<ListSkillCommandsForAgents>
+): ReturnType<ListSkillCommandsForAgents> {
+  return listSkillCommandsForAgentsImpl(...args);
+}

remoteclaw/remoteclaw

extensions/slack/src/monitor/slash-commands.runtime.ts
@@ -1,7 +1,47 @@
-export {
-  buildCommandTextFromArgs,
-  findCommandByNativeName,
-  listNativeCommandSpecsForConfig,
-  parseCommandArgs,
-  resolveCommandArgMenu,
-} from "../../../../src/auto-reply/commands-registry.js";
+import {
+  buildCommandTextFromArgs as buildCommandTextFromArgsImpl,
+  findCommandByNativeName as findCommandByNativeNameImpl,
+  listNativeCommandSpecsForConfig as listNativeCommandSpecsForConfigImpl,
+  parseCommandArgs as parseCommandArgsImpl,
+  resolveCommandArgMenu as resolveCommandArgMenuImpl,
+} from "remoteclaw/plugin-sdk/reply-runtime";
+
+type BuildCommandTextFromArgs =
+  typeof import("remoteclaw/plugin-sdk/reply-runtime").buildCommandTextFromArgs;
+type FindCommandByNativeName =
+  typeof import("remoteclaw/plugin-sdk/reply-runtime").findCommandByNativeName;
+type ListNativeCommandSpecsForConfig =
+  typeof import("remoteclaw/plugin-sdk/reply-runtime").listNativeCommandSpecsForConfig;
+type ParseCommandArgs = typeof import("remoteclaw/plugin-sdk/reply-runtime").parseCommandArgs;
+type ResolveCommandArgMenu =
+  typeof import("remoteclaw/plugin-sdk/reply-runtime").resolveCommandArgMenu;
+
+export function buildCommandTextFromArgs(
+  ...args: Parameters<BuildCommandTextFromArgs>
+): ReturnType<BuildCommandTextFromArgs> {
+  return buildCommandTextFromArgsImpl(...args);
+}
+
+export function findCommandByNativeName(
+  ...args: Parameters<FindCommandByNativeName>
+): ReturnType<FindCommandByNativeName> {
+  return findCommandByNativeNameImpl(...args);
+}
+
+export function listNativeCommandSpecsForConfig(
+  ...args: Parameters<ListNativeCommandSpecsForConfig>
+): ReturnType<ListNativeCommandSpecsForConfig> {
+  return listNativeCommandSpecsForConfigImpl(...args);
+}
+
+export function parseCommandArgs(
+  ...args: Parameters<ParseCommandArgs>
+): ReturnType<ParseCommandArgs> {
+  return parseCommandArgsImpl(...args);
+}
+
+export function resolveCommandArgMenu(
+  ...args: Parameters<ResolveCommandArgMenu>
+): ReturnType<ResolveCommandArgMenu> {
+  return resolveCommandArgMenuImpl(...args);
+}

extensions/slack/src/monitor/slash-skill-commands.runtime.ts
@@ -1,0 +1,10 @@
+import { listSkillCommandsForAgents as listSkillCommandsForAgentsImpl } from "remoteclaw/plugin-sdk/reply-runtime";
+
+type ListSkillCommandsForAgents =
+  typeof import("remoteclaw/plugin-sdk/reply-runtime").listSkillCommandsForAgents;
+
+export function listSkillCommandsForAgents(
+  ...args: Parameters<ListSkillCommandsForAgents>
+): ReturnType<ListSkillCommandsForAgents> {
+  return listSkillCommandsForAgentsImpl(...args);
+}
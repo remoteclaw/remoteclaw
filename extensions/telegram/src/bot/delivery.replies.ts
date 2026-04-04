wonglaitung/openclaw_mini

Format: apply import ordering cleanup

extensions/telegram/src/bot/delivery.replies.ts
@@ -1,8 +1,6 @@
+import { type Bot, GrammyError, InputFile } from "grammy";
 import type { ReplyToMode } from "openclaw/plugin-sdk/config-runtime";
 import type { MarkdownTableMode } from "openclaw/plugin-sdk/config-runtime";
-import type { ReplyPayload } from "openclaw/plugin-sdk/reply-runtime";
-import type { RuntimeEnv } from "openclaw/plugin-sdk/runtime-env";
-import { type Bot, GrammyError, InputFile } from "grammy";
 import { fireAndForgetHook } from "openclaw/plugin-sdk/hook-runtime";
 import { createInternalHookEvent, triggerInternalHook } from "openclaw/plugin-sdk/hook-runtime";
 import {
@@ -15,7 +13,9 @@ import { formatErrorMessage } from "openclaw/plugin-sdk/infra-runtime";
 import { buildOutboundMediaLoadOptions } from "openclaw/plugin-sdk/media-runtime";
 import { isGifMedia, kindFromMime } from "openclaw/plugin-sdk/media-runtime";
 import { getGlobalHookRunner } from "openclaw/plugin-sdk/plugin-runtime";
+import type { ReplyPayload } from "openclaw/plugin-sdk/reply-runtime";
 import { chunkMarkdownTextWithMode, type ChunkMode } from "openclaw/plugin-sdk/reply-runtime";
+import type { RuntimeEnv } from "openclaw/plugin-sdk/runtime-env";
 import { danger, logVerbose } from "openclaw/plugin-sdk/runtime-env";
 import { loadWebMedia } from "openclaw/plugin-sdk/web-media";
 import type { TelegramInlineButtons } from "../button-types.js";

remoteclaw/remoteclaw

extensions/telegram/src/bot/delivery.replies.ts
@@ -1,8 +1,6 @@
+import { type Bot, GrammyError, InputFile } from "grammy";
 import type { ReplyToMode } from "remoteclaw/plugin-sdk/config-runtime";
 import type { MarkdownTableMode } from "remoteclaw/plugin-sdk/config-runtime";
-import type { ReplyPayload } from "remoteclaw/plugin-sdk/reply-runtime";
-import type { RuntimeEnv } from "remoteclaw/plugin-sdk/runtime-env";
-import { type Bot, GrammyError, InputFile } from "grammy";
 import { fireAndForgetHook } from "remoteclaw/plugin-sdk/hook-runtime";
 import { createInternalHookEvent, triggerInternalHook } from "remoteclaw/plugin-sdk/hook-runtime";
 import {
@@ -15,7 +13,9 @@ import { formatErrorMessage } from "remoteclaw/plugin-sdk/infra-runtime";
 import { buildOutboundMediaLoadOptions } from "remoteclaw/plugin-sdk/media-runtime";
 import { isGifMedia, kindFromMime } from "remoteclaw/plugin-sdk/media-runtime";
 import { getGlobalHookRunner } from "remoteclaw/plugin-sdk/plugin-runtime";
+import type { ReplyPayload } from "remoteclaw/plugin-sdk/reply-runtime";
 import { chunkMarkdownTextWithMode, type ChunkMode } from "remoteclaw/plugin-sdk/reply-runtime";
+import type { RuntimeEnv } from "remoteclaw/plugin-sdk/runtime-env";
 import { danger, logVerbose } from "remoteclaw/plugin-sdk/runtime-env";
 import { loadWebMedia } from "remoteclaw/plugin-sdk/web-media";
 import type { TelegramInlineButtons } from "../button-types.js";
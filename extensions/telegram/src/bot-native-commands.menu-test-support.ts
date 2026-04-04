wonglaitung/openclaw_mini

refactor: enforce scoped plugin sdk imports

extensions/telegram/src/bot-native-commands.menu-test-support.ts
@@ -1,4 +1,4 @@
-import type { RuntimeEnv } from "openclaw/plugin-sdk";
+import type { RuntimeEnv } from "openclaw/plugin-sdk/runtime-env";
 import type { OpenClawConfig } from "openclaw/plugin-sdk/telegram";
 import { expect, vi } from "vitest";
 import {

remoteclaw/remoteclaw

extensions/telegram/src/bot-native-commands.menu-test-support.ts
@@ -1,4 +1,4 @@
-import type { RuntimeEnv } from "remoteclaw/plugin-sdk";
+import type { RuntimeEnv } from "remoteclaw/plugin-sdk/runtime-env";
 import type { RemoteClawConfig } from "remoteclaw/plugin-sdk/telegram";
 import { expect, vi } from "vitest";
 import {
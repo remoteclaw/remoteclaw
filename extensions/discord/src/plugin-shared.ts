wonglaitung/openclaw_mini

refactor: restore public sdk seams after rebase

extensions/discord/src/plugin-shared.ts
@@ -1,9 +1,9 @@
+import type { OpenClawConfig } from "openclaw/plugin-sdk/account-resolution";
 import { formatAllowFromLowercase } from "openclaw/plugin-sdk/allow-from";
 import {
   createScopedAccountConfigAccessors,
   createScopedChannelConfigBase,
 } from "openclaw/plugin-sdk/channel-config-helpers";
-import type { OpenClawConfig } from "../../../src/config/config.js";
 import { inspectDiscordAccount } from "./account-inspect.js";
 import {
   listDiscordAccountIds,

extensions/imessage/src/accounts.ts
@@ -1,10 +1,10 @@
-import type { OpenClawConfig } from "../../../src/config/config.js";
-import type { IMessageAccountConfig } from "../../../src/config/types.js";
 import {
   createAccountListHelpers,
   normalizeAccountId,
   resolveAccountEntry,
-} from "../../../src/plugin-sdk-internal/accounts.js";
+  type OpenClawConfig,
+} from "openclaw/plugin-sdk/account-resolution";
+import type { IMessageAccountConfig } from "openclaw/plugin-sdk/imessage";
 
 export type ResolvedIMessageAccount = {
   accountId: string;

remoteclaw/remoteclaw

extensions/discord/src/plugin-shared.ts
@@ -1,9 +1,9 @@
+import type { RemoteClawConfig } from "remoteclaw/plugin-sdk/account-resolution";
 import { formatAllowFromLowercase } from "remoteclaw/plugin-sdk/allow-from";
 import {
   createScopedAccountConfigAccessors,
   createScopedChannelConfigBase,
 } from "remoteclaw/plugin-sdk/channel-config-helpers";
-import type { RemoteClawConfig } from "../../../src/config/config.js";
 import { inspectDiscordAccount } from "./account-inspect.js";
 import {
   listDiscordAccountIds,

extensions/imessage/src/accounts.ts
@@ -1,10 +1,10 @@
-import type { RemoteClawConfig } from "../../../src/config/config.js";
-import type { IMessageAccountConfig } from "../../../src/config/types.js";
 import {
   createAccountListHelpers,
   normalizeAccountId,
   resolveAccountEntry,
-} from "../../../src/plugin-sdk-internal/accounts.js";
+  type RemoteClawConfig,
+} from "remoteclaw/plugin-sdk/account-resolution";
+import type { IMessageAccountConfig } from "remoteclaw/plugin-sdk/imessage";
 
 export type ResolvedIMessageAccount = {
   accountId: string;
@@ -25,10 +25,7 @@ function resolveAccountConfig(
   return resolveAccountEntry(cfg.channels?.imessage?.accounts, accountId);
 }
 
-function mergeIMessageAccountConfig(
-  cfg: RemoteClawConfig,
-  accountId: string,
-): IMessageAccountConfig {
+function mergeIMessageAccountConfig(cfg: RemoteClawConfig, accountId: string): IMessageAccountConfig {
   const { accounts: _ignored, ...base } = (cfg.channels?.imessage ??
     {}) as IMessageAccountConfig & { accounts?: unknown };
   const account = resolveAccountConfig(cfg, accountId) ?? {};
Nachx639/clawdbot

test: trim plugin-sdk import-heavy startup

src/plugin-sdk/index.bundle.test.ts
@@ -12,58 +12,55 @@ const bundledRepresentativeEntrypoints = [
   "matrix-runtime-heavy",
   "windows-spawn",
 ] as const;
-
-function buildBundledCoverageEntrySources() {
-  const allEntrySources = buildPluginSdkEntrySources();
-  return Object.fromEntries(
-    bundledRepresentativeEntrypoints.map((entry) => [entry, allEntrySources[entry]]),
-  );
-}
+const bundledCoverageEntrySources = buildPluginSdkEntrySources(bundledRepresentativeEntrypoints);
 
 describe("plugin-sdk bundled exports", () => {
   it("emits importable bundled subpath entries", { timeout: 120_000 }, async () => {
-    const bundleTempRoot = path.join(process.cwd(), ".tmp");
+    const bundleTempRoot = path.join(
+      process.cwd(),
+      "node_modules",
+      ".cache",
+      "openclaw-plugin-sdk-build",
+    );
     await fs.mkdir(bundleTempRoot, { recursive: true });
-    const outDir = await fs.mkdtemp(path.join(bundleTempRoot, "openclaw-plugin-sdk-build-"));
+    const outDir = path.join(bundleTempRoot, "bundle");
+    await fs.rm(outDir, { recursive: true, force: true });
+    await fs.mkdir(outDir, { recursive: true });
 
-    try {
-      const { build } = await import(tsdownModuleUrl);
-      await build({
-        clean: false,
-        config: false,
-        dts: false,
-        // Full plugin-sdk coverage belongs to `pnpm build`, package contract
-        // guardrails, and `subpaths.test.ts`. This file only keeps the expensive
-        // bundler path honest across representative entrypoint families.
-        entry: buildBundledCoverageEntrySources(),
-        env: { NODE_ENV: "production" },
-        fixedExtension: false,
-        logLevel: "error",
-        outDir,
-        platform: "node",
-      });
+    const { build } = await import(tsdownModuleUrl);
+    await build({
+      clean: false,
+      config: false,
+      dts: false,
+      // Full plugin-sdk coverage belongs to `pnpm build`, package contract
+      // guardrails, and `subpaths.test.ts`. This file only keeps the expensive
+      // bundler path honest across representative entrypoint families.
+      entry: bundledCoverageEntrySources,
+      env: { NODE_ENV: "production" },
+      fixedExtension: false,
+      logLevel: "error",
+      outDir,
+      platform: "node",
+    });
 
-      expect(pluginSdkEntrypoints.length).toBeGreaterThan(bundledRepresentativeEntrypoints.length);
-      await Promise.all(
-        bundledRepresentativeEntrypoints.map(async (entry) => {
-          await expect(fs.stat(path.join(outDir, `${entry}.js`))).resolves.toBeTruthy();
-        }),
-      );
+    expect(pluginSdkEntrypoints.length).toBeGreaterThan(bundledRepresentativeEntrypoints.length);
+    await Promise.all(
+      bundledRepresentativeEntrypoints.map(async (entry) => {
+        await expect(fs.stat(path.join(outDir, `${entry}.js`))).resolves.toBeTruthy();
+      }),
+    );
 
-      // Export list and package-specifier coverage already live in
-      // package-contract-guardrails.test.ts and subpaths.test.ts. Keep this file
-      // focused on the expensive part: can tsdown emit working bundle artifacts?
-      const importResults = await Promise.all(
-        bundledRepresentativeEntrypoints.map(async (entry) => [
-          entry,
-          typeof (await import(pathToFileURL(path.join(outDir, `${entry}.js`)).href)),
-        ]),
-      );
-      expect(Object.fromEntries(importResults)).toEqual(
-        Object.fromEntries(bundledRepresentativeEntrypoints.map((entry) => [entry, "object"])),
-      );
-    } finally {
-      await fs.rm(outDir, { recursive: true, force: true });
-    }
+    // Export list and package-specifier coverage already live in
+    // package-contract-guardrails.test.ts and subpaths.test.ts. Keep this file
+    // focused on the expensive part: can tsdown emit working bundle artifacts?
+    const importResults = await Promise.all(
+      bundledRepresentativeEntrypoints.map(async (entry) => [
+        entry,
+        typeof (await import(pathToFileURL(path.join(outDir, `${entry}.js`)).href)),
+      ]),
+    );
+    expect(Object.fromEntries(importResults)).toEqual(
+      Object.fromEntries(bundledRepresentativeEntrypoints.map((entry) => [entry, "object"])),
+    );
   });
 });

remoteclaw/remoteclaw

src/plugin-sdk/index.bundle.test.ts
@@ -1,6 +1,5 @@
 import fs from "node:fs/promises";
 import { createRequire } from "node:module";
-import os from "node:os";
 import path from "node:path";
 import { pathToFileURL } from "node:url";
 import { describe, expect, it } from "vitest";
@@ -9,68 +8,59 @@ import { buildPluginSdkEntrySources, pluginSdkEntrypoints } from "./entrypoints.
 const require = createRequire(import.meta.url);
 const tsdownModuleUrl = pathToFileURL(require.resolve("tsdown")).href;
 const bundledRepresentativeEntrypoints = [
-  "index",
-  "runtime",
   "channel-runtime",
-  "provider-setup",
   "matrix-runtime-heavy",
   "windows-spawn",
 ] as const;
-
-function buildBundledCoverageEntrySources() {
-  const allEntrySources = buildPluginSdkEntrySources();
-  return Object.fromEntries(
-    bundledRepresentativeEntrypoints.map((entry) => [entry, allEntrySources[entry]]),
-  );
-}
+const bundledCoverageEntrySources = buildPluginSdkEntrySources(bundledRepresentativeEntrypoints);
 
 describe("plugin-sdk bundled exports", () => {
   it("emits importable bundled subpath entries", { timeout: 120_000 }, async () => {
-    const outDir = await fs.mkdtemp(path.join(os.tmpdir(), "remoteclaw-plugin-sdk-build-"));
+    const bundleTempRoot = path.join(
+      process.cwd(),
+      "node_modules",
+      ".cache",
+      "remoteclaw-plugin-sdk-build",
+    );
+    await fs.mkdir(bundleTempRoot, { recursive: true });
+    const outDir = path.join(bundleTempRoot, "bundle");
+    await fs.rm(outDir, { recursive: true, force: true });
+    await fs.mkdir(outDir, { recursive: true });
 
-    try {
-      const { build } = await import(tsdownModuleUrl);
-      await build({
-        clean: true,
-        config: false,
-        dts: false,
-        // Full plugin-sdk coverage belongs to `pnpm build`, package contract
-        // guardrails, and `subpaths.test.ts`. This file only keeps the expensive
-        // bundler path honest across representative entrypoint families.
-        entry: buildBundledCoverageEntrySources(),
-        env: { NODE_ENV: "production" },
-        fixedExtension: false,
-        logLevel: "error",
-        outDir,
-        platform: "node",
-      });
-      await fs.symlink(
-        path.join(process.cwd(), "node_modules"),
-        path.join(outDir, "node_modules"),
-        "dir",
-      );
+    const { build } = await import(tsdownModuleUrl);
+    await build({
+      clean: false,
+      config: false,
+      dts: false,
+      // Full plugin-sdk coverage belongs to `pnpm build`, package contract
+      // guardrails, and `subpaths.test.ts`. This file only keeps the expensive
+      // bundler path honest across representative entrypoint families.
+      entry: bundledCoverageEntrySources,
+      env: { NODE_ENV: "production" },
+      fixedExtension: false,
+      logLevel: "error",
+      outDir,
+      platform: "node",
+    });
 
-      expect(pluginSdkEntrypoints.length).toBeGreaterThan(bundledRepresentativeEntrypoints.length);
-      await Promise.all(
-        bundledRepresentativeEntrypoints.map(async (entry) => {
-          await expect(fs.stat(path.join(outDir, `${entry}.js`))).resolves.toBeTruthy();
-        }),
-      );
+    expect(pluginSdkEntrypoints.length).toBeGreaterThan(bundledRepresentativeEntrypoints.length);
+    await Promise.all(
+      bundledRepresentativeEntrypoints.map(async (entry) => {
+        await expect(fs.stat(path.join(outDir, `${entry}.js`))).resolves.toBeTruthy();
+      }),
+    );
 
-      // Export list and package-specifier coverage already live in
-      // package-contract-guardrails.test.ts and subpaths.test.ts. Keep this file
-      // focused on the expensive part: can tsdown emit working bundle artifacts?
-      const importResults = await Promise.all(
-        bundledRepresentativeEntrypoints.map(async (entry) => [
-          entry,
-          typeof (await import(pathToFileURL(path.join(outDir, `${entry}.js`)).href)),
-        ]),
-      );
-      expect(Object.fromEntries(importResults)).toEqual(
-        Object.fromEntries(bundledRepresentativeEntrypoints.map((entry) => [entry, "object"])),
-      );
-    } finally {
-      await fs.rm(outDir, { recursive: true, force: true });
-    }
+    // Export list and package-specifier coverage already live in
+    // package-contract-guardrails.test.ts and subpaths.test.ts. Keep this file
+    // focused on the expensive part: can tsdown emit working bundle artifacts?
+    const importResults = await Promise.all(
+      bundledRepresentativeEntrypoints.map(async (entry) => [
+        entry,
+        typeof (await import(pathToFileURL(path.join(outDir, `${entry}.js`)).href)),
+      ]),
+    );
+    expect(Object.fromEntries(importResults)).toEqual(
+      Object.fromEntries(bundledRepresentativeEntrypoints.map((entry) => [entry, "object"])),
+    );
   });
 });